fc39acf180989c485eb8411db81e7432c2968cf3
Onur Yilmaz <oyilmaz@nvidia.com>
2018-10-16 17:05:45 -0400

    fixed explained variance bug of tsvd.

--- ml-prims/src/stats/stddev.h
+++ ml-prims/src/stats/stddev.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2018, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 
 #include "cuda_utils.h"
@@ -52,6 +68,26 @@ __global__ void stddevKernelColMajor(Type* std, const Type* data, const Type* mu
     }
 }
 
+
+template <typename Type, int TPB>
+__global__ void varsKernelColMajor(Type* var, const Type* data, const Type* mu,
+                                     int D, int N) {
+    typedef cub::BlockReduce<Type, TPB> BlockReduce;
+    __shared__ typename BlockReduce::TempStorage temp_storage;
+    Type thread_data = Type(0);
+    int colStart = blockIdx.x * N;
+    Type m = mu[blockIdx.x];
+    for(int i=threadIdx.x;i<N;i+=TPB) {
+        int idx = colStart + i;
+        Type diff = data[idx] - m;
+        thread_data += diff * diff;
+    }
+    Type acc = BlockReduce(temp_storage).Sum(thread_data);
+    if(threadIdx.x == 0) {
+    	var[blockIdx.x] = acc / N;
+    }
+}
+
 /**
  * @brief Compute stddev of the input matrix
  *
@@ -85,6 +121,44 @@ void stddev(Type* std, const Type* data, const Type* mu, int D, int N,
                          });
     } else {
         stddevKernelColMajor<Type,TPB><<<D,TPB>>>(std, data, mu, D, N);
+    }
+    CUDA_CHECK(cudaPeekAtLastError());
+}
+
+
+/**
+ * @brief Compute variance of the input matrix
+ *
+ * Variance operation is assumed to be performed on a given column.
+ *
+ * @tparam Type the data type
+ * @param var the output stddev vector
+ * @param data the input matrix
+ * @param mu the mean vector
+ * @param D number of columns of data
+ * @param N number of rows of data
+ * @param sample whether to evaluate sample stddev or not. In other words, whether
+ *  to normalize the output using N-1 or N, for true or false, respectively
+ * @param rowMajor whether the input data is row or col major
+ */
+template <typename Type>
+void vars(Type* var, const Type* data, const Type* mu, int D, int N,
+            bool sample, bool rowMajor) {
+    static const int TPB = 256;
+    if(rowMajor) {
+        static const int RowsPerThread = 4;
+        static const int ColsPerBlk = 32;
+        static const int RowsPerBlk = (TPB / ColsPerBlk) * RowsPerThread;
+        dim3 grid(ceildiv(N,RowsPerBlk), ceildiv(D,ColsPerBlk));
+        CUDA_CHECK(cudaMemset(var, 0, sizeof(Type)*D));
+        stddevKernelRowMajor<Type,TPB,ColsPerBlk><<<grid,TPB>>>(var, data, D, N);
+        Type ratio = Type(1) / (sample? Type(N-1) : Type(N));
+        LinAlg::binaryOp(var, var, mu, D,
+                         [ratio] __device__ (Type a, Type b) {
+                             return a * ratio - b * b;
+                         });
+    } else {
+    	varsKernelColMajor<Type,TPB><<<D,TPB>>>(var, data, mu, D, N);
     }
     CUDA_CHECK(cudaPeekAtLastError());
 }
---
+++ ml-prims/src/stats/sum.h
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2018, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "cuda_utils.h"
+#include "linalg/eltwise.h"
+#include <cub/cub.cuh>
+
+
+namespace MLCommon {
+namespace Stats {
+
+
+///@todo: ColsPerBlk has been tested only for 32!
+template <typename Type, int TPB, int ColsPerBlk=32>
+__global__ void sumKernelRowMajor(Type* mu, const Type* data, int D, int N) {
+    const int RowsPerBlkPerIter = TPB / ColsPerBlk;
+    int thisColId = threadIdx.x % ColsPerBlk;
+    int thisRowId = threadIdx.x / ColsPerBlk;
+    int colId = thisColId + (blockIdx.y * ColsPerBlk);
+    int rowId = thisRowId + (blockIdx.x * RowsPerBlkPerIter);
+    Type thread_data = Type(0);
+    const int stride = RowsPerBlkPerIter * gridDim.x;
+    for(int i=rowId;i<N;i+=stride)
+        thread_data += (colId < D)? data[i*D+colId] : Type(0);
+    __shared__ Type smu[ColsPerBlk];
+    if(threadIdx.x < ColsPerBlk)
+        smu[threadIdx.x] = Type(0);
+    __syncthreads();
+    myAtomicAdd(smu+thisColId, thread_data);
+    __syncthreads();
+    if(threadIdx.x < ColsPerBlk)
+        myAtomicAdd(mu+colId, smu[thisColId]);
+}
+
+template <typename Type, int TPB>
+__global__ void sumKernelColMajor(Type* mu, const Type* data, int D, int N) {
+    typedef cub::BlockReduce<Type, TPB> BlockReduce;
+    __shared__ typename BlockReduce::TempStorage temp_storage;
+    Type thread_data = Type(0);
+    int colStart = blockIdx.x * N;
+    for(int i=threadIdx.x;i<N;i+=TPB) {
+        int idx = colStart + i;
+        thread_data += data[idx];
+    }
+    Type acc = BlockReduce(temp_storage).Sum(thread_data);
+    if(threadIdx.x == 0) {
+        mu[blockIdx.x] = acc;
+    }
+}
+
+/**
+ * @brief Compute sum of the input matrix
+ *
+ * Mean operation is assumed to be performed on a given column.
+ *
+ * @tparam Type the data type
+ * @param output the output mean vector
+ * @param input the input matrix
+ * @param D number of columns of data
+ * @param N number of rows of data
+ * @param rowMajor whether the input data is row or col major
+ */
+template <typename Type>
+void sum(Type* output, const Type* input, int D, int N, bool rowMajor) {
+    static const int TPB = 256;
+    if(rowMajor) {
+        static const int RowsPerThread = 4;
+        static const int ColsPerBlk = 32;
+        static const int RowsPerBlk = (TPB / ColsPerBlk) * RowsPerThread;
+        dim3 grid(ceildiv(N,RowsPerBlk), ceildiv(D,ColsPerBlk));
+        CUDA_CHECK(cudaMemset(output, 0, sizeof(Type)*D));
+        sumKernelRowMajor<Type,TPB,ColsPerBlk><<<grid,TPB>>>(output, input, D, N);
+    } else {
+        sumKernelColMajor<Type,TPB><<<D,TPB>>>(output, input, D, N);
+    }
+    CUDA_CHECK(cudaPeekAtLastError());
+}
+
+}; // end namespace Stats
+}; // end namespace MLCommon
