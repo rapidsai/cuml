025b7e19afc0e29ccba49535460c1cba040c691e
Onur Yilmaz <oyilmaz@nvidia.com>
2018-10-30 12:50:17 -0400

    Added the sphinx friendly docstrings to kmeans_wrapper.pyx

--- python/cuML/kmeans/kmeans_wrapper.pyx
+++ python/cuML/kmeans/kmeans_wrapper.pyx
@@ -10,6 +10,67 @@ from c_kmeans cimport *
 
 class KMeans:
 
+    """
+    Create a DataFrame, fill it with data, and compute Kmeans:
+
+    .. code-block:: python
+
+        from cuML import KMeans
+        import pygdf
+        import numpy as np
+        import pandas as pd
+
+        def np2pygdf(df):
+            # convert numpy array to pygdf dataframe 
+            df = pd.DataFrame({'fea%d'%i:df[:,i] for i in range(df.shape[1])})
+            pdf = pygdf.DataFrame()
+            for c,column in enumerate(df):
+            pdf[str(c)] = df[column]
+            return pdf
+
+        
+        a = np.asarray([[1.0, 1.0], [1.0, 2.0], [3.0, 2.0], [4.0, 3.0]],dtype=np.float32)
+        b = np2pygdf(a)
+        print("input:")
+        print(b)
+
+        print("\nCalling fit")
+        kmeans_float = KMeans(n_clusters=2, n_gpu=-1)
+        kmeans_float.fit(b)
+        print("labels:")
+        print(kmeans_float.labels_)
+        print("cluster_centers:")
+        print(kmeans_float.cluster_centers_)
+ 
+
+    Output:
+
+    .. code-block:: python
+
+          input:
+               0    1
+           0  1.0  1.0
+           1  1.0  2.0
+           2  3.0  2.0
+           3  4.0  3.0
+
+          Calling fit
+          labels:
+             0    1
+             1    1
+             2    0
+             3    0
+
+          cluster_centers:
+             0    1
+          0  3.5  2.5
+          1  1.0  1.5
+
+
+    For an additional example see `the PCA notebook <https://github.com/rapidsai/cuml/blob/master/python/notebooks/kmeans_demo.ipynb>`_. For additional docs, see `scikitlearn's Kmeans <http://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html>`_.
+
+    """
+
     def __init__(self, n_clusters=8, max_iter=300, tol=1e-4, verbose=0, random_state=1, precompute_distances='auto', init='kmeans++', n_init=1, algorithm='auto', n_gpu=1, gpu_id=0):
         self.n_clusters = n_clusters
         self.verbose = verbose
@@ -40,6 +101,16 @@ class KMeans:
         c = gdf.as_gpu_matrix(order='C').shape
         return self._get_ctype_ptr(gdf.as_gpu_matrix(order='C'))
 
+
+    """
+    Compute k-means clustering with input_gdf.
+
+    Parameters
+    ----------
+    input_gdf : PyGDF DataFrame
+        Dense matrix (floats or doubles) of shape (n_samples, n_features)
+
+    """
     def fit(self, input_gdf):
         x = []
         for col in input_gdf.columns:
@@ -106,9 +177,28 @@ class KMeans:
 
         return self
 
+    """
+    Compute cluster centers and predict cluster index for each sample.
+
+    Parameters
+    ----------
+    input_gdf : PyGDF DataFrame
+                Dense matrix (floats or doubles) of shape (n_samples, n_features)
+
+    """
     def fit_predict(self, input_gdf):
         return self.fit(input_gdf).labels_
 
+
+    """
+    Predict the closest cluster each sample in input_gdf belongs to.
+
+    Parameters
+    ----------
+    input_gdf : PyGDF DataFrame
+                Dense matrix (floats or doubles) of shape (n_samples, n_features)
+
+    """
     def predict(self, input_gdf):
         x = []
         for col in input_gdf.columns:
@@ -171,6 +261,15 @@ class KMeans:
         return self.labels_
 
 
+    """
+    Transform input_gdf to a cluster-distance space.
+
+    Parameters
+    ----------
+    input_gdf : PyGDF DataFrame
+                Dense matrix (floats or doubles) of shape (n_samples, n_features)
+
+    """
     def transform(self, input_gdf):
         x = []
         for col in input_gdf.columns:
@@ -226,5 +325,14 @@ class KMeans:
         return preds_gdf
 
 
+    """
+    Compute clustering and transform input_gdf to cluster-distance space.
+
+    Parameters
+    ----------
+    input_gdf : PyGDF DataFrame
+                Dense matrix (floats or doubles) of shape (n_samples, n_features)
+
+    """
     def fit_transform(self, input_gdf):
         return self.fit(input_gdf).transform(input_gdf)
