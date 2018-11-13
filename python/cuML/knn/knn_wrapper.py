c99489ada8f41a0ffb93d513aff886e8bd9db704
Dante Gama Dessavre <danteg@nvidia.com>
2018-11-12 21:20:26 -0600

    FIX minor pep8 code corrections

--- python/cuML/knn/knn_wrapper.py
+++ python/cuML/knn/knn_wrapper.py
@@ -1,26 +1,28 @@
- # Copyright (c) 2018, NVIDIA CORPORATION.
- #
- # Licensed under the Apache License, Version 2.0 (the "License");
- # you may not use this file except in compliance with the License.
- # You may obtain a copy of the License at
- #
- #     http://www.apache.org/licenses/LICENSE-2.0
- #
- # Unless required by applicable law or agreed to in writing, software
- # distributed under the License is distributed on an "AS IS" BASIS,
- # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- # See the License for the specific language governing permissions and
- # limitations under the License.
- #
+# Copyright (c) 2018, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 import faiss
 import numpy as np
 import pandas as pd
 import cudf
 
+
 class KNNparams:
-    def __init__(self,n_gpus):
+    def __init__(self, n_gpus):
         self.n_gpus = n_gpus
+
 
 class KNN:
     """
@@ -61,11 +63,11 @@ class KNN:
       n_samples = 3, n_dims = 3
 
       dim_0 dim_1 dim_2
-         
+
       0   1.0   2.0   3.0
       1   1.0   2.0   4.0
       2   2.0   2.0   4.0
-      
+
       # Index:
 
                index_neighbor_0 index_neighbor_1 index_neighbor_2
@@ -82,38 +84,43 @@ class KNN:
     For an additional example see `the KNN notebook <https://github.com/rapidsai/cuml/blob/master/python/notebooks/knn_demo.ipynb>`_. For additional docs, see `scikitlearn's KDtree <http://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KDTree.html#sklearn.neighbors.KDTree>`_.
 
     """
-    def __init__(self, n_gpus=-1): # -1 means using all gpus
+    def __init__(self, n_gpus=-1):
+        # -1 means using all gpus
         self.params = KNNparams(n_gpus)
 
-    def fit(self,X):
-        X = self.to_nparray(X)
-        assert len(X.shape)==2, 'data should be two dimensional'
+    def fit(self, X):
+        if (isinstance(X, cudf.DataFrame)):
+            X = self.to_nparray(X)
+        assert len(X.shape) == 2, 'data should be two dimensional'
         n_dims = X.shape[1]
-        cpu_index = faiss.IndexFlatL2(n_dims) # build a flat (CPU) index
-        if self.params.n_gpus==1:
-            res = faiss.StandardGpuResources()  # use a single GPU
+        cpu_index = faiss.IndexFlatL2(n_dims)
+        # build a flat (CPU) index
+        if self.params.n_gpus == 1:
+            res = faiss.StandardGpuResources()
+            # use a single GPU
             # make it a flat GPU index
             gpu_index = faiss.index_cpu_to_gpu(res, 0, cpu_index)
         else:
-            gpu_index = faiss.index_cpu_to_all_gpus(cpu_index,ngpu=self.params.n_gpus)
+            gpu_index = faiss.index_cpu_to_all_gpus(cpu_index,
+                                                    ngpu=self.params.n_gpus)
         gpu_index.add(X)
         self.gpu_index = gpu_index
 
-    def query(self,X,k):
+    def query(self, X, k):
         X = self.to_nparray(X)
-        D,I = self.gpu_index.search(X, k)
-        D = self.to_cudf(D,col='distance')
-        I = self.to_cudf(I,col='index')
-        return D,I
+        D, I = self.gpu_index.search(X, k)
+        D = self.to_cudf(D, col='distance')
+        I = self.to_cudf(I, col='index')
+        return D, I
 
-    def to_nparray(self,x):
-        if isinstance(x,cudf.DataFrame):
+    def to_nparray(self, x):
+        if isinstance(x, cudf.DataFrame):
             x = x.to_pandas()
         return np.ascontiguousarray(x)
 
-    def to_cudf(self,df,col=''):
+    def to_cudf(self, df, col=''):
         # convert pandas dataframe to cudf dataframe
         if isinstance(df,np.ndarray):
-            df = pd.DataFrame({'%s_neighbor_%d'%(col,i):df[:,i] for i in range(df.shape[1])})
+            df = pd.DataFrame({'%s_neighbor_%d'%(col, i): df[:, i] for i in range(df.shape[1])})
         pdf = cudf.DataFrame.from_pandas(df)
         return pdf
