1484953bd225582bf903d51d39cd4d8881a11471
Dante Gama Dessavre <danteg@nvidia.com>
2018-12-03 12:52:15 -0600

    FEA Added new initial conda recipe scripts

--- conda-recipes/cuml/build.sh
+++ conda-recipes/cuml/build.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-CMAKE_COMMON_VARIABLES=" -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release"
-
-
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
+cd python
+$PYTHON setup.py build_ext --inplace
+$PYTHON setup.py install
