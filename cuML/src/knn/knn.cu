224a5c7c56b87a044ecac99e23211f4f1089179e
Corey J. Nolet <cjnolet@gmail.com>
2018-12-13 18:21:23 -0800

    Calling FAISS API in C++ and using cython to wrap calls within cuml

--- cuML/src/knn/knn.cu
+++ cuML/src/knn/knn.cu
@@ -15,35 +15,23 @@
  */
 
 #include "knn_c.h"
-#include <faiss/IndexFlat.h>
-#include <faiss/gpu/GpuIndexFlat.h>
-#include <faiss/gpu/GpuIndexIVFFlat.h>
-#include <faiss/gpu/StandardGpuResources.h>
 
 namespace ML {
 
-using namespace MLCommon;
 
-class kNN {
-
-    faiss::gpu::GpuIndexFlatL2 index_flat;
-    
-    public:
-
-	kNN::kNN() {}
+	kNN::kNN(int D): index_flat(&res, D) {}
 	kNN::~kNN() {}
 
-        void kNN::fit(float *input, int N, int D) {
+        void kNN::fit(float *input, int N) {
             
-	    faiss::gpu::StandardGpuResources res;
-            this->index_flat(&res, D);
-	    index_flat.add(N, input);
+            this->res.setDefaultNullStreamAllDevices();
+	    this->index_flat.add(N, input);
         }
 
 
         void kNN::search(float *search_items, int search_items_size, long *res_I, float *res_D, int k) {
 	    
-            index_flat.search(search_items_size, search_items, k, res_D, res_I);
+            this->index_flat.search(search_items_size, search_items, k, res_D, res_I);
         }
 
 
