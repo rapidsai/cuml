9f076d33ef08c688bfe85d30f3cb5011acf5c131
Corey J. Nolet <cjnolet@gmail.com>
2018-12-14 14:40:30 -0500

    Removing extra external cutlass release. This is no longer needed.

--- cuML/src/dbscan/vertexdeg/runner.h
+++ cuML/src/dbscan/vertexdeg/runner.h
@@ -16,16 +16,13 @@
 
 #pragma once
 #include "naive.h"
-#include "algo4.h"
 #include "pack.h"
-#include "algo5.h"
 #include "algo6.h"
 
 
 namespace Dbscan {
 namespace VertexDeg {
 
-using namespace cutlass::gemm;
 
 template <typename Type>
 void run(bool* adj, int* vd, Type* x, Type* dots, Type eps, int N, int D,
@@ -33,45 +30,6 @@ void run(bool* adj, int* vd, Type* x, Type* dots, Type eps, int N, int D,
     Pack<Type> data = {vd, adj, x, eps, N, D, dots};
 
     switch(algo) {
-    case 0:
-        Naive::launcher<Type>(data, stream, startVertexId, batchSize);
-        break;
-     case 400:
-         Algo4::launcher<Type, tiling_strategy::Small>(data, stream, startVertexId, batchSize);
-         break;
-     case 401:
-         Algo4::launcher<Type, tiling_strategy::Medium>(data, stream, startVertexId, batchSize);
-         break;
-     case 402:
-         Algo4::launcher<Type, tiling_strategy::Large>(data, stream, startVertexId, batchSize);
-         break;
-     case 403:
-         Algo4::launcher<Type, tiling_strategy::Tall>(data, stream, startVertexId, batchSize);
-         break;
-     case 404:
-         Algo4::launcher<Type, tiling_strategy::Wide>(data, stream, startVertexId, batchSize);
-         break;
-     case 405:
-         Algo4::launcher<Type, tiling_strategy::Huge>(data, stream, startVertexId, batchSize);
-         break;
-     case 500:
-         Algo5::launcher<Type, tiling_strategy::Small>(data, stream, startVertexId, batchSize);
-         break;
-     case 501:
-         Algo5::launcher<Type, tiling_strategy::Medium>(data, stream, startVertexId, batchSize);
-         break;
-    case 502:
-        Algo5::launcher<Type, tiling_strategy::Large>(data, stream, startVertexId, batchSize);
-        break;
-     case 503:
-         Algo5::launcher<Type, tiling_strategy::Tall>(data, stream, startVertexId, batchSize);
-         break;
-     case 504:
-         Algo5::launcher<Type, tiling_strategy::Wide>(data, stream, startVertexId, batchSize);
-         break;
-     case 505:
-         Algo5::launcher<Type, tiling_strategy::Huge>(data, stream, startVertexId, batchSize);
-         break;
     case 600:
     	 Algo6::launcher<Type>(data, stream, startVertexId, batchSize);
     	 break;
