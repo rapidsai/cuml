c8f4d95880280ebb5dcd04b286c1b7ad46492bc8
Corey J. Nolet <cjnolet@gmail.com>
2019-01-07 13:15:51 -0500

    Pushing current state of mean. Tried to use MPI to better parallelize the setting of the GPU device id in the C++ layer but this appeared to have some negative effects on the memory allocations.

--- ml-prims/src/stats/mean.h
+++ ml-prims/src/stats/mean.h
@@ -151,11 +151,6 @@ void meanMG(TypeMG<Type>* mu, const TypeMG<Type>* data, int n_gpus,
 		 */
 		ASSERT(false, "meanMG: row_split not implemented");
 	} else {
-		//TODO: The cost of creating the CUDA context here seems to get quite high from
-		// time to time. Would it make sense to run the following loops in separate threads?
-		#pragma omp parallel for
-
-
 		for (int i = 0; i < n_gpus; i++) {
 			std::cout << "Running on device " << data[i].gpu_id << std::endl;
 			CUDA_CHECK(cudaSetDevice(data[i].gpu_id));
