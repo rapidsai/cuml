222df78afcaf6e31e611cc09340c8e6ae9f43588
Mike Wendt <mike.wendt@me.com>
2019-01-28 09:31:30 -0500

    ENH Match conda dirs with cuDF

--- ci/cpu/libcuml/build_libcuml.sh
+++ ci/cpu/libcuml/build_libcuml.sh
@@ -4,5 +4,5 @@ set -e
 
 if [ "$BUILD_LIBCUML" == '1' -o "$BUILD_CUML" == '1' ]; then
   echo "Building libcuml"
-  conda build conda-recipes/libcuml -c nvidia -c rapidsai -c numba -c pytorch -c conda-forge -c defaults --python=${PYTHON}
+  conda build conda/recipes/libcuml -c nvidia -c rapidsai -c numba -c pytorch -c conda-forge -c defaults --python=${PYTHON}
 fi
