8ae59baae44a038a99fa851943d35674e18cf45c
Mike Wendt <mike.wendt@me.com>
2019-01-29 00:51:27 -0800

    FIX Set builds to pull based on cuda label

--- travisci/cuml/build_cuml.sh
+++ travisci/cuml/build_cuml.sh
@@ -4,5 +4,10 @@ set -e
 
 if [ "$BUILD_CUML" == '1' ]; then
     echo "Building cuML"
-    conda build conda-recipes/cuml -c defaults -c conda-forge -c numba -c rapidsai -c nvidia -c pytorch --python=${PYTHON}
+    CUDA_REL=${CUDA:0:3}
+    if [ "${CUDA:0:2}" == '10' ]; then
+        # CUDA 10 release
+        CUDA_REL=${CUDA:0:4}
+    fi
+    conda build conda-recipes/cuml -c defaults -c conda-forge -c numba -c rapidsai/label/cuda${CUDA_REL} -c nvidia/label/cuda${CUDA_REL} -c pytorch --python=${PYTHON}
 fi
