bd075342a6b8fa1f29dea3d0a03d927f08f0ba3d
Mike Wendt <mike.wendt@me.com>
2019-01-29 09:29:03 -0800

    FIX Pull packages with correct CUDA label

--- ci/cpu/cuml/build_cuml.sh
+++ ci/cpu/cuml/build_cuml.sh
@@ -4,5 +4,5 @@ set -e
 
 if [ "$BUILD_CUML" == '1' ]; then
     echo "Building cuML"
-    conda build conda/recipes/cuml -c defaults -c conda-forge -c numba -c rapidsai/label/branch-0.5-cuda${CUDA} -c nvidia -c pytorch --python=${PYTHON}
+    conda build conda/recipes/cuml -c defaults -c conda-forge -c numba -c rapidsai/label/cuda${CUDA} -c nvidia/label/cuda${CUDA} -c pytorch --python=${PYTHON}
 fi
