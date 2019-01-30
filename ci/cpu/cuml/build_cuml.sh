1568c6172a99a50ef97644971851f93991aebd6b
Mike Wendt <mike.wendt@me.com>
2019-01-29 16:37:43 -0800

    FIX Set defaults as last channel

--- ci/cpu/cuml/build_cuml.sh
+++ ci/cpu/cuml/build_cuml.sh
@@ -11,8 +11,8 @@ if [ "$BUILD_CUML" == '1' ]; then
     fi
 
     if [ "$BUILD_ABI" == "1" ]; then
-        conda build conda/recipes/cuml -c defaults -c conda-forge -c numba -c rapidsai/label/cuda${CUDA_REL} -c nvidia/label/cuda${CUDA_REL} -c pytorch --python=${PYTHON}
+        conda build conda/recipes/cuml -c conda-forge -c numba -c rapidsai/label/cuda${CUDA_REL} -c nvidia/label/cuda${CUDA_REL} -c pytorch -c defaults  --python=${PYTHON}
     else
-        conda build conda/recipes/cuml -c defaults -c conda-forge/label/cf201901 -c numba -c rapidsai/label/cf201901-cuda${CUDA_REL} -c nvidia/label/cf201901-cuda${CUDA_REL} -c pytorch --python=${PYTHON}
+        conda build conda/recipes/cuml -c conda-forge/label/cf201901 -c numba -c rapidsai/label/cf201901-cuda${CUDA_REL} -c nvidia/label/cf201901-cuda${CUDA_REL} -c pytorch -c defaults --python=${PYTHON}
     fi
 fi
