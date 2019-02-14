26de38d01b142b061e8b202da034ee8140255fc3
Dante Gama Dessavre <danteg@nvidia.com>
2019-02-14 12:49:28 -0600

    FEA Added dbscan stress test file with dummy test

---
+++ python/cuML/test/test_dbscan_stress.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2018, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import pytest
+from cuml import DBSCAN as cuDBSCAN
+from sklearn.cluster import DBSCAN as skDBSCAN
+import cudf
+import numpy as np
+
+
+@pytest.mark.stress
+@pytest.mark.parametrize('datatype', [np.float32, np.float64])
+@pytest.mark.parametrize('input_type', ['dataframe', 'ndarray'])
+def test_dbscan_stress_dummy(datatype, input_type):
+
+    X = np.array([[1, 2], [2, 2], [2, 3], [8, 7], [8, 8], [25, 80]],
+                 dtype=datatype)
+    skdbscan = skDBSCAN(eps=3, min_samples=2)
+    sk_labels = skdbscan.fit_predict(X)
+
+    cudbscan = cuDBSCAN(eps=3, min_samples=2)
+
+    if input_type == 'dataframe':
+        gdf = cudf.DataFrame()
+        gdf['0'] = np.asarray([1, 2, 2, 8, 8, 25], dtype=datatype)
+        gdf['1'] = np.asarray([2, 2, 3, 7, 8, 80], dtype=datatype)
+        cu_labels = cudbscan.fit_predict(gdf)
+    else:
+        cu_labels = cudbscan.fit_predict(X)
+
+    for i in range(X.shape[0]):
+        assert cu_labels[i] == sk_labels[i]
