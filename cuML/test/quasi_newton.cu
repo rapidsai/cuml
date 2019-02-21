0b92dd697bf6d05327bce650865265d3f934d344
Young-Jun Ko <youngjun.ko@gmail.com>
2019-02-21 09:13:18 -0800

    test cleanup

--- cuML/test/quasi_newton.cu
+++ cuML/test/quasi_newton.cu
@@ -1,10 +1,11 @@
-#include <gtest/gtest.h>
-
+#include "test_utils.h"
 #include <glm/glm_linear.h>
 #include <glm/glm_logistic.h>
 #include <glm/glm_softmax.h>
 #include <glm/lbfgs.h>
+#include <gtest/gtest.h>
 #include <linalg/transpose.h>
+#include <vector>
 
 using namespace ML;
 using namespace ML::GLM;
@@ -12,8 +13,7 @@ using namespace ML::GLM;
 template <typename T, typename LossFunction, STORAGE_ORDER Storage = COL_MAJOR>
 int qn_fit(LossFunction *loss, T *Xptr, T *yptr, T *zptr, int N, bool has_bias,
            T l1, T l2, int max_iter, T grad_tol, T value_rel_tol,
-           int linesearch_max_iter, int lbfgs_memory, int verbosity,
-           T *w0, // initial value and result
+           int linesearch_max_iter, int lbfgs_memory, int verbosity, T *w0,
            T *fx, int *num_iters);
 
 namespace ML {
@@ -22,11 +22,27 @@ namespace GLM {
 using namespace MLCommon;
 
 struct QuasiNewtonTest : ::testing::Test {
+  static constexpr int N = 10;
+  static constexpr int D = 2;
+  const static double tol;
+  const static double X[N][D];
   cublasHandle_t cublas;
   void SetUp() { cublasCreate(&cublas); }
-
   void TearDown() {}
 };
+
+const double QuasiNewtonTest::tol = 1e-6;
+const double QuasiNewtonTest::X[QuasiNewtonTest::N][QuasiNewtonTest::D] = {
+    {-0.2047076594847130, 0.4789433380575482},
+    {-0.5194387150567381, -0.5557303043474900},
+    {1.9657805725027142, 1.3934058329729904},
+    {0.0929078767437177, 0.2817461528302025},
+    {0.7690225676118387, 1.2464347363862822},
+    {1.0071893575830049, -1.2962211091122635},
+    {0.2749916334321240, 0.2289128789353159},
+    {1.3529168351654497, 0.8864293405915888},
+    {-2.0016373096603974, -0.3718425371402544},
+    {1.6690253095248706, -0.4385697358355719}};
 
 struct InputSpec {
   int n_row;
@@ -50,7 +66,7 @@ template <class T> struct DevUpload {
 
 template <typename T, class LossFunction>
 T run(LossFunction &loss, DevUpload<T> &devUpload, InputSpec &in, T l1, T l2,
-      T *w, SimpleVec<T> &z, int verbosity=0) {
+      T *w, SimpleVec<T> &z, int verbosity = 0) {
 
   int max_iter = 100;
   T grad_tol = 1e-8;
@@ -71,18 +87,8 @@ T run(LossFunction &loss, DevUpload<T> &devUpload, InputSpec &in, T l1, T l2,
 }
 
 TEST_F(QuasiNewtonTest, binary_logistic_vs_sklearn) {
-
+  CompareApprox<double> compApprox(tol);
   // Test case generated in python and solved with sklearn
-  double X[10][2] = {{-0.2047076594847130, 0.4789433380575482},
-                     {-0.5194387150567381, -0.5557303043474900},
-                     {1.9657805725027142, 1.3934058329729904},
-                     {0.0929078767437177, 0.2817461528302025},
-                     {0.7690225676118387, 1.2464347363862822},
-                     {1.0071893575830049, -1.2962211091122635},
-                     {0.2749916334321240, 0.2289128789353159},
-                     {1.3529168351654497, 0.8864293405915888},
-                     {-2.0016373096603974, -0.3718425371402544},
-                     {1.6690253095248706, -0.4385697358355719}};
   double y[10] = {1, 1, 1, 0, 1, 0, 1, 0, 1, 0};
 
   InputSpec in;
@@ -105,9 +111,7 @@ TEST_F(QuasiNewtonTest, binary_logistic_vs_sklearn) {
   double obj_l1_b = 0.44295941481024703;
 
   fx = run(loss_b, devUpload, in, alpha, 0.0, w0.data, z);
-
-  w0.print();
-  printf("Ref=%f, %f\n", obj_l1_b, fx);
+  ASSERT_TRUE(compApprox(obj_l1_b, fx));
 
   in.fit_intercept = true;
   double w_l2_b[2] = {-1.5339880402781370, 1.6788639581350926};
@@ -116,38 +120,26 @@ TEST_F(QuasiNewtonTest, binary_logistic_vs_sklearn) {
 
   fx = run(loss_b, devUpload, in, 0.0, alpha, w0.data, z);
 
-  w0.print();
-  printf("Ref=%f, %f\n", obj_l2_b, fx);
+  ASSERT_TRUE(compApprox(obj_l2_b, fx));
 
   in.fit_intercept = false;
   double w_l1_no_b[2] = {-1.6215035298864591, 2.3650868394981086};
   double obj_l1_no_b = 0.4769896009200278;
 
   fx = run(loss_no_b, devUpload, in, alpha, 0.0, w0.data, z);
-  w0.print();
-  printf("Ref=%f, %f\n", obj_l1_no_b, fx);
+  ASSERT_TRUE(compApprox(obj_l1_no_b, fx));
 
   in.fit_intercept = false;
   double w_l2_no_b[2] = {-1.3931049893764620, 2.0140103094119621};
   double obj_l2_no_b = 0.47502098062114273;
 
   fx = run(loss_no_b, devUpload, in, 0.0, alpha, w0.data, z);
-  w0.print();
-  printf("Ref=%f, %f\n", obj_l2_no_b, fx);
+  ASSERT_TRUE(compApprox(obj_l2_no_b, fx));
 }
 
 TEST_F(QuasiNewtonTest, multiclass_logistic_vs_sklearn) {
 
-  double X[10][2] = {{-0.2047076594847130, 0.4789433380575482},
-                     {-0.5194387150567381, -0.5557303043474900},
-                     {1.9657805725027142, 1.3934058329729904},
-                     {0.0929078767437177, 0.2817461528302025},
-                     {0.7690225676118387, 1.2464347363862822},
-                     {1.0071893575830049, -1.2962211091122635},
-                     {0.2749916334321240, 0.2289128789353159},
-                     {1.3529168351654497, 0.8864293405915888},
-                     {-2.0016373096603974, -0.3718425371402544},
-                     {1.6690253095248706, -0.4385697358355719}};
+  CompareApprox<double> compApprox(tol);
   double y[10] = {2, 2, 0, 3, 3, 0, 0, 0, 1, 0};
 
   double fx, l1, l2;
@@ -179,8 +171,7 @@ TEST_F(QuasiNewtonTest, multiclass_logistic_vs_sklearn) {
   double obj_l1_b = 0.5407911382311313;
 
   fx = run(loss_b, devUpload, in, l1, l2, w0.data, z);
-  printf("Ref: %f, %f\n", obj_l1_b, fx);
-  w0.print();
+  ASSERT_TRUE(compApprox(obj_l1_b, fx));
 
   l1 = 0.0;
   l2 = alpha;
@@ -194,9 +185,7 @@ TEST_F(QuasiNewtonTest, multiclass_logistic_vs_sklearn) {
   double obj_l2_b = 0.5721784062720949;
 
   fx = run(loss_b, devUpload, in, l1, l2, w0.data, z);
-  printf("Ref: %f, %f\n", obj_l2_b, fx);
-
-  w0.print();
+  ASSERT_TRUE(compApprox(obj_l2_b, fx));
 
   l1 = alpha;
   l2 = 0.0;
@@ -208,9 +197,8 @@ TEST_F(QuasiNewtonTest, multiclass_logistic_vs_sklearn) {
   double obj_l1_no_b = 0.6606929813245878;
 
   fx = run(loss_no_b, devUpload, in, l1, l2, w0.data, z);
-  printf("Ref: %f, %f\n", obj_l1_no_b, fx);
+  ASSERT_TRUE(compApprox(obj_l1_no_b, fx));
 
-  w0.print();
   l1 = 0.0;
   l2 = alpha;
   in.fit_intercept = false;
@@ -221,21 +209,12 @@ TEST_F(QuasiNewtonTest, multiclass_logistic_vs_sklearn) {
   double obj_l2_no_b = 0.6597171282106854;
 
   fx = run(loss_no_b, devUpload, in, l1, l2, w0.data, z);
-  printf("Ref: %f, %f\n", obj_l2_no_b, fx);
-  w0.print();
+  ASSERT_TRUE(compApprox(obj_l2_no_b, fx));
 }
 
 TEST_F(QuasiNewtonTest, linear_regression_vs_sklearn) {
-  double X[10][2] = {{-0.2047076594847130, 0.4789433380575482},
-                     {-0.5194387150567381, -0.5557303043474900},
-                     {1.9657805725027142, 1.3934058329729904},
-                     {0.0929078767437177, 0.2817461528302025},
-                     {0.7690225676118387, 1.2464347363862822},
-                     {1.0071893575830049, -1.2962211091122635},
-                     {0.2749916334321240, 0.2289128789353159},
-                     {1.3529168351654497, 0.8864293405915888},
-                     {-2.0016373096603974, -0.3718425371402544},
-                     {1.6690253095248706, -0.4385697358355719}};
+
+  CompareApprox<double> compApprox(tol);
   double y[10] = {0.2675836026202781,  -0.0678277759663704, -0.6334027174275105,
                   -0.1018336189077367, 0.0933815935886932,  -1.1058853496996381,
                   -0.1658298189619160, -0.2954290675648911, 0.7966520536712608,
@@ -257,21 +236,19 @@ TEST_F(QuasiNewtonTest, linear_regression_vs_sklearn) {
   l2 = 0.0;
   double w_l1_b[2] = {-0.4952397281519840, 0.3813315300180231};
   double b_l1_b = -0.08140861819001188;
-  double obj_l1_b=0.011136986298775138;
+  double obj_l1_b = 0.011136986298775138;
   fx = run(loss_b, devUpload, in, l1, l2, w0.data, z);
-  printf("Ref: %f, %f\n", obj_l1_b, fx);
-  w0.print();
+  ASSERT_TRUE(compApprox(obj_l1_b, fx));
 
   in.fit_intercept = true;
   l1 = 0.0;
   l2 = alpha;
   double w_l2_b[2] = {-0.5077686639114126, 0.4016402760929909};
   double b_l2_b = -0.0796525493999091;
-double obj_l2_b=0.004268621967866347;
-  
+  double obj_l2_b = 0.004268621967866347;
+
   fx = run(loss_b, devUpload, in, l1, l2, w0.data, z);
-  printf("Ref: %f, %f\n", obj_l2_b, fx);
-  w0.print();
+  ASSERT_TRUE(compApprox(obj_l2_b, fx));
 
   in.fit_intercept = false;
   l1 = alpha;
@@ -280,18 +257,16 @@ double obj_l2_b=0.004268621967866347;
   double obj_l1_no_b = 0.013981355746112447;
 
   fx = run(loss_no_b, devUpload, in, l1, l2, w0.data, z);
-  printf("Ref: %f, %f\n", obj_l1_no_b, fx);
-  w0.print();
+  ASSERT_TRUE(compApprox(obj_l1_no_b, fx));
 
   in.fit_intercept = false;
   l1 = 0.0;
   l2 = alpha;
   double w_l2_no_b[2] = {-0.5295526023453346, 0.3925980845025058};
-  double obj_l2_no_b=0.007061261366969662;
+  double obj_l2_no_b = 0.007061261366969662;
 
   fx = run(loss_no_b, devUpload, in, l1, l2, w0.data, z);
-  printf("Ref: %f, %f\n", obj_l2_no_b, fx);
-  w0.print();
+  ASSERT_TRUE(compApprox(obj_l2_no_b, fx));
 }
 
 } // namespace GLM
