9fe00966688518e077f28422086c76c4d2a70ce3
Corey J. Nolet <cjnolet@gmail.com>
2019-02-22 17:44:51 -0500

    Adding get_device() for verifying the device of a pointer (when it's not the currently selected device.)

--- python/setup.py
+++ python/setup.py
@@ -17,6 +17,8 @@
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
 from Cython.Build import cythonize
+import os
+from os.path import join as pjoin
 import versioneer
 from distutils.sysconfig import get_python_lib
 
@@ -25,6 +27,49 @@ install_requires = [
     'numpy',
     'cython'
 ]
+
+def find_in_path(name, path):
+    "Find a file in a search path"
+    for dir in path.split(os.pathsep):
+        binpath = pjoin(dir, name)
+        if os.path.exists(binpath):
+            return os.path.abspath(binpath)
+    return None
+
+
+def locate_cuda():
+    """Locate the CUDA environment on the system
+    Returns a dict with keys 'home', 'nvcc', 'include', and 'lib64'
+    and values giving the absolute path to each directory.
+    Starts by looking for the CUDAHOME env variable. If not found, everything
+    is based on finding 'nvcc' in the PATH.
+    """
+
+    # first check if the CUDAHOME env variable is in use
+    if 'CUDAHOME' in os.environ:
+        home = os.environ['CUDAHOME']
+        nvcc = pjoin(home, 'bin', 'nvcc')
+    else:
+        # otherwise, search the PATH for NVCC
+        nvcc = find_in_path('nvcc', os.environ['PATH'])
+        if nvcc is None:
+            raise EnvironmentError('The nvcc binary could not be '
+                                   'located in your $PATH.')
+
+        home = os.path.dirname(os.path.dirname(nvcc))
+
+    cudaconfig = {'home': home, 'nvcc': nvcc,
+                  'include': pjoin(home, 'include'),
+                  'lib64': pjoin(home, 'lib64')}
+    for k, v in cudaconfig.items():
+        if not os.path.exists(v):
+            raise EnvironmentError('The CUDA %s path could not '
+                                   'be located in %s' % (k, v))
+
+    return cudaconfig
+
+
+CUDA = locate_cuda()
 
 cython_files = ['cuML/cuml.pyx']
 
@@ -37,8 +82,9 @@ extensions = [
                             '../cuML/external/ml-prims/external/cutlass',
                             '../cuML/external/cutlass',
                             '../cuML/external/ml-prims/external/cub',
-                            '/usr/local/cuda/include'],
+                            CUDA["include"]],
               library_dirs=[get_python_lib()],
+              runtime_library_dirs=[CUDA["lib64"]],
               libraries=['cuda', 'cuml'],
               language='c++',
               extra_compile_args=['-std=c++11'])
