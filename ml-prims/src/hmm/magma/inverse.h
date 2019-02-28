e2e28f929a0f3c9a5d469f345bdd1087f935462b
MehdiAbbanaBennani <mehdi-b-@live.fr>
2019-02-28 17:33:27 -0600

    Add Inverse Googletests and refactor

---
+++ ml-prims/src/hmm/magma/inverse.h
@@ -0,0 +1,68 @@
+#include "hmm/magma/magma_test_utils.h"
+#include "hmm/magma/magma_batched_wrappers.h"
+
+using namespace MLCommon::LinAlg;
+
+// TODO : ADD batched cublas
+// Using cublas for large batch sizes and magma otherwise
+// https://github.com/pytorch/pytorch/issues/13546
+
+namespace MLCommon {
+
+
+template <typename T>
+__global__ void ID_kernel (int n, T *A, int ldda) {
+        int j = threadIdx.x + blockDim.x * blockIdx.x;
+        int i = threadIdx.y + blockDim.y * blockIdx.y;
+        if (i < n && j < n)
+        {
+                if (i == j)
+                        A[IDX(i, j, ldda)] = 1.0;
+                else
+                        A[IDX(i, j, ldda)] = 0.0;
+        }
+}
+
+
+template <typename T>
+void make_ID_matrix(int n, T *A, int ldda) {
+        dim3 block(32,32);
+        dim3 grid(ceildiv(n, (int)block.x), ceildiv(n, (int)block.y));
+        ID_kernel<T> <<< grid, block >>>(n, A, ldda);
+        cudaDeviceSynchronize();
+        CUDA_CHECK(cudaPeekAtLastError());
+}
+
+template <typename T>
+void inverse_batched_magma(magma_int_t n, T** dA_array, magma_int_t ldda,
+                           T**& dinvA_array, magma_int_t batchCount,
+                           magma_queue_t queue){
+
+        int **dipiv_array, *info_array;
+        T **dA_array_cpy;
+        allocate_pointer_array(dipiv_array, n, batchCount);
+        allocate_pointer_array(dA_array_cpy, ldda * n, batchCount);
+        allocate(info_array, batchCount);
+        copy_batched(batchCount, dA_array_cpy, dA_array, ldda * n);
+
+        magma_getrf_batched(n, n, dA_array_cpy, ldda, dipiv_array, info_array,
+                            batchCount, queue);
+        assert_batched(batchCount, info_array);
+
+        magma_getri_outofplace_batched(n, dA_array_cpy, ldda, dipiv_array,
+                                       dinvA_array, ldda, info_array,
+                                       batchCount, queue);
+        assert_batched(batchCount, info_array);
+
+        free_pointer_array(dipiv_array, batchCount);
+        free_pointer_array(dA_array_cpy, batchCount);
+        CUDA_CHECK(cudaFree(info_array));
+}
+
+template <typename T>
+void inverse_batched(magma_int_t n, T** dA_array, magma_int_t ldda,
+                     T** dinvA_array, magma_int_t batchCount, magma_queue_t queue){
+        inverse_batched_magma(n, dA_array, ldda, dinvA_array, batchCount, queue);
+
+}
+}
