b10066b13a1b6b889cac2b9b4cf86e04638ddfbb
Vishal Mehta (Content & Technology) <vishalm@nvidia.com>
2019-02-28 12:07:44 +0100

    single channel histogram added based on cub

--- cuML/src/decisiontree/error_handler.h
+++ cuML/src/decisiontree/error_handler.h
@@ -14,11 +14,14 @@
  * limitations under the License.
  */
 
-#include "histogram/histogram.cuh"
-#include "error_handler.h"
+#pragma once
 
-namespace ML {
-
-  
-};
-// end namespace ML
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+  if (code != cudaSuccess)
+    {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+    }
+}
---
+++ cuML/src/decisiontree/histogram/histogram.cuh
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2018, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+#include "cub/cub.cuh"
+#include "../error_handler.h"
+
+namespace DecisionTree {
+  
+  void histogram(float *d_samples, int *d_histogram, int num_levels,float lower_level,float upper_level,int num_samples)
+  {
+    void*    d_temp_storage = NULL;
+    size_t   temp_storage_bytes = 0;
+    
+    //first call to compute temp storage, no kernel launched
+    gpuErrchk(cub::DeviceHistogram::HistogramEven(d_temp_storage, temp_storage_bytes,
+						  d_samples, d_histogram, num_levels, lower_level, upper_level, num_samples));  
+    // Allocate temporary storage
+    gpuErrchk(cudaMalloc((void**)&d_temp_storage, temp_storage_bytes));
+    gpuErrchk(cub::DeviceHistogram::HistogramEven(d_temp_storage, temp_storage_bytes,
+						  d_samples, d_histogram, num_levels, lower_level, upper_level, num_samples));
+    gpuErrchk(cudaFree(d_temp_storage));
+    
+  }
+  
+}// end namespace DecisionTree
