901eb35fd9bbf5c57be113f75ba0047d5f4e051e
Corey J. Nolet <cjnolet@gmail.com>
2019-03-01 13:11:10 -0500

    Adding wrapper for nvgraph spectral clustering for now. Will replace with a more suitable solver later.

--- cuML/src/umap/init_embed/spectral_algo.h
+++ cuML/src/umap/init_embed/spectral_algo.h
@@ -16,6 +16,9 @@
 
 #include "umap/umapparams.h"
 
+#include <nvgraph.h>
+#include <cusparse_v2.h>
+
 #pragma once
 
 namespace UMAPAlgo {
@@ -26,6 +29,13 @@ namespace UMAPAlgo {
 
             using namespace ML;
 
+            void check(nvgraphStatus_t status) {
+                if (status != NVGRAPH_STATUS_SUCCESS) {
+                    printf("ERROR : %d\n",status);
+                    exit(0);
+                }
+            }
+
             template<typename T>
             void launcher(const T *X, int n, int d,
                           const long *knn_indices, const T *knn_dists,
@@ -34,8 +44,45 @@ namespace UMAPAlgo {
 
                 /**
                  * Calculate the eigenvectors (ordered by eigenvalue)
-                 * of the normalized laplacian
+                 * of the normalized laplacian from the 1-skeleton
                  */
+
+                float *csrValA_h = (float*)malloc(sizeof(float));
+                int *csrRowPtrA_h = (int*)malloc(sizeof(int));
+                int *csrColIndA_h = (int*)malloc(sizeof(int));
+
+                const size_t nnz = 1, n_ev = params->n_components, edge_numsets = 1;
+
+                int weight_index = 0, *clustering_h;
+                float *eigVals_h, *eigVecs_h;
+
+                nvgraphHandle_t handle;
+                nvgraphGraphDescr_t graph;
+                cudaDataType_t edge_dimT = CUDA_R_32F;
+                nvgraphCSRTopology32I_st CSR_input = {n, nnz, csrRowPtrA_h, csrColIndA_h};
+
+                // Allocate host data for nvgraphSpectralClustering output
+                clustering_h = (int*)malloc(n*sizeof(int));
+                eigVals_h = (float*)malloc(n_ev*sizeof(float));
+                eigVecs_h = (float*)malloc(n_ev*n*sizeof(float));
+
+                // Spectral clustering parameters
+                struct SpectralClusteringParameter clustering_params;
+                clustering_params.n_clusters = n_ev;
+                clustering_params.n_eig_vects = n_ev;
+                clustering_params.algorithm = NVGRAPH_MODULARITY_MAXIMIZATION;
+                clustering_params.evs_tolerance = 0.0f;
+                clustering_params.evs_max_iter = 0;
+                clustering_params.kmean_tolerance = 0.0f;
+                clustering_params.kmean_max_iter = 0;
+
+                check(nvgraphCreate (&handle));
+                check(nvgraphCreateGraphDescr(handle, &graph));
+                check(nvgraphSetGraphStructure(handle, graph, (void*)&CSR_input, NVGRAPH_CSR_32));
+                check(nvgraphAllocateEdgeData(handle, graph, edge_numsets, &edge_dimT));
+                check(nvgraphSetEdgeData(handle, graph, (void*)csrValA_h, 0));
+
+                check(nvgraphSpectralClustering(handle, graph, weight_index, &clustering_params, clustering_h, eigVals_h, eigVecs_h));
 
             }
         }
