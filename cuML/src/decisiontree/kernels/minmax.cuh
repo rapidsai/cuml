199c4b49c88f77fea0978ba61fd8d6a963c1f5dd
Vishal Mehta (Content & Technology) <vishalm@nvidia.com>
2019-03-01 16:18:36 +0100

    best fruit calculator complete

---
+++ cuML/src/decisiontree/kernels/minmax.cuh
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2018, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+#include <utils.h>
+#include <thrust/extrema.h>
+
+float minimum(float *d_samples, int num_samples)
+{
+  float minval;
+  float *min_ptr = thrust::min_element(thrust::device,d_samples,d_samples + num_samples);
+  CUDA_CHECK(cudaMemcpy(&minval,min_ptr,sizeof(float),cudaMemcpyDeviceToHost));
+  return minval;
+}
+
+float maximum(float *d_samples, int num_samples)
+{
+  float maxval;
+  float *max_ptr = thrust::max_element(thrust::device,d_samples,d_samples + num_samples);
+  CUDA_CHECK(cudaMemcpy(&maxval,max_ptr,sizeof(float),cudaMemcpyDeviceToHost));
+  return maxval;
+}
