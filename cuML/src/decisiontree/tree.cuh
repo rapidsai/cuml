a18638d6456b5eea98d5cbf3d387e21c04b79630
Vishal Mehta (Content & Technology) <vishalm@nvidia.com>
2019-03-04 10:26:38 +0100

    tabbing

--- cuML/src/decisiontree/tree.cuh
+++ cuML/src/decisiontree/tree.cuh
@@ -25,151 +25,151 @@
 #include <numeric>
 
 namespace ML {
-  namespace DecisionTree {
-
-    struct Question
-    {
-      int column;
-      float value;
-    };
-
-    struct LeafNode
-    {
-      int class_predict;
-    };
-    
-    struct TreeNode
-    {
-      TreeNode *left = NULL;
-      TreeNode *right = NULL;
-      LeafNode *leaf = NULL;  
-      Question question;
-    };
-    
-    class DecisionTreeClassifier
-    {
-    public:
-      TreeNode *root = NULL;
-      unsigned long long *mask;
-      const int nbins = 8;
-      
-      void fit(float *data,const int ncols,const int nrows,const float colper,int *labels,unsigned long long* mask = NULL)
-      {
-	return plant(data,ncols,nrows,colper,labels,mask);
-      }
-      
-      void plant(float *data,const int ncols,const int nrows,const float colper,int *labels,unsigned long long* mask = NULL)
-      {
-	bool allocmaskflag = False;
-	if(mask == NULL)
-	  {
-	    CUDA_CHECK(cudaMalloc((void**)&mask,nrows*sizeof(unsigned long long)));
-	    CUDA_CHECK(cudaMemset(mask,0,nrows*sizeof(unsigned int)));
-	    allocmaskflag = True;
-	  }
+	namespace DecisionTree {
+		
+		struct Question
+		{
+			int column;
+			float value;
+		};
+		
+		struct LeafNode
+		{
+			int class_predict;
+		};
+		
+		struct TreeNode
+		{
+			TreeNode *left = NULL;
+			TreeNode *right = NULL;
+			LeafNode *leaf = NULL;  
+			Question question;
+		};
+		
+		class DecisionTreeClassifier
+		{
+		public:
+			TreeNode *root = NULL;
+			unsigned long long *mask;
+			const int nbins = 8;
+			
+			void fit(float *data,const int ncols,const int nrows,const float colper,int *labels,unsigned long long* mask = NULL)
+			{
+				return plant(data,ncols,nrows,colper,labels,mask);
+			}
+			
+			void plant(float *data,const int ncols,const int nrows,const float colper,int *labels,unsigned long long* mask = NULL)
+			{
+				bool allocmaskflag = False;
+				if(mask == NULL)
+					{
+						CUDA_CHECK(cudaMalloc((void**)&mask,nrows*sizeof(unsigned long long)));
+						CUDA_CHECK(cudaMemset(mask,0,nrows*sizeof(unsigned int)));
+						allocmaskflag = True;
+					}
+				
+				root = grow_tree(data,ncols,nrows,colper,labels,mask,0);
+				
+				if(allocmaskflag)
+					CUDA_CHECK(cudaFree(mask));
+				
+				return;
+			}
+			
+			TreeNode* grow_tree(float *data,const int ncols,const int nrows,const float colper,int *labels,unsigned long long *mask,int depth)
+			{
+				TreeNode *node = new TreeNode();
+				Question ques;
+				float gain = 0.0;
+				
+				find_best_fruit(data,labels,ncols,nrows,colper,ques,gain,mask);  //ques and gain are output here
+				if(gain == 0.0)
+					{
+						
+					}
+				else
+					{
+						int nrowsleft,nrowsright;
+						split_branch(data,labels,ques,nrows,ncols,nrowsleft,nrowsright,mask,depth);
+					}
+				return node;
+			}
+			
+			void find_best_fruit(float *data,int *labels,const int ncols,const int nrows,const float colper,Question& ques,float& gain,unsigned long long* mask)
+			{
+				float maxinfo = 0.0;
+				int splitcol;
+				float splitval;
+				float ginibefore = gini(labels,nrows);
+				
+				// Bootstrap columns
+				std::vector<int> colselector(ncols);
+				std::iota(colselector.begin(),colselector.end(),0);
+				std::random_shuffle(colselector.begin(),colselector.end());
+				colselector.resize((int)(colper*ncols));
+				
+				int *leftlabels, *rightlabels;
+				CUDA_CHECK(cudaMalloc((void**)&leftlabels,nrows*sizeof(int)));
+				CUDA_CHECK(cudaMalloc((void**)&rightlabels,nrows*sizeof(int)));
+				
+				for(int i=0;i<colselector.size();i++)
+					{
+						float *colptr = &data[nrows*colselector[i]];
+						
+						float min = minimum(colptr,nrows);
+						float max = maximum(colptr,nrows);
+						float delta = (max - min)/ nbins ;
+						
+						for(int j=1;j<nbins;j++)
+							{
+								float quesval = min + delta*j;
+								float info_gain = evaluate_split(colptr,labels,leftlabels,rightlabels,ginibefore,quesval,nrows);
+								if(info_gain == -1)
+									continue;
+								
+								if(info_gain > maxinfo)
+									{
+										maxinfo = info_gain;
+										splitval = quesval;
+										splitcol = colselector[i];
+									}
+							}
+						
+					}
+				
+				ques.column = splitcol;
+				ques.value = splitval;
+				gain = maxinfo;
+				
+				CUDA_CHECK(cudaFree(leftlabels));
+				CUDA_CHECK(cudaFree(rightlabels));
+				
+			}
+			
+			float evaluate_split(float *column,int* labels,int* leftlabels,int* rightlabels,float ginibefore,float quesval,const int nrows)
+			{
+				int lnrows,rnrows;
+				
+				split_labels(column,labels,leftlabels,rightlabels,nrows,lnrows,rnrows,quesval);
+				
+				if(lnrows == 0 || rnrows == 0)
+					return -1;
+				
+				float ginileft = gini(leftlabels,lnrows);
+				float giniright = gini(rightlabels,rnrows);
+				
+				float impurity = (lnrows/nrows) * ginileft + (rnrows/nrows) * giniright;
+				return (ginibefore - impurity);
+			}
+			
+			void split_branch(float *data,int *labels,const Question ques,const int nrows,const int ncols,int& nrowsleft,int& nrowsright,unsigned long long* mask,int depth)
+			{
+				unsigned int masker = 1 << depth;
+				
+			}
+			
+		};
+		
+	} //End namespace DecisionTree
 	
-	root = grow_tree(data,ncols,nrows,colper,labels,mask,0);
-	
-	if(allocmaskflag)
-	  CUDA_CHECK(cudaFree(mask));
-	
-	return;
-      }
-      
-      TreeNode* grow_tree(float *data,const int ncols,const int nrows,const float colper,int *labels,unsigned long long *mask,int depth)
-      {
-	TreeNode *node = new TreeNode();
-	Question ques;
-	float gain = 0.0;
-	
-	find_best_fruit(data,labels,ncols,nrows,colper,ques,gain,mask);  //ques and gain are output here
-	if(gain == 0.0)
-	  {
-	    
-	  }
-	else
-	  {
-	    int nrowsleft,nrowsright;
-	    split_branch(data,labels,ques,nrows,ncols,nrowsleft,nrowsright,mask,depth);
-	  }
-	return node;
-      }
-
-      void find_best_fruit(float *data,int *labels,const int ncols,const int nrows,const float colper,Question& ques,float& gain,unsigned long long* mask)
-      {
-	float maxinfo = 0.0;
-	int splitcol;
-	float splitval;
-	float ginibefore = gini(labels,nrows);
-
-	// Bootstrap columns
-	std::vector<int> colselector(ncols);
-	std::iota(colselector.begin(),colselector.end(),0);
-	std::random_shuffle(colselector.begin(),colselector.end());
-	colselector.resize((int)(colper*ncols));
-
-	int *leftlabels, *rightlabels;
-	CUDA_CHECK(cudaMalloc((void**)&leftlabels,nrows*sizeof(int)));
-	CUDA_CHECK(cudaMalloc((void**)&rightlabels,nrows*sizeof(int)));
-	
-	for(int i=0;i<colselector.size();i++)
-	  {
-	    float *colptr = &data[nrows*colselector[i]];
-	    
-	    float min = minimum(colptr,nrows);
-	    float max = maximum(colptr,nrows);
-	    float delta = (max - min)/ nbins ;
-	   	 
-	    for(int j=1;j<nbins;j++)
-	      {
-		float quesval = min + delta*j;
-		float info_gain = evaluate_split(colptr,labels,leftlabels,rightlabels,ginibefore,quesval,nrows);
-		if(info_gain == -1)
-		  continue;
-
-		if(info_gain > maxinfo)
-		  {
-		    maxinfo = info_gain;
-		    splitval = quesval;
-		    splitcol = colselector[i];
-		  }
-	      }
-	    
-	  }
-
-	ques.column = splitcol;
-	ques.value = splitval;
-	gain = maxinfo;
-	
-	CUDA_CHECK(cudaFree(leftlabels));
-	CUDA_CHECK(cudaFree(rightlabels));
-
-      }
-
-      float evaluate_split(float *column,int* labels,int* leftlabels,int* rightlabels,float ginibefore,float quesval,const int nrows)
-      {
-	int lnrows,rnrows;
-
-	split_labels(column,labels,leftlabels,rightlabels,nrows,lnrows,rnrows,quesval);
-
-	if(lnrows == 0 || rnrows == 0)
-	  return -1;
-	
-	float ginileft = gini(leftlabels,lnrows);
-	float giniright = gini(rightlabels,rnrows);
-	
-	float impurity = (lnrows/nrows) * ginileft + (rnrows/nrows) * giniright;
-	return (ginibefore - impurity);
-      }
-      
-      void split_branch(float *data,int *labels,const Question ques,const int nrows,const int ncols,int& nrowsleft,int& nrowsright,unsigned long long* mask,int depth)
-      {
-	unsigned int masker = 1 << depth;
-	
-      }
-      
-    };
-    
-  } //End namespace DecisionTree
-
 } //End namespace ML 
