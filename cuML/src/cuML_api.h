d90f276db0f838e7a230cb2c3192a270d690473d
Jiri Kraus <jkraus@nvidia.com>
2019-03-05 12:46:04 +0100

    Extended C API proposal.

--- cuML/src/cuML_api.h
+++ cuML/src/cuML_api.h
@@ -16,6 +16,8 @@
 
 #pragma once
 
+#include <stddef.h>
+
 #include <cuda_runtime_api.h>
 
 #ifdef __cplusplus
@@ -29,11 +31,66 @@ typedef struct
 
 enum cumlError_t { CUML_SUCCESS, CUML_ERROR_UNKOWN };
 
+typedef cudaError_t (*cuml_allocate)(void** p,size_t n, cudaStream_t stream);
+typedef cudaError_t (*cuml_deallocate)(void* p, size_t n, cudaStream_t stream);
+
 const char* cumlGetErrorString ( cumlError_t error );
 
 cumlError_t cumlCreate( cumlHandle_t* handle );
 
 cumlError_t cumlSetStream( cumlHandle_t handle, cudaStream_t stream );
+cumlError_t cumlGetStream( cumlHandle_t handle, cudaStream_t* stream );
+
+/**
+ * @code{.c}
+ * cudaError_t device_allocate(void** p,size_t n, cudaStream_t)
+ * {
+ *     return cudaMalloc(p,n);
+ * }
+ * 
+ * cudaError_t device_deallocate(void* p, size_t, cudaStream_t)
+ * {
+ *     return cudaFree(p);
+ * }
+ * 
+ * void foo()
+ * {
+ *     cumlHandle_t cumlHandle;
+ *     cumlCreate( &cumlHandle );
+
+ *     cumlSetDeviceAllocator( cumlHandle, device_allocate, device_deallocate );
+
+ *     cumlDestroy( cumlHandle );
+ * }
+ * @endcode
+ */
+cumlError_t cumlSetDeviceAllocator( cumlHandle_t handle, cuml_allocate allocate_fn, cuml_deallocate deallocate_fn );
+/**
+ * @code{.c}
+ * cudaError_t host_allocate(void** p,size_t n, cudaStream_t)
+ * {
+ *     *p = malloc(n);
+ *     return NULL != *p ? cudaSuccess : cudaErrorUnknown;
+ * }
+ * 
+ * cudaError_t host_deallocate(void* p, size_t, cudaStream_t stream)
+ * {
+ *     free(p);
+ *     return cudaSuccess;
+ * }
+ * 
+ * void foo()
+ * {
+ *     cumlHandle_t cumlHandle;
+ *     cumlCreate( &cumlHandle );
+
+ *     cumlSetHostAllocator( cumlHandle, host_allocate, host_deallocate );
+
+ *     cumlDestroy( cumlHandle );
+ * }
+ * @endcode
+ */
+cumlError_t cumlSetHostAllocator( cumlHandle_t handle, cuml_allocate allocate_fn, cuml_deallocate deallocate_fn );
 
 cumlError_t cumlDestroy( cumlHandle_t handle );
 
