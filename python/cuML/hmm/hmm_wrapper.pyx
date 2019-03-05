ce47fe3842257362a5000412822ef90d3a2725ca
MehdiAbbanaBennani <mehdi-b-@live.fr>
2019-03-05 01:07:51 -0600

    Clean up python code

--- python/cuML/hmm/hmm_wrapper.pyx
+++
@@ -1,4 +0,0 @@
-from c_hmm cimport *
-
-cpdef log_multivariate_normal_density(x, mean, variance):
-    print(log_multivariate_normal_density(x, mean, variance))
--- python/cuML/hmm/prototype_hmm.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cudf
-
-from hmm import GaussianHMM
-
-data = cudf.read_csv('data/data.csv')
-
-hmm = GaussianHMM()
-hmm.fit(data)
