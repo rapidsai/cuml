51c17481be2c21282a6605073f507a13df5f1481
MehdiAbbanaBennani <mehdi-b-@live.fr>
2019-03-05 11:49:40 -0600

    [GMM] Fix python linking

--- python/cuml/test/test_gmm.py
+++ python/cuml/test/test_gmm.py
@@ -32,7 +32,7 @@ def np_to_dataframe(df):
 @pytest.mark.parametrize('precision', ['single', 'double'])
 def test_linear_kalman_filter_base(precision):
 
-    f = GMM(nCl=2, nDim=3, nObs=100, n_iter=5, precision=precision)
+    gmm = GMM(nCl=2, nDim=3, nObs=100, n_iter=5, precision=precision)
 
     if precision == 'single':
         dt = np.float32
@@ -41,70 +41,64 @@ def test_linear_kalman_filter_base(precision):
 
     f.x = cuda.to_device(np.array([[0], [1]], dtype=dt))
 
-    for i in range(n_iter):
-        f.step(cuda.to_device(np.array([z], dtype=dt)))
-        x = f.x.copy_to_host()
-        rmse_x = rmse_x + ((x[0] - i)**2)
-        rmse_v = rmse_v + ((x[1] - 1)**2)
+    gmm.fit()
 
-    assert sqrt(rmse_x / n) < 0.1
     assert sqrt(rmse_v / n) < 0.1
 
 
-@pytest.mark.parametrize('dim_x', [2, 10, 25])
-@pytest.mark.parametrize('dim_z', [1, 2, 10, 25])
-@pytest.mark.parametrize('precision', ['single', 'double'])
-@pytest.mark.parametrize('input_type', ['numpy', 'cudf'])
-def test_linear_kalman_filter(precision, dim_x, dim_z, input_type):
-    f = GMM(dim_x=dim_x, dim_z=dim_z, precision=precision)
-
-    if precision == 'single':
-        dt = np.float32
-    else:
-        dt = np.float64
-
-    if input_type == 'numpy':
-
-        f.x = np.zeros((dim_x, 1), dtype=dt)
-
-        f.F = np.eye(dim_x, dtype=dt)
-
-        h = np.zeros((dim_x, dim_z), dtype=dt)
-        h[0] = 1
-
-        f.H = h
-        f.P = np.eye(dim_x, dtype=dt) * 1000
-        f.R = np.eye(dim_z, dtype=dt) * 5.0
-
-    else:
-
-        f.x = np_to_dataframe(np.zeros((dim_x, 1), dtype=dt))
-
-        tmp = np.eye(dim_x, dtype=dt, order='F')
-
-        f.F = np_to_dataframe(tmp)
-
-        h = np.zeros((dim_x, dim_z), dtype=dt, order='F')
-        h[0] = 1
-
-        f.H = np_to_dataframe(h)
-        f.P = np_to_dataframe(np.eye(dim_x, dtype=dt, order='F') * 1000)
-        f.R = np_to_dataframe(np.eye(dim_z, dtype=dt, order='F') * 5.0)
-
-    rmse_x = 0
-    rmse_v = 0
-
-    n = 10
-
-    for i in range(n):
-        f.predict()
-
-        z = i * np.ones(dim_z, dtype=dt)
-
-        f.update(cuda.to_device(np.array(z, dtype=dt)))
-        x = f.x.copy_to_host()
-        rmse_x = rmse_x + ((x[0] - i)**2)
-        rmse_v = rmse_v + ((x[1] - 1)**2)
-
-    assert sqrt(rmse_x / n) < 0.1
-    assert sqrt(rmse_v / n) == 1.0
+# @pytest.mark.parametrize('dim_x', [2, 10, 25])
+# @pytest.mark.parametrize('dim_z', [1, 2, 10, 25])
+# @pytest.mark.parametrize('precision', ['single', 'double'])
+# @pytest.mark.parametrize('input_type', ['numpy', 'cudf'])
+# def test_linear_kalman_filter(precision, dim_x, dim_z, input_type):
+#     f = GMM(dim_x=dim_x, dim_z=dim_z, precision=precision)
+#
+#     if precision == 'single':
+#         dt = np.float32
+#     else:
+#         dt = np.float64
+#
+#     if input_type == 'numpy':
+#
+#         f.x = np.zeros((dim_x, 1), dtype=dt)
+#
+#         f.F = np.eye(dim_x, dtype=dt)
+#
+#         h = np.zeros((dim_x, dim_z), dtype=dt)
+#         h[0] = 1
+#
+#         f.H = h
+#         f.P = np.eye(dim_x, dtype=dt) * 1000
+#         f.R = np.eye(dim_z, dtype=dt) * 5.0
+#
+#     else:
+#
+#         f.x = np_to_dataframe(np.zeros((dim_x, 1), dtype=dt))
+#
+#         tmp = np.eye(dim_x, dtype=dt, order='F')
+#
+#         f.F = np_to_dataframe(tmp)
+#
+#         h = np.zeros((dim_x, dim_z), dtype=dt, order='F')
+#         h[0] = 1
+#
+#         f.H = np_to_dataframe(h)
+#         f.P = np_to_dataframe(np.eye(dim_x, dtype=dt, order='F') * 1000)
+#         f.R = np_to_dataframe(np.eye(dim_z, dtype=dt, order='F') * 5.0)
+#
+#     rmse_x = 0
+#
+#     n = 10
+#
+#     for i in range(n):
+#         f.predict()
+#
+#         z = i * np.ones(dim_z, dtype=dt)
+#
+#         f.update(cuda.to_device(np.array(z, dtype=dt)))
+#         x = f.x.copy_to_host()
+#         rmse_x = rmse_x + ((x[0] - i)**2)
+#         rmse_v = rmse_v + ((x[1] - 1)**2)
+#
+#     assert sqrt(rmse_x / n) < 0.1
+#     assert sqrt(rmse_v / n) == 1.0
