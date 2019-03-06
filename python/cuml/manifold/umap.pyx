fdf875dcb7d83ec79834555eeefc44be2c270e5c
Corey J. Nolet <cjnolet@gmail.com>
2019-03-06 13:40:18 -0500

    Updating gitignore to ignore any cpp files in python dirs
    Adding docs to cython exposed functions. Adding some asserts.

--- python/cuml/manifold/umap.pyx
+++ python/cuml/manifold/umap.pyx
@@ -52,7 +52,9 @@ cdef extern from "umap/umapparams.h" namespace "ML":
         float repulsion_strength,
         int negative_sample_rate,
         float transform_queue_size,
-        bool verbose
+        bool verbose,
+        float a,
+        float b
 
 
 cdef extern from "umap/umap.h" namespace "ML":
@@ -78,18 +80,97 @@ cdef extern from "umap/umap.h" namespace "ML":
 
 cdef class UMAP:
 
+    """Uniform Manifold Approximation and Projection
+    Finds a low dimensional embedding of the data that approximates
+    an underlying manifold.
+
+    Adapted from https://github.com/lmcinnes/umap/blob/master/umap/umap_.py
+
+    Parameters
+    ----------
+    n_neighbors: float (optional, default 15)
+        The size of local neighborhood (in terms of number of neighboring
+        sample points) used for manifold approximation. Larger values
+        result in more global views of the manifold, while smaller
+        values result in more local data being preserved. In general
+        values should be in the range 2 to 100.
+    n_components: int (optional, default 2)
+        The dimension of the space to embed into. This defaults to 2 to
+        provide easy visualization, but can reasonably be set to any
+    n_epochs: int (optional, default None)
+        The number of training epochs to be used in optimizing the
+        low dimensional embedding. Larger values result in more accurate
+        embeddings. If None is specified a value will be selected based on
+        the size of the input dataset (200 for large datasets, 500 for small).
+    learning_rate: float (optional, default 1.0)
+        The initial learning rate for the embedding optimization.
+    init: string (optional, default 'spectral')
+        How to initialize the low dimensional embedding. Options are:
+            * 'spectral': use a spectral embedding of the fuzzy 1-skeleton
+            * 'random': assign initial embedding positions at random.
+    min_dist: float (optional, default 0.1)
+        The effective minimum distance between embedded points. Smaller values
+        will result in a more clustered/clumped embedding where nearby points
+        on the manifold are drawn closer together, while larger values will
+        result on a more even dispersal of points. The value should be set
+        relative to the ``spread`` value, which determines the scale at which
+        embedded points will be spread out.
+    spread: float (optional, default 1.0)
+        The effective scale of embedded points. In combination with ``min_dist``
+        this determines how clustered/clumped the embedded points are.
+    set_op_mix_ratio: float (optional, default 1.0)
+        Interpolate between (fuzzy) union and intersection as the set operation
+        used to combine local fuzzy simplicial sets to obtain a global fuzzy
+        simplicial sets. Both fuzzy set operations use the product t-norm.
+        The value of this parameter should be between 0.0 and 1.0; a value of
+        1.0 will use a pure fuzzy union, while 0.0 will use a pure fuzzy
+        intersection.
+    local_connectivity: int (optional, default 1)
+        The local connectivity required -- i.e. the number of nearest
+        neighbors that should be assumed to be connected at a local level.
+        The higher this value the more connected the manifold becomes
+        locally. In practice this should be not more than the local intrinsic
+        dimension of the manifold.
+    repulsion_strength: float (optional, default 1.0)
+        Weighting applied to negative samples in low dimensional embedding
+        optimization. Values higher than one will result in greater weight
+        being given to negative samples.
+    negative_sample_rate: int (optional, default 5)
+        The number of negative samples to select per positive sample
+        in the optimization process. Increasing this value will result
+        in greater repulsive force being applied, greater optimization
+        cost, but slightly more accuracy.
+    transform_queue_size: float (optional, default 4.0)
+        For transform operations (embedding new points using a trained model_
+        this will control how aggressively to search for nearest neighbors.
+        Larger values will result in slower performance but more accurate
+        nearest neighbor evaluation.
+    a: float (optional, default None)
+        More specific parameters controlling the embedding. If None these
+        values are set automatically as determined by ``min_dist`` and
+        ``spread``.
+    b: float (optional, default None)
+        More specific parameters controlling the embedding. If None these
+        values are set automatically as determined by ``min_dist`` and
+        ``spread``.
+    verbose: bool (optional, default False)
+        Controls verbosity of logging.
+    """
+
     cpdef UMAPParams *umap_params
     cpdef UMAP_API *umap
     cpdef kNN *knn
     cdef uintptr_t embeddings
     cdef uintptr_t raw_data
 
+    cpdef object n_neighbors
     cpdef object arr_embed
+    cpdef object n_dims
 
     cdef bool _should_downcast
 
     def __cinit__(self,
-                  n_neighbors=2,
+                  n_neighbors=15,
                   n_components=2,
                   n_epochs=500,
                   learning_rate=1.0,
@@ -102,10 +183,14 @@ cdef class UMAP:
                   transform_queue_size=4.0,
                   init="spectral",
                   verbose = False,
+                  a = None,
+                  b = None,
                   should_downcast = True):
 
         self.umap_params = new UMAPParams()
-        self.umap_params.n_neighbors = <int>n_neighbors
+
+        self.n_neighbors = n_neighbors
+
         self.umap_params.n_components = <int>n_components
         self.umap_params.n_epochs = <int>n_epochs
         self.umap_params.verbose = <bool>verbose
@@ -116,6 +201,12 @@ cdef class UMAP:
             self.umap_params.init = <int>0
         else:
             raise Exception("Initialization strategy not support: [init=%d]" % init)
+
+        if a is not None:
+            self.umap_params.a = <float>a
+
+        if b is not None:
+            self.umap_params.b = <float>b
 
         self.umap_params.learning_rate = <float>learning_rate
         self.umap_params.min_dist = <float>min_dist
@@ -178,15 +269,23 @@ cdef class UMAP:
 
 
     def fit(self, X):
+        """Fit X into an embedded space.
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features) or (n_samples, n_samples)
+            X contains a sample per row.
+        """
 
         if self.knn != NULL:
             del self.knn
 
-        """
-        Fit a UMAP model
-        """
         assert len(X.shape) == 2, 'data should be two dimensional'
         assert X.shape[0] > 1, 'need more than 1 sample to build nearest neighbors graph'
+
+        self.umap_params.n_neighbors = min(X.shape[0],
+                                           self.umap_params.n_neighbors)
+
+        self.n_dims = X.shape[1]
 
         X_m = self._downcast(X)
 
@@ -210,16 +309,38 @@ cdef class UMAP:
         del X_m
 
     def fit_transform(self, X):
+        """Fit X into an embedded space and return that transformed
+        output.
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features) or (n_samples, n_samples)
+            X contains a sample per row.
+        Returns
+        -------
+        X_new : array, shape (n_samples, n_components)
+            Embedding of the training data in low-dimensional space.
+        """
         self.fit(X)
         return self.arr_embed
 
 
     def transform(self, X):
+        """Transform X into the existing embedded space and return that
+        transformed output.
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features)
+            New data to be transformed.
+        Returns
+        -------
+        X_new : array, shape (n_samples, n_components)
+            Embedding of the new data in low-dimensional space.
+        """
 
         assert self.knn != NULL, "a model needs to be fit before transform can be called"
-
         assert len(X.shape) == 2, 'data should be two dimensional'
         assert X.shape[0] > 1, 'need more than 1 sample to build nearest neighbors graph'
+        assert X.shape[1] == self.n_dims, "n_features of X must match n_features of training data"
 
         X_m = self._downcast(X)
 
