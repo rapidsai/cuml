201789bc78d91baa52f1241d96367dc00f963a03
Myrto Papadopoulou <myrtop@prometheus>
2019-03-08 08:31:40 -0800

    add thrust minmax_element call

--- cuML/src/decisiontree/kernels/minmax.cuh
+++ cuML/src/decisiontree/kernels/minmax.cuh
@@ -8,7 +8,7 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS, 
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -21,15 +21,25 @@
 float minimum(float *d_samples, int num_samples)
 {
   float minval;
-  float *min_ptr = thrust::min_element(thrust::device,d_samples,d_samples + num_samples);
-  CUDA_CHECK(cudaMemcpy(&minval,min_ptr,sizeof(float),cudaMemcpyDeviceToHost));
+  float *min_ptr = thrust::min_element(thrust::device, d_samples, d_samples + num_samples);
+  CUDA_CHECK(cudaMemcpy(&minval, min_ptr, sizeof(float), cudaMemcpyDeviceToHost));
   return minval;
 }
 
 float maximum(float *d_samples, int num_samples)
 {
   float maxval;
-  float *max_ptr = thrust::max_element(thrust::device,d_samples,d_samples + num_samples);
-  CUDA_CHECK(cudaMemcpy(&maxval,max_ptr,sizeof(float),cudaMemcpyDeviceToHost));
+  float *max_ptr = thrust::max_element(thrust::device, d_samples, d_samples + num_samples);
+  CUDA_CHECK(cudaMemcpy(&maxval, max_ptr, sizeof(float), cudaMemcpyDeviceToHost));
   return maxval;
+}
+
+// According to https://thrust.github.io/doc/group__extrema.html#gaa9dcee5e36206a3ef7215a4b3984e002
+// minmax_element "[..] function is potentially more efficient than separate cols to min_element and max_element."
+
+void min_and_max(float * d_samples, int num_samples, float & min_val, float & max_val) {
+	thrust::pair<float *, float *> result;
+	result = thrust::minmax_element(thrust::device, d_samples, d_samples + num_samples);
+	CUDA_CHECK(cudaMemcpy(&min_val, result.first, sizeof(float), cudaMemcpyDeviceToHost));
+ 	CUDA_CHECK(cudaMemcpy(&max_val, result.second, sizeof(float), cudaMemcpyDeviceToHost));
 }
