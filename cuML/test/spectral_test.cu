360f4210f68406354dc3c8a2af66cd5236db81a5
Corey J. Nolet <cjnolet@gmail.com>
2019-03-11 22:39:28 -0400

    Add gtests for spectral clustering and spectral embedding

--- cuML/test/spectral_test.cu
+++ cuML/test/spectral_test.cu
@@ -16,6 +16,7 @@
 
 #include "spectral/spectral.h"
 
+#include "random/rng.h"
 
 #include <vector>
 #include <gtest/gtest.h>
@@ -27,25 +28,10 @@ namespace ML {
 
 using namespace MLCommon;
 
-
-/**
- *
- * NOTE: Not exhaustively testing the kNN implementation since
- * we are using FAISS for this. Just testing API to verify the
- * knn.cu class is accepting inputs and providing outputs as
- * expected.
- */
 template<typename T>
 class SpectralTest: public ::testing::Test {
 protected:
-    void SetUp() override {
-
-        float *X;
-
-        int *out;
-
-        ML::Spectral::fit_clusters(X, 50, 50, 10, 10, 10.0f, out );
-    }
+    void SetUp() override {}
 
     void TearDown() override {}
 
@@ -54,8 +40,47 @@ protected:
 };
 
 
-typedef SpectralTest<float> SpectralTestF;
-TEST_F(SpectralTestF, Fit) {
+typedef SpectralTest<float> TestSpectralClustering;
+TEST_F(TestSpectralClustering, Fit) {
+
+    int n = 500;
+    int d = 30;
+    int k = 3;
+
+    float *X;
+    MLCommon::allocate(X, n*d);
+
+    Random::Rng r(150, MLCommon::Random::GenTaps);
+    r.uniform(X, n*d, -1.0f, 1.0f);
+
+    int *out;
+    MLCommon::allocate(out, n, true);
+
+    ML::Spectral::fit_clusters(X, n, d, k, 10, 1e-3f, out);
+
+    std::cout << MLCommon::arr2Str(out, n, "out") << std::endl;
 }
+
+typedef SpectralTest<float> TestSpectralEmbedding;
+TEST_F(TestSpectralEmbedding, Fit) {
+
+    int n = 500;
+    int d = 30;
+    int k = 3;
+
+    float *X;
+    MLCommon::allocate(X, n*d);
+
+    Random::Rng r(150, MLCommon::Random::GenTaps);
+    r.uniform(X, n*d, -1.0f, 1.0f);
+
+    float *out;
+    MLCommon::allocate(out, n*2, true);
+
+    ML::Spectral::fit_embedding(X, n, d, k, 2, out);
+
+    std::cout << MLCommon::arr2Str(out, n, "out") << std::endl;
+}
+
 
 } // end namespace ML
