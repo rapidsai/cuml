2d41f8438ed1fb3fca4164c6ef3b231ddb84dfad
Myrto Papadopoulou <myrtop@prometheus>
2019-03-19 03:03:29 -0700

    Updates in batch_evaluate_gini and tree.
    - Updates in batch_evaluate_gini and find_best_fruit.
    - Minor cleanup in tempmemory; more needed.
    - Notes:
      => Slightly negative info gain in batch_evaluate_gini is
      possible. It won't be propagated to find_best_fruit though.
      => In find_best_fruit, if all values in a column (feature) are
      identical, only the first batch is launched. This still includes some
      wasteful computation, a single batch of 1 would suffice for that case.

--- cuML/src/decisiontree/kernels/evaluate.cuh
+++ cuML/src/decisiontree/kernels/evaluate.cuh
@@ -121,7 +121,9 @@ void evaluate_and_leftgini(const float *column, const int *labels, const float q
    Outputs: split_info[1] and split_info[2] are updated with the correct info for the best split among the considered batch.
    batch_id specifies which question (bin) within the batch  gave the best split.
 */
-float batch_evaluate_gini(const float *column, const int *labels, const float base_quesval, const float delta, const int batch_bins, int & batch_id, const int nrows, const int n_unique_labels, GiniInfo split_info[3], TemporaryMemory* tempmem) {
+float batch_evaluate_gini(const float *column, const int *labels, const float base_quesval, const float delta,
+							const int batch_bins, int & batch_id, const int nrows, const int n_unique_labels,
+							GiniInfo split_info[3], TemporaryMemory* tempmem) {
 
 	int *dhist = tempmem->d_hist;
 	int *hhist = tempmem->h_hist;
@@ -130,23 +132,29 @@ float batch_evaluate_gini(const float *column, const int *labels, const float ba
 	CUDA_CHECK(cudaMemsetAsync(dhist, 0, n_hists_bytes, tempmem->stream));
 	// Each thread does more work: it answers batch_bins questions for the same column data. Could change this in the future.
 	ASSERT((n_unique_labels <= 128), "Error! Kernel cannot support %d labels. Current limit is 128", n_unique_labels);
-	batch_evaluate_kernel<<< (int)(nrows /128) + 1, 128, n_hists_bytes, tempmem->stream>>>(column, labels, base_quesval, batch_bins, delta, nrows, n_unique_labels, dhist);
+
+	//Kernel launch
+	batch_evaluate_kernel<<< (int)(nrows /128) + 1, 128, n_hists_bytes, tempmem->stream>>>(column, labels,
+		base_quesval, batch_bins, delta, nrows, n_unique_labels, dhist);
+
 	CUDA_CHECK(cudaGetLastError());
 	CUDA_CHECK(cudaMemcpyAsync(hhist, dhist, n_hists_bytes, cudaMemcpyDeviceToHost, tempmem->stream));
 	CUDA_CHECK(cudaStreamSynchronize(tempmem->stream));
 
 	float gain = 0.0f;
-	int best_batch_id = -1;
+	int best_batch_id = 0;
 
 	// hhist holds batch_bins of n_unique_labels each.
 	// Todo note: we could do some of these computations on the gpu side too.
 	for (int i = 0; i < batch_bins; i++) {
 
+		// if tmp_lnrows or tmp_rnrows is 0, the corresponding gini will be 1 but that doesn't
+		// matter as it won't count in the info_gain computation.
 		float tmp_gini_left = 1.0f;
 		float tmp_gini_right = 1.0f;
 		int tmp_lnrows = 0;
 
-		//separate loop for now to avoid overflow. FIXME
+		//separate loop for now to avoid overflow.
 		for (int j = 0; j < n_unique_labels; j++) {
 			int hist_index = i * n_unique_labels + j;
 			tmp_lnrows += hhist[hist_index];
@@ -157,23 +165,37 @@ float batch_evaluate_gini(const float *column, const int *labels, const float ba
 		for (int j = 0; j < n_unique_labels; j++) {
 			int hist_index = i * n_unique_labels + j;
 
-			float prob_left = (float) (hhist[hist_index]) / tmp_lnrows;
-			tmp_gini_left -= prob_left * prob_left;
+			if (tmp_lnrows != 0) {
+				float prob_left = (float) (hhist[hist_index]) / tmp_lnrows;
+				tmp_gini_left -= prob_left * prob_left;
+			}
 
-			float prob_right = (float) (split_info[0].hist[j] - hhist[hist_index]) / tmp_rnrows;
-			tmp_gini_right -=  prob_right * prob_right;
+			if (tmp_rnrows != 0) {
+				float prob_right = (float) (split_info[0].hist[j] - hhist[hist_index]) / tmp_rnrows;
+				tmp_gini_right -=  prob_right * prob_right;
+			}
 		}
+
+		/*std::cout << "\nBatch id is " << i <<  ":\n";
+		std::cout << "nrows/lnrows/rnrows " << nrows << ", " << tmp_lnrows << ", " << tmp_rnrows << std::endl;
+		std::cout << "Gini parent/left/right " << split_info[0].best_gini << ", " << tmp_gini_left << ", " << tmp_gini_right << std::endl;*/
 
 		ASSERT((tmp_gini_left >= 0.0f) && (tmp_gini_left <= 1.0f), "gini left value %f not in [0.0, 1.0]", tmp_gini_left);
 		ASSERT((tmp_gini_right >= 0.0f) && (tmp_gini_right <= 1.0f), "gini right value %f not in [0.0, 1.0]", tmp_gini_right);
 
 		float impurity = (tmp_lnrows * 1.0f/nrows) * tmp_gini_left + (tmp_rnrows * 1.0f/nrows) * tmp_gini_right;
-
 		float info_gain = split_info[0].best_gini - impurity;
-		ASSERT(info_gain + FLT_EPSILON >= 0.0f, "Cannot have negative info_gain %f", info_gain);
+
+		/*std::cout << "Impurity is " << impurity << " info gain is " << info_gain << " gain so far is " << gain << std::endl;
+		ASSERT(info_gain + FLT_EPSILON >= 0.0, "Cannot have negative info_gain %f", info_gain);
+
+		// Note: It is possible to get negative (a bit below <0) information gain. By default this will result in no gain update due to its
+		// initialization to zero.
+		*/
+
 
 		// Compute best information gain so far in the batch.
-		if (info_gain >= gain) {
+		if (info_gain > gain) {
 			gain = info_gain;
 			best_batch_id = i;
 			split_info[1].best_gini = tmp_gini_left;
@@ -181,10 +203,12 @@ float batch_evaluate_gini(const float *column, const int *labels, const float ba
 		}
 	}
 
-	ASSERT(best_batch_id != -1, "Invalid best_batch_id"); // To ensure if-statement in loop above executed. Otherwise dummy gini values.
 
-	// The batch id best_batch_id, within the batch, resulted in the best split. Update split_info accordingly
+	// The batch id best_batch_id, within the batch, resulted in the best split. Update split_info accordingly.
 	// This code is to avoid the hist copy every time within above loop.
+
+	// The best_batch_id and rest info is dummy if we didn't go through the if-statement above. But that's OK because this will be treated as a leaf?
+	// FIXME What should best_gini vals be in that case?
 	split_info[1].hist.resize(n_unique_labels);
 	split_info[2].hist.resize(n_unique_labels);
 	for (int j = 0; j < n_unique_labels; j++) {
