a280e30bef49b342009419c9fb0f2077c933cfa1
mt-jones <m.t.j.rider@icloud.com>
2019-03-21 17:13:18 -0600

    adding openblas and openmp to CI build

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -31,7 +31,7 @@ nvidia-smi
 
 logger "Activate conda env..."
 source activate gdf
-sudo apt-get update && sudo apt-get install libopenblas-dev libomp-dev
+apt-get install libopenblas-dev libomp-dev
 conda install -c nvidia -c rapidsai/label/cuda$CUDA_REL -c rapidsai-nightly/label/cuda$CUDA_REL -c numba -c conda-forge -c defaults cudf=$CUDF_VERSION
 
 logger "Check versions..."
