b3f95560c9b9998f33d022dfc4af9860f9c053c1
Thejaswi Rao <rao.thejaswi@gmail.com>
2019-04-03 02:31:41 -0700

    Initial change to expose cumlHandle to the cython world. Also has an accompanying developer guide.

---
+++ python/cuml/common/base.pyx
@@ -0,0 +1,92 @@
+#
+# Copyright (c) 2019, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# cython: profile=False
+# distutils: language = c++
+# cython: embedsignature = True
+# cython: language_level = 3
+
+
+import cuml.common.handle
+import cuml.common.cuda
+
+
+class Base:
+    """
+    Base class for all the ML algos. It handles some of the common operations
+    across all algos. Every ML algo class exposed at cython level must inherit
+    from this class.
+    """
+
+    def __init__(self, handle=None, verbose=False):
+        """
+        Constructor. All children must call init method of this base class.
+
+        Parameters
+        ----------
+        handle : cuml.common.handle.Handle
+               If it is None, a new one is created just for this class
+        verbose : bool
+                Whether to print debug spews
+        """
+        if handle is None:
+            self.handle = cuml.common.handle.Handle()
+            self.stream = cuml.common.cuda.Stream()
+            self.handle.setStream(self.stream)
+        else:
+            self.handle = handle
+        self.verbose = verbose
+
+
+    def get_param_names(self):
+        """
+        Returns a list of parameter names owned by this class. It is expected
+        that every child class overrides this method and appends its extra set
+        of parameter that it needs. This will simplify the `get_params` and
+        `set_params` methods. (Refer below)
+        """
+        return ["handle", "verbose"]
+
+
+    def get_params(self, deep=True):
+        """
+        Returns a dict of all params owned by this class. If the child class has
+        appropriately overridden the `get_param_names` method, then it doesn't
+        have to override this method
+        """
+        params = dict()
+        variables = self.get_param_names()
+        for key in variables:
+            var_value = getattr(self, key, None)
+            params[key] = var_value
+        return params
+
+
+    def set_params(self, **params):
+        """
+        Accepts a dict of params and updates the corresponding ones owned by
+        this class. If the child class has appropriately overridden the
+        `get_param_names` method, then it doesn't have to override this method
+        """
+        if not params:
+            return self
+        variables = self.get_param_names()
+        for key, value in params.items():
+            if key not in variables:
+                raise ValueError("Bad param '%s' passed to set_params" % key)
+            else:
+                setattr(self, key, value)
+        return self
---
+++ python/cuml/common/handle.pyx
@@ -0,0 +1,88 @@
+#
+# Copyright (c) 2019, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# cython: profile=False
+# distutils: language = c++
+# cython: embedsignature = True
+# cython: language_level = 3
+
+
+from libcpp.memory cimport shared_ptr
+from cuml.common.cuda cimport _Stream
+
+
+cdef extern from "common/rmmAllocatorAdapter.hpp" namespace "ML" nogil:
+    cdef cppclass rmmAllocatorAdapter(deviceAllocator):
+        pass
+
+
+# TODO: name this properly!
+cdef class Handle:
+    """
+    Handle is a lightweight python wrapper around the corresponding C++ class
+    of cumlHandle exposed by cuML's C++ interface. Refer to the header file
+    cuML.hpp for interface level details of this struct
+
+    Examples
+    --------
+
+    .. code-block:: python
+
+        import cuml
+        from cuml.common.cuda import Stream
+        stream = Stream()
+        handle = cuml.common.handle.Handle()
+        handle.setStream(stream)
+        handle.enableRMM()   # Enable RMM as the device-side allocator
+
+        # call ML algos here
+
+        # final synchronization of all work launched/dependent on this stream
+        stream.sync()
+        del handle  # optional!
+    """
+
+    # ML::cumlHandle doesn't have copy operator. So, use pointer for the object
+    # python world cannot access to this raw object directly, hence use 'size_t'!
+    cdef size_t h
+
+    def __cinit__(self):
+        self.h = <size_t>(new cumlHandle())
+
+    def __dealloc_(self):
+        h_ = <cumlHandle*>self.h
+        del h_
+
+    def setStream(self, stream):
+        cdef size_t s = <size_t>stream.getStream()
+        cdef cumlHandle* h_ = <cumlHandle*>self.h
+        h_.setStream(<_Stream>s)
+
+    # TODO: in future, we should just enable RMM by default
+    def enableRMM(self):
+        """
+        Enables to use RMM as the allocator for all device memory allocations
+        inside cuML C++ world. Currently, there are only 2 kinds of allocators.
+        First, the usual cudaMalloc/Free, which is the default for cumlHandle.
+        Second, the allocator based on RMM. So, this function, basically makes
+        the cumlHandle use a more efficient allocator, instead of the default.
+        """
+        cdef shared_ptr[deviceAllocator] rmmAlloc = shared_ptr[deviceAllocator](new rmmAllocatorAdapter())
+        cdef cumlHandle* h_ = <cumlHandle*>self.h
+        h_.setDeviceAllocator(rmmAlloc)
+
+    def getHandle(self):
+        return self.h
