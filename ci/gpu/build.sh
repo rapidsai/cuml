2d9b73f31c4eb2ac4522a4b0ea7dfd0a716fd607
Dante Gama Dessavre <danteg@nvidia.com>
2019-04-03 14:13:25 -0500

    FIX Remove defaults channel from ci scripts

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -31,7 +31,7 @@ nvidia-smi
 
 logger "Activate conda env..."
 source activate gdf
-conda install -c nvidia -c rapidsai/label/cuda$CUDA_REL -c rapidsai-nightly/label/cuda$CUDA_REL -c numba -c conda-forge -c defaults cudf=$CUDF_VERSION
+conda install -c nvidia -c rapidsai/label/cuda$CUDA_REL -c rapidsai-nightly/label/cuda$CUDA_REL -c numba -c conda-forge cudf=$CUDF_VERSION
 
 logger "Check versions..."
 python --version
