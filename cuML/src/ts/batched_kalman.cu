2b6c38c0a177290f0dfa3efa8358f45af2fe7ce5
Max Rietmann <mrietmann@nvidia.com>
2019-04-08 11:53:03 +0200

    WIP Commit: Compiles, Test starts, but fails in an exception.

--- cuML/src/ts/batched_kalman.cu
+++ cuML/src/ts/batched_kalman.cu
@@ -2,12 +2,17 @@
 #include "batched_kalman.h"
 #include <matrix/batched_matrix.h>
 #include <utils.h>
+#include <thrust/iterator/counting_iterator.h>
+#include <thrust/for_each.h>
+// #include <thrust/lo
 
 using std::vector;
 
 using MLCommon::Matrix::BatchedMatrix;
 using MLCommon::Matrix::b_gemm;
 using MLCommon::allocate;
+using MLCommon::updateDevice;
+using MLCommon::updateHost;
 
 __global__ void vs_eq_ys_m_alpha00_kernel(double* d_vs, int it,
                                           const double* ys_it,
@@ -24,7 +29,9 @@ void vs_eq_ys_m_alpha00(double* d_vs,int it,const vector<double*>& ptr_ys_b,cons
   const int block_size = 16;
   const int num_blocks = std::ceil((double)num_batches/(double)block_size);
 
-  vs_eq_ys_m_alpha00_kernel<<<num_blocks, block_size>>>(d_vs, it, ptr_ys_b[it], alpha.data(), alpha.shape().first, num_batches);
+  vs_eq_ys_m_alpha00_kernel<<<num_blocks, block_size>>>(d_vs, it, ptr_ys_b[it],
+                                                        alpha.data(), alpha.shape().first, num_batches);
+  CUDA_CHECK(cudaPeekAtLastError());
   
 }
 
@@ -42,6 +49,7 @@ void fs_it_P00(double* d_Fs, int it, const BatchedMatrix& P) {
   const int num_blocks = std::ceil((double)num_batches/(double)block_size);
 
   fs_it_P00_kernel<<<num_blocks, block_size>>>(d_Fs, it, P.data(), num_batches);
+  CUDA_CHECK(cudaPeekAtLastError());
 
 }
 
@@ -71,6 +79,7 @@ BatchedMatrix _1_Fsit_TPZt(double* d_Fs, int it, const BatchedMatrix& TPZt) {
 
   // call kernel
   _1_Fsit_TPZt_kernel<<<num_blocks,block_size>>>(d_Fs, it, TPZt.data(), TPZt_size, num_batches, K.data());
+  CUDA_CHECK(cudaPeekAtLastError());
 
   return K;
 }
@@ -95,29 +104,68 @@ double* sumLogFs(double* d_Fs, const int num_batches, const int nobs) {
   double* d_sumLogFs;
   allocate(d_sumLogFs, num_batches);
   // compute sum(log(Fs[0:nobs]))
-  const int block_size = 32;
-  const int num_blocks = std::ceil((double)num_batches/(double)block_size);
-  sumLogFs_kernel<<<num_blocks, block_size>>>(d_Fs, num_batches, nobs, d_sumLogFs);
+  // const int block_size = 32;
+  // const int num_blocks = std::ceil((double)num_batches/(double)block_size);
+  // sumLogFs_kernel<<<num_blocks, block_size>>>(d_Fs, num_batches, nobs, d_sumLogFs);
+  // CUDA_CHECK(cudaPeekAtLastError());
+  auto counting = thrust::make_counting_iterator(0);
+  thrust::for_each(counting, counting + num_batches,
+                   [=]__device__(int bid) {
+                     double sum = 0.0;
+                     for (int it = 0; it < nobs; it++) {
+                       sum += log(d_Fs[bid]);
+                     }
+                     d_sumLogFs[bid] = sum;
+                   });
+
   return d_sumLogFs;
 }
 
-void batched_kalman_filter(const vector<double*>& ptr_ys_b,
-                           const vector<double*>& ptr_Zb,
-                           const vector<double*>& ptr_Rb,
-                           const vector<double*>& ptr_Tb,
+void batched_kalman_filter(const vector<double*>& h_ys_b, // { vector size nobs, each item size batches }
+                           const vector<double*>& h_Zb, // { vector size batches, each item size Zb }
+                           const vector<double*>& h_Rb, // { vector size batches, each item size Rb }
+                           const vector<double*>& h_Tb, // { vector size batches, each item size Tb }
                            int r,
-                           vector<double*>& ptr_vs_b,
-                           vector<double*>& ptr_Fs_b,
-                           vector<double>& ptr_loglike_b,
-                           vector<double>& ptr_sigma2_b) {
+                           vector<double*>& h_vs_b,
+                           vector<double*>& h_Fs_b,
+                           vector<double>& h_loglike_b,
+                           vector<double>& h_sigma2_b) {
 
 
-  const size_t num_batches = ptr_Zb.size();
-  const size_t ys_len = ptr_ys_b.size();
+  const size_t ys_len = h_ys_b.size();
+  const size_t num_batches = h_Zb.size();
 
-  BatchedMatrix Zb(ptr_Zb, {1, r});
-  BatchedMatrix Tb(ptr_Tb, {r, r});
-  BatchedMatrix Rb(ptr_Rb, {r, 1});
+  ////////////////////////////////////////////////////////////
+  // xfer from host to device
+
+  //TODO: Far too many allocations for these. Definitely want to fix after getting this working
+
+  vector<double*> d_ys_b(ys_len);
+  for(int it=0;it<ys_len;it++) {
+    allocate(d_ys_b[it], num_batches);
+    updateDevice(d_ys_b[it], h_ys_b[it], num_batches);
+  }
+  
+  vector<double*> d_Zb(num_batches);
+  vector<double*> d_Rb(num_batches);
+  vector<double*> d_Tb(num_batches);
+
+  for(int bi=0; bi<num_batches; bi++) {
+    allocate(d_Zb[bi], r);
+    updateDevice(d_Zb[bi], h_Zb[bi], r);
+
+    allocate(d_Rb[bi], r);
+    updateDevice(d_Rb[bi], h_Rb[bi], r);
+
+    allocate(d_Tb[bi], r*r);
+    updateDevice(d_Tb[bi], h_Tb[bi], r*r);
+  }
+  
+  ////////////////////////////////////////////////////////////
+  // Computation
+  BatchedMatrix Zb(d_Zb, {1, r});
+  BatchedMatrix Tb(d_Tb, {r, r});
+  BatchedMatrix Rb(d_Rb, {r, 1});
 
   BatchedMatrix RRT = b_gemm(Rb, Rb, false, true);
 
@@ -138,7 +186,7 @@ void batched_kalman_filter(const vector<double*>& ptr_ys_b,
 
     // 1.
     // vs[it] = ys[it] - alpha(0,0);
-    vs_eq_ys_m_alpha00(d_vs, it, ptr_ys_b, alpha);
+    vs_eq_ys_m_alpha00(d_vs, it, d_ys_b, alpha);
 
     // 2.
     // Fs[it] = P(0,0);
@@ -166,18 +214,64 @@ void batched_kalman_filter(const vector<double*>& ptr_ys_b,
 
   // 7.
   // loglikelihood = sum(log(Fs[:]))
-  double* sumLogFs = sumLogFs(d_Fs, num_batches, ys_len);
-  
+  double* loglikelihood = sumLogFs(d_Fs, num_batches, ys_len);
+
   // 8.
   // sigma2 = ((vs.array().pow(2.0)).array() / Fs.array()).mean();
+
+  double* sigma2;
+  allocate(sigma2, num_batches);
+  {
+    auto counting = thrust::make_counting_iterator(0);
+    thrust::for_each(counting, counting+num_batches,
+                     [=]__device__(int bid) {
+                       sigma2[bid] = 0.0;
+                       double sigma2_sum = 0.0;
+                       for (int it = 0; it < ys_len; it++) {
+                         auto vsit = d_vs[bid + num_batches*it];
+                         sigma2_sum += vsit*vsit / d_Fs[bid + num_batches*it];
+                       }
+                       sigma2[bid] = sigma2_sum / ys_len;
+                     });
+  }
   // 9.
   // loglike = -.5 * (loglikelihood + nobs * std::log(sigma2)) - nobs / 2. * (std::log(2 * M_PI) + 1);
+  double* loglike;
+  allocate(loglike, num_batches);
+  {
+    auto counting = thrust::make_counting_iterator(0);
+    int nobs = ys_len;
+    thrust::for_each(counting, counting+num_batches,
+                     [=]__device__(int bid) {
+                       loglike[bid] = -.5 * (loglikelihood[bid] + nobs * log(sigma2[bid]))
+                         - nobs / 2. * (logf(2 * M_PI) + 1);
+                     });
+  }
+  
+  ////////////////////////////////////////////////////////////
+  // xfer results from GPU
+  // need to fill:
+  // vector<double*>& h_vs_b,   
+  // vector<double*>& h_Fs_b,   
+  // vector<double>& h_loglike_b
+  // vector<double>& h_sigma2_b)
+  //
+  h_vs_b.resize(ys_len);
+  h_Fs_b.resize(ys_len);
+  h_loglike_b.resize(num_batches);
+  h_sigma2_b.resize(num_batches);
 
-  //xfer results from GPU
+  // vs, Fs
+  vector<double> h_vs_raw(ys_len*num_batches);
+  vector<double> h_Fs_raw(ys_len*num_batches);
+  updateHost(h_vs_raw.data(), d_vs, ys_len*num_batches);
+  updateHost(h_Fs_raw.data(), d_Fs, ys_len*num_batches);
+  for(int it=0;it<ys_len;it++) {
+    h_vs_b[it] = &h_vs_raw[it*num_batches];
+    h_Fs_b[it] = &h_Fs_raw[it*num_batches];
+  }
 
-  // for(int i=0; i<num_batches; i++) {
-  //   kalman_filter(ptr_ys_b[i], ys_len[i], ptr_Zb[i], ptr_Rb[i], ptr_Tb[i], r,
-  //                 ptr_vs_b[0], ptr_Fs_b[0], &ptr_loglike_b[i], &ptr_sigma2_b[i]);
-  // }
+  updateHost(h_loglike_b.data(), loglike, num_batches);
+  updateHost(h_sigma2_b.data(), sigma2, num_batches);
 
 }
