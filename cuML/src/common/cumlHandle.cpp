38451ed4b79058a9c27a47a1e4847ed92628ea42
Rene Mueller <muellerr@nvidia.com>
2019-04-11 12:05:01 +0200

    added thread-safe handle ID to handle object map.

--- cuML/src/common/cumlHandle.cpp
+++ cuML/src/common/cumlHandle.cpp
@@ -366,4 +366,85 @@ void cumlHandle_impl::destroyResources()
     }
 }
 
+HandleMap handleMap;
+
+std::pair<cumlHandle_t, cumlError_t> HandleMap::createAndInsertHandle()
+{
+    cumlError_t status = CUML_SUCCESS;
+	  cumlHandle_t chosen_handle;
+    try
+    {
+        auto handle_ptr = new ML::cumlHandle();
+        bool inserted;
+        {
+            std::lock_guard<std::mutex> guard(_mapMutex);
+            cumlHandle_t initial_next = _nextHandle;
+            do {
+                // try to insert using next free handle identifier
+                chosen_handle = _nextHandle;
+                inserted = _handleMap.insert({ chosen_handle, handle_ptr}).second;
+                _nextHandle += 1;
+            } while(!inserted && _nextHandle != initial_next);
+        }
+        if (!inserted) {
+            // no free handle identifier available
+            chosen_handle = INVALID_HANDLE;
+            status = CUML_ERROR_UNKNOWN;
+        }
+    }
+    //TODO: Implement this
+    //catch (const MLCommon::Exception& e)
+    //{
+    //    //log e.what()?
+    //    status =  e.getErrorCode();
+    //}
+    catch (...)
+    {
+        status = CUML_ERROR_UNKNOWN;
+        chosen_handle = CUML_ERROR_UNKNOWN;
+    }
+    return std::pair<cumlHandle_t, cumlError_t>(chosen_handle, status);
+}
+
+std::pair<cumlHandle*, cumlError_t> HandleMap::lookupHandlePointer(cumlHandle_t handle) const
+{
+    std::lock_guard<std::mutex> guard(_mapMutex);
+    auto it = _handleMap.find(handle);
+    if (it == _handleMap.end()) {
+        return std::pair<cumlHandle*, cumlError_t>(nullptr, CUML_INVALID_HANDLE);
+    } else {
+        return std::pair<cumlHandle*, cumlError_t>(it->second, CUML_SUCCESS);
+    }
+}
+
+cumlError_t HandleMap::removeAndDestroyHandle(cumlHandle_t handle)
+{
+    ML::cumlHandle *handle_ptr;
+    {
+        std::lock_guard<std::mutex> guard(_mapMutex);
+        auto it = _handleMap.find(handle);
+        if (it == _handleMap.end()) {
+            return CUML_INVALID_HANDLE;
+        }
+        handle_ptr = it->second;
+        _handleMap.erase(it);
+    }
+    cumlError_t status = CUML_SUCCESS;
+    try
+    {
+        delete handle_ptr;
+    }
+    //TODO: Implement this
+    //catch (const MLCommon::Exception& e)
+    //{
+    //    //log e.what()?
+    //    status =  e.getErrorCode();
+    //}
+    catch (...)
+    {
+        status = CUML_ERROR_UNKNOWN;
+    }
+    return status;
+}
+
 } // end namespace ML
