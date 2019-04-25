e2feb82618f15b442fa671f2fe89861ee1b14e1a
Dante Gama Dessavre <danteg@nvidia.com>
2019-04-25 17:06:58 -0500

    FEA Add metrics.hpp with instantiated functions to call from python

---
+++ cuML/src/metrics/metrics.hpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <cuML.hpp>
+
+// #include <linalg/eltwise.h>
+// #include <metrics/metrics.h>
+// #include <common/cumlHandle.hpp>
+
+namespace ML {
+
+    namespace Metrics {
+
+        /**
+         * Calculates the "Coefficient of Determination" (R-Squared) score
+         * normalizing the sum of squared errors by the total sum of squares.
+         *
+         * This score indicates the proportionate amount of variation in an
+         * expected response variable is explained by the independent variables
+         * in a linear regression model. The larger the R-squared value, the
+         * more variability is explained by the linear regression model.
+         *
+         * @param y: Array of ground-truth response variables
+         * @param y_hat: Array of predicted response variables
+         * @param n: Number of elements in y and y_hat
+         * @return: The R-squared value.
+         */
+
+        float r2_score_py(const cumlHandle& handle, float *y, float *y_hat, int n);
+
+        double r2_score_py(const cumlHandle& handle, double *y, double *y_hat, int n);
+
+    }
+}
