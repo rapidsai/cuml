87ef34d69b1f8684cb8ae9a141fcb71440f2cf40
Dante Gama Dessavre <danteg@nvidia.com>
2019-04-24 19:29:04 -0500

    FIX Update gpu ci script for rmm and nvstirng conda packages

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -14,7 +14,11 @@ function logger() {
 export PATH=/conda/bin:/usr/local/cuda/bin:$PATH
 export PARALLEL_LEVEL=4
 export CUDA_REL=${CUDA_VERSION%.*}
-export CUDF_VERSION=0.7
+
+# Set versions of packages needed to be grabbed
+export CUDF_VERSION=0.7.*
+export NVSTRINGS_VERSION=0.7.*
+export RMM_VERSION=0.7.*
 
 # Set home to the job's workspace
 export HOME=$WORKSPACE
@@ -31,7 +35,7 @@ nvidia-smi
 
 logger "Activate conda env..."
 source activate gdf
-conda install cudf=$CUDF_VERSION
+conda install -c rapidsai/label/cuda${CUDA_REL} -c rapidsai-nightly/label/cuda${CUDA_REL} cudf=${CUDF_VERSION} rmm=${RMM_VERSION} nvstrings=${NVSTRINGS_VERSION}
 
 logger "Check versions..."
 python --version
