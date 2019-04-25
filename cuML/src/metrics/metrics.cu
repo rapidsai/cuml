4ce02a9957ed9a5e9d7fb9ddd7ee98b6ef66c34b
Dante Gama Dessavre <danteg@nvidia.com>
2019-04-25 17:07:18 -0500

    FEA Add implementation file of r2 functions in cuml

---
+++ cuML/src/metrics/metrics.cu
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// #include "metrics.h"
+#include "metrics.hpp"
+#include "cuda_utils.h"
+
+#include "score/metrics.h"
+
+namespace ML {
+
+    namespace Metrics {
+
+        float r2_score_py(const cumlHandle& handle, float *y, float *y_hat, int n){
+            return MLCommon::Metrics::r2_score(y, y_hat, n, handle.getStream());
+        }
+
+        double r2_score_py(const cumlHandle& handle, double *y, double *y_hat, int n){
+            return MLCommon::Metrics::r2_score(y, y_hat, n, handle.getStream());
+        }
+
+    }
+}
