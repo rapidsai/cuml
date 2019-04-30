aca84403d212031264c1e26b74e4869fb5e94e11
Thejaswi Rao <rao.thejaswi@gmail.com>
2019-04-30 03:26:33 -0700

    updated the subsampling logic as per discussion with Vishal to support the existing use-case inside RF

--- ml-prims/src/stats/minmax.h
+++ ml-prims/src/stats/minmax.h
@@ -37,7 +37,7 @@ __global__ void minmaxInitKernel(int ncols, T* globalmin, T* globalmax,
 template <typename T>
 __global__ void minmaxKernel(const T* data, const int* rowids,
                              const int* colids, int nrows, int ncols,
-                             T* g_min, T* g_max, T* sampledcols,
+                             int row_stride, T* g_min, T* g_max, T* sampledcols,
                              T init_min_val) {
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
     extern __shared__ char shmem[];
@@ -57,10 +57,7 @@ __global__ void minmaxKernel(const T* data, const int* rowids,
         if(rowids != nullptr) {
             row = rowids[row];
         }
-        if(row < 0 || col < 0) {
-            continue;
-        }
-        int index = row + col * nrows;
+        int index = row + col * row_stride;
         T coldata = data[index];
         myAtomicMin(&s_min[col], coldata);
         myAtomicMax(&s_max[col], coldata);
@@ -77,17 +74,21 @@ __global__ void minmaxKernel(const T* data, const int* rowids,
 }
 
 /**
- * @brief Computes min/max across every column of the input matrix.
+ * @brief Computes min/max across every column of the input matrix, as well as
+ * optionally allow to subsample based on the given row/col ID mapping vectors
  *
  * @tparam T the data type
  * @tparam TPB number of threads per block
  * @param data input data
- * @param rowids actual row ID mappings. If you need to skip a row, pass a -1
- * at that location. If you want to skip this map lookup entirely, pass nullptr
- * @param colids actual col ID mappings. If you need to skip a col, pass a -1
- * at that location. If you want to skip this map lookup entirely, pass nullptr
- * @param nrows number of rows of data
- * @param ncols number of cols of data
+ * @param rowids actual row ID mappings. It is of length nrows. If you want to
+ * skip this index lookup entirely, pass nullptr
+ * @param colids actual col ID mappings. It is of length ncols. If you want to
+ * skip this index lookup entirely, pass nullptr
+ * @param nrows number of rows of data to be worked upon. The actual rows of the
+ * input "data" can be bigger than this!
+ * @param ncols number of cols of data to be worked upon. The actual cols of the
+ * input "data" can be bigger than this!
+ * @param row_stride stride (in number of elements) between 2 adjacent columns
  * @param globalmin final col-wise global minimum (size = ncols)
  * @param globalmax final col-wise global maximum (size = ncols)
  * @param sampledcols output sampled data. Pass nullptr if you don't need this
@@ -100,8 +101,8 @@ __global__ void minmaxKernel(const T* data, const int* rowids,
  */
 template <typename T, int TPB = 512>
 void minmax(const T* data, const int* rowids, const int* colids, int nrows,
-            int ncols, T* globalmin, T* globalmax, T* sampledcols,
-            cudaStream_t stream) {
+            int ncols, int row_stride, T* globalmin, T* globalmax,
+            T* sampledcols, cudaStream_t stream) {
     int nblks = ceildiv(ncols, TPB);
     T init_val = std::numeric_limits<T>::max();
     minmaxInitKernel<T><<<nblks, TPB, 0, stream>>>(ncols, globalmin,
@@ -111,8 +112,8 @@ void minmax(const T* data, const int* rowids, const int* colids, int nrows,
     nblks = max(nblks, 65536);
     size_t smemSize = sizeof(T) * 2 * ncols;
     minmaxKernel<T><<<nblks, TPB, smemSize, stream>>>(
-        data, rowids, colids, nrows, ncols, globalmin, globalmax, sampledcols,
-        init_val);
+        data, rowids, colids, nrows, ncols, row_stride, globalmin, globalmax,
+        sampledcols, init_val);
     CUDA_CHECK(cudaPeekAtLastError());
 }
 
