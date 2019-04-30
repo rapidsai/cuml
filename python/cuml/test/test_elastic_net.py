e1ea001caf54320c148a815c652615cd3bd63430
Salonijain27 <sj7856@g.rit.edu>
2019-04-30 14:58:46 -0500

    Update test_elastic_net.py

--- python/cuml/test/test_elastic_net.py
+++ python/cuml/test/test_elastic_net.py
@@ -13,10 +13,10 @@
 # limitations under the License.
 #
 
-import cuml
 import cudf
 import pytest
 import numpy as np
+import pandas as pd
 from cuml.linear_model import ElasticNet as cuElasticNet
 from sklearn.linear_model import ElasticNet
 from sklearn.datasets import make_regression
@@ -26,49 +26,48 @@ from cuml.test.utils import array_equal
 
 @pytest.mark.parametrize('input_type', ['dataframe', 'ndarray'])
 @pytest.mark.parametrize('selection', ['cyclic', 'random'])
-
 def test_elastic_net(input_type, selection):
-	n_samples = 20
-	n_feats = 5
-	dtype = np.float64
-	train_rows = np.int32(n_samples*0.8)
-	X, y = make_regression(n_samples=n_samples, n_features=n_feats, 
-		n_informative=n_feats, random_state=0)
-	X_test = np.array(X[train_rows:, 0:]).astype(dtype)
-	y_train = np.array(y[0: train_rows, ]).astype(dtype)
-	y_test = np.array(y[train_rows:, ]).astype(dtype)
-	X_train = np.array(X[0: train_rows, :]).astype(dtype)
+    n_samples = 20
+    n_feats = 5
+    dtype = np.float64
+    train_rows = np.int32(n_samples*0.8)
+    X, y = make_regression(n_samples=n_samples, n_features=n_feats,
+                           n_informative=n_feats, random_state=0)
+    X_test = np.array(X[train_rows:, 0:]).astype(dtype)
+    y_train = np.array(y[0: train_rows, ]).astype(dtype)
+    y_test = np.array(y[train_rows:, ]).astype(dtype)
+    X_train = np.array(X[0: train_rows, :]).astype(dtype)
 
-	elastic_sk = ElasticNet(alpha=np.array([0.001]), fit_intercept=True, 
-			normalize=False, max_iter=1000, 
-			selection=selection, tol=1e-10)
-	elastic_sk.fit(X_train, y_train)
-	sk_predict = elastic_sk.predict(X_test)
+    elastic_sk = ElasticNet(alpha=np.array([0.001]), fit_intercept=True,
+                            normalize=False, max_iter=1000,
+                            selection=selection, tol=1e-10)
+    elastic_sk.fit(X_train, y_train)
+    sk_predict = elastic_sk.predict(X_test)
 
-	elastic_cu = cuElasticNet(alpha=np.array([0.001]), fit_intercept=True, 
-			normalize=False, max_iter=1000, 
-			selection=selection, tol=1e-10)
+    elastic_cu = cuElasticNet(alpha=np.array([0.001]), fit_intercept=True,
+                              normalize=False, max_iter=1000,
+                              selection=selection, tol=1e-10)
 
-	if input_type=='datafame':
-		X_train = pd.DataFrame(
-				{'fea%d' %i: X_train[0:, i] for i in range(
-              X_train.shape[1])})
-		y_train = pd.DataFrame(
-				{'fea0': y[0:train_rows, ]})
-		X_test = pd.DataFrame(
-				{'fea%d' %i: X_test[0:, i] for i in range(
-              X_test.shape[1])})
-		X_cudf = cudf.DataFrame.from_pandas(X_train) 
-		y_cudf = y_train.values
-		y_cudf = y_cudf[:, 0]
-		y_cudf = cudf.Series(y_cudf) 
-		X_cudf_test = cudf.DataFrame.from_pandas(X_test)
-		elastic_cu.fit(X_cudf, y_cudf)
-		cu_predict = elastic_cu.predict(X_cudf_test).to_array()
-	else:
-		elastic_cu.fit(X, y)
-		cu_predict = elastic_cu.predict(X_test).to_array()
+    if input_type == 'datafame':
+        X_train = pd.DataFrame(
+                               {'fea%d' % i: X_train[0:, i] for i in range(
+                                                        X_train.shape[1])})
+        y_train = pd.DataFrame(
+                               {'fea0': y[0:train_rows, ]})
+        X_test = pd.DataFrame(
+                              {'fea%d' % i: X_test[0:, i] for i in range(
+                                                           X_test.shape[1])})
+        X_cudf = cudf.DataFrame.from_pandas(X_train)
+        y_cudf = y_train.values
+        y_cudf = y_cudf[:, 0]
+        y_cudf = cudf.Series(y_cudf)
+        X_cudf_test = cudf.DataFrame.from_pandas(X_test)
+        elastic_cu.fit(X_cudf, y_cudf)
+        cu_predict = elastic_cu.predict(X_cudf_test).to_array()
+    else:
+        elastic_cu.fit(X, y)
+        cu_predict = elastic_cu.predict(X_test).to_array()
 
-	error_sk = mean_squared_error(y_test, sk_predict)
-	error_cu = mean_squared_error(y_test, cu_predict)
-	assert array_equal(error_sk, error_cu, 1e-1, with_sign=True)
+    error_sk = mean_squared_error(y_test, sk_predict)
+    error_cu = mean_squared_error(y_test, cu_predict)
+    assert array_equal(error_sk, error_cu, 1e-1, with_sign=True)
