16df1444de59fdc24306e3d9ab8bb2a4bc5de5f7
Corey J. Nolet <cjnolet@gmail.com>
2019-05-01 15:54:59 -0400

    Supervised and semisupervised fit_transform() is passing Leland's pytests!

--- python/cuml/test/test_umap.py
+++ python/cuml/test/test_umap.py
@@ -50,6 +50,26 @@ def test_umap_transform_on_iris():
     trust = trustworthiness(new_data, embedding, 10)
     assert trust >= 0.90
 
+def test_supervised_umap_trustworthiness_on_iris():
+    iris = datasets.load_iris()
+    data = iris.data
+    embedding = UMAP(n_neighbors=10, min_dist=0.01).fit_transform(
+        data, iris.target
+    )
+    trust = trustworthiness(iris.data, embedding, 10)
+    assert trust >= 0.97
+
+def test_semisupervised_umap_trustworthiness_on_iris():
+    iris = datasets.load_iris()
+    data = iris.data
+    target = iris.target.copy()
+    target[25:75] = -1
+    embedding = UMAP(n_neighbors=10, min_dist=0.01).fit_transform(
+        data, target
+    )
+    trust = trustworthiness(iris.data, embedding, 10)
+    assert trust >= 0.97
+
 
 def test_umap_trustworthiness_on_iris():
     iris = datasets.load_iris()
