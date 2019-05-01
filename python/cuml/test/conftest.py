820741a6580f7c3e31521773904d7f72a8fc4c6c
Salonijain27 <sj7856@g.rit.edu>
2019-05-01 01:44:38 -0500

    Update conftest.py

--- python/cuml/test/conftest.py
+++ python/cuml/test/conftest.py
@@ -2,9 +2,12 @@ import pytest
 
 
 def pytest_addoption(parser):
-    parser.addoption("--run_correctness_test", action="store_true", default=False, help="run correctness tests")
+    parser.addoption("--run_correctness_test", action="store_true",
+                     default=False, help="run correctness tests")
     parser.addoption("--run_stress", action="store_true",
                      default=False, help="run stress tests")
+    
+    
 @pytest.fixture
 def run_stress(request):
     return request.config.getoption("--run_stress")
