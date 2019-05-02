71e33329e1ae2b1ed5914b9fe9c7fd1a3c17adf9
John Zedlewski <jzedlewski@nvidia.com>
2019-05-01 17:31:48 -0700

    Update umap docs based on review feedback

--- python/cuml/manifold/umap.pyx
+++ python/cuml/manifold/umap.pyx
@@ -152,14 +152,21 @@ cdef class UMAP:
 
     Notes
     -----
-    This UMAP module is heavily based on Leland McInnes' umap.py package.
+    This module is heavily based on Leland McInnes' reference UMAP package.
     However, there are a number of differences and features that are not yet
     implemented in cuml.umap:
-      * Supervised training (planned for implementation soon)
       * Specifying the random seed
-      * Specifying a custom distance metric (cuml.umap always uses euclidean
-        distance)
+      * Using a non-euclidean distance metric (support for a fixed set
+        of non-euclidean metrics is planned for an upcoming release).
+      * Using a pre-computed pairwise distance matrix (under consideration
+        for future releases)
       * Manual initialization of initial embedding positions
+
+    In addition to these missing features, you should expect to see
+    the final embeddings differing between cuml.umap and the reference
+    UMAP. In particular, the reference UMAP uses an approximate kNN
+    algorithm for large data sizes while cuml.umap always uses exact
+    kNN.
 
     References
     ----------
