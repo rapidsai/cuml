1d7b32447be139198cd1dd3d17e1bb7de5ae3a89
Salonijain27 <sj7856@g.rit.edu>
2019-05-02 15:28:14 -0500

    Update test_sgd.py

--- python/cuml/test/test_sgd.py
+++ python/cuml/test/test_sgd.py
@@ -2,6 +2,9 @@ import numpy as np
 import cudf
 from cuml.solvers import SGD as cumlSGD
 import pytest
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn import datasets
+import pandas as pd
 
 
 @pytest.mark.parametrize('lrate', ['constant', 'invscaling', 'adaptive'])
@@ -9,21 +12,51 @@ import pytest
 @pytest.mark.parametrize('input_type', ['dataframe', 'ndarray'])
 @pytest.mark.parametrize('penalty', ['none', 'l1', 'l2', 'elasticnet'])
 @pytest.mark.parametrize('loss', ['hinge', 'log', 'squared_loss'])
-def test_svd(datatype, lrate, input_type, penalty, loss):
-    X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]], dtype=datatype)
-    y = np.array([1, 1, 2, 2], dtype=datatype)
-    pred_data = np.array([[3.0, 5.0], [2.0, 5.0]]).astype(datatype)
-    if input_type == 'dataframe':
-        X = cudf.DataFrame()
-        X['col1'] = np.asarray([-1, -2, 1, 2], dtype=datatype)
-        X['col2'] = np.asarray([-1, -1, 2, 2], dtype=datatype)
-        y = cudf.Series(np.array(y, dtype=np.float32))
-        pred_data = cudf.DataFrame()
-        pred_data['col1'] = np.asarray([3, 2], dtype=datatype)
-        pred_data['col2'] = np.asarray([5, 5], dtype=datatype)
+def test_svd(datatype, lrate, input_type, penalty,
+             loss, run_stress, run_quality):
+    n_samples = 10000
+    n_feats = 50
+    if run_stress:
+        train_rows = np.int32(n_samples*80)
+        X, y = make_blobs(n_samples=n_samples*50,
+                          n_features=n_feats, random_state=0)
+        X_test = np.array(X[train_rows:, 0:]).astype(datatype)
+        X_train = np.array(X[0:train_rows, :]).astype(datatype)
+        y_train = np.array(y[0:train_rows, ]).astype(datatype)
+
+    elif run_quality:
+        iris = datasets.load_iris()
+        X = iris.data
+        y = iris.target
+        train_rows = np.int32((np.shape(X)[0])*0.8)
+        X_test = np.array(X[train_rows:, 0:]).astype(datatype)
+        X_train = np.array(X[0:train_rows, :]).astype(datatype)
+        y_train = np.array(y[0:train_rows, ]).astype(datatype)
+
+    else:
+        X_train = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]],
+                           dtype=datatype)
+        y_train = np.array([1, 1, 2, 2], dtype=datatype)
+        X_test = np.array([[3.0, 5.0], [2.0, 5.0]]).astype(datatype)
+
     cu_sgd = cumlSGD(learning_rate=lrate, eta0=0.005, epochs=2000,
                      fit_intercept=True, batch_size=2,
                      tol=0.0, penalty=penalty, loss=loss)
-    cu_sgd.fit(X, y)
-    cu_pred = cu_sgd.predict(pred_data).to_array()
+
+    if input_type == 'dataframe':
+        y_train_pd = pd.DataFrame({'fea0': y_train[0:, ]})
+        X_train_pd = pd.DataFrame(
+                     {'fea%d' % i: X_train[0:, i] for i in range(
+                             X_train.shape[1])})
+        X_test_pd = pd.DataFrame(
+                     {'fea%d' % i: X_test[0:, i] for i in range(
+                             X_test.shape[1])})
+        X_train = cudf.DataFrame.from_pandas(X_train_pd)
+        X_test = cudf.DataFrame.from_pandas(X_test_pd)
+        y_train = y_train_pd.values
+        y_train = y_train[:, 0]
+        y_train = cudf.Series(y_train)
+
+    cu_sgd.fit(X_train, y_train)
+    cu_pred = cu_sgd.predict(X_test).to_array()
     print("cuML predictions : ", cu_pred)
