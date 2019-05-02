009344dc09d1223880e5a96915a994895d76a076
Kamesh Arumugam <a.k.kamesh001@gmail.com>
2019-05-01 20:27:12 -0700

    add single gpu kmeans implementation which is built using ml-prims

--- cuML/examples/kmeans/kmeans_example.cpp
+++ cuML/examples/kmeans/kmeans_example.cpp
@@ -23,7 +23,17 @@
 
 #include <cuda_runtime.h>
 
-#include <kmeans/kmeans_c.h>
+#ifdef HAVE_CUB
+#include <cub/util_allocator.cuh>
+#endif //HAVE_CUB
+
+#ifdef HAVE_RMM
+#include <rmm/rmm.h>
+#include <common/rmmAllocatorAdapter.hpp>
+#endif //HAVE_RMM
+
+#include <cuML.hpp>
+#include <kmeans/kmeans.hpp>
 
 #ifndef CUDA_RT_CALL
 #define CUDA_RT_CALL( call )                                                                       \
@@ -54,6 +64,38 @@ bool get_arg(char ** begin, char ** end, const std::string& arg) {
     return false;
 }
 
+class cachingDeviceAllocator : public ML::deviceAllocator {
+public:
+    cachingDeviceAllocator()
+#ifdef HAVE_CUB
+          , _allocator(8, 3, cub::CachingDeviceAllocator::INVALID_BIN, cub::CachingDeviceAllocator::INVALID_SIZE)
+#endif //HAVE_CUB
+    {}
+
+    virtual void* allocate( std::size_t n, cudaStream_t stream ) {
+        void* ptr = 0;
+#ifdef HAVE_CUB
+        _allocator.DeviceAllocate( &ptr, n, stream );
+#else //!HAVE_CUB
+        CUDA_RT_CALL( cudaMalloc( &ptr, n ) );
+#endif //HAVE_CUB
+        return ptr;
+    }
+
+    virtual void deallocate( void* p, std::size_t, cudaStream_t ) {
+#ifdef HAVE_CUB
+        _allocator.DeviceFree(p);
+#else //!HAVE_CUB
+        CUDA_RT_CALL( cudaFree(p) );
+#endif //HAVE_CUB
+    }
+
+#ifdef HAVE_CUB
+private:
+    cub::CachingDeviceAllocator _allocator;
+#endif //HAVE_CUB
+};
+
 int main(int argc, char * argv[])
 {
     const int dev_id        = get_argval<int>(argv, argv+argc,"-dev_id", 0);
@@ -78,6 +120,16 @@ int main(int argc, char * argv[])
             std::cerr<<"ERROR: Could not initialize CUDA on device: "<<dev_id<<"("<<cudaGetErrorString(cudaStatus)<<")"<<std::endl;
             return 1;
         }
+#ifdef HAVE_RMM
+	rmmOptions_t rmmOptions;
+	rmmOptions.allocation_mode = PoolAllocation;
+	rmmOptions.initial_pool_size = 0;
+	rmmOptions.enable_logging = false;
+	rmmError_t rmmStatus = rmmInitialize(&rmmOptions);
+	if ( RMM_SUCCESS != rmmStatus ){
+	    std::cerr<<"WARN: Could not initialize RMM: "<<rmmGetErrorString(rmmStatus)<<std::endl;
+	}
+#endif //HAVE_RMM
     }
     
     std::vector<double> h_srcdata;
@@ -100,73 +152,81 @@ int main(int argc, char * argv[])
     bool results_correct = true;
     if ( 0 == h_srcdata.size() || (num_rows*num_cols) == h_srcdata.size() )
     {
-        int k_max = k;
-
         double threshold = 1.0E-4; //Scikit-Learn default
 
         //Input parameters copied from kmeans_test.cu
         if (0 == h_srcdata.size()) {
             k = 2;
-            k_max = k;
             max_iterations = 300;
             threshold = 0.05;
         }
-        int dopredict = 0;
-        int verbose = 0;
-        int seed = 1;
-        int n_gpu = 1;
-        int init_from_data = 0;
+        int seed = 0;
+	int metric = 1;
+	ML::kmeans::InitMethod method = ML::kmeans::InitMethod::Random;
         
         //Inputs copied from kmeans_test.cu
-        size_t mTrain = 4; 
-        size_t n = 2;
-        char ord = 'c'; // here c means col order, NOT C (vs F) order
+ 	size_t n_samples = 4;
+	size_t n_features = 2;
         if (0 == h_srcdata.size())
         {
-            h_srcdata = {1.0,1.0,3.0,4.0, 1.0,2.0,2.0,3.0};
+            h_srcdata = {1.0,1.0,
+			 3.0,4.0,
+			 1.0,2.0,
+			 2.0,3.0};
         }
         else
         {
-            ord = 'r'; // r means row order
-            mTrain = num_rows;
-            n = num_cols;
+	    n_samples = num_rows; 
+	    n_features = num_cols;
         }
         std::cout<<"Run KMeans with k="<<k<<", max_iterations="<<max_iterations<<std::endl;
 
-        //srcdata size n * mTrain
-        double *srcdata = nullptr;
-        CUDA_RT_CALL( cudaMalloc(&srcdata, n*mTrain*sizeof(double) ) );
-        CUDA_RT_CALL( cudaMemcpy( srcdata, h_srcdata.data(), n*mTrain*sizeof(double), cudaMemcpyHostToDevice ) );
+	ML::cumlHandle cumlHandle;
+#ifdef HAVE_RMM
+	std::shared_ptr<ML::rmmAllocatorAdapter> allocator( new ML::rmmAllocatorAdapter() );
+#else //!HAVE_RMM
+	std::shared_ptr<cachingDeviceAllocator> allocator( new cachingDeviceAllocator() );
+#endif //HAVE_RMM
+	cumlHandle.setDeviceAllocator( allocator );
 
-        // centroids can be nullpr
-        double *centroids = nullptr;
+	cudaStream_t stream;
+	CUDA_RT_CALL( cudaStreamCreate( &stream ) );
+	cumlHandle.setStream( stream );
 
-        //output pred_centroids size k * n
-        double *pred_centroids = nullptr;
-        CUDA_RT_CALL( cudaMalloc(&pred_centroids, k*n*sizeof(double) ) );
-        //output pred_labels size mTrain
-        int *pred_labels = nullptr;
-        CUDA_RT_CALL( cudaMalloc(&pred_labels, mTrain*sizeof(int) ) );
+        //srcdata size n_samples * n_features
+        double *d_srcdata = nullptr;
+	CUDA_RT_CALL( cudaMalloc(&d_srcdata, n_samples * n_features * sizeof(double) ) );
+	CUDA_RT_CALL( cudaMemcpyAsync(d_srcdata, h_srcdata.data(), n_samples * n_features * sizeof(double), cudaMemcpyHostToDevice, stream ) );
 
-        ML::make_ptr_kmeans(dopredict, verbose, seed, dev_id, n_gpu, mTrain, n, ord, k, k_max, max_iterations, init_from_data, threshold, srcdata, centroids, pred_centroids, pred_labels);
+        //output pred_centroids size k * n_features
+        double *d_pred_centroids = nullptr;
+        CUDA_RT_CALL( cudaMalloc(&d_pred_centroids, k*n_features*sizeof(double) ) );
+        //output pred_labels size n_samples
+        int *d_pred_labels = nullptr;
+        CUDA_RT_CALL( cudaMalloc(&d_pred_labels, n_samples*sizeof(int) ) );
 
-        std::vector<int> h_pred_labels(mTrain);
-        CUDA_RT_CALL( cudaMemcpy( h_pred_labels.data(), pred_labels, mTrain*sizeof(int), cudaMemcpyDeviceToHost ) );
-        std::vector<double> h_pred_centroids(k * n);
-        CUDA_RT_CALL( cudaMemcpy( h_pred_centroids.data(), pred_centroids, k*n*sizeof(double), cudaMemcpyDeviceToHost ) );
+	ML::kmeans::fit_predict(cumlHandle, k, metric, method, max_iterations, threshold, seed, d_srcdata, n_samples, n_features, d_pred_centroids, d_pred_labels);
+
+        std::vector<int> h_pred_labels(n_samples);
+        CUDA_RT_CALL( cudaMemcpyAsync( h_pred_labels.data(), d_pred_labels, n_samples*sizeof(int), cudaMemcpyDeviceToHost, stream ) );
+        std::vector<double> h_pred_centroids(k * n_features);
+        CUDA_RT_CALL( cudaMemcpyAsync( h_pred_centroids.data(), d_pred_centroids, k*n_features*sizeof(double), cudaMemcpyDeviceToHost, stream ) );
+
+	CUDA_RT_CALL( cudaStreamSynchronize( stream ) );
 
         if (8 == h_srcdata.size())
         {
-            int h_labels_ref_fit[mTrain] = {1, 1, 0, 0};
-            for ( int i = 0; i < mTrain; ++i ) {
+	    int h_labels_ref_fit[n_samples] = {0, 1, 0, 1};
+            for ( int i = 0; i < n_samples; ++i ) {
                 if ( h_labels_ref_fit[i] != h_pred_labels[i] ) {
                 std::cerr<<"ERROR: h_labels_ref_fit["<<i<<"] = "<<h_labels_ref_fit[i]<<" != "<<h_pred_labels[i]<<" = h_pred_labels["<<i<<"]!"<<std::endl;
                     results_correct = false;
                 }
             }
 
-            double h_centroids_ref[k * n] = {3.5,2.5, 1.0,1.5};
-            for ( int i = 0; i < k * n; ++i ) {
+	    double h_centroids_ref[k * n_features] = {1.0, 1.5,
+						      2.5, 3.5};
+            for ( int i = 0; i < k * n_features; ++i ) {
                 if ( std::abs(h_centroids_ref[i] - h_pred_centroids[i])/std::abs(h_centroids_ref[i]) > std::numeric_limits<double>::epsilon() ) {
                 std::cerr<<"ERROR: h_centroids_ref["<<i<<"] = "<<h_centroids_ref[i]<<" !~= "<<h_pred_centroids[i]<<" = h_pred_centroids["<<i<<"]!"<<std::endl;
                     results_correct = false;
@@ -178,15 +238,15 @@ int main(int argc, char * argv[])
             std::vector<std::pair<size_t,double> > cluster_stats(k,std::make_pair(static_cast<size_t>(0),0.0));
             double global_inertia = 0.0;
             size_t max_points = 0;
-            for ( size_t i = 0; i < mTrain; ++i ) {
+            for ( size_t i = 0; i < n_samples; ++i ) {
                 int label = h_pred_labels[i];
                 cluster_stats[label].first += 1;
                 max_points = std::max(cluster_stats[label].first,max_points);
                 
                 double sd = 0.0;
-                for ( int j = 0; j < n; ++j ) {
-                    const double cluster_centroid_comp = h_pred_centroids[label*n+j];
-                    const double point_comp = h_srcdata[i*n+j];
+                for ( int j = 0; j < n_features; ++j ) {
+                    const double cluster_centroid_comp = h_pred_centroids[label*n_features+j];
+                    const double point_comp = h_srcdata[i*n_features+j];
                     sd += (cluster_centroid_comp-point_comp)*(cluster_centroid_comp-point_comp);
                 }
                 cluster_stats[label].second += sd;
@@ -217,18 +277,24 @@ int main(int argc, char * argv[])
             std::cout<<"Global inertia = "<<global_inertia<<std::endl;
         }
 
-        CUDA_RT_CALL( cudaFree( pred_labels ) );
-        pred_labels  = nullptr;
-        CUDA_RT_CALL( cudaFree( pred_centroids ) );
-        pred_centroids  = nullptr;
-        CUDA_RT_CALL( cudaFree( srcdata ) );
-        srcdata  = nullptr;
+        CUDA_RT_CALL( cudaFree( d_pred_labels ) );
+        d_pred_labels  = nullptr;
+        CUDA_RT_CALL( cudaFree( d_pred_centroids ) );
+        d_pred_centroids  = nullptr;
+        CUDA_RT_CALL( cudaFree( d_srcdata ) );
+        d_srcdata  = nullptr;
+	CUDA_RT_CALL( cudaStreamDestroy( stream ) );	
     }
     else
     {
         std::cerr<<"ERROR: Number of input values = "<<h_srcdata.size()<<" != "<<num_rows*num_cols<<" = "<<num_rows<<"*"<<num_cols<<" !"<<std::endl;
         return 1;
     }
+#ifdef HAVE_RMM
+    if ( rmmIsInitialized( NULL ) ){
+	rmmFinalize();
+    }
+#endif //HAVE_RMM
     CUDA_RT_CALL( cudaDeviceReset() );
     return results_correct ? 0 : 1;
 }
---
+++ cuML/src/kmeans/kmeans.cuh
@@ -0,0 +1,216 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <linalg/matrix_vector_op.h>
+#include <linalg/reduce_rows_by_key.h>
+#include <linalg/mean_squared_error.h>
+#include <linalg/binary_op.h>
+#include <distance/distance.h>
+#include <matrix/gather.h>
+#include <random>
+#include <random/rng.h>
+#include <random/permute.h>
+
+#include <numeric>
+#include <thrust/fill.h>
+#include <thrust/for_each.h>
+#include <thrust/scan.h>
+#include <thrust/execution_policy.h>
+
+#include <ml_cuda_utils.h>
+#include <common/tensor.hpp>
+#include <common/cumlHandle.hpp>
+#include <common/device_buffer.hpp>
+#include <common/host_buffer.hpp>
+#include <cuML.hpp>
+
+#include "kmeans.hpp"
+
+namespace ML {
+using namespace MLCommon;
+
+/**
+ * @tparam DataT 
+ * @tparam IndexT
+ */
+template <typename DataT,
+	  typename IndexT = int>
+class KMeans{
+public:
+
+    struct Options{
+	int seed;
+	int oversampling_factor;
+	Random::GeneratorType gtype;
+	bool inertia_check;
+    public:
+	Options(){
+	    seed = -1;
+	    oversampling_factor = 0;
+	    gtype = Random::GeneratorType::GenPhilox;
+	    inertia_check = false;
+	}
+    };
+
+    
+    /**
+     * @param[in] cumlHandle  The handle to the cuML library context that manages the CUDA resources.
+     * @param[in] n_clusters  The number of clusters to form as well as the number of centroids to generate (default:8).
+     * @param[in] metric      Metric to use for distance computation. Any metric from MLCommon::Distance::DistanceType can be used
+     * @param[in] init        Method for initialization, defaults to k-means++:
+     *                            - InitMethod::KMeansPlusPlus (k-means++): Use scalable k-means++ algorithm to select the initial cluster centers.
+     *                            - InitMethod::Random (random): Choose 'n_clusters' observations (rows) at random from the input data for the initial centroids.
+     *                            - InitMethod::Array (ndarray): Use the user provided input for initial centroids, it should be set using setCentroids method. 
+     * @param[in] max_iter    Maximum number of iterations for the k-means algorithm.
+     * @param[in] tol         Relative tolerance with regards to inertia to declare convergence.
+     * @param[in] seed        Seed to the random number generator.
+     */
+    KMeans(const ML::cumlHandle_impl &cumlHandle,
+	   int n_clusters = 8,
+	   int metric = static_cast<MLCommon::Distance::DistanceType>(0),
+	   kmeans::InitMethod init = kmeans::InitMethod::KMeansPlusPlus,
+	   int max_iter = 300,
+	   double tol = 1e-4,
+	   int seed = -1,
+	   int verbose = 0);
+
+
+    /**
+     * @brief Compute k-means clustering.
+     *
+     * @param[in] X          Training instances to cluster. It must be noted that the data must be in row-major format and stored in device accessible location.
+     * @param[in] n_samples  Number of samples in the input X.
+     * @param[in] n_features Number of features or the dimensions of each sample.
+     */
+    void
+    fit(const DataT *X,
+	int n_samples,
+	int n_features);
+
+
+    /**
+     * @brief Predict the closest cluster each sample in X belongs to.
+     *
+     * @param[in]  X          New data to predict.
+     * @param[in]  n_samples  Number of samples in the input X.
+     * @param[in]  n_features Number of features or the dimensions of each sample.
+     * @param[out] labels     Index of the cluster each sample belongs to.
+     */
+    void
+    predict(const DataT *X,
+	    int n_samples,
+	    int n_features,
+	    IndexT *labels);
+
+    /**
+     * @brief Transform X to a cluster-distance space. In the new space, each dimension is the distance to the cluster centers.
+     *
+     * @param[in]  X          New data to transform.
+     * @param[in]  n_samples  Number of samples in the input X.
+     * @param[in]  n_features Number of features or the dimensions of each sample.
+     * @param[out] X_new      X transformed in the new space (output size is [n_samples x n_clusters].
+     */
+    void
+    transform(const DataT *X,
+	      int n_samples,
+	      int n_features,
+	      DataT *X_new);
+ 
+
+    /**
+     * @brief Set centroids to be user provided value X
+     *
+     * @param[in]  X          New data to centroids.
+     * @param[in]  n_samples  Number of samples in the input X.
+     * @param[in]  n_features Number of features or the dimensions of each sample.
+     */
+    void
+    setCentroids(const DataT *X,
+		 int n_samples,
+		 int n_features);
+    
+    // returns the raw pointer to the generated centroids
+    DataT*
+    centroids();
+
+
+    // release local resources
+    ~KMeans();
+
+  
+    // internal functions (ideally must be private methods, but its not possible today because of the lambda limitations of CUDA compiler)
+    void
+    fit(Tensor<DataT, 2, IndexT> &);
+
+    void
+    predict(Tensor<DataT, 2, IndexT> &);
+       
+    void
+    initRandom(Tensor<DataT, 2, IndexT> &);
+    
+    void
+    initKMeansPlusPlus(Tensor<DataT, 2, IndexT> &);
+    
+    
+
+private:
+
+    // Maximum number of iterations of the k-means algorithm for a single run.
+    int max_iter;
+
+    // Relative tolerance with regards to inertia to declare convergence.
+    double tol;
+
+    // Sum of squared distances of samples to their closest cluster center.
+    double inertia;
+
+    // Number of iterations run.
+    int n_iter;
+  
+    // number of clusters to form as well as the number of centroids to generate.
+    int n_clusters;
+
+    // number of features or the dimensions of the input.
+    int _n_features;
+
+    // verbosity mode.  
+    int _verbose;
+  
+    // Device-accessible allocation of expandable storage used as temorary buffers
+    MLCommon::device_buffer<char> _workspace;
+
+    // underlying expandable storage that holds centroids data 
+    MLCommon::device_buffer<DataT> _centroidsRawData;
+    
+    // underlying expandable storage that holds labels 
+    MLCommon::device_buffer<IndexT> _labelsRawData;
+       
+    const ML::cumlHandle_impl &_handle; 
+        
+    // method for initialization, defaults to 'k-means++':
+    kmeans::InitMethod _init;
+
+    MLCommon::Distance::DistanceType _metric;
+
+    // optional 
+    Options options;
+};
+}
+
+#include "kmeans-inl.cuh"
+
