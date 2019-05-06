7168d05f4577630e6fc2d4f24da5cc4013015ce0
Salonijain27 <sj7856@g.rit.edu>
2019-05-06 18:18:18 -0500

    Update test_kmeans.py

--- python/cuml/test/test_kmeans.py
+++ python/cuml/test/test_kmeans.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019, NVIDIA CORPORATION.
+# Copyright (c) 2018, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,12 +14,15 @@
 #
 
 import pytest
+
 import numpy as np
+
 import cuml
+
 from sklearn import cluster
 from sklearn.preprocessing import StandardScaler
-from cuml.test.utils import fit_predict, get_pattern, clusters_equal
 
+from cuml.test.utils import fit_predict, get_pattern, clusters_equal
 
 dataset_names = ['blobs', 'noisy_circles'] + \
                 [pytest.param(ds, marks=pytest.mark.xfail)
@@ -27,9 +30,8 @@ dataset_names = ['blobs', 'noisy_circles'] + \
 
 
 @pytest.mark.parametrize('name', dataset_names)
-def test_kmeans_sklearn_comparison(name, run_stress, run_quality):
-    if name == 'noisy_circles':
-        pytest.skip('fails with noisy_circles')
+def test_kmeans_sklearn_comparison(name):
+
     default_base = {'quantile': .3,
                     'eps': .3,
                     'damping': .9,
@@ -39,15 +41,15 @@ def test_kmeans_sklearn_comparison(name, run_stress, run_quality):
 
     pat = get_pattern(name, 10000)
 
-    else:
-        pat = get_pattern(name, np.int32(n_samples/2))
-        params = default_base.copy()
-        params.update(pat[1])
-        X, y = pat[0]
+    params = default_base.copy()
+    params.update(pat[1])
 
-    X = StandardScaler().fit_transform(X)
     kmeans = cluster.KMeans(n_clusters=params['n_clusters'])
     cuml_kmeans = cuml.KMeans(n_clusters=params['n_clusters'])
+
+    X, y = pat[0]
+
+    X = StandardScaler().fit_transform(X)
 
     clustering_algorithms = (
         ('sk_Kmeans', kmeans),
@@ -57,10 +59,12 @@ def test_kmeans_sklearn_comparison(name, run_stress, run_quality):
     sk_y_pred, _ = fit_predict(clustering_algorithms[0][1],
                                clustering_algorithms[0][0], X)
 
-    kmeans.fit(X)
     cu_y_pred, _ = fit_predict(clustering_algorithms[1][1],
                                clustering_algorithms[1][0], X)
 
+    # Noisy circles clusters are rotated in the results,
+    # since we are comparing 2 we just need to compare that both clusters
+    # have approximately the same number of points.
     if name == 'noisy_circles':
         assert (np.sum(sk_y_pred) - np.sum(cu_y_pred))/len(sk_y_pred) < 2e-3
 
