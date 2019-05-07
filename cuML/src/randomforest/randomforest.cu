e333239dd140627961192ec59b9dbf068cc5a9ba
Myrto Papadopoulou <myrtop@nvidia.com>
2019-05-07 06:19:12 -0700

    Added rfRegressor class to random forest.
    - Added rfRegressor class to random forest, along with relevant stateless API functions.
      Implementation currently commented out as DecisionTreeRegressor is not yet implemented.
    - Updated RF_metrics struct to include regression metrics too. Metrics supported
      are mean absolute error, mean squared error and median absolute error as per SKL-rf.

--- cuML/src/randomforest/randomforest.cu
+++ cuML/src/randomforest/randomforest.cu
@@ -23,13 +23,30 @@ namespace ML {
  * @brief Construct RF_metrics.
  * @param[in] cfg_accuracy: accuracy.
  */
-RF_metrics::RF_metrics(float cfg_accuracy) : accuracy(cfg_accuracy) {};
+RF_metrics::RF_metrics(float cfg_accuracy) : rf_type(RF_type::CLASSIFICATION), accuracy(cfg_accuracy) {};
 
 /**
- * @brief Print accuracy metric.
+ * @brief Construct RF_metrics.
+ * @param[in] cfg_mean_abs_error: mean absolute error.
+ * @param[in] cfg_mean_squared_error: mean squared error.
+ * @param[in] cfg_median_abs_error: median absolute error.
+ */
+RF_metrics::RF_metrics(double cfg_mean_abs_error, double cfg_mean_squared_error, double cfg_median_abs_error) :
+	rf_type(RF_type::REGRESSION), mean_abs_error(cfg_mean_abs_error), mean_squared_error(cfg_mean_squared_error),
+    median_abs_error(cfg_median_abs_error) {};
+
+/**
+ * @brief Print either accuracy metric for classification, or mean absolute error, mean squared error, 
+   and median absolute error metrics for regression.
  */
 void RF_metrics::print() {
-	std::cout << "Accuracy: " << accuracy << std::endl;
+	if (rf_type == RF_type::CLASSIFICATION) {
+		std::cout << "Accuracy: " << accuracy << std::endl;
+	} else if (rf_type == RF_type::REGRESSION) {
+		std::cout << "Mean Absolute Error: " << mean_abs_error << std::endl;
+		std::cout << "Mean Squared Error: " << mean_squared_error << std::endl;
+		std::cout << "Median Absolute Error: " << median_abs_error << std::endl;
+	}
 }
 
 /**
@@ -340,13 +357,180 @@ RF_metrics rfClassifier<T>::cross_validate(const cumlHandle& user_handle, const 
 	return stats;
 }
 
+
+/**
+ * @brief Construct rfRegressor object.
+ * @tparam T: data type for input data (float or double).
+ * @param[in] cfg_rf_params: Random forest hyper-parameter struct.
+ */
+template <typename T>
+rfRegressor<T>::rfRegressor(RF_params cfg_rf_params): rf<T>::rf(cfg_rf_params, RF_type::REGRESSION) {};
+
+/**
+ * @brief Build (i.e., fit, train) random forest regressor for input data.
+ * @tparam T: data type for input data (float or double).
+ * @param[in] user_handle: cumlHandle
+ * @param[in] input: train data (n_rows samples, n_cols features) in column major format, excluding labels. Device pointer.
+ * @param[in] n_rows: number of training data samples.
+ * @param[in] n_cols: number of features (i.e., columns) excluding target feature.
+ * @param[in] labels: 1D array of target features (float or double), with one label per training sample. Device pointer.
+ */
+template <typename T>
+void rfRegressor<T>::fit(const cumlHandle& user_handle, T * input, int n_rows, int n_cols, T * labels) {
+
+	ASSERT(!this->trees, "Cannot fit an existing forest.");
+	ASSERT((n_rows > 0), "Invalid n_rows %d", n_rows);
+	ASSERT((n_cols > 0), "Invalid n_cols %d", n_cols);
+
+#if 0 // commented out as DecisionTreeRegressor not implemented
+	rfRegressor::trees = new DecisionTree::DecisionTreeRegressor<T>[this->rf_params.n_trees];
+	int n_sampled_rows = this->rf_params.rows_sample * n_rows;
+
+	const cumlHandle_impl& handle = user_handle.getImpl();
+	cudaStream_t stream = user_handle.getStream();
+
+	for (int i = 0; i < this->rf_params.n_trees; i++) {
+		// Select n_sampled_rows (with replacement) numbers from [0, n_rows) per tree.
+		// selected_rows: randomly generated IDs for bootstrapped samples (w/ replacement); a device ptr.
+		MLCommon::device_buffer<unsigned int> selected_rows(handle.getDeviceAllocator(), stream, n_sampled_rows);
+
+		if (this->rf_params.bootstrap) {
+			MLCommon::Random::Rng r(i * 1000); // Ensure the seed for each tree is different and meaningful.
+			r.uniformInt(selected_rows.data(), n_sampled_rows, (unsigned int) 0, (unsigned int) n_rows, stream);
+		} else {
+			std::vector<unsigned int> h_selected_rows(n_rows);
+			std::iota(h_selected_rows.begin(), h_selected_rows.end(), 0);
+			std::random_shuffle(h_selected_rows.begin(), h_selected_rows.end());
+			h_selected_rows.resize(n_sampled_rows);
+			MLCommon::updateDevice(selected_rows.data(), h_selected_rows.data(), n_sampled_rows, stream);
+		}
+
+		/* Build individual tree in the forest.
+		   - input is a pointer to orig data that have n_cols features and n_rows rows.
+		   - n_sampled_rows: # rows sampled for tree's bootstrap sample.
+		   - selected_rows: points to a list of row #s (w/ n_sampled_rows elements) used to build the bootstrapped sample.
+			Expectation: Each tree node will contain (a) # n_sampled_rows and (b) a pointer to a list of row numbers w.r.t original data.
+		*/
+		this->trees[i].fit(user_handle, input, n_cols, n_rows, labels, selected_rows.data(), n_sampled_rows, /*n_unique_labels,*/ this->rf_params.tree_params);
+
+		//Cleanup
+		selected_rows.release(stream);
+	}
+#endif
+
+}
+
+/**
+ * @brief Predict target feature for input data; regression for single feature supported.
+ * @tparam T: data type for input data (float or double).
+ * @param[in] user_handle: cumlHandle (currently unused; API placeholder)
+ * @param[in] input: test data (n_rows samples, n_cols features) in row major format. CPU pointer.
+ * @param[in] n_rows: number of  data samples.
+ * @param[in] n_cols: number of features (excluding target feature).
+ * @param[in, out] predictions: n_rows predicted labels. CPU pointer, user allocated.
+ * @param[in] verbose: flag for debugging purposes.
+ */
+template<typename T>
+void rfRegressor<T>::predict(const cumlHandle& user_handle, const T * input, int n_rows, int n_cols, T * predictions, bool verbose) const {
+
+	ASSERT(this->trees, "Cannot predict! No trees in the forest.");
+	ASSERT((n_rows > 0), "Invalid n_rows %d", n_rows);
+	ASSERT((n_cols > 0), "Invalid n_cols %d", n_cols);
+	ASSERT(predictions != nullptr, "Error! User has not allocated memory for predictions.");
+
+#if 0 // commented out as DecisionTreeRegressor not implemented
+	int row_size = n_cols;
+
+	for (int row_id = 0; row_id < n_rows; row_id++) {
+
+		if (verbose) {
+			std::cout << "\n\n";
+			std::cout << "Predict for sample: ";
+			for (int i = 0; i < n_cols; i++) std::cout << input[row_id*row_size + i] << ", ";
+			std::cout << std::endl;
+		}
+
+		T sum_predictions = 0;
+
+		for (int i = 0; i < this->rf_params.n_trees; i++) {
+			//Return prediction for one sample.
+			if (verbose) {
+				std::cout << "Printing tree " << i << std::endl;
+				this->trees[i].print();
+			}
+			T prediction;
+			this->trees[i].predict(user_handle, &input[row_id * row_size], 1, n_cols, &prediction, verbose);
+			sum_predictions += prediction;
+		}
+		// Random forest's prediction is the arithmetic mean of all its decision tree predictions.
+		predictions[row_id] = sum_predictions / this->rf_params.n_trees;
+	}
+#endif
+}
+
+/**
+ * @brief Predict target feature for input data and validate against ref_labels.
+ * @tparam T: data type for input data (float or double).
+ * @param[in] user_handle: cumlHandle (currently unused; API placeholder)
+ * @param[in] input: test data (n_rows samples, n_cols features) in row major format. CPU pointer.
+ * @param[in] ref_labels: label values for cross validation (n_rows elements); CPU pointer.
+ * @param[in] n_rows: number of  data samples.
+ * @param[in] n_cols: number of features (excluding target feature).
+ * @param[in, out] predictions: n_rows predicted labels. CPU pointer, user allocated.
+ * @param[in] verbose: flag for debugging purposes.
+ */
+template<typename T>
+RF_metrics rfRegressor<T>::cross_validate(const cumlHandle& user_handle, const T * input, const T * ref_labels, int n_rows, int n_cols, T * predictions, bool verbose) const {
+
+	predict(user_handle, input, n_rows, n_cols, predictions, verbose);
+
+#if 0 // commented out as DecisionTreeRegressor not implemented
+	double abs_difference_sum = 0;
+	double mse_sum = 0;
+	std::vector<double> abs_diffs;
+
+	for (int i = 0; i < n_rows; i++) {
+		double abs_diff = abs(predictions[i] - ref_labels[i]);
+		abs_difference_sum += abs_diff;
+		mse_sum += pow(predictions[i] - ref_labels[i], 2);
+		abs_diffs.push_back(abs_diff);
+	}
+
+	double mean_abs_error = abs_difference_sum / n_rows;
+	double mean_squared_error = mse_sum / n_rows;
+
+	std::sort(abs_diffs.begin(), abs_diffs.end());
+	double median_abs_error = 0;
+	int middle = n_rows / 2;
+	if (n_rows % 2 == 1) {
+		median_abs_error = abs_diffs[middle];	
+	} else {
+		median_abs_error = (abs_diffs[middle] + abs_diffs[middle - 1]) / 2;
+	}
+
+	RF_metrics stats(mean_abs_error, mean_squared_error, median_abs_error);
+	if (verbose) stats.print();
+
+	return stats;
+#endif
+   RF_metrics placeholder(0); // temp. code so code compiles. TODO remove once code above is uncommented.
+   return placeholder;
+}
+
+//=================== regression end
+
 template class rf<float>;
 template class rf<double>;
 
 template class rfClassifier<float>;
 template class rfClassifier<double>;
 
+template class rfRegressor<float>;
+template class rfRegressor<double>;
+
 // Stateless API functions: fit, predict and cross_validate
+
+// ----------------------------- Classification ----------------------------------- //
 
 /**
  * @brief Build (i.e., fit, train) random forest classifier for input data of type float.
@@ -438,6 +622,94 @@ RF_metrics cross_validate(const cumlHandle& user_handle, const rfClassifier<floa
 RF_metrics cross_validate(const cumlHandle& user_handle, const rfClassifier<double> * rf_classifier, const double * input, const int * ref_labels,
 							int n_rows, int n_cols, int * predictions, bool verbose) {
 	return rf_classifier->cross_validate(user_handle, input, ref_labels, n_rows, n_cols, predictions, verbose);
+}
+
+// ----------------------------- Regression ----------------------------------- //
+
+/**
+ * @brief Build (i.e., fit, train) random forest regressor for input data of type float.
+ * @param[in] user_handle: cumlHandle
+ * @param[in,out] rf_regreesor: pointer to the rfRegressor object, previously constructed by the user.
+ * @param[in] input: train data (n_rows samples, n_cols features) in column major format, excluding labels. Device pointer.
+ * @param[in] n_rows: number of training data samples.
+ * @param[in] n_cols: number of features (i.e., columns) excluding target feature.
+ * @param[in] labels: 1D array of target features (float), with one label per training sample. Device pointer.
+ */
+void fit(const cumlHandle& user_handle, rfRegressor<float> * rf_regressor, float * input, int n_rows, int n_cols, float * labels) {
+	rf_regressor->fit(user_handle, input, n_rows, n_cols, labels);
+}
+
+/**
+ * @brief Build (i.e., fit, train) random forest regressor for input data of type double.
+ * @param[in] user_handle: cumlHandle
+ * @param[in,out] rf_regressor: pointer to the rfRegressor object, previously constructed by the user.
+ * @param[in] input: train data (n_rows samples, n_cols features) in column major format, excluding labels. Device pointer.
+ * @param[in] n_rows: number of training data samples.
+ * @param[in] n_cols: number of features (i.e., columns) excluding target feature.
+ * @param[in] labels: 1D array of target features (double), with one label per training sample. Device pointer.
+ */
+void fit(const cumlHandle& user_handle, rfRegressor<double> * rf_regressor, double * input, int n_rows, int n_cols, double * labels) {
+	rf_regressor->fit(user_handle, input, n_rows, n_cols, labels);
+}
+
+/**
+ * @brief Predict target feature for input data of type float; regression for single feature supported.
+ * @param[in] user_handle: cumlHandle (currently unused; API placeholder)
+ * @param[in] rf_regressor: pointer to the rfRegressor object. The user should have previously called fit to build the random forest.
+ * @param[in] input: test data (n_rows samples, n_cols features) in row major format. CPU pointer.
+ * @param[in] n_rows: number of  data samples.
+ * @param[in] n_cols: number of features (excluding target feature).
+ * @param[in, out] predictions: n_rows predicted labels. CPU pointer, user allocated.
+ * @param[in] verbose: flag for debugging purposes.
+ */
+void predict(const cumlHandle& user_handle, const rfRegressor<float> * rf_regressor, const float * input, int n_rows, int n_cols, float * predictions, bool verbose) {
+	rf_regressor->predict(user_handle, input, n_rows, n_cols, predictions, verbose);
+}
+
+/**
+ * @brief Predict target feature for input data of type double; regression for single feature supported.
+ * @param[in] user_handle: cumlHandle (currently unused; API placeholder)
+ * @param[in] rf_regressor: pointer to the rfRegressor object. The user should have previously called fit to build the random forest.
+ * @param[in] input: test data (n_rows samples, n_cols features) in row major format. CPU pointer.
+ * @param[in] n_rows: number of  data samples.
+ * @param[in] n_cols: number of features (excluding target feature).
+ * @param[in, out] predictions: n_rows predicted labels. CPU pointer, user allocated.
+ * @param[in] verbose: flag for debugging purposes.
+ */
+void predict(const cumlHandle& user_handle, const rfRegressor<double> * rf_regressor, const double * input, int n_rows, int n_cols, double * predictions, bool verbose) {
+	rf_regressor->predict(user_handle, input, n_rows, n_cols, predictions, verbose);
+}
+
+/**
+ * @brief Predict target feature for input data of type float and validate against ref_labels.
+ * @param[in] user_handle: cumlHandle (currently unused; API placeholder)
+ * @param[in] rf_regressor: pointer to the rfRegressor object. The user should have previously called fit to build the random forest.
+ * @param[in] input: test data (n_rows samples, n_cols features) in row major format. CPU pointer.
+ * @param[in] ref_labels: label values for cross validation (n_rows elements); CPU pointer.
+ * @param[in] n_rows: number of  data samples.
+ * @param[in] n_cols: number of features (excluding target feature).
+ * @param[in, out] predictions: n_rows predicted labels. CPU pointer, user allocated.
+ * @param[in] verbose: flag for debugging purposes.
+ */
+RF_metrics cross_validate(const cumlHandle& user_handle, const rfRegressor<float> * rf_regressor, const float * input, const float * ref_labels,
+							int n_rows, int n_cols, float * predictions, bool verbose) {
+	return rf_regressor->cross_validate(user_handle, input, ref_labels, n_rows, n_cols, predictions, verbose);
+}
+
+/**
+ * @brief Predict target feature for input data of type double and validate against ref_labels.
+ * @param[in] user_handle: cumlHandle (currently unused; API placeholder)
+ * @param[in] rf_regressor: pointer to the rfRegressor object. The user should have previously called fit to build the random forest.
+ * @param[in] input: test data (n_rows samples, n_cols features) in row major format. CPU pointer.
+ * @param[in] ref_labels: label values for cross validation (n_rows elements); CPU pointer.
+ * @param[in] n_rows: number of  data samples.
+ * @param[in] n_cols: number of features (excluding target feature).
+ * @param[in, out] predictions: n_rows predicted labels. CPU pointer, user allocated.
+ * @param[in] verbose: flag for debugging purposes.
+ */
+RF_metrics cross_validate(const cumlHandle& user_handle, const rfRegressor<double> * rf_regressor, const double * input, const double * ref_labels,
+							int n_rows, int n_cols, double * predictions, bool verbose) {
+	return rf_regressor->cross_validate(user_handle, input, ref_labels, n_rows, n_cols, predictions, verbose);
 }
 
 };
