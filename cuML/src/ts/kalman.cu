22320b517306937669bcb736f74563972b43bf1d
Max Rietmann <mrietmann@nvidia.com>
2019-05-09 12:13:05 +0200

    Made changes to our reference to match statsmodels

--- cuML/src/ts/kalman.cu
+++ cuML/src/ts/kalman.cu
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include "Eigen/Dense"
+#include "unsupported/Eigen/KroneckerProduct"
 #include <stdexcept>
 
 using namespace Eigen;
@@ -13,7 +14,7 @@ using MapMatrixT = Map<MatrixT>;
 using MapVectorT = Map<VectorXd>;
 
 void kalman_filter(double* ptr_ys, int ys_len, double* ptr_Z, double* ptr_R, double* ptr_T, int r,
-                   double* ptr_vs, double* ptr_loglike) {
+                   double* ptr_vs, double* ptr_loglike, bool P_init_by_single_iteration) {
   
   int nobs = ys_len;
 
@@ -25,9 +26,22 @@ void kalman_filter(double* ptr_ys, int ys_len, double* ptr_Z, double* ptr_R, dou
   // return results
   MapVectorT vs(ptr_vs, nobs);
   VectorT Fs(nobs);
-
-  // use a single kalman iteration as covariance (P) initialization
-  MatrixT P = T * T.transpose() - T * Z.transpose() * Z * T.transpose() + R * R.transpose();
+  MatrixT P(r,r);
+  if(P_init_by_single_iteration) {
+    // use a single kalman iteration as covariance (P) initialization
+    P = T * T.transpose() - T * Z.transpose() * Z * T.transpose() + R * R.transpose();
+  }
+  else {
+    // Uses a "fancy" initialization found in D&K's TSA 5.6.2. Statsmodels uses this.
+    MatrixT T_x_T = kroneckerProduct(T, T);
+    MatrixT invImTT = (MatrixT::Identity(r*r, r*r) - T_x_T).inverse();
+    MatrixT RRT = R * R.transpose();
+    Map<VectorT> RRT_ravel(RRT.data(), RRT.size());
+    VectorT P0_vec = invImTT * RRT_ravel;
+    Map<MatrixT> P0(P0_vec.data(), r, r);
+    P = P0;
+  }
+  
   MatrixT alpha = MatrixT::Zero(r, 1);
 
   double loglikelihood = 0.0;
--- python/cuml/ts/kalman.pyx
+++ python/cuml/ts/kalman.pyx
@@ -4,10 +4,11 @@ import ctypes
 cimport numpy as np
 from libcpp.vector cimport vector
 from libc.stdlib cimport malloc, free
+from libcpp cimport bool
 
 cdef extern from "ts/kalman.h":
   cdef void kalman_filter(double* ptr_ys, int ys_len, double* ptr_Z, double* ptr_R, double* ptr_T, int r,
-                          double* ptr_vs, double* ptr_loglike)
+                          double* ptr_vs, double* ptr_loglike, bool P_init_by_single_iteration)
 
 
 def kfilter(np.ndarray[double, ndim=1, mode="fortran"] ys,
@@ -20,10 +21,9 @@ def kfilter(np.ndarray[double, ndim=1, mode="fortran"] ys,
     cdef np.ndarray[double, ndim=1, mode="fortran"] vs = np.zeros(nobs)
     cdef double loglike = 0.0
 
-    kalman_filter(&ys[0], len(ys), &Z[0,0], &R[0,0], &T[0,0], r, &vs[0], &loglike)
+    kalman_filter(&ys[0], len(ys), &Z[0,0], &R[0,0], &T[0,0], r, &vs[0], &loglike, False)
 
     return vs, loglike
-
 
 def init_kalman_matrices(ar_params, ma_params, r=None):
     p = len(ar_params)
