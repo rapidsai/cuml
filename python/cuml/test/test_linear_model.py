9d5348901a9906b6f7dc6521fd09d82cb7663d9b
Salonijain27 <sj7856@g.rit.edu>
2019-05-09 16:37:19 -0500

    Update test_linear_model.py

--- python/cuml/test/test_linear_model.py
+++ python/cuml/test/test_linear_model.py
@@ -24,105 +24,34 @@ import numpy as np
 import pandas as pd
 from sklearn.datasets import make_regression
 
-
+@pytest.mark.unit
 @pytest.mark.parametrize('datatype', [np.float32, np.float64])
 @pytest.mark.parametrize('X_type', ['dataframe', 'ndarray'])
 @pytest.mark.parametrize('y_type', ['series', 'ndarray'])
 @pytest.mark.parametrize('algorithm', ['eig', 'svd'])
-def test_ols(datatype, X_type, y_type, algorithm,
-             run_stress, run_quality):
-    nrows = 5000
-    ncols = 1000
-    n_info = 500
-    if run_stress:
-        train_rows = np.int32(nrows*80)
-        X, y = make_regression(n_samples=(nrows*100), n_features=ncols,
-                               n_informative=n_info, random_state=0)
-        X_test = np.array(X[train_rows:, 0:]).astype(datatype)
-        X_train = np.array(X[0:train_rows, :]).astype(datatype)
-        y_train = np.array(y[0:train_rows, ]).astype(datatype)
+def test_unit(datatype, X_type, y_type, algorithm):
+    nrows = 10
+    ncols = 3
+    n_info = 2
+    train_rows = np.int32(nrows*0.8)
+    X, y = make_regression(n_samples=(nrows), n_features=ncols,
+                           n_informative=n_info, random_state=0)
+    X_test = np.asarray(X[train_rows:, 0:]).astype(datatype)
+    X_train = np.asarray(X[0:train_rows, :]).astype(datatype)
+    y_train = np.asarray(y[0:train_rows, ]).astype(datatype)
 
-    elif run_quality:
-        train_rows = np.int32(nrows*0.8)
-        X, y = make_regression(n_samples=nrows, n_features=int(ncols/2),
-                               n_informative=int(n_info/2), random_state=0)
-        X_test = np.array(X[train_rows:, 0:]).astype(datatype)
-        X_train = np.array(X[0:train_rows, :]).astype(datatype)
-        y_train = np.array(y[0:train_rows, ]).astype(datatype)
-
-    else:
-        X_train = np.array([[2.0, 5.0], [6.0, 9.0], [2.0, 2.0], [2.0, 3.0]],
-                           dtype=datatype)
-        train_rows = X_train.shape[0]
-        y_train = np.dot(X_train, np.array([5.0, 10.0]).astype(datatype))
-        X_test = np.array([[3.0, 5.0], [2.0, 5.0]]).astype(datatype)
-
+    # sklearn linear and ridge regression model initialization and fit
     skols = skLinearRegression(fit_intercept=True,
                                normalize=False)
     skols.fit(X_train, y_train)
-
-    cuols = cuLinearRegression(fit_intercept=True,
-                               normalize=False,
-                               algorithm=algorithm)
-
-    if X_type == 'dataframe':
-        y_train = pd.DataFrame({'fea0': y_train[0:, ]})
-        X_train = pd.DataFrame(
-            {'fea%d' % i: X_train[0:, i] for i in range(X_train.shape[1])})
-        X_test = pd.DataFrame(
-            {'fea%d' % i: X_test[0:, i] for i in range(X_test.shape[1])})
-        X_cudf = cudf.DataFrame.from_pandas(X_train)
-        X_cudf_test = cudf.DataFrame.from_pandas(X_test)
-        y_cudf = y_train.values
-        y_cudf = y_cudf[:, 0]
-        y_cudf = cudf.Series(y_cudf)
-        cuols.fit(X_cudf, y_cudf)
-        cu_predict = cuols.predict(X_cudf_test).to_array()
-
-    elif X_type == 'ndarray':
-
-        cuols.fit(X_train, y_train)
-        cu_predict = cuols.predict(X_test).to_array()
-
-    sk_predict = skols.predict(X_test)
-    assert array_equal(sk_predict, cu_predict, 1e-1, with_sign=True)
-
-
-@pytest.mark.parametrize('datatype', [np.float32, np.float64])
-@pytest.mark.parametrize('X_type', ['dataframe', 'ndarray'])
-@pytest.mark.parametrize('y_type', ['series', 'ndarray'])
-@pytest.mark.parametrize('algorithm', ['eig', 'svd'])
-def test_ridge(datatype, X_type, y_type, algorithm,
-               run_stress, run_quality):
-    nrows = 5000
-    ncols = 1000
-    n_info = 500
-    if run_stress:
-        train_rows = np.int32(nrows*80)
-        X, y = make_regression(n_samples=(nrows*100), n_features=ncols,
-                               n_informative=n_info, random_state=0)
-        X_test = np.asarray(X[train_rows:, 0:]).astype(datatype)
-        X_train = np.asarray(X[0:train_rows, :]).astype(datatype)
-        y_train = np.asarray(y[0:train_rows, ]).astype(datatype)
-
-    elif run_quality:
-        train_rows = np.int32(nrows*0.8)
-        X, y = make_regression(n_samples=nrows, n_features=ncols,
-                               n_informative=n_info, random_state=0)
-        X_test = np.asarray(X[train_rows:, 0:]).astype(datatype)
-        X_train = np.asarray(X[0:train_rows, :]).astype(datatype)
-        y_train = np.asarray(y[0:train_rows, ]).astype(datatype)
-
-    else:
-        X_train = np.array([[2.0, 5.0], [6.0, 9.0], [2.0, 2.0], [2.0, 3.0]],
-                           dtype=datatype)
-        train_rows = X_train.shape[0]
-        y_train = np.dot(X_train, np.array([5.0, 10.0]).astype(datatype))
-        X_test = np.array([[3.0, 5.0], [2.0, 5.0]]).astype(datatype)
-
     skridge = skRidge(fit_intercept=False,
                       normalize=False)
     skridge.fit(X_train, y_train)
+
+    # Initialization of cuML's linear and ridge regression models
+    cuols = cuLinearRegression(fit_intercept=True,
+                               normalize=False,
+                               algorithm=algorithm)
 
     curidge = cuRidge(fit_intercept=False,
                       normalize=False,
@@ -139,14 +68,168 @@ def test_ridge(datatype, X_type, y_type, algorithm,
         y_cudf = y_train.values
         y_cudf = y_cudf[:, 0]
         y_cudf = cudf.Series(y_cudf)
+
+        # fit and predict cuml linear regression model
+        cuols.fit(X_cudf, y_cudf)
+        cuols_predict = cuols.predict(X_cudf_test).to_array()
+
+        # fit and predict cuml ridge regression model
         curidge.fit(X_cudf, y_cudf)
-        cu_predict = curidge.predict(X_cudf_test).to_array()
+        curidge_predict = curidge.predict(X_cudf_test).to_array()
 
     elif X_type == 'ndarray':
 
+        # fit and predict cuml linear regression model
+        cuols.fit(X_train, y_train)
+        cuols_predict = cuols.predict(X_test).to_array()
+
+        # fit and predict cuml ridge regression model
         curidge.fit(X_train, y_train)
-        cu_predict = curidge.predict(X_test).to_array()
+        curidge_predict = curidge.predict(X_test).to_array()
 
-    sk_predict = skridge.predict(X_test)
+    skols_predict = skols.predict(X_test)
+    skridge_predict = skridge.predict(X_test)
 
-    assert array_equal(sk_predict, cu_predict, 1e-1, with_sign=True)
+    assert array_equal(skols_predict, cuols_predict, 1e-1, with_sign=True)
+    assert array_equal(skridge_predict, curidge_predict, 1e-1, with_sign=True)
+
+
+@pytest.mark.stress
+@pytest.mark.parametrize('datatype', [np.float32, np.float64])
+@pytest.mark.parametrize('X_type', ['dataframe', 'ndarray'])
+@pytest.mark.parametrize('y_type', ['series', 'ndarray'])
+@pytest.mark.parametrize('algorithm', ['eig', 'svd'])
+def test_stress_test(datatype, X_type, y_type, algorithm):
+    nrows = 500000
+    ncols = 1000
+    n_info = 700
+    train_rows = np.int32(nrows*0.8)
+    X, y = make_regression(n_samples=(nrows), n_features=ncols,
+                           n_informative=n_info, random_state=0)
+    X_test = np.asarray(X[train_rows:, 0:]).astype(datatype)
+    X_train = np.asarray(X[0:train_rows, :]).astype(datatype)
+    y_train = np.asarray(y[0:train_rows, ]).astype(datatype)
+
+    # sklearn linear and ridge regression model initialization and fit
+    skols = skLinearRegression(fit_intercept=True,
+                               normalize=False)
+    skols.fit(X_train, y_train)
+    skridge = skRidge(fit_intercept=False,
+                      normalize=False)
+    skridge.fit(X_train, y_train)
+
+    # Initialization of cuML's linear and ridge regression models
+    cuols = cuLinearRegression(fit_intercept=True,
+                               normalize=False,
+                               algorithm=algorithm)
+
+    curidge = cuRidge(fit_intercept=False,
+                      normalize=False,
+                      solver=algorithm)
+
+    if X_type == 'dataframe':
+        y_train = pd.DataFrame({'fea0': y_train[0:, ]})
+        X_train = pd.DataFrame(
+            {'fea%d' % i: X_train[0:, i] for i in range(X_train.shape[1])})
+        X_test = pd.DataFrame(
+            {'fea%d' % i: X_test[0:, i] for i in range(X_test.shape[1])})
+        X_cudf = cudf.DataFrame.from_pandas(X_train)
+        X_cudf_test = cudf.DataFrame.from_pandas(X_test)
+        y_cudf = y_train.values
+        y_cudf = y_cudf[:, 0]
+        y_cudf = cudf.Series(y_cudf)
+
+        # fit and predict cuml linear regression model
+        cuols.fit(X_cudf, y_cudf)
+        cuols_predict = cuols.predict(X_cudf_test).to_array()
+
+        # fit and predict cuml ridge regression model
+        curidge.fit(X_cudf, y_cudf)
+        curidge_predict = curidge.predict(X_cudf_test).to_array()
+
+    elif X_type == 'ndarray':
+
+        # fit and predict cuml linear regression model
+        cuols.fit(X_train, y_train)
+        cuols_predict = cuols.predict(X_test).to_array()
+
+        # fit and predict cuml ridge regression model
+        curidge.fit(X_train, y_train)
+        curidge_predict = curidge.predict(X_test).to_array()
+
+    skols_predict = skols.predict(X_test)
+    skridge_predict = skridge.predict(X_test)
+
+    assert array_equal(skols_predict, cuols_predict, 1e-1, with_sign=True)
+    assert array_equal(skridge_predict, curidge_predict, 1e-1, with_sign=True)
+
+
+@pytest.mark.quality
+@pytest.mark.parametrize('datatype', [np.float32, np.float64])
+@pytest.mark.parametrize('X_type', ['dataframe', 'ndarray'])
+@pytest.mark.parametrize('y_type', ['series', 'ndarray'])
+@pytest.mark.parametrize('algorithm', ['eig', 'svd'])
+def test_quality(datatype, X_type, y_type, algorithm):
+    nrows = 5000
+    ncols = 100
+    n_info = 80
+    train_rows = np.int32(nrows*0.8)
+    X, y = make_regression(n_samples=(nrows), n_features=ncols,
+                           n_informative=n_info, random_state=0)
+    X_test = np.asarray(X[train_rows:, 0:]).astype(datatype)
+    X_train = np.asarray(X[0:train_rows, :]).astype(datatype)
+    y_train = np.asarray(y[0:train_rows, ]).astype(datatype)
+
+    # sklearn linear and ridge regression model initialization and fit
+    skols = skLinearRegression(fit_intercept=True,
+                               normalize=False)
+    skols.fit(X_train, y_train)
+    skridge = skRidge(fit_intercept=False,
+                      normalize=False)
+    skridge.fit(X_train, y_train)
+
+    # Initialization of cuML's linear and ridge regression models
+    cuols = cuLinearRegression(fit_intercept=True,
+                               normalize=False,
+                               algorithm=algorithm)
+
+    curidge = cuRidge(fit_intercept=False,
+                      normalize=False,
+                      solver=algorithm)
+
+    if X_type == 'dataframe':
+        y_train = pd.DataFrame({'fea0': y_train[0:, ]})
+        X_train = pd.DataFrame(
+            {'fea%d' % i: X_train[0:, i] for i in range(X_train.shape[1])})
+        X_test = pd.DataFrame(
+            {'fea%d' % i: X_test[0:, i] for i in range(X_test.shape[1])})
+        X_cudf = cudf.DataFrame.from_pandas(X_train)
+        X_cudf_test = cudf.DataFrame.from_pandas(X_test)
+        y_cudf = y_train.values
+        y_cudf = y_cudf[:, 0]
+        y_cudf = cudf.Series(y_cudf)
+
+        # fit and predict cuml linear regression model
+        cuols.fit(X_cudf, y_cudf)
+        cuols_predict = cuols.predict(X_cudf_test).to_array()
+
+        # fit and predict cuml ridge regression model
+        curidge.fit(X_cudf, y_cudf)
+        curidge_predict = curidge.predict(X_cudf_test).to_array()
+
+    elif X_type == 'ndarray':
+
+        # fit and predict cuml linear regression model
+        cuols.fit(X_train, y_train)
+        cuols_predict = cuols.predict(X_test).to_array()
+
+        # fit and predict cuml ridge regression model
+        curidge.fit(X_train, y_train)
+        curidge_predict = curidge.predict(X_test).to_array()
+
+    skols_predict = skols.predict(X_test)
+    skridge_predict = skridge.predict(X_test)
+
+    assert array_equal(skols_predict, cuols_predict, 1e-1, with_sign=True)
+    assert array_equal(skridge_predict, curidge_predict, 1e-1, with_sign=True)
+
