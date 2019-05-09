2659a1141158bb4b6c35f604fa795f17f0dc6c8d
Onur Yilmaz <oyilmaz@nvidia.com>
2019-05-09 18:42:05 -0400

    Cython side of cumlhandle added for ols and ridge.

--- cuML/src/glm/glm.cu
+++ cuML/src/glm/glm.cu
@@ -25,76 +25,61 @@ namespace GLM {
 
 using namespace MLCommon;
 
-void olsFit(float *input, int n_rows, int n_cols, float *labels, float *coef,
+void olsFit(const cumlHandle &handle, float *input, int n_rows, int n_cols, float *labels, float *coef,
             float *intercept, bool fit_intercept, bool normalize, int algo) {
-    cumlHandle handle;
+
     olsFit(handle.getImpl(), input, n_rows, n_cols, labels, coef, intercept, fit_intercept,
            normalize, handle.getStream(), algo);
-    ///@todo this should go away after cumlHandle exposure in the interface
-    CUDA_CHECK(cudaStreamSynchronize(handle.getStream()));
+
 }
 
-void olsFit(double *input, int n_rows, int n_cols, double *labels, double *coef,
+void olsFit(const cumlHandle &handle, double *input, int n_rows, int n_cols, double *labels, double *coef,
 		double *intercept, bool fit_intercept, bool normalize, int algo) {
-    cumlHandle handle;
+
     olsFit(handle.getImpl(), input, n_rows, n_cols, labels, coef, intercept, fit_intercept,
            normalize, handle.getStream(), algo);
-    ///@todo this should go away after cumlHandle exposure in the interface
-    CUDA_CHECK(cudaStreamSynchronize(handle.getStream()));
 }
 
-void olsPredict(const float *input, int n_rows, int n_cols, const float *coef,
+void olsPredict(const cumlHandle &handle, const float *input, int n_rows, int n_cols, const float *coef,
 		float intercept, float *preds) {
-    cumlHandle handle;
-    olsPredict(handle.getImpl(), input, n_rows, n_cols, coef, intercept, preds,
+
+	olsPredict(handle.getImpl(), input, n_rows, n_cols, coef, intercept, preds,
                handle.getStream());
-    ///@todo this should go away after cumlHandle exposure in the interface
-    CUDA_CHECK(cudaStreamSynchronize(handle.getStream()));
 }
 
-void olsPredict(const double *input, int n_rows, int n_cols, const double *coef,
+void olsPredict(const cumlHandle &handle, const double *input, int n_rows, int n_cols, const double *coef,
 		double intercept, double *preds) {
-    cumlHandle handle;
+
     olsPredict(handle.getImpl(), input, n_rows, n_cols, coef, intercept, preds,
                handle.getStream());
-    ///@todo this should go away after cumlHandle exposure in the interface
-    CUDA_CHECK(cudaStreamSynchronize(handle.getStream()));
 }
 
-void ridgeFit(float *input, int n_rows, int n_cols, float *labels, float *alpha,
+void ridgeFit(const cumlHandle &handle, float *input, int n_rows, int n_cols, float *labels, float *alpha,
 		int n_alpha, float *coef, float *intercept, bool fit_intercept,
 		bool normalize, int algo) {
-    cumlHandle handle;
+
     ridgeFit(handle.getImpl(), input, n_rows, n_cols, labels, alpha, n_alpha, coef, intercept,
              fit_intercept, normalize, handle.getStream(), algo);
-    ///@todo this should go away after cumlHandle exposure in the interface
-    CUDA_CHECK(cudaStreamSynchronize(handle.getStream()));
 }
 
-void ridgeFit(double *input, int n_rows, int n_cols, double *labels,
+void ridgeFit(const cumlHandle &handle, double *input, int n_rows, int n_cols, double *labels,
 		double *alpha, int n_alpha, double *coef, double *intercept,
 		bool fit_intercept, bool normalize, int algo) {
-    cumlHandle handle;
+
     ridgeFit(handle.getImpl(), input, n_rows, n_cols, labels, alpha, n_alpha, coef, intercept,
              fit_intercept, normalize, handle.getStream(), algo);
-    ///@todo this should go away after cumlHandle exposure in the interface
-    CUDA_CHECK(cudaStreamSynchronize(handle.getStream()));
 }
 
-void ridgePredict(const float *input, int n_rows, int n_cols, const float *coef,
+void ridgePredict(const cumlHandle &handle, const float *input, int n_rows, int n_cols, const float *coef,
 		float intercept, float *preds) {
-    cumlHandle handle;
+
     ridgePredict(handle.getImpl(), input, n_rows, n_cols, coef, intercept, preds, handle.getStream());
-    ///@todo this should go away after cumlHandle exposure in the interface
-    CUDA_CHECK(cudaStreamSynchronize(handle.getStream()));
 }
 
-void ridgePredict(const double *input, int n_rows, int n_cols, const double *coef,
+void ridgePredict(const cumlHandle &handle, const double *input, int n_rows, int n_cols, const double *coef,
 		double intercept, double *preds) {
-    cumlHandle handle;
+
     ridgePredict(handle.getImpl(), input, n_rows, n_cols, coef, intercept, preds, handle.getStream());
-    ///@todo this should go away after cumlHandle exposure in the interface
-    CUDA_CHECK(cudaStreamSynchronize(handle.getStream()));
 }
 
 void qnFit(const cumlHandle &cuml_handle, float *X, float *y, int N, int D,
