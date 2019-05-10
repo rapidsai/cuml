0072e26e19bc269458d06fd750910eb0d160281a
Corey J. Nolet <cjnolet@gmail.com>
2019-05-10 11:48:37 -0400

    Adding c++ unit tests for make_monotonic

--- ml-prims/src/array/array.h
+++ ml-prims/src/array/array.h
@@ -21,12 +21,12 @@ namespace Array {
 
 template <typename Type, int TPB_X, typename Lambda>
 __global__ void map_label_kernel(Type *map_ids, Type *in, Type *out,
-        Type N, Lambda filter_op) {
+        size_t N, Lambda filter_op) {
     int tid = threadIdx.x + blockIdx.x*TPB_X;
     if(tid < N) {
 
         if(!filter_op(in[tid])) {
-            for(int i=0; i < N; i++) {
+            for(size_t i=0; i < N; i++) {
                 if(in[tid] == map_ids[i]) {
                     out[tid] = i + 1;
                     break;
@@ -54,11 +54,12 @@ __global__ void map_label_kernel(Type *map_ids, Type *in, Type *out,
  * @param filter_op an optional function for specifying which values
  * should have monotonically increasing labels applied to them.
  */
-template <typename Type, typename Lambda>
-void make_monotonic(Type *out, Type *in, Type N, cudaStream_t stream,
-        Lambda filter_op = [] __device__ (int val) {return false;}) {
+template <typename Type, typename Lambda >
+void make_monotonic(Type *out, Type *in, size_t N,
+                    cudaStream_t stream,
+                    Lambda filter_op) {
 
-    static const int TPB_X = 256;
+    static const size_t TPB_X = 256;
 
     dim3 blocks(ceildiv(N, TPB_X));
     dim3 threads(TPB_X);
@@ -87,7 +88,24 @@ void make_monotonic(Type *out, Type *in, Type N, cudaStream_t stream,
     map_label_kernel<Type,TPB_X><<<blocks, threads, 0, stream>>>(map_ids, in, out, N, filter_op);
 }
 
-
-
+/**
+ * Maps an input array containing a series of numbers into a new array
+ * where numbers have been mapped to a monotonically increasing set
+ * of labels. This can be useful in machine learning algorithms, for instance,
+ * where a given set of labels is not taken from a monotonically increasing
+ * set. This can happen if they are filtered or if only a subset of the
+ * total labels are used in a dataset. This is also useful in graph algorithms
+ * where a set of vertices need to be labeled in a monotonically increasing
+ * order.
+ * @tparam Type the numeric type of the input and output arrays
+ * @tparam Lambda the type of an optional filter function, which determines
+ * which items in the array to map.
+ * @param N number of elements in the input array
+ * @param stream cuda stream to use
+ */
+template <typename Type>
+void make_monotonic(Type *out, Type *in, size_t N, cudaStream_t stream) {
+    make_monotonic<Type>(out, in, N, stream, [] __device__ (Type val) {return false;});
+}
 };
 };
