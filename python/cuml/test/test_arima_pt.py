101b37a5c26e51038eafa72b4ce3d527da99125f
Max Rietmann <mrietmann@nvidia.com>
2019-05-10 15:36:46 +0200

    Profiling for test

--- python/cuml/test/test_arima_pt.py
+++ python/cuml/test/test_arima_pt.py
@@ -10,58 +10,75 @@ from IPython.core.debugger import set_trace
 
 from timeit import default_timer as timer
 
-data = pd.read_csv("/home/max/dev/arima-experiments/data/data_paper_towel.csv",
-                   names=["store", "week", "sold"])
+import cProfile
+import pstats
+from pstats import SortKey
 
-w = data.groupby("store")["week"].apply(np.array)
-s = data.groupby("store")["sold"].apply(np.array)
 
-nb = len(w)
-ns_all = np.zeros(nb, dtype=np.int32)
-for (i, si) in enumerate(s[:nb]):
-    ns_all[i-1] = len(si)
+def paperTowels(plot=False):
+    data = pd.read_csv("/home/max/dev/arima-experiments/data/data_paper_towel.csv",
+                       names=["store", "week", "sold"])
 
-ns = np.min(ns_all)
-print("shortest data: {}, vs longest: {}".format(ns, np.max(ns_all)))
+    w = data.groupby("store")["week"].apply(np.array)
+    s = data.groupby("store")["sold"].apply(np.array)
 
-yb = np.zeros((ns, nb), order="F")
+    nb = len(w)
+    ns_all = np.zeros(nb, dtype=np.int32)
+    for (i, si) in enumerate(s[:nb]):
+        ns_all[i-1] = len(si)
 
-for (i, si) in enumerate(s[:nb]):
-    yb[:, i] = si[0:ns]
+    ns = np.min(ns_all)
+    print("shortest data: {}, vs longest: {}".format(ns, np.max(ns_all)))
 
-y = yb[:, 0]
-start = timer()
-sm_model = sm.ARIMA(y, (1, 1, 1))
-sm_model_fit = sm_model.fit(disp=-1)
-y_sm_p = sm_model_fit.predict(start=1, end=ns)
-end = timer()
-print("CPU Time 1 batch = {}s, estimate for {} batches = {}s".format(end-start, nb, (end-start)*nb))
+    yb = np.zeros((ns, nb), order="F")
 
-# best_model, ic = batched_arima.grid_search(y, d=1)
+    for (i, si) in enumerate(s[:nb]):
+        yb[:, i] = si[0:ns]
 
-start = timer()
-batched_model = batched_arima.BatchedARIMAModel.fit(yb, (1, 1, 1),
-                                                    -200.0,
-                                                    np.array([-0.005]),
-                                                    np.array([-1.0]),
-                                                    opt_disp=-1)
+    y = yb[:, 0]
+    start = timer()
+    sm_model = sm.ARIMA(y, (1, 1, 1))
+    sm_model_fit = sm_model.fit(disp=-1)
+    y_sm_p = sm_model_fit.predict(start=1, end=ns)
+    end = timer()
+    print("CPU Time 1 batch = {}s, estimate for {} batches = {}s".format(end-start, nb, (end-start)*nb))
 
-y_b = batched_arima.BatchedARIMAModel.predict_in_sample(batched_model)
-end = timer()
+    # best_model, ic = batched_arima.grid_search(y, d=1)
 
-print("GPU Time for all batches ({}) = {}s".format(nb, (end-start)))
+    start = timer()
+    batched_model = batched_arima.BatchedARIMAModel.fit(yb, (1, 1, 1),
+                                                        -200.0,
+                                                        np.array([-0.005]),
+                                                        np.array([-1.0]),
+                                                        opt_disp=-1)
 
-# plt.clf()
+    y_b = batched_arima.BatchedARIMAModel.predict_in_sample(batched_model)
+    end = timer()
 
-plot = False
-if plot:
+    print("GPU Time for all batches ({}) = {}s".format(nb, (end-start)))
 
-    fig, axes = plt.subplots(nb, 1)
-    axes[0].plot(w[1][:-1], s[1][:-1] + y_sm_p, "b-")
-    axes[0].plot(w[1], s[1], "k", w[1][:ns], y_b[:, 0], "r--")
-    for (i, (wi, si)) in enumerate(zip(w, s)):
-        if i >= nb:
-            break
-        axes[i].plot(wi, si, "k", wi[:ns], y_b[:, i], "r--")
+    # plt.clf()
 
-    plt.show()
+    if plot:
+
+        fig, axes = plt.subplots(nb, 1)
+        axes[0].plot(w[1][:-1], s[1][:-1] + y_sm_p, "b-")
+        axes[0].plot(w[1], s[1], "k", w[1][:ns], y_b[:, 0], "r--")
+        for (i, (wi, si)) in enumerate(zip(w, s)):
+            if i >= nb:
+                break
+            axes[i].plot(wi, si, "k", wi[:ns], y_b[:, i], "r--")
+
+        plt.show()
+
+def run_profile():
+    cProfile.run("paperTowels()", "profile.dat")
+
+def analyze_profile():
+    p = pstats.Stats("profile.dat")
+    p.strip_dirs().sort_stats(SortKey.TIME).print_stats(5)
+    p.sort_stats(SortKey.TIME).print_stats(20)
+
+
+if __name__ == "__main__":
+    paperTowels()
