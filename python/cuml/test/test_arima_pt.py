433cf082c06d8f7c074020eba098e2793e639d1f
Max Rietmann <mrietmann@nvidia.com>
2019-05-10 13:51:33 +0200

    Benchmark paper-towel

--- python/cuml/test/test_arima_pt.py
+++ python/cuml/test/test_arima_pt.py
@@ -8,15 +8,21 @@ import cuml.ts.arima as arima
 
 from IPython.core.debugger import set_trace
 
+from timeit import default_timer as timer
+
 data = pd.read_csv("/home/max/dev/arima-experiments/data/data_paper_towel.csv",
                    names=["store", "week", "sold"])
 
 w = data.groupby("store")["week"].apply(np.array)
 s = data.groupby("store")["sold"].apply(np.array)
-plt.plot(w[1], s[1])
 
-nb = 1
-ns = len(w[1])-1
+nb = len(w)
+ns_all = np.zeros(nb, dtype=np.int32)
+for (i, si) in enumerate(s[:nb]):
+    ns_all[i-1] = len(si)
+
+ns = np.min(ns_all)
+print("shortest data: {}, vs longest: {}".format(ns, np.max(ns_all)))
 
 yb = np.zeros((ns, nb), order="F")
 
@@ -24,22 +30,38 @@ for (i, si) in enumerate(s[:nb]):
     yb[:, i] = si[0:ns]
 
 y = yb[:, 0]
+start = timer()
+sm_model = sm.ARIMA(y, (1, 1, 1))
+sm_model_fit = sm_model.fit(disp=-1)
+y_sm_p = sm_model_fit.predict(start=1, end=ns)
+end = timer()
+print("CPU Time 1 batch = {}s, estimate for {} batches = {}s".format(end-start, nb, (end-start)*nb))
+
 # best_model, ic = batched_arima.grid_search(y, d=1)
 
+start = timer()
 batched_model = batched_arima.BatchedARIMAModel.fit(yb, (1, 1, 1),
-                                                    -20.0,
+                                                    -200.0,
                                                     np.array([-0.005]),
                                                     np.array([-1.0]),
-                                                    opt_disp=5, h=1e-10)
-
-sm_model = sm.ARIMA(y, (1, 1, 1))
-sm_model_fit = sm_model.fit(disp=5)
-
-y_sm_p = sm_model_fit.predict(start=1, end=ns)
+                                                    opt_disp=-1)
 
 y_b = batched_arima.BatchedARIMAModel.predict_in_sample(batched_model)
+end = timer()
 
-plt.clf()
-plt.plot(w[1], s[1], "k", w[1][:-1], y_b[:, 0], "r--")
-plt.plot(w[1][:-1], s[1][:-1] + y_sm_p, "b-")
-plt.show()
+print("GPU Time for all batches ({}) = {}s".format(nb, (end-start)))
+
+# plt.clf()
+
+plot = False
+if plot:
+
+    fig, axes = plt.subplots(nb, 1)
+    axes[0].plot(w[1][:-1], s[1][:-1] + y_sm_p, "b-")
+    axes[0].plot(w[1], s[1], "k", w[1][:ns], y_b[:, 0], "r--")
+    for (i, (wi, si)) in enumerate(zip(w, s)):
+        if i >= nb:
+            break
+        axes[i].plot(wi, si, "k", wi[:ns], y_b[:, i], "r--")
+
+    plt.show()
