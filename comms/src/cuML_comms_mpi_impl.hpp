9bbf78640dae665043a77faa03b06ff62ae96a24
Jiri Kraus <jkraus@nvidia.com>
2019-05-13 11:33:54 +0200

    Added missing NCCL collectives.

--- comms/src/cuML_comms_mpi_impl.hpp
+++ comms/src/cuML_comms_mpi_impl.hpp
@@ -43,13 +43,23 @@ public:
 
     virtual void barrier() const;
 
-    virtual void isend(const void *buf, std::size_t size, int dest, int tag, request_t *request) const;
+    virtual void isend(const void *buf, int size, int dest, int tag, request_t *request) const;
 
-    virtual void irecv(void *buf, std::size_t size, int source, int tag, request_t *request) const;
+    virtual void irecv(void *buf, int size, int source, int tag, request_t *request) const;
 
     virtual void waitall(int count, request_t array_of_requests[]) const;
 
-    virtual void allreduce(const void* sendbuff, void* recvbuff, size_t count, datatype_t datatype, op_t op, cudaStream_t stream) const;
+    virtual void allreduce(const void* sendbuff, void* recvbuff, int count, datatype_t datatype, op_t op, cudaStream_t stream) const;
+
+    virtual void bcast(void* buff, int count, datatype_t datatype, int root, cudaStream_t stream) const;
+
+    virtual void reduce(const void* sendbuff, void* recvbuff, int count, datatype_t datatype, op_t op, int root, cudaStream_t stream) const;
+
+    virtual void allgather(const void* sendbuff, void* recvbuff, int sendcount, datatype_t datatype, cudaStream_t stream) const;
+
+    virtual void allgatherv(const void *sendbuf, void *recvbuf, const int recvcounts[], const int displs[], datatype_t datatype, cudaStream_t stream) const;
+
+    virtual void reducescatter(const void* sendbuff, void* recvbuff, int recvcount, datatype_t datatype, op_t op, cudaStream_t stream) const;
 
 private:
     MPI_Comm                                            _mpi_comm;
