e7a3f0ca5dd5bc671ff7152d30a24695d9ed1c0a
Jiri Kraus <jkraus@nvidia.com>
2019-05-14 15:46:51 +0200

    Added tests for host_buffer and improved device_buffer and host_buffer implemenation

--- cpp/src_prims/common/host_buffer.hpp
+++ cpp/src_prims/common/host_buffer.hpp
@@ -17,9 +17,6 @@
 #pragma once
 
 #include "buffer_base.hpp"
-
-#include "../utils.h"
-
 #include "cuml_allocator.hpp"
 
 namespace MLCommon {
@@ -43,17 +40,16 @@ namespace MLCommon {
  * @endcode
  * @todo: Add missing doxygen documentation
  */
-template <typename T>
-class host_buffer : public buffer_base<T> {
- public:
-  using size_type = typename buffer_base<T>::size_type;
-  using value_type = typename buffer_base<T>::value_type;
-  using iterator = typename buffer_base<T>::iterator;
-  using const_iterator = typename buffer_base<T>::const_iterator;
-  using reference = typename buffer_base<T>::reference;
-  using const_reference = typename buffer_base<T>::const_reference;
-  using buffer_base<T>::get_stream;
-  using buffer_base<T>::set_stream;
+template<typename T>
+class host_buffer : public buffer_base<T,hostAllocator>
+{
+public:
+  using size_type         = typename buffer_base<T,hostAllocator>::size_type;
+  using value_type        = typename buffer_base<T,hostAllocator>::value_type;
+  using iterator          = typename buffer_base<T,hostAllocator>::iterator;
+  using const_iterator    = typename buffer_base<T,hostAllocator>::const_iterator;
+  using reference         = typename buffer_base<T,hostAllocator>::reference;
+  using const_reference   = typename buffer_base<T,hostAllocator>::const_reference;
 
   host_buffer() = delete;
 
@@ -61,68 +57,24 @@ class host_buffer : public buffer_base<T> {
 
   host_buffer& operator=(const host_buffer& other) = delete;
 
-  host_buffer(std::shared_ptr<hostAllocator> allocator, cudaStream_t stream,
-              size_type n = 0)
-    : buffer_base<T>(stream, n), _allocator(allocator) {
-    if (_capacity > 0) {
-      _data = static_cast<value_type*>(
-        _allocator->allocate(_capacity * sizeof(value_type), get_stream()));
-      CUDA_CHECK(cudaStreamSynchronize(get_stream()));
-    }
+  host_buffer(std::shared_ptr<hostAllocator> allocator, cudaStream_t stream, size_type n = 0)
+      : buffer_base<T,hostAllocator>(allocator,stream,n)
+  {}
+
+  ~host_buffer() {}
+
+  reference operator[]( size_type pos )
+  {
+      return _data[pos];
   }
 
-  ~host_buffer() {
-    if (nullptr != _data) {
-      _allocator->deallocate(_data, _capacity * sizeof(value_type),
-                             get_stream());
-    }
+  const_reference operator[]( size_type pos ) const
+  {
+    return _data[pos];
   }
 
-  reference operator[](size_type pos) { return _data[pos]; }
-
-  const_reference operator[](size_type pos) const { return _data[pos]; }
-
-  void reserve(const size_type new_capacity, cudaStream_t stream) {
-    set_stream(stream);
-    if (new_capacity > _capacity) {
-      value_type* new_data = static_cast<value_type*>(
-        _allocator->allocate(new_capacity * sizeof(value_type), get_stream()));
-      if (_size > 0) {
-        CUDA_CHECK(cudaMemcpyAsync(new_data, _data, _size * sizeof(value_type),
-                                   cudaMemcpyHostToHost, get_stream()));
-      }
-      if (nullptr != _data) {
-        _allocator->deallocate(_data, _capacity * sizeof(value_type),
-                               get_stream());
-      }
-      _data = new_data;
-      _capacity = new_capacity;
-    }
-  }
-
-  void resize(const size_type new_size, cudaStream_t stream) {
-    reserve(new_size, stream);
-    _size = new_size;
-  }
-
-  void release(cudaStream_t stream) {
-    set_stream(stream);
-    if (nullptr != _data) {
-      _allocator->deallocate(_data, _capacity * sizeof(value_type),
-                             get_stream());
-    }
-    _data = nullptr;
-    _capacity = 0;
-    _size = 0;
-  }
-
-  std::shared_ptr<hostAllocator> getAllocator() const { return _allocator; }
-
- private:
-  std::shared_ptr<hostAllocator> _allocator;
-  using buffer_base<T>::_size;
-  using buffer_base<T>::_capacity;
-  using buffer_base<T>::_data;
+private:
+  using buffer_base<T,hostAllocator>::_data;
 };
 
-}  // namespace MLCommon
+} // end namespace ML
