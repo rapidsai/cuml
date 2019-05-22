e78467340a089ab50a5b909b03ec03a6d052c880
Daniel Han-Chen <danielhanchen@gmail.com>
2019-05-23 01:28:56 +1000

    Fixed spacing

--- cuML/src/tsne/bounding_box.h
+++ cuML/src/tsne/bounding_box.h
@@ -23,80 +23,80 @@ void boundingBoxKernel(const int N_NODES,
                         volatile float * __restrict__ minxd, 
                         volatile float * __restrict__ minyd) 
 {
-	int i, j, k, inc;
-	float val, minx, maxx, miny, maxy;
-	__shared__ volatile float sminx[THREADS1], smaxx[THREADS1], sminy[THREADS1], smaxy[THREADS1];
+    int i, j, k, inc;
+    float val, minx, maxx, miny, maxy;
+    __shared__ volatile float sminx[THREADS1], smaxx[THREADS1], sminy[THREADS1], smaxy[THREADS1];
 
-	// initialize with valid data (in case #bodies < #threads)
-	minx = maxx = posxd[0];
-	miny = maxy = posyd[0];
+    // initialize with valid data (in case #bodies < #threads)
+    minx = maxx = posxd[0];
+    miny = maxy = posyd[0];
 
-	// scan all bodies
-	i = threadIdx.x;
-	inc = THREADS1 * gridDim.x;
-	for (j = i + blockIdx.x * THREADS1; j < n; j += inc) {
-		val = posxd[j];
-		minx = MIN(minx, val);
-		maxx = MAX(maxx, val);
-		val = posyd[j];
-		miny = MIN(miny, val);
-		maxy = MAX(maxy, val);
-	}
+    // scan all bodies
+    i = threadIdx.x;
+    inc = THREADS1 * gridDim.x;
+    for (j = i + blockIdx.x * THREADS1; j < n; j += inc) {
+        val = posxd[j];
+        minx = MIN(minx, val);
+        maxx = MAX(maxx, val);
+        val = posyd[j];
+        miny = MIN(miny, val);
+        maxy = MAX(maxy, val);
+    }
 
-	// reduction in shared memory
-	sminx[i] = minx;
-	smaxx[i] = maxx;
-	sminy[i] = miny;
-	smaxy[i] = maxy;
+    // reduction in shared memory
+    sminx[i] = minx;
+    smaxx[i] = maxx;
+    sminy[i] = miny;
+    smaxy[i] = maxy;
 
-	for (j = THREADS1 / 2; j > 0; j /= 2) {
-		kernel_sync();
-		if (i < j) {
-			k = i + j;
-			sminx[i] = minx = MIN(minx, sminx[k]);
-			smaxx[i] = maxx = MAX(maxx, smaxx[k]);
-			sminy[i] = miny = MIN(miny, sminy[k]);
-			smaxy[i] = maxy = MAX(maxy, smaxy[k]);
-		}
-	}
+    for (j = THREADS1 / 2; j > 0; j /= 2) {
+        kernel_sync();
+        if (i < j) {
+            k = i + j;
+            sminx[i] = minx = MIN(minx, sminx[k]);
+            smaxx[i] = maxx = MAX(maxx, smaxx[k]);
+            sminy[i] = miny = MIN(miny, sminy[k]);
+            smaxy[i] = maxy = MAX(maxy, smaxy[k]);
+        }
+    }
 
-	// write block result to global memory
-	if (i == 0) {
-		k = blockIdx.x;
-		minxd[k] = minx;
-		maxxd[k] = maxx;
-		minyd[k] = miny;
-		maxyd[k] = maxy;
-		kernel_fence();
+    // write block result to global memory
+    if (i == 0) {
+        k = blockIdx.x;
+        minxd[k] = minx;
+        maxxd[k] = maxx;
+        minyd[k] = miny;
+        maxyd[k] = maxy;
+        kernel_fence();
 
-		inc = gridDim.x - 1;
-		if (inc == atomicInc(&blkcntd, inc)) {
-			// I'm the last block, so combine all block results
-			for (j = 0; j <= inc; j++) {
-				minx = MIN(minx, minxd[j]);
-				maxx = MAX(maxx, maxxd[j]);
-				miny = MIN(miny, minyd[j]);
-				maxy = MAX(maxy, maxyd[j]);
-			}
+        inc = gridDim.x - 1;
+        if (inc == atomicInc(&blkcntd, inc)) {
+            // I'm the last block, so combine all block results
+            for (j = 0; j <= inc; j++) {
+                minx = MIN(minx, minxd[j]);
+                maxx = MAX(maxx, maxxd[j]);
+                miny = MIN(miny, minyd[j]);
+                maxy = MAX(maxy, maxyd[j]);
+            }
 
-			// compute 'radius'
-			radiusd = MAX(maxx - minx, maxy - miny) * 0.5f + 1e-5f;
+            // compute 'radius'
+            radiusd = MAX(maxx - minx, maxy - miny) * 0.5f + 1e-5f;
 
-			// create root node
-			k = N_NODES;
-			bottomd = k;
+            // create root node
+            k = N_NODES;
+            bottomd = k;
 
-			massd[k] = -1.0f;
-			startd[k] = 0;
-			posxd[k] = (minx + maxx) * 0.5f;
-			posyd[k] = (miny + maxy) * 0.5f;
-			k *= 4;
-			for (i = 0; i < 4; i++)
-				childd[k + i] = -1;
+            massd[k] = -1.0f;
+            startd[k] = 0;
+            posxd[k] = (minx + maxx) * 0.5f;
+            posyd[k] = (miny + maxy) * 0.5f;
+            k *= 4;
+            for (i = 0; i < 4; i++)
+                childd[k + i] = -1;
 
-			stepd++;
-		}
-	}
+            stepd++;
+        }
+    }
 }
 
 // end namespace
--- cuML/src/tsne/repulsion.h
+++ cuML/src/tsne/repulsion.h
@@ -12,122 +12,122 @@ namespace Repulsion_ {
 __global__
 __launch_bounds__(THREADS5, FACTOR5)
 void repulsionKernel(int nnodesd, 
-					int nbodiesd, 
-					volatile int * __restrict errd, 
-					float theta, 
-					float epssqd, // correction for zero distance
-					volatile int * __restrict sortd, 
-					volatile int * __restrict childd, 
-					volatile float * __restrict massd, 
-					volatile float * __restrict posxd, 
-					volatile float * __restrict posyd, 
-					volatile float * __restrict velxd, 
-					volatile float * __restrict velyd,
-					volatile float * __restrict normd) 
+                    int nbodiesd, 
+                    volatile int * __restrict errd, 
+                    float theta, 
+                    float epssqd, // correction for zero distance
+                    volatile int * __restrict sortd, 
+                    volatile int * __restrict childd, 
+                    volatile float * __restrict massd, 
+                    volatile float * __restrict posxd, 
+                    volatile float * __restrict posyd, 
+                    volatile float * __restrict velxd, 
+                    volatile float * __restrict velyd,
+                    volatile float * __restrict normd) 
 {
-	int i, j, k, n, depth, base, sbase, diff, pd, nd;
-	float px, py, vx, vy, dx, dy, normsum, tmp, mult;
-	__shared__ volatile int pos[MAXDEPTH * THREADS5/WARPSIZE], node[MAXDEPTH * THREADS5/WARPSIZE];
-	__shared__ float dq[MAXDEPTH * THREADS5/WARPSIZE];
+    int i, j, k, n, depth, base, sbase, diff, pd, nd;
+    float px, py, vx, vy, dx, dy, normsum, tmp, mult;
+    __shared__ volatile int pos[MAXDEPTH * THREADS5/WARPSIZE], node[MAXDEPTH * THREADS5/WARPSIZE];
+    __shared__ float dq[MAXDEPTH * THREADS5/WARPSIZE];
 
-	if (0 == threadIdx.x) {
-		dq[0] = (radiusd * radiusd) / (theta * theta); 
-		for (i = 1; i < maxdepthd; i++) {
-				dq[i] = dq[i - 1] * 0.25f; // radius is halved every level of tree so squared radius is quartered
-				dq[i - 1] += epssqd;
-		}
-		dq[i - 1] += epssqd;
+    if (0 == threadIdx.x) {
+        dq[0] = (radiusd * radiusd) / (theta * theta); 
+        for (i = 1; i < maxdepthd; i++) {
+                dq[i] = dq[i - 1] * 0.25f; // radius is halved every level of tree so squared radius is quartered
+                dq[i - 1] += epssqd;
+        }
+        dq[i - 1] += epssqd;
 
-		if (maxdepthd > MAXDEPTH)
-			*errd = maxdepthd;
-	}
-	kernel_sync();
+        if (maxdepthd > MAXDEPTH)
+            *errd = maxdepthd;
+    }
+    kernel_sync();
 
 
-	if (maxdepthd <= MAXDEPTH) {
-		// figure out first thread in each warp (lane 0)
-		base = threadIdx.x / WARPSIZE;
-		sbase = base * WARPSIZE;
-		j = base * MAXDEPTH;
+    if (maxdepthd <= MAXDEPTH) {
+        // figure out first thread in each warp (lane 0)
+        base = threadIdx.x / WARPSIZE;
+        sbase = base * WARPSIZE;
+        j = base * MAXDEPTH;
 
-		diff = threadIdx.x - sbase;
-		// make multiple copies to avoid index calculations later
-		if (diff < MAXDEPTH)
-			dq[diff+j] = dq[diff];
+        diff = threadIdx.x - sbase;
+        // make multiple copies to avoid index calculations later
+        if (diff < MAXDEPTH)
+            dq[diff+j] = dq[diff];
 
-		kernel_sync();
-		__threadfence_block();
+        kernel_sync();
+        __threadfence_block();
 
-		// iterate over all bodies assigned to thread
-		for (k = threadIdx.x + blockIdx.x * blockDim.x; k < nbodiesd; k += blockDim.x * gridDim.x) {
-			i = sortd[k];    // get permuted/sorted index
-			// cache position info
-			px = posxd[i];
-			py = posyd[i];
+        // iterate over all bodies assigned to thread
+        for (k = threadIdx.x + blockIdx.x * blockDim.x; k < nbodiesd; k += blockDim.x * gridDim.x) {
+            i = sortd[k];    // get permuted/sorted index
+            // cache position info
+            px = posxd[i];
+            py = posyd[i];
 
-			vx = 0.0f;
-			vy = 0.0f;
-			normsum = 0.0f;
+            vx = 0.0f;
+            vy = 0.0f;
+            normsum = 0.0f;
 
-			// initialize iteration stack, i.e., push root node onto stack
-			depth = j;
-			if (sbase == threadIdx.x) {
-				pos[j] = 0;
-				node[j] = nnodesd * 4;
-			}
+            // initialize iteration stack, i.e., push root node onto stack
+            depth = j;
+            if (sbase == threadIdx.x) {
+                pos[j] = 0;
+                node[j] = nnodesd * 4;
+            }
 
-			do {
-				// stack is not empty
-				pd = pos[depth];
-				nd = node[depth];
-				while (pd < 4) {
-					// node on top of stack has more children to process
-					n = childd[nd + pd];    // load child pointer
-					pd++;
+            do {
+                // stack is not empty
+                pd = pos[depth];
+                nd = node[depth];
+                while (pd < 4) {
+                    // node on top of stack has more children to process
+                    n = childd[nd + pd];    // load child pointer
+                    pd++;
 
-					if (n >= 0) {
-						dx = px - posxd[n];
-						dy = py - posyd[n];
-						tmp = dx*dx + dy*dy + epssqd; // distance squared plus small constant to prevent zeros
-						#if (CUDART_VERSION >= 9000)
-							if ((n < nbodiesd) || __all_sync(__activemask(), tmp >= dq[depth])) {    // check if all threads agree that cell is far enough away (or is a body)
-						#else
-							if ((n < nbodiesd) || __all(tmp >= dq[depth])) {    // check if all threads agree that cell is far enough away (or is a body)
-						#endif
-							// from bhtsne - sptree.cpp
-							tmp = 1 / (1 + tmp);
-							mult = massd[n] * tmp;
-							normsum += mult;
-							mult *= tmp;
-							vx += dx * mult;
-							vy += dy * mult;
-						}
-						else {
-							// push cell onto stack
-							if (sbase == threadIdx.x) {    // maybe don't push and inc if last child
-								pos[depth] = pd;
-								node[depth] = nd;
-							}
-							depth++;
-							pd = 0;
-							nd = n * 4;
-						}
-					}
-					else
-						pd = 4;    // early out because all remaining children are also zero
-				}
-				depth--;    // done with this level
-				
-			} while (depth >= j);
+                    if (n >= 0) {
+                        dx = px - posxd[n];
+                        dy = py - posyd[n];
+                        tmp = dx*dx + dy*dy + epssqd; // distance squared plus small constant to prevent zeros
+                        #if (CUDART_VERSION >= 9000)
+                            if ((n < nbodiesd) || __all_sync(__activemask(), tmp >= dq[depth])) {    // check if all threads agree that cell is far enough away (or is a body)
+                        #else
+                            if ((n < nbodiesd) || __all(tmp >= dq[depth])) {    // check if all threads agree that cell is far enough away (or is a body)
+                        #endif
+                            // from bhtsne - sptree.cpp
+                            tmp = 1 / (1 + tmp);
+                            mult = massd[n] * tmp;
+                            normsum += mult;
+                            mult *= tmp;
+                            vx += dx * mult;
+                            vy += dy * mult;
+                        }
+                        else {
+                            // push cell onto stack
+                            if (sbase == threadIdx.x) {    // maybe don't push and inc if last child
+                                pos[depth] = pd;
+                                node[depth] = nd;
+                            }
+                            depth++;
+                            pd = 0;
+                            nd = n * 4;
+                        }
+                    }
+                    else
+                        pd = 4;    // early out because all remaining children are also zero
+                }
+                depth--;    // done with this level
+                
+            } while (depth >= j);
 
-			if (stepd >= 0) {
-				// update velocity
-				velxd[i] += vx;
-				velyd[i] += vy;
-				normd[i] = normsum - 1.0f; // subtract one for self computation (qii)
-			}
-		}
-	}
+            if (stepd >= 0) {
+                // update velocity
+                velxd[i] += vx;
+                velyd[i] += vy;
+                normd[i] = normsum - 1.0f; // subtract one for self computation (qii)
+            }
+        }
+    }
 }
 
 // end namespace
