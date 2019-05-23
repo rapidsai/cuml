8868fb6b62b413244bffdbea385c069790cc1ab2
Rick Ratzel <rratzel@nvidia.com>
2019-05-23 11:44:40 -0700

    Initial untested updates for build.sh script

--- build.sh
+++ build.sh
@@ -18,17 +18,19 @@ ARGS=$*
 # script, and that this script resides in the repo dir!
 REPODIR=$(cd $(dirname $0); pwd)
 
-VALIDARGS="clean libcuml cuml -v -g -n -h"
+VALIDARGS="clean libcuml cuml prims -v -g -n --buildAllGPUArch -h"
 HELP="$0 [clean] [libcuml] [cuml] [-v] [-g] [-n] [-h]
-   clean   - remove all existing build artifacts and configuration (start over)
-   libcuml - build the cuml C++ code only
-   cuml    - build the cuml Python package
-   -v      - verbose build mode
-   -g      - build for debug
-   -n      - no install step
-   -h      - print this text
+   clean             - remove all existing build artifacts and configuration (start over)
+   libcuml           - build the cuml C++ code only
+   cuml              - build the cuml Python package
+   prims             - build the ML prims tests
+   -v                - verbose build mode
+   -g                - build for debug
+   -n                - no install step
+   --buildAllGPUArch - build for all supported GPU architectures
+   -h                - print this text
 
-   default action (no args) is to build and install 'libcuml' then 'cuml' targets
+   default action (no args) is to build and install 'libcuml', 'cuml', and 'prims' targets only for the detected GPU arch
 "
 LIBCUML_BUILD_DIR=${REPODIR}/cpp/build
 CUML_BUILD_DIR=${REPODIR}/python/build
@@ -38,13 +40,14 @@ BUILD_DIRS="${LIBCUML_BUILD_DIR} ${CUML_BUILD_DIR}"
 VERBOSE=""
 BUILD_TYPE=Release
 INSTALL_TARGET=install
+BUILD_ALL_GPU_ARCH=0
 
 # Set defaults for vars that may not have been defined externally
 #  FIXME: if INSTALL_PREFIX is not set, check PREFIX, then check
 #         CONDA_PREFIX, but there is no fallback from there!
 INSTALL_PREFIX=${INSTALL_PREFIX:=${PREFIX:=${CONDA_PREFIX}}}
-PYTHON=${PYTHON:=python}
 PARALLEL_LEVEL=${PARALLEL_LEVEL:=""}
+BUILD_ABI=${BUILD_ABI:=ON}
 
 function hasArg {
     (( ${NUMARGS} != 0 )) && (echo " ${ARGS} " | grep -q " $1 ")
@@ -75,6 +78,16 @@ fi
 if hasArg -n; then
     INSTALL_TARGET=""
 fi
+if hasArg --buildAllGPUArch; then
+    BUILD_ALL_GPU_ARCH=1
+fi
+
+# Various build options use nvidia-smi for querying the system
+HAS_NVIDIA_SMI=1
+which nvidia-smi > /dev/null
+if (( $? != 0 )); then
+    HAS_NVIDIA_SMI=0
+fi
 
 # If clean given, run it prior to any other steps
 if hasArg clean; then
@@ -91,28 +104,77 @@ if hasArg clean; then
 fi
 
 ################################################################################
-# Configure, build, and install libcuml
-if (( ${NUMARGS} == 0 )) || hasArg libcuml; then
+# Configure for building
+if (( ${NUMARGS} == 0 )) || hasArg libcuml || hasArg prims; then
+
+    # Configure the build for the GPU type of this machine, or all (GPU_ARCH="")
+    # if it cannot be detected.
+    if (( ${BUILD_ALL_GPU_ARCH} == 0 )); then
+        if (( ${HAS_NVIDIA_SMI} )); then
+            GPU="$(nvidia-smi | awk '{print $4}' | sed '8!d')"
+            if [[ $GPU == *"P100"* ]]; then
+                GPU_ARCH="-DGPU_ARCHS=\"60\""
+                echo "Building for Pascal..."
+            elif [[ $GPU == *"V100"* ]]; then
+                GPU_ARCH="-DGPU_ARCHS=\"70\""
+                echo "Building for Volta..."
+            elif [[ $GPU == *"T4"* ]]; then
+                GPU_ARCH="-DGPU_ARCHS=\"75\""
+                echo "Building for Turing..."
+            fi
+        else
+            echo "nvidia-smi was not found on PATH and is needed for detecting the GPU arch"
+            echo "Ensure nvidia-smi is on PATH or use --buildAllGPUArch"
+            exit 1
+        fi
+    else
+        GPU_ARCH=""
+        echo "Building for *ALL* GPU architectures..."
+    fi
 
     mkdir -p ${LIBCUML_BUILD_DIR}
     cd ${LIBCUML_BUILD_DIR}
 
     cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
+          -DCMAKE_CXX11_ABI=${BUILD_ABI} \
           -DBLAS_LIBRARIES=${INSTALL_PREFIX}/lib/libopenblas.so \
           -DLAPACK_LIBRARIES=${INSTALL_PREFIX}/lib/libopenblas.so \
-          -DCMAKE_CXX11_ABI=ON \
+          ${GPU_ARCH} \
           -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ..
-    make -j${PARALLEL_LEVEL} VERBOSE=${VERBOSE} ${INSTALL_TARGET}
 fi
 
-# Build and install the cuml Python package
+# Build and (optionally) install libcuml + tests
+if (( ${NUMARGS} == 0 )) || hasArg libcuml; then
+
+    cd ${LIBCUML_BUILD_DIR}
+    make -j${PARALLEL_LEVEL} cuml++ ml ml_mg VERBOSE=${VERBOSE} ${INSTALL_TARGET}
+fi
+
+# Build and (optionally) install the cuml Python package
 if (( ${NUMARGS} == 0 )) || hasArg cuml; then
+
+    # Set the multigpu option based on CUDA version: version 9.2 will disable
+    # multigpu, all others assume it should be enabled.
+    MULTIGPU_OPTION=--multigpu
+    if (( ${HAS_NVIDIA_SMI} )); then
+        CUDA_VERSION=$(nvidia-smi -q|grep "CUDA Version"|awk '{print $NF}')
+        if [[ ${CUDA_VERSION} == "9.2" ]]; then
+            MULTIGPU_OPTION=""
+        fi
+    fi
 
     cd ${REPODIR}/python
     if [[ ${INSTALL_TARGET} != "" ]]; then
-	${PYTHON} setup.py build_ext --inplace ${MULTIGPU_OPTION}
-	${PYTHON} setup.py install --single-version-externally-managed --record=record.txt ${MULTIGPU_OPTION}
+	python setup.py build_ext --inplace ${MULTIGPU_OPTION}
+	python setup.py install --single-version-externally-managed --record=record.txt ${MULTIGPU_OPTION}
     else
-	${PYTHON} setup.py build_ext --inplace --library-dir=${LIBCUML_BUILD_DIR} ${MULTIGPU_OPTION}
+	python setup.py build_ext --inplace --library-dir=${LIBCUML_BUILD_DIR} ${MULTIGPU_OPTION}
     fi
+fi
+
+# Build the ML prims tests
+if (( ${NUMARGS} == 0 )) || hasArg prims; then
+
+    cd ${LIBCUML_BUILD_DIR}
+    make -j${PARALLEL_LEVEL} VERBOSE=${VERBOSE} prims
 fi
