0655e23e29708ae1b4ed5547a6d0fb35ec275b10
Dante Gama Dessavre <danteg@nvidia.com>
2019-05-23 12:45:35 -0500

    FIX Reduce test size of trustworthiness since it takes too long for unit test

--- python/cuml/test/test_trustworthiness.py
+++ python/cuml/test/test_trustworthiness.py
@@ -24,9 +24,9 @@ import numpy as np
 
 
 @pytest.mark.parametrize('input_type', ['dataframe', 'ndarray'])
-@pytest.mark.parametrize('n_samples', [100, 1000])
-@pytest.mark.parametrize('n_features', [100, 500])
-@pytest.mark.parametrize('n_components', [10, 100])
+@pytest.mark.parametrize('n_samples', [10, 100])
+@pytest.mark.parametrize('n_features', [10, 100])
+@pytest.mark.parametrize('n_components', [2, 10])
 def test_trustworthiness(input_type, n_samples, n_features, n_components):
     centers = round(n_samples*0.4)
     X, y = make_blobs(n_samples=n_samples, centers=centers,
