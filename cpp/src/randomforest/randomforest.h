544a8fa693e6d4c3359a064bdb9543abc2fb290b
Salonijain27 <sj7856@g.rit.edu>
2019-05-28 14:19:07 -0500

    Update randomforest.h

--- cpp/src/randomforest/randomforest.h
+++ cpp/src/randomforest/randomforest.h
@@ -14,50 +14,10 @@
  * limitations under the License.
  */
 
-#pragma once
-#include "decisiontree/decisiontree.h"
-#include <iostream>
-#include <utils.h>
-#include "random/rng.h"
-#include <map>
-#include <common/cumlHandle.hpp>
-#include <common/device_buffer.hpp>
+#include "rf_basic.h"
 
 namespace ML {
 
-enum RF_type {
-	CLASSIFICATION, REGRESSION,
-};
-
-struct RF_params {
-	/**
-	 * Control bootstrapping. If set, each tree in the forest is built on a bootstrapped sample with replacement.
-	 * If false, sampling without replacement is done.
-	 */
-	bool bootstrap = true;
-	/**
-	 * Control bootstrapping for features. If features are drawn with or without replacement
-	 */
-	bool bootstrap_features = false;
-	/**
-	 * Number of decision trees in the random forest.
-	 */
-	int n_trees;
-	/**
-	 * Ratio of dataset rows used while fitting each tree.
-	 */
-	float rows_sample = 1.0f;
-	/**
-	 * Decision tree traingin hyper parameter struct.
-	 */
-	DecisionTree::DecisionTreeParams tree_params;
-
-	RF_params(int cfg_n_trees);
-	RF_params(bool cfg_bootstrap, bool cfg_bootstrap_features, int cfg_n_trees, float cfg_rows_sample);
-	RF_params(bool cfg_bootstrap, bool cfg_bootstrap_features, int cfg_n_trees, float cfg_rows_sample, DecisionTree::DecisionTreeParams cfg_tree_params);
-	void validity_check() const;
-	void print() const;
-};
 
 /* Update labels so they are unique from 0 to n_unique_vals.
    		Create an old_label to new_label map per random forest.
@@ -67,20 +27,17 @@ void preprocess_labels(int n_rows, std::vector<int> & labels, std::map<int, int>
 /* Revert preprocessing effect, if needed. */
 void postprocess_labels(int n_rows, std::vector<int> & labels, std::map<int, int> & labels_map, bool verbose=false);
 
-template<class T>
-class rf {
-	protected:
-		RF_params rf_params;
-		int rf_type;
-		DecisionTree::DecisionTreeClassifier<T> * trees;
 
-	public:
-		rf(RF_params cfg_rf_params, int cfg_rf_type=RF_type::CLASSIFICATION);
-		~rf();
+// Stateless API functions: fit, predict and cross_validate.
+void fit(const cumlHandle& user_handle, rfClassifier<float> * rf_classifier, float * input, int n_rows, int n_cols, int * labels, int n_unique_labels);
+void fit(const cumlHandle& user_handle, rfClassifier<double> * rf_classifier, double * input, int n_rows, int n_cols, int * labels, int n_unique_labels);
 
-		int get_ntrees();
-		void print_rf_summary();
-		void print_rf_detailed();
-};
+void predict(const cumlHandle& user_handle, const rfClassifier<float> * rf_classifier, const float * input, int n_rows, int n_cols, int * predictions, bool verbose=false);
+void predict(const cumlHandle& user_handle, const rfClassifier<double> * rf_classifier, const double * input, int n_rows, int n_cols, int * predictions, bool verbose=false);
+
+RF_metrics cross_validate(const cumlHandle& user_handle, const rfClassifier<float> * rf_classifier, const float * input, const int * ref_labels,
+							int n_rows, int n_cols, int * predictions, bool verbose=false);
+RF_metrics cross_validate(const cumlHandle& user_handle, const rfClassifier<double> * rf_classifier, const double * input, const int * ref_labels,
+							int n_rows, int n_cols, int * predictions, bool verbose=false);
 
 };
