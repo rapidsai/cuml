a80c5be50aaabb87bb293eb06fa0aca70de06b8a
Ganesh Venkataramana <gvenkatarama@dbcluster.nvidia.com>
2019-06-04 03:55:26 -0700

    refactoring comments
    - modifying the dOxygen comments
    - minor changes to formatting and comments

--- cpp/src_prims/metrics/adjustedRandIndex.h
+++ cpp/src_prims/metrics/adjustedRandIndex.h
@@ -13,6 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/**
+* @file adjustedRandIndex.h
+* @brief The adjusted Rand index is the corrected-for-chance version of the Rand index.
+* Such a correction for chance establishes a baseline by using the expected similarity 
+* of all pair-wise comparisons between clusterings specified by a random model.
+*/
 
 #include "common/cuml_allocator.hpp"
 #include "common/device_buffer.hpp"
@@ -27,6 +33,11 @@ namespace MLCommon {
 
 /**
 * @brief Lambda to calculate the number of unordered pairs in a given input
+*
+* @tparam Type: Data type of the input 
+* @tparam IdxType : type of the indexing (by default int)
+* @param in: the input to the functional mapping
+* @param i: the indexing(not used in this case)
 */
 template <typename Type, typename IdxType = int>
 struct nCTwo {
@@ -45,6 +56,9 @@ namespace Metrics {
 * @param firstClusterArray: the array of classes of type T
 * @param secondClusterArray: the array of classes of type T
 * @param size: the size of the data points of type int
+* @param numUniqueClasses: number of Unique classes used for clustering
+* @param lowerLabelRange: the lower bound of the range of labels
+* @param upperLabelRange: the upper bound of the range of labels
 * @param allocator: object that takes care of temporary device memory allocation of type std::shared_ptr<MLCommon::deviceAllocator>
 * @param stream: the cudaStream object
 */
@@ -70,17 +84,20 @@ double computeAdjustedRandIndex (T* firstClusterArray, T* secondClusterArray, in
   MLCommon::Metrics::contingencyMatrix(firstClusterArray, secondClusterArray, (int)size, (int*)dContingencyMatrix.data(),
                                         stream, (void*)pWorkspace, workspaceSz, lowerLabelRange, upperLabelRange);
 
+  //creating device buffers for all the parameters involved in ARI calculation
+  //device variables
   MLCommon::device_buffer<int> a(allocator, stream, numUniqueClasses);
   MLCommon::device_buffer<int> b(allocator, stream, numUniqueClasses);
   MLCommon::device_buffer<int> d_aCTwoSum(allocator, stream, 1);
   MLCommon::device_buffer<int> d_bCTwoSum(allocator, stream, 1);
   MLCommon::device_buffer<int> d_nChooseTwoSum(allocator, stream, 1);
+  //host variables
   int h_aCTwoSum;
   int h_bCTwoSum;
   int h_nChooseTwoSum;
 
 
-  //allocating and initializing device memory
+  //initializing device memory
   CUDA_CHECK(cudaMemsetAsync(a.data(), 0, numUniqueClasses*sizeof(int), stream));
   CUDA_CHECK(cudaMemsetAsync(b.data(), 0, numUniqueClasses*sizeof(int), stream));
   CUDA_CHECK(cudaMemsetAsync(d_aCTwoSum.data(), 0, sizeof(int), stream));
@@ -118,6 +135,7 @@ double computeAdjustedRandIndex (T* firstClusterArray, T* secondClusterArray, in
   double maxIndex = ((double)(h_bCTwoSum+h_aCTwoSum))/2.0;
   double index = (double)h_nChooseTwoSum;
 
+  //checking if the denominator is zero
   if(maxIndex - expectedIndex)
     return (index - expectedIndex)/(maxIndex - expectedIndex);
   else return 0;
