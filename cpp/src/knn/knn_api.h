255a3a37d8c818577e7033a139def12a96fd1410
Corey J. Nolet <cjnolet@gmail.com>
2019-06-04 13:52:16 -0400

    Adding docs to headers of c/c++ api functions

--- cpp/src/knn/knn_api.h
+++ cpp/src/knn/knn_api.h
@@ -22,6 +22,22 @@
 extern "C" {
 #endif
 
+  /**
+   * @brief Flat C API function to perform a brute force knn on
+   * a series of input arrays and combine the results into a single
+   * output array for indexes and distances.
+   *
+   * @param handle the cuml handle to use
+   * @param input an array of pointers to the input arrays
+   * @param sizes an array of sizes of input arrays
+   * @param n_params array size of input and sizes
+   * @param D the dimensionality of the arrays
+   * @param search_items array of items to search of dimensionality D
+   * @param n number of rows in search_items
+   * @param res_I the resulting index array of size n * k
+   * @param res_D the resulting distance array of size n * k
+   * @param k the number of nearest neighbors to return
+   */
 cumlError_t knn_search(
   const cumlHandle_t handle,
   float **input, int *size, int n_params, int D,
@@ -29,6 +45,18 @@ cumlError_t knn_search(
   long *res_I, float *res_D, int k
 );
 
+/**
+ * @brief A flat C++ API function that chunks a host array up into
+ * some number of different devices
+ *
+ * @param ptr an array on host to chunk
+ * @param n number of rows in host array
+ * @param D number of cols in host array
+ * @param devices array of devices to use
+ * @param output an array of output pointers to allocate and use
+ * @param sizes output array sizes
+ * @param n_chunks number of chunks to spread across device arrays
+ */
 cumlError_t chunk_host_array(
   const cumlHandle_t handle,
   const float *ptr, int n, int D,
