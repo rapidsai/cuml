af93d2dd0a02b76efd4afa54b99ce73cb30d6469
Corey J. Nolet <cjnolet@gmail.com>
2019-06-10 12:06:32 -0400

    Renaming "initialize_comms" to be more descriptive of actual purpose

--- cpp/nccl_comms/include/cuML_comms.hpp
+++ cpp/nccl_comms/include/cuML_comms.hpp
@@ -21,6 +21,6 @@
 
 namespace ML {
 
-void initialize_comms(cumlHandle& handle, ncclComm_t comm, int size, int rank);
+void inject_comms(cumlHandle& handle, ncclComm_t comm, int size, int rank);
 
 } // end namespace ML
