11ba2dc9fac3729b3b0e4e7d30bf7aa16fb2fda5
Dante Gama Dessavre <dante.gamadessavre@gmail.com>
2019-06-11 11:03:08 -0500

    Update ci/gpu/build.sh
    Co-Authored-By: Keith Kraus <keith.j.kraus@gmail.com>

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -35,7 +35,7 @@ nvidia-smi
 
 logger "Activate conda env..."
 source activate gdf
-conda install -c rapidsai/label/cuda${CUDA_REL} -c rapidsai-nightly/label/cuda${CUDA_REL} -c conda-forge -c rapidsai cudf=${CUDF_VERSION} rmm=${RMM_VERSION} nvstrings=${NVSTRINGS_VERSION} lapack cmake==3.14.3 umap-learn libclang cupy
+conda install cudf=${CUDF_VERSION} rmm=${RMM_VERSION} nvstrings=${NVSTRINGS_VERSION} lapack cmake==3.14.3 umap-learn libclang cupy
 
 logger "Check versions..."
 python --version
