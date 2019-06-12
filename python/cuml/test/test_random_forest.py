b919dc72490b8bbe1f0d633be7ef69c791934d8f
Salonijain27 <sj7856@g.rit.edu>
2019-06-11 20:30:48 -0500

    Update test_random_forest.py

--- python/cuml/test/test_random_forest.py
+++ python/cuml/test/test_random_forest.py
@@ -1,35 +1,33 @@
 import pytest
+import numpy as np
 from cuml import RandomForest as curfc
 from cuml.test.utils import get_handle
+from cuml.test.utils import array_equal
+from sklearn.datasets import make_classification
 from sklearn.ensemble import RandomForestClassifier as skrfc
-import numpy as np
-from cuml.test.utils import get_pattern
-
-dataset_names = ['varied', 'aniso', 'blobs', 'noisy_circles',
-                 'no_structure']
 
 
-@pytest.mark.parametrize('name', dataset_names)
 @pytest.mark.parametrize('datatype', [np.float32, np.float64])
 @pytest.mark.parametrize('use_handle', [True, False])
-def test_rf_predict_numpy(datatype, name, use_handle):
-    pat = get_pattern(name, 100)
-    X, y = pat[0]
-    unique_labels = np.shape(np.unique(y))[0]
+def test_rf_predict_numpy(datatype, use_handle):
+
+    X, y = make_classification(n_samples=100, n_features=40,
+                               n_clusters_per_class=1, n_informative=30,
+                               random_state=123, n_classes=5)
+    y = y.astype(np.int32)
     handle, stream = get_handle(use_handle)
 
-    cuml_model = curfc(max_depth=3, max_leaves=2,
-                       max_features=1.0, n_bins=2,
-                       split_algo=0, min_rows_per_node=2,
-                       n_estimators=1, handle=handle,
-                       n_unique_labels=unique_labels)
+    cuml_model = curfc(max_depth=-1, max_leaves=-1, max_features=1.0,
+                       n_bins=4, split_algo=0, min_rows_per_node=2,
+                       n_estimators=20, handle=handle)
     cuml_model.fit(X, y)
-    cu_labels = cuml_model.predict(X) 
-    
-    sk_model = skrfc(n_estimators=1, max_depth=2,
+    cu_predict = cuml_model.predict(X)
+
+    sk_model = skrfc(n_estimators=20, max_depth=None,
                      min_samples_split=2, max_features=1.0)
     sk_model.fit(X, y)
-    sk_labels = sk_model.predict(X)
+    sk_predict = sk_model.predict(X)
+
     cuml_model.handle.sync()
-    if sk_labels == cu_labels:
-        assert 1
+
+    assert array_equal(sk_predict, cu_predict, 1e-1, with_sign=True)
