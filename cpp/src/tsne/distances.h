3d0fcc4deef776604ec46131b299873d90c48031
Corey J. Nolet <cjnolet@gmail.com>
2019-06-13 15:33:03 -0400

    Using random vector to initialize Y.

--- cpp/src/tsne/distances.h
+++ cpp/src/tsne/distances.h
@@ -2,73 +2,68 @@
 #pragma once
 #include <knn/knn.h>
 #include <linalg/eltwise.h>
+#include "kernels.h"
 #include "utils.h"
-
 
 namespace ML {
 using namespace MLCommon;
 
-
-void
-get_distances(const float *X, const int n, const int p,
-	long *indices, float *distances, const int n_neighbors,
-	cuda_stream_t stream)
-{	
-
+void get_distances(const float *X, const int n, const int p, long *indices,
+                   float *distances, const int n_neighbors,
+                   cuda_stream_t stream) {
   cumlHandle handle;
-	kNNParams *params = new kNNParams[1];
-    params[0].ptr = (float*) X;
-    params[0].N = n;
-    kNN *knn = new kNN(handle, p, false);
+  kNNParams *params = new kNNParams[1];
+  params[0].ptr = (float *)X;
+  params[0].N = n;
+  kNN *knn = new kNN(handle, p, false);
 
-    // Fit KNN and find best approximate neighbors
-    knn->fit(params, 1);
-    knn->search(X, n, indices, distances, n_neighbors);
-    cudaDeviceSynchronize();
+  // Fit KNN and find best approximate neighbors
+  knn->fit(params, 1);
+  knn->search(X, n, indices, distances, n_neighbors);
+  cudaDeviceSynchronize();
 
-    // Now D / max(abs(D)) to allow exp(D) to not explode
-    thrust_t<float> begin = to_thrust(distances);
-    thrust_t<float> end = begin + n*n_neighbors;
+  // Now D / max(abs(D)) to allow exp(D) to not explode
+  thrust_t<float> begin = to_thrust(distances);
+  thrust_t<float> end = begin + n * n_neighbors;
 
-    float maxNorm = MAX(*(thrust::max_element(__STREAM__, begin, end)), 
-    					*(thrust::min_element(__STREAM__, begin, end)));
-    if (maxNorm == 0.0f) maxNorm = 1.0f;
+  float maxNorm = MAX(*(thrust::max_element(__STREAM__, begin, end)),
+                      *(thrust::min_element(__STREAM__, begin, end)));
+  if (maxNorm == 0.0f) maxNorm = 1.0f;
 
-    // Divide distances inplace by max
-    float div_maxNorm = 1.0f/maxNorm; // Mult faster than div
-    LinAlg::scalarMultiply(distances, distances, div_maxNorm, n*n_neighbors, stream);
+  // Divide distances inplace by max
+  float div_maxNorm = 1.0f / maxNorm;  // Mult faster than div
+  LinAlg::scalarMultiply(distances, distances, div_maxNorm, n * n_neighbors,
+                         stream);
 
-    // Remove temp variables
-    delete knn, params;
+  // Remove temp variables
+  delete knn, params;
 }
 
+void symmetrize_perplexity(float *P, long *indices, COO_t<float> *P_PT,
+                           const int n, const int k, const float P_sum,
+                           const float exaggeration, cuda_stream_t stream) {
+  // Convert to COO
+  COO_t<float> P_COO;
+  Sparse::from_knn(indices, P, n, k, &P_COO);
+  cfree(P);
+  cfree(indices);
 
+  // Perform (P + P.T) / P_sum * early_exaggeration
+  Sparse::coo_symmetrize<32, float>(
+    &P_COO, P_PT,
+    [] __device__(int row, int col, float val, float trans) {
+      return val + trans;
+    },
+    stream);
+  cudaDeviceSynchronize();
 
-void
-symmetrize_perplexity(float *P, long *indices, COO_t<float> *P_PT,
-	const int n, const int k, const float P_sum, const float exaggeration, cuda_stream_t stream)
-{
-	// Convert to COO
-	COO_t<float> P_COO;
-	Sparse::from_knn(indices, P, n, k, &P_COO);
-	cfree(P);
-	cfree(indices);
+  // Divide by P_sum
+  // Notice P_sum is *2 since symmetric.
+  const float div = exaggeration / (2.0f * P_sum);
 
-	// Perform (P + P.T) / P_sum * early_exaggeration
-	Sparse::coo_symmetrize<32, float>(&P_COO, P_PT,
-        [] __device__ (int row, int col, float val, float trans) { 
-        	return val + trans; 
-        }, stream);
-    cudaDeviceSynchronize();
+  inplace_multiply(P_PT->vals, P_PT->nnz, div);
 
-    // Divide by P_sum
-    // Notice P_sum is *2 since symmetric.
-    const float div = exaggeration / (2.0f * P_sum);
-
-    inplace_multiply(P_PT->vals, P_PT->nnz, div);
-
-    P_COO.destroy();
+  P_COO.destroy();
 }
 
-
-}
+}  // namespace ML
--- cpp/test/sg/tsne_test.cu
+++ cpp/test/sg/tsne_test.cu
@@ -1,62 +1,57 @@
 #ifndef IF_DEBUG
-    #define IF_DEBUG 1 
+#define IF_DEBUG 1
 #endif
 
 #include <gtest/gtest.h>
-#include "tsne/tsne.h"
-#include "tsne/digits.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
+#include "tsne/digits.h"
+#include "tsne/tsne.h"
 
 using namespace ML;
 using namespace MLCommon;
 
+class TSNETest : public ::testing::Test {
+ protected:
+  void basicTest() {
+    cumlHandle handle;
+    cudaStream_t stream;
+    CUDA_CHECK(cudaStreamCreate(&stream));
 
-class TSNETest: public ::testing::Test {
-protected:
-    void basicTest() {
+    float *X_d, *Y_d;
+    MLCommon::allocate(X_d, n * p);
+    MLCommon::allocate(Y_d, n * 2);
+    MLCommon::updateDevice(X_d, digits.data(), n * p, stream);
 
-        cumlHandle handle;
-        cudaStream_t stream; CUDA_CHECK(cudaStreamCreate(&stream));
+    std::cout << "[>>>>]    Starting TSNE....\n";
+    TSNE(handle, X_d, Y_d, n, p);
+    std::cout << "[>>>>]    Got embeddings!....\n";
 
-        float *X_d, *Y_d;
-        MLCommon::allocate(X_d, n*p);
-        MLCommon::allocate(Y_d, n*2);
-        MLCommon::updateDevice(X_d, digits.data(), n*p, stream);
+    std::cout << MLCommon::arr2Str(Y_d, 20, "embeddings", stream) << std::endl;
 
-        std::cout << "[>>>>]    Starting TSNE....\n";
-        TSNE(handle, X_d, Y_d, n, p);
-        std::cout << "[>>>>]    Got embeddings!....\n";
+    std::cout << "Updating host" << std::endl;
+    float embeddings_h[n * 2];
+    MLCommon::updateHost(embeddings_h, Y_d, n * 2, stream);
 
-        std::cout << MLCommon::arr2Str(Y_d, n*2, "embeddings", stream) << std::endl;
+    std::cout << "DONE!" << std::endl;
 
-        std::cout << "Updating host" << std::endl;
-        float embeddings_h[n*2];
-        MLCommon::updateHost(embeddings_h, Y_d, n*2, stream);
+    CUDA_CHECK(cudaFree(Y_d));
+    CUDA_CHECK(cudaFree(X_d));
 
-        std::cout << "DONE!" << std::endl;
+    std::cout << "End!" << std::endl;
 
-        CUDA_CHECK(cudaFree(Y_d));
-        CUDA_CHECK(cudaFree(X_d));
+    CUDA_CHECK(cudaStreamDestroy(stream));
+  }
 
-        std::cout << "End!" << std::endl;
+  void SetUp() override { basicTest(); }
 
-        CUDA_CHECK(cudaStreamDestroy(stream));
-    }
+  void TearDown() override {}
 
-    void SetUp() override {
-        basicTest();
-    }
-
-    void TearDown() override {
-    }
-
-protected:
-    int n = 1797;
-    int p = 64;
+ protected:
+  int n = 1797;
+  int p = 64;
 };
-
 
 typedef TSNETest TSNETestF;
 TEST_F(TSNETestF, Result) {}
