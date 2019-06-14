5ff965d51202acc7d15167b654a5a5aeaf28c787
Daniel Han-Chen <danielhanchen@gmail.com>
2019-06-15 03:40:40 +1000

    Cleaned up some code

---
+++ cpp/src/tsne/fast_kernels.h
@@ -0,0 +1,262 @@
+
+#pragma once
+
+#include <float.h>
+#include <math.h>
+#include "utils.h"
+#define ceil(a, b) ((a + b - 1) / b)
+
+namespace ML {
+
+using namespace ML;
+using namespace MLCommon;
+
+
+__global__ void 
+__determine_sigmas(const float *__restrict__ distances,
+				 float *__restrict__ P,
+				 const float perplexity,
+				 const float desired_entropy,
+				 float *__restrict__ P_sum,
+				 const int epochs, const float tol,
+				 const int n, const int k)
+{
+	// For every item in row
+	const int i = (blockIdx.x * blockDim.x) + threadIdx.x;
+	if (i < n) {
+		float beta_min = -INFINITY;
+		float beta_max = INFINITY;
+		float beta = 1;
+		float sum_Pi = 0;
+		float sum_P_row, sum_disti_Pi, div_sum_Pi;
+		float entropy, entropy_diff;
+		register int ik = i * k;
+
+		for (int step = 0; step < epochs; step++) {
+			sum_Pi = FLT_EPSILON;
+			sum_disti_Pi = 0;
+			sum_P_row = 0;
+
+			// Exponentiate to get guassian
+			for (int j = 0; j < k; j++) {
+				P[ik + j] = __expf(-distances[ik + j] * beta);
+				sum_Pi += P[ik + j];
+			}
+
+			// Normalize
+			div_sum_Pi = 1.0f / sum_Pi;
+			for (int j = 0; j < k; j++) {
+				P[ik + j] *= div_sum_Pi;  // P[i*k + j] / sum_Pi
+				sum_disti_Pi += distances[ik + j] * P[ik + j];
+				sum_P_row += P[ik + j];
+			}
+
+			entropy = __logf(sum_Pi) + beta * sum_disti_Pi;
+			entropy_diff = entropy - desired_entropy;
+			if (fabs(entropy_diff) <= tol) 
+				break;
+
+			// Bisection search
+			if (entropy_diff > 0) {
+				beta_min = beta;
+				if (isinf(beta_max))
+					beta *= 2.0f;
+				else
+					beta = (beta + beta_max) * 0.5f;
+			} else {
+				beta_max = beta;
+				if (isinf(beta_min))
+					beta *= 0.5f;
+				else
+					beta = (beta + beta_min) * 0.5f;
+			}
+		}
+		atomicAdd(P_sum, sum_P_row);
+	}
+}
+float determine_sigmas(const float *__restrict__ distances,
+					 float *__restrict__ P, const float perplexity,
+					 const int epochs, const float tol, const int n,
+					 const int k, cudaStream_t stream) {
+	const float desired_entropy = logf(perplexity);
+	float *P_sum_, P_sum;
+	cudaMalloc(&P_sum_, sizeof(float));
+	cudaMemset(P_sum_, 0, sizeof(float));
+
+	__determine_sigmas<<<ceil(n, 1024), 1024, 0, stream>>>(
+		distances, P, perplexity, desired_entropy, P_sum_, epochs, tol, n, k);
+
+	cudaMemcpy(&P_sum, P_sum_, sizeof(float), cudaMemcpyDeviceToHost);
+	CUDA_CHECK(cudaPeekAtLastError());
+	return P_sum;
+}
+
+
+
+
+__global__ void
+__get_norm(const float *__restrict__ Y, float *__restrict__ norm, 
+			const int n, const int n_components)
+{
+	const int i = (blockIdx.x * blockDim.x) + threadIdx.x;  // for every item in col
+	const int j = (blockIdx.y * blockDim.y) + threadIdx.y;  // for every col
+	if (i < n && j < n_components)
+		atomicAdd(&norm[i], Y[j*n + i] * Y[j*n + i]);
+}
+
+template <int TPB_X = 32, int TPB_Y = 32>
+void get_norm(const float *__restrict__ Y, float *__restrict__ norm,
+			  const int n, const int n_components, cudaStream_t stream) {
+	// Notice Y is F-Contiguous
+	cudaMemset(norm, 0, sizeof(float) * n);
+
+	static const dim3 threadsPerBlock(TPB_X, TPB_Y);
+	const dim3 numBlocks(ceil(n, threadsPerBlock.x), ceil(K, threadsPerBlock.y));
+	__get_norm<<<numBlocks, threadsPerBlock, 0, stream>>>(Y, norm, n, n_components);
+	CUDA_CHECK(cudaPeekAtLastError());
+}
+
+
+
+__global__ void
+__attractive_fast(const float *__restrict__ VAL,
+                    const int *__restrict__ COL,
+                    const int *__restrict__ ROW,
+                    const float *__restrict__ Y,
+                    const float *__restrict__ norm,
+                    float *__restrict__ attract, const int NNZ,
+                    const int n, const int n_components) {
+    // Notice attract, Y and repel are all F-contiguous
+    const int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+    if (index < NNZ) {
+        const int i = ROW[index];
+        const int j = COL[index];
+
+        float d = 0.0f;
+        for (int k = 0; k < n_components; k++)
+            //d += Y[i, k] * Y[j, k]
+            d += (Y[k*n + i] * Y[k*n + j]);
+
+        const float PQ = VAL[index] / (1.0f - 2.0f*d + norm[i] + norm[j]);
+
+        for (int k = 0; k < n_components; k++)
+            atomicAdd(&attract[k*n + i],     PQ * (Y[k*n + i] - Y[k*n + j]));
+            // attract[i*K + j] += PQ * (Y[i, j] - Y[j, j]);
+    }
+}
+void attractive_fast(const float *__restrict__ VAL,
+                    const int *__restrict__ COL,
+                    const int *__restrict__ ROW,
+                    const float *__restrict__ Y,
+                    const float *__restrict__ norm,
+                    float *__restrict__ attract, const int NNZ,
+                    const int n, const int n_components,
+                    cudaStream_t stream) {
+    cudaMemset(attract, 0, sizeof(float) * n * n_components);
+
+    __attractive_fast<<<ceil(NNZ, 1024), 1024, 0, stream>>>(VAL, COL, ROW, Y,
+        norm, attract, NNZ, n, n_components);
+    CUDA_CHECK(cudaPeekAtLastError());
+}
+
+
+
+__global__ void
+__repulsive_fast(const float *__restrict__ Y,
+                float *__restrict__ repel,
+                const float *__restrict__ norm,
+                float *__restrict__ sum_Z,
+                const int n, const int n_components)
+{
+    const int j = (blockIdx.x * blockDim.x) + threadIdx.x;  // for every item in row
+    const int i = (blockIdx.y * blockDim.y) + threadIdx.y;  // for every row
+
+    if (i < n && j < n && j > i) {
+        float d = 0.0f;
+        for (int k = 0; k < n_components; k++)
+            //d += Y[i, k] * Y[j, k]
+            d += (Y[k*n + i] * Y[k*n + j]);
+
+        const float Q = 1.0f  /  (1.0f - 2.0f*d  + norm[i] + norm[j]);
+        atomicAdd(&sum_Z[i], Q); // Z += Q
+        const float Q2 = Q*Q;
+
+        for (int k = 0; k < n_components; k++) {
+            const float force = Q2 * (Y[k*n + i] - Y[k*n + j]);
+            // repel = Q2 * (Y[i, k] - Y[j, k]);
+
+            atomicAdd(&repel[k*n + i],  - force);  // repel[k*n + i] -= force
+            atomicAdd(&repel[k*n + j],  force);  // repel[k*n + j] += force
+        }
+    }
+}
+
+template <int TPB_X = 32, int TPB_Y = 32>
+float repulsive_fast(const float *__restrict__ Y,
+                    float *__restrict__ repel,
+                    const float *__restrict__ norm,
+                    float *__restrict__ sum_Z,
+                    const int n, const int n_components,
+                    cudaStream_t stream)
+{
+    cudaMemset(sum_Z, 0, sizeof(float) * n);
+    cudaMemset(repel, 0, sizeof(float) * n * n_components);
+
+    const dim3 threadsPerBlock(TPB_X, TPB_Y);
+    const dim3 numBlocks(ceil(n, threadsPerBlock.x), ceil(n, threadsPerBlock.y));
+    __repulsive_fast<<<numBlocks, threadsPerBlock, 0, stream>>>(Y, repel,
+                                            norm, sum_Z, n, n_components);
+    CUDA_CHECK(cudaPeekAtLastError());
+
+    double Z = (double) thrust::reduce(__STREAM__, sum_Z, sum_Z + n);
+    return 1.0f / (2.0f * Z);
+}
+
+
+__global__ void 
+__apply_forces(const float *__restrict__ attract,
+				 const float *__restrict__ repel,
+				 float *__restrict__ Y, float *__restrict__ iY,
+				 const float *__restrict__ noise,
+				 float *__restrict__ gains, const int n,
+				 const int K, const float Z, const float min_gain,
+				 const float momentum, const float eta) {
+	// Everything is F-Contiguous
+	// NOTICE noise is a 1D array
+
+	const int j = (blockIdx.x * blockDim.x) + threadIdx.x;  // for every column
+	const int i = (blockIdx.y * blockDim.y) + threadIdx.y;  // for every item in column
+	if (j < K && i < n) {
+		const int index = j*n + i;
+		const float dy = attract[index] + Z * repel[index];
+
+		if (signbit(dy) != signbit(iY[index]))
+			gains[index] += 0.2f;
+		else
+			gains[index] *= 0.8f;
+
+		if (gains[index] < min_gain)
+			gains[index] = min_gain;
+
+		iY[index] = momentum * iY[index] - eta * (gains[index] * dy);
+
+		Y[index] += iY[index];
+	}
+}
+
+template <int TPB_X = 32, int TPB_Y = 32>
+void apply_forces(const float *__restrict__ attract,
+				const float *__restrict__ repel, float *__restrict__ Y,
+				float *__restrict__ iY, const float *__restrict__ noise,
+				float *__restrict__ gains, const int n, const int K,
+				const double Z, const float min_gain, const float momentum,
+				const float eta, cudaStream_t stream) {
+	static const dim3 threadsPerBlock(TPB_X, TPB_Y);
+	const dim3 numBlocks(ceil(K, threadsPerBlock.x), ceil(n, threadsPerBlock.y));
+	__apply_forces<<<numBlocks, threadsPerBlock, 0, stream>>>(
+		attract, repel, Y, iY, noise, gains, n, K, Z, min_gain, momentum, eta);
+	CUDA_CHECK(cudaPeekAtLastError());
+}
+
+
+}
--- cpp/src/tsne/tsne.cu
+++ cpp/src/tsne/tsne.cu
@@ -7,7 +7,8 @@
 
 #include "cublas_v2.h"
 #include "distances.h"
-#include "kernels.h"
+//#include "kernels.h"
+#include "fast_kernels.h"
 #include "utils.h"
 
 #define TEST_NNZ 12021
@@ -17,280 +18,236 @@ namespace ML {
 using namespace MLCommon;
 
 void TSNE(const cumlHandle &handle, const float *X, float *Y, const int n,
-          const int p, const int n_components = 2, int n_neighbors = 90,
-          const float *distances_vector = NULL, const long *indices_vector = NULL,
-          float *VAL_vector = NULL, const int *COL_vector = NULL, const int *ROW_vector = NULL,
-          const float perplexity = 30.0f, const int perplexity_max_iter = 100,
-          const int perplexity_tol = 1e-5,
-          const float early_exaggeration = 12.0f,
-          const int exaggeration_iter = 250, const float min_gain = 0.01f,
-          const float eta = 500.0f, const int max_iter = 1000,
-          const float pre_momentum = 0.8, const float post_momentum = 0.5,
-          const long long seed = -1, const bool initialize_embeddings = false) {
-  auto d_alloc = handle.getDeviceAllocator();
+					const int p, const int n_components = 2, int n_neighbors = 90,
+					const float *distances_vector = NULL, const long *indices_vector = NULL,
+					float *VAL_vector = NULL, const int *COL_vector = NULL, const int *ROW_vector = NULL,
+					const float perplexity = 30.0f, const int perplexity_max_iter = 100,
+					const int perplexity_tol = 1e-5,
+					const float early_exaggeration = 12.0f,
+					const int exaggeration_iter = 250, const float min_gain = 0.01f,
+					const float eta = 500.0f, const int max_iter = 1000,
+					const float pre_momentum = 0.8, const float post_momentum = 0.5,
+					const long long seed = -1, const bool initialize_embeddings = false,
+					const bool verbose = false, const char *method = "Fast") {
 
-  cudaStream_t stream = handle.getStream();
+	auto d_alloc = handle.getDeviceAllocator();
+	cudaStream_t stream = handle.getStream();
 
-  assert(n > 0 && p > 0 && n_components > 0 && n_neighbors > 0);
-  if (n_neighbors > n) n_neighbors = n;
+	assert(n > 0 && p > 0 && n_components > 0 && n_neighbors > 0);
+	if (n_neighbors > n) n_neighbors = n;
 
-  // Some preliminary intializations for cuBLAS and cuML
-  DEBUG("[Info] Create cuBLAS and cuML handles.\n");
-  const int k = n_components;
-  cublasHandle_t BLAS = handle.getImpl().getCublasHandle();
+	// Some preliminary intializations for cuBLAS and cuML
+	DEBUG("[Info] Create cuBLAS and cuML handles.\n");
+	const int k = n_components;
+	cublasHandle_t BLAS = handle.getImpl().getCublasHandle();
 
-  const float neg2 = -2.0f, beta = 0.0f, one = 1.0f;
-
-
-
-  // Get distances
-  DEBUG("[Info] Get distances\n");
-  float *distances =
-      (float *)d_alloc->allocate(n * n_neighbors * sizeof(float), stream);
-  long *indices =
-    (long *)d_alloc->allocate(sizeof(long) * n * n_neighbors, stream);
-
-  if (distances == NULL && indices == NULL) {
-    get_distances(X, n, p, indices, distances, n_neighbors, stream);
-  }
-  else {
-    MLCommon::updateDevice(distances, distances_vector, n * n_neighbors, stream);
-    MLCommon::updateDevice(indices, indices_vector, n * n_neighbors, stream);
-
-    std::cout << MLCommon::arr2Str(distances, 20, "Distances", stream) << std::endl;
-    std::cout << MLCommon::arr2Str(indices, 20, "indices", stream) << std::endl;
-  }
+	const float neg2 = -2.0f, beta = 0.0f, one = 1.0f;
 
 
+	// Get distances
+	DEBUG("[Info] Get distances\n");
+	float *distances =
+			(float *)d_alloc->allocate(n * n_neighbors * sizeof(float), stream);
+	long *indices =
+		(long *)d_alloc->allocate(sizeof(long) * n * n_neighbors, stream);
 
-  normalize_distances(n, distances, n_neighbors, stream);
+	if (distances == NULL && indices == NULL) {
+		get_distances(X, n, p, indices, distances, n_neighbors, stream);
+	}
+	else {
+		MLCommon::updateDevice(distances, distances_vector, n * n_neighbors, stream);
+		MLCommon::updateDevice(indices, indices_vector, n * n_neighbors, stream);
+
+		std::cout << MLCommon::arr2Str(distances, 20, "Distances", stream) << std::endl;
+		std::cout << MLCommon::arr2Str(indices, 20, "indices", stream) << std::endl;
+	}
+
+
+
+	normalize_distances(n, distances, n_neighbors, stream);
 #if IF_DEBUG
-    printf("[Info]  Normalized distances\n\n");
-    std::cout << MLCommon::arr2Str(distances, 20, "Distances", stream) << std::endl;
+		printf("[Info]  Normalized distances\n\n");
+		std::cout << MLCommon::arr2Str(distances, 20, "Distances", stream) << std::endl;
 #endif
 
 
-  // Get perplexity
-  DEBUG("[Info] Get perplexity\n");
-  float *P = (float *)d_alloc->allocate(sizeof(float) * n * n_neighbors, stream);
-  float P_sum = determine_sigmas(distances, P, perplexity, perplexity_max_iter,
-                                 perplexity_tol, n, n_neighbors, stream);
-  d_alloc->deallocate(distances, n * n_neighbors * sizeof(float), stream);
-  DEBUG("[Info] P_sum = %f\n", P_sum);
+	// Get perplexity
+	DEBUG("[Info] Get perplexity\n");
+	float *P = (float *)d_alloc->allocate(sizeof(float) * n * n_neighbors, stream);
+	float P_sum = determine_sigmas(distances, P, perplexity, perplexity_max_iter,
+									perplexity_tol, n, n_neighbors, stream);
+	d_alloc->deallocate(distances, n * n_neighbors * sizeof(float), stream);
+	DEBUG("[Info] P_sum = %f\n", P_sum);
 #if IF_DEBUG
-    printf("[Info]  Perplexity results\n\n");
-    std::cout << MLCommon::arr2Str(P, 20, "Perplexity", stream) << std::endl;
-#endif
-
-
-
-  // Convert data to COO layout
-  float *VAL;
-  int *COL, *ROW;
-  int NNZ;
-
-  if (VAL_vector == NULL) {
-    DEBUG("[Info] Convert to COO layout and symmetrize\n");
-    COO_t<float> P_PT;
-    symmetrize_perplexity(P, indices, &P_PT, n, n_neighbors, P_sum,
-                          early_exaggeration, stream);
-    CUDA_CHECK(cudaPeekAtLastError());
-    NNZ = P_PT.nnz;
-    VAL = P_PT.vals;
-    COL = P_PT.rows;
-    ROW = P_PT.cols;
-  }
-  else {
-    NNZ = TEST_NNZ;
-    VAL = (float *)d_alloc->allocate(sizeof(float) * NNZ, stream);
-    COL = (int *)d_alloc->allocate(sizeof(int) * NNZ, stream);
-    ROW = (int *)d_alloc->allocate(sizeof(int) * NNZ, stream);
-    MLCommon::updateDevice(VAL, VAL_vector, NNZ, stream);
-    MLCommon::updateDevice(COL, COL_vector, NNZ, stream);
-    MLCommon::updateDevice(ROW, ROW_vector, NNZ, stream);
-  }
-
-#if IF_DEBUG
-    printf("[Info]  Symmetrized Perplexity results\n\n");
-    std::cout << MLCommon::arr2Str(VAL, 20, "Perplexity", stream) << std::endl;
-
-    printf("[Info]  COL\n\n");
-    std::cout << MLCommon::arr2Str(COL, 20, "Perplexity", stream) << std::endl;
-
-    printf("[Info]  RWW\n\n");
-    std::cout << MLCommon::arr2Str(ROW, 20, "Perplexity", stream) << std::endl;
+		printf("[Info]  Perplexity results\n\n");
+		std::cout << MLCommon::arr2Str(P, 20, "Perplexity", stream) << std::endl;
 #endif
 
 
 
-  // Allocate data [NOTICE all Fortran Contiguous]
-  DEBUG("[Info] Malloc data and space\n");
-  float *noise = (float *)d_alloc->allocate(sizeof(float) * n, stream);
-  cudaMemset(noise, 0, sizeof(float) * n);
-  //random_vector(noise, -0.003f, 0.003f, n, stream, seed);
-  CUDA_CHECK(cudaPeekAtLastError());
+	// Convert data to COO layout
+	float *VAL;
+	int *COL, *ROW;
+	int NNZ;
 
-  if (initialize_embeddings) {
-    random_vector(Y, -0.1f, 0.1f, n * k, stream, seed);
-    CUDA_CHECK(cudaPeekAtLastError());
-  }
+	if (VAL_vector == NULL) {
+		DEBUG("[Info] Convert to COO layout and symmetrize\n");
+		COO_t<float> P_PT;
+		symmetrize_perplexity(P, indices, &P_PT, n, n_neighbors, P_sum,
+													early_exaggeration, stream);
+		CUDA_CHECK(cudaPeekAtLastError());
+		NNZ = P_PT.nnz;
+		VAL = P_PT.vals;
+		COL = P_PT.rows;
+		ROW = P_PT.cols;
+	}
+	else {
+		NNZ = TEST_NNZ;
+		VAL = (float *)d_alloc->allocate(sizeof(float) * NNZ, stream);
+		COL = (int *)d_alloc->allocate(sizeof(int) * NNZ, stream);
+		ROW = (int *)d_alloc->allocate(sizeof(int) * NNZ, stream);
+		MLCommon::updateDevice(VAL, VAL_vector, NNZ, stream);
+		MLCommon::updateDevice(COL, COL_vector, NNZ, stream);
+		MLCommon::updateDevice(ROW, ROW_vector, NNZ, stream);
+	}
 
-  float *Q = (float *)d_alloc->allocate(sizeof(float) * n * n, stream);
-  float *norm = (float *)d_alloc->allocate(sizeof(float) * n, stream);
-  float *Q_sum = (float *)d_alloc->allocate(sizeof(float) * n, stream);
-  double *sum = (double *)d_alloc->allocate(sizeof(double), stream);
-
-  float *attract = (float *)d_alloc->allocate(sizeof(float) * n * k, stream);
-  float *repel = (float *)d_alloc->allocate(sizeof(float) * n * k, stream);
-
-  float *iY = (float *)d_alloc->allocate(sizeof(float) * n * k, stream);
-  float *gains = (float *)d_alloc->allocate(sizeof(float) * n * k, stream);
-
-  // Do gradient updates
-  float momentum = pre_momentum;
-  double Z;
-  int error;
-
-  DEBUG("[Info] Start iterations\n");
-  for (int iter = 0; iter < max_iter; iter++) {
-
-    if (iter == exaggeration_iter) {
-      momentum = post_momentum;
-      float div = 1.0f / early_exaggeration;
-      thrust::transform(__STREAM__, VAL, VAL + NNZ, VAL, div * _1);
-    }
-
-    // Get norm(Y)
-    get_norm(Y, norm, n, k, stream);
-// #if IF_DEBUG
-//     printf("[Info]  Norm(y)\n\n");
-//     std::cout << MLCommon::arr2Str(norm, 20, "norm", stream) << std::endl;
-//     std::cout << MLCommon::arr2Str(norm + n/2, 20, "norm", stream) << std::endl;
-// #endif
-
-    // Do -2 * (Y @ Y.T)
-    // if (error = cublasSsyrk(BLAS, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N, n, k,
-    //                         &neg2, Y, n, &beta, Q, n)) {
-    //   DEBUG("[ERROR]  Error from BLAS = %d", error);
-    //   break;
-    // }
 #if IF_DEBUG
-    printf("[Info]  -2Y @ Y.T\n\n");
-    // std::cout << MLCommon::arr2Str(Q, 20, "-2YYT", stream) << std::endl;
-    // std::cout << MLCommon::arr2Str(Q + n*n - n, 20, "-2YYT", stream) << std::endl;
-#endif
+		printf("[Info]  Symmetrized Perplexity results\n\n");
+		std::cout << MLCommon::arr2Str(VAL, 20, "Perplexity", stream) << std::endl;
 
-    // Form T = 1 / (1+d)
-    //Z = form_t_distribution(Q, norm, n, Q_sum, sum, stream);
-    CUDA_CHECK(cudaPeekAtLastError());
+		printf("[Info]  COL\n\n");
+		std::cout << MLCommon::arr2Str(COL, 20, "Perplexity", stream) << std::endl;
 
-    DEBUG("[Info] Z =  %lf iter = %d\n", Z, iter);
-// #if IF_DEBUG
-//     printf("[Info]  Q 1/(1+d)\n\n");
-//     std::cout << MLCommon::arr2Str(Q, 20, "QQ", stream);
-//     std::cout << MLCommon::arr2Str(Q + n*n - n, 20, "QQ", stream);
-// #endif
-
-    // Compute attractive forces with COO matrix
-    //attractive_forces(VAL, COL, ROW, Q, Y, attract, NNZ, n, k, stream);
-
-    attractive_fast(VAL, COL, ROW, Y, norm, attract, NNZ, n, n_components, stream);
-
-
-    Z = repulsive_fast(Y, repel, norm, Q_sum, n, n_components, stream);
-
-
-    CUDA_CHECK(cudaPeekAtLastError());
-#if IF_DEBUG
-    printf("[Info]  Attractive forces\n\n");
-    std::cout << MLCommon::arr2Str(attract, 20, "attract", stream);
-    std::cout << MLCommon::arr2Str(attract + n, 20, "attract", stream);
+		printf("[Info]  RWW\n\n");
+		std::cout << MLCommon::arr2Str(ROW, 20, "Perplexity", stream) << std::endl;
 #endif
 
 
-    // Change Q to Q**2 for repulsion
-    //postprocess_Q(Q, Q_sum, n, stream);
-// #if IF_DEBUG
-//     printf("[Info]  Q**2\n\n");
-//     std::cout << MLCommon::arr2Str(Q, 20, "Q**2", stream);
-//     std::cout << MLCommon::arr2Str(Q + n*n - n, 20, "Q**2", stream);
 
-//     printf("[Info]  Q_sum\n\n");
-//     std::cout << MLCommon::arr2Str(Q_sum, 20, "Q_sum", stream);
-//     std::cout << MLCommon::arr2Str(Q_sum + n/2, 20, "Q_sum", stream);
-// #endif
+	// Allocate data [NOTICE all Fortran Contiguous]
+	DEBUG("[Info] Malloc data and space\n");
+	float *noise = (float *)d_alloc->allocate(sizeof(float) * n, stream);
+	cudaMemset(noise, 0, sizeof(float) * n);
+	//random_vector(noise, -0.003f, 0.003f, n, stream, seed);
+	CUDA_CHECK(cudaPeekAtLastError());
 
-
-    // Compute repel_1 = Q @ Y
-    // if (error = cublasSsymm(BLAS, CUBLAS_SIDE_LEFT, CUBLAS_FILL_MODE_LOWER, n,
-    //                         k, &one, Q, n, Y, n, &beta, repel, n)) {
-    //   DEBUG("[ERROR]  Error from BLAS = %d", error);
-    //   break;
-    // }
-    CUDA_CHECK(cudaPeekAtLastError());
-#if IF_DEBUG
-    printf("[Info]  Q @ Y\n\n");
-    std::cout << MLCommon::arr2Str(repel, 20, "Q @ Y", stream);
-    std::cout << MLCommon::arr2Str(repel + n, 20, "Q @ Y", stream);
-#endif
+	if (initialize_embeddings) {
+		random_vector(Y, -0.1f, 0.1f, n * k, stream, seed);
+		CUDA_CHECK(cudaPeekAtLastError());
+	}
 
 
-    // Repel_2 = mean contributions yi - yj
-    // Repel = Repel_1 - Repel_2
-    //repel_minus_QY(repel, Q_sum, Y, n, k, stream);
-    CUDA_CHECK(cudaPeekAtLastError());
-#if IF_DEBUG
-    printf("[Info]  repel - mean @ Y\n\n");
-    std::cout << MLCommon::arr2Str(repel, 20, "repel - mean @ Y", stream);
-    std::cout << MLCommon::arr2Str(repel + n, 20, "repel - mean @ Y", stream);
-#endif
+	float *norm = (float *)d_alloc->allocate(sizeof(float) * n, stream);
+	float *Q_sum = (float *)d_alloc->allocate(sizeof(float) * n, stream);
+	double *sum = (double *)d_alloc->allocate(sizeof(double), stream);
 
+	float *attract = (float *)d_alloc->allocate(sizeof(float) * n * k, stream);
+	float *repel = (float *)d_alloc->allocate(sizeof(float) * n * k, stream);
 
-    // Integrate forces with momentum
-    apply_forces(attract, repel, Y, iY, noise, gains, n, k, Z, min_gain,
-                 momentum, eta, stream);
-    CUDA_CHECK(cudaPeekAtLastError());
+	float *iY = (float *)d_alloc->allocate(sizeof(float) * n * k, stream);
+	float *gains = (float *)d_alloc->allocate(sizeof(float) * n * k, stream);
 
-#if IF_DEBUG
-    printf("@@@[%d]@@@[Info]  Y after integration\n\n", iter);
-    std::cout << MLCommon::arr2Str(Y, 20, "Y", stream);
-    std::cout << MLCommon::arr2Str(Y + n, 20, "Y", stream);
+	// Do gradient updates
+	float momentum = pre_momentum;
+	float Z;
+	int error;
 
-    // printf("[Info]  gains after integration\n\n");
-    // std::cout << MLCommon::arr2Str(gains, 20, "gains", stream);
-    // std::cout << MLCommon::arr2Str(gains + n, 20, "gains", stream);
+	if (method == "Fast") {
 
-    // printf("[Info]  iY after integration\n\n");
-    // std::cout << MLCommon::arr2Str(iY, 20, "iY", stream);
-    // std::cout << MLCommon::arr2Str(iY + n, 20, "iY", stream);
-#endif
+		for (int iter = 0; iter < max_iter; iter++) {
+			if (iter == exaggeration_iter) {
+				momentum = post_momentum;
+				// Divide perplexities
+				float div = 1.0f / early_exaggeration;
+				thrust::transform(__STREAM__, VAL, VAL + NNZ, VAL, div * _1);
+			}
+			// Get norm(Y)
+			get_norm(Y, norm, n, k, stream);
 
+			// Fast compute attractive forces from COO matrix
+			attractive_fast(VAL, COL, ROW, Y, norm, attract, NNZ, n, n_components, stream);
 
-// #if IF_DEBUG
-//     if (iter == 3) break;
-// #endif
-  }
+			// Fast compute repulsive forces
+			Z = repulsive_fast(Y, repel, norm, Q_sum, n, n_components, stream);
+			if (verbose) printf("[INFO]	Z at iter = %d is %lf", iter, Z);
+
+			// Integrate forces with momentum
+			apply_forces(attract, repel, Y, iY, noise, gains, n, k, Z, min_gain,
+						momentum, eta, stream);
+		}
+	}
+
+	else if (method == "Naive") {
+
+		float *Q = (float *)d_alloc->allocate(sizeof(float) * n * n, stream);
+
+		for (int iter = 0; iter < max_iter; iter++) {
+			if (iter == exaggeration_iter) {
+				momentum = post_momentum;
+				// Divide perplexities
+				float div = 1.0f / early_exaggeration;
+				thrust::transform(__STREAM__, VAL, VAL + NNZ, VAL, div * _1);
+			}
+			// Get norm(Y)
+			get_norm(Y, norm, n, k, stream);
+
+			// Find Y @ Y.T
+			if (error = cublasSsyrk(BLAS, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N, n, k,
+									&neg2, Y, n, &beta, Q, n)) {
+				if (verbose) printf("[ERROR]	BLAS failed. Terminating TSNE\n");
+				break;
+			}
+			CUDA_CHECK(cudaPeekAtLastError());
+
+			// Form T = 1 / (1 + d) = 1 / (1 + -2*Y@Y.T )
+			Z = form_t_distribution(Q, norm, n, Q_sum, sum, stream);
+			if (verbose) printf("[INFO]	Z at iter = %d is %lf", iter, Z);
+
+			// Compute attractive forces from COO matrix
+			attractive_forces(VAL, COL, ROW, Q, Y, attract, NNZ, n, k, stream);
+
+			// Change Q to Q**2
+			postprocess_Q(Q, Q_sum, n, stream);
+
+			// Do Q**2 @ Y
+			if (error = cublasSsymm(BLAS, CUBLAS_SIDE_LEFT, CUBLAS_FILL_MODE_LOWER, n,
+			                        k, &one, Q, n, Y, n, &beta, repel, n)) {
+				if (verbose) printf("[ERROR]	BLAS failed. Terminating TSNE\n");
+				break;
+			}
+			CUDA_CHECK(cudaPeekAtLastError());
+
+			// Compute repel - Q**2 @ mean_Y
+			repel_minus_QY(repel, Q_sum, Y, n, k, stream);
+
+			// Integrate forces with momentum
+			apply_forces(attract, repel, Y, iY, noise, gains, n, k, Z, min_gain,
+						momentum, eta, stream);
+		}
+
+		d_alloc->deallocate(Q, sizeof(float) * n * n, stream);
+	}
 
 
 #if not IF_DEBUG
-  P_PT.destroy();
+	P_PT.destroy();
 #else
-  d_alloc->deallocate(VAL, sizeof(float) * NNZ, stream);
-  d_alloc->deallocate(COL, sizeof(int) * NNZ, stream);
-  d_alloc->deallocate(ROW, sizeof(int) * NNZ, stream);
+	d_alloc->deallocate(VAL, sizeof(float) * NNZ, stream);
+	d_alloc->deallocate(COL, sizeof(int) * NNZ, stream);
+	d_alloc->deallocate(ROW, sizeof(int) * NNZ, stream);
 #endif
 
-  d_alloc->deallocate(noise, sizeof(float) * n, stream);
+	d_alloc->deallocate(noise, sizeof(float) * n, stream);
 
-  d_alloc->deallocate(Q, sizeof(float) * n * n, stream);
-  d_alloc->deallocate(norm, sizeof(float) * n, stream);
-  d_alloc->deallocate(Q_sum, sizeof(float) * n, stream);
-  d_alloc->deallocate(sum, sizeof(double), stream);
+	
+	d_alloc->deallocate(norm, sizeof(float) * n, stream);
+	d_alloc->deallocate(Q_sum, sizeof(float) * n, stream);
+	d_alloc->deallocate(sum, sizeof(double), stream);
 
-  d_alloc->deallocate(attract, sizeof(float) * n * k, stream);
-  d_alloc->deallocate(repel, sizeof(float) * n * k, stream);
+	d_alloc->deallocate(attract, sizeof(float) * n * k, stream);
+	d_alloc->deallocate(repel, sizeof(float) * n * k, stream);
 
-  d_alloc->deallocate(iY, sizeof(float) * n * k, stream);
-  d_alloc->deallocate(gains, sizeof(float) * n * k, stream);
+	d_alloc->deallocate(iY, sizeof(float) * n * k, stream);
+	d_alloc->deallocate(gains, sizeof(float) * n * k, stream);
 }
 }  // namespace ML
