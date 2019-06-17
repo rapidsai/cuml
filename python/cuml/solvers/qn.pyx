fb630c282a8a4802de7ce8b09dac448d246ae0a2
Dante Gama Dessavre <danteg@nvidia.com>
2019-06-17 13:04:42 -0500

    FEA Fully working qn cython

--- python/cuml/solvers/qn.pyx
+++ python/cuml/solvers/qn.pyx
@@ -101,20 +101,27 @@ class QN(Base):
     """
 
     def __init__(self, loss='sigmoid', fit_intercept=True,
-                 l1_ratio=0.15, max_iter=1000, tol=1e-3,
-                 linesearch_max_iter=1000, lbfgs_memory=5, verbose=False,
-                 num_classes=1):
+                 l1_ratio=0, l2_ratio=0, max_iter=1000, tol=1e-3,
+                 linesearch_max_iter=50, lbfgs_memory=5, verbose=False,
+                 num_classes=1, handle=None):
 
-        super(CD, self).__init__(handle=handle, verbose=verbose)
+        super(QN, self).__init__(handle=handle, verbose=False)
+
+
 
         self.fit_intercept = fit_intercept
         self.l1_ratio = l1_ratio
+        self.l2_ratio = l2_ratio
         self.max_iter = max_iter
         self.tol = tol
         self.linesearch_max_iter = linesearch_max_iter
         self.lbfgs_memory = lbfgs_memory
         self.alpha = 0.0
         self.num_iter = 0
+
+        if loss not in ['sigmoid', 'softmax', 'normal']:
+            raise ValueError("loss " + str(loss) + " not supported.")
+
         self.loss_type = self._get_loss_int(loss)
 
     def _get_loss_int(self, loss):
@@ -142,25 +149,34 @@ class QN(Base):
 
         cdef uintptr_t X_ptr, y_ptr
         X_m, X_ptr, n_rows, self.n_cols, self.dtype = \
-            input_to_dev_array(X)
+            input_to_dev_array(X, order='F')
 
-        y_m, y_ptr, _, _, _ = \
+        y_m, y_ptr, lab_rows, _, _ = \
             input_to_dev_array(y)
 
-        self.num_classes = len(cupy.unique(y_m))
+        self.num_classes = len(cupy.unique(y_m)) - 1
 
         if self.loss_type != 1 and self.num_classes > 2:
             raise ValueError("Only softmax (multinomial) loss supports more"
                              "than 2 classes.")
 
-        self.coef_ = cuda.to_device(zeros(n_cols, self.num_classes,
-                                          dtype=self.dtype))
+        if n_rows != lab_rows:
+            raise ValueError("Number of rows must match between X and y.")
+
+        if self.fit_intercept:
+            coef_size = (self.n_cols + 1, self.num_classes)
+        else:
+            coef_size = (self.n_cols, self.num_classes)
+
+        self.coef_ = cuda.to_device(zeros(coef_size, dtype=self.dtype))
 
         cdef uintptr_t coef_ptr = get_dev_array_ptr(self.coef_)
 
-        cdef float intercept32, objective32
-        cdef double intercept64, objective64
+        cdef float objective32
+        cdef double objective64
         cdef cumlHandle* handle_ = <cumlHandle*><size_t>self.handle.getHandle()
+
+        cdef int num_iters
 
         if self.dtype == np.float32:
             qnFit(handle_[0],
@@ -171,17 +187,19 @@ class QN(Base):
                   <int> self.num_classes,
                   <bool> self.fit_intercept,
                   <float> self.l1_ratio,
-                  <float> 1.0-self.l2_ratio,
+                  <float> self.l2_ratio,
                   <int> self.max_iter,
                   <float> self.tol,
                   <int> self.linesearch_max_iter,
                   <int> self.lbfgs_memory,
                   <int> 0,
                   <float*> coef_ptr,
-                  <float*> objective32,
-                  <int*> self.num_iters,
+                  <float*> &objective32,
+                  <int*> &num_iters,
                   <bool> True,
                   <int> self.loss_type)
+
+            self.objective = objective32
 
         else:
             qnFit(handle_[0],
@@ -191,18 +209,22 @@ class QN(Base):
                   <int>self.n_cols,
                   <int> self.num_classes,
                   <bool> self.fit_intercept,
-                  <double> self.l1,
-                  <double> self.l2,
+                  <double> self.l1_ratio,
+                  <double> self.l2_ratio,
                   <int> self.max_iter,
                   <double> self.tol,
                   <int> self.linesearch_max_iter,
                   <int> self.lbfgs_memory,
                   <int> 0,
                   <double*> coef_ptr,
-                  <double*> objective64,
-                  <int*> self.num_iters,
+                  <double*> &objective64,
+                  <int*> &num_iters,
                   <bool> True,
                   <int> self.loss_type)
+
+            self.objective = objective64
+
+        self.num_iters = num_iters
 
         self.handle.sync()
 
@@ -231,8 +253,7 @@ class QN(Base):
             input_to_dev_array(X, check_dtype=self.dtype,
                                check_cols=self.n_cols)
 
-        preds = cuda.to_device(zeros(n_rows, self.num_classes,
-                                     dtype=self.dtype))
+        preds = cuda.to_device(zeros(n_rows, dtype=self.dtype))
 
         cdef uintptr_t coef_ptr = get_dev_array_ptr(self.coef_)
         cdef uintptr_t pred_ptr = get_dev_array_ptr(preds)
