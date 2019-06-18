611cb0d12302ec0f620b225d1b33cdbb7252e686
Roy Finkelberg <roy.finkelberg@gmail.com>
2019-06-18 14:26:55 -0500

    Update python/cuml/test/test_train_test_split.py
    Co-Authored-By: Dante Gama Dessavre <dante.gamadessavre@gmail.com>

--- python/cuml/test/test_train_test_split.py
+++ python/cuml/test/test_train_test_split.py
@@ -19,7 +19,7 @@ from cuml.preprocessing.model_selection import train_test_split
 
 
 @pytest.mark.parametrize("n_rows", [100, 100000])
-@pytest.mark.parametrize("train_size", [0.1, 0.5, 0.8])
+@pytest.mark.parametrize("train_size", [0.0, 0.1, 0.5, 0.75, 1.0])
 def test_split(n_rows, train_size):
     X = cudf.DataFrame({"x": range(n_rows)})
     y = cudf.Series(([0] * (n_rows // 2)) + ([1] * (n_rows // 2)))
