516954307a779824b79a2f062e022f3cc002bfb7
Jiri Kraus <jkraus@nvidia.com>
2019-06-18 13:13:31 +0200

    Applied code formatting rules.

--- cpp/src_prims/common/buffer_base.hpp
+++ cpp/src_prims/common/buffer_base.hpp
@@ -16,8 +16,8 @@
 
 #pragma once
 
-#include <memory>
 #include <cuda_runtime.h>
+#include <memory>
 
 #include "../utils.h"
 
@@ -26,16 +26,15 @@ namespace MLCommon {
 /**
  * @todo: Add missing doxygen documentation
  */
-template<typename T, typename Allocator>
-class buffer_base
-{
-public:
-  using size_type         = std::size_t;
-  using value_type        = T;
-  using iterator          = value_type*;
-  using const_iterator    = const value_type*;
-  using reference         = T&;
-  using const_reference   = const T&;
+template <typename T, typename Allocator>
+class buffer_base {
+ public:
+  using size_type = std::size_t;
+  using value_type = T;
+  using iterator = value_type*;
+  using const_iterator = const value_type*;
+  using reference = T&;
+  using const_reference = const T&;
 
   buffer_base() = delete;
 
@@ -43,125 +42,95 @@ public:
 
   buffer_base& operator=(const buffer_base& other) = delete;
 
-  buffer_base(std::shared_ptr<Allocator> allocator, cudaStream_t stream, size_type n=0)
-    :  _size(n), _capacity(n), _data(nullptr), _stream(stream), _allocator(allocator)
-  {
-    if ( _capacity > 0 )
-    {
-      _data = static_cast<value_type*>(_allocator->allocate( _capacity*sizeof(value_type), _stream ));
-      CUDA_CHECK( cudaStreamSynchronize( _stream ) );
+  buffer_base(std::shared_ptr<Allocator> allocator, cudaStream_t stream,
+              size_type n = 0)
+    : _size(n),
+      _capacity(n),
+      _data(nullptr),
+      _stream(stream),
+      _allocator(allocator) {
+    if (_capacity > 0) {
+      _data = static_cast<value_type*>(
+        _allocator->allocate(_capacity * sizeof(value_type), _stream));
+      CUDA_CHECK(cudaStreamSynchronize(_stream));
     }
   }
 
-  ~buffer_base()
-  {
-    if ( nullptr != _data ) 
-    {
-       _allocator->deallocate( _data, _capacity*sizeof(value_type), _stream );
+  ~buffer_base() {
+    if (nullptr != _data) {
+      _allocator->deallocate(_data, _capacity * sizeof(value_type), _stream);
     }
   }
 
-  value_type* data()
-  {
-    return _data;
-  }
+  value_type* data() { return _data; }
 
-  const value_type* data() const
-  {
-    return _data;
-  }
+  const value_type* data() const { return _data; }
 
-  size_type size() const
-  {
-    return _size;
-  }
+  size_type size() const { return _size; }
 
-  void clear()
-  {
-    _size = 0;
-  }
+  void clear() { _size = 0; }
 
-  iterator begin()
-  {
-    return _data;
-  }
+  iterator begin() { return _data; }
 
-  const_iterator begin() const
-  {
-    return _data;
-  }
+  const_iterator begin() const { return _data; }
 
-  iterator end()
-  {
-    return _data+_size;
-  }
+  iterator end() { return _data + _size; }
 
-  const_iterator end() const
-  {
-    return _data+_size;
-  }
+  const_iterator end() const { return _data + _size; }
 
-  void reserve( const size_type new_capacity, cudaStream_t stream )
-  {
-    set_stream( stream );
-    if ( new_capacity > _capacity )
-    {
-      value_type* new_data = static_cast<value_type*>(_allocator->allocate( new_capacity*sizeof(value_type), _stream ));
-      if ( _size > 0 ) {
-        CUDA_CHECK( cudaMemcpyAsync( new_data, _data, _size*sizeof(value_type), cudaMemcpyDefault, _stream ) );
+  void reserve(const size_type new_capacity, cudaStream_t stream) {
+    set_stream(stream);
+    if (new_capacity > _capacity) {
+      value_type* new_data = static_cast<value_type*>(
+        _allocator->allocate(new_capacity * sizeof(value_type), _stream));
+      if (_size > 0) {
+        CUDA_CHECK(cudaMemcpyAsync(new_data, _data, _size * sizeof(value_type),
+                                   cudaMemcpyDefault, _stream));
       }
-      if ( nullptr != _data ) {
-        _allocator->deallocate( _data, _capacity*sizeof(value_type), _stream );
+      if (nullptr != _data) {
+        _allocator->deallocate(_data, _capacity * sizeof(value_type), _stream);
       }
       _data = new_data;
       _capacity = new_capacity;
-      }
+    }
   }
 
-  void resize(const size_type new_size, cudaStream_t stream )
-  {
-    reserve( new_size, stream );
+  void resize(const size_type new_size, cudaStream_t stream) {
+    reserve(new_size, stream);
     _size = new_size;
   }
 
-  void release( cudaStream_t stream )
-  {
-    set_stream( stream );
-    if ( nullptr != _data ) {
-      _allocator->deallocate( _data, _capacity*sizeof(value_type), _stream );
+  void release(cudaStream_t stream) {
+    set_stream(stream);
+    if (nullptr != _data) {
+      _allocator->deallocate(_data, _capacity * sizeof(value_type), _stream);
     }
     _data = nullptr;
     _capacity = 0;
     _size = 0;
   }
 
-  std::shared_ptr<Allocator> getAllocator() const
-  {
-    return _allocator;
-  }
+  std::shared_ptr<Allocator> getAllocator() const { return _allocator; }
 
-protected:
+ protected:
+  value_type* _data;
 
-  value_type*                         _data;
+ private:
+  size_type _size;
+  size_type _capacity;
+  cudaStream_t _stream;
+  std::shared_ptr<Allocator> _allocator;
 
-private:
-  size_type                           _size;
-  size_type                           _capacity;
-  cudaStream_t                        _stream;
-  std::shared_ptr<Allocator>          _allocator;
-
-  void set_stream( cudaStream_t stream )
-  {
-    if ( _stream != stream )
-    {
+  void set_stream(cudaStream_t stream) {
+    if (_stream != stream) {
       cudaEvent_t event;
-      CUDA_CHECK( cudaEventCreateWithFlags( &event, cudaEventDisableTiming ) );
-      CUDA_CHECK( cudaEventRecord( event, _stream ) );
-      CUDA_CHECK( cudaStreamWaitEvent ( stream, event, 0 ) );
+      CUDA_CHECK(cudaEventCreateWithFlags(&event, cudaEventDisableTiming));
+      CUDA_CHECK(cudaEventRecord(event, _stream));
+      CUDA_CHECK(cudaStreamWaitEvent(stream, event, 0));
       _stream = stream;
-      CUDA_CHECK( cudaEventDestroy( event ) );
+      CUDA_CHECK(cudaEventDestroy(event));
     }
   }
 };
 
-} // end namespace ML
+}  // namespace MLCommon
--- cpp/test/prims/host_buffer.cu
+++ cpp/test/prims/host_buffer.cu
@@ -14,10 +14,10 @@
  * limitations under the License.
  */
 
-#include <iterator>
 #include <cstdlib>
-#include <iostream>
 #include <ctime>
+#include <iostream>
+#include <iterator>
 
 #include <gtest/gtest.h>
 #include "common/host_buffer.hpp"
@@ -25,82 +25,83 @@
 namespace MLCommon {
 
 TEST(HostBufferTest, ctor) {
-    std::shared_ptr<hostAllocator> allocator(new defaultHostAllocator);
-    cudaStream_t stream = 0;
+  std::shared_ptr<hostAllocator> allocator(new defaultHostAllocator);
+  cudaStream_t stream = 0;
 
-    const int size = 4;
-    host_buffer<int> buffer(allocator,stream,size);
-    ASSERT_EQ( size, buffer.size() );
+  const int size = 4;
+  host_buffer<int> buffer(allocator, stream, size);
+  ASSERT_EQ(size, buffer.size());
 }
 
 TEST(HostBufferTest, clear) {
-    std::shared_ptr<hostAllocator> allocator(new defaultHostAllocator);
-    cudaStream_t stream = 0;
+  std::shared_ptr<hostAllocator> allocator(new defaultHostAllocator);
+  cudaStream_t stream = 0;
 
-    const int size = 8;
-    host_buffer<int> buffer(allocator,stream,size);
-    ASSERT_EQ( size, buffer.size() );
-    buffer.clear();
-    ASSERT_EQ( 0, buffer.size() );
+  const int size = 8;
+  host_buffer<int> buffer(allocator, stream, size);
+  ASSERT_EQ(size, buffer.size());
+  buffer.clear();
+  ASSERT_EQ(0, buffer.size());
 }
 
 TEST(HostBufferTest, itiface) {
-    std::shared_ptr<hostAllocator> allocator(new defaultHostAllocator);
-    cudaStream_t stream = 0;
+  std::shared_ptr<hostAllocator> allocator(new defaultHostAllocator);
+  cudaStream_t stream = 0;
 
-    const int size = 8;
-    host_buffer<int> buffer(allocator,stream,size);
-    ASSERT_EQ( std::distance(buffer.begin(), buffer.end()), buffer.size() );
+  const int size = 8;
+  host_buffer<int> buffer(allocator, stream, size);
+  ASSERT_EQ(std::distance(buffer.begin(), buffer.end()), buffer.size());
 }
 
 TEST(HostBufferTest, reserve) {
-    std::shared_ptr<hostAllocator> allocator(new defaultHostAllocator);
-    cudaStream_t stream = 0;
+  std::shared_ptr<hostAllocator> allocator(new defaultHostAllocator);
+  cudaStream_t stream = 0;
 
-    constexpr int size = 8;
-    constexpr int capacity = 16;
-    static_assert(capacity > size, "capacity must be larger than size for test to work");
+  constexpr int size = 8;
+  constexpr int capacity = 16;
+  static_assert(capacity > size,
+                "capacity must be larger than size for test to work");
 
-    host_buffer<int> buffer(allocator,stream,0);
-    buffer.reserve(capacity,stream);
-    ASSERT_NE( nullptr, buffer.data() );
+  host_buffer<int> buffer(allocator, stream, 0);
+  buffer.reserve(capacity, stream);
+  ASSERT_NE(nullptr, buffer.data());
 
-    const int* const data_ptr = buffer.data();
-    buffer.resize(size,stream);
+  const int* const data_ptr = buffer.data();
+  buffer.resize(size, stream);
 
-    ASSERT_EQ( data_ptr, buffer.data() );
+  ASSERT_EQ(data_ptr, buffer.data());
 }
 
 TEST(HostBufferTest, resize) {
-    std::shared_ptr<hostAllocator> allocator(new defaultHostAllocator);
-    cudaStream_t stream = 0;
+  std::shared_ptr<hostAllocator> allocator(new defaultHostAllocator);
+  cudaStream_t stream = 0;
 
-    std::srand(std::time(nullptr)); 
-    const int random_variable = std::rand();
+  std::srand(std::time(nullptr));
+  const int random_variable = std::rand();
 
-    const int size = 1;
-    host_buffer<int> buffer(allocator,stream,size);
-    buffer[0] = random_variable;
+  const int size = 1;
+  host_buffer<int> buffer(allocator, stream, size);
+  buffer[0] = random_variable;
 
-    const int* const data_ptr = buffer.data();
-    buffer.resize(4,stream);
-    
-    ASSERT_EQ( random_variable, buffer[0]);
-    ASSERT_NE( data_ptr, buffer.data() );
+  const int* const data_ptr = buffer.data();
+  buffer.resize(4, stream);
+
+  ASSERT_EQ(random_variable, buffer[0]);
+  ASSERT_NE(data_ptr, buffer.data());
 }
 
 TEST(HostBufferTest, release) {
-    std::shared_ptr<hostAllocator> allocator(new defaultHostAllocator);
-    cudaStream_t stream = 0;
+  std::shared_ptr<hostAllocator> allocator(new defaultHostAllocator);
+  cudaStream_t stream = 0;
 
-    const int size = 8;
-    host_buffer<int> buffer(allocator,stream,size);
-    ASSERT_EQ( size, buffer.size() );
-    ASSERT_NE( nullptr, buffer.data() );
+  const int size = 8;
+  host_buffer<int> buffer(allocator, stream, size);
+  ASSERT_EQ(size, buffer.size());
+  ASSERT_NE(nullptr, buffer.data());
 
-    buffer.release(stream);
-    ASSERT_EQ( 0, buffer.size() );
-    ASSERT_EQ( nullptr, buffer.data() );
+  buffer.release(stream);
+  ASSERT_EQ(0, buffer.size());
+  ASSERT_EQ(nullptr, buffer.data());
 }
 
-} // end namespace MLCommon
+}  // end namespace MLCommon
