9c441e0183a8693ddced80ab0c51f9259c95208c
Salonijain27 <sj7856@g.rit.edu>
2019-06-18 17:58:21 -0500

    Update randomforest.pyx

--- python/cuml/ensemble/randomforest.pyx
+++ python/cuml/ensemble/randomforest.pyx
@@ -238,7 +238,7 @@ cdef class RandomForest_impl():
         if n_cols != self.n_cols:
             raise ValueError(" The number of columns/features in the training"
                              " and test data should be the same ")
-        if self.dtype != X.dtype:
+        if X.dtype != self.dtype:
             raise ValueError(" The datatype of the training data is different"
                              " from the datatype of the testing data")
 
@@ -288,7 +288,7 @@ cdef class RandomForest_impl():
         if y.dtype != np.int32:
             raise TypeError(" The labels need to have dtype = np.int32")
 
-        if self.dtype != X.dtype:
+        if X.dtype != self.dtype:
             raise ValueError(" The datatype of the training data is different"
                              " from the datatype of the testing data")
 
