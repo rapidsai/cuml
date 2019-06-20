92cc9998d63671b491eaa07f88af2c5eb6ac1df8
Max Rietmann <mrietmann@nvidia.com>
2019-06-20 12:18:45 +0200

    WIP Commit: Added initial L-BFGS solver which passes a rosenbrock test

---
+++ python/cuml/ts/batched_lbfgs.py
@@ -0,0 +1,97 @@
+import scipy.optimize as optimize
+import numpy as np
+from IPython.core.debugger import set_trace
+
+from collections import deque
+
+def batched_fmin_lbfgs(func, x0, fprime=None, args=(), approx_grad=0, bounds=None, m=10,
+                       factr=10000000.0, pgtol=1e-05, epsilon=1e-08, iprint=-1,
+                       maxfun=15000, maxiter=1000, disp=None, callback=None, maxls=20):
+
+
+    pk = np.zeros(len(x0))
+    sk = np.zeros(len(x0))
+    yk = np.zeros(len(x0))
+
+    N = len(x0)
+    y_k_m_1 = np.ones(N)
+    s_k_m_1 = np.ones(N)
+
+    xkm1 = x0
+    xk = x0
+    yk = deque(maxlen=m)
+    sk = deque(maxlen=m)
+    pk = deque(maxlen=m)
+    rho = deque(maxlen=m)
+    fk = []
+    gk = np.zeros(N)
+
+    Hk_bfgs = np.eye(N)
+
+    for k in range(maxiter):
+
+        ######################
+        # check convergence/stopping condition
+        fk.append(func(xk))
+        gkm1 = gk
+        gk = fprime(xk)
+
+        if iprint > 0 and k % iprint == 0:
+            print("k:{} f={:0.5g}, |\/f|_inf={:0.5g}".format(k, fk[-1], np.linalg.norm(gk, np.inf)))
+
+        if np.linalg.norm(gk, np.inf) < pgtol:
+            print("INFO: |g|_{inf} < PGTOL, STOPPING.")
+            break
+
+        if k > 1 and np.abs(fk[-2] - fk[-1]) < 1e-10:
+            print("INFO: Last two iterations essentially identical, stopping.")
+            break
+
+        ######################
+        # compute pk
+
+        # gradient descent for first step
+        if k == 0:
+            pk = -fprime(x0)
+            
+        # L-BFGS after first step
+        else:
+
+            sk.append(xk-xkm1)
+            yk.append(gk-gkm1)
+            rho.append(1/np.dot(yk[-1], sk[-1]))
+
+            gamma_k = np.dot(sk[-1], yk[-1]) / np.dot(yk[-1], yk[-1])
+
+            #############
+            # Alg. 7.4
+            # first loop recursion
+            alpha_i = np.zeros(min(k, m))
+            q = gk
+            for i in range(min(k, m)):
+                idx = -1-i
+                alpha_i[idx] = rho[idx] * np.dot(sk[idx], q)
+                q = q - alpha_i[idx] * yk[idx]
+
+            
+            H0k = gamma_k * np.eye(N)
+            r = H0k @ q
+            # second loop recursion
+            for i in range(min(k, m)):
+                beta = rho[i] * np.dot(yk[i], r)
+                r = r + sk[i] * (alpha_i[i] - beta)
+
+
+            # note r = Hk \/f
+            pk = -r
+
+        alpha, fc, gc, fkp1, _, _ = optimize.line_search(func, fprime,
+                                                         xk, pk)
+
+        xkp1 = xk + alpha*pk
+
+        xkm1 = xk
+        xk = xkp1
+        
+
+    return xk
