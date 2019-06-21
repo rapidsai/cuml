1c927d7bfd4017dc578eae2a3c36d71af1e44795
Corey J. Nolet <cjnolet@gmail.com>
2019-06-21 14:14:54 -0700

    Stgyle checker ...

--- python/cuml/neighbors/nearest_neighbors.pyx
+++ python/cuml/neighbors/nearest_neighbors.pyx
@@ -217,9 +217,9 @@ class NearestNeighbors(Base):
     def __getstate__(self):
 
         if self.n_indices > 1 or self.sizes is not None or
-            self.inputs is not None:
-            raise pickle.PickleError("Pickling if models using " 
-                  "multiple GPUs is not yet supported")
+        self.inputs is not None:
+            raise pickle.PickleError("Pickling if models using "
+                "multiple GPUs is not yet supported")
 
         state = self.__dict__.copy()
 
