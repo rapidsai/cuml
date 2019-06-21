25ae1a5bb46a0cfd42bbd702a18f0f92a24aa72e
Max Rietmann <mrietmann@nvidia.com>
2019-06-21 10:01:37 +0200

    WIP Commit: Initial working batched L-BFGS implementation.

--- python/cuml/test/test_batched_bfgs.py
+++ python/cuml/test/test_batched_bfgs.py
@@ -105,6 +105,67 @@ def test_batched_bfgs_rosenbrock():
     np.testing.assert_almost_equal(np.max(np.abs(res_xk-res_true)), 0.0)
     
 
+def test_batched_lbfgs_rosenbrock():
+
+    num_batches = 5
+    np.random.seed(42)
+    a = np.random.normal(1, scale=0.1, size=num_batches)
+    b = np.random.normal(100, scale=10, size=num_batches)
+
+    def f(x, n=None):
+        nonlocal a
+        nonlocal b
+        nonlocal num_batches
+
+        if n is not None:
+            return rosenbrock(x, a[n], b[n])
+
+        fb = batched_rosenbrock(x, num_batches, a, b)
+        return fb.sum()/num_batches
+
+    def gf(x, n=None):
+        nonlocal a
+        nonlocal b
+        nonlocal num_batches
+
+        if n is not None:
+            return g_rosenbrock(x, a[n], b[n])
+        
+        g = g_batched_rosenbrock(x, num_batches, a, b)
+        return g
+
+    x0 = np.zeros(2*num_batches)
+    x0[0] = 0.0
+    x0[1] = 0.0
+    
+    # global optimizer
+    options = {"disp": 10}
+    # res_ref = optimize.minimize(f, x0, jac=gf, method="L-BFGS-B", options=options)
+
+    # problem-at-a-time optimizer
+    # for ib in range(num_batches):
+    #     res_ref_batch = optimize.minimize(f, x0[2*ib:2*(ib+1)],
+    #                                       jac=gf,
+    #                                       method="L-BFGS-B",
+    #                                       options=options, args=(ib,))
+
+    # print("res_ref=", res_ref)
+
+    res_true = np.zeros(num_batches*2)
+    for i in range(num_batches):
+        res_true[i*2:(i+1)*2] = np.array([a[i], a[i]**2])
+
+    # print("|res_diff_ref|_max", np.max(res_ref.x-res_true))
+
+    # our new batch-aware l-bfgs optimizer
+    res_xk, niter = batched_fmin_lbfgs(f, x0, num_batches, gf, iprint=-1, factr=100)
+    
+    # print("batched res_xk:", res_xk)
+    print("|res_diff_my_batched|_max", np.max(np.abs(res_xk-res_true)))
+    np.testing.assert_almost_equal(np.max(np.abs(res_xk-res_true)), 0.0)
+    
+
+
 def test_batch_arima_fit():
     num_samples = 100
     xs = np.linspace(0, 1, num_samples)
@@ -154,12 +215,13 @@ def test_single_arima_fit():
     
 
 
-def test_lbfgs():
+def test_batched_lbfgs():
 
     x0 = np.array([0, 0])
 
-    xs = batched_fmin_lbfgs(rosenbrock, x0, g_rosenbrock, iprint=1, maxiter=100, m=100)
+    xs = batched_fmin_lbfgs(rosenbrock, x0, g_rosenbrock, iprint=1, maxiter=100, m=10)
     xs2 = optimize.fmin_l_bfgs_b(rosenbrock, x0, g_rosenbrock, iprint=1)
+
 
 if __name__ == "__main__":
     test_lbfgs()
