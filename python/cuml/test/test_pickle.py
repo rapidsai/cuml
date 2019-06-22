97b62653bb3e265bba0112cb7d73d0e14646ec52
Corey J. Nolet <cjnolet@gmail.com>
2019-06-22 07:23:16 -0700

    Fixing trailing whitespace

--- python/cuml/test/test_pickle.py
+++ python/cuml/test/test_pickle.py
@@ -185,7 +185,7 @@ def test_umap_pickle(tmpdir, datatype, model, nrows, ncols):
 
     # transforming the space requires a new optimization step. Just verify
     # the original embedded space loaded properly (same as fit_transform)
-    cu_trust_after = trustworthiness(X_train, 
+    cu_trust_after = trustworthiness(X_train,
                                      cu_after_pickle_model.arr_embed, 10)
 
     assert cu_trust_before == cu_trust_after
