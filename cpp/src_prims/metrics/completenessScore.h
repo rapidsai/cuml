5f3748c6b3a52a4374846b1231e7e17baf3204bc
Ganesh Venkataramana <gvenkatarama@dbcluster.nvidia.com>
2019-06-25 02:30:09 -0700

    fixed bugs, added wrapper exposing to API

--- cpp/src_prims/metrics/completenessScore.h
+++ cpp/src_prims/metrics/completenessScore.h
@@ -31,11 +31,10 @@ namespace Metrics {
 
 /**
 * @brief Function to calculate the completeness score between two clusters
-* <a href="https://en.wikipedia.org/wiki/Completeness_(statistics)">more info on mutual information</a> 
+*
 * @param truthClusterArray: the array of truth classes of type T
 * @param predClusterArray: the array of predicted classes of type T
 * @param size: the size of the data points of type int
-* @param numUniqueClasses: number of Unique classes used for clustering
 * @param lowerLabelRange: the lower bound of the range of labels
 * @param upperLabelRange: the upper bound of the range of labels
 * @param allocator: object that takes care of temporary device memory allocation of type std::shared_ptr<MLCommon::deviceAllocator>
@@ -46,7 +45,6 @@ double completenessScore(const T *truthClusterArray, const T *predClusterArray,
                          int size, T lowerLabelRange, T upperLabelRange,
                          std::shared_ptr<MLCommon::deviceAllocator> allocator,
                          cudaStream_t stream) {
-  //calculating the golden output
   double computedMI, computedEntropy;
 
   computedMI = MLCommon::Metrics::mutualInfoScore(
