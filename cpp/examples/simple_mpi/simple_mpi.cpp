4bec241eede7ec6c453069f60bd49be414ebd2c5
Corey J. Nolet <cjnolet@gmail.com>
2019-06-26 14:44:47 -0400

    Changing initialize_comms to inject_comms

--- cpp/examples/simple_mpi/simple_mpi.cpp
+++ cpp/examples/simple_mpi/simple_mpi.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 #include <mpi.h>
 
 #include <cuML.hpp>
@@ -21,67 +21,70 @@
 #include <sandbox/test.hpp>
 
 #ifndef CUDA_RT_CALL
-#define CUDA_RT_CALL( call )                                                                       \
-{                                                                                                  \
-    cudaError_t cudaStatus = call;                                                                 \
-    if ( cudaSuccess != cudaStatus )                                                               \
-        fprintf(stderr, "ERROR: CUDA RT call \"%s\" in line %d of file %s failed with %s (%d).\n", \
-                        #call, __LINE__, __FILE__, cudaGetErrorString(cudaStatus), cudaStatus);    \
-}
-#endif //CUDA_RT_CALL
+#define CUDA_RT_CALL(call)                                                    \
+  {                                                                           \
+    cudaError_t cudaStatus = call;                                            \
+    if (cudaSuccess != cudaStatus)                                            \
+      fprintf(stderr,                                                         \
+              "ERROR: CUDA RT call \"%s\" in line %d of file %s failed with " \
+              "%s (%d).\n",                                                   \
+              #call, __LINE__, __FILE__, cudaGetErrorString(cudaStatus),      \
+              cudaStatus);                                                    \
+  }
+#endif  //CUDA_RT_CALL
 
-#define MPI_CALL(call)                                                                \
-    {                                                                                 \
-        int mpi_status = call;                                                        \
-        if (0 != mpi_status) {                                                        \
-            char mpi_error_string[MPI_MAX_ERROR_STRING];                              \
-            int mpi_error_string_length = 0;                                          \
-            MPI_Error_string(mpi_status, mpi_error_string, &mpi_error_string_length); \
-            if (NULL != mpi_error_string)                                             \
-                fprintf(stderr,                                                       \
-                        "ERROR: MPI call \"%s\" in line %d of file %s failed "        \
-                        "with %s "                                                    \
-                        "(%d).\n",                                                    \
-                        #call, __LINE__, __FILE__, mpi_error_string, mpi_status);     \
-            else                                                                      \
-                fprintf(stderr,                                                       \
-                        "ERROR: MPI call \"%s\" in line %d of file %s failed "        \
-                        "with %d.\n",                                                 \
-                        #call, __LINE__, __FILE__, mpi_status);                       \
-        }                                                                             \
-    }
+#define MPI_CALL(call)                                                    \
+  {                                                                       \
+    int mpi_status = call;                                                \
+    if (0 != mpi_status) {                                                \
+      char mpi_error_string[MPI_MAX_ERROR_STRING];                        \
+      int mpi_error_string_length = 0;                                    \
+      MPI_Error_string(mpi_status, mpi_error_string,                      \
+                       &mpi_error_string_length);                         \
+      if (NULL != mpi_error_string)                                       \
+        fprintf(stderr,                                                   \
+                "ERROR: MPI call \"%s\" in line %d of file %s failed "    \
+                "with %s "                                                \
+                "(%d).\n",                                                \
+                #call, __LINE__, __FILE__, mpi_error_string, mpi_status); \
+      else                                                                \
+        fprintf(stderr,                                                   \
+                "ERROR: MPI call \"%s\" in line %d of file %s failed "    \
+                "with %d.\n",                                             \
+                #call, __LINE__, __FILE__, mpi_status);                   \
+    }                                                                     \
+  }
 
-int main(int argc, char * argv[])
-{
-    MPI_CALL(MPI_Init(&argc, &argv));
-    int rank = -1;
-    MPI_CALL(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
-    
-    int local_rank = -1;
-    {
-        MPI_Comm local_comm;
-        MPI_CALL(MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, rank, MPI_INFO_NULL,
-                                     &local_comm));
+int main(int argc, char* argv[]) {
+  MPI_CALL(MPI_Init(&argc, &argv));
+  int rank = -1;
+  MPI_CALL(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
 
-        MPI_CALL(MPI_Comm_rank(local_comm, &local_rank));
+  int local_rank = -1;
+  {
+    MPI_Comm local_comm;
+    MPI_CALL(MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, rank,
+                                 MPI_INFO_NULL, &local_comm));
 
-        MPI_CALL(MPI_Comm_free(&local_comm));
-    }
+    MPI_CALL(MPI_Comm_rank(local_comm, &local_rank));
 
-    CUDA_RT_CALL(cudaSetDevice(local_rank));
-    CUDA_RT_CALL(cudaFree(0));
+    MPI_CALL(MPI_Comm_free(&local_comm));
+  }
 
-    MPI_Comm cuml_mpi_comm;
-    MPI_CALL(MPI_Comm_dup(MPI_COMM_WORLD, &cuml_mpi_comm));
+  CUDA_RT_CALL(cudaSetDevice(local_rank));
+  CUDA_RT_CALL(cudaFree(0));
 
-    {
-        ML::cumlHandle cumlHandle;
-        initialize_comms(cumlHandle, cuml_mpi_comm);
-        ML::sandbox::mpi_test(cumlHandle);
-    }
+  MPI_Comm cuml_mpi_comm;
+  MPI_CALL(MPI_Comm_dup(MPI_COMM_WORLD, &cuml_mpi_comm));
 
-    MPI_CALL(MPI_Comm_free(&cuml_mpi_comm));
+  {
+    ML::cumlHandle cumlHandle;
+    inject_comms(cumlHandle, cuml_mpi_comm);
+    ML::sandbox::mpi_test(cumlHandle);
+  }
 
-    MPI_CALL(MPI_Finalize());
-    return 0;
+  MPI_CALL(MPI_Comm_free(&cuml_mpi_comm));
+
+  MPI_CALL(MPI_Finalize());
+  return 0;
 }
