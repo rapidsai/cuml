c7bf7bcd149d8e4aec74253e700b97a5f63fb903
Corey J. Nolet <cjnolet@gmail.com>
2019-06-27 12:17:45 -0400

    Adding pytests for allreduce and send_recv

--- python/cuml/dask/common/comms_utils.pyx
+++ python/cuml/dask/common/comms_utils.pyx
@@ -21,6 +21,9 @@
 from libc.stdlib cimport malloc, free
 from cython.operator cimport dereference as deref
 
+from libcpp cimport bool
+
+
 from libc.stdint cimport uintptr_t
 
 import cudf
@@ -42,6 +45,32 @@ cdef extern from "cuML.hpp" namespace "ML" nogil:
 
 cdef extern from "cuML_comms.hpp" namespace "ML":
     void inject_comms_py(cumlHandle *handle, ncclComm_t comm, void *ucp_worker, void *eps, int size, int rank)
+
+cdef extern from "comms/cuML_comms_test.hpp" namespace "ML::sandbox" nogil:
+    bool test_collective_allreduce(const cumlHandle &h)
+    bool test_pointToPoint_simple_send_recv(const cumlHandle &h)
+
+
+def test_allreduce(handle):
+    """
+    Performs a simple allreduce on a rank and returns whether
+    or not it received values from the whole clique
+    :param handle: 
+    :return: 
+    """"
+    cdef const cumlHandle* h = <cumlHandle*><size_t>handle.getHandle()
+    return test_collective_allreduce(deref(h))
+
+
+def test_send_recv(handle):
+    """
+    Performs a simple p2p send/recv and returns whether or
+    not it recieved values from the whole clique
+    :param handle:
+    :return:
+    """
+    cdef const cumlHandle *h = <cumlHandle*><size_t>handle.getHandle()
+    return test_pointToPoint_simple_send_recv(deref(h))
 
 
 def inject_comms_on_handle(handle, nccl_inst, ucp_worker, eps, size, rank):
