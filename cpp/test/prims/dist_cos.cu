682b9a04598759649558f09cba77085c88f600c4
Thejaswi Rao <rao.thejaswi@gmail.com>
2019-06-27 06:59:01 -0700

    added col-major tests for cosine distance

--- cpp/test/prims/dist_cos.cu
+++ cpp/test/prims/dist_cos.cu
@@ -27,11 +27,17 @@ const std::vector<DistanceInputs<float>> inputsf = {
   {0.001f, 1024, 32, 1024, true, 1234ULL},
   {0.001f, 32, 1024, 1024, true, 1234ULL},
   {0.003f, 1024, 1024, 1024, true, 1234ULL},
+  {0.001f, 1024, 1024, 32, false, 1234ULL},
+  {0.001f, 1024, 32, 1024, false, 1234ULL},
+  {0.001f, 32, 1024, 1024, false, 1234ULL},
+  {0.003f, 1024, 1024, 1024, false, 1234ULL},
 };
 typedef DistanceExpCos<float> DistanceExpCosF;
 TEST_P(DistanceExpCosF, Result) {
-  ASSERT_TRUE(devArrMatch(dist_ref, dist, params.m, params.n,
-                          CompareApprox<float>(params.tolerance)));
+  int m = params.isRowMajor ? params.m : params.n;
+  int n = params.isRowMajor ? params.n : params.m;
+  ASSERT_TRUE(
+    devArrMatch(dist_ref, dist, m, n, CompareApprox<float>(params.tolerance)));
 }
 INSTANTIATE_TEST_CASE_P(DistanceTests, DistanceExpCosF,
                         ::testing::ValuesIn(inputsf));
@@ -41,11 +47,17 @@ const std::vector<DistanceInputs<double>> inputsd = {
   {0.001, 1024, 32, 1024, true, 1234ULL},
   {0.001, 32, 1024, 1024, true, 1234ULL},
   {0.003, 1024, 1024, 1024, true, 1234ULL},
+  // {0.001f, 1024, 1024, 32, false, 1234ULL},
+  // {0.001f, 1024, 32, 1024, false, 1234ULL},
+  // {0.001f, 32, 1024, 1024, false, 1234ULL},
+  // {0.003f, 1024, 1024, 1024, false, 1234ULL},
 };
 typedef DistanceExpCos<double> DistanceExpCosD;
 TEST_P(DistanceExpCosD, Result) {
-  ASSERT_TRUE(devArrMatch(dist_ref, dist, params.m, params.n,
-                          CompareApprox<double>(params.tolerance)));
+  int m = params.isRowMajor ? params.m : params.n;
+  int n = params.isRowMajor ? params.n : params.m;
+  ASSERT_TRUE(
+    devArrMatch(dist_ref, dist, m, n, CompareApprox<double>(params.tolerance)));
 }
 INSTANTIATE_TEST_CASE_P(DistanceTests, DistanceExpCosD,
                         ::testing::ValuesIn(inputsd));
