edbaceea9c6f23b4a242f417fdf81ac90c4e39e3
jianlind <jianlind@nvidia.com>
2019-07-03 14:49:32 -0500

    .

--- python/cuml/preprocessing/data.py
+++ python/cuml/preprocessing/data.py
@@ -82,7 +82,35 @@ def div(df, ser):
         df[col] = df[col] / ser[i]
 
 
-class MinMaxScaler():
+class TransformerMixin:
+    """Mixin class for all transformers in scikit-learn."""
+
+    def fit_transform(self, X, y=None, **fit_params):
+        """Fit to data, then transform it.
+        Fits transformer to X and y with optional parameters fit_params
+        and returns a transformed version of X.
+        Parameters
+        ----------
+        X : numpy array of shape [n_samples, n_features]
+            Training set.
+        y : numpy array of shape [n_samples]
+            Target values.
+        Returns
+        -------
+        X_new : numpy array of shape [n_samples, n_features_new]
+            Transformed array.
+        """
+        # non-optimized default implementation; override when a better
+        # method is possible for a given clustering algorithm
+        if y is None:
+            # fit method of arity 1 (unsupervised transformation)
+            return self.fit(X, **fit_params).transform(X)
+        else:
+            # fit method of arity 2 (supervised transformation)
+            return self.fit(X, y, **fit_params).transform(X)
+
+
+class MinMaxScaler(TransformerMixin):
     def __init__(self, feature_range=(0, 1), copy=True):
         self.feature_range = feature_range
         self.copy = copy
@@ -189,3 +217,64 @@ class MinMaxScaler():
         sub(X, self.min_)
         div(X, self.scale_)
         return X
+
+
+def minmax_scale(X, feature_range=(0, 1), axis=0, copy=True):
+    """Transforms features by scaling each feature to a given range.
+    This estimator scales and translates each feature individually such
+    that it is in the given range on the training set, i.e. between
+    zero and one.
+    The transformation is given by (when ``axis=0``)::
+        X_std = (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0))
+        X_scaled = X_std * (max - min) + min
+    where min, max = feature_range.
+    The transformation is calculated as (when ``axis=0``)::
+       X_scaled = scale * X + min - X.min(axis=0) * scale
+       where scale = (max - min) / (X.max(axis=0) - X.min(axis=0))
+    This transformation is often used as an alternative to zero mean,
+    unit variance scaling.
+    Read more in the :ref:`User Guide <preprocessing_scaler>`.
+    .. versionadded:: 0.17
+       *minmax_scale* function interface
+       to :class:`sklearn.preprocessing.MinMaxScaler`.
+    Parameters
+    ----------
+    X : array-like, shape (n_samples, n_features)
+        The data.
+    feature_range : tuple (min, max), default=(0, 1)
+        Desired range of transformed data.
+    axis : int (0 by default)
+        axis used to scale along. If 0, independently scale each feature,
+        otherwise (if 1) scale each sample.
+    copy : boolean, optional, default is True
+        Set to False to perform inplace scaling and avoid a copy (if the input
+        is already a numpy array).
+    See also
+    --------
+    MinMaxScaler: Performs scaling to a given range using the``Transformer`` API
+        (e.g. as part of a preprocessing :class:`sklearn.pipeline.Pipeline`).
+    Notes
+    -----
+    For a comparison of the different scalers, transformers, and normalizers,
+    see :ref:`examples/preprocessing/plot_all_scaling.py
+    <sphx_glr_auto_examples_preprocessing_plot_all_scaling.py>`.
+    """  # noqa
+    # Unlike the scaler object, this function allows 1d input.
+    # If copy is required, it will be done inside the scaler object.
+
+    # X = check_array(X, copy=False, ensure_2d=False,
+    #                 dtype=FLOAT_DTYPES, force_all_finite='allow-nan')
+
+    if isinstance(X, cudf.Series):
+        X = X.reshape(X.shape[0], 1)
+
+    s = MinMaxScaler(feature_range=feature_range, copy=copy)
+    if axis == 0:
+        X = s.fit_transform(X)
+    else:
+        X = s.fit_transform(X.T).T
+
+    if original_ndim == 1:
+        X = X.ravel()
+
+    return X
