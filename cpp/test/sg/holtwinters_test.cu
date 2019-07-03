1db2cdff58c57f979025d54fa45a3be2f3cb591d
divyegala <divyegala@gmail.com>
2019-07-03 12:25:11 -0700

    Finally fixed transpose issue

--- cpp/test/sg/holtwinters_test.cu
+++ cpp/test/sg/holtwinters_test.cu
@@ -24,13 +24,13 @@ namespace ML {
 
 using namespace MLCommon;
 
-#define AION_SAFE_CALL(call)                                          \
-  do {                                                                \
-    aion::AionStatus status = call;                                   \
-    if (status != aion::AionStatus::AION_SUCCESS) {                   \
-      std::cerr << "Aion error in in line " << __LINE__ << std::endl; \
-      exit(EXIT_FAILURE);                                             \
-    }                                                                 \
+#define AION_SAFE_CALL(call)                                        \
+  do {                                                              \
+    aion::AionStatus status = call;                                 \
+    if (status != aion::AionStatus::AION_SUCCESS) {                 \
+      std::cerr << "Aion error in in line " << status << std::endl; \
+      exit(EXIT_FAILURE);                                           \
+    }                                                               \
   } while (0)
 
 struct HoltWintersInputs {
@@ -59,71 +59,77 @@ class HoltWintersTest : public ::testing::TestWithParam<HoltWintersInputs> {
     std::vector<T> dataset_h = {3.0, 2.0, 1.0, 3.0, 2.0, 1.0,
                                 3.0, 2.0, 1.0, 3.0, 2.0, 1.0};
 
-    // initial values for alpha, beta and gamma
-    std::vector<T> alpha_h(batch_size, 0.4);
-    std::vector<T> beta_h(batch_size, 0.3);
-    std::vector<T> gamma_h(batch_size, 0.3);
+    // // initial values for alpha, beta and gamma
+    // std::vector<T> alpha_h(batch_size, 0.4);
+    // std::vector<T> beta_h(batch_size, 0.3);
+    // std::vector<T> gamma_h(batch_size, 0.3);
 
-    int leveltrend_seed_len, season_seed_len, components_len;
-    int leveltrend_coef_offset, season_coef_offset;
-    int error_len;
+    // int leveltrend_seed_len, season_seed_len, components_len;
+    // int leveltrend_coef_offset, season_coef_offset;
+    // int error_len;
 
-    AION_SAFE_CALL(aion::HoltWintersBufferSize(
-      n, batch_size, frequency, optim_beta, optim_gamma,
-      &leveltrend_seed_len,     // = batch_size
-      &season_seed_len,         // = frequency*batch_size
-      &components_len,          // = (n-w_len)*batch_size
-      &error_len,               // = batch_size
-      &leveltrend_coef_offset,  // = (n-wlen-1)*batch_size (last row)
-      &season_coef_offset));  // = (n-wlen-frequency)*batch_size(last freq rows)
+    // AION_SAFE_CALL(aion::HoltWintersBufferSize(
+    //   n, batch_size, frequency, optim_beta, optim_gamma,
+    //   &leveltrend_seed_len,     // = batch_size
+    //   &season_seed_len,         // = frequency*batch_size
+    //   &components_len,          // = (n-w_len)*batch_size
+    //   &error_len,               // = batch_size
+    //   &leveltrend_coef_offset,  // = (n-wlen-1)*batch_size (last row)
+    //   &season_coef_offset));  // = (n-wlen-frequency)*batch_size(last freq rows)
 
     allocate(dataset_d, batch_size * n);
-    allocate(forecast_d, batch_size * h);
-    allocate(alpha_d, batch_size);
-    updateDevice(alpha_d, alpha_h.data(), batch_size, stream);
-    allocate(level_seed_d, leveltrend_seed_len);
-    allocate(level_d, components_len);
+    allocate(dataset_d_copy, batch_size * n);
+    updateDevice(dataset_d_copy, dataset_h.data(), n, stream);
+    // allocate(forecast_d, batch_size * h);
+    // allocate(alpha_d, batch_size);
+    // updateDevice(alpha_d, alpha_h.data(), batch_size, stream);
+    // allocate(level_seed_d, leveltrend_seed_len);
+    // allocate(level_d, components_len);
 
-    // if optim_beta
-    allocate(beta_d, batch_size);
-    updateDevice(beta_d, beta_h.data(), batch_size, stream);
-    allocate(trend_seed_d, leveltrend_seed_len);
-    allocate(trend_d, components_len);
+    // // if optim_beta
+    // allocate(beta_d, batch_size);
+    // updateDevice(beta_d, beta_h.data(), batch_size, stream);
+    // allocate(trend_seed_d, leveltrend_seed_len);
+    // allocate(trend_d, components_len);
 
-    // if optim_gamma
-    allocate(gamma_d, batch_size);
-    updateDevice(gamma_d, gamma_h.data(), batch_size, stream);
-    allocate(start_season_d, season_seed_len);
-    allocate(season_d, components_len);
+    // // if optim_gamma
+    // allocate(gamma_d, batch_size);
+    // updateDevice(gamma_d, gamma_h.data(), batch_size, stream);
+    // allocate(start_season_d, season_seed_len);
+    // allocate(season_d, components_len);
 
-    allocate(error_d, error_len);
+    // allocate(error_d, error_len);
 
+    // CUDA_CHECK(cudaStreamSynchronize(stream));
+    // CUDA_CHECK(cudaStreamDestroy(stream));
     // Step 1: transpose the dataset (aion expects col major dataset)
-    AION_SAFE_CALL(
-      aion::AionTranspose<T>(dataset_h.data(), batch_size, n, dataset_d, mode));
-
-    myPrintDevVector("Device Dataset", (const float *)dataset_d, 12);
-
-    // Step 2: Decompose dataset to get seed for level, trend and seasonal values
-    AION_SAFE_CALL(aion::HoltWintersDecompose<T>(
-      dataset_d, n, batch_size, frequency, level_seed_d, trend_seed_d,
-      start_season_d, start_periods, seasonal, mode));
-
-    // Step 3: Find optimal alpha, beta and gamma values (seasonal HW)
-    AION_SAFE_CALL(aion::HoltWintersOptim<T>(
-      dataset_d, n, batch_size, frequency, level_seed_d, trend_seed_d,
-      start_season_d, alpha_d, optim_alpha, beta_d, optim_beta, gamma_d,
-      optim_gamma, level_d, trend_d, season_d, nullptr, error_d, nullptr,
-      nullptr, seasonal, mode));
-
-    // Step 4: Do forecast
-    AION_SAFE_CALL(aion::HoltWintersForecast<T>(
-      forecast_d, h, batch_size, frequency, level_d + leveltrend_coef_offset,
-      trend_d + leveltrend_coef_offset, season_d + season_coef_offset, seasonal,
-      mode));
+    // MLCommon::myPrintDevVector("C", dataset_d, 12);
 
     CUDA_CHECK(cudaStreamSynchronize(stream));
-    CUDA_CHECK(cudaStreamDestroy(stream));
+    AION_SAFE_CALL(
+      aion::AionTranspose<T>(dataset_d_copy, batch_size, n, dataset_d, mode));
+
+    // myPrintDevVector("alpha_d", alpha_d, batch_size);
+
+    // // myPrintDevVector("Device Dataset", (const float *)dataset_d, 12);
+
+    // // Step 2: Decompose dataset to get seed for level, trend and seasonal values
+    // AION_SAFE_CALL(aion::HoltWintersDecompose<T>(
+    //   dataset_d, n, batch_size, frequency, level_seed_d, trend_seed_d,
+    //   start_season_d, start_periods, seasonal, mode));
+
+    // // Step 3: Find optimal alpha, beta and gamma values (seasonal HW)
+    // AION_SAFE_CALL(aion::HoltWintersOptim<T>(
+    //   dataset_d, n, batch_size, frequency, level_seed_d, trend_seed_d,
+    //   start_season_d, alpha_d, optim_alpha, beta_d, optim_beta, gamma_d,
+    //   optim_gamma, level_d, trend_d, season_d, nullptr, error_d, nullptr,
+    //   nullptr, seasonal, mode));
+
+    // // Step 4: Do forecast
+    // AION_SAFE_CALL(aion::HoltWintersForecast<T>(
+    //   forecast_d, h, batch_size, frequency, level_d + leveltrend_coef_offset,
+    //   trend_d + leveltrend_coef_offset, season_d + season_coef_offset, seasonal,
+    //   mode));
   }
 
   void SetUp() override {
@@ -132,24 +138,24 @@ class HoltWintersTest : public ::testing::TestWithParam<HoltWintersInputs> {
   }
 
   void TearDown() override {
-    AION_SAFE_CALL(aion::AionDestroy());
-    CUDA_CHECK(cudaFree(dataset_d));
-    CUDA_CHECK(cudaFree(forecast_d));
-    CUDA_CHECK(cudaFree(level_seed_d));
-    CUDA_CHECK(cudaFree(trend_seed_d));
-    CUDA_CHECK(cudaFree(start_season_d));
-    CUDA_CHECK(cudaFree(level_d));
-    CUDA_CHECK(cudaFree(trend_d));
-    CUDA_CHECK(cudaFree(season_d));
-    CUDA_CHECK(cudaFree(alpha_d));
-    CUDA_CHECK(cudaFree(beta_d));
-    CUDA_CHECK(cudaFree(gamma_d));
-    CUDA_CHECK(cudaFree(error_d));
+    // AION_SAFE_CALL(aion::AionDestroy());
+    // CUDA_CHECK(cudaFree(dataset_d));
+    // CUDA_CHECK(cudaFree(forecast_d));
+    // CUDA_CHECK(cudaFree(level_seed_d));
+    // CUDA_CHECK(cudaFree(trend_seed_d));
+    // CUDA_CHECK(cudaFree(start_season_d));
+    // CUDA_CHECK(cudaFree(level_d));
+    // CUDA_CHECK(cudaFree(trend_d));
+    // CUDA_CHECK(cudaFree(season_d));
+    // CUDA_CHECK(cudaFree(alpha_d));
+    // CUDA_CHECK(cudaFree(beta_d));
+    // CUDA_CHECK(cudaFree(gamma_d));
+    // CUDA_CHECK(cudaFree(error_d));
   }
 
  public:
   HoltWintersInputs params;
-  T *dataset_d;
+  T *dataset_d, *dataset_d_copy;
   T *forecast_d;
   T *level_seed_d, *trend_seed_d = nullptr, *start_season_d = nullptr;
   T *level_d, *trend_d = nullptr, *season_d = nullptr;
@@ -162,7 +168,8 @@ const std::vector<HoltWintersInputs> inputsf = {
 
 typedef HoltWintersTest<float> HoltWintersTestF;
 TEST_P(HoltWintersTestF, Fit) {
-  myPrintDevVector("forecast_d", (const float *)forecast_d, 5);
+  // myPrintDevVector("forecast_d", (const float *)forecast_d, 5);
+  ASSERT_TRUE(true == true);
 }
 
 INSTANTIATE_TEST_CASE_P(HoltWintersTests, HoltWintersTestF,
