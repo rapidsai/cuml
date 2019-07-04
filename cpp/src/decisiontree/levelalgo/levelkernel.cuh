eb5ce1bec5dd5a75caaf530dc234d41c1567a405
Vishal Mehta <vishalm@nvidia.com>
2019-07-04 12:28:00 +0200

    fixed threads blocks bug. and benchmarked the code

--- cpp/src/decisiontree/levelalgo/levelkernel.cuh
+++ cpp/src/decisiontree/levelalgo/levelkernel.cuh
@@ -9,12 +9,11 @@ __global__ void get_me_hist_kernel(
   const unsigned int* __restrict__ flags, const int nrows, const int ncols,
   const int n_unique_labels, const int nbins, const int n_nodes,
   const T* __restrict__ quantile, unsigned int* histout) {
-  extern __shared__ char shmem[];
-  unsigned int* shmemhist = (unsigned int*)(shmem);
+  extern __shared__ unsigned int shmemhist[];
   unsigned int local_flag;
   int local_label;
-  unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x;
-
+  int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    
   if (tid < nrows) {
     local_flag = flags[tid];
     local_label = labels[tid];
@@ -22,9 +21,9 @@ __global__ void get_me_hist_kernel(
     local_flag = LEAF;
     local_label = -1;
   }
-
-  for (int colid = 0; colid < ncols; colid++) {
-    for (int i = threadIdx.x; i < nbins * n_nodes * n_unique_labels;
+  
+  for (unsigned int colid = 0; colid < ncols; colid++) {
+    for (unsigned int i = threadIdx.x; i < nbins * n_nodes * n_unique_labels;
          i += blockDim.x) {
       shmemhist[i] = 0;
     }
@@ -35,11 +34,11 @@ __global__ void get_me_hist_kernel(
       T local_data = data[tid + colid * nrows];
       //Loop over nbins
 
-#pragma unroll(8)
-      for (int binid = 0; binid < nbins; binid++) {
+      //#pragma unroll(8)
+      for (unsigned int binid = 0; binid < nbins; binid++) {
         T quesval = quantile[colid * nbins + binid];
         if (local_data <= quesval) {
-          int nodeoff = local_flag * nbins * n_unique_labels;
+          unsigned int nodeoff = local_flag * nbins * n_unique_labels;
           atomicAdd(&shmemhist[nodeoff + binid * n_unique_labels + local_label],
                     1);
         }
@@ -47,24 +46,24 @@ __global__ void get_me_hist_kernel(
     }
 
     __syncthreads();
-    for (int i = threadIdx.x; i < nbins * n_nodes * n_unique_labels;
+    for (unsigned int i = threadIdx.x; i < nbins * n_nodes * n_unique_labels;
          i += blockDim.x) {
-      int offset = colid * nbins * n_nodes * n_unique_labels;
+      unsigned int offset = colid * nbins * n_nodes * n_unique_labels;
       atomicAdd(&histout[offset + i], shmemhist[i]);
     }
     __syncthreads();
   }
+  
 }
 
 template <typename T>
-__global__ void split_level_kernel(const T* __restrict__ data,
-                                   const T* __restrict__ quantile,
-                                   const int* __restrict__ split_col_index,
-                                   const int* __restrict__ split_bin_index,
-                                   const int nrows, const int ncols,
-                                   const int nbins, const int n_nodes,
-                                   const unsigned int* __restrict__ new_node_flags,
-                                   unsigned int* __restrict__ flags) {
+__global__ void split_level_kernel(
+  const T* __restrict__ data, const T* __restrict__ quantile,
+  const unsigned int* __restrict__ split_col_index,
+  const unsigned int* __restrict__ split_bin_index, const int nrows,
+  const int ncols, const int nbins, const int n_nodes,
+  const unsigned int* __restrict__ new_node_flags,
+  unsigned int* __restrict__ flags) {
   unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x;
   unsigned int local_flag;
 
@@ -73,18 +72,18 @@ __global__ void split_level_kernel(const T* __restrict__ data,
   } else {
     local_flag = LEAF;
   }
-  
+
   if (local_flag != LEAF) {
     unsigned int local_leaf_flag = new_node_flags[local_flag];
-    if(local_leaf_flag != LEAF) {
+    if (local_leaf_flag != LEAF) {
       int colidx = split_col_index[local_flag];
       T quesval = quantile[colidx * nbins + split_bin_index[local_flag]];
       T local_data = data[colidx * nrows + tid];
       //The inverse comparision here to push right instead of left
       if (local_data <= quesval) {
-	local_flag = local_leaf_flag << 1;
+        local_flag = local_leaf_flag << 1;
       } else {
-	local_flag = (local_leaf_flag << 1) | PUSHRIGHT;
+        local_flag = (local_leaf_flag << 1) | PUSHRIGHT;
       }
     } else {
       local_flag = LEAF;
