d15c24935892b1c2f9c4472b51dfdd3c92baa5fd
Vishal Mehta <vishalm@nvidia.com>
2019-07-11 14:12:17 +0200

    adding sampling for rows

--- cpp/src/decisiontree/levelalgo/levelhelper.cuh
+++ cpp/src/decisiontree/levelalgo/levelhelper.cuh
@@ -7,16 +7,16 @@ void setup_sampling(unsigned int *flagsptr, int *sample_cnt,
   int threads = 256;
   int blocks = MLCommon::ceildiv(nrows, threads);
   if (blocks > 65536) blocks = 65536;
-  setup_count_kernel<<<blocks, threads, 0, stream>>>(sample_cnt, rowids,
-                                                     n_sampled_rows);
+  setup_counts_kernel<<<blocks, threads, 0, stream>>>(sample_cnt, rowids,
+                                                      n_sampled_rows);
   CUDA_CHECK(cudaGetLastError());
-  setup_flags_kernel<<<blocks, threads, 0, stream>>>(flagsptr, sample_cnt,
+  setup_flags_kernel<<<blocks, threads, 0, stream>>>(sample_cnt, flagsptr,
                                                      nrows);
   CUDA_CHECK(cudaGetLastError());
 }
 template <typename T>
 void get_me_histogram(T *data, int *labels, unsigned int *flags,
-                      const int nrows, const int ncols,
+                      int *sample_cnt, const int nrows, const int ncols,
                       const int n_unique_labels, const int nbins,
                       const int n_nodes, const int maxnodes,
                       LevelTemporaryMemory<T> *tempmem, unsigned int *histout) {
@@ -29,15 +29,15 @@ void get_me_histogram(T *data, int *labels, unsigned int *flags,
   int blocks = MLCommon::ceildiv(nrows, threads);
   if ((n_nodes == node_batch) && (blocks < 65536)) {
     get_me_hist_kernel<<<blocks, threads, shmem, tempmem->stream>>>(
-      data, labels, flags, nrows, ncols, n_unique_labels, nbins, n_nodes,
-      tempmem->d_quantile->data(), histout);
+      data, labels, flags, sample_cnt, nrows, ncols, n_unique_labels, nbins,
+      n_nodes, tempmem->d_quantile->data(), histout);
   } else {
     /*get_me_hist_kernel_batched<<<blocks, threads, shmem, tempmem->stream>>>(
       data, labels, flags, nrows, ncols, n_unique_labels, nbins, n_nodes,
       tempmem->d_quantile->data(), node_batch, histout);*/
     get_me_hist_kernel_global<<<blocks, threads, 0, tempmem->stream>>>(
-      data, labels, flags, nrows, ncols, n_unique_labels, nbins, n_nodes,
-      tempmem->d_quantile->data(), histout);
+      data, labels, flags, sample_cnt, nrows, ncols, n_unique_labels, nbins,
+      n_nodes, tempmem->d_quantile->data(), histout);
   }
   CUDA_CHECK(cudaGetLastError());
 }
--- cpp/src/decisiontree/levelalgo/levelkernel.cuh
+++ cpp/src/decisiontree/levelalgo/levelkernel.cuh
@@ -3,40 +3,44 @@
 
 #define LEAF 0xFFFFFFFF
 #define PUSHRIGHT 0x00000001
+
 __global__ void setup_counts_kernel(int* sample_cnt,
                                     const unsigned int* __restrict__ rowids,
                                     const int n_sampled_rows) {
   int threadid = threadIdx.x + blockIdx.x * blockDim.x;
-  for (tid = threadid; tid < n_sampled_rows; tid += blockDim.x * gridDim.x) {
+  for (int tid = threadid; tid < n_sampled_rows;
+       tid += blockDim.x * gridDim.x) {
     unsigned int stid = rowids[tid];
     atomicAdd(&sample_cnt[stid], 1);
   }
 }
-__global__ void setup_flags_kernel(unsigned int* flags,
-                                   const int* __restict__ sample_cnt,
-                                   const int nrows) {
+__global__ void setup_flags_kernel(const int* __restrict__ sample_cnt,
+                                   unsigned int* flags, const int nrows) {
   int threadid = threadIdx.x + blockIdx.x * blockDim.x;
-  if (tid = threadid; tid < nrows; tid += blockDim.x * gridDim.x) {
+  for (int tid = threadid; tid < nrows; tid += blockDim.x * gridDim.x) {
     int local_cnt = sample_cnt[tid];
     unsigned int local_flag = LEAF;
     if (local_cnt != 0) local_flag = 0x00000000;
     flags[tid] = local_flag;
   }
 }
+
 template <typename T>
 __global__ void get_me_hist_kernel(
   const T* __restrict__ data, const int* __restrict__ labels,
-  const unsigned int* __restrict__ flags, const int nrows, const int ncols,
-  const int n_unique_labels, const int nbins, const int n_nodes,
-  const T* __restrict__ quantile, unsigned int* histout) {
+  const unsigned int* __restrict__ flags, const int* __restrict__ sample_cnt,
+  const int nrows, const int ncols, const int n_unique_labels, const int nbins,
+  const int n_nodes, const T* __restrict__ quantile, unsigned int* histout) {
   extern __shared__ unsigned int shmemhist[];
   unsigned int local_flag = LEAF;
   int local_label = -1;
+  int local_cnt;
   int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
   if (tid < nrows) {
     local_flag = flags[tid];
     local_label = labels[tid];
+    local_cnt = sample_cnt[tid];
   }
 
   for (unsigned int colid = 0; colid < ncols; colid++) {
@@ -56,7 +60,7 @@ __global__ void get_me_hist_kernel(
         if (local_data <= quesval) {
           unsigned int nodeoff = local_flag * nbins * n_unique_labels;
           atomicAdd(&shmemhist[nodeoff + binid * n_unique_labels + local_label],
-                    1);
+                    local_cnt);
         }
       }
     }
@@ -74,17 +78,18 @@ __global__ void get_me_hist_kernel(
 template <typename T>
 __global__ void get_me_hist_kernel_global(
   const T* __restrict__ data, const int* __restrict__ labels,
-  const unsigned int* __restrict__ flags, const int nrows, const int ncols,
-  const int n_unique_labels, const int nbins, const int n_nodes,
-  const T* __restrict__ quantile, unsigned int* histout) {
+  const unsigned int* __restrict__ flags, const int* __restrict__ sample_cnt,
+  const int nrows, const int ncols, const int n_unique_labels, const int nbins,
+  const int n_nodes, const T* __restrict__ quantile, unsigned int* histout) {
   unsigned int local_flag;
   int local_label;
+  int local_cnt;
   int threadid = threadIdx.x + blockIdx.x * blockDim.x;
 
   for (int tid = threadid; tid < nrows; tid += gridDim.x * blockDim.x) {
     local_flag = flags[tid];
     local_label = labels[tid];
-
+    local_cnt = sample_cnt[tid];
     for (unsigned int colid = 0; colid < ncols; colid++) {
       //Check if leaf
       if (local_flag != LEAF) {
@@ -99,7 +104,7 @@ __global__ void get_me_hist_kernel_global(
             unsigned int nodeoff = local_flag * nbins * n_unique_labels;
             atomicAdd(&histout[coloff + nodeoff + binid * n_unique_labels +
                                local_label],
-                      1);
+                      local_cnt);
           }
         }
       }
