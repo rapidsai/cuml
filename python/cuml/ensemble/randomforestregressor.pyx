0560a96f538b87d4d938bed8a47807686d2c36b7
Saloni Jain <saljain@dgx14.aselab.nvidia.com>
2019-07-15 12:50:03 -0700

    updated docs for regressor

--- python/cuml/ensemble/randomforestregressor.pyx
+++ python/cuml/ensemble/randomforestregressor.pyx
@@ -137,77 +137,47 @@ cdef extern from "randomforest/randomforest.hpp" namespace "ML":
 
 
 class RandomForestRegressor(Base):
-    """
-    Implements a Random Forest classifier model which fits multiple decision
-    tree classifiers in an ensemble.
 
+    """
+    Implements a Random Forest regressor model which fits multiple decision
+    trees in an ensemble.
     Note that the underlying algorithm for tree node splits differs from that
     used in scikit-learn. By default, the cuML Random Forest uses a
     histogram-based algorithms to determine splits, rather than an exact
     count. You can tune the size of the histograms with the n_bins parameter.
-
-    **Known Limitations**: This is an initial preview release of the cuML
-    Random Forest code. It contains a number of known
-    limitations:
-
-       * Only classification is supported. Regression support is planned for
-         the next release.
-
-       * The implementation relies on limited CUDA shared memory for scratch
-         space, so models with a very large number of features or bins will
-         generate a memory limit exception. This limitation will be lifted in
-         the next release.
-
-       * Inference/prediction takes place on the CPU. A GPU-based inference
-         solution is planned for a near-future release release.
-
-       * Instances of RandomForestRegressor cannot be pickled currently.
-
-    The code is under heavy development, so users who need these features may
-    wish to pull from nightly builds of cuML. (See https://rapids.ai/start.html
-    for instructions to download nightly packages via conda.)
-
+    The instances of RandomForestRegressor cannot be pickled currently.
     Examples
     ---------
     .. code-block:: python
-
             import numpy as np
-            from cuml.ensemble import RandomForestRegressor as cuRFC
-
-            X = np.random.normal(size=(10,4)).astype(np.float32)
-            y = np.asarray([0,1]*5, dtype=np.int32)
-
-            cuml_model = cuRFC(max_features=1.0,
-                               n_bins=8,
-                               n_estimators=40)
+            from cuml.test.utils import get_handle
+            from cuml.ensemble import RandomForestRegressor as curfc
+            from cuml.test.utils import get_handle
+            X = np.asarray([[0,10],[0,20],[0,30],[0,40]], dtype=np.float32)
+            y = np.asarray([0.0,1.0,2.0,3.0], dtype=np.float32)
+            cuml_model = curfc(max_features=1.0, n_bins=8,
+                               split_algo=0, min_rows_per_node=2,
+                               n_estimators=40, accuracy_metric='mse')
             cuml_model.fit(X,y)
-            cuml_predict = cuml_model.predict(X)
-
-            print("Predicted labels : ", cuml_predict)
-
+            cuml_score = cuml_model.score(X,y)
+            print("MSE score of cuml : ", cuml_score)
     Output:
-
-    .. code-block:: none
-
-            Predicted labels :  [0 1 0 1 0 1 0 1 0 1]
-
+    .. code-block:: python
+            MSE score of cuml :  0.1123437201231765
     Parameters
     -----------
     n_estimators : int (default = 10)
                    number of trees in the forest.
     handle : cuml.Handle
              If it is None, a new one is created just for this class.
-    split_criterion: The criterion used to split nodes.
-                     0 for GINI, 1 for ENTROPY, 4 for CRITERION_END.
-                     2 and 3 not valid for classification
-                     (default = 0)
-    split_algo : 0 for HIST and 1 for GLOBAL_QUANTILE
-                 (default = 0)
-                 the algorithm to determine how nodes are split in the tree.
-    split_criterion: The criterion used to split nodes.
-                     0 for GINI, 1 for ENTROPY, 4 for CRITERION_END.
-                     2 and 3 not valid for classification
-                     (default = 0)
+    split_algo : int (default = 1)
+                 0 for HIST, 1 for GLOBAL_QUANTILE and 2 for SPLIT_ALGO_END
+                 The type of algorithm to be used to create the trees.
+    split_criterion: int (default = 2)
+                     The criterion used to split nodes.
+                     0 for GINI, 1 for ENTROPY,
+                     2 for MSE, 3 for MAE and 4 for CRITERION_END.
+                     0 and 1 not valid for regression
     bootstrap : boolean (default = True)
                 Control bootstrapping.
                 If set, each tree in the forest is built
@@ -224,18 +194,26 @@ class RandomForestRegressor(Base):
     max_leaves : int (default = -1)
                  Maximum leaf nodes per tree. Soft constraint. Unlimited,
                  if -1.
-    max_features : float (default = 1.0)
+    max_features : int or float or string or None (default = 1.0)
                    Ratio of number of features (columns) to consider
                    per node split.
+                   If int then max_features/n_features.
+                   If float then max_features is a fraction.
+                   If 'auto' then max_features=n_features which is 1.0.
+                   If 'sqrt' then max_features=1/sqrt(n_features).
+                   If 'log2' then max_features=log2(n_features)/n_features.
+                   If None, then max_features=n_features which is 1.0.
     n_bins :  int (default = 8)
               Number of bins used by the split algorithm.
-    min_rows_per_node : int (default = 2)
+    min_rows_per_node : int or float (default = 2)
                         The minimum number of samples (rows) needed
                         to split a node.
-    quantile_per_tree : boolean (default = False)
-                        Whether quantile is computed for individal trees in RF.
-                        Only relevant for GLOBAL_QUANTILE split_algo.
-
+                        If int then number of sample rows
+                        If float the min_rows_per_sample*n_rows
+    accuracy_metric : string (default = 'mse')
+                      Decides the metric used to evaluate the performance
+                      of the model.
+                      'median'
     """
 
     variables = ['n_estimators', 'max_depth', 'handle',
