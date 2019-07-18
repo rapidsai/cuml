eaf84cc6ce8f912dbc256af4a188cd69b9498a3b
Thejaswi Rao <rao.thejaswi@gmail.com>
2019-07-17 23:06:04 -0700

    initial unit-tests for histogram prim

---
+++ cpp/test/prims/histogram.cu
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cuda_utils.h>
+#include <gtest/gtest.h>
+#include <stats/histogram.h>
+#include <random/rng.h>
+#include "test_utils.h"
+
+namespace MLCommon {
+namespace Stats {
+
+__global__ void naiveHistKernel(int* bins, int nbins, const int* in, int n) {
+  int tid = threadIdx.x + blockIdx.x * blockDim.x;
+  int stride = blockDim.x * gridDim.x;
+  for (; tid < n; tid += stride) {
+    int id = in[tid];
+    atomicAdd(bins + id, 1);
+  }
+}
+
+void naiveHist(int* bins, int nbins, const int* in, int n,
+               cudaStream_t stream) {
+  const int TPB = 128;
+  int nblks = ceildiv(n, TPB);
+  naiveHistKernel<<<nblks, TPB, 0, stream>>>(bins, nbins, in, i);
+  CUDA_CHECK(cudaGetLastError());
+}
+
+struct HistInputs {
+  int n, nbins;
+  bool isNormal;
+  HistType type;
+  int start, end;
+  unsigned long long int seed;
+};
+
+template <typename DataT>
+class HistTest : public ::testing::TestWithParam<HistInputs> {
+ protected:
+  void SetUp() override {
+    params = ::testing::TestWithParam<HistInputs>::GetParam();
+    Random::Rng r(params.seed);
+    CUDA_CHECK(cudaStreamCreate(&stream));
+    allocate(in, params.n);
+    if (params.isNormal) {
+      r.normalInt(in, n, params.start, params.end, stream);
+    } else {
+      r.uniformInt(in, n, params.start, params.end, stream);
+    }
+    allocate(bins, params.nbins);
+    allocate(ref_bins, params.nbins);
+    naiveHist(ref_bins, params.nbins, in, params.n, stream);
+    histogram(params.type, bins, params.nbins, in, params.n, stream);
+    CUDA_CHECK(cudaStreamSynchronize(stream));
+  }
+
+  void TearDown() override {
+    CUDA_CHECK(cudaFree(in));
+    CUDA_CHECK(cudaFree(bins));
+    CUDA_CHECK(cudaFree(ref_bins));
+    CUDA_CHECK(cudaStreamDestroy(stream));
+  }
+
+ protected:
+  cudaStream_t stream;
+  HistInputs params;
+  DataT *in;
+  int *bins, *ref_bins;
+};
+
+static const int oneM = 1024 * 1024;
+const std::vector<HistInputs> inputs = {
+  {oneM, 2 * oneM, false, HistTypeGmem, 0, 2 * oneM, 1234ULL},
+  {oneM, 2 * oneM, true, HistTypeGmem, 1000, 50, 1234ULL},
+};
+
+typedef HistTest<float> HistTestF;
+TEST_P(HistTestF, Result) {
+  ASSERT_TRUE(devArrMatch(ref_bins, bins, params.nbins, Compare<int>()));
+}
+INSTANTIATE_TEST_CASE_P(HistTests, HistTestF, ::testing::ValuesIn(inputs));
+
+typedef HistTest<double> HistTestD;
+TEST_P(HistTestD, Result) {
+  ASSERT_TRUE(devArrMatch(ref_bins, bins, params.nbins, Compare<int>()));
+}
+INSTANTIATE_TEST_CASE_P(HistTests, HistTestD, ::testing::ValuesIn(inputs));
+
+}  // end namespace Stats
+}  // end namespace MLCommon
