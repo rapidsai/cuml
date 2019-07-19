7fe70ce9997bdbb1ae030d43d20165585ff9d9fe
divyegala <divyegala@gmail.com>
2019-07-19 10:36:27 -0700

    Added runner.h to cover all implementation

--- cpp/src/holtwinters/runner.h
+++ cpp/src/holtwinters/runner.h
@@ -14,9 +14,66 @@
  * limitations under the License.
  */
 
-#include "holtwinters.h"
+#pragma once
+
+#include "holtwinters_utils.h"
+#include "holtwinters_utils.h"
+#include "linalg/transpose.h"
 
 namespace ML {
+
+template <typename Dtype>
+void HWTranspose(const ML::cumlHandle &handle, Dtype *data_in, int m, int n,
+                 Dtype *data_out);
+
+void HoltWintersBufferSize(int n, int batch_size, int frequency, bool use_beta,
+                           bool use_gamma, int *start_leveltrend_len,
+                           int *start_season_len, int *components_len,
+                           int *error_len, int *leveltrend_coef_shift,
+                           int *season_coef_shift);
+
+template <typename Dtype>
+void HoltWintersDecompose(const ML::cumlHandle &handle, const Dtype *ts, int n,
+                          int batch_size, int frequency, Dtype *start_level,
+                          Dtype *start_trend, Dtype *start_season,
+                          int start_periods, SeasonalType seasonal = ADDITIVE);
+
+template <typename Dtype>
+void HoltWintersOptim(const ML::cumlHandle &handle, const Dtype *ts, int n,
+                      int batch_size, int frequency, const Dtype *start_level,
+                      const Dtype *start_trend, const Dtype *start_season,
+                      Dtype *alpha, bool optim_alpha, Dtype *beta,
+                      bool optim_beta, Dtype *gamma, bool optim_gamma,
+                      Dtype *level, Dtype *trend, Dtype *season, Dtype *xhat,
+                      Dtype *error, OptimCriterion *optim_result,
+                      OptimParams<Dtype> *optim_params = nullptr,
+                      SeasonalType seasonal = ADDITIVE);
+
+template <typename Dtype>
+void HoltWintersEval(const ML::cumlHandle &handle, const Dtype *ts, int n,
+                     int batch_size, int frequency, const Dtype *start_level,
+                     const Dtype *start_trend, const Dtype *start_season,
+                     const Dtype *alpha, const Dtype *beta, const Dtype *gamma,
+                     Dtype *level, Dtype *trend, Dtype *season, Dtype *xhat,
+                     Dtype *error, SeasonalType seasonal = ADDITIVE);
+
+template <typename Dtype>
+void HoltWintersForecast(const ML::cumlHandle &handle, Dtype *forecast, int h,
+                         int batch_size, int frequency, const Dtype *level_coef,
+                         const Dtype *trend_coef, const Dtype *season_coef,
+                         SeasonalType seasonal = ADDITIVE);
+
+template <typename Dtype>
+void HoltWintersFit(const ML::cumlHandle &handle, int n, int batch_size,
+                    int frequency, int start_periods, SeasonalType seasonal,
+                    Dtype *data, Dtype *level_ptr, Dtype *trend_ptr,
+                    Dtype *season_ptr, Dtype *SSE_error_ptr);
+
+template <typename Dtype>
+void HoltWintersPredict(const ML::cumlHandle &handle, int n, int batch_size,
+                        int frequency, int h, SeasonalType seasonal,
+                        Dtype *level_ptr, Dtype *trend_ptr, Dtype *season_ptr,
+                        Dtype *forecast_ptr);
 
 template <typename Dtype>
 void HWTranspose(const ML::cumlHandle &handle, Dtype *data_in, int m, int n,
