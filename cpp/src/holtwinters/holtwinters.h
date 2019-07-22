330a0232d91bbaa204466d1c16b2f5fab850d47e
divyegala <divyegala@gmail.com>
2019-07-22 09:29:39 -0700

    Doxygen style dosctrings

--- cpp/src/holtwinters/holtwinters.h
+++ cpp/src/holtwinters/holtwinters.h
@@ -21,25 +21,92 @@
 namespace ML {
 namespace HoltWinters {
 
+  /**
+             * Provifes buffer sizes for HoltWinters algorithm
+             * @param n
+             *        n_samples in time-series
+             * @param batch_size
+             *        number of time-series in X
+             * @param frequency
+             *        number of periods in a season of the time-series
+             * @param start_leveltrend_len
+             *        pointer which will hold the length of the level/trend array buffers
+             * @param start_season_len
+             *        pointer which will hold the length of the seasonal array buffer
+             * @param components_len
+             *        pointer which will hold the length of all three components
+             * @param error_len
+             *        pointer which will hold the length of the SSE Error
+             * @param leveltrend_coef_shift
+             *        pointer which will hold the offset to level/trend arrays
+             * @param season_coef_shift
+             *        pointer which will hold the offset to season array
+             */
 void buffer_size(int n, int batch_size, int frequency,
                  int *start_leveltrend_len, int *start_season_len,
                  int *components_len, int *error_len,
                  int *leveltrend_coef_shift, int *season_coef_shift);
 
+  /**
+             * Fits a HoltWinters model
+             * @param handle
+             *        cuml handle to use across the algorithm
+             * @param n
+             *        n_samples in time-series
+             * @param batch_size
+             *        number of time-series in X
+             * @param frequency
+             *        number of periods in a season of the time-series
+             * @param start_periods
+             *        number of seasons to be used for seasonal seed values
+             * @param seasonal
+             *        type of seasonal component (ADDITIVE or MULTIPLICATIVE)
+             * @param data
+             *        device pointer to the data to fit on
+             * @param level_ptr
+             *        host pointer to array which will hold level components
+             * @param trend_ptr
+             *        host pointer to array which will hold trend components
+             * @param season_ptr
+             *        host pointer to array which will hold season components
+             * @param SSE_error_ptr
+             *        host pointer to array which will hold training SSE error
+             */
 void fit(const ML::cumlHandle &handle, int n, int batch_size, int frequency,
          int start_periods, ML::SeasonalType seasonal, float *data,
          float *level_ptr, float *trend_ptr, float *season_ptr,
          float *SSE_error_ptr);
-
 void fit(const ML::cumlHandle &handle, int n, int batch_size, int frequency,
          int start_periods, ML::SeasonalType seasonal, double *data,
          double *level_ptr, double *trend_ptr, double *season_ptr,
          double *SSE_error_ptr);
 
+  /**
+             * Fits a HoltWinters model
+             * @param handle
+             *        cuml handle to use across the algorithm
+             * @param n
+             *        n_samples in time-series
+             * @param batch_size
+             *        number of time-series in X
+             * @param frequency
+             *        number of periods in a season of the time-series
+             * @param h
+             *        number of future points to predict in the time-series
+             * @param seasonal
+             *        type of seasonal component (ADDITIVE or MULTIPLICATIVE)
+             * @param level_ptr
+             *        host pointer to array which holds level components
+             * @param trend_ptr
+             *        host pointer to array which holds trend components
+             * @param season_ptr
+             *        host pointer to array which holds season components
+             * @param forecast_ptr
+             *        host pointer to array which will hold the predicted points
+             */
 void predict(const ML::cumlHandle &handle, int n, int batch_size, int frequency,
              int h, ML::SeasonalType seasonal, float *level_ptr,
              float *trend_ptr, float *season_ptr, float *forecast_ptr);
-
 void predict(const ML::cumlHandle &handle, int n, int batch_size, int frequency,
              int h, ML::SeasonalType seasonal, double *level_ptr,
              double *trend_ptr, double *season_ptr, double *forecast_ptr);
