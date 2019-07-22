91cced0f656faba46f7a373dd9cacbd1eae5b737
Corey J. Nolet <cjnolet@gmail.com>
2019-07-22 15:36:04 -0400

    Using the proper utils file

--- python/cuml/test/dask/utils.py
+++ python/cuml/test/dask/utils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018, NVIDIA CORPORATION.
+# Copyright (c) 2019, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,125 +11,66 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-
-import numpy as np
+import dask.dataframe as dd
 import pandas as pd
-from copy import deepcopy
-
-from numbers import Number
-from numba import cuda
-from sklearn import datasets
 
 import cudf
-import cuml
+
+import dask_cudf
+
+from dask.distributed import default_client, wait
+
+from sklearn.datasets import make_blobs
+
+import numpy as np
+
+import random
+import math
 
 
-def array_equal(a, b, tol=1e-4, with_sign=True):
-    a = to_nparray(a)
-    b = to_nparray(b)
-    if not with_sign:
-        a, b = np.abs(a), np.abs(b)
-    res = np.max(np.abs(a-b)) < tol
-    return res
+def create_df(f, m, n, centers, cluster_std, random_state, r):
+    X, y = make_blobs(m, n, centers=centers, cluster_std=cluster_std, random_state=random_state)
+    ret = pd.DataFrame(X)
+    return ret
 
 
-def to_nparray(x):
-    if isinstance(x, Number):
-        return np.array([x])
-    elif isinstance(x, pd.DataFrame):
-        return x.values
-    elif isinstance(x, cudf.DataFrame):
-        return x.to_pandas().values
-    elif isinstance(x, cudf.Series):
-        return x.to_pandas().values
-    return np.array(x)
+def get_meta(df):
+    ret = df.iloc[:0]
+    return ret
 
 
-def get_pattern(name, n_samples):
-    np.random.seed(0)
-    random_state = 170
-
-    if name == 'noisy_circles':
-        data = datasets.make_circles(n_samples=n_samples, factor=.5, noise=.05)
-        params = {'damping': .77, 'preference': -240,
-                  'quantile': .2, 'n_clusters': 2}
-
-    elif name == 'noisy_moons':
-        data = datasets.make_moons(n_samples=n_samples, noise=.05)
-        params = {'damping': .75, 'preference': -220, 'n_clusters': 2}
-
-    elif name == 'varied':
-        data = datasets.make_blobs(n_samples=n_samples,
-                                   cluster_std=[1.0, 2.5, 0.5],
-                                   random_state=random_state)
-        params = {'eps': .18, 'n_neighbors': 2}
-
-    elif name == 'blobs':
-        data = datasets.make_blobs(n_samples=n_samples, random_state=8)
-        params = {}
-
-    elif name == 'aniso':
-        X, y = datasets.make_blobs(n_samples=n_samples,
-                                   random_state=random_state)
-        transformation = [[0.6, -0.6], [-0.4, 0.8]]
-        X_aniso = np.dot(X, transformation)
-        data = (X_aniso, y)
-        params = {'eps': .15, 'n_neighbors': 2}
-
-    elif name == 'no_structure':
-        data = np.random.rand(n_samples, 2), None
-        params = {}
-
-    return [data, params]
+def to_cudf(df, r):
+    return cudf.from_pandas(df)
 
 
-def np_to_cudf(X):
-    df = cudf.DataFrame()
-    for i in range(X.shape[1]):
-        df['fea%d' % i] = cuda.to_device(np.ascontiguousarray(X[:, i]))
-    return df
+def dask_make_blobs(nrows, ncols, n_centers=8, cluster_std=1.0,
+                    center_box=(-10, 10), random_state=None, verbose=False):
 
+    client = default_client()
 
-def fit_predict(algorithm, name, X):
-    if name.startswith('sk'):
-        algorithm.fit(X)
-        if hasattr(algorithm, 'labels_'):
-            y_pred = algorithm.labels_.astype(np.int)
-        else:
-            y_pred = algorithm.predict(X)
-    else:
-        df = np_to_cudf(X)
-        algorithm.fit(df)
-        y_pred = algorithm.labels_.to_pandas().values.astype(np.int)
+    workers = client.has_what().keys()
 
-    n_clusters = len(set(y_pred)) - (1 if -1 in y_pred else 0)
-    return y_pred, n_clusters
+    centers = np.random.uniform(center_box[0], center_box[1],
+                                size=(n_centers, ncols)).astype(np.float32)
 
+    if verbose:
+        print("Generating %d samples on %d workers (total=%d samples)" %
+              (math.ceil(nrows/len(workers)), len(workers), nrows))
 
-def normalize_clusters(a0, b0, n_clusters):
-    a = to_nparray(a0)
-    b = to_nparray(b0)
+    # Create dfs on each worker (gpu)
+    dfs = [client.submit(create_df, n, math.ceil(nrows/len(workers)), ncols, centers, cluster_std,
+                         random_state, random.random(), workers=[worker])
+           for worker, n in list(zip(workers, list(range(len(workers)))))]
+    # Wait for completion
+    wait(dfs)
 
-    c = deepcopy(b)
+    ddfs = [client.submit(to_cudf, df, random.random()) for df in dfs]
+    # Wait for completion
+    wait(ddfs)
 
-    for i in range(n_clusters):
-        idx, = np.where(a == i)
-        a_to_b = c[idx[0]]
-        b[c == a_to_b] = i
-
-    return a, b
-
-
-def clusters_equal(a0, b0, n_clusters):
-    a, b = normalize_clusters(a0, b0, n_clusters)
-    return array_equal(a, b)
-
-
-def get_handle(use_handle):
-    if not use_handle:
-        return None, None
-    h = cuml.Handle()
-    s = cuml.cuda.Stream()
-    h.setStream(s)
-    return h, s
+    meta_ddf = client.submit(get_meta, dfs[0]).result()
+    meta_cudf = client.submit(get_meta, ddfs[0]).result()
+    return dd.from_delayed(dfs, meta=meta_ddf), \
+           dask_cudf.from_delayed(ddfs, meta=meta_cudf)
