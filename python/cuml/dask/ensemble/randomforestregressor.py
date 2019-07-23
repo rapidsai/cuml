6085b78c548609898781e74fb7b346ad98fc8ab6
Onur Yilmaz <35306097+oyilmaz-nvidia@users.noreply.github.com>
2019-07-23 13:55:34 -0400

    Update python/cuml/dask/ensemble/randomforestregressor.py
    Co-Authored-By: Dante Gama Dessavre <dante.gamadessavre@gmail.com>

--- python/cuml/dask/ensemble/randomforestregressor.py
+++ python/cuml/dask/ensemble/randomforestregressor.py
@@ -227,7 +227,7 @@ class RandomForestRegressor:
 
         Parameters
         ----------
-        X : Acceptable format: dask-cudf. Dense matrix (floats or doubles) of shape (n_samples, n_features).
+        X : dask_cudf.Dataframe containing dense matrix (floats or doubles) of shape (n_samples, n_features).
         y : Acceptable format: dask-cudf. Dense matrix (floats or doubles) of shape (n_samples, 1)
         """
         c = default_client()
