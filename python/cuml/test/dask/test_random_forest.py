a5e0cfe8832c460803b5293a6146b7fece3375e4
Corey J. Nolet <cjnolet@gmail.com>
2019-07-26 15:41:12 -0400

    Removing duplicated code from dask rf impls. Adding missing regression test

--- python/cuml/test/dask/test_random_forest.py
+++ python/cuml/test/dask/test_random_forest.py
@@ -14,10 +14,11 @@
 #
 
 from cuml import RandomForestClassifier as cuRF
-from cuml.dask.ensemble import RandomForestClassifier as cuRF_mg
-from sklearn.datasets import make_classification
+from cuml.dask.ensemble import RandomForestClassifier as cuRFC_mg
+from cuml.dask.ensemble import RandomForestRegressor as cuRFR_mg
+from sklearn.datasets import make_regression, make_classification
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, r2_score
 from dask.distributed import Client, wait
 from dask_cuda import LocalCUDACluster
 
@@ -27,16 +28,15 @@ import cudf
 import numpy as np
 import pandas as pd
 
-def test_rf():
+
+def test_rf_classification():
 
     cluster = LocalCUDACluster(threads_per_worker=1)
     c = Client(cluster)
 
-
     X, y = make_classification(n_samples=10000, n_features=20,
                                n_clusters_per_class=1, n_informative=10,
                                random_state=123, n_classes=5)
-
 
     y = y.astype(np.int32)
 
@@ -58,13 +58,51 @@ def test_rf():
     y_cudf = cudf.Series(y_cudf)
     y_train_df = dask_cudf.from_cudf(y_cudf, npartitions=len(workers)).persist()
 
-    cu_rf_mg = cuRF_mg(**cu_rf_params)
+    cu_rf_mg = cuRFC_mg(**cu_rf_params)
     cu_rf_mg.fit(X_train_df, y_train_df)
     cu_rf_mg_predict = cu_rf_mg.predict(X_test)
 
     acc_score = accuracy_score(cu_rf_mg_predict, y_test, normalize=True)
 
     assert acc_score > 0.8
+
+    c.close()
+    cluster.close()
+
+
+def test_rf_regression():
+    cluster = LocalCUDACluster(threads_per_worker=1)
+    c = Client(cluster)
+
+    X, y = make_regression(n_samples=10000, n_features=20,
+                           n_informative=10, random_state=123)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1_000)
+
+    cu_rf_params = {
+        'n_estimators': 25,
+        'max_depth': 13,
+    }
+
+    workers = c.has_what().keys()
+
+    X_cudf = cudf.DataFrame.from_pandas(pd.DataFrame(X_train))
+    X_train_df = dask_cudf.from_cudf(X_cudf, npartitions=len(workers)).persist()
+
+    y_cudf = np.array(pd.DataFrame(y_train).values)
+    y_cudf = y_cudf[:, 0]
+    y_cudf = cudf.Series(y_cudf)
+    y_train_df = dask_cudf.from_cudf(y_cudf, npartitions=len(workers)).persist()
+
+    cu_rf_mg = cuRFR_mg(**cu_rf_params)
+    cu_rf_mg.fit(X_train_df, y_train_df)
+    cu_rf_mg_predict = cu_rf_mg.predict(X_test)
+
+    acc_score = r2_score(cu_rf_mg_predict, y_test)
+
+    print(str(acc_score))
+
+    assert acc_score >= 0.70
 
     c.close()
     cluster.close()
