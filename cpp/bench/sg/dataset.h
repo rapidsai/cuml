0cff7f25f1eaab0c65bda8eeafeb83ec43fabb17
Thejaswi Rao <rao.thejaswi@gmail.com>
2019-07-29 01:45:52 -0700

    initial commit of benchmark suite for cuml-c++

---
+++ cpp/bench/sg/dataset.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <common/device_buffer.hpp>
+#include <cuML.hpp>
+#include <cuda_runtime.h>
+
+namespace ML {
+namespace Bench {
+
+struct Dataset {
+  int nrows, ncols;
+  float* X;
+  int* y;
+
+  void allocate(int nr, int nc, const cumlHandle& handle);
+};
+
+bool blobs(Dataset& ret, const cumlHandle& handle, char** argv, int start,
+           int end);
+bool load(Dataset& ret, const cumlHandle& handle, char** argv, int start,
+          int end);
+
+} // end namespace Bench
+} // end namespace ML
