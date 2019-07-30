5ee82baa54eb73b481fe62e1c14678fdaef11813
Ethan Goolish <ethangoolish@gmail.com>
2019-07-30 16:54:09 +0000

    Renamed HoltWinters to ExponentialSmoothing

--- python/cuml/__init__.py
+++ python/cuml/__init__.py
@@ -46,7 +46,7 @@ from cuml.solvers.cd import CD
 from cuml.solvers.sgd import SGD
 from cuml.solvers.qn import QN
 
-from cuml.tsa.holtwinters import HoltWinters
+from cuml.tsa.holtwinters import ExponentialSmoothing
 
 from cuml.manifold.umap import UMAP
 
