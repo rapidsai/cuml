210f4fc71187e952a64371a61af664034aa2eda9
Tamas Bela Feher <tfeher@nvidia.com>
2019-07-30 17:34:48 +0200

    Add Support Vector Machine classifier

---
+++ cpp/src/svm/smosolver.h
@@ -0,0 +1,269 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <cuda_utils.h>
+#include <math.h>
+#include <iostream>
+#include <limits>
+
+#include "common/cumlHandle.hpp"
+#include "gram/grammatrix.h"
+#include "gram/kernelfactory.h"
+#include "gram/kernelparams.h"
+#include "kernelcache.h"
+#include "linalg/cublas_wrappers.h"
+#include "linalg/gemv.h"
+#include "linalg/unary_op.h"
+#include "smo_sets.h"
+#include "smoblocksolve.h"
+#include "workingset.h"
+#include "ws_util.h"
+
+#include "common/device_buffer.hpp"
+#include "results.h"
+
+namespace ML {
+namespace SVM {
+
+using namespace MLCommon;
+
+/**
+ * Solve the quadratic optimization problem using two level decomposition and
+ * Sequential Minimal Optimization (SMO).
+ *
+ * The general decomposition idea by Osuna is to choose q examples from all the
+ * training examples, and solve the QP problem for this subset (discussed in
+ * section 11.2 by Joachims [1]). SMO is the extreme case where we choose q=2.
+ *
+ * Here we follow [2] and [3] and use two level decomposition. First we set
+ * q_1=1024, and solve the QP sub-problem for that (let's call it QP1). This is
+ * the outer iteration, implemented in SmoSolver::Solve.
+ *
+ * To solve QP1, we use another decomposition, specifically the SMO (q_2 = 2),
+ * which is implemented in SmoBlockSolve.
+ *
+ * References:
+ * [1] Joachims, T. Making large-scale support vector machine learning
+ *      practical. In B. Scholkopf, C. Burges, & A. Smola (Eds.), Advances in
+ *      kernel methods: Support vector machines. Cambridge, MA: MIT Press (1998)
+ * [2] J. Vanek et al. A GPU-Architecture Optimized Hierarchical Decomposition
+ *      Algorithm for Support VectorMachine Training, IEEE Transactions on
+ *      Parallel and Distributed Systems, vol 28, no 12, 3330, (2017)
+ * [3] Z. Wen et al. ThunderSVM: A Fast SVM Library on GPUs and CPUs, Journal
+ *      of Machine Learning Research, 19, 1-5 (2018)
+ */
+template <typename math_t>
+class SmoSolver {
+ public:
+  SmoSolver(const cumlHandle_impl &handle, math_t C, math_t tol,
+            GramMatrix::GramMatrixBase<math_t> *kernel, float cache_size = 200)
+    : handle(handle),
+      n_rows(n_rows),
+      C(C),
+      tol(tol),
+      kernel(kernel),
+      cache_size(cache_size),
+      stream(handle.getStream()),
+      return_buff(handle.getDeviceAllocator(), stream, 2),
+      alpha(handle.getDeviceAllocator(), stream),
+      delta_alpha(handle.getDeviceAllocator(), stream),
+      f(handle.getDeviceAllocator(), stream) {}
+
+#define SMO_WS_SIZE 1024
+  /**
+   * Solve the quadratic optimization problem.
+   *
+   * The output arrays (dual_coefs, x_support, idx) will be allocated on the
+   * device, they should be unallocated on entry.
+   *
+   * @param [in] x training vectors in column major format, size [n_rows x n_cols]
+   * @param [in] n_rows number of rows (training vectors)
+   * @param [in] n_cols number of columns (features)
+   * @param [in] y labels (values +/-1), size [n_rows]
+   * @param [out] dual_coefs, size [n_support] on exit
+   * @param [out] n_support number of support vectors
+   * @param [out] x_support support vectors in column major format, size [n_support, n_cols]
+   * @param [out] idx the original training set indices of the support vectors, size [n_support]
+   * @param [out] b scalar constant for the decision function
+   * @param [in] max_out_iter maximum number of outer iteration (default 100 * n_rows)
+   * @param [in] xm_inner_iter maximum number of inner iterations (default 10000)
+   */
+  void Solve(math_t *x, int n_rows, int n_cols, math_t *y, math_t **dual_coefs,
+             int *n_support, math_t **x_support, int **idx, math_t *b,
+             int max_outer_iter = -1, int max_inner_iter = 10000) {
+    if (max_outer_iter == -1) {
+      max_outer_iter = n_rows < std::numeric_limits<int>::max() / 100
+                         ? n_rows * 100
+                         : std::numeric_limits<int>::max();
+      max_outer_iter = max(100000, max_outer_iter);
+    }
+
+    WorkingSet<math_t> ws(handle, stream, n_rows, SMO_WS_SIZE);
+    int n_ws = ws.GetSize();
+    ResizeBuffers(n_rows, n_cols, n_ws);
+    Initialize(y);
+
+    KernelCache<math_t> cache(handle, x, n_rows, n_cols, n_ws, kernel,
+                              cache_size);
+
+    int n_iter = 0;
+    int n_inner_iter = 0;
+    diff_prev = 0;
+    n_small_diff = 0;
+    bool keep_going = true;
+
+    while (n_iter < max_outer_iter && keep_going) {
+      CUDA_CHECK(
+        cudaMemsetAsync(delta_alpha.data(), 0, n_ws * sizeof(math_t), stream));
+      ws.Select(f.data(), alpha.data(), y, C);
+
+      math_t *cacheTile = cache.GetTile(ws.GetIndices());
+
+      SmoBlockSolve<math_t, SMO_WS_SIZE><<<1, n_ws, 0, stream>>>(
+        y, n_rows, alpha.data(), n_ws, delta_alpha.data(), f.data(), cacheTile,
+        ws.GetIndices(), C, tol, return_buff.data(), max_inner_iter);
+
+      CUDA_CHECK(cudaPeekAtLastError());
+
+      updateHost(host_return_buff, return_buff.data(), 2, stream);
+
+      UpdateF(f.data(), n_rows, delta_alpha.data(), n_ws, cacheTile);
+
+      CUDA_CHECK(cudaStreamSynchronize(stream));
+
+      math_t diff = host_return_buff[0];
+      keep_going = CheckStoppingCondition(diff);
+
+      n_inner_iter += host_return_buff[1];
+      n_iter++;
+      if (n_iter % 500 == 0) {
+        std::cout << "SMO iteration " << n_iter << ", diff " << diff << "\n";
+      }
+    }
+
+    std::cout << "SMO solver finished after " << n_iter << " outer iterations, "
+              << n_inner_iter << " total inner iterations, and diff "
+              << diff_prev << "\n";
+    Results<math_t> res(handle, x, y, n_rows, n_cols, C, kernel);
+    res.Get(alpha.data(), dual_coefs, n_support, idx, x_support, b);
+    ReleaseBuffers();
+  }
+
+  /**
+   * Update the f vector after a block solve step.
+   *
+   * \f[ f_i = f_i + \sum_{k\in WS} K_{i,k} * \Delta \alpha_k, \f]
+   * where i = [0..n_rows-1], WS is the set of workspace indices,
+   * and \f$K_{i,k}\f$ is the kernel function evaluated for training vector x_i and workspace vector x_k.
+   *
+   * @param f size [n_rows]
+   * @param n_rows
+   * @param delta_alpha size [n_ws]
+   * @param n_ws
+   * @param cacheTile kernel function evaluated for the following set K[X,x_ws], size [n_rows, n_ws]
+   * @param cublas_handle
+   */
+  void UpdateF(math_t *f, int n_rows, const math_t *delta_alpha, int n_ws,
+               const math_t *cacheTile) {
+    math_t one =
+      1;  // multipliers used in the equation : f = 1*cachtile * delta_alpha + 1*f
+    CUBLAS_CHECK(LinAlg::cublasgemv(handle.getCublasHandle(), CUBLAS_OP_N,
+                                    n_rows, n_ws, &one, cacheTile, n_rows,
+                                    delta_alpha, 1, &one, f, 1, stream));
+  }
+
+  /// Initialize the values of alpha and f
+  void Initialize(math_t *y) {
+    // we initialize alpha_i = 0 and
+    // f_i = -y_i
+    CUDA_CHECK(
+      cudaMemsetAsync(alpha.data(), 0, n_rows * sizeof(math_t), stream));
+    LinAlg::unaryOp(
+      f.data(), y, n_rows, [] __device__(math_t y) { return -y; }, stream);
+  }
+
+ private:
+  const cumlHandle_impl &handle;
+  cudaStream_t stream;
+
+  int n_rows = 0;  //!< training data number of rows
+  int n_cols = 0;  //!< training data number of columns
+  int n_ws = 0;    //!< size of the working set
+
+  // Buffers for the domain [n_rows]
+  device_buffer<math_t> alpha;  //!< dual coordinates
+  device_buffer<math_t> f;      //!< optimality indicator vector
+
+  // Buffers for the working set [n_ws]
+  //! change in alpha parameter during a blocksolve step
+  device_buffer<math_t> delta_alpha;
+
+  // Buffers to return some parameters from the kernel (iteration number, and
+  // convergence information)
+  device_buffer<math_t> return_buff;
+  math_t host_return_buff[2];
+
+  math_t C;
+  math_t tol;  //!< tolerance for stopping condition
+
+  GramMatrix::GramMatrixBase<math_t> *kernel;
+  float cache_size;  //!< size of kernel cache in MiB
+
+  // Variables to track convergence of training
+  math_t diff_prev;
+  int n_small_diff;
+
+  bool CheckStoppingCondition(math_t diff) {
+    // TODO improve stopping condition to detect oscillations
+    bool keep_going = true;
+    if (abs(diff - diff_prev) < 0.001 * tol) {
+      n_small_diff++;
+    } else {
+      diff_prev = diff;
+      n_small_diff = 0;
+    }
+    if (diff < tol || n_small_diff > 10) {
+      keep_going = false;
+    }
+    // ASSERT(!isnan(diff), "SMO: NaN found during fitting")
+    if (isnan(diff)) {
+      std::cout << "SMO error: NaN found during fitting\n";
+      keep_going = false;
+    }
+    return keep_going;
+  }
+
+  void ResizeBuffers(int n_rows, int n_cols, int n_ws) {
+    // This needs to know n_ws, therefore it can be only called during the solve step
+    this->n_rows = n_rows;
+    this->n_cols = n_cols;
+    this->n_ws = n_ws;
+    alpha.resize(n_rows, stream);
+    f.resize(n_rows, stream);
+    delta_alpha.resize(n_ws, stream);
+  }
+
+  void ReleaseBuffers() {
+    alpha.release(stream);
+    delta_alpha.release(stream);
+    f.release(stream);
+  }
+};
+
+};  // end namespace SVM
+};  // end namespace ML
---
+++ cpp/src/svm/workingset.h
@@ -0,0 +1,411 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <cuda_utils.h>
+#include <limits.h>
+#include <cub/cub.cuh>
+#include "common/cumlHandle.hpp"
+#include "common/device_buffer.hpp"
+#include "linalg/add.h"
+#include "ml_utils.h"
+#include "smo_sets.h"
+#include "ws_util.h"
+
+namespace ML {
+namespace SVM {
+
+using namespace MLCommon;
+
+__device__ bool dummy_select_op(int idx) { return true; }
+
+/**
+* Working set selection for the SMO algorithm.
+*
+* The working set is a subset of the training vectors, by default it has 1024 elements.
+* At every outer iteration in SmoSolver::Solve, we select a different working set, and
+* optimize the dual coefficients for the working set.
+*
+* The vectors are selected based on the f values, which is the difference between the
+* target label and the decision function value.
+*/
+template <typename math_t>
+class WorkingSet {
+ public:
+  bool verbose = false;
+
+  //!> Workspace selection strategy, note that only FIFO is tested so far
+  bool FIFO_strategy = true;
+
+  /** Create a working set
+   * \param n_rows number of training vectors
+   * \param n_ws number of elements in the working set (default 1024)
+   */
+  WorkingSet(const cumlHandle_impl &handle, cudaStream_t stream, int n_rows = 0,
+             int n_ws = 0)
+    : handle(handle),
+      stream(stream),
+      available(handle.getDeviceAllocator(), stream),
+      available_sorted(handle.getDeviceAllocator(), stream),
+      cub_storage(handle.getDeviceAllocator(), stream),
+      f_idx(handle.getDeviceAllocator(), stream),
+      f_idx_sorted(handle.getDeviceAllocator(), stream),
+      f_sorted(handle.getDeviceAllocator(), stream),
+      idx_tmp(handle.getDeviceAllocator(), stream),
+      idx(handle.getDeviceAllocator(), stream),
+      ws_idx_sorted(handle.getDeviceAllocator(), stream),
+      ws_idx_selected(handle.getDeviceAllocator(), stream),
+      ws_idx_save(handle.getDeviceAllocator(), stream),
+      ws_priority(handle.getDeviceAllocator(), stream),
+      ws_priority_sorted(handle.getDeviceAllocator(), stream) {
+    SetSize(n_rows, n_ws);
+  }
+
+  ~WorkingSet() {
+    handle.getDeviceAllocator()->deallocate(d_num_selected, 1 * sizeof(int),
+                                            stream);
+  }
+
+  /**
+   * Set the size of the working set and allocate buffers accordingly.
+   *
+   * \param n_rows number of training vectors
+   * \param n_ws working set size (default min(1024, n_rows))
+   */
+  void SetSize(int n_rows, int n_ws = 0) {
+    if (n_ws == 0 || n_ws > n_rows) {
+      n_ws = n_rows;
+    }
+    n_ws = min(1024, n_ws);
+    this->n_ws = n_ws;
+    this->n_rows = n_rows;
+    AllocateBuffers();
+  }
+
+  /** Return the size of the working set. */
+  int GetSize() { return n_ws; }
+
+  /** Return a pointer the the working set indices */
+  int *GetIndices() { return idx.data(); }
+
+  /**
+   * Select new elements for a working set.
+   *
+   * Here we follow the working set selection strategy by Joachims [1], we
+   * select n traning instances as:
+   *   - select n/2 element of upper set, where f is largest
+   *   - select n/2 from lower set, wher f is smallest
+   *
+   * The difference compared to Joachims' strategy is that we can already have
+   * some elements selected by a different strategy, therefore we select only
+   * n = n_ws - n_already_selected.
+   *
+   * @param f optimality indicator vector, size [n_rows]
+   * @param alpha dual coefficients, size [n_rows]
+   * @param y target labels (+/- 1)
+   * @param C penalty parameter
+   * @param n_already_selected
+   *
+   * References:
+   * [1] Joachims, T. (1998). Making large-scale support vector machine learning
+   *     practical. In B. Scholkopf, C. Burges, & A. Smola (Eds.), Advances in
+   *     kernel methods: Support vector machines. Cambridge, MA: MIT Press
+   */
+
+  void SimpleSelect(math_t *f, math_t *alpha, math_t *y, math_t C,
+                    int n_already_selected = 0) {
+    // We are not using the topK kernel, because of the additional lower/upper
+    // constraint
+    int n_needed = n_ws - n_already_selected;
+
+    // Zero the priority of the elements that will be newly selected
+    CUDA_CHECK(cudaMemsetAsync(ws_priority.data() + n_already_selected, 0,
+                               n_needed * sizeof(int), stream));
+
+    cub::DeviceRadixSort::SortPairs(
+      (void *)cub_storage.data(), cub_bytes, f, f_sorted.data(), f_idx.data(),
+      f_idx_sorted.data(), n_rows, 0, (int)8 * sizeof(int), stream);
+
+    if (verbose && n_rows < 20) {
+      myPrintDevVector("idx_sorted", f_idx_sorted.data(), n_rows, std::cout);
+    }
+    // Select top n_ws/2 elements
+    bool *available = this->available.data();
+    set_upper<<<ceildiv(n_rows, TPB), TPB, 0, stream>>>(available, n_rows,
+                                                        alpha, y, C);
+    CUDA_CHECK(cudaPeekAtLastError());
+    n_already_selected +=
+      GatherAvailable(n_already_selected, n_needed / 2, true);
+
+    // Select bottom n_ws/2 elements
+    set_lower<<<ceildiv(n_rows, TPB), TPB, 0, stream>>>(available, n_rows,
+                                                        alpha, y, C);
+    CUDA_CHECK(cudaPeekAtLastError());
+    n_already_selected +=
+      GatherAvailable(n_already_selected, n_ws - n_already_selected, false);
+
+    // In case we could not find enough elements, then we just fill using the
+    // still available elements.
+    if (n_already_selected < n_ws) {
+      if (verbose)
+        std::cout << "Warning: could not fill working set, found only "
+                  << n_already_selected << " elements.\n";
+      if (verbose) std::cout << "Filling up with unused elements\n";
+      CUDA_CHECK(cudaMemset(available, 1, sizeof(bool) * n_rows));
+      n_already_selected +=
+        GatherAvailable(n_already_selected, n_ws - n_already_selected, true);
+    }
+  }
+
+  /**
+  * Select working set indices.
+  *
+  * To avoid training vectors oscillating in and out of the working set, we
+  * keep half of the previous working set, and fill new elements only to the
+  * other half.
+  *
+  * We can have a FIFO retention policy, or we can
+  * consider the time (=ws_priority) a vector already spent in the ws.
+  * References:
+  * [1] ThunderSVM
+  *
+  */
+  void Select(math_t *f, math_t *alpha, math_t *y, math_t C) {
+    if (n_ws >= n_rows) {
+      // All elements are selected, we have initialized idx to cover this case
+      return;
+    }
+    int nc = n_ws / 4;
+    int n_selected = 0;
+    if (firstcall) {
+      firstcall = false;
+    } else {
+      // keep 1/2 of the old working set
+      if (FIFO_strategy) {
+        // FIFO selection following ThunderSVM
+        // TODO: confirm this: we should shift ws_idx_save.data() by nc, so that we do not keep
+        // always the same elements there.
+        copy(idx.data(), ws_idx_save.data() + 2 * nc, 2 * nc, stream);
+        //copy(idx.data() + nc, ws_idx_save.data() + 3*nc, nc, stream);
+        n_selected = nc * 2;
+      } else {
+        // priority based selection preferring to keep newer elements in ws
+        n_selected = PrioritySelect(alpha, C, nc);
+      }
+    }
+    SimpleSelect(f, alpha, y, C, n_selected);
+    copy(ws_idx_save.data(), idx.data(), n_ws, stream);
+  }
+
+  /**
+   * Select elements from the previous working set based on their priority and
+   * dual coefficients.
+   *
+   * We sort the old working set based on their priority in ascending order,
+   * and then select nc elements from free, and then lower/upper bound vectors.
+   * For details see [2].
+   *
+   * References:
+   * [2] T Serafini, L Zanni: On the Working Set selection in grad. projection
+   *     based decomposition techniques for Support Vector Machines
+   *     DOI: 10.1080/10556780500140714
+   */
+  int PrioritySelect(math_t *alpha, math_t C, int nc) {
+    int n_selected = 0;
+
+    cub::DeviceRadixSort::SortPairs(
+      (void *)cub_storage.data(), cub_bytes, ws_priority.data(),
+      ws_priority_sorted.data(), idx.data(), ws_idx_sorted.data(), n_ws);
+
+    //Select first from free vectors (0<alpha<C)
+    n_selected += SelectPrevWs(2 * nc, n_selected, [alpha, C] HD(int idx) {
+      return 0 < alpha[idx] && alpha[idx] < C;
+    });
+
+    //then from lower bound (alpha=0)
+    n_selected += SelectPrevWs(2 * nc, n_selected,
+                               [alpha] HD(int idx) { return alpha[idx] <= 0; });
+    // and in the end from upper bound vectors (alpha=c)
+    n_selected += SelectPrevWs(
+      2 * nc, n_selected, [alpha, C] HD(int idx) { return alpha[idx] >= C; });
+    // we have now idx[0:n_selected] indices from the old working set
+    // we need to update their priority.
+    update_priority<<<ceildiv(n_selected, TPB), TPB, 0, stream>>>(
+      ws_priority.data(), n_selected, idx.data(), n_ws, ws_idx_sorted.data(),
+      ws_priority_sorted.data());
+    return n_selected;
+  }
+
+ private:
+  const cumlHandle_impl &handle;
+  cudaStream_t stream;
+
+  bool firstcall = true;
+  int n_rows = 0;
+  int n_ws = 0;
+
+  int TPB = 256;  //!< Threads per block for workspace selection kernels
+
+  // Buffers for the domain size [n_rows]
+  device_buffer<int> f_idx;  //!< Arrays used for sorting for sorting
+  device_buffer<int> f_idx_sorted;
+  device_buffer<int> idx_tmp;  //!< Temporary buffer for index manipulation
+  device_buffer<math_t> f_sorted;
+
+  device_buffer<bool> available;  //!< Flag vectors available for selection
+  device_buffer<bool> available_sorted;
+
+  // working set buffers size [n_ws]
+  device_buffer<int> idx;  //!< Indices of the worknig set
+  device_buffer<int> ws_idx_sorted;
+  device_buffer<int> ws_idx_selected;
+  device_buffer<int> ws_idx_save;
+
+  device_buffer<int> ws_priority;
+  device_buffer<int> ws_priority_sorted;
+
+  int *d_num_selected = nullptr;
+  size_t cub_bytes = 0;
+  device_buffer<char> cub_storage;
+
+  void AllocateBuffers() {
+    if (n_ws > 0) {
+      f_idx.resize(n_rows, stream);
+      f_idx_sorted.resize(n_rows, stream);
+      idx_tmp.resize(n_rows, stream);
+      f_sorted.resize(n_rows, stream);
+      available.resize(n_rows, stream);
+      available_sorted.resize(n_rows, stream);
+
+      idx.resize(n_ws, stream);  //allocate(idx, n_ws, stream);
+      ws_idx_sorted.resize(n_ws, stream);
+      ws_idx_save.resize(n_ws, stream);
+      ws_idx_selected.resize(n_ws, stream);
+      ws_priority.resize(n_ws, stream);
+      ws_priority_sorted.resize(n_ws, stream);
+
+      d_num_selected =
+        (int *)handle.getDeviceAllocator()->allocate(1 * sizeof(int), stream);
+
+      // Determine temporary device storage requirements for cub
+      size_t cub_bytes2 = 0;
+      cub::DeviceRadixSort::SortPairs(
+        NULL, cub_bytes, f_idx.data(), f_idx_sorted.data(), f_sorted.data(),
+        f_sorted.data(), n_rows, 0, 8 * sizeof(int), stream);
+      cub::DeviceSelect::If(NULL, cub_bytes2, f_idx.data(), f_idx.data(),
+                            d_num_selected, n_rows, dummy_select_op, stream);
+      cub_bytes = max(cub_bytes, cub_bytes2);
+      cub_storage.resize(cub_bytes, stream);
+      Initialize();
+    }
+  }
+
+  /**
+   * Gather available elements from the working set.
+   *
+   * We select the first (last) n_needed element from the front (end) of
+   * f_idx_sorted. We ignore the elements that are already selected, and those
+   * where this->available is false.
+   *
+   * @param n_already_selected number of element already selected (their indices
+   *   are stored in idx[0:n_already_selected])
+   * @param n_needed number of elements to be selected
+   * @param copy_front if true, then copy the elements from the front of the
+   *        selected list, otherwise copy from the end of the list
+   * @return the number of elements copied (which might be less than n_needed)
+   */
+  int GatherAvailable(int n_already_selected, int n_needed, bool copy_front) {
+    // First we update the mask to ignores already selected elements
+    bool *available = this->available.data();
+    if (n_already_selected > 0) {
+      set_unavailable<<<ceildiv(n_rows, TPB), TPB, 0, stream>>>(
+        available, n_rows, idx.data(), n_already_selected);
+      CUDA_CHECK(cudaPeekAtLastError());
+    }
+    if (verbose && n_rows < 20) {
+      myPrintDevVector("avail", available, n_rows, std::cout);
+    }
+
+    // Map the mask to the sorted indices
+    map_to_sorted<<<ceildiv(n_rows, TPB), TPB, 0, stream>>>(
+      available, n_rows, available_sorted.data(), f_idx_sorted.data());
+    CUDA_CHECK(cudaPeekAtLastError());
+    if (verbose && n_rows < 20) {
+      myPrintDevVector("avail_sorted", available_sorted.data(), n_rows,
+                       std::cout);
+    }
+
+    // Select the available elements
+    cub::DeviceSelect::Flagged((void *)cub_storage.data(), cub_bytes,
+                               f_idx_sorted.data(), available_sorted.data(),
+                               idx_tmp.data(), d_num_selected, n_rows);
+    int n_selected;
+    updateHost(&n_selected, d_num_selected, 1, stream);
+    CUDA_CHECK(cudaStreamSynchronize(stream));
+
+    // Copy to output
+    int n_copy = n_selected > n_needed ? n_needed : n_selected;
+    if (copy_front) {
+      copy(idx.data() + n_already_selected, idx_tmp.data(), n_copy, stream);
+    } else {
+      copy(idx.data() + n_already_selected,
+           idx_tmp.data() + n_selected - n_copy, n_copy, stream);
+    }
+    if (verbose && n_rows < 20) {
+      myPrintDevVector("selected", idx.data(), n_already_selected + n_copy,
+                       std::cout);
+    }
+    return n_copy;
+  }
+
+  void Initialize() {
+    range<<<ceildiv(n_rows, TPB), TPB>>>(f_idx.data(), n_rows);
+    CUDA_CHECK(cudaPeekAtLastError());
+    range<<<ceildiv(n_ws, TPB), TPB>>>(idx.data(), n_rows);
+    CUDA_CHECK(cudaPeekAtLastError());
+  }
+
+  /**
+   * Select the first n_needed elements from ws_idx_sorted where op is true.
+   *
+   * The selected elements are appended to this->idx.
+   *
+   * @param n_needed number of elements that should be selected
+   * @param n_already_selected number of already selected elements
+   * @param op selection condition
+   * @return the number of elements selected
+   */
+  template <typename select_op>
+  int SelectPrevWs(int n_needed, int n_already_selected, select_op op) {
+    n_needed -= n_already_selected;
+    if (n_needed <= 0) {
+      return 0;
+    }
+    cub::DeviceSelect::If(cub_storage.data(), cub_bytes, ws_idx_sorted.data(),
+                          ws_idx_selected.data(), d_num_selected, n_ws, op);
+    int n_selected;
+    updateHost(&n_selected, d_num_selected, 1, stream);
+    CUDA_CHECK(cudaStreamSynchronize(stream));
+    int n_copy = n_selected < n_needed ? n_selected : n_needed;
+    copy(idx.data() + n_already_selected, ws_idx_selected.data(), n_copy,
+         stream);
+    return n_copy;
+  }
+};
+
+};  // end namespace SVM
+};  // end namespace ML
