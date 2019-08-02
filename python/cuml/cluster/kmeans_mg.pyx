e5d01d5bd0b609b13928973a01b695fc7d9b79fa
Kamesh AK <a.k.kamesh001@gmail.com>
2019-08-02 14:39:54 -0700

    [kmeans] code refactor to make the API completely stateless

--- python/cuml/cluster/kmeans_mg.pyx
+++ python/cuml/cluster/kmeans_mg.pyx
@@ -37,36 +37,42 @@ from cuml.utils import get_cudf_column_ptr, get_dev_array_ptr, \
 
 from cuml.cluster import KMeans
 
-cdef extern from "cumlprims/mg/kmeans_mg.hpp" namespace "ML::kmeans" nogil:
 
+cdef extern from "cumlprims/mg/kmeans_mg.hpp" namespace "ML::kmeans::KMeansParams" nogil:
     enum InitMethod:
         KMeansPlusPlus, Random, Array
 
+cdef extern from "cumlprims/mg/kmeans_mg.hpp" namespace "ML::kmeans" nogil:
+
+    cdef struct KMeansParams:
+       int n_clusters,
+       InitMethod init
+       int max_iter,
+       double tol,
+       int verbose,
+       int seed,
+       int metric,
+       int oversampling_factor,
+       int batch_size,
+       bool inertia_check,
+
     cdef void fit_mnmg(cumlHandle& handle,
-                       int n_clusters,
-                       int metric,
-                       InitMethod init,
-                       int max_iter,
-                       double tol,
-                       int seed,
+                       KMeansParams& params,
                        const float *X,
                        int n_samples,
                        int n_features,
                        float *centroids,
-                       int verbose)
+                       float &inertia,
+                       int &n_iter)
 
     cdef void fit_mnmg(cumlHandle& handle,
-                       int n_clusters,
-                       int metric,
-                       InitMethod init,
-                       int max_iter,
-                       double tol,
-                       int seed,
+                       KMeansParams& params,
                        const double *X,
                        int n_samples,
                        int n_features,
                        double *centroids,
-                       int verbose)
+                       double &inertia,
+                       int &n_iter)
 
 
 class KMeansMG(KMeans):
@@ -108,81 +114,51 @@ class KMeansMG(KMeans):
 
         cdef cumlHandle* handle_ = <cumlHandle*><size_t>self.handle.getHandle()
 
-        if (isinstance(self.init, cudf.DataFrame)):
-            if(len(self.init) != self.n_clusters):
-                raise ValueError('The shape of the initial centers (%s) '
-                                 'does not match the number of clusters %i'
-                                 % (self.init.shape, self.n_clusters))
-            init_value = Array
-            dim_cc = self.n_clusters * self.n_cols
-            self.cluster_centers_ = cuda.device_array(dim_cc,
-                                                      dtype=self.dtype)
-            si = self.init
-            self.cluster_centers_.copy_to_device(numba_utils.row_matrix(si))
-
-        elif (isinstance(self.init, np.ndarray)):
-            if(self.init.shape[0] != self.n_clusters):
-                raise ValueError('The shape of the initial centers (%s) '
-                                 'does not match the number of clusters %i'
-                                 % (self.init.shape, self.n_clusters))
-            init_value = Array
-            self.cluster_centers_ = cuda.to_device(self.init.flatten())
-
-        elif (self.init in ['scalable-k-means++', 'k-means||']):
-            init_value = KMeansPlusPlus
+        if (self.init in ['scalable-k-means++', 'k-means||', 'random']):
             clust_cent = zeros(self.n_clusters * self.n_cols,
                                dtype=self.dtype)
             self.cluster_centers_ = cuda.to_device(clust_cent)
-
-        elif (self.init == 'random'):
-            init_value = Random
-            clust_cent = zeros(self.n_clusters * self.n_cols,
-                               dtype=self.dtype)
-            self.cluster_centers_ = cuda.to_device(clust_cent)
-
-        else:
-            raise TypeError('initialization method not supported')
 
         cdef uintptr_t cluster_centers_ptr = \
             get_dev_array_ptr(self.cluster_centers_)
-        cdef int verbose = self.verbose
+
         cdef size_t n_rows = self.n_rows
         cdef size_t n_cols = self.n_cols
-        cdef int max_iter = self.max_iter
-        cdef double tol = self.tol
-        cdef int random_state = self.random_state
-        cdef int n_clusters = self.n_clusters
+
+        cdef float inertiaf = 0;
+        cdef double inertiad = 0;
+
+        cdef KMeansParams params = self._params
+        cdef int n_iter = 0
 
         if self.dtype == np.float32:
             with nogil:
                 fit_mnmg(
                     handle_[0],
-                    <int> n_clusters,         # n_clusters
-                    <int> 0,                       # distance metric as squared L2: @todo - support other metrics # noqa: E501
-                    <InitMethod> init_value,       # init method
-                    <int> max_iter,           # max_iterations
-                    <double> tol,             # threshold
-                    <int> random_state,       # seed
-                    <float*> input_ptr,            # srcdata
-                    <size_t> n_rows,          # n_samples (rows)
-                    <size_t> n_cols,          # n_features (cols)
-                    <float*> cluster_centers_ptr,  # pred_centroids);
-                    <int> verbose)
+                    <KMeansParams> params,
+                    <const float*> input_ptr,
+                    <size_t> n_rows,
+                    <size_t> n_cols,
+                    <float*> cluster_centers_ptr,
+                    inertiaf,
+                    n_iter)
+            self.handle.sync()
+            self.inertia_ = inertiaf
+            self.n_iter_ = n_iter
         elif self.dtype == np.float64:
             with nogil:
                 fit_mnmg(
                     handle_[0],
-                    <int> n_clusters,          # n_clusters
-                    <int> 0,                        # distance metric as squared L2: @todo - support other metrics # noqa: E501
-                    <InitMethod> init_value,        # init method
-                    <int> max_iter,            # max_iterations
-                    <double> tol,              # threshold
-                    <int> random_state,        # seed
-                    <double*> input_ptr,            # srcdata
-                    <size_t> n_rows,           # n_samples (rows)
-                    <size_t> n_cols,           # n_features (cols)
-                    <double*> cluster_centers_ptr,  # pred_centroids);
-                    <int> verbose)
+                    <KMeansParams> params,
+                    <const double*> input_ptr,
+                    <size_t> n_rows,
+                    <size_t> n_cols,
+                    <double*> cluster_centers_ptr,
+                    inertiad,
+                    n_iter)
+            self.handle.sync()
+            self.inertia_ = inertiad
+            self.n_iter_ = n_iter
         else:
             raise TypeError('KMeans supports only float32 and float64 input,'
                             'but input type ' + str(self.dtype) +
