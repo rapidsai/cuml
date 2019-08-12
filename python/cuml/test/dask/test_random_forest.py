7dea830d23a0f54bc60b5baaf350b949f86461e1
John Zedlewski <jzedlewski@nvidia.com>
2019-08-11 23:52:16 -0700

    Update dask-RF tests and asserts

--- python/cuml/test/dask/test_random_forest.py
+++ python/cuml/test/dask/test_random_forest.py
@@ -13,9 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import pytest
 
 from cuml.dask.ensemble import RandomForestClassifier as cuRFC_mg
 from cuml.dask.ensemble import RandomForestRegressor as cuRFR_mg
+from cuml.dask.common import utils as dask_utils
 from sklearn.datasets import make_regression, make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, r2_score
@@ -23,14 +25,18 @@ from dask.distributed import Client
 from dask_cuda import LocalCUDACluster
 
 import dask_cudf
+import dask_cuda
 import cudf
 import numpy as np
 import pandas as pd
 
 
-def test_rf_classification():
+@pytest.mark.parametrize('n_workers', [1,4,8])
+def test_rf_classification(n_workers):
+    if dask_cuda.utils.get_n_gpus() < n_workers:
+        pytest.skip("too few GPUs")
 
-    cluster = LocalCUDACluster(threads_per_worker=1)
+    cluster = LocalCUDACluster(threads_per_worker=1, n_workers=n_workers)
     c = Client(cluster)
 
     X, y = make_classification(n_samples=10000, n_features=20,
@@ -51,14 +57,18 @@ def test_rf_classification():
 
     X_cudf = cudf.DataFrame.from_pandas(pd.DataFrame(X_train))
     X_train_df = \
-        dask_cudf.from_cudf(X_cudf, npartitions=len(workers)).persist()
+            dask_cudf.from_cudf(X_cudf, npartitions=len(workers))
 
     y_cudf = np.array(pd.DataFrame(y_train).values)
     y_cudf = y_cudf[:, 0]
     y_cudf = cudf.Series(y_cudf)
     y_train_df = \
-        dask_cudf.from_cudf(y_cudf, npartitions=len(workers)).persist()
+        dask_cudf.from_cudf(y_cudf, npartitions=len(workers))
 
+    X_train_df, y_train_df = dask_utils.persist_across_workers(c,
+                                                               [X_train_df,
+                                                                y_train_df],
+                                                               workers=workers)
     cu_rf_mg = cuRFC_mg(**cu_rf_params)
     cu_rf_mg.fit(X_train_df, y_train_df)
     cu_rf_mg_predict = cu_rf_mg.predict(X_test)
@@ -71,11 +81,15 @@ def test_rf_classification():
     cluster.close()
 
 
-def test_rf_regression():
-    cluster = LocalCUDACluster(threads_per_worker=1)
+@pytest.mark.parametrize('n_workers', [1,4,8])
+def test_rf_regression(n_workers):
+    if dask_cuda.utils.get_n_gpus() < n_workers:
+        pytest.skip("too few GPUs")
+
+    cluster = LocalCUDACluster(threads_per_worker=1, n_workers=n_workers)
     c = Client(cluster)
 
-    X, y = make_regression(n_samples=10000, n_features=20,
+    X, y = make_regression(n_samples=40000, n_features=20,
                            n_informative=10, random_state=123)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1_000)
@@ -89,13 +103,16 @@ def test_rf_regression():
 
     X_cudf = cudf.DataFrame.from_pandas(pd.DataFrame(X_train))
     X_train_df = \
-        dask_cudf.from_cudf(X_cudf, npartitions=len(workers)).persist()
+        dask_cudf.from_cudf(X_cudf, npartitions=len(workers))
 
     y_cudf = np.array(pd.DataFrame(y_train).values)
     y_cudf = y_cudf[:, 0]
     y_cudf = cudf.Series(y_cudf)
     y_train_df = \
-        dask_cudf.from_cudf(y_cudf, npartitions=len(workers)).persist()
+        dask_cudf.from_cudf(y_cudf, npartitions=len(workers))
+
+    X_train_df, y_train_df = dask_utils.persist_across_workers(
+        c, [X_train_df, y_train_df], workers=workers)
 
     cu_rf_mg = cuRFR_mg(**cu_rf_params)
     cu_rf_mg.fit(X_train_df, y_train_df)
