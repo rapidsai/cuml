c1d9b5eae4c749246ae3dbc998a8ce96ba8c27b7
Vishal Mehta <vishalm@nvidia.com>
2019-08-12 23:04:51 +0200

    compulsory depth

--- cpp/src/decisiontree/decisiontree.cu
+++ cpp/src/decisiontree/decisiontree.cu
@@ -56,8 +56,7 @@ void set_tree_params(DecisionTreeParams &params, int cfg_max_depth,
  * @param[in] params: decision tree hyper-parameters.
  */
 void validity_check(const DecisionTreeParams params) {
-  ASSERT((params.max_depth == -1) || (params.max_depth > 0),
-         "Invalid max depth %d", params.max_depth);
+  ASSERT((params.max_depth > 0), "Invalid max depth %d", params.max_depth);
   ASSERT((params.max_leaves == -1) || (params.max_leaves > 0),
          "Invalid max leaves %d", params.max_leaves);
   ASSERT((params.max_features > 0) && (params.max_features <= 1.0),
