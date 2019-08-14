08d1f1fb8a463c5bffd7c4e0355126ff6dd6774c
Dante Gama Dessavre <danteg@nvidia.com>
2019-08-14 14:56:17 -0500

    FIX ci scripts

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -31,9 +31,8 @@ export HOME=$WORKSPACE
 cd $WORKSPACE
 
 # Get latest tag and number of commits since tag
-export GIT_DESCRIBE_TAG=`git describe --tags`
+export GIT_DESCRIBE_TAG=`git describe --abbrev=0 --tags`
 export GIT_DESCRIBE_NUMBER=`git rev-list ${GIT_DESCRIBE_TAG}..HEAD --count`
-export MINOR_VERSION=`echo $GIT_DESCRIBE_TAG | grep -o -E '([0-9]+\.[0-9]+)'`
 
 ################################################################################
 # SETUP - Check environment
