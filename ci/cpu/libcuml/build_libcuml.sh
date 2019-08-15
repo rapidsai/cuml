8e961bef04b6cb7d125d6e0f68cdad388efd49d8
Dante Gama Dessavre <danteg@nvidia.com>
2019-08-15 10:42:10 -0500

    FIX different version format

--- ci/cpu/libcuml/build_libcuml.sh
+++ ci/cpu/libcuml/build_libcuml.sh
@@ -6,6 +6,8 @@ if [ "$BUILD_LIBCUML" == '1' -o "$BUILD_CUML" == '1' ]; then
   echo "Building libcuml"
   CUDA_REL=${CUDA_VERSION%.*}
 
+  conda clean -i
+
   if [ "$BUILD_ABI" == "1" ]; then
     conda build conda/recipes/libcuml --python=${PYTHON}
   else
