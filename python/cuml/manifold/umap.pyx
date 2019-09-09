b703fcc8b525fef32705b8af2b89aee1623fcdc2
Corey J. Nolet <cjnolet@gmail.com>
2019-09-09 00:40:13 -0400

    Making style checker happy

--- python/cuml/manifold/umap.pyx
+++ python/cuml/manifold/umap.pyx
@@ -326,7 +326,6 @@ class UMAP(Base):
         cdef UMAPParams* umap_params = <UMAPParams*><size_t>self.umap_params
         del umap_params
 
-
     def __setstate__(self, state):
         super(UMAP, self).__init__(handle=None, verbose=state['verbose'])
 
