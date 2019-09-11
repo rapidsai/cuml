0a7a4038d2cdb58da5a4db439a676df6e6391d31
Saloni Jain <saljain@dgx14.aselab.nvidia.com>
2019-09-10 18:22:30 -0700

    removed float64 option from rf test

--- python/cuml/test/test_random_forest.py
+++ python/cuml/test/test_random_forest.py
@@ -46,7 +46,7 @@ def stress_param(*args, **kwargs):
                          stress_param(200)])
 @pytest.mark.parametrize('n_info', [unit_param(7), quality_param(50),
                          stress_param(100)])
-@pytest.mark.parametrize('datatype', [np.float32, np.float64])
+@pytest.mark.parametrize('datatype', [np.float32])
 @pytest.mark.parametrize('split_algo', [0, 1])
 @pytest.mark.parametrize('max_depth', [1, 16])
 def test_rf_classification(datatype, split_algo,
@@ -92,13 +92,14 @@ def test_rf_classification(datatype, split_algo,
         if max_depth > 1:
             assert cu_acc >= (sk_acc - 0.07)
 
+
 @pytest.mark.parametrize('mode', [unit_param('unit'), quality_param('quality'),
                          stress_param('stress')])
 @pytest.mark.parametrize('ncols', [unit_param(10), quality_param(100),
                          stress_param(200)])
 @pytest.mark.parametrize('n_info', [unit_param(7), quality_param(50),
                          stress_param(100)])
-@pytest.mark.parametrize('datatype', [np.float32, np.float64])
+@pytest.mark.parametrize('datatype', [np.float32])
 @pytest.mark.parametrize('use_handle', [True, False])
 @pytest.mark.parametrize('split_algo', [0, 1])
 def test_rf_regression(datatype, use_handle, split_algo,
