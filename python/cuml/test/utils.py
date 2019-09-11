8a6a06870213a179d8716d7552e90fe6ae278fc9
Thejaswi Rao <rao.thejaswi@gmail.com>
2019-09-10 22:00:35 -0700

    flake8 style fixes

--- python/cuml/test/utils.py
+++ python/cuml/test/utils.py
@@ -126,7 +126,7 @@ def clusters_equal(a0, b0, n_clusters):
     return array_equal(a, b)
 
 
-def get_handle(use_handle, n_streams = 0):
+def get_handle(use_handle, n_streams=0):
     if not use_handle:
         return None, None
     h = cuml.Handle(n_streams)
