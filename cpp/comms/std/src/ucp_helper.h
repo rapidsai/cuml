52fb951ce309fb9ec7a00396fd51563f4b18008a
Corey J. Nolet <cjnolet@gmail.com>
2019-09-24 16:34:41 -0400

    Using ANY_RANK constant in irecv to mask out only tag and not sending rank

--- cpp/comms/std/src/ucp_helper.h
+++ cpp/comms/std/src/ucp_helper.h
@@ -21,6 +21,10 @@
 
 static const ucp_tag_t default_tag_mask = -1;
 
+static const ucp_tag_t any_rank_tag_mask = ((uint32_t)0x00000000 | (uint32_t)0xffffffff);
+
+static const int UCP_ANY_RANK = -1;
+
 
 /**
  * @brief Asynchronous send callback sets request to completed
@@ -79,8 +83,12 @@ struct ucx_context *ucp_irecv(ucp_worker_h worker, ucp_ep_h ep_ptr, void *buf,
                               int size, int tag, int sender_rank) {
   ucp_tag_t ucp_tag = ((uint32_t)sender_rank << 31) | (uint32_t)tag;
 
+  ucp_tag_t tag_mask = default_tag_mask;
+  if(sender_rank == UCP_ANY_RANK)
+    tag_mask = any_rank_tag_mask;
+
   struct ucx_context *ucp_request = (struct ucx_context *)ucp_tag_recv_nb(
-    worker, buf, size, ucp_dt_make_contig(1), ucp_tag, default_tag_mask,
+    worker, buf, size, ucp_dt_make_contig(1), ucp_tag, tag_mask,
     recv_handle);
 
   if (UCS_PTR_IS_ERR(ucp_request)) {
