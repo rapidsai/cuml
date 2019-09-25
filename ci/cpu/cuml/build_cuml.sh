5fc236c54431df119b6d16841fbc4f6d52de1faa
Dante Gama Dessavre <danteg@nvidia.com>
2019-09-25 16:09:37 -0500

    FIX Review feedback changes

--- ci/cpu/cuml/build_cuml.sh
+++ ci/cpu/cuml/build_cuml.sh
@@ -5,11 +5,6 @@ set -e
 if [ "$BUILD_CUML" == '1' ]; then
   echo "Building cuML"
   CUDA_REL=${CUDA_VERSION%.*}
-
-  if [ "$BUILD_ABI" == "1" ]; then
-    conda build conda/recipes/cuml --python=${PYTHON}
-  else
-    conda build conda/recipes/cuml --python=${PYTHON}
-  fi
+  conda build conda/recipes/cuml --python=${PYTHON}
 
 fi
