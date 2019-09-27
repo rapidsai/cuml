1bf1fe6aa7f90b9daa46511f4a17a50138b464dd
Daniel Han-Chen <danielhanchen@gmail.com>
2019-09-28 03:55:51 +1000

    Test all distance measures

--- cpp/test/sg/tsne_test.cu
+++ cpp/test/sg/tsne_test.cu
@@ -79,8 +79,30 @@ class TSNETest : public ::testing::Test
     CUDA_CHECK(cudaStreamSynchronize(handle.getStream()));
 
     // Test trustworthiness
-    score_bh = trustworthiness_score<float, EucUnexpandedL2Sqrt>(
+    score_bh = trustworthiness_score<float, EucExpandedL2>(
       X_d.data(), Y_d.data(), n, p, 2, 5, handle.getDeviceAllocator(), handle.getStream());
+    printf("EucExpandedL2 with score = %lf\n", score_bh);
+    
+    score_bh = trustworthiness_score<float, EucExpandedL2Sqrt >(
+      X_d.data(), Y_d.data(), n, p, 2, 5, handle.getDeviceAllocator(), handle.getStream());
+    printf("EucExpandedL2Sqrt  with score = %lf\n", score_bh);
+    
+    score_bh = trustworthiness_score<float, EucExpandedCosine  >(
+      X_d.data(), Y_d.data(), n, p, 2, 5, handle.getDeviceAllocator(), handle.getStream());
+    printf("EucExpandedCosine   with score = %lf\n", score_bh);
+    
+    score_bh = trustworthiness_score<float, EucUnexpandedL1   >(
+      X_d.data(), Y_d.data(), n, p, 2, 5, handle.getDeviceAllocator(), handle.getStream());
+    printf("EucUnexpandedL1    with score = %lf\n", score_bh);
+    
+    score_bh = trustworthiness_score<float, EucUnexpandedL2    >(
+      X_d.data(), Y_d.data(), n, p, 2, 5, handle.getDeviceAllocator(), handle.getStream());
+    printf("EucUnexpandedL2     with score = %lf\n", score_bh);
+    
+    score_bh = trustworthiness_score<float, EucUnexpandedL2Sqrt     >(
+      X_d.data(), Y_d.data(), n, p, 2, 5, handle.getDeviceAllocator(), handle.getStream());
+    printf("EucUnexpandedL2Sqrt      with score = %lf\n", score_bh);
+    
 
 
     // Test Exact TSNE
