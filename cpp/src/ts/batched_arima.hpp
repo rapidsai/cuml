b5913492e611387fd1bdd4ae925f84df37aade2a
Max Rietmann <mrietmann@nvidia.com>
2019-10-01 14:06:04 +0200

    Added missing docstrings for C++ interface

--- cpp/src/ts/batched_arima.hpp
+++ cpp/src/ts/batched_arima.hpp
@@ -21,6 +21,7 @@
 namespace ML {
 
 //! Compute the loglikelihood of the given parameter on the given time series in a batched context.
+//! @param handle cuML handle
 //! @param y series to fit: shape = (nobs, num_bathces) and expects column major data layout. Memory on Device.
 //! @param num_batches number of time series
 //! @param order ARIMA order (p: number of ar-parameters, d: difference parameter, q: number of ma-parameters)
@@ -34,14 +35,48 @@ void batched_loglike(cumlHandle& handle, double* d_y, int num_batches, int nobs,
                      std::vector<double>& loglike, double* d_vs,
                      bool trans = true);
 
-//! Compute in-sample prediction (size= (nobs - d))
+//! Batched in-sample prediction of a time-series given trend, AR, and MA parameters.
+//! @param handle cuML handle
+//! @param d_y Batched Time series to predict. Shape: (num_samples, batch size) (device)
+//! @param num_batches Total number of batched time series
+//! @param nobs Number of samples per time series (all series must be identical)
+//! @param p Number of AR parameters
+//! @param d Trend parameter
+//! @param q Number of MA parameters
+//! @param d_params Zipped trend, AR, and MA parameters [mu, ar, ma] x batches (device)
+//! @param d_vs Residual output (device)
+//! @param d_y_p Prediction output (device)
 void predict_in_sample(cumlHandle& handle, double* d_y, int num_batches,
                        int nobs, int p, int d, int q, double* d_params,
                        double* d_vs, double* d_y_p);
 
+//! Residual of in-sample prediction of a time-series given trend, AR, and MA parameters.
+//! @param handle cuML handle
+//! @param d_y Batched Time series to predict. Shape: (num_samples, batch size) (device)
+//! @param num_batches Total number of batched time series
+//! @param nobs Number of samples per time series (all series must be identical)
+//! @param p Number of AR parameters
+//! @param d Trend parameter
+//! @param q Number of MA parameters
+//! @param d_params Zipped trend, AR, and MA parameters [mu, ar, ma] x batches (device)
+//! @param d_vs Residual output (device)
 void residual(cumlHandle& handle, double* d_y, int num_batches, int nobs, int p,
               int d, int q, double* d_params, double* d_vs, bool trans);
 
+//! Batched forecast of a time-series given trend, AR, and MA parameters.
+//! @param handle cuML handle
+//! @param num_steps The number of steps to forecast
+//! @param p Number of AR parameters
+//! @param d Trend parameter
+//! @param q Number of MA parameters
+//! @param batch_size Total number of batched time series
+//! @param nobs Number of samples per time series (all series must be identical)
+//! @param d_y Batched Time series to predict. Shape: (num_samples, batch size) (device)
+//! @param d_y_diff Diffed (e.g., np.diff) of the batched Time series to
+//! predict. Shape: (num_samples, batch size) (device)
+//! @param d_vs Residual input (device)
+//! @param d_params Zipped trend, AR, and MA parameters [mu, ar, ma] x batches (device)
+//! @param d_y_fc Forecast output (device)
 void forecast(cumlHandle& handle, int num_steps, int p, int d, int q,
               int batch_size, int nobs, double* d_y, double* d_y_diff,
               double* d_vs, double* d_params, double* d_y_fc);
