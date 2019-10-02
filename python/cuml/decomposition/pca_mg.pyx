fa23a1b11df4b5fc822c76085a2a29e156f8adbe
Corey J. Nolet <cjnolet@gmail.com>
2019-10-02 17:19:34 -0400

    Finished filling in initial fit() function for pca_mg. Should be ready for end-to-end test.

--- python/cuml/decomposition/pca_mg.pyx
+++ python/cuml/decomposition/pca_mg.pyx
@@ -40,11 +40,11 @@ from cuml.decomposition import PCA
 
 cdef extern from "cumlprims/opg/matrix/data.hpp" namespace "MLCommon::Matrix":
 
-    cdef cppclass floatData:
+    cdef cppclass floatData_t:
         float *ptr
         size_t totalSize
 
-    cdef cppclass doubleData:
+    cdef cppclass doubleData_t:
         double *ptr
         size_t totalSize
 
@@ -56,9 +56,10 @@ cdef extern from "cumlprims/opg/matrix/part_descriptor.hpp" namespace "MLCommon:
 
 cdef extern from "cumlprims/opg/pca.hpp" namespace "ML::PCA::opg":
 
-    cdef void fit(cumlHandle& handle,
+    cdef void fitF(cumlHandle& handle,
                   RankSizePair **input,
-                  floatData **rank_sizes,
+                  size_t n_parts,
+                  floatData_t **rank_sizes,
                   float *components,
                   float *explained_var,
                   float *explained_var_ratio,
@@ -67,10 +68,10 @@ cdef extern from "cumlprims/opg/pca.hpp" namespace "ML::PCA::opg":
                   float *noise_vars,
                   paramsPCA prms) except +
 
-    cdef void fit(cumlHandle& handle,
+    cdef void fitD(cumlHandle& handle,
                   RankSizePair **input,
-                  doubleData **rank_sizes,
-                  double *input,
+                  size_t n_parts,
+                  doubleData_t **rank_sizes,
                   double *components,
                   double *explained_var,
                   double *explained_var_ratio,
@@ -85,7 +86,7 @@ class PCAMG(PCA):
     def __init__(self, **kwargs):
         super(PCAMG, self).__init__(**kwargs)
 
-    def fit(self, local_x, M, N, partsToRanks):
+    def fit(self, X, M, N, partsToRanks, _transform=False):
         """
         Fit function for PCA MG. This not meant to be used as
         part of the public API.
@@ -99,18 +100,20 @@ class PCAMG(PCA):
         # TODO: Create outputs, convert X to **Data, use M, N to build paramsPCA, & partsToRanks to build **RankSizePair
         arr_interfaces = []
         cdef uintptr_t input_ptr
-        for idx, arr in enumerate(local_x):
+        for idx, arr in enumerate(X):
             X_m, input_ptr, n_rows, n_cols, dtype = \
-                input_to_dev_array(local_x, check_dtype=[np.float32, np.float64])
+                input_to_dev_array(X, check_dtype=[np.float32, np.float64])
             arr_interfaces[idx] = {"obj": X_m, "data": input_ptr, "shape": (n_rows, n_cols)}
 
-        cdef floatData **data = <floatData**> malloc(sizeof(floatData*) * len(local_x))
+        n_parts = len(X)
+        cdef floatData_t **dataF = <floatData_t**> malloc(sizeof(floatData_t*) * len(X))
+        cdef doubleData_t **dataD = <doubleData_t**> malloc(sizeof(doubleData_t*) * len(X))
         for x_i in range(len(arr_interfaces)):
-            x = local_x[x_i]
+            x = X[x_i]
             input_ptr = x["data"]
-            data[x_i] = <floatData*>malloc(sizeof(floatData))
-            data[x_i].ptr = <float*>input_ptr
-            data[x_i].totalSize = <size_t>x["shape"][0]
+            dataF[x_i] = <floatData_t*>malloc(sizeof(floatData_t))
+            dataF[x_i].ptr = <float*>input_ptr
+            dataF[x_i].totalSize = <size_t>x["shape"][0]
 
         cdef RankSizePair **rankSizePair = <RankSizePair**>malloc(sizeof(RankSizePair**)
                                                                   * len(partsToRanks))
@@ -120,6 +123,82 @@ class PCAMG(PCA):
             rankSizePair[idx].rank = <int>rank
             rankSizePair[idx].size = <size_t>size
 
+        cpdef paramsPCA params
+        params.n_components = self.n_components
+        params.n_rows = M
+        params.n_cols = N
+        params.whiten = self.whiten
+        params.n_iterations = self.iterated_power
+        params.tol = self.tol
+        params.algorithm = self.c_algorithm
 
+        if self.n_components > self.n_cols:
+            raise ValueError('Number of components should not be greater than'
+                             'the number of columns in the data')
+
+        self._initialize_arrays(params.n_components,
+                                params.n_rows, params.n_cols)
+
+        cdef uintptr_t comp_ptr = get_dev_array_ptr(self.components_ary)
+
+        cdef uintptr_t explained_var_ptr = \
+            get_cudf_column_ptr(self.explained_variance_)
+
+        cdef uintptr_t explained_var_ratio_ptr = \
+            get_cudf_column_ptr(self.explained_variance_ratio_)
+
+        cdef uintptr_t singular_vals_ptr = \
+            get_cudf_column_ptr(self.singular_values_)
+
+        cdef uintptr_t mean_ptr = get_cudf_column_ptr(self.mean_)
+
+        cdef uintptr_t noise_vars_ptr = \
+            get_cudf_column_ptr(self.noise_variance_)
+
+        cdef cumlHandle* handle_ = <cumlHandle*><size_t>self.handle.getHandle()
+
+        if self.dtype == np.float32:
+            fitF(handle_[0],
+                <RankSizePair**>rankSizePair,
+                <size_t> n_parts,
+                <floatData_t**>dataF,
+                <float*> comp_ptr,
+                <float*> explained_var_ptr,
+                <float*> explained_var_ratio_ptr,
+                <float*> singular_vals_ptr,
+                <float*> mean_ptr,
+                <float*> noise_vars_ptr,
+                params)
+        else:
+            fitD(handle_[0],
+                <RankSizePair**>rankSizePair,
+                <size_t> n_parts,
+                <doubleData_t**>dataF,
+                <double*> comp_ptr,
+                <double*> explained_var_ptr,
+                <double*> explained_var_ratio_ptr,
+                <double*> singular_vals_ptr,
+                <double*> mean_ptr,
+                <double*> noise_vars_ptr,
+                params)
+
+        # make sure the previously scheduled gpu tasks are complete before the
+        # following transfers start
+        self.handle.sync()
+
+        # Keeping the additional dataframe components during cuml 0.8.
+        # See github issue #749
+        self.components_ = cudf.DataFrame()
+        for i in range(0, params.n_cols):
+            n_c = params.n_components
+            self.components_[str(i)] = self.components_ary[i*n_c:(i+1)*n_c]
+
+        if (isinstance(X, cudf.DataFrame)):
+            del(X_m)
+
+        if not _transform:
+            del(self.trans_input_)
+
+        return self
 
 
