2bc9243e7b21f8491009dd3d07633c41291122c0
Daniel Han-Chen <danielhanchen@gmail.com>
2019-10-03 03:39:08 +1000

    Update runner.h

--- cpp/src/dbscan/runner.h
+++ cpp/src/dbscan/runner.h
@@ -43,6 +43,8 @@ static const int TPB = 256;
 template <typename Index_ = int>
 __global__ void relabelForSkl(Index_* labels, Index_ N, Index_ MAX_LABEL) {
   Index_ tid = threadIdx.x + blockDim.x * blockIdx.x;
+  if (tid >= N) return;
+  
   if (labels[tid] == MAX_LABEL)
     labels[tid] = -1;
   else if (tid < N)
