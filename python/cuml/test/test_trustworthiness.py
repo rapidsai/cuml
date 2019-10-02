4dd2dc406a589bba5a6fb880eae7c1e19841a9de
Daniel Han-Chen <danielhanchen@gmail.com>
2019-10-03 03:36:20 +1000

    Update test_trustworthiness.py

--- python/cuml/test/test_trustworthiness.py
+++ python/cuml/test/test_trustworthiness.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import faulthandler
-faulthandler.enable()
-
 import pytest
 from sklearn.manifold.t_sne import trustworthiness as sklearn_trustworthiness
 from cuml.metrics import trustworthiness as cuml_trustworthiness
@@ -32,17 +29,14 @@ import numpy as np
 @pytest.mark.parametrize('n_components', [2, 8])
 def test_trustworthiness(input_type, n_samples, n_features, n_components):
     centers = round(n_samples*0.4)
-    print("Making blobs")
     X, y = make_blobs(n_samples=n_samples, centers=centers,
                       n_features=n_features)
-    print("Blobs done")
+
     X_embedded = \
         UMAP(n_components=n_components).fit_transform(X)
-    print("UMAP done")
     X = X.astype(np.float32)
     X_embedded = X_embedded.astype(np.float32)
-    print("As type done")
-    
+
     if input_type == 'dataframe':
         gdf = cudf.DataFrame()
         for i in range(X.shape[1]):
@@ -52,15 +46,12 @@ def test_trustworthiness(input_type, n_samples, n_features, n_components):
         for i in range(X_embedded.shape[1]):
             gdf_embedded[str(i)] = np.asarray(X_embedded[:, i],
                                               dtype=np.float32)
-        
-        print("cudf done")
+
         score = cuml_trustworthiness(gdf, gdf_embedded)
     else:
         score = cuml_trustworthiness(X, X_embedded)
-    
-    print("cuml trust done")
+
     sk_score = sklearn_trustworthiness(X, X_embedded)
-    print("sk trust done")
 
     eps = 0.001
     assert (sk_score * (1 - eps) <= score and
