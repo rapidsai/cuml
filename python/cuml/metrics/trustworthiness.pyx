8a7e6af86b4af560385f6bfa1f1bec4f8011fbb2
Daniel Han-Chen <danielhanchen@gmail.com>
2019-10-04 04:31:44 +1000

    Update trustworthiness.pyx

--- python/cuml/metrics/trustworthiness.pyx
+++ python/cuml/metrics/trustworthiness.pyx
@@ -44,7 +44,8 @@ cdef extern from "metrics/trustworthiness_c.h" namespace "ML::Metrics":
                                                        T* X_embedded,
                                                        int n, int m,
                                                        int d,
-                                                       int n_neighbors) \
+                                                       int n_neighbors,
+                                                       int skip) \
         except +
 
 
@@ -122,7 +123,8 @@ def trustworthiness(X, X_embedded, handle=None, n_neighbors=5,
                                                       n_samples,
                                                       n_features,
                                                       n_components,
-                                                      n_neighbors)
+                                                      n_neighbors,
+                                                      1)
         del X_m
         del X_m2
     else:
