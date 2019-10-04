a663433f7e497a60cc5765722d929674b067831a
Corey J. Nolet <cjnolet@gmail.com>
2019-10-03 21:43:44 -0400

    Adding try/finally blocks to make sure dask resources get freed in tests that throw exceptions.
    Computing correct batch size for make_blobs even when it doesn't distribute evenly

--- python/cuml/test/dask/test_kmeans.py
+++ python/cuml/test/dask/test_kmeans.py
@@ -34,52 +34,55 @@ def test_end_to_end(nrows, ncols, nclusters, n_parts, client=None):
         cluster = LocalCUDACluster(threads_per_worker=1)
         client = Client(cluster)
 
-    from cuml.dask.cluster import KMeans as cumlKMeans
+    try:
+        from cuml.dask.cluster import KMeans as cumlKMeans
 
-    from cuml.dask.datasets import make_blobs
+        from cuml.dask.datasets import make_blobs
 
-    X_cudf, y = make_blobs(nrows, ncols, nclusters, n_parts,
-                           cluster_std=0.01, verbose=True,
-                           random_state=10)
+        X_cudf, y = make_blobs(nrows, ncols, nclusters, n_parts,
+                               cluster_std=0.01, verbose=True,
+                               random_state=10)
 
-    wait(X_cudf)
+        wait(X_cudf)
 
-    cumlModel = cumlKMeans(verbose=0, init="k-means||", n_clusters=nclusters,
-                           random_state=10)
+        cumlModel = cumlKMeans(verbose=0, init="k-means||", n_clusters=nclusters,
+                               random_state=10)
 
-    cumlModel.fit(X_cudf)
+        cumlModel.fit(X_cudf)
 
-    cumlLabels = cumlModel.predict(X_cudf)
+        cumlLabels = cumlModel.predict(X_cudf)
 
-    n_workers = len(list(client.has_what().keys()))
+        n_workers = len(list(client.has_what().keys()))
 
-    # Verifying we are grouping partitions. This should be changed soon.
-    if n_parts is not None and n_parts < n_workers:
-        assert cumlLabels.npartitions == n_parts
-    else:
-        assert cumlLabels.npartitions == n_workers
+        # Verifying we are grouping partitions. This should be changed soon.
+        if n_parts is not None and n_parts < n_workers:
+            assert cumlLabels.npartitions == n_parts
+        else:
+            assert cumlLabels.npartitions == n_workers
 
-    from sklearn.metrics import adjusted_rand_score
+        from sklearn.metrics import adjusted_rand_score
 
-    cumlPred = cumlLabels.compute().to_pandas().values
+        cumlPred = cumlLabels.compute().to_pandas().values
 
-    assert cumlPred.shape[0] == nrows
-    assert np.max(cumlPred) == nclusters-1
-    assert np.min(cumlPred) == 0
+        assert cumlPred.shape[0] == nrows
+        assert np.max(cumlPred) == nclusters-1
+        assert np.min(cumlPred) == 0
 
-    labels = y.compute().to_pandas().values
+        labels = y.compute().to_pandas().values
 
-    score = adjusted_rand_score(labels.reshape(labels.shape[0]), cumlPred)
+        score = adjusted_rand_score(labels.reshape(labels.shape[0]), cumlPred)
 
-    if owns_cluster:
-        client.close()
-        cluster.close()
+        assert 1.0 == score
 
-    assert 1.0 == score
+    finally:
+        if owns_cluster:
+            client.close()
+            cluster.close()
+
 
 
 @pytest.mark.mg
-@pytest.mark.parametrize("nrows", [100, 500])
+@pytest.mark.parametrize("nrows", [5000, 10000])
 @pytest.mark.parametrize("ncols", [10, 30, 50])
 @pytest.mark.parametrize("nclusters", [1, 5, 10, 100])
 @pytest.mark.parametrize("n_parts", [None, 5])
@@ -91,34 +94,37 @@ def test_transform(nrows, ncols, nclusters, n_parts, client=None):
         cluster = LocalCUDACluster(threads_per_worker=1)
         client = Client(cluster)
 
-    from cuml.dask.cluster import KMeans as cumlKMeans
+    try:
 
-    from cuml.dask.datasets import make_blobs
+        from cuml.dask.cluster import KMeans as cumlKMeans
 
-    X_cudf, y = make_blobs(nrows, ncols, nclusters, n_parts,
-                           cluster_std=0.01, verbose=True,
-                           random_state=10)
+        from cuml.dask.datasets import make_blobs
 
-    wait(X_cudf)
+        X_cudf, y = make_blobs(nrows, ncols, nclusters, n_parts,
+                               cluster_std=0.01, verbose=True,
+                               random_state=10)
 
-    cumlModel = cumlKMeans(verbose=0, init="k-means||", n_clusters=nclusters,
-                           random_state=10)
+        wait(X_cudf)
 
-    cumlModel.fit(X_cudf)
+        cumlModel = cumlKMeans(verbose=0, init="k-means||", n_clusters=nclusters,
+                               random_state=10)
 
-    labels = y.compute().to_pandas().values
-    labels = labels.reshape(labels.shape[0])
+        cumlModel.fit(X_cudf)
 
-    xformed = cumlModel.transform(X_cudf).compute()
+        labels = y.compute().to_pandas().values
+        labels = labels.reshape(labels.shape[0])
 
-    assert xformed.shape == (nrows, nclusters)
+        xformed = cumlModel.transform(X_cudf).compute()
 
-    # The argmin of the transformed values should be equal to the labels
-    xformed_labels = np.argmin(xformed.to_pandas().to_numpy(), axis=1)
+        assert xformed.shape == (nrows, nclusters)
 
-    from sklearn.metrics import adjusted_rand_score
-    assert adjusted_rand_score(labels, xformed_labels)
+        # The argmin of the transformed values should be equal to the labels
+        xformed_labels = np.argmin(xformed.to_pandas().to_numpy(), axis=1)
 
-    if owns_cluster:
-        client.close()
-        cluster.close()
+        from sklearn.metrics import adjusted_rand_score
+        assert adjusted_rand_score(labels, xformed_labels)
+
+    finally:
+        if owns_cluster:
+            client.close()
+            cluster.close()
