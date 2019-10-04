77dab72f09dce3af26813ec26ccf041b2ad22cc4
Dante Gama Dessavre <danteg@nvidia.com>
2019-10-04 15:15:05 -0500

    FEA git cloning and CUML_BUILD_PATH functionality in setup.py

--- python/setup.py
+++ python/setup.py
@@ -21,11 +21,16 @@ import os
 import versioneer
 from distutils.sysconfig import get_python_lib
 import sys
+import subprocess
 
 install_requires = [
-    'numpy',
+    'numba',
     'cython'
 ]
+
+
+##############################################################################
+# - Dependencies include and lib folder setup --------------------------------
 
 cuda_include_dir = '/usr/local/cuda/include'
 cuda_lib_dir = "/usr/local/cuda/lib"
@@ -35,45 +40,104 @@ if os.environ.get('CUDA_HOME', False):
     cuda_include_dir = os.path.join(os.environ.get('CUDA_HOME'), 'include')
 
 
-rmm_include_dir = '/include'
-rmm_lib_dir = '/lib'
-
-# including cumlprims dirs, in case rmm paths change in the future
-cumlprims_include_dir = '/include'
-cumlprims_lib_dir = '/lib'
+conda_lib_dir = os.path.normpath(sys.prefix) + '/lib'
+conda_include_dir = os.path.normpath(sys.prefix) + '/include'
 
 if os.environ.get('CONDA_PREFIX', None):
     conda_prefix = os.environ.get('CONDA_PREFIX')
-    rmm_include_dir = conda_prefix + rmm_include_dir
-    rmm_lib_dir = conda_prefix + rmm_lib_dir
+    conda_include_dir = conda_prefix + '/include'
+    conda_lib_dir = conda_prefix + '/lib'
 
-    cumlprims_include_dir = conda_prefix + cumlprims_include_dir
-    cumlprims_lib_dir = conda_prefix + cumlprims_lib_dir
 
-exc_list = []
+##############################################################################
+# - Subrepo checking and cloning ---------------------------------------------
 
-libs = ['cuda', 'cuml++', 'cumlcomms', 'nccl', 'rmm']
+
+def clone_repo(name, GIT_REPOSITORY, GIT_TAG):
+    """
+    Function to clone repos in case they were not cloned by cmake.
+    Variables are named identical to the cmake counterparts for clarity,
+    in spite of not being very pythonic.
+    """
+    subprocess.check_call(['rm', '-rf', 'external'])
+    subprocess.check_call(['git', 'clone',
+                           GIT_REPOSITORY,
+                           'external/' + name])
+    wd = os.getcwd()
+    os.chdir("external/" + name)
+    subprocess.check_call(['git', 'checkout',
+                          GIT_TAG])
+    os.chdir(wd)
+
+
+# This should match their equivalent repos and tags of the CMakeLists that was
+# used to build libcuml++
+if "--clonedeps" in sys.argv:
+    clone_repo(name='treelite',
+               GIT_REPOSITORY='https://github.com/dmlc/treelite.git',
+               GIT_TAG='600afd55d1fa9bb94fc88fd3a3043cb2d5b20651')
+    treelite_path = 'external/treelite'
+
+    clone_repo(name='cub',
+               GIT_REPOSITORY='https://github.com/NVlabs/cub.git',
+               GIT_TAG='v1.8.0')
+    cub_path = 'external/cub'
+
+    clone_repo(name='cutlass',
+               GIT_REPOSITORY='https://github.com/NVIDIA/cutlass.git',
+               GIT_TAG='v1.0.1')
+    cutlass_path = 'external/cutlass'
+
+    sys.argv.remove('--clonedeps')
+else:
+
+    if os.environ.get('CUML_BUILD_PATH', False):
+        libcuml_path = '../' + os.environ.get('CUML_BUILD_PATH')
+    else:
+        libcuml_path = '../cpp/build/'
+
+    if not os.path.exists(libcuml_path):
+        raise RuntimeError("Third party repositories have not been found. \
+                           Use the --clondepes option to have setup.py clone \
+                           them automatically, or set the environment \
+                           variable CUML_BUILD_PATH, containing the relative \
+                           path of the root of the repository to the folder \
+                           where libcuml++ was built.")
+    else:
+        treelite_path = libcuml_path + 'treelite/src/treelite'
+        cub_path = libcuml_path + 'cub/src/cub'
+        cutlass_path = 'cutlass/src/cutlass'
+
+
+##############################################################################
+# - Cython extensions build and parameters -----------------------------------
+
+libs = ['cuda',
+        'cuml++',
+        'cumlcomms',
+        'nccl',
+        'rmm']
 
 include_dirs = ['../cpp/src',
                 '../cpp/external',
                 '../cpp/src_prims',
-                '../thirdparty/cutlass',
-                '../thirdparty/cub',
-                '../thirdparty/treelite/include',
+                cutlass_path,
+                cub_path,
+                treelite_path + '/include',
                 '../cpp/comms/std/src',
                 '../cpp/comms/std/include',
                 cuda_include_dir,
-                rmm_include_dir,
-                cumlprims_include_dir]
+                conda_include_dir]
 
-if "--multigpu" not in sys.argv:
+# Exclude multigpu components that use libcumlprims if --singlegpu is ued
+exc_list = []
+if "--singlegpu" in sys.argv:
     exc_list.append('cuml/linear_model/linear_regression_mg.pyx')
     exc_list.append('cuml/decomposition/tsvd_mg.pyx')
     exc_list.append('cuml/cluster/kmeans_mg.pyx')
+    sys.argv.remove('--singlegpu')
 else:
     libs.append('cumlprims')
-    sys.argv.remove('--multigpu')
-
 
 extensions = [
     Extension("*",
@@ -81,12 +145,15 @@ extensions = [
               include_dirs=include_dirs,
               library_dirs=[get_python_lib()],
               runtime_library_dirs=[cuda_lib_dir,
-                                    rmm_lib_dir,
-                                    cumlprims_lib_dir],
+                                    conda_lib_dir],
               libraries=libs,
               language='c++',
               extra_compile_args=['-std=c++11'])
 ]
+
+
+##############################################################################
+# - Python package generation ------------------------------------------------
 
 setup(name='cuml',
       description="cuML - RAPIDS ML Algorithms",
