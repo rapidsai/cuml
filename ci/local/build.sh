6ddb755f32e019fd9808c183f6ffdda05cac222f
John Zedlewski <jzedlewski@nvidia.com>
2019-10-09 18:07:11 -0700

    Shrink impact of distance optimization chagne

--- ci/local/build.sh
+++ ci/local/build.sh
@@ -116,7 +116,7 @@ fi
 
 # Run the generated build script in a container
 docker pull "${DOCKER_IMAGE}"
-docker run --runtime=nvidia --rm -it -e NVIDIA_VISIBLE_DEVICES="${NVIDIA_VISIBLE_DEVICES}" \
+docker run --gpus 1 --rm -it -e NVIDIA_VISIBLE_DEVICES="${NVIDIA_VISIBLE_DEVICES}" \
        --user "$(id -u)":"$(id -g)" \
        -v "${REPO_PATH}:${REPO_PATH_IN_CONTAINER}" \
        -v "${CPP_CONTAINER_BUILD_DIR}:${CPP_BUILD_DIR_IN_CONTAINER}" \
