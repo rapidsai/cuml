1ce00b6c64c8315e77654df64c61c3106bc83545
Corey J. Nolet <cjnolet@gmail.com>
2019-10-18 10:50:13 -0400

    Initial Dask kneighbors impl for NearestNeighbors

--- python/cuml/dask/neighbors/nearest_neighbors.py
+++ python/cuml/dask/neighbors/nearest_neighbors.py
@@ -19,6 +19,10 @@ from cuml.dask.common.comms import worker_state, CommsContext
 from dask.distributed import wait
 import numpy as np
 
+from collections import OrderedDict
+
+from functools import reduce
+
 from uuid import uuid1
 
 import cudf
@@ -38,7 +42,6 @@ class NearestNeighbors(object):
         :param X : dask_cudf.Dataframe
         :return : NearestNeighbors model
         """
-
         self.X = X
 
         return self
@@ -63,5 +66,82 @@ class NearestNeighbors(object):
         if self.n_neighbors is not None and k is None:
             k = self.n_neighbors
 
+        index_futures = self.client.sync(extract_ddf_partitions, self.X, agg=False)
+        query_futures = self.client.sync(extract_ddf_partitions, X, agg=False)
 
+        index_worker_to_parts = OrderedDict()
+        for w, p in index_futures:
+            if w not in index_worker_to_parts:
+                index_worker_to_parts[w] = []
+                index_worker_to_parts[w].append(p)
 
+        query_worker_to_parts = OrderedDict()
+        for w, p in query_futures:
+            if w not in query_worker_to_parts:
+                query_worker_to_parts[w] = []
+                query_worker_to_parts[w].append(p)
+
+        workers = set(map(lambda x: x[0], index_futures))
+        workers.extend(list(map(lambda x: x[0], query_futures)))
+
+        comms = CommsContext(comms_p2p=False)
+        comms.init(workers=workers)
+
+        worker_info = comms.worker_info(comms.worker_addresses)
+
+        key = uuid1()
+        idx_partsToRanks = [(worker_info[wf[0]]["r"], self.client.submit(
+            NearestNeighbors._func_get_size,
+            wf[1],
+            workers=[wf[0]],
+            key="%s-%s" % (key, idx)).result())
+            for idx, wf in enumerate(index_worker_to_parts)]
+
+        query_partsToRanks = [(worker_info[wf[0]]["r"], self.client.submit(
+            NearestNeighbors._func_get_size,
+            wf[1],
+            workers=[wf[0]],
+            key="%s-%s" % (key, idx)).result())
+            for idx, wf in enumerate(query_futures)]
+
+        N = X.shape[1]
+        idx_M = reduce(lambda a,b: a+b, map(lambda x: x[1], idx_partsToRanks))
+        query_M = reduce(lambda a,b: a+b, map(lambda x: x[1], query_partsToRanks))
+
+        key = uuid1()
+        nn_models = [(worker, self.client.submit(
+            NearestNeighbors._func_create_model,
+            comms.sessionId,
+            **self.kwargs,
+            workers=[worker],
+            key="%s-%s" % (key, idx)))
+            for idx, worker in enumerate(workers)]
+
+        print(str(nn_models))
+
+        key = uuid1()
+        nn_fit = [self.client.submit(
+            NearestNeighbors._func_kneighbors,
+            index_worker_to_parts[worker] if worker in index_worker_to_parts else [],
+            idx_M,
+            N,
+            idx_partsToRanks,
+            query_worker_to_parts[worker] if worker in query_worker_to_parts else [],
+            query_M,
+            query_partsToRanks,
+            worker_info[worker]["r"],
+            key="%s-%s" % (key, idx),
+            workers=[worker])
+            for idx, worker in enumerate(workers)]
+
+        wait(nn_fit)
+
+        print(str([f.exception() for f in nn_fit]))
+
+        comms.destroy()
+
+        self.local_model = self.client.submit(NearestNeighbors._func_get_first,
+                                              nn_models[0][1]).result()
+
+        # TODO: Need to build output DFs for `output_i` and `output_d`
+        
