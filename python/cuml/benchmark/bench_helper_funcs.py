c64a8f7baf1b5a700f7cf7b570a66cc54e6b8854
Rui Lan <rlan@nvidia.com>
2019-10-23 14:47:08 -0700

    WIP: add new benchmark support for FIL.

--- python/cuml/benchmark/bench_helper_funcs.py
+++ python/cuml/benchmark/bench_helper_funcs.py
@@ -26,3 +26,27 @@ def fit(m, x, y=None):
 
 def fit_transform(m, x):
     m.fit_transform(x)
+
+
+def predict(m, x, y=None):
+    m.predict(x) if y is None else m.predict(x, y)
+
+
+def fil_classification_set_up(m, data, arg={}):
+    from cuml.utils.import_utils import has_xgboost
+    if has_xgboost():
+        import xgboost as xgb
+    else:
+        raise ImportError("No XGBoost package found which is required for benchmarking FIL")
+    import os 
+
+    dtrain = xgb.DMatrix(data[0], label=data[1])
+    params = {"silent": 1, "eval_metric": "error", "objective": "binary:logistic"}
+    params.update(arg)
+    tmp_path = "./"
+    model_path = os.path.join(tmp_path, 'xgb_class.model')
+    bst = xgb.train(params, dtrain, arg["num_rounds"])
+    bst.save_model(model_path)
+
+    obj = m.load(model_path, algo=arg["algo"], output_class=arg["output_class"], threshold=arg["threshold"])
+    return obj
