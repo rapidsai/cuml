95c41c8a1c530612959192451553b7ae6470f35f
Rui Lan <rlan@nvidia.com>
2019-10-29 10:10:39 -0700

    Save trained models for later use.

--- python/cuml/benchmark/bench_helper_funcs.py
+++ python/cuml/benchmark/bench_helper_funcs.py
@@ -40,14 +40,26 @@ def _fil_classification_setup(m, data, arg={}):
         raise ImportError("No XGBoost package found which is required for benchmarking FIL")
     import os 
 
+    tmp_path = "./fil_models"
+    if not os.path.exists(tmp_path):
+        os.makedirs(tmp_path)
+
     # use maximum 1e6 rows to train the model 
     train_size = min(data[0].shape[0], 1000000)
     dtrain = xgb.DMatrix(data[0][:train_size, :], label=data[1][:train_size])
     params = {"silent": 1, "eval_metric": "error", "objective": "binary:logistic"}
     params.update(arg)
-    tmp_path = "./"
-    model_path = os.path.join(tmp_path, 'xgb_class.model')
-    bst = xgb.train(params, dtrain, arg["num_rounds"])
-    bst.save_model(model_path)
 
+    max_depth = arg["max_depth"]
+    num_rounds = arg["num_rounds"]
+    n_features = data[0].shape[1]
+    model_name = "xgb_" + str(max_depth) + "_" + str(num_rounds) + "_" + str(n_features) + "_" + str(train_size) + ".model"
+    model_path = os.path.join(tmp_path, model_name)
+
+    from pathlib import Path
+    if Path(model_path).is_file():
+        return m.load(model_path, algo=arg["algo"], output_class=arg["output_class"], threshold=arg["threshold"])
+
+    bst = xgb.train(params, dtrain, num_rounds)
+    bst.save_model(model_path)
     return m.load(model_path, algo=arg["algo"], output_class=arg["output_class"], threshold=arg["threshold"])
