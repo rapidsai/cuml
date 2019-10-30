8b194320f07e461f616438f6ab2738ca5f10d9d8
Dante Gama Dessavre <danteg@nvidia.com>
2019-10-30 13:58:34 -0500

    FIX Set required cupy to <7.0

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -43,7 +43,7 @@ nvidia-smi
 logger "Activate conda env..."
 source activate gdf
 conda install -c conda-forge -c rapidsai -c rapidsai-nightly -c rapidsai/label/xgboost -c nvidia \
-      "cupy>=6.5" \
+      "cupy>=6.5,<7.0" \
       "cudatoolkit=${CUDA_REL}" \
       "cudf=${MINOR_VERSION}" \
       "rmm=${MINOR_VERSION}" \
