1bc061752d48cced60c8a5bb4dff2eafdfeccdf1
Rui Lan <rlan@nvidia.com>
2019-10-31 13:08:24 -0700

    Address comments

--- python/cuml/benchmark/bench_helper_funcs.py
+++ python/cuml/benchmark/bench_helper_funcs.py
@@ -13,7 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+import os
+import tempfile
 
 def fit_kneighbors(m, x):
     m.fit(x)
@@ -28,37 +29,30 @@ def fit_transform(m, x):
     m.fit_transform(x)
 
 
-def predict(m, x, y=None):
-    m.predict(x) if y is None else m.predict(x, y)
+def predict(m, x):
+    m.predict(x)
 
 
-def _fil_classification_setup(m, data, arg={}):
+def _build_fil_classifier(m, data, arg={}):
+    """Setup function for FIL classification benchmarking"""
     from cuml.utils.import_utils import has_xgboost
     if has_xgboost():
         import xgboost as xgb
     else:
         raise ImportError("No XGBoost package found which is required for benchmarking FIL")
-    import os 
-
-    tmp_path = "./fil_models"
-    if not os.path.exists(tmp_path):
-        os.makedirs(tmp_path)
 
     # use maximum 1e6 rows to train the model 
     train_size = min(data[0].shape[0], 1000000)
     dtrain = xgb.DMatrix(data[0][:train_size, :], label=data[1][:train_size])
     params = {"silent": 1, "eval_metric": "error", "objective": "binary:logistic"}
     params.update(arg)
-
     max_depth = arg["max_depth"]
     num_rounds = arg["num_rounds"]
     n_features = data[0].shape[1]
-    model_name = "xgb_" + str(max_depth) + "_" + str(num_rounds) + "_" + str(n_features) + "_" + str(train_size) + ".model"
-    model_path = os.path.join(tmp_path, model_name)
 
-    from pathlib import Path
-    if Path(model_path).is_file():
-        return m.load(model_path, algo=arg["algo"], output_class=arg["output_class"], threshold=arg["threshold"])
+    tmpdir = tempfile.mkdtemp()
+    model_name = f"xgb_{max_depth}_{num_rounds}_{n_features}_{train_size}.model"
+    model_path = os.path.join(tmpdir, model_name)
 
     bst = xgb.train(params, dtrain, num_rounds)
     bst.save_model(model_path)
