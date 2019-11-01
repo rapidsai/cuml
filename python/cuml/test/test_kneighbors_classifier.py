90563c2c7235edfc4ccd6ac03b70254c5f5f3c77
Corey J. Nolet <cjnolet@gmail.com>
2019-10-31 21:32:12 -0400

    Fixing some formatting issues '

--- python/cuml/test/test_kneighbors_classifier.py
+++ python/cuml/test/test_kneighbors_classifier.py
@@ -22,17 +22,6 @@ from sklearn.datasets import make_blobs
 import numpy as np
 from cuml.test.utils import array_equal
 
-def unit_param(*args, **kwargs):
-    return pytest.param(*args, **kwargs, marks=pytest.mark.unit)
-
-
-def quality_param(*args, **kwargs):
-    return pytest.param(*args, **kwargs, marks=pytest.mark.quality)
-
-
-def stress_param(*args, **kwargs):
-    return pytest.param(*args, **kwargs, marks=pytest.mark.stress)
-
 
 @pytest.mark.parametrize("nrows", [1000, 10000])
 @pytest.mark.parametrize("ncols", [50, 100])
