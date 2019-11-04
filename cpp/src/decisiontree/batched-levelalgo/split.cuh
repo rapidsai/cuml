423e272a2c0d2fbf89af621c5ca4b801dae7d5fe
Thejaswi Rao <rao.thejaswi@gmail.com>
2019-11-03 21:52:35 -0800

    first commit containing the Split struct logic, migrated from the decisiontree optimization branch

---
+++ cpp/src/decisiontree/batched-levelalgo/split.cuh
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <cuda_utils.h>
+
+namespace ML {
+namespace decisiontree {
+
+/** All info pertaining to splitting a node */
+template <typename DataT, typename IdxT>
+struct Split {
+  /** start with this as the initial gain */
+  static constexpr DataT Min = std::numeric_limits<DataT>::min();
+  /** special value to represent invalid column id */
+  static constexpr IdxT Invalid = static_cast<IdxT>(-1);
+
+  /** threshold to compare in this node */
+  DataT quesval;
+  /** feature index */
+  IdxT colid;
+  /** best info gain on this node */
+  DataT gain;
+  /** number of samples in the left child */
+  IdxT nLeft;
+
+  DI void init() {
+    quesval = gain = Min;
+    colid = Invalid;
+    nLeft = 0;
+  }
+
+  DI Split<DataT, IdxT>& operator=(const Split<DataT, IdxT>& other) {
+    quesval = other.quesval;
+    colid = other.colid;
+    gain = other.gain;
+    nLeft = other.nLeft;
+  }
+
+  /** updates the current split if the input gain is better */
+  DI void update(const Split<DataT, IdxT>& other) {
+    if (other.gain > gain) *this = other;
+  }
+
+  /** reduce the split info in the warp. Best split will be with 0th lane */
+  DI void warpReduce() {
+    auto lane = MLCommon::laneId();
+#pragma unroll
+    for (int i = MLCommon::WarpSize / 2; i >= 1; i /= 2) {
+      auto id = lane + i;
+      auto qu = MLCommon::shfl(quesval, id);
+      auto co = MLCommon::shfl(colid, id);
+      auto ga = MLCommon::shfl(gain, id);
+      auto nl = MLCommon::shfl(nLeft, id);
+      update(Split<DataT, IdxT>(qu, co, ga, nl));
+    }
+  }
+};  // struct Split
+
+template <typename DataT, typename IdxT>
+__global__ void initSplitKernel(Split<DataT, IdxT>* splits, IdxT batchSize) {
+  IdxT tid = threadIdx.x + blockDim.x * blockIdx.x;
+  if (tid < batchSize) splits[tid].init();
+}
+
+}  // namespace decisiontree
+}  // namespace ML
