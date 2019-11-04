dc952a486f2f22640e257e4d7dc91b7fc14f9653
Daniel Han-Chen <danielhanchen@gmail.com>
2019-11-05 02:36:51 +1100

    Update euclidean.h

--- cpp/src_prims/distance/euclidean.h
+++ cpp/src_prims/distance/euclidean.h
@@ -158,9 +158,11 @@ void euclideanAlgo2(Index_ m, Index_ n, Index_ k, const InType *pA,
                GemmEpilogueTraits_, GemmEpilogue_>(
     transa, transb, gemm_m, gemm_n, k, (EffOutType)1, aPtr, lda, bPtr, ldb,
     (EffOutType)0, nullptr, ldd, pDCast,
-    [enable_sqrt] HD(EpiParams & p) {
-      int err = p.initializeExtra(nullptr, nullptr, enable_sqrt);
-      return err;
+    __host__ 
+    [enable_sqrt] HD(EpiParams) {
+      return 0;
+      // int err = p.initializeExtra(nullptr, nullptr, enable_sqrt);
+      // return err;
     },
     fin_op, stream);
 }
