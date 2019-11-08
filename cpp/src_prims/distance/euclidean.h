53a0e98ee527929b62e4176636b2998f785debb4
Daniel Han-Chen <danielhanchen@gmail.com>
2019-11-09 01:49:31 +1100

    Update euclidean.h

--- cpp/src_prims/distance/euclidean.h
+++ cpp/src_prims/distance/euclidean.h
@@ -159,16 +159,10 @@ void euclideanAlgo2(Index_ m, Index_ n, Index_ k, const InType *pA,
   fprintf(stderr, "[%d]%s\n", __LINE__, __FILE__);
   fprintf(stderr, "[%d]%s\n", __LINE__, __FILE__);
 
-    LinAlg::gemm<InType, AccType, EffOutType, OutputTile_, AccumulatorsPerThread_,
-               MainLoopFunctor_, Index_, GemmConfig_, EpilogueFunctor_,
-               GemmEpilogueTraits_, GemmEpilogue_>(
-    transa, transb, gemm_m, gemm_n, k, (EffOutType)1, aPtr, lda, bPtr, ldb,
-    (EffOutType)0, nullptr, ldd, pDCast,
-    [](typename EpilogueFunctor_::Params &p) {
-      return 0;
-    },
-    fin_op,  // missing final lambda here
-    stream);
+  LinAlg::gemm<InType, AccType, EffOutType, OutputTile_, AccumulatorsPerThread_,
+                 MainLoopFunctor_, Index_, EpilogueFunctor_>(
+      transa, transb, gemm_m, gemm_n, k, (EffOutType)1, aPtr, lda, bPtr, ldb,
+      (EffOutType)0, nullptr, ldd, pDCast, stream);
 }
 
 // #if CUDART_VERSION >= 10010
