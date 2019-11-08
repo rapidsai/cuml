bb035898b030bdd1ae2dc885f86a871ea027ce68
Daniel Han-Chen <danielhanchen@gmail.com>
2019-11-09 02:23:05 +1100

    Update euclidean.h

--- cpp/src_prims/distance/euclidean.h
+++ cpp/src_prims/distance/euclidean.h
@@ -145,22 +145,22 @@ void euclideanAlgo2(Index_ m, Index_ n, Index_ k, const InType *pA,
     gemm_m = m;
     gemm_n = n;
   }
-  
+
   fprintf(stderr, "Lambda+op[%d]%s\n", __LINE__, __FILE__);
   fprintf(stderr, "[%d]%s\n", __LINE__, __FILE__);
   fprintf(stderr, "[%d]%s\n", __LINE__, __FILE__);
   fprintf(stderr, "[%d]%s\n", __LINE__, __FILE__);
 
-  LinAlg::gemm<InType, AccType, EffOutType, OutputTile_, AccumulatorsPerThread_,
-             MainLoopFunctor_, Index_, GemmConfig_, EpilogueFunctor_,
-             GemmEpilogueTraits_, GemmEpilogue_>(
-  transa, transb, gemm_m, gemm_n, k, (EffOutType)1, aPtr, lda, bPtr, ldb,
-  (EffOutType)0, nullptr, ldd, pDCast,
-  [enable_sqrt] __host__ __device__ (typename EpilogueFunctor_::Params &p) {
-    return p.initializeExtra(nullptr, nullptr, enable_sqrt);
-  },
-  fin_op,
-  stream);
+    LinAlg::gemm<InType, AccType, EffOutType, OutputTile_, AccumulatorsPerThread_,
+               MainLoopFunctor_, Index_, GemmConfig_, EpilogueFunctor_,
+               GemmEpilogueTraits_, GemmEpilogue_>(
+    transa, transb, gemm_m, gemm_n, k, (EffOutType)1, aPtr, lda, bPtr, ldb,
+    (EffOutType)0, nullptr, ldd, pDCast,
+    [enable_sqrt] (typename EpilogueFunctor_::Params &p) {
+      return p.initializeExtra(nullptr, nullptr, enable_sqrt);
+    },
+    fin_op,
+    stream);
 }
 
 };  // end namespace Distance
