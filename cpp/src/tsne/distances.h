9952d38f01ec862062d8f5215a2d74f43db39a76
Daniel Han-Chen <danielhanchen@gmail.com>
2019-11-10 17:49:48 +1100

    Update distances.h

--- cpp/src/tsne/distances.h
+++ cpp/src/tsne/distances.h
@@ -34,20 +34,30 @@ namespace TSNE {
  * @input param n_neighbors: The number of nearest neighbors you want.
  * @input param stream: The GPU stream.
  */
-void get_distances(const float *X, const int n, const int p, long *indices,
-                   float *distances, const int n_neighbors,
-                   cudaStream_t stream) {
+void get_distances(const float *X,
+                   const int n,
+                   const int p,
+                   long *indices,
+                   float *distances,
+                   const int n_neighbors,
+                   cudaStream_t stream)
+{
   // TODO: for TSNE transform first fit some points then transform with 1/(1+d^2)
   // #861
   float **knn_input = new float *[1];
+  ASSERT(knn_input != NULL, "Out of memory");
+
   int *sizes = new int[1];
+  ASSERT(sizes != NULL, "Out of memory");
+  
   knn_input[0] = (float *)X;
   sizes[0] = n;
 
   MLCommon::Selection::brute_force_knn(knn_input, sizes, 1, p,
                                        const_cast<float *>(X), n, indices,
                                        distances, n_neighbors, stream);
-  delete knn_input, sizes;
+  delete[] knn_input;
+  delete[] sizes;
 }
 
 /**
@@ -58,8 +68,11 @@ void get_distances(const float *X, const int n, const int p, long *indices,
  * @input param n_neighbors: The number of nearest neighbors you want.
  * @input param stream: The GPU stream.
  */
-void normalize_distances(const int n, float *distances, const int n_neighbors,
-                         cudaStream_t stream) {
+void normalize_distances(const int n,
+                         float *distances,
+                         const int n_neighbors,
+                         cudaStream_t stream)
+{
   // Now D / max(abs(D)) to allow exp(D) to not explode
   thrust::device_ptr<float> begin = thrust::device_pointer_cast(distances);
   float maxNorm = *thrust::max_element(thrust::cuda::par.on(stream), begin,
@@ -85,22 +98,27 @@ void normalize_distances(const int n, float *distances, const int n_neighbors,
  * @input param handle: The GPU handle.
  */
 template <int TPB_X = 32>
-void symmetrize_perplexity(float *P, long *indices, const int n, const int k,
-                           const float P_sum, const float exaggeration,
-                           MLCommon::Sparse::COO<float> *COO_Matrix,
-                           cudaStream_t stream, const cumlHandle &handle) {
+void symmetrize_perplexity(float *P,
+                           long *indices,
+                           const int n,
+                           const int k,
+                           const float P_sum,
+                           const float exaggeration,
+                           float *VAL,
+                           int *COL,
+                           int *ROW,
+                           cudaStream_t stream,
+                           const cumlHandle &handle)
+{
   // Perform (P + P.T) / P_sum * early_exaggeration
   const float div = exaggeration / (2.0f * P_sum);
   MLCommon::LinAlg::scalarMultiply(P, P, div, n * k, stream);
 
   // Symmetrize to form P + P.T
   MLCommon::Sparse::from_knn_symmetrize_matrix(
-    indices, P, n, k, COO_Matrix, stream, handle.getDeviceAllocator());
-
-  handle.getDeviceAllocator()->deallocate(P, sizeof(float) * n * k, stream);
-  handle.getDeviceAllocator()->deallocate(indices, sizeof(long) * n * k,
-                                          stream);
+    indices, P, n, k, VAL, COL, ROW, stream, handle.getDeviceAllocator());
 }
+
 
 }  // namespace TSNE
 }  // namespace ML
