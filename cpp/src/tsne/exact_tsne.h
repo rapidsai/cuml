b29a259ccd3bc48d83ff53aa40793df0f70b27c9
Daniel Han-Chen <danielhanchen@gmail.com>
2019-11-12 20:49:56 +1100

    changelog, tsne

--- cpp/src/tsne/exact_tsne.h
+++ cpp/src/tsne/exact_tsne.h
@@ -55,7 +55,7 @@ void Exact_TSNE(float *VAL, const int *COL, const int *ROW, const int NNZ,
                 const int max_iter = 1000, const float min_grad_norm = 1e-7,
                 const float pre_momentum = 0.5, const float post_momentum = 0.8,
                 const long long random_state = -1, const bool verbose = true,
-                const bool pca_intialization = false, int SAVED_SPACE = 0;)
+                const bool pca_intialization = false, int SAVED_SPACE = 0)
 {
   auto d_alloc = handle.getDeviceAllocator();
   cudaStream_t stream = handle.getStream();
