800aeaeb4bf14f1907d6496dbf904faffedd5770
Daniel Han-Chen <danielhanchen@gmail.com>
2019-11-15 04:30:26 +1100

    Update t_sne.pyx

--- python/cuml/manifold/t_sne.pyx
+++ python/cuml/manifold/t_sne.pyx
@@ -42,9 +42,9 @@ cimport cuml.common.handle
 cimport cuml.common.cuda
 
 cdef extern from "cuml/manifold/tsne.h" namespace "ML" nogil:
-    enum IntializationType:
-      Random_Intialization = 0;
-      PCA_Intialization = 1;
+    ctypedef enum IntializationType:
+      Random_Intialization
+      PCA_Intialization
 
 cdef extern from "cuml/manifold/tsne.h" namespace "ML" nogil:
     cdef void TSNE_fit[IntializationType](const cumlHandle &handle,
