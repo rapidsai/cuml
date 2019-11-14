29efea054f8e28c44c794ef1a56a4f73f99dfd24
Saloni Jain <sj7856@rit.edu>
2019-11-14 17:08:01 -0600

    removed the rf reg seed test

--- python/cuml/test/test_random_forest.py
+++ python/cuml/test/test_random_forest.py
@@ -21,7 +21,7 @@ from cuml.ensemble import RandomForestClassifier as curfc
 from cuml.ensemble import RandomForestRegressor as curfr
 from cuml.metrics import r2_score
 from cuml.test.utils import get_handle, unit_param, \
-    quality_param, stress_param, array_equal
+    quality_param, stress_param
 
 from sklearn.ensemble import RandomForestClassifier as skrfc
 from sklearn.ensemble import RandomForestRegressor as skrfr
@@ -45,10 +45,6 @@ def test_rf_classification(datatype, split_algo, rows_sample,
                            nrows, column_info, max_features):
     use_handle = True
     ncols, n_info = column_info
-
-    if datatype == np.float64:
-        pytest.xfail("Datatype np.float64 will run only on the CPU"
-                     " please convert the data to dtype np.float32")
 
     X, y = make_classification(n_samples=nrows, n_features=ncols,
                                n_clusters_per_class=1, n_informative=n_info,
@@ -76,7 +72,7 @@ def test_rf_classification(datatype, split_algo, rows_sample,
     cu_predict = cuml_model.predict(X_test, predict_model="CPU")
     cuml_acc = accuracy_score(y_test, cu_predict)
     fil_acc = accuracy_score(y_test, fil_preds)
-    assert fil_acc >= (cuml_acc - 0.02)
+
     if nrows < 500000:
         sk_model = skrfc(n_estimators=40,
                          max_depth=16,
@@ -86,6 +82,7 @@ def test_rf_classification(datatype, split_algo, rows_sample,
         sk_predict = sk_model.predict(X_test)
         sk_acc = accuracy_score(y_test, sk_predict)
         assert fil_acc >= (sk_acc - 0.07)
+    assert fil_acc >= (cuml_acc - 0.02)
 
 
 @pytest.mark.parametrize('mode', [unit_param('unit'), quality_param('quality'),
@@ -103,9 +100,6 @@ def test_rf_regression(datatype, split_algo, mode,
 
     ncols, n_info = column_info
     use_handle = True
-    if datatype == np.float64:
-        pytest.xfail("Datatype np.float64 will run only on the CPU"
-                     " please convert the data to dtype np.float32")
 
     if mode == 'unit':
         X, y = make_regression(n_samples=500, n_features=ncols,
@@ -141,15 +135,15 @@ def test_rf_regression(datatype, split_algo, mode,
     fil_r2 = r2_score(y_test, fil_preds, convert_dtype=datatype)
     # Initialize, fit and predict using
     # sklearn's random forest regression model
-    sk_model = skrfr(n_estimators=50, max_depth=16,
-                     min_samples_split=2, max_features=max_features,
-                     random_state=10)
-    sk_model.fit(X_train, y_train)
-    sk_predict = sk_model.predict(X_test)
-    sk_r2 = r2_score(y_test, sk_predict, convert_dtype=datatype)
-    print(fil_r2, cu_r2, sk_r2)
+    if mode != "stress":
+        sk_model = skrfr(n_estimators=50, max_depth=16,
+                         min_samples_split=2, max_features=max_features,
+                         random_state=10)
+        sk_model.fit(X_train, y_train)
+        sk_predict = sk_model.predict(X_test)
+        sk_r2 = r2_score(y_test, sk_predict, convert_dtype=datatype)
+        assert fil_r2 >= (sk_r2 - 0.07)
     assert fil_r2 >= (cu_r2 - 0.02)
-    assert fil_r2 >= (sk_r2 - 0.07)
 
 
 @pytest.mark.parametrize('datatype', [np.float32])
@@ -179,20 +173,13 @@ def test_rf_classification_default(datatype, column_info, nrows):
 
     # sklearn random forest classification model
     # initialization, fit and predict
-    sk_model = skrfc(max_depth=16, random_state=10)
-    sk_model.fit(X_train, y_train)
-    sk_predict = sk_model.predict(X_test)
-    sk_acc = accuracy_score(y_test, sk_predict)
-
-    # compare the accuracy of the two models
-    # github issue 1306: had to increase margin to avoid random CI fails
-    # assert cu_acc >= (sk_acc - 0.07)
-    try:
-        assert fil_acc >= (cu_acc - 0.02)
+    if nrows < 500000:
+        sk_model = skrfc(max_depth=16, random_state=10)
+        sk_model.fit(X_train, y_train)
+        sk_predict = sk_model.predict(X_test)
+        sk_acc = accuracy_score(y_test, sk_predict)
         assert fil_acc >= (sk_acc - 0.07)
-    except AssertionError:
-        pytest.xfail("Failed due to the results changing as the seeds is, "
-                     " not set")
+    assert fil_acc >= (cu_acc - 0.02)
 
 
 @pytest.mark.parametrize('datatype', [np.float32])
@@ -201,11 +188,10 @@ def test_rf_classification_default(datatype, column_info, nrows):
                          stress_param([500, 350])])
 @pytest.mark.parametrize('nrows', [unit_param(500), quality_param(5000),
                          stress_param(500000)])
-@pytest.mark.xfail(raises=AssertionError)
 def test_rf_regression_default(datatype, column_info, nrows):
 
     ncols, n_info = column_info
-    X, y = make_regression(n_samples=100, n_features=ncols,
+    X, y = make_regression(n_samples=500, n_features=ncols,
                            n_informative=n_info,
                            random_state=123)
     X = X.astype(datatype)
@@ -226,68 +212,17 @@ def test_rf_regression_default(datatype, column_info, nrows):
 
     # Initialize, fit and predict using
     # sklearn's random forest regression model
-    sk_model = skrfr(max_depth=16, random_state=10)
-    sk_model.fit(X_train, y_train)
-    sk_predict = sk_model.predict(X_test)
-    sk_r2 = r2_score(y_test, sk_predict, convert_dtype=datatype)
-    print(fil_r2, cu_r2, sk_r2)
+    if nrows < 500000:
+        sk_model = skrfr(max_depth=16, random_state=10)
+        sk_model.fit(X_train, y_train)
+        sk_predict = sk_model.predict(X_test)
+        sk_r2 = r2_score(y_test, sk_predict, convert_dtype=datatype)
+        assert fil_r2 >= (sk_r2 - 0.08)
     assert fil_r2 >= (cu_r2 - 0.02)
-    assert fil_r2 >= (sk_r2 - 0.07)
 
 
 @pytest.mark.parametrize('datatype', [np.float32])
-@pytest.mark.parametrize('column_info', [unit_param([16, 7]),
-                         quality_param([200, 100]),
-                         stress_param([500, 350])])
-@pytest.mark.parametrize('nrows', [unit_param(500), quality_param(5000),
-                         stress_param(500000)])
-@pytest.mark.xfail(raises=AssertionError)
-def test_rf_regression_seed(datatype, column_info, nrows):
-
-    ncols, n_info = column_info
-    X, y = make_regression(n_samples=100, n_features=ncols,
-                           n_informative=n_info,
-                           random_state=123)
-    X = X.astype(datatype)
-    y = y.astype(datatype)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8,
-                                                        random_state=0)
-    for i in range(20):
-        seed = random.randint(100, 1e5)
-        # Initialize, fit and predict using cuML's
-        # random forest classification model
-        cu_reg = curfr(seed=seed, n_streams=1)
-        cu_reg.fit(X_train, y_train)
-
-        # predict using FIL
-        fil_preds_orig = cu_reg.predict(X_test,
-                                        predict_model="GPU").copy_to_host()
-        cu_preds_orig = cu_reg.predict(X_test,
-                                       predict_model="CPU")
-        cu_r2_orig = r2_score(y_test, cu_preds_orig, convert_dtype=datatype)
-        fil_r2_orig = r2_score(y_test, fil_preds_orig, convert_dtype=datatype)
-
-        # Initialize, fit and predict using cuML's
-        # random forest classification model
-        cu_reg2 = curfr(seed=seed, n_streams=1)
-        cu_reg2.fit(X_train, y_train)
-
-        # predict using FIL
-        fil_preds_rerun = cu_reg2.predict(X_test,
-                                          predict_model="GPU").copy_to_host()
-        cu_preds_rerun = cu_reg2.predict(X_test, predict_model="CPU")
-        cu_r2_rerun = r2_score(y_test, cu_preds_rerun, convert_dtype=datatype)
-        fil_r2_rerun = r2_score(y_test, fil_preds_rerun,
-                                convert_dtype=datatype)
-
-        assert array_equal(fil_r2_orig, fil_r2_rerun, tol=1e-3)
-        assert array_equal(fil_preds_orig, fil_preds_rerun, tol=1e-3)
-        assert array_equal(cu_r2_orig, cu_r2_rerun, tol=1e-3)
-        assert array_equal(cu_preds_orig, cu_preds_rerun, tol=1e-3)
-
-
-@pytest.mark.parametrize('datatype', [np.float32])
-@pytest.mark.parametrize('column_info', [unit_param([16, 7]),
+@pytest.mark.parametrize('column_info', [unit_param([20, 10]),
                          quality_param([200, 100]),
                          stress_param([500, 350])])
 @pytest.mark.parametrize('nrows', [unit_param(500), quality_param(5000),
@@ -328,6 +263,7 @@ def test_rf_classification_seed(datatype, column_info, nrows):
         cu_preds_rerun = cu_class2.predict(X_test, predict_model="CPU")
         cu_acc_rerun = accuracy_score(y_test, cu_preds_rerun)
         fil_acc_rerun = accuracy_score(y_test, fil_preds_rerun)
+
         assert fil_acc_orig == fil_acc_rerun
         assert cu_acc_orig == cu_acc_rerun
         assert (fil_preds_orig == fil_preds_rerun).all()
