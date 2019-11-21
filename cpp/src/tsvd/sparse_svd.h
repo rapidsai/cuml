95a2daaf294b08fd24a5e413fda0efc92b626f9d
Daniel Han-Chen <danielhanchen@gmail.com>
2019-11-22 02:33:39 +1100

    Update sparse_svd.h

--- cpp/src/tsvd/sparse_svd.h
+++ cpp/src/tsvd/sparse_svd.h
@@ -17,22 +17,49 @@
 #pragma once
 
 #include "sparse_svd.h"
+#include <random/rng.h>
+#include <sys/time.h>
+
+#define device_buffer    MLCommon::device_buffer
+#define MIN(x, y)        (((x) > (y)) ? (y) : (x))
 
 namespace ML {
 
-template <typename math_t = float>
+template <typename T = float>
 void SparseSVD_fit(const cumlHandle &handle,
-                   const math_t *__restrict X,// (n, p)
+                   const T *__restrict X,// (n, p)
                    const int n,
                    const int p,
-                   math_t *__restrict U,      // (n, n_components)
-                   math_t *__restrict S,      // (n_components)
-                   math_t *__restrict VT,     // (n_components, p)
+                   T *__restrict U,      // (n, n_components)
+                   T *__restrict S,      // (n_components)
+                   T *__restrict VT,     // (n_components, p)
                    const int n_components = 2,
                    const int n_oversamples = 10,
                    const int max_iter = 3)
 {
-  printf("Hi from SparseSVD!\n");
+  ASSERT(n > 0 and p > 0 and U != NULL and S != NULL and VT != NULL, "Bad input");
+  ASSERT(n_components > 0 and n_oversamples > 0 and max_iter >= 0, "Bad input");
+
+  const auto d_alloc = handle.getDeviceAllocator();
+  const cudaStream_t stream = handle.getStream();
+  const int K = MIN(n_components + n_oversamples, p);
+
+  // Prepare device buffers
+  device_buffer<T> Y(d_alloc, stream, n*K); // Y(n, K)
+  device_buffer<T> Z(d_alloc, stream, p*K); // Z(p, K)
+
+  // Fill Z with random normal(0, 1)
+  struct timeval tp; gettimeofday(&tp, NULL);
+  MLCommon::Random::Rng random(tp.tv_sec * 1000 + tp.tv_usec);
+  random.normal<T>(Z, p*K, 0, 1, stream);
+  CUDA_CHECK(cudaPeekAtLastError());
+
+  device_buffer<T> T(d_alloc, stream, K*K); // T(K, K)
+
+
 }
 
 }  // namespace ML
+
+#undef device_buffer
+#undef MIN
