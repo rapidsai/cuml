2536b8198ab3b5c3932d5e679e0dc26885b56c75
Corey J. Nolet <cjnolet@gmail.com>
2019-11-22 14:38:50 -0500

    Tests for both sparse and dense in place

--- python/cuml/naive_bayes/naive_bayes.py
+++ python/cuml/naive_bayes/naive_bayes.py
@@ -17,25 +17,21 @@
 
 import math
 
-from cupy import prof
-
 import cupy as cp
-from cuml.prims.label import make_monotonic
-
-from cuml.preprocessing import LabelBinarizer
+from cuml.prims.label import make_monotonic, check_labels
 
 """
 A simple reduction kernel that takes in a sparse (COO) array
 of features and computes the sum and sum squared for each class
 label 
 """
-count_features = cp.RawKernel(r'''
+count_features_coo = cp.RawKernel(r'''
 extern "C" __global__
-void count_features(float *out,
+void count_features_coo(float *out,
                     int *rows, int *cols,
                     float *vals, int nnz,
                     int n_rows, int n_cols,
-                    int *labels, int n_labels,
+                    int *labels,
                     int n_classes,
                     bool square) {
 
@@ -53,7 +49,35 @@ void count_features(float *out,
   
   atomicAdd(out + ((col * n_classes) + label), val);
 }
-''', 'count_features')
+''', 'count_features_coo')
+
+count_features_dense = cp.RawKernel(r'''
+extern "C" __global__
+void count_features_dense(
+                    float *out,
+                    float *in,
+                    int n_rows, 
+                    int n_cols,
+                    int *labels,
+                    int n_classes,
+                    bool square,
+                    bool rowMajor) {
+
+  int row = blockIdx.x * blockDim.x + threadIdx.x;
+  int col = blockIdx.y * blockDim.y + threadIdx.y;
+  
+  if(row >= n_rows || col >= n_cols) return;
+
+  float val = !rowMajor ? in[col * n_rows + row] : in[row * n_cols + col];
+  
+  if(val == 0.0) return;
+  
+  if(square) val *= val;
+  int label = labels[row];
+
+  atomicAdd(out + ((col * n_classes) + label), val);
+}
+''', 'count_features_dense')
 
 
 class GaussianNB(object):
@@ -95,7 +119,7 @@ class GaussianNB(object):
         sq_counts = cp.zeros((self.n_classes_, self.n_features_),
                              order="F", dtype=cp.float32)
 
-        count_features((math.ceil(x_coo.nnz / 32),), (32,),
+        count_features_coo((math.ceil(x_coo.nnz / 32),), (32,),
                        (counts,
                         x_coo.row,
                         x_coo.col,
@@ -107,7 +131,7 @@ class GaussianNB(object):
                         self.n_classes_,
                         False))
 
-        count_features((math.ceil(x_coo.nnz / 32),), (32,),
+        count_features_coo((math.ceil(x_coo.nnz / 32),), (32,),
                        (counts,
                         x_coo.row,
                         x_coo.col,
@@ -132,20 +156,36 @@ class MultinomialNB(object):
         self.fit_prior = fit_prior
         self.class_prior = class_prior
 
+        self.fit_called_ = False
+
         self.classes_ = None
         self.n_classes_ = 0
 
         self.n_features_ = None
 
     @cp.prof.TimeRangeDecorator(message="fit()", color_id=0)
-    def fit(self, X, y, classes=None):
+    def fit(self, X, y, sample_weight=None):
+        return self.partial_fit(X, y, sample_weight)
 
-        Y, self.classes_ = make_monotonic(y, copy=True)
+    @cp.prof.TimeRangeDecorator(message="fit()", color_id=0)
+    def _partial_fit(self, X, y, sample_weight=None, _classes=None):
 
-        self.n_classes_ = self.classes_.shape[0]
+        Y, label_classes = make_monotonic(y, copy=True)
 
-        self.n_features_ = X.shape[1]
-        self._init_counters(self.n_classes_, self.n_features_)
+        if not self.fit_called_:
+            self.fit_called_ = True
+            if _classes is not None:
+                check_labels(Y, _classes)
+                self.classes_ = _classes
+            else:
+                self.classes_ = label_classes
+
+            self.n_classes_ = self.classes_.shape[0]
+            self.n_features_ = X.shape[1]
+            self._init_counters(self.n_classes_, self.n_features_, dtype=X.dtype)
+        else:
+            check_labels(Y, self.classes_)
+
         self._count(X, Y)
 
         self._update_feature_log_prob(self.alpha)
@@ -153,7 +193,7 @@ class MultinomialNB(object):
 
         return self
 
-    def partial_fit(self, X, y, classes=None, _sparse_labels=False, sample_weight=None):
+    def partial_fit(self, X, y, classes=None, sample_weight=None):
         """
         Incremental fit on a batch of samples
 
@@ -163,7 +203,7 @@ class MultinomialNB(object):
         :param sample_weight:
         :return:
         """
-        return self.fit(X, y, classes=classes, _sparse_labels=_sparse_labels)
+        return self._partial_fit(X, y, sample_weight=sample_weight, _classes=classes)
 
     @cp.prof.TimeRangeDecorator(message="predict()", color_id=1)
     def predict(self, X):
@@ -171,35 +211,54 @@ class MultinomialNB(object):
         indices = cp.argmax(jll, axis=1)
         return indices
 
-    def _init_counters(self, n_effective_classes, n_features):
-        self.class_count_ = cp.zeros(n_effective_classes, order="F", dtype=cp.float32)
+    def _init_counters(self, n_effective_classes, n_features, dtype=cp.float32):
+        self.class_count_ = cp.zeros(n_effective_classes, order="F", dtype=dtype)
         self.feature_count_ = cp.zeros((n_effective_classes, n_features),
-                                       order="F", dtype=cp.float32)
+                                       order="F", dtype=dtype)
 
     def _count(self, X, Y):
-
         """
-
         :param X: cupy.sparse matrix of size (n_rows, n_features)
         :param Y: cupy.array of monotonic class labels
         """
 
-        x_coo = X.tocoo()
+        if X.ndim != 2:
+            raise ValueError("Input samples should be a 2D array")
+
+        print(str(X.shape))
 
         counts = cp.zeros((self.n_classes_, self.n_features_),
-                          order="F", dtype=cp.float32)
+                          order="F", dtype=X.dtype)
 
-        count_features((math.ceil(x_coo.nnz / 32),), (32,),
-                       (counts,
-                        x_coo.row,
-                        x_coo.col,
-                        x_coo.data,
-                        x_coo.nnz,
-                        x_coo.shape[0],
-                        x_coo.shape[1],
-                        Y, Y.shape[0],
-                        self.n_classes_, False),
-                       )
+        n_rows = X.shape[0]
+        n_cols = X.shape[1]
+
+        if cp.sparse.isspmatrix(X):
+            X = X.tocoo()
+            count_features_coo((math.ceil(X.nnz / 32),), (32,),
+                           (counts,
+                            X.row,
+                            X.col,
+                            X.data,
+                            X.nnz,
+                            n_rows,
+                            n_cols,
+                            Y,
+                            self.n_classes_, False))
+        else:
+            count_features_dense((math.ceil(n_rows / 32),
+                                  math.ceil(n_cols / 32), 1),
+                                 (32, 32, 1),
+                           (counts,
+                            X,
+                            n_rows,
+                            n_cols,
+                            Y,
+                            self.n_classes_,
+                            False,
+                            X.flags["C_CONTIGUOUS"]))
+
+        print(str(counts))
 
         self.feature_count_ += counts
         self.class_count_ += counts.sum(axis=1).reshape(self.n_classes_)
