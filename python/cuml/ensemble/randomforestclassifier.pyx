3173c314be176bf14caeb8115c3390d135b0d136
Dante Gama Dessavre <danteg@nvidia.com>
2019-11-22 12:39:41 -0600

    FIX Bugfix and docstring improvements in RF Classifier

--- python/cuml/ensemble/randomforestclassifier.pyx
+++ python/cuml/ensemble/randomforestclassifier.pyx
@@ -232,60 +232,57 @@ class RandomForestClassifier(Base):
     Parameters
     -----------
     n_estimators : int (default = 10)
-                   number of trees in the forest.
+        Number of trees in the forest.
     handle : cuml.Handle
-             If it is None, a new one is created just for this class.
+        If it is None, a new one is created just for this class.
     split_criterion: The criterion used to split nodes.
-                     0 for GINI, 1 for ENTROPY
-                     2 and 3 not valid for classification
-                     (default = 0)
-    split_algo : 0 for HIST and 1 for GLOBAL_QUANTILE
-                 (default = 1)
-                 the algorithm to determine how nodes are split in the tree.
-                 HIST curently uses a slower tree-building algorithm
-                 so GLOBAL_QUANTILE is recommended for most cases.
+        0 for GINI, 1 for ENTROPY
+        2 and 3 not valid for classification
+        (default = 0)
+    split_algo : int (default = 1)
+        The algorithm to determine how nodes are split in the tree.
+        0 for HIST and 1 for GLOBAL_QUANTILE. HIST curently uses a slower
+        tree-building algorithm so GLOBAL_QUANTILE is recommended for most
+        cases.
     bootstrap : boolean (default = True)
-                Control bootstrapping.
-                If set, each tree in the forest is built
-                on a bootstrapped sample with replacement.
-                If false, sampling without replacement is done.
+        Control bootstrapping.
+        If True, each tree in the forest is built
+        on a bootstrapped sample with replacement.
+        If False, sampling without replacement is done.
     bootstrap_features : boolean (default = False)
-                         Control bootstrapping for features.
-                         If features are drawn with or without replacement
+        Control bootstrapping for features.
+        If features are drawn with or without replacement
     rows_sample : float (default = 1.0)
-                  Ratio of dataset rows used while fitting each tree.
+        Ratio of dataset rows used while fitting each tree.
     max_depth : int (default = 16)
-                Maximum tree depth. Unlimited (i.e, until leaves are pure),
-                if -1. Unlimited depth is not supported with split_algo=1.
-                *Note that this default differs from scikit-learn's
-                random forest, which defaults to unlimited depth.*
+        Maximum tree depth. Unlimited (i.e, until leaves are pure),
+        if -1. Unlimited depth is not supported with split_algo=1.
+        *Note that this default differs from scikit-learn's
+        random forest, which defaults to unlimited depth.*
     max_leaves : int (default = -1)
-                 Maximum leaf nodes per tree. Soft constraint. Unlimited,
-                 if -1.
-    max_features : int or float or string or None (default = 'auto')
-                   Ratio of number of features (columns) to consider
-                   per node split.
-                   If int then max_features/n_features.
-                   If float then max_features is a fraction.
-                   If 'auto' then max_features=1/sqrt(n_features).
-                   If 'sqrt' then max_features=1/sqrt(n_features).
-                   If 'log2' then max_features=log2(n_features)/n_features.
-                   If None, then max_features=1/sqrt(n_features).
+        Maximum leaf nodes per tree. Soft constraint. Unlimited,
+        if -1.
+    max_features : int, float, or string (default = 'auto')
+        Ratio of number of features (columns) to consider per node split.
+        If int then max_features/n_features.
+        If float then max_features is used as a fraction.
+        If 'auto' then max_features=1/sqrt(n_features).
+        If 'sqrt' then max_features=1/sqrt(n_features).
+        If 'log2' then max_features=log2(n_features)/n_features.
     n_bins :  int (default = 8)
-              Number of bins used by the split algorithm.
+        Number of bins used by the split algorithm.
     min_rows_per_node : int or float (default = 2)
-                        The minimum number of samples (rows) needed
-                        to split a node.
-                        If int then number of sample rows
-                        If float the min_rows_per_sample*n_rows
+        The minimum number of samples (rows) needed to split a node.
+        If int then number of sample rows
+        If float the min_rows_per_sample*n_rows
     min_impurity_decrease : float (default = 0.0)
-                            Minimum decrease in impurity requried for
-                            node to be spilt.
+        Minimum decrease in impurity requried for
+        node to be spilt.
     quantile_per_tree : boolean (default = False)
-                        Whether quantile is computed for individal trees in RF.
-                        Only relevant for GLOBAL_QUANTILE split_algo.
+        Whether quantile is computed for individal trees in RF.
+        Only relevant for GLOBAL_QUANTILE split_algo.
     seed : int (default = None)
-           Seed for the random number generator. Unseeded by default.
+        Seed for the random number generator. Unseeded by default.
     """
 
     variables = ['n_estimators', 'max_depth', 'handle',
@@ -480,8 +477,10 @@ class RandomForestClassifier(Base):
             input_to_dev_array(X, order='F')
 
         if self.dtype == np.float64:
-            warnings.warn("In order to run predict on the GPU convert"
-                          " the data to float32")
+            warnings.warn("Model is being trained with float64 data, which \
+                          means only CPU based predict can be used. \
+                          To use GPU based predict train use float32 training"
+                          "data to fit the estimator.")
 
         cdef cumlHandle* handle_ =\
             <cumlHandle*><size_t>self.handle.getHandle()
@@ -558,11 +557,13 @@ class RandomForestClassifier(Base):
 
     def _predict_model_on_gpu(self, X, output_class,
                               threshold, algo,
-                              num_classes):
+                              num_classes, convert_dtype):
 
         cdef ModelHandle cuml_model_ptr = NULL
         _, _, n_rows, n_cols, X_type = \
             input_to_dev_array(X, check_dtype=self.dtype,
+                               convert_to_dtype=(self.dtype if convert_dtype
+                                                 else None)
                                check_cols=self.n_cols)
 
         cdef RandomForestMetaData[float, int] *rf_forest = \
@@ -584,10 +585,12 @@ class RandomForestClassifier(Base):
         preds = tl_to_fil_model.predict(X)
         return preds
 
-    def _predict_model_on_cpu(self, X):
+    def _predict_model_on_cpu(self, X, convert_dtype):
         cdef uintptr_t X_ptr
         X_m, X_ptr, n_rows, n_cols, _ = \
             input_to_dev_array(X, order='C', check_dtype=self.dtype,
+                               convert_to_dtype=(self.dtype if convert_dtype
+                                                 else None),
                                check_cols=self.n_cols)
 
         preds = np.zeros(n_rows, dtype=np.int32)
@@ -634,7 +637,7 @@ class RandomForestClassifier(Base):
     def predict(self, X, predict_model="GPU",
                 output_class=True, threshold=0.5,
                 algo='BATCH_TREE_REORG',
-                num_classes=2):
+                num_classes=2, convert_dtype=False):
         """
         Predicts the labels for X.
 
@@ -644,49 +647,58 @@ class RandomForestClassifier(Base):
             Dense matrix (floats or doubles) of shape (n_samples, n_features).
             Acceptable formats: cuDF DataFrame, NumPy ndarray, Numba device
             ndarray, cuda array interface compliant array like CuPy
-        predict_model : String
-                        "GPU" if prediction should be carried out on the GPU
-                        "CPU" or None if prediction should be carried out
-                        on the CPU
-        output_class: boolean
-                      This is optional and required only while performing the
-                      predict operation on the GPU.
-                      If true, return a 1 or 0 depending on whether the raw
-                      prediction exceeds the threshold. If False, just return
-                      the raw prediction.
-        algo : string name of the algo from (from algo_t enum)
-               This is optional and required only while performing the
-               predict operation on the GPU.
-               'NAIVE' - simple inference using shared memory
-               'TREE_REORG' - similar to naive but trees rearranged to be more
-                              coalescing-friendly
-               'BATCH_TREE_REORG' - similar to TREE_REORG but predicting
-                                    multiple rows per thread block
-        threshold : float
-                    threshold is used to for classification
-                    This is optional and required only while performing the
-                    predict operation on the GPU.
-                    It is applied if output_class == True, else it is ignored
-        num_classes : integer
+        predict_model : String (default = 'GPU')
+            'GPU' to predict using the GPU, 'CPU' otherwise. The GPU can only
+            be used if the model was trained on float32 data and `X` is float32
+            or convert_dtype is set to True.
+        output_class: boolean (default = True)
+            This is optional and required only while performing the
+            predict operation on the GPU.
+            If true, return a 1 or 0 depending on whether the raw
+            prediction exceeds the threshold. If False, just return
+            the raw prediction.
+        algo : string (default = 'BATCH_TREE_REORG')
+            This is optional and required only while performing the
+            predict operation on the GPU.
+            'NAIVE' - simple inference using shared memory
+            'TREE_REORG' - similar to naive but trees rearranged to be more
+                           coalescing-friendly
+            'BATCH_TREE_REORG' - similar to TREE_REORG but predicting
+                                 multiple rows per thread block
+        threshold : float (default = 0.5)
+            Threshold used for classification. Optional and required only
+            while performing the predict operation on the GPU.
+            It is applied if output_class == True, else it is ignored
+        num_classes : int (default = 2)
                       number of different classes present in the dataset
+        convert_dtype : bool, optional (default = False)
+            When set to True, the predict method will, when necessary, convert
+            the input to the data type which was used to train the model. This
+            will increase memory used for the method.
 
         Returns
         ----------
         y : NumPy
            Dense vector (int) of shape (n_samples, 1)
         """
-        if self.dtype == np.float64:
-            raise TypeError("GPU predict model only accepts float32 dtype"
-                            " as input, convert the data to float32 or "
-                            "use the CPU predict with `predict_model='CPU'`.")
 
-        elif predict_model == "CPU" or predict_model is None:
-            preds = self._predict_model_on_cpu(X)
+        if predict_model == "CPU":
+            preds = self._predict_model_on_cpu(X, convert_dtype)
+
+        elif self.dtype == np.float64 and convert_dtype is False:
+            warnings.watn("Using CPU based predict. \
+                          GPU predict model only accepts float32 and \
+                          model was trained with float64. TO use GPU based \
+                          predict, convert `X` to float32 or set the \
+                          parameter convert_dtype to True to have the \
+                          estimator do it for you.")
+
+            preds = self._predict_model_on_cpu(X, convert_dtype)
 
         else:
             preds = self._predict_model_on_gpu(X, output_class,
                                                threshold, algo,
-                                               num_classes)
+                                               num_classes, convert_dtype)
 
         return preds
 
@@ -768,19 +780,19 @@ class RandomForestClassifier(Base):
         y : NumPy
            Dense vector (int) of shape (n_samples, 1)
         algo : string name of the algo from (from algo_t enum)
-               This is optional and required only while performing the
-               predict operation on the GPU.
-               'NAIVE' - simple inference using shared memory
-               'TREE_REORG' - similar to naive but trees rearranged to be more
-                              coalescing-friendly
-               'BATCH_TREE_REORG' - similar to TREE_REORG but predicting
-                                    multiple rows per thread block
+            This is optional and required only while performing the
+            predict operation on the GPU.
+            'NAIVE' - simple inference using shared memory
+            'TREE_REORG' - similar to naive but trees rearranged to be more
+            coalescing-friendly
+            'BATCH_TREE_REORG' - similar to TREE_REORG but predicting
+            multiple rows per thread block
         threshold : float
-                    threshold is used to for classification
-                    This is optional and required only while performing the
-                    predict operation on the GPU.
+            threshold is used to for classification
+            This is optional and required only while performing the
+            predict operation on the GPU.
         num_classes : integer
-                      number of different classes present in the dataset
+            number of different classes present in the dataset
         Returns
         -------
         float
