cd0b05534a9f86425b0517ae9db80a74a55fb96d
Vishal Mehta <vishalm@nvidia.com>
2019-12-02 10:04:39 +0100

    fix

--- cpp/src/decisiontree/levelalgo/levelkernel_classifier.cuh
+++ cpp/src/decisiontree/levelalgo/levelkernel_classifier.cuh
@@ -474,7 +474,7 @@ __global__ void make_leaf_gather_classification_kernel(
     SparseTreeNode<T, int> localnode;
     localnode.prediction =
       get_class_hist_shared(shmemhist_parent, n_unique_labels);
-    localnode.colid = colid;
+    localnode.colid = -1;
     localnode.best_metric_val = parent_metric;
     d_sparsenodes[d_nodelist[blockIdx.x]] = localnode;
   }
