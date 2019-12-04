074600714eb1a8f269f204627ccfea8d003d8e47
Corey J. Nolet <cjnolet@gmail.com>
2019-12-04 11:24:35 -0500

    Using a thread outside Dask's threadpool for initializing UCX listener

--- python/cuml/test/dask/conftest.py
+++ python/cuml/test/dask/conftest.py
@@ -1,9 +1,12 @@
 import pytest
-from dask_cuda import LocalCUDACluster
 
+from dask_cuda import initialize
+from dask_cuda import LocalCUDACluster
 
 @pytest.fixture(scope="module")
 def cluster():
-    cluster = LocalCUDACluster()
+    initialize.initialize(create_cuda_context=True,
+                          enable_tcp_over_ucx=True)
+    cluster = LocalCUDACluster(protocol="ucx", threads_per_worker=1)
     yield cluster
     cluster.close()
