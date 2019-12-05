f052fdd7a274769ddda8a9b458edab8601f9189f
Corey J. Nolet <cjnolet@gmail.com>
2019-12-05 17:22:44 -0500

    Removing verbose logging. Fixing comms tests

--- python/cuml/test/dask/conftest.py
+++ python/cuml/test/dask/conftest.py
@@ -1,17 +1,22 @@
 import pytest
 
+from dask_cuda import initialize
 from dask_cuda import LocalCUDACluster
+
+enable_tcp_over_ucx = True
 
 
 @pytest.fixture(scope="module")
-def ucx_cluster():
-    cluster = LocalCUDACluster(protocol="ucx", threads_per_worker=1)
+def cluster():
+    cluster = LocalCUDACluster()
     yield cluster
     cluster.close()
 
 
 @pytest.fixture(scope="module")
-def cluster():
-    cluster = LocalCUDACluster(threads_per_worker=1)
+def ucx_cluster():
+    initialize.initialize(create_cuda_context=True,
+                          enable_tcp_over_ucx=enable_tcp_over_ucx)
+    cluster = LocalCUDACluster(protocol="ucx")
     yield cluster
     cluster.close()
