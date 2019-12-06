1e936f0cf16028b7f3623706adf82cfaa7f4fb35
Corey J. Nolet <cjnolet@gmail.com>
2019-12-05 19:24:24 -0500

    Updates based on recent dask-cuda updates

--- python/cuml/test/dask/conftest.py
+++ python/cuml/test/dask/conftest.py
@@ -3,7 +3,9 @@ import pytest
 from dask_cuda import initialize
 from dask_cuda import LocalCUDACluster
 
-enable_tcp_over_ucx = True
+enable_tcp_over_ucx=True
+enable_nvlink=False
+enable_infiniband=False
 
 
 @pytest.fixture(scope="module")
@@ -16,7 +18,13 @@ def cluster():
 @pytest.fixture(scope="module")
 def ucx_cluster():
     initialize.initialize(create_cuda_context=True,
-                          enable_tcp_over_ucx=enable_tcp_over_ucx)
-    cluster = LocalCUDACluster(protocol="ucx")
+                          enable_tcp_over_ucx=enable_tcp_over_ucx,
+                          enable_nvlink=enable_nvlink,
+                          enable_infiniband=enable_infiniband)
+    cluster = LocalCUDACluster(protocol="ucx",
+                               enable_tcp_over_ucx=enable_tcp_over_ucx,
+                               enable_nvlink=enable_nvlink,
+                               enable_infiniband=enable_infiniband,
+                               ucx_net_devices="auto")
     yield cluster
     cluster.close()
