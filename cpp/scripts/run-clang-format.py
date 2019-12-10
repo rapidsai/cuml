16f4a72f8f52dc8496de6bd8e2b64c735a100693
Thejaswi Rao <rao.thejaswi@gmail.com>
2019-12-10 01:45:23 -0800

    simplified run-clang-format.py script to just work on all source files

--- cpp/scripts/run-clang-format.py
+++ cpp/scripts/run-clang-format.py
@@ -19,10 +19,39 @@ import re
 import os
 import subprocess
 import argparse
-import gitutils
 
 
-def listAllSourceFiles(fileRegex, srcdirs, dstdir, inplace):
+VersionRegex = re.compile(r"clang-format version ([0-9.]+)")
+
+
+def parse_args():
+    argparser = argparse.ArgumentParser("Runs clang-format on a project")
+    argparser.add_argument("-dstdir", type=str,
+                           default="/tmp/cuml-clang-format",
+                           help="Path to the current build directory.")
+    argparser.add_argument("-exe", type=str, default="clang-format",
+                           help="Path to clang-format exe")
+    argparser.add_argument("-inplace", default=False, action="store_true",
+                           help="Replace the source files itself.")
+    argparser.add_argument("-regex", type=str,
+                           default=r"[.](cu|cuh|h|hpp|cpp)$",
+                           help="Regex string to filter in sources")
+    argparser.add_argument("dirs", type=str, nargs="*",
+                           help="List of dirs where to find sources")
+    args = argparser.parse_args()
+    args.regexCompiled = re.compile(args.regex)
+    ret = subprocess.check_output("%s --version" % args.exe, shell=True)
+    ret = ret.decode("utf-8")
+    version = VersionRegex.match(ret)
+    if version is None:
+        raise Exception("Failed to figure out clang-format version!")
+    version = version.group(1)
+    if version != "8.0.0":
+        raise Exception("clang-format exe must be v8.0.0 found '%s'" % version)
+    return args
+
+
+def list_all_src_files(fileRegex, srcdirs, dstdir, inplace):
     allFiles = []
     for srcdir in srcdirs:
         for root, dirs, files in os.walk(srcdir):
@@ -38,97 +67,46 @@ def listAllSourceFiles(fileRegex, srcdirs, dstdir, inplace):
     return allFiles
 
 
-def listAllChangedFiles(fileRegex, dstdir, inplace):
-    allFiles = []
-
-    def checkThisFile(f):
-        return True if fileRegex.search(f) else False
-
-    files = gitutils.modifiedFiles(filter=checkThisFile)
-    for f in files:
-        dst = f if inplace else os.path.join(dstdir, f)
-        allFiles.append((f, dst))
-    return allFiles
-
-
-def parseArgs():
-    argparser = argparse.ArgumentParser("Runs clang-format on a project")
-    argparser.add_argument("-dstdir", type=str, default=".",
-                           help="Path to the current build directory.")
-    argparser.add_argument("-exe", type=str, default="clang-format",
-                           help="Path to clang-format exe")
-    argparser.add_argument("-inplace", default=False, action="store_true",
-                           help="Replace the source files itself.")
-    argparser.add_argument("-regex", type=str,
-                           default=r"[.](cu|cuh|h|hpp|cpp)$",
-                           help="Regex string to filter in sources")
-    argparser.add_argument("-onlyChangedFiles", default=False,
-                           action="store_true",
-                           help="Run the formatter on changedFiles only.")
-    argparser.add_argument("-srcdir", type=str, default=".",
-                           help="Path to directory containing the dirs.")
-    argparser.add_argument("dirs", type=str, nargs="*",
-                           help="List of dirs where to find sources")
-    args = argparser.parse_args()
-    args.regexCompiled = re.compile(args.regex)
-    return args
-
-
-def isNewer(src, dst):
-    if not os.path.exists(dst):
-        return True
-    a = os.path.getmtime(src)
-    b = os.path.getmtime(dst)
-    return a >= b
-
-
-def runClangFormat(src, dst, exe):
+def run_clang_format(src, dst, exe):
     dstdir = os.path.dirname(dst)
     if not os.path.exists(dstdir):
         os.makedirs(dstdir)
     # run the clang format command itself
-    if isNewer(src, dst):
-        if src == dst:
-            cmd = "%s -i %s" % (exe, src)
-        else:
-            cmd = "%s %s > %s" % (exe, src, dst)
-        try:
-            subprocess.check_call(cmd, shell=True)
-        except subprocess.CalledProcessError:
-            print("Unable to run clang-format! Maybe your env is not "
-                  "configured properly?")
-            raise
+    if src == dst:
+        cmd = "%s -i %s" % (exe, src)
+    else:
+        cmd = "%s %s > %s" % (exe, src, dst)
+    try:
+        subprocess.check_call(cmd, shell=True)
+    except subprocess.CalledProcessError:
+        print("Failed to run clang-format! Maybe your env is not proper?")
+        raise
     # run the diff to check if there are any formatting issues
     cmd = "diff -q %s %s >/dev/null" % (src, dst)
     try:
         subprocess.check_call(cmd, shell=True)
     except subprocess.CalledProcessError:
         srcPath = os.path.join(os.getcwd(), src)
-        print("clang-format failed! Run 'diff -y %s %s | more' to know more"
-              " about the formatting violations!" % (srcPath, dst))
+        print("clang-format failed! Run 'diff -y %s %s' to know more about "
+              "the formatting violations!" % (srcPath, dst))
         return False
     return True
 
 
 def main():
-    args = parseArgs()
-    # Either run the format checker on only the changed files or all the source
-    # files as specified by the user
-    if args.onlyChangedFiles:
-        allFiles = listAllChangedFiles(args.regexCompiled, args.dstdir,
-                                       args.inplace)
-    else:
-        allFiles = listAllSourceFiles(args.regexCompiled, args.dirs,
-                                      args.dstdir, args.inplace)
+    args = parse_args()
+    allFiles = list_all_src_files(args.regexCompiled, args.dirs, args.dstdir,
+                                  args.inplace)
     # actual format checker
     status = True
     for src, dst in allFiles:
-        if not runClangFormat(src, dst, args.exe):
+        if not run_clang_format(src, dst, args.exe):
             status = False
     if not status:
-        print("Clang-format failed! Look at errors above and fix them, you ")
-        print("can also run the following command to bulk fix all the flagged")
-        print(" violations above:  make fix-format")
+        print("Clang-format failed! You have 2 options:")
+        print(" 1. Look at formatting differences above and fix them manually")
+        print(" 2. Or run the below command to bulk-fix all these at once")
+        print("Bulk-fix command: python %s -inplace" % " ".join(sys.argv))
         sys.exit(-1)
     return
 
