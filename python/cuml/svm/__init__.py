8787759235b6295ec27988a65011c0d186902b02
Tamas Bela Feher <tfeher@nvidia.com>
2019-12-12 14:59:13 +0100

    ENH Update init.py for the SVM package.

--- python/cuml/svm/__init__.py
+++ python/cuml/svm/__init__.py
@@ -13,4 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from cuml.svm.svm import SVC
+from cuml.svm.svc import SVC
+from cuml.svm.svr import SVR
