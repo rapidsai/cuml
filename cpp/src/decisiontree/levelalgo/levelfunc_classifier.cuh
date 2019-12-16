49788f1bbc5895b5ab604c94909cdc91d21216e0
Vishal Mehta <vishalm@nvidia.com>
2019-12-16 18:03:28 +0100

    fixed childsz issue on memset

--- cpp/src/decisiontree/levelalgo/levelfunc_classifier.cuh
+++ cpp/src/decisiontree/levelalgo/levelfunc_classifier.cuh
@@ -163,11 +163,11 @@ void grow_deep_tree_classification(
   SparseTreeNode<T, int>* h_sparsenodes;
   int *h_nodelist, *d_nodelist, *d_new_nodelist;
   int max_nodes = tempmem->max_nodes_per_level;
-  d_nodecount = (unsigned int*)(tempmem->d_split_colidx->data());
+  d_nodecount = (unsigned int*)(tempmem->d_child_best_metric->data());
   d_nodestart = (unsigned int*)(tempmem->d_split_binidx->data());
   d_samplelist = (unsigned int*)(tempmem->d_parent_metric->data());
   d_nodelist = (int*)(tempmem->d_outgain->data());
-  d_new_nodelist = (int*)(tempmem->d_child_best_metric->data());
+  d_new_nodelist = (int*)(tempmem->d_split_colidx->data());
   h_nodelist = (int*)(tempmem->h_outgain->data());
   d_sparsenodes = tempmem->d_sparsenodes->data();
   h_sparsenodes = tempmem->h_sparsenodes->data();
