b6ac1e57c014f1d2fb6f30435955be038f61eed6
Dante Gama Dessavre <danteg@nvidia.com>
2020-01-06 16:56:31 -0600

    FIX Remove straggling import in __init__

--- python/cuml/utils/__init__.py
+++ python/cuml/utils/__init__.py
@@ -16,8 +16,6 @@
 
 from cuml.utils.pointer_utils import device_of_gpu_matrix
 
-from cuml.utils.cupy_utils import checked_cupy_fn
-
 from cuml.utils.numba_utils import row_matrix, zeros, device_array_from_ptr
 
 from cuml.utils.input_utils import get_cudf_column_ptr, get_dev_array_ptr, \
