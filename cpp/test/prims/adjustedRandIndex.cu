72493995947d51df70a269c898ea81a576e3792b
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-01-07 21:52:14 -0800

    updated adjusted-rand-index unit-test with the changes done for the prim

--- cpp/test/prims/adjustedRandIndex.cu
+++ cpp/test/prims/adjustedRandIndex.cu
@@ -25,7 +25,6 @@
 namespace MLCommon {
 namespace Metrics {
 
-//parameter structure definition
 struct AdjustedRandIndexParam {
   int nElements;
   int lowerLabelRange;
@@ -34,30 +33,21 @@ struct AdjustedRandIndexParam {
   double tolerance;
 };
 
-//test fixture class
-template
-
-  <typename T>
-  class adjustedRandIndexTest
+template <typename T, typename MathT = int>
+class AdjustedRandIndexTest
   : public ::testing::TestWithParam<AdjustedRandIndexParam> {
  protected:
-  //the constructor
   void SetUp() override {
-    //getting the parameters
     params = ::testing::TestWithParam<AdjustedRandIndexParam>::GetParam();
-
     nElements = params.nElements;
     lowerLabelRange = params.lowerLabelRange;
     upperLabelRange = params.upperLabelRange;
-
-    //generating random value test input
     std::vector<int> arr1(nElements, 0);
     std::vector<int> arr2(nElements, 0);
     std::random_device rd;
     std::default_random_engine dre(rd());
     std::uniform_int_distribution<int> intGenerator(lowerLabelRange,
                                                     upperLabelRange);
-
     std::generate(arr1.begin(), arr1.end(),
                   [&]() { return intGenerator(dre); });
     if (params.sameArrays) {
@@ -66,15 +56,12 @@ template
       std::generate(arr2.begin(), arr2.end(),
                     [&]() { return intGenerator(dre); });
     }
-
-    //generating the golden output
-    //calculating the contingency matrix
+    // calculating golden output
     int numUniqueClasses = upperLabelRange - lowerLabelRange + 1;
     size_t sizeOfMat = numUniqueClasses * numUniqueClasses * sizeof(int);
     int *hGoldenOutput = (int *)malloc(sizeOfMat);
     memset(hGoldenOutput, 0, sizeOfMat);
-    int i, j;
-    for (i = 0; i < nElements; i++) {
+    for (int i = 0; i < nElements; i++) {
       int row = arr1[i] - lowerLabelRange;
       int column = arr2[i] - lowerLabelRange;
 
@@ -87,63 +74,52 @@ template
     memset(b, 0, numUniqueClasses * sizeof(int));
     int sumOfAiCTwo = 0;
     int sumOfBiCTwo = 0;
-
     //calculating the sum of number of pairwise points in each index
     //and also the reducing contingency matrix along row and column
-    for (i = 0; i < numUniqueClasses; ++i) {
-      for (j = 0; j < numUniqueClasses; ++j) {
+    for (int i = 0; i < numUniqueClasses; ++i) {
+      for (int j = 0; j < numUniqueClasses; ++j) {
         int Nij = hGoldenOutput[i * numUniqueClasses + j];
         sumOfNijCTwo += ((Nij) * (Nij - 1)) / 2;
         a[i] += hGoldenOutput[i * numUniqueClasses + j];
         b[i] += hGoldenOutput[j * numUniqueClasses + i];
       }
     }
-
     //claculating the sum of number pairwise points in ever column sum
     //claculating the sum of number pairwise points in ever row sum
-    for (i = 0; i < numUniqueClasses; ++i) {
+    for (int i = 0; i < numUniqueClasses; ++i) {
       sumOfAiCTwo += ((a[i]) * (a[i] - 1)) / 2;
       sumOfBiCTwo += ((b[i]) * (b[i] - 1)) / 2;
     }
-
     //calculating the ARI
     int nCTwo = ((nElements) * (nElements - 1)) / 2;
     double expectedIndex =
       ((double)(sumOfBiCTwo * sumOfAiCTwo)) / ((double)(nCTwo));
     double maxIndex = ((double)(sumOfAiCTwo + sumOfBiCTwo)) / 2.0;
     double index = (double)sumOfNijCTwo;
-
     if (maxIndex - expectedIndex)
       truthAdjustedRandIndex =
         (index - expectedIndex) / (maxIndex - expectedIndex);
     else
       truthAdjustedRandIndex = 0;
-
     //allocating and initializing memory to the GPU
     CUDA_CHECK(cudaStreamCreate(&stream));
     MLCommon::allocate(firstClusterArray, nElements, true);
     MLCommon::allocate(secondClusterArray, nElements, true);
-
-    MLCommon::updateDevice(firstClusterArray, &arr1[0], (int)nElements, stream);
-    MLCommon::updateDevice(secondClusterArray, &arr2[0], (int)nElements,
-                           stream);
+    MLCommon::updateDevice(firstClusterArray, &arr1[0], nElements, stream);
+    MLCommon::updateDevice(secondClusterArray, &arr2[0], nElements, stream);
     std::shared_ptr<MLCommon::deviceAllocator> allocator(
       new defaultDeviceAllocator);
-
-    //calling the adjustedRandIndex CUDA implementation
-    computedAdjustedRandIndex = MLCommon::Metrics::computeAdjustedRandIndex(
+    computedAdjustedRandIndex = computeAdjustedRandIndex<T, MathT>(
       firstClusterArray, secondClusterArray, nElements, lowerLabelRange,
       upperLabelRange, allocator, stream);
   }
 
-  //the destructor
   void TearDown() override {
     CUDA_CHECK(cudaFree(firstClusterArray));
     CUDA_CHECK(cudaFree(secondClusterArray));
     CUDA_CHECK(cudaStreamDestroy(stream));
   }
 
-  //declaring the data values
   AdjustedRandIndexParam params;
   T lowerLabelRange, upperLabelRange;
   T *firstClusterArray = nullptr;
@@ -163,14 +139,12 @@ const std::vector<AdjustedRandIndexParam> inputs = {
   {100, 1, 20, true, 0.000001},   {10, 1, 10, true, 0.000001},
   {198, 1, 100, true, 0.000001},  {300, 3, 99, true, 0.000001}};
 
-//writing the test suite
-typedef adjustedRandIndexTest<int> adjustedRandIndexTestClass;
-TEST_P(adjustedRandIndexTestClass, Result) {
+typedef AdjustedRandIndexTest<int, int> ARI_ii;
+TEST_P(ARI_ii, Result) {
   ASSERT_NEAR(computedAdjustedRandIndex, truthAdjustedRandIndex,
               params.tolerance);
 }
-INSTANTIATE_TEST_CASE_P(adjustedRandIndex, adjustedRandIndexTestClass,
-                        ::testing::ValuesIn(inputs));
+INSTANTIATE_TEST_CASE_P(AdjustedRandIndex, ARI_ii, ::testing::ValuesIn(inputs));
 
 }  //end namespace Metrics
 }  //end namespace MLCommon
