bbe6edd7413d245e7e9e33032f78194dc3ac5096
Onur Yilmaz <oyilmaz@nvidia.com>
2020-01-14 12:39:36 -0500

    Updates to address PR reviews

--- python/cuml/linear_model/ridge_mg.pyx
+++ python/cuml/linear_model/ridge_mg.pyx
@@ -162,11 +162,11 @@ class RidgeMG(Ridge):
             free(d[x_i])
         free(d)
 
-    def _build_predData(self, partsToRanks, rnk, n_cols, dtype):
+    def _build_predData(self, partsToSizes, rank, n_cols, dtype):
         arr_interfaces_trans = []
-        for idx, rankSize in enumerate(partsToRanks):
-            rank, size = rankSize
-            if rnk == rank:
+        for idx, rankSize in enumerate(partsToSizes):
+            rk, size = rankSize
+            if rank == rk:
                 trans_ary = rmm.to_device(zeros((size, n_cols),
                                                 order="F",
                                                 dtype=dtype))
@@ -178,27 +178,27 @@ class RidgeMG(Ridge):
 
         return arr_interfaces_trans
 
-    def fit(self, X, y, M, N, partsToRanks, rnk):
+    def fit(self, X, y, n_rows, n_cols, partsToSizes, rank):
         arr_interfaces = []
         for arr in X:
-            X_m, input_ptr, n_rows, self.n_cols, self.dtype = \
+            X_m, input_ptr, n_rows_X, self.n_cols, self.dtype = \
                 input_to_dev_array(arr, check_dtype=[np.float32, np.float64])
             arr_interfaces.append({"obj": X_m,
                                    "data": input_ptr,
-                                   "shape": (n_rows, self.n_cols)})
+                                   "shape": (n_rows_X, self.n_cols)})
 
         arr_interfaces_y = []
         for arr in y:
-            y_m, input_ptr, n_rows, n_cols, self.dtype = \
+            y_m, input_ptr, n_rows_y, n_cols_y, self.dtype = \
                 input_to_dev_array(arr, check_dtype=[np.float32, np.float64])
             arr_interfaces_y.append({"obj": y_m,
                                      "data": input_ptr,
-                                     "shape": (n_rows, n_cols)})
+                                     "shape": (n_rows_y, n_cols_y)})
 
         n_total_parts = 0
-        for idx, rankSize in enumerate(partsToRanks):
-            rank, size = rankSize
-            if rnk == rank:
+        for idx, rankSize in enumerate(partsToSizes):
+            rk, size = rankSize
+            if rank == rk:
                 n_total_parts = n_total_parts + 1
 
         cdef RankSizePair **rankSizePair = <RankSizePair**> \
@@ -208,9 +208,9 @@ class RidgeMG(Ridge):
         indx = 0
         n_part_row = 0
 
-        for idx, rankSize in enumerate(partsToRanks):
-            rank, size = rankSize
-            if rnk == rank:
+        for idx, rankSize in enumerate(partsToSizes):
+            rk, size = rankSize
+            if rank == rk:
                 rankSizePair[indx] = <RankSizePair*> \
                     malloc(sizeof(RankSizePair))
                 rankSizePair[indx].rank = <int>rank
@@ -222,61 +222,61 @@ class RidgeMG(Ridge):
                                        dtype=self.dtype))
         cdef uintptr_t coef_ptr = get_cudf_column_ptr(self.coef_)
 
-        cdef float c_intercept1
-        cdef double c_intercept2
+        cdef float float_intercept
+        cdef double double_intercept
         cdef cumlHandle* handle_ = <cumlHandle*><size_t>self.handle.getHandle()
         cdef uintptr_t data
         cdef uintptr_t labels
-        cdef float c_alpha1
-        cdef double c_alpha2
+        cdef float float_alpha
+        cdef double double_alpha
         # Only one alpha is supported.
         self.n_alpha = 1
 
         if self.dtype == np.float32:
             data = self._build_dataFloat(arr_interfaces)
             labels = self._build_dataFloat(arr_interfaces_y)
-            c_alpha1 = self.alpha
+            float_alpha = self.alpha
 
             fit(handle_[0],
                 <RankSizePair**>rankSizePair,
                 <size_t> n_total_parts,
                 <floatData_t**>data,
-                <size_t>M,
-                <size_t>N,
+                <size_t>n_rows,
+                <size_t>n_cols,
                 <floatData_t**>labels,
-                <float*>&c_alpha1,
+                <float*>&float_alpha,
                 <int>self.n_alpha,
                 <float*>coef_ptr,
-                <float*>&c_intercept1,
+                <float*>&float_intercept,
                 <bool>self.fit_intercept,
                 <bool>self.normalize,
                 <int>self.algo,
                 False)
 
-            self.intercept_ = c_intercept1
+            self.intercept_ = float_intercept
 
         else:
             data = self._build_dataDouble(arr_interfaces)
             labels = self._build_dataDouble(arr_interfaces_y)
-            c_alpha2 = self.alpha
+            double_alpha = self.alpha
 
             fit(handle_[0],
                 <RankSizePair**>rankSizePair,
                 <size_t> n_total_parts,
                 <doubleData_t**>data,
-                <size_t>M,
-                <size_t>N,
+                <size_t>n_rows,
+                <size_t>n_cols,
                 <doubleData_t**>labels,
-                <double*>&c_alpha2,
+                <double*>&double_alpha,
                 <int>self.n_alpha,
                 <double*>coef_ptr,
-                <double*>&c_intercept2,
+                <double*>&double_intercept,
                 <bool>self.fit_intercept,
                 <bool>self.normalize,
                 <int>self.algo,
                 False)
 
-            self.intercept_ = c_intercept2
+            self.intercept_ = double_intercept
 
         self.handle.sync()
 
@@ -294,15 +294,15 @@ class RidgeMG(Ridge):
             self._freeDoubleD(data, arr_interfaces)
             self._freeDoubleD(labels, arr_interfaces_y)
 
-    def fit_colocated(self, input_data, M, N, partsToRanks, rnk):
+    def fit_colocated(self, input_data, n_rows, n_cols, partsToSizes, rank):
         """
         Fit function for MNMG Ridge Regression.
         This not meant to be used as
         part of the public API.
         :param X: array of local dataframes / array partitions
-        :param M: total number of rows
-        :param N: total number of cols
-        :param partsToRanks: array of tuples in the format: [(rank,size)]
+        :param n_rows: total number of rows
+        :param n_cols: total number of cols
+        :param partsToSizes: array of tuples in the format: [(rank,size)]
         :return: self
         """
 
@@ -310,21 +310,21 @@ class RidgeMG(Ridge):
         arr_interfaces_y = []
 
         for i in range(len(input_data)):
-            X_m, input_ptr, n_rows, self.n_cols, self.dtype = \
+            X_m, input_ptr, n_rows_X, self.n_cols, self.dtype = \
                 input_to_dev_array(input_data[i][0],
                                    check_dtype=[np.float32, np.float64])
 
             arr_interfaces.append({"obj": X_m,
                                    "data": input_ptr,
-                                   "shape": (n_rows, self.n_cols)})
+                                   "shape": (n_rows_X, self.n_cols)})
 
-            y_m, input_ptr, n_rows, n_cols, self.dtype = \
+            y_m, input_ptr, n_rows_y, n_cols_y, self.dtype = \
                 input_to_dev_array(input_data[i][1],
                                    check_dtype=[np.float32, np.float64])
 
             arr_interfaces_y.append({"obj": y_m,
                                      "data": input_ptr,
-                                     "shape": (n_rows, n_cols)})
+                                     "shape": (n_rows_y, n_cols_y)})
 
         n_total_parts = len(input_data)
         cdef RankSizePair **rankSizePair = <RankSizePair**> \
@@ -334,68 +334,68 @@ class RidgeMG(Ridge):
         for i in range(len(input_data)):
             rankSizePair[i] = <RankSizePair*> \
                 malloc(sizeof(RankSizePair))
-            rankSizePair[i].rank = <int>rnk
+            rankSizePair[i].rank = <int>rank
             rankSizePair[i].size = <size_t>len(input_data[i][0])
 
         self.coef_ = cudf.Series(zeros(self.n_cols,
                                        dtype=self.dtype))
         cdef uintptr_t coef_ptr = get_cudf_column_ptr(self.coef_)
 
-        cdef float c_intercept1
-        cdef double c_intercept2
+        cdef float float_intercept
+        cdef double double_intercept
         cdef cumlHandle* handle_ = <cumlHandle*><size_t>self.handle.getHandle()
         cdef uintptr_t data
         cdef uintptr_t labels
-        cdef float c_alpha1
-        cdef double c_alpha2
+        cdef float float_alpha
+        cdef double double_alpha
         # Only one alpha is supported.
         self.n_alpha = 1
 
         if self.dtype == np.float32:
             data = self._build_dataFloat(arr_interfaces)
             labels = self._build_dataFloat(arr_interfaces_y)
-            c_alpha1 = self.alpha
+            float_alpha = self.alpha
 
             fit(handle_[0],
                 <RankSizePair**>rankSizePair,
                 <size_t> n_total_parts,
                 <floatData_t**>data,
-                <size_t>M,
-                <size_t>N,
+                <size_t>n_rows,
+                <size_t>n_cols,
                 <floatData_t**>labels,
-                <float*>&c_alpha1,
+                <float*>&float_alpha,
                 <int>self.n_alpha,
                 <float*>coef_ptr,
-                <float*>&c_intercept1,
+                <float*>&float_intercept,
                 <bool>self.fit_intercept,
                 <bool>self.normalize,
                 <int>self.algo,
                 False)
 
-            self.intercept_ = c_intercept1
+            self.intercept_ = float_intercept
 
         else:
             data = self._build_dataDouble(arr_interfaces)
             labels = self._build_dataDouble(arr_interfaces_y)
-            c_alpha2 = self.alpha
+            double_alpha = self.alpha
 
             fit(handle_[0],
                 <RankSizePair**>rankSizePair,
                 <size_t> n_total_parts,
                 <doubleData_t**>data,
-                <size_t>M,
-                <size_t>N,
+                <size_t>n_rows,
+                <size_t>n_cols,
                 <doubleData_t**>labels,
-                <double*>&c_alpha2,
+                <double*>&double_alpha,
                 <int>self.n_alpha,
                 <double*>coef_ptr,
-                <double*>&c_intercept2,
+                <double*>&double_intercept,
                 <bool>self.fit_intercept,
                 <bool>self.normalize,
                 <int>self.algo,
                 False)
 
-            self.intercept_ = c_intercept2
+            self.intercept_ = double_intercept
 
         self.handle.sync()
 
@@ -413,34 +413,34 @@ class RidgeMG(Ridge):
             self._freeDoubleD(data, arr_interfaces)
             self._freeDoubleD(labels, arr_interfaces_y)
 
-    def predict(self, X, M, N, partsToRanks, rnk):
+    def predict(self, X, n_rows, n_cols, partsToSizes, rank):
         """
         Transform function for Ridge Regression MG.
         This not meant to be used as
         part of the public API.
         :param X: array of local dataframes / array partitions
-        :param M: total number of rows
-        :param N: total number of cols
-        :param partsToRanks: array of tuples in the format: [(rank,size)]
+        :param n_rows: total number of rows
+        :param n_cols: total number of cols
+        :param partsToSizes: array of tuples in the format: [(rank,size)]
         :return: self
         """
 
-        if N != self.n_cols:
+        if n_cols != self.n_cols:
             raise Exception("Number of columns of the X has to match with "
                             "number of columns of the data was fit to model.")
 
         arr_interfaces = []
         for arr in X:
-            X_m, input_ptr, n_rows, n_cols, self.dtype = \
+            X_m, input_ptr, n_rows_X, n_cols_X, self.dtype = \
                 input_to_dev_array(arr, check_dtype=[np.float32, np.float64])
             arr_interfaces.append({"obj": X_m,
                                    "data": input_ptr,
-                                   "shape": (n_rows, n_cols)})
+                                   "shape": (n_rows_X, n_cols_X)})
 
         n_total_parts = 0
-        for idx, rankSize in enumerate(partsToRanks):
-            rank, size = rankSize
-            if rnk == rank:
+        for idx, rankSize in enumerate(partsToSizes):
+            rk, size = rankSize
+            if rank == rk:
                 n_total_parts = n_total_parts + 1
 
         cdef RankSizePair **rankSizePair = <RankSizePair**> \
@@ -448,9 +448,9 @@ class RidgeMG(Ridge):
                    * n_total_parts)
 
         indx = 0
-        for idx, rankSize in enumerate(partsToRanks):
-            rank, size = rankSize
-            if rnk == rank:
+        for idx, rankSize in enumerate(partsToSizes):
+            rk, size = rankSize
+            if rank == rk:
                 rankSizePair[indx] = <RankSizePair*> \
                     malloc(sizeof(RankSizePair))
                 rankSizePair[indx].rank = <int>rank
@@ -466,8 +466,8 @@ class RidgeMG(Ridge):
 
         if self.dtype == np.float32:
             data = self._build_dataFloat(arr_interfaces)
-            arr_interfaces_pred = self._build_predData(partsToRanks,
-                                                       rnk,
+            arr_interfaces_pred = self._build_predData(partsToSizes,
+                                                       rank,
                                                        1,
                                                        np.float32)
             pred_data = self._build_dataFloat(arr_interfaces_pred)
@@ -476,8 +476,8 @@ class RidgeMG(Ridge):
                     <RankSizePair**>rankSizePair,
                     <size_t> n_total_parts,
                     <floatData_t**> data,
-                    <size_t>M,
-                    <size_t>N,
+                    <size_t>n_rows,
+                    <size_t>n_cols,
                     <float*> coef_ptr,
                     <float>self.intercept_,
                     <floatData_t**> pred_data,
@@ -485,8 +485,8 @@ class RidgeMG(Ridge):
 
         else:
             data = self._build_dataDouble(arr_interfaces)
-            arr_interfaces_pred = self._build_predData(partsToRanks,
-                                                       rnk,
+            arr_interfaces_pred = self._build_predData(partsToSizes,
+                                                       rank,
                                                        1,
                                                        np.float64)
             pred_data = self._build_dataDouble(arr_interfaces_pred)
@@ -495,8 +495,8 @@ class RidgeMG(Ridge):
                     <RankSizePair**>rankSizePair,
                     <size_t> n_total_parts,
                     <doubleData_t**> data,
-                    <size_t>M,
-                    <size_t>N,
+                    <size_t>n_rows,
+                    <size_t>n_cols,
                     <double*> coef_ptr,
                     <double>self.intercept_,
                     <doubleData_t**> pred_data,
