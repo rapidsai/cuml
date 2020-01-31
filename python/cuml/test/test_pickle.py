db4e73584f930328fc85a8478ebdc74971d3e273
Simon Andersen <sandersen@nvidia.com>
2020-01-31 17:39:03 -0600

    Added tests for ModuleConfig class

--- python/cuml/test/test_pickle.py
+++ python/cuml/test/test_pickle.py
@@ -539,3 +539,59 @@ def test_svr_pickle(tmpdir, datatype, nrows, ncols, n_info):
         assert array_equal(result["svr"], pickled_model.predict(X_test))
 
     pickle_save_load(tmpdir, create_mod, assert_model)
+
+
+def test_module_config():
+    class SomeModule:
+        class SomeClass:
+            pass
+
+        class ExcludedClass:
+            pass
+
+        class CustomConstructorClass:
+            def __init__(self, some_parameter):
+                self.some_parameter = some_parameter
+
+            def __eq__(self, other):
+                return self.some_parameter == other.some_parameter
+
+    module = ModuleConfig(module=SomeModule,
+                          exclude_classes=[SomeModule.ExcludedClass],
+                          custom_constructors={
+                              "CustomConstructorClass":
+                                  lambda: SomeModule.CustomConstructorClass(some_parameter=1)
+                          })
+
+    models = module.get_models()
+    ref = {
+        "SomeClass": SomeModule.SomeClass,
+        "CustomConstructorClass": lambda: SomeModule.CustomConstructorClass(some_parameter=1)
+    }
+
+    # Here we don't do `assert models == ref` because CustomConstructorClass is a lambda.
+    assert len(models) == len(ref)
+    assert models['SomeClass'] == ref['SomeClass']
+    assert models['CustomConstructorClass']() == ref['CustomConstructorClass']()
+
+
+def test_module_config_empty_module():
+    class EmptyModule:
+        pass
+    assert {} == ModuleConfig(EmptyModule).get_models()
+
+
+def test_module_config_parameters():
+    class SomeModule:
+        class SomeClass:
+            def __eq__(self, other):
+                return type(other) == type(self)
+
+    models1 = ModuleConfig(module=SomeModule).get_models()
+    models2 = ModuleConfig(module=SomeModule, exclude_classes=[SomeModule.SomeClass]).get_models()
+    models3 = ModuleConfig(module=SomeModule,
+                           custom_constructors={'SomeClass': lambda: SomeModule.SomeClass()}).get_models()
+
+    assert models1 == {'SomeClass': SomeModule.SomeClass}
+    assert models2 == {}
+    assert len(models3) == 1 and models3['SomeClass']() == SomeModule.SomeClass()
