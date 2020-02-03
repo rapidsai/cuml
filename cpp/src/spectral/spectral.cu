d78bf8024ff3d878e13f19ba334da0b64b8c9ba8
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-02-03 03:26:29 -0800

    removed unnecessary includes of nvgraph_wrappers in cuml header files

--- cpp/src/spectral/spectral.cu
+++ cpp/src/spectral/spectral.cu
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2020, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 
 #include <cuml/cuml.hpp>
 #include "sparse/coo.h"
-#include "sparse/nvgraph_wrappers.h"
 
 #include "sparse/spectral.h"
 
