8e13a83eb14139e824a6fb134b046fed059455d8
Dante Gama Dessavre <danteg@nvidia.com>
2020-02-04 23:16:45 -0600

    FEA input_to_dev_array uses cuml Array to simplify code

--- python/cuml/utils/input_utils.py
+++ python/cuml/utils/input_utils.py
@@ -24,6 +24,8 @@ import warnings
 
 from collections import namedtuple
 from collections.abc import Collection
+from copy import deepcopy
+
 from cuml.utils import rmm_cupy_ary
 from numba import cuda
 
@@ -34,6 +36,8 @@ inp_array = namedtuple('inp_array', 'array pointer n_rows n_cols dtype')
 def get_dev_array_ptr(ary):
     """
     Returns ctype pointer of a numba style device array
+
+    Deprecated: will be removed once all codebase uses cuml Array
     """
     return ary.device_ctypes_pointer.value
 
@@ -41,34 +45,16 @@ def get_dev_array_ptr(ary):
 def get_cudf_column_ptr(col):
     """
     Returns ctype pointer of a cudf column
+
+    Deprecated: will be removed once all codebase uses cuml Array
     """
     return cudf._lib.cudf.get_column_data_ptr(col._column)
-
-
-def get_dtype(X):
-    """
-    Returns dtype of obj as a Numpy style dtype (like np.float32)
-    """
-    if isinstance(X, cudf.DataFrame):
-        dtype = np.dtype(X[X.columns[0]]._column.dtype)
-    elif (isinstance(X, cudf.Series)):
-        dtype = np.dtype(X._column.dtype)
-    elif isinstance(X, np.ndarray):
-        dtype = X.dtype
-    elif cuda.is_cuda_array(X):
-        dtype = X.dtype
-    elif cuda.devicearray.is_cuda_ndarray(X):
-        dtype = X.dtype
-    else:
-        raise TypeError("Input object not understood for dtype detection.")
-
-    return dtype
 
 
 def input_to_dev_array(X, order='F', deepcopy=False,
                        check_dtype=False, convert_to_dtype=False,
                        check_cols=False, check_rows=False,
-                       fail_on_order=False):
+                       fail_on_order=False, legacy=True):
     """
     Convert input X to device array suitable for C++ methods.
 
@@ -115,6 +101,10 @@ def input_to_dev_array(X, order='F', deepcopy=False,
         Set to True if you want the method to raise a ValueError if X is not
         of order `order`.
 
+    legacy: boolean (default: True)
+        If true, return `inp_array` namedtuple, otherwiser return cuml Array.
+        This parameter will be dropped once cuml Array is adopted in all algos.
+
 
     Returns
     -------
@@ -126,75 +116,55 @@ def input_to_dev_array(X, order='F', deepcopy=False,
 
     """
 
+    # temporarily importing here, until github issue #xx reorganizing utils
+    # is dealt with. Otherwise circular import causes issues
+    from cuml.common import Array
+
+    # dtype conversion
+
     if convert_to_dtype:
         X = convert_dtype(X, to_dtype=convert_to_dtype)
         check_dtype = False
 
+    # format conversion
+
+    if (isinstance(X, cudf.Series)):
+        if X.null_count != 0:
+            raise ValueError("Error: cuDF Series has missing/null values")
+
     if isinstance(X, cudf.DataFrame):
         dtype = np.dtype(X[X.columns[0]]._column.dtype)
         if order == 'F':
-            X_m = X.as_gpu_matrix(order='F')
+            X_m = Array(data=X.as_gpu_matrix(order='F'))
         elif order == 'C':
-            X_m = cuml.utils.numba_utils.row_matrix(X)
+            X_m = Array(data=cuml.utils.numba_utils.row_matrix(X))
 
-    elif (isinstance(X, cudf.Series)):
-        if deepcopy:
-            X_m = X.to_gpu_array()
-        else:
-            if X.null_count == 0:
-                # using __cuda_array_interface__ support of cudf.Series for
-                # this temporarily while switching from rmm device_array to
-                # rmm deviceBuffer https://github.com/rapidsai/cuml/issues/1379
-                X_m = cuda.as_cuda_array(X._column)
-            else:
-                raise ValueError("Error: cuDF Series has missing/null values")
-
-    elif isinstance(X, np.ndarray):
-        dtype = X.dtype
-        X_m = rmm.to_device(np.array(X, order=order, copy=False))
-
-    elif cuda.is_cuda_array(X):
-        # Use cuda array interface to create a device array by reference
-        X_m = cuda.as_cuda_array(X)
+    elif cuda.is_cuda_array(X) or isinstance(X, np.ndarray):
+        X_m = Array(data=X)
 
         if deepcopy:
-            out_dev_array = rmm.device_array_like(X_m)
-            out_dev_array.copy_to_device(X_m)
-            X_m = out_dev_array
-
-    elif cuda.devicearray.is_cuda_ndarray(X):
-        if deepcopy:
-            out_dev_array = rmm.device_array_like(X)
-            out_dev_array.copy_to_device(X)
-            X_m = out_dev_array
-        else:
-            X_m = X
+            X_m = deepcopy(X_m)
 
     else:
         msg = "X matrix format " + str(X.__class__) + " not supported"
         raise TypeError(msg)
 
-    dtype = X_m.dtype
-
     if check_dtype:
-        if isinstance(check_dtype, type) or isinstance(check_dtype, np.dtype):
-            if dtype != check_dtype:
-                del X_m
-                raise TypeError("Expected " + str(check_dtype) + "input but" +
-                                " got " + str(dtype) + " instead.")
-        elif isinstance(check_dtype, Collection) and \
-                not isinstance(check_dtype, str):
-            # The 'not isinstance(check_dtype, string)' condition is needed,
-            # because the 'float32' string is a Collection, but in this
-            # branch we only want to process collections like
-            # [np.float32, np.float64].
-            if dtype not in check_dtype:
-                del X_m
-                raise TypeError("Expected input to be of type in " +
-                                str(check_dtype) + " but got " + str(dtype))
-        else:
-            raise ValueError("Expected a type as check_dtype arg, but got " +
-                             str(check_dtype))
+        if not isinstance(check_dtype, list):
+            check_dtype = [check_dtype]
+
+        check_dtype = [np.dtype(dtype) for dtype in check_dtype]
+
+        print(check_dtype)
+        print(X_m.dtype)
+
+        if X_m.dtype not in check_dtype:
+            type_str = X_m.dtype
+            del X_m
+            raise TypeError("Expected input to be of type in " +
+                            str(check_dtype) + " but got " + str(type_str))
+
+    # Checks based on parameters
 
     n_rows = X_m.shape[0]
     if len(X_m.shape) > 1:
@@ -214,7 +184,7 @@ def input_to_dev_array(X, order='F', deepcopy=False,
                              " rows but got " + str(n_rows) +
                              " rows.")
 
-    if not check_numba_order(X_m, order):
+    if X_m.order != order:
         if fail_on_order:
             raise ValueError("Expected " + order_to_str(order) +
                              " major order, but got the opposite.")
@@ -223,21 +193,28 @@ def input_to_dev_array(X, order='F', deepcopy=False,
                           "but got the opposite. Converting data, this will "
                           "result in additional memory utilization.")
             X_m = rmm_cupy_ary(cp.array, X_m, copy=False, order=order)
-            X_m = cuda.as_cuda_array(X_m)
-
-    X_ptr = get_dev_array_ptr(X_m)
-
-    return inp_array(array=X_m, pointer=X_ptr, n_rows=n_rows, n_cols=n_cols,
-                     dtype=dtype)
+            X_m = Array(data=X_m)
 
 
-def convert_dtype(X, to_dtype=np.float32):
+    ptr = X_m.ptr
+    if legacy:
+        X_m = cuda.as_cuda_array(X_m)
+
+    return inp_array(array=X_m, pointer=ptr, n_rows=n_rows, n_cols=n_cols,
+                     dtype=X_m.dtype)
+
+
+def convert_dtype(X, to_dtype=np.float32, legacy=True):
     """
     Convert X to be of dtype `dtype`
 
     Supported float dtypes for overflow checking.
     Todo: support other dtypes if needed.
     """
+
+    # temporarily importing here, until github issue #xx reorganizing utils
+    # is dealt with. Otherwise circular import causes issues
+    from cuml.common import Array
 
     if isinstance(X, np.ndarray):
         dtype = X.dtype
@@ -252,9 +229,12 @@ def convert_dtype(X, to_dtype=np.float32):
         return X.astype(to_dtype)
 
     elif cuda.is_cuda_array(X):
-        X_m = cp.asarray(X)
+        X_m = rmm_cupy_ary(cp.asarray, X)
         X_m = X_m.astype(to_dtype)
-        return cuda.as_cuda_array(X_m)
+        if legacy:
+            return cuda.as_cuda_array(X_m)
+        else:
+            return Array(data=X_m)
 
     else:
         raise TypeError("Received unsupported input type " % type(X))
