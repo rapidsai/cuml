1675284dd5e159c0561f3849d3a5cf3857647f82
Simon Andersen <sandersen@nvidia.com>
2020-02-07 16:28:09 -0600

    Clarify reason for custom_constructor for QN model

--- python/cuml/test/test_pickle.py
+++ python/cuml/test/test_pickle.py
@@ -33,6 +33,8 @@ regression_models = regression_config.get_models()
 
 solver_config = ClassEnumerator(
     module=cuml.solvers,
+    # QN uses softmax here because some of the tests uses multiclass
+    # logistic regression which requires a softmax loss
     custom_constructors={"QN": lambda: cuml.QN(loss="softmax")}
 )
 solver_models = solver_config.get_models()
