2ccb6972e82830069d1af786f62b1d8cf2175f53
John Zedlewski <jzedlewski@nvidia.com>
2020-02-12 10:10:39 -0800

    Updates based on code review

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -59,8 +59,8 @@ conda install -c conda-forge -c rapidsai -c rapidsai-nightly -c rapidsai/label/x
       "dask-cudf=${MINOR_VERSION}" \
       "dask-cuda=${MINOR_VERSION}" \
       "ucx-py=0.12*" \
-      "statsmodels"
-
+      "statsmodels" \
+      "rapids-xgboost>=0.12"
 
 
 # Install the master version of dask, distributed, and dask-ml
