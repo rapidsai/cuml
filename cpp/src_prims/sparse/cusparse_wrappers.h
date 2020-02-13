e50b3f077fa1797966666f8f095eda112d90685b
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-02-12 21:21:00 -0800

    added a reason string to the cusparse assert on failures

--- cpp/src_prims/sparse/cusparse_wrappers.h
+++ cpp/src_prims/sparse/cusparse_wrappers.h
@@ -20,10 +20,11 @@
 
 namespace MLCommon {
 namespace Sparse {
-#define CUSPARSE_CHECK(call)                                               \
-  do {                                                                     \
-    cusparseStatus_t status = call;                                        \
-    ASSERT(status == CUSPARSE_STATUS_SUCCESS, "FAIL: call='%s'\n", #call); \
+#define CUSPARSE_CHECK(call)                                            \
+  do {                                                                  \
+    cusparseStatus_t status = call;                                     \
+    ASSERT(status == CUSPARSE_STATUS_SUCCESS, "FAIL: call='%s' Reason='%s'\n", \
+           #call, cusparseGetErrorString(status));                      \
   } while (0)
 
 /**
