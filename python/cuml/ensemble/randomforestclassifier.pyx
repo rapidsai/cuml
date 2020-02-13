6ee4181df8f68a9aec1e69a57a141a1b8ca6393c
Saloni Jain <sj7856@rit.edu>
2020-02-13 14:57:22 -0600

    fixed typo

--- python/cuml/ensemble/randomforestclassifier.pyx
+++ python/cuml/ensemble/randomforestclassifier.pyx
@@ -609,7 +609,7 @@ class RandomForestClassifier(Base):
                            coalescing-friendly
             'batch_tree_reorg' - similar to tree_reorg but predicting
                                  multiple rows per thread block
-            `algo` - choose the algorithm automatically. Currently
+            `auto` - choose the algorithm automatically. Currently
                      'batch_tree_reorg' is used for dense storage
                      and 'naive' for sparse storage
         threshold : float (default = 0.5)
@@ -741,7 +741,7 @@ class RandomForestClassifier(Base):
                            coalescing-friendly
             'batch_tree_reorg' - similar to tree_reorg but predicting
                                  multiple rows per thread block
-            `algo` - choose the algorithm automatically. Currently
+            `auto` - choose the algorithm automatically. Currently
                      'batch_tree_reorg' is used for dense storage
                      and 'naive' for sparse storage
         threshold : float
