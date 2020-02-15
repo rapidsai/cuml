48d6f3488d605d2bae44fd66ea0ec6d8343f5a66
John Kirkham <jakirkham@gmail.com>
2020-02-15 14:21:00 -0800

    Add framework for registering cuML serializers
    This adds the basic framework for cuML serializers analogous to the one
    in cuDF. Currently it doesn't register anything for serialization.
    However this could be used to serialize cuML Array objects once added.
    Potentially it could be used to serialize other objects as cuML chooses.

---
+++ python/cuml/comm/serialize.py
@@ -0,0 +1,41 @@
+import pickle
+
+import cudf
+
+
+# all (de-)serializtion are attached to cuML Objects
+serializable_classes = ()
+
+
+try:
+    from distributed.protocol import dask_deserialize, dask_serialize
+    from distributed.protocol.cuda import cuda_deserialize, cuda_serialize
+    from distributed.utils import log_errors
+
+    @cuda_serialize.register(serializable_classes)
+    def cuda_serialize_cuml_dataframe(x):
+        with log_errors():
+            header, frames = x.serialize()
+            assert all(isinstance(f, cudf.core.buffer.Buffer) for f in frames)
+            return header, frames
+
+    # all (de-)serializtion are attached to cuML Objects
+    @dask_serialize.register(serializable_classes)
+    def dask_serialize_cuml_dataframe(x):
+        with log_errors():
+            header, frames = x.serialize()
+            frames = [f.to_host_array().data for f in frames]
+            return header, frames
+
+    @cuda_deserialize.register(serializable_classes)
+    @dask_deserialize.register(serializable_classes)
+    def deserialize_cuml_dataframe(header, frames):
+        with log_errors():
+            cuml_typ = pickle.loads(header["type-serialized"])
+            cuml_obj = cuml_typ.deserialize(header, frames)
+            return cuml_obj
+
+
+except ImportError:
+    # distributed is probably not installed on the system
+    pass
