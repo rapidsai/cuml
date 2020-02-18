41489fa97cd7786929f2db23a296842c6c606c1b
Simon Andersen <sandersen@nvidia.com>
2020-02-18 17:02:11 -0600

    Fix minor coding style

--- python/cuml/metrics/cluster/homogeneity_score.pyx
+++ python/cuml/metrics/cluster/homogeneity_score.pyx
@@ -72,6 +72,9 @@ def homogeneity_score(labels_true, labels_pred, handle=None):
       The homogeneity of the predicted labeling given the ground truth.
       Score between 0.0 and 1.0. 1.0 stands for perfectly homogeneous labeling.
     """
+    handle = cuml.common.handle.Handle() if handle is None else handle
+    cdef cumlHandle *handle_ = <cumlHandle*> <size_t> handle.getHandle()
+
     cdef uintptr_t preds_ptr
     cdef uintptr_t ground_truth_ptr
 
