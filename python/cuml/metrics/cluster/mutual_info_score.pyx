9e9450ab43b63489e03204dd5b074951b7ec69dd
Simon Andersen <sandersen@nvidia.com>
2020-02-18 11:32:23 -0600

    Added mutual_info_utils file to avoid code duplication

--- python/cuml/metrics/cluster/mutual_info_score.pyx
+++ python/cuml/metrics/cluster/mutual_info_score.pyx
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2019, NVIDIA CORPORATION.
+# Copyright (c) 2020, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,15 +19,11 @@
 # cython: embedsignature = True
 # cython: language_level = 3
 
-import numpy as np
-import cupy as cp
-
-from libc.stdint cimport uintptr_t
-
 from cuml.common.handle cimport cumlHandle
-from cuml.utils import input_to_dev_array
+
+from cuml.metrics.cluster.mutual_info_utils import prepare_data
 import cuml.common.handle
-cimport cuml.common.cuda
+
 
 cdef extern from "cuml/metrics/metrics.hpp" namespace "ML::Metrics":
     double mutualInfoScore(const cumlHandle &handle,
@@ -55,44 +51,32 @@ def mutual_info_score(labels_true, labels_pred, handle=None):
     dataset when the real ground truth is not known.
 
     The labels in labels_pred and labels_true are assumed to be drawn from a
-    contiguous set (Ex: drawn from { 2, 3, 4}, but not from { 2, 4 }). If your
-    set of labels looks like { 2, 4 }, convert them to something like { 0, 1 }.
+    contiguous set (Ex: drawn from {2, 3, 4}, but not from {2, 4}). If your
+    set of labels looks like {2, 4}, convert them to something like {0, 1}.
 
     Parameters
     ----------
     handle : cuml.Handle
-    labels_pred : int32 NumPy ndarray, int32 Numba device or int32 cudf Series
-       A clustering of the data into disjoint subsets.
-    labels_true : int32 NumPy ndarray, int32 Numba device or int32 cudf Series
-       A clustering of the data into disjoint subsets.
+    labels_pred : array-like (device or host) shape = (n_samples,)
+        A clustering of the data (ints) into disjoint subsets.
+        Acceptable formats: cuDF DataFrame, NumPy ndarray, Numba device
+        ndarray, cuda array interface compliant array like CuPy
+    labels_true : array-like (device or host) shape = (n_samples,)
+        A clustering of the data (ints) into disjoint subsets.
+        Acceptable formats: cuDF DataFrame, NumPy ndarray, Numba device
+        ndarray, cuda array interface compliant array like CuPy
 
     Returns
     -------
     float
       Mutual information, a non-negative value
     """
-    handle = cuml.common.handle.Handle() if handle is None else handle
-    cdef cumlHandle*handle_ = <cumlHandle*> <size_t> handle.getHandle()
-
-    cdef uintptr_t preds_ptr
-    cdef uintptr_t ground_truth_ptr
-
-    preds_m, preds_ptr, n_rows, _, _ = input_to_dev_array(
-        labels_pred,
-        convert_to_dtype=np.int32,
-    )
-
-    ground_truth_m, ground_truth_ptr, _, _, _ = input_to_dev_array(
-        labels_true,
-        convert_to_dtype=np.int32,
-        check_rows=n_rows,
-    )
-
-    cp_ground_truth_m = cp.asarray(ground_truth_m)
-    cp_preds_m = cp.asarray(preds_m)
-
-    lower_class_range = min(cp.min(cp_ground_truth_m), cp.min(cp_preds_m))
-    upper_class_range = max(cp.max(cp_ground_truth_m), cp.max(cp_preds_m))
+    (handle_,
+     ground_truth_ptr, preds_ptr,
+     n_rows,
+     lower_class_range, upper_class_range) = prepare_data(labels_true,
+                                                          labels_pred,
+                                                          handle)
 
     mi = mutualInfoScore(handle_[0],
                          <int*> ground_truth_ptr,
