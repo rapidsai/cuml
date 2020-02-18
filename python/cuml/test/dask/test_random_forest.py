8c7d1779da54316ac3212e882f9ce1d5f902ce5e
Saloni Jain <sj7856@rit.edu>
2020-02-18 11:43:28 -0600

    style fix

--- python/cuml/test/dask/test_random_forest.py
+++ python/cuml/test/dask/test_random_forest.py
@@ -82,7 +82,7 @@ def test_rf_classification_dask(partitions_per_worker, cluster):
         cu_rf_mg_predict = cu_rf_mg.predict(X_test)
 
         acc_score = accuracy_score(cu_rf_mg_predict, y_test, normalize=True)
-        print(acc_score)
+
         assert acc_score > 0.8
 
     finally:
@@ -147,8 +147,8 @@ def test_rf_throws_exceptions(cluster):
         c.close()
 
 
-@pytest.mark.parametrize('partitions_per_worker', [1])
-def test_rf_regression_dask_fil(partitions_per_worker, cluster):
+@pytest.mark.parametrize('partitions_per_worker', [1, 3])
+def test_rf_regression_dask(partitions_per_worker, cluster):
 
     # Use CUDA_VISIBLE_DEVICES to control the number of workers
     c = Client(cluster)
@@ -181,63 +181,20 @@ def test_rf_regression_dask_fil(partitions_per_worker, cluster):
         y_cudf = cudf.Series(y_cudf)
         y_train_df = \
             dask_cudf.from_cudf(y_cudf, npartitions=n_partitions)
-        X_cudf_test = cudf.DataFrame.from_pandas(pd.DataFrame(X_test))
-        X_test_df = \
-            dask_cudf.from_cudf(X_cudf_test, npartitions=n_partitions)
 
         X_train_df, y_train_df = dask_utils.persist_across_workers(
             c, [X_train_df, y_train_df], workers=workers)
 
+        X_test_cudf = cudf.DataFrame.from_gpu_matrix(rmm.to_device(X_test))
         cu_rf_mg = cuRFR_mg(**cu_rf_params)
         cu_rf_mg.fit(X_train_df, y_train_df)
-
-        cu_rf_mg_predict = cu_rf_mg.predict(X_test_df)
+        cu_rf_mg_predict = cu_rf_mg.predict(X_test_cudf)
 
         acc_score = r2_score(cu_rf_mg_predict, y_test)
 
         print(str(acc_score))
 
         assert acc_score >= 0.70
-
-    finally:
-        c.close()
-
-
-@pytest.mark.parametrize('partitions_per_worker', [1])
-def test_rf_classification_dask_fil(partitions_per_worker, cluster):
-
-    # Use CUDA_VISIBLE_DEVICES to control the number of workers
-    c = Client(cluster)
-
-    try:
-
-        X, y = make_classification(n_samples=10000, n_features=30,
-                                   n_clusters_per_class=1, n_informative=20,
-                                   random_state=123, n_classes=2)
-
-        X = X.astype(np.float32)
-        y = y.astype(np.int32)
-
-        X_train, X_test, y_train, y_test = \
-            train_test_split(X, y, test_size=1000)
-
-        cu_rf_params = {
-            'n_estimators': 25,
-            'max_depth': 13,
-            'n_bins': 15,
-        }
-
-        X_train_df, y_train_df = _prep_training_data(c, X_train, y_train,
-                                                     partitions_per_worker)
-        X_test_df, _ = _prep_training_data(c, X_test, y_test,
-                                           partitions_per_worker)
-        cu_rf_mg = cuRFC_mg(**cu_rf_params)
-        cu_rf_mg.fit(X_train_df, y_train_df)
-        cu_rf_mg_predict = cu_rf_mg.predict(X_test_df)
-
-        acc_score = accuracy_score(cu_rf_mg_predict, y_test, normalize=True)
-
-        assert acc_score > 0.8
 
     finally:
         c.close()
