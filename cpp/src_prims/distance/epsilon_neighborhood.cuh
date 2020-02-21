a10262a07abb6bb6b22edf092b3d8075c54a1615
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-02-21 01:56:09 -0800

    initial code for eps-neigh class based on contractions

--- cpp/src_prims/distance/epsilon_neighborhood.cuh
+++ cpp/src_prims/distance/epsilon_neighborhood.cuh
@@ -17,6 +17,7 @@
 #pragma once
 
 #include "distance.h"
+#include <linalg/contractions.cuh>
 
 namespace MLCommon {
 namespace Distance {
@@ -79,6 +80,118 @@ size_t epsilon_neighborhood(const T *a, const T *b, bool *adj, Index_ m,
                                            worksize, stream, lambda);
 }
 /** @} */
+
+template <typename DataT, typename IdxT, typename Policy, typename FusedOp,
+          typename BaseClass = LinAlg::Contractions_NT<DataT, IdxT, Policy>>
+struct EpsUnexpL2SqNeighborhood : public BaseClass {
+ private:
+  typedef Policy P;
+
+  bool* adj;
+  DataT eps;
+
+  FusedOp fusedOp;
+
+  DataT acc[P::AccRowsPerTh][P::AccColsPerTh];
+
+ public:
+  DI EpsUnexpL2SqNeighborhood(bool* _adj, const DataT* _x, const DataT* _y,
+                              IdxT _m, IdxT _n, IdxT _k, DataT _eps,
+                              FusedOp fop, char* _smem)
+    : BaseClass(_x, _y, _m, _n, _k, _smem), adj(_adj), eps(_eps), fusedOp(fop) {
+  }
+
+  DI void run() {
+    prolog();
+    loop();
+    __syncthreads();  // so that we can safely reuse smem
+    epilog();
+  }
+
+ private:
+  DI void prolog() {
+    this->ldgsts(0);
+    this->pageWr ^= 1;
+#pragma unroll
+    for (int i = 0; i < P::AccRowsPerTh; ++i) {
+#pragma unroll
+      for (int j = 0; j < P::AccColsPerTh; ++j) {
+        acc[i][j] = BaseClass::Zero;
+      }
+    }
+    __syncthreads();
+  }
+
+  DI void loop() {
+    for (int kidx = P::Kblk; kidx < this->k; kidx += P::Kblk) {
+      this->ldgsts(kidx);
+      accumulate();  // on the previous k-block
+      __syncthreads();
+      this->pageWr ^= 1;
+      this->pageRd ^= 1;
+    }
+    accumulate();  // last iteration
+  }
+
+  DI void epilog() {
+    IdxT startx = blockIdx.x * P::Mblk;
+    IdxT starty = blockIdx.y * P::Nblk;
+#pragma unroll
+    for (int i = 0; i < P::AccRowsPerTh; ++i) {
+      auto xid = startx + i * P::AccThRows;
+#pragma unroll
+      for (int j = 0; j < P::AccColsPerTh; ++j) {
+        auto is_neigh = acc[i][j] <= eps;
+        auto yid = starty + j * P::AccThCols;
+        ///@todo: fix uncoalesced writes using shared mem
+        if (xid < this->m && yid < this->n) {
+          adj[xid * this->n + yid] = is_neigh;
+          fusedOp(is_neigh, xid, yid);
+        }
+      }
+    }
+  }
+
+  DI void accumulate() {
+#pragma unroll
+    for (int ki = 0; ki < P::Kblk; ki += P::Veclen) {
+      this->ldsXY(ki);
+#pragma unroll
+      for (int i = 0; i < P::AccRowsPerTh; ++i) {
+#pragma unroll
+        for (int j = 0; j < P::AccColsPerTh; ++j) {
+#pragma unroll
+          for (int v = 0; v < P::Veclen; ++v) {
+            auto diff = this->regx[i][v] - this->regy[j][v];
+            acc[i][j] += diff * diff;
+          }
+        }
+      }
+    }
+  }
+};  // struct EpsUnexpL2SqNeighborhood
+
+template <typename DataT, typename IdxT, typename Policy, typename FusedOp>
+__global__ __launch_bounds__(Policy::Nthreads, 2) void epsUnexpL2SqNeighKernel(
+  bool* adj, const DataT* x, const DataT* y, IdxT m, IdxT n, IdxT k, DataT eps,
+  FusedOp fop) {
+  extern __shared__ char smem[];
+  EpsUnexpL2SqNeighborhood<DataT, IdxT, Policy, FusedOp> obj(
+    adj, x, y, m, n, k, eps, fop, smem);
+  obj.run();
+}
+
+template <typename DataT, typename IdxT, int VecLen, typename FusedOp>
+void espUnexpL2SqNeighImpl(bool* adj, const DataT* x, const DataT* y, IdxT m,
+                           IdxT n, IdxT k, DataT eps, FusedOp fop,
+                           cudaStream_t stream) {
+  typedef typename LinAlg::Policy4x4<DataT, VecLen>::Policy Policy;
+  dim3 grid(ceildiv<int>(m, Policy::Mblk), ceildiv<int>(n, Policy::Nblk));
+  dim3 blk(Policy::Nthreads);
+  epsUnexpL2SqNeighKernel<DataT, IdxT, Policy, FusedOp>
+    <<<grid, blk, Policy::SmemSize, stream>>>(adj, x, y, m, n, k, eps, fop);
+  CUDA_CHECK(cudaGetLastError());
+}
 
 }  // namespace Distance
 }  // namespace MLCommon
