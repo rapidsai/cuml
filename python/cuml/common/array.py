8b66db7d288c54f39dbfb70e28523c21f342d5f7
Dante Gama Dessavre <dante.gamadessavre@gmail.com>
2020-02-23 10:01:08 -0600

    FIX Use with_cupy_rmm decorator

--- python/cuml/common/array.py
+++ python/cuml/common/array.py
@@ -19,7 +19,7 @@ import numpy as np
 
 from rmm import DeviceBuffer
 from cudf.core import Buffer, Series, DataFrame
-from cuml.utils import rmm_cupy_ary
+from cuml.utils import with_cupy_rmm
 from cuml.utils.memory_utils import _get_size_from_shape
 from cuml.utils.memory_utils import _order_to_strides
 from cuml.utils.memory_utils import _strides_to_order
@@ -172,6 +172,7 @@ class CumlArray(Buffer):
         }
         return output
 
+    @with_cupy_rmm
     def to_output(self, output_type='cupy'):
         """
         Convert array to output format
@@ -199,7 +200,7 @@ class CumlArray(Buffer):
                 output_type = 'series'
 
         if output_type == 'cupy':
-            return rmm_cupy_ary(cp.asarray, self)
+            return cp.asarray(self)
 
         elif output_type == 'numba':
             return cuda.as_cuda_array(self)
