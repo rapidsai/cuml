d2c67832bd01c5e2ae717c1c0eba01ba0cb050b2
divyegala <divyegala@gmail.com>
2020-02-24 16:57:01 -0600

    style fix

--- python/cuml/test/test_linear_model.py
+++ python/cuml/test/test_linear_model.py
@@ -291,9 +291,10 @@ def test_logistic_regression_predict_proba(dtype, nrows, column_info,
     culog.fit(X_train, y_train)
 
     if num_classes > 2:
-        sklog = skLog(fit_intercept=fit_intercept, solver="lbfgs", multi_class="multinomial")
+        sklog = skLog(fit_intercept=fit_intercept, solver="lbfgs",
+                      multi_class="multinomial")
     else:
-        sklog=skLog(fit_intercept=fit_intercept)
+        sklog = skLog(fit_intercept=fit_intercept)
     sklog.coef_ = culog.coef_.copy_to_host().T
     if fit_intercept:
         sklog.intercept_ = culog.intercept_.copy_to_host()
