6e17aaabb944fe0759149ffe088ef7fb81b6f621
Simon Andersen <sandersen@nvidia.com>
2020-02-28 17:35:11 -0600

    Add entropy metric for clusters

--- python/cuml/test/test_metrics.py
+++ python/cuml/test/test_metrics.py
@@ -31,10 +31,13 @@ from numpy.testing import assert_almost_equal
 from sklearn.datasets import make_classification
 from sklearn.metrics import accuracy_score as sk_acc_score
 from sklearn.metrics.cluster import adjusted_rand_score as sk_ars
+from cuml.metrics.cluster import entropy
 from sklearn.preprocessing import StandardScaler
 
 from cuml.metrics.regression import mean_squared_error
 from sklearn.metrics.regression import mean_squared_error as sklearn_mse
+
+from scipy.stats import entropy as sp_entropy
 
 
 @pytest.mark.parametrize('datatype', [np.float32, np.float64])
@@ -217,3 +220,31 @@ def test_mean_squared_error_custom_weights():
     skl_mse = sklearn_mse(y_true, y_pred, sample_weight=weights)
 
     assert_almost_equal(mse, skl_mse, decimal=2)
+
+
+@pytest.mark.parametrize('use_handle', [True, False])
+def test_entropy(use_handle):
+    handle, stream = get_handle(use_handle)
+
+    # Bernoulli trial with different p.
+    # The outcome of a fair coin is the most uncertain:
+    # in base 2 the result is 1 (One bit of entropy).
+    pk = np.array([1, 1])
+    assert_almost_equal(entropy(pk, base=2., handle=handle), 1.)
+
+    # The outcome of a biased coin is less uncertain:
+    pk = np.array([9, 1])
+    # base e
+    assert_almost_equal(entropy(pk, handle=handle), 0.32508297339144826)
+
+
+@pytest.mark.parametrize('n_samples', [50, stress_param(500000)])
+@pytest.mark.parametrize('base', [None, 2, 10, 50])
+@pytest.mark.parametrize('use_handle', [True, False])
+def test_entropy_random(n_samples, base, use_handle):
+    handle, stream = get_handle(use_handle)
+
+    pk, _ = generate_random_labels(lambda rng: rng.randint(0, 1000, n_samples))
+    S = entropy(pk, base, handle=handle)
+    sp_S = sp_entropy(pk, base)
+    assert_almost_equal(S, sp_S, decimal=2)
