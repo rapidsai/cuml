fec9a5ead86b224d8a1e5214543f720021160c8e
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-02-27 20:52:19 -0800

    fixed a compilation issue with the unaryOp template argument order in subtractScalar

--- cpp/src_prims/linalg/subtract.h
+++ cpp/src_prims/linalg/subtract.h
@@ -41,7 +41,7 @@ template <typename InT, typename OutT = InT, typename IdxType = int>
 void subtractScalar(OutT *out, const InT *in, InT scalar, IdxType len,
                     cudaStream_t stream) {
   auto op = [scalar] __device__(InT in) { return OutT(in - scalar); };
-  unaryOp<InT, decltype(op), OutT, IdxType>(out, in, len, op, stream);
+  unaryOp<InT, decltype(op), IdxType, OutT>(out, in, len, op, stream);
 }
 
 /**
