bfece458a680d6f0838f396c745e7508aa3b0a08
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-02-27 21:36:47 -0800

    removed the old unused eps-neigh methods

--- cpp/src_prims/distance/epsilon_neighborhood.cuh
+++ cpp/src_prims/distance/epsilon_neighborhood.cuh
@@ -18,69 +18,9 @@
 
 #include <common/device_utils.cuh>
 #include <linalg/contractions.cuh>
-#include "distance.h"
 
 namespace MLCommon {
 namespace Distance {
-
-/**
- * @defgroup EpsNeigh Epsilon Neighborhood computation
- * @{
- * @brief Constructs an epsilon neighborhood adjacency matrix by filtering the
- *        final distance by some epsilon.
- *
- * @tparam distanceType distance metric to compute between a and b matrices
- * @tparam T            the type of input matrices a and b
- * @tparam Lambda       Lambda function
- * @tparam Index_       Index type
- * @tparam OutputTile_  output tile size per thread
- *
- * @param a         first matrix [row-major] [on device] [dim = m x k]
- * @param b         second matrix [row-major] [on device] [dim = n x k]
- * @param adj       a boolean output adjacency matrix [row-major] [on device]
- *                  [dim = m x n]
- * @param m         number of points in a
- * @param n         number of points in b
- * @param k         dimensionality
- * @param eps       epsilon value to use as a filter for neighborhood
- *                  construction. It is important to note that if the distance
- *                  type returns a squared variant for efficiency, epsilon will
- *                  need to be squared as well.
- * @param workspace temporary workspace needed for computations
- * @param worksize  number of bytes of the workspace
- * @param stream    cuda stream
- * @param fused_op  optional functor taking the output index into c
- *                  and a boolean denoting whether or not the inputs are part of
- *                  the epsilon neighborhood.
- * @return          the workspace size in bytes
- */
-template <DistanceType distanceType, typename T, typename Lambda,
-          typename Index_ = int, typename OutputTile_ = OutputTile_8x128x128>
-size_t epsilon_neighborhood(const T* a, const T* b, bool* adj, Index_ m,
-                            Index_ n, Index_ k, T eps, void* workspace,
-                            size_t worksize, cudaStream_t stream,
-                            Lambda fused_op) {
-  auto epsilon_op = [n, eps, fused_op] __device__(T val, Index_ global_c_idx) {
-    bool acc = val <= eps;
-    fused_op(global_c_idx, acc);
-    return acc;
-  };
-  distance<distanceType, T, T, bool, OutputTile_, decltype(epsilon_op), Index_>(
-    a, b, adj, m, n, k, (void*)workspace, worksize, epsilon_op, stream);
-  return worksize;
-}
-
-template <DistanceType distanceType, typename T, typename Index_ = int,
-          typename OutputTile_ = OutputTile_8x128x128>
-size_t epsilon_neighborhood(const T* a, const T* b, bool* adj, Index_ m,
-                            Index_ n, Index_ k, T eps, void* workspace,
-                            size_t worksize, cudaStream_t stream) {
-  auto lambda = [] __device__(Index_ c_idx, bool acc) {};
-  return epsilon_neighborhood<distanceType, T, decltype(lambda), Index_,
-                              OutputTile_>(a, b, adj, m, n, k, eps, workspace,
-                                           worksize, stream, lambda);
-}
-/** @} */
 
 template <typename DataT, typename IdxT, typename Policy,
           typename BaseClass = LinAlg::Contractions_NT<DataT, IdxT, Policy>>
