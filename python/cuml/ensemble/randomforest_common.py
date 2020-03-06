069fc368c419f36987cde6fd613884c42cc5efd8
salonijain27 <salj7856@gmail.com>
2020-03-05 18:46:10 -0600

    fixed style errors

--- python/cuml/ensemble/randomforest_common.py
+++ python/cuml/ensemble/randomforest_common.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2019-2020, NVIDIA CORPORATION.
+# Copyright (c) 2020, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 
 def _check_fil_parameter_validity(depth, storage_format, algo):
     if (depth > 16 and (storage_format == 'DENSE' or
