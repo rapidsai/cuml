eea9f2245c1a49f4016bf9745bd730f9626f7c7a
Jiwei Liu <jiweil@dgx11.aselab.nvidia.com>
2020-03-06 08:13:20 -0800

    flake8 style fixed

--- python/cuml/metrics/_ranking.py
+++ python/cuml/metrics/_ranking.py
@@ -20,28 +20,29 @@ from cuml.utils.memory_utils import with_cupy_rmm
 from cuml.utils import input_to_cuml_array
 import math
 
+
 @with_cupy_rmm
 def roc_auc_score(y_true, y_score):
     """
     Compute Area Under the Receiver Operating Characteristic Curve (ROC AUC)
     from prediction scores.
-    Note: this implementation can only be used with binary classification for now.
+    Note: this implementation can only be used with binary classification.
     Parameters
     ----------
         y_true : array-like of shape (n_samples,)
         True labels. The binary cases
         expect labels with shape (n_samples,)
-        
-    y_score : array-like of shape (n_samples,) 
+
+    y_score : array-like of shape (n_samples,)
         Target scores. In the binary cases, these can be either
         probability estimates or non-thresholded decision values (as returned
         by `decision_function` on some classifiers). The binary
         case expects a shape (n_samples,), and the scores must be the scores of
-        the class with the greater label. 
+        the class with the greater label.
     Returns
     -------
         auc : float
-        
+
     Examples
     --------
     >>> import numpy as np
@@ -49,39 +50,41 @@ def roc_auc_score(y_true, y_score):
     >>> y_true = np.array([0, 0, 1, 1])
     >>> y_scores = np.array([0.1, 0.4, 0.35, 0.8])
     >>> roc_auc_score(y_true, y_scores)
-    0.75    
+    0.75
     """
-    
+
     y_true, n_rows, n_cols, ytype = \
-        input_to_cuml_array(y_true, check_dtype=[np.int32, np.int64, np.float32, np.float64])
+        input_to_cuml_array(y_true, check_dtype=[np.int32, np.int64,
+                                                 np.float32, np.float64])
 
     y_score, _, _, _ = \
-        input_to_cuml_array(y_score, check_dtype=[np.int32, np.int64, np.float32, np.float64],
+        input_to_cuml_array(y_score, check_dtype=[np.int32, np.int64,
+                                                  np.float32, np.float64],
                             check_rows=n_rows, check_cols=n_cols)
-    
+
     return _binary_roc_auc_score(y_true, y_score)
 
-def _binary_roc_auc_score(y_true, y_score):    
+
+def _binary_roc_auc_score(y_true, y_score):
     """Compute binary roc_auc_score using cupy"""
     y_true = y_true.to_output()
     y_score = y_score.to_output()
-    
-    if cp.unique(y_true).shape[0]==1:
+
+    if cp.unique(y_true).shape[0] == 1:
         raise ValueError("roc_auc_score cannot be used when "
-               "only one class present in y_true. ROC AUC score "
-                              "is not defined in that case.") 
-        
+                         "only one class present in y_true. ROC AUC score "
+                         "is not defined in that case.")
+
     if y_true.dtype.kind == 'f' and np.any(y_true != y_true.astype(int)):
         raise ValueError("Continuous format of y_true  "
-               "is not supported by roc_auc_score")
-        
-    if cp.unique(y_score).shape[0]==1:
+                         "is not supported by roc_auc_score")
+
+    if cp.unique(y_score).shape[0] == 1:
         return 0.5
-  
+
     y_true = y_true.astype('float32')
-    ids = cp.argsort(-y_score) # we want descedning order
-  
-    
+    ids = cp.argsort(-y_score)  # we want descedning order
+
     sorted_score = y_score[ids]
     ones = y_true[ids]
     zeros = 1 - ones
@@ -89,39 +92,43 @@ def _binary_roc_auc_score(y_true, y_score):
     mask = cp.empty(sorted_score.shape, dtype=cp.bool_)
     mask[0] = True
     mask[1:] = sorted_score[1:] != sorted_score[:-1]
-  
+
     mask = mask.astype('int32')
-    group = cp.cumsum(mask,dtype=cp.int32)
-  
+    group = cp.cumsum(mask, dtype=cp.int32)
+
     sum_ones = cp.sum(ones)
     sum_zeros = cp.sum(zeros)
-  
+
     num = int(group[-1])
-  
-    tps = cp.zeros(num).astype('float32') # true positives
-    fps = cp.zeros(num).astype('float32') # false positives
-  
+
+    tps = cp.zeros(num).astype('float32')  # true positives
+    fps = cp.zeros(num).astype('float32')  # false positives
+
     update_counter_kernel = cp.RawKernel(r'''
         extern "C" __global__
-        void update_counter(const int* group, const float* truth, float* counter, int N) {
+        void update_counter(const int* group, const float* truth,
+            float* counter, int N)
+        {
             int tid = blockDim.x * blockIdx.x + threadIdx.x;
             if(tid<N){
                 atomicAdd(counter + group[tid] - 1, truth[tid]);
             }
         }
     ''', 'update_counter')
-    
+
     N = ones.shape[0]
-    tpb = 128 
+    tpb = 128
     bpg = math.ceil(N/tpb)
-    update_counter_kernel((bpg,), (tpb,), (group, ones, tps, N))  # grid, block and arguments
+    update_counter_kernel((bpg,), (tpb,),
+                          (group, ones, tps, N))  # grid, block and arguments
     update_counter_kernel((bpg,), (tpb,), (group, zeros, fps, N))
-    
+
     tpr = cp.cumsum(tps)/sum_ones
     fpr = cp.cumsum(fps)/sum_zeros
-    
-    return _calculate_area_under_curve(fpr,tpr)
 
-def _calculate_area_under_curve(fpr,tpr):
+    return _calculate_area_under_curve(fpr, tpr)
+
+
+def _calculate_area_under_curve(fpr, tpr):
     """helper function to calculate area under curve given fpr & tpr arrays"""
     return cp.sum((fpr[1:]-fpr[:-1])*(tpr[1:]+tpr[:-1]))/2 + tpr[0]*fpr[0]/2
