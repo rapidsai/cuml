7d187681e96e8161f349cf1139cad01e5edc2ab6
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-03-13 08:33:37 -0700

    initial version of cuml logger class based on spdlog

---
+++ cpp/src/common/logger.cpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#define SPDLOG_HEADER_ONLY
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+#include "logger.h"
+
+namespace ML {
+
+std::string format(const char* fmt, va_list& vl) {
+  char buf[4096];
+  vsnprintf(buf, sizeof(buf), fmt, vl);
+  return std::string(buf);
+}
+
+std::string format(const char* fmt, ...) {
+  va_list vl;
+  va_start(vl, fmt);
+  std::string str = format(fmt, vl);
+  va_end(vl);
+  return str;
+}
+
+const std::string Logger::DefaultPattern("[%L] [%H:%M:%S.%f] %v");
+
+Logger& Logger::get() {
+  static Logger logger;
+  return logger;
+}
+
+Logger::Logger() : logger{spdlog::stdout_color_mt("cuml")} {
+  logger->set_pattern(DefaultPattern);
+  setLevel(CUML_LEVEL_INFO);
+}
+
+void Logger::setLevel(int level) {
+  logger->set_level(static_cast<spdlog::level::level_enum>(level));
+}
+
+void Logger::setPattern(const std::string& pattern) {
+  logger->set_pattern(pattern);
+}
+
+void Logger::log(int level, const char *fmt, ...) {
+  auto level_e = static_cast<spdlog::level::level_enum>(level);
+  // explicit check to make sure that we only expand messages when required
+  if (logger->should_log(level_e)) {
+    va_list vl;
+    va_start(vl, fmt);
+    auto msg = format(fmt, vl);
+    va_end(vl);
+    logger->log(level_e, msg);
+  }
+}
+
+}  // namespace ML
