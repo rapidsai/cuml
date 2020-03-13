c9525802677a85be9894cc597d306dd7c4737ea0
Nanthini <nathanb@nvidia.com>
2020-03-13 18:43:50 +0000

    restructure plotting utils and add copyright

--- python/cuml/__init__.py
+++ python/cuml/__init__.py
@@ -68,10 +68,7 @@ from cuml.svm import SVR
 from cuml.tsa import stationarity
 from cuml.tsa.holtwinters import ExponentialSmoothing
 
-from cuml.experimental import utils
-
 from cuml.utils.pointer_utils import device_of_gpu_matrix
-
 
 # Output type configuration
 
--- python/cuml/experimental/hyperopt_utils/__init__.py
+++ python/cuml/experimental/hyperopt_utils/__init__.py
@@ -13,3 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+from cuml.experimental.hyperopt_utils import plotting_utils
