93fda062a37eb29099ef1b8525ea372c625f2661
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-03-15 21:03:22 -0700

    updated tsne test to not be verbose

--- cpp/test/sg/tsne_test.cu
+++ cpp/test/sg/tsne_test.cu
@@ -51,7 +51,8 @@ class TSNETest : public ::testing::Test {
                              n * 2);
 
     // Test Barnes Hut
-    TSNE_fit(handle, X_d.data(), Y_d.data(), n, p, 2, 90);
+    TSNE_fit(handle, X_d.data(), Y_d.data(), n, p, 2, 90, 0.5, 0.0025, 50, 100,
+             1e-5, 12, 250, 0.01, 200, 500, 1000, 1e-7, 0.5, 0.8, -1, false);
 
     // Move embeddings to host.
     // This can be used for printing if needed.
@@ -82,7 +83,7 @@ class TSNETest : public ::testing::Test {
 
     // Test Exact TSNE
     TSNE_fit(handle, X_d.data(), Y_d.data(), n, p, 2, 90, 0.5, 0.0025, 50, 100,
-             1e-5, 12, 250, 0.01, 200, 500, 1000, 1e-7, 0.5, 0.8, -1, true,
+             1e-5, 12, 250, 0.01, 200, 500, 1000, 1e-7, 0.5, 0.8, -1, false,
              false, false);
 
     MLCommon::updateHost(&embeddings_h[0], Y_d.data(), n * 2,
