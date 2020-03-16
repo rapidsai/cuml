68f95828ad6f8abe8aefd599e8f8e4b4f2e76507
Corey J. Nolet <cjnolet@gmail.com>
2020-03-16 14:12:15 -0700

    Debugging

--- python/cuml/test/dask/test_comms.py
+++ python/cuml/test/dask/test_comms.py
@@ -117,7 +117,6 @@ def test_allreduce(cluster):
 
 @pytest.mark.ucx
 @pytest.mark.parametrize("n_trials", [5])
-@pytest.mark.skip("ucx functionality available in cuML 0.12+")
 def test_send_recv(n_trials, ucx_cluster):
 
     client = Client(ucx_cluster)
@@ -146,10 +145,9 @@ def test_send_recv(n_trials, ucx_cluster):
 
 @pytest.mark.ucx
 @pytest.mark.parametrize("n_trials", [5])
-@pytest.mark.skip("ucx functionality available in cuML 0.12+")
-def test_recv_any_rank(n_trials, ucx_cluster):
+def test_recv_any_rank(n_trials, cluster):
 
-    client = Client(ucx_cluster)
+    client = Client(cluster)
 
     try:
 
