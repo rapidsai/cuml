df601b8ac5fa2fcbf28f7fd08b0c7d616bf4dd6c
divyegala <divyegala@gmail.com>
2020-03-19 22:28:03 -0500

    satisfying flake8 while i am at it

--- python/cuml/test/dask/test_input_utils.py
+++ python/cuml/test/dask/test_input_utils.py
@@ -1,11 +1,8 @@
 import pytest
-import dask.array as da
-import dask.dataframe as ddf
-import cudf
-from dask_cudf.core import DataFrame as dcDataFrame
 from cuml.dask.datasets.blobs import make_blobs
 from cuml.dask.common.input_utils import _extract_partitions
 from dask.distributed import Client
+
 
 @pytest.mark.mg
 @pytest.mark.parametrize("nrows", [1e4])
@@ -13,12 +10,14 @@ from dask.distributed import Client
 @pytest.mark.parametrize("n_parts", [2, 23])
 @pytest.mark.parametrize("input_type", ["dataframe", "array"])
 @pytest.mark.parametrize("colocated", [True, False])
-def test_extract_partitions_worker_list(nrows, ncols, n_parts, input_type, colocated, cluster):
+def test_extract_partitions_worker_list(nrows, ncols, n_parts, input_type,
+                                        colocated, cluster):
     client = Client(cluster)
 
     try:
-        X, y = make_blobs(nrows=nrows, ncols=ncols, n_parts=n_parts, output=input_type)
-        
+        X, y = make_blobs(nrows=nrows, ncols=ncols, n_parts=n_parts,
+                          output=input_type)
+
         if colocated:
             gpu_futures = client.sync(_extract_partitions, (X, y), client)
         else:
