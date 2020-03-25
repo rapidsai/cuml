f696f2681753d7c3c505922b09951fce760ae224
Corey J. Nolet <cjnolet@gmail.com>
2020-03-25 15:28:17 -0400

    Delaying futures for array.

--- python/cuml/dask/datasets/blobs.py
+++ python/cuml/dask/datasets/blobs.py
@@ -166,13 +166,13 @@ def make_blobs(nrows, ncols, centers=8, n_parts=None, cluster_std=1.0,
 
     elif output == 'array':
 
-        X = [da.from_delayed(chunk, shape=(worker_rows[idx], ncols),
+        X = [da.from_delayed(dask.delayed(chunk), shape=(worker_rows[idx], ncols),
                              dtype=dtype,
-                             meta=cp.zeros((1,), dtype=cp.float32))
+                             meta=cp.zeros((1), dtype=cp.float32))
              for idx, chunk in enumerate(X)]
-        Y = [da.from_delayed(chunk, shape=(worker_rows[idx],),
+        Y = [da.from_delayed(dask.delayed(chunk), shape=(worker_rows[idx],),
                              dtype=dtype,
-                             meta=cp.zeros((1,), dtype=cp.float32))
+                             meta=cp.zeros((1), dtype=cp.float32))
              for idx, chunk in enumerate(Y)]
 
         X = da.concatenate(X, axis=0)
