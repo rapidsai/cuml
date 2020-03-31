edc823acce4fc66cce134a3cf5eb889f0659cff1
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-03-31 00:40:24 -0700

    explicitly add doxygen in the conda install command inside ci/gpu/build.sh

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -61,7 +61,8 @@ conda install -c conda-forge -c rapidsai -c rapidsai-nightly -c nvidia \
       "ucx-py=${MINOR_VERSION}" \
       "statsmodels" \
       "xgboost====1.0.2dev.rapidsai0.13" \
-      "lightgbm"
+      "lightgbm" \
+      "doxygen"
 
 
 # Install the master version of dask, distributed, and dask-ml
