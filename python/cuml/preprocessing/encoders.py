492c8798e5e8b4eda424d97496e9f85b897e9cfe
Simon Andersen <sandersen@nvidia.com>
2020-04-15 17:14:50 -0500

    Tests with cupy input for all features

--- python/cuml/preprocessing/encoders.py
+++ python/cuml/preprocessing/encoders.py
@@ -112,47 +112,141 @@ class OneHotEncoder:
                    "with appropriate arguments before using this estimator.")
             raise NotFittedError(msg)
 
+    def _take_feature(self, collection, key):
+        """Helper to handle both df and array as input"""
+        if self.input_type == 'df':
+            return collection[key]
+        else:
+            return collection[:, key]
+
     def _compute_drop_idx(self):
         if self.drop is None:
             return None
         elif isinstance(self.drop, str) and self.drop == 'first':
-            return {feature: 0 for feature in self._encoders.keys()}
-        elif isinstance(self.drop, dict):
-            if len(self.drop.keys()) != len(self._encoders):
+            if self.input_type == 'df':
+                return {feature: 0 for feature in self._encoders.keys()}
+            else:
+                return cp.zeros(shape=(len(self._encoders),), dtype=cp.int32)
+        elif isinstance(self.drop, (dict, list)):
+            if self.input_type == 'df':
+                drop_columns = self.drop.keys()
+                drop_idx = dict()
+                make_collection, get_size = Series, len
+            else:
+                drop_columns = range(len(self.drop))
+                drop_idx = cp.empty(shape=(len(drop_columns),), dtype=cp.int32)
+                make_collection, get_size = cp.array, cp.size
+
+            if len(drop_columns) != len(self._encoders):
                 msg = ("`drop` should have as many columns as the number "
                        "of features ({}), got {}")
                 raise ValueError(msg.format(len(self._encoders),
-                                            len(self.drop.keys())))
-            drop_idx = dict()
-            for feature in self.drop.keys():
-                self.drop[feature] = Series(self.drop[feature])
-                if len(self.drop[feature]) != 1:
+                                            len(drop_columns)))
+            for feature in drop_columns:
+                drop_feature = make_collection(self.drop[feature])
+                if get_size(drop_feature) != 1:
                     msg = ("Trying to drop multiple values for feature {}, "
                            "this is not supported.").format(feature)
                     raise ValueError(msg)
                 cats = self._encoders[feature].classes_
-                if not self.drop[feature].isin(cats).all():
+                if not self.isin(drop_feature, cats).all():
                     msg = ("Some categories for feature {} were supposed "
                            "to be dropped, but were not found in the encoder "
                            "categories.".format(feature))
                     raise ValueError(msg)
+                idx = self.isin(cats, drop_feature)
                 cats = Series(cats)
-                idx = cats.isin(self.drop[feature])
-                drop_idx[feature] = cp.asarray(cats[idx].index)
+                idx_val = cats[idx].index.values
+                if self.input_type == 'array':
+                    idx_val = idx_val[0]
+                drop_idx[feature] = idx_val
             return drop_idx
         else:
             msg = ("Wrong input for parameter `drop`. Expected "
-                   "'first', None or a dict, got {}")
+                   "'first', None, a dict or a list, got {}")
             raise ValueError(msg.format(type(self.drop)))
 
-    def get_categories_(self):
+    @property
+    def categories_(self):
         """
-        Returns categories used for the one hot encoding in the correct order.
+        Returns categories used for the one hot encoding in the order used by
+        transform.
+        """
+        return [self._encoders[f].classes_ for f in self._features]
 
-        This copies the categories to the CPU and should only be used to check
-        the order of the categories.
+    def _set_input_type(self, X):
+        if isinstance(X, cp.ndarray):
+            self.input_type = 'array'
+            self.isin = cp.isin
+        elif isinstance(X, DataFrame):
+            self.input_type = 'df'
+            self.isin = lambda a, b: Series(a).isin(b)
+        else:
+            raise TypeError(
+                'Expected input to be cupy.ndarray or cudf.DataFrame, '
+                'got {}'.format(type(X)))
+
+    class _ArrayEncoder:
+        """Helper for OneHotEncoder.
+
+        This simplified LabelEncoder reflect the same interface
+        but using cp.arrays instead of cudf.Series internally.
         """
-        return [self._encoders[f].classes_.to_array() for f in self._features]
+
+        def __init__(self, handle_unknown='error'):
+            self.classes_ = None
+            self.handle_unknown = handle_unknown
+
+        def fit(self, X):
+            self.classes_ = cp.unique(X)
+            return self
+
+        def transform(self, X):
+            sorted_index = cp.searchsorted(self.classes_, X)
+
+            xindex = cp.take(cp.arange(len(self.classes_)), sorted_index)
+            mask = self.classes_[xindex] != X
+
+            if mask.any():
+                if self.handle_unknown == 'error':
+                    raise KeyError("Attempted to encode unseen key")
+                else:
+                    xindex[mask] = -1
+
+            return xindex
+
+    def _fit_encoders(self, X, categories=None):
+        """
+        Helper to reduce code duplication in fit method
+        """
+        fit_from_categories = categories is not None
+        _X = categories if fit_from_categories else X
+
+        if self.input_type == 'df':
+            _encoders = dict()
+            def append(d, k, v): d[k] = v
+            Encoder = LabelEncoder
+            self._features = X.columns
+        else:
+            _encoders = list()
+            def append(l, _, v): l.append(v)
+            Encoder = self._ArrayEncoder
+            # used as indices for a list, no need to use a gpu array here
+            self._features = np.arange(0, _X.shape[1], dtype=cp.int32)
+
+        for feature in self._features:
+            le = Encoder(handle_unknown=self.handle_unknown)
+            x_feature = self._take_feature(_X, feature)
+            append(_encoders, feature, le.fit(x_feature))
+
+            if fit_from_categories and self.handle_unknown == 'error':
+                x_categories = x_feature
+                if not self.isin(self._take_feature(X, feature),
+                                 x_categories).all():
+                    msg = ("Found unknown categories in column {0}"
+                           " during fit".format(feature))
+                    raise KeyError(msg)
+        return _encoders
 
     def fit(self, X):
         """
@@ -166,24 +260,16 @@ class OneHotEncoder:
         self
         """
         self._validate_keywords()
+
+        self._set_input_type(X)
+
         if type(self.categories) is str and self.categories == 'auto':
-            self._features = X.columns
-            self._encoders = {
-                feature: LabelEncoder(handle_unknown=self.handle_unknown).fit(
-                    X[feature])
-                for feature in X.columns
-            }
+            self._encoders = self._fit_encoders(X)
         else:
-            self._features = self.categories.columns
-            self._encoders = dict()
-            for feature in self.categories.columns:
-                le = LabelEncoder(handle_unknown=self.handle_unknown)
-                self._encoders[feature] = le.fit(self.categories[feature])
-                if self.handle_unknown == 'error':
-                    if not X[feature].isin(self.categories[feature]).all():
-                        msg = ("Found unknown categories in column {0}"
-                               " during fit".format(feature))
-                        raise KeyError(msg)
+            _categories = self.categories
+            if self.input_type == 'array':
+                _categories = _categories.transpose()  # same format as X
+            self._encoders = self._fit_encoders(X, categories=_categories)
 
         self.drop_idx_ = self._compute_drop_idx()
         self._fitted = True
@@ -222,10 +308,13 @@ class OneHotEncoder:
 
         cols, rows = list(), list()
         j = 0
-        for feature in X.columns:
+        for feature in self._features:
             encoder = self._encoders[feature]
-            col_idx = encoder.transform(X[feature])
-            col_idx = cp.asarray(col_idx.to_gpu_array(fillna="pandas"))
+
+            col_idx = encoder.transform(self._take_feature(X, feature))
+            if self.input_type == 'df':
+                col_idx = cp.asarray(col_idx.to_gpu_array(fillna="pandas"))
+
             idx_to_keep = col_idx > -1
 
             # increase indices to take previous features into account
@@ -278,44 +367,68 @@ class OneHotEncoder:
         """
         self._check_is_fitted()
         if cp.sparse.issparse(X):
-            # cupy.sparse 7.x does not support argmax, when we upgrade cupy to
-            # 8.x, we should add a condition in the
-            # if close: `and cp.sparse.issparsecsc(X)`
-            # and change the following line by `X = X.tocsc()`
             X = X.toarray()
-        result = DataFrame(columns=self._encoders.keys())
+
+        if self.input_type == 'df':
+            result = DataFrame(columns=self._features)
+            def add_result_column(res, key, col): res[key] = col
+
+            def dropped_1cat_inverse(value):
+                return Series(GenericIndex(value).repeat(X.shape[0]))
+
+            def drop_inverse(enc, drop_index):
+                return enc.inverse_transform(Series(drop_index))[0]
+        else:
+            result = cp.empty(shape=(len(X), len(self._features)))
+            def add_result_column(res, key, col): res[:, key] = col
+
+            def dropped_1cat_inverse(value):
+                return cp.full(len(X), value.item(), dtype=self.dtype)
+
+            def drop_inverse(enc, drop_index):
+                return enc.classes_[drop_index]
+
         j = 0
-        for feature in self._encoders.keys():
+        for feature in self._features:
             feature_enc = self._encoders[feature]
             cats = feature_enc.classes_
 
             if self.drop is not None:
                 # Remove dropped categories
-                dropped_class_idx = Series(self.drop_idx_[feature])
-                dropped_class_mask = Series(cats).isin(cats[dropped_class_idx])
+                drop_idx = self.drop_idx_[feature]
+                dropped_class_mask = self.isin(cats, cats[drop_idx])
                 if len(cats) == 1:
-                    inv = Series(GenericIndex(cats[0]).repeat(X.shape[0]))
-                    result[feature] = inv
+                    # if there is only one category and we drop it, then we
+                    # know that the full inverse column is this category
+                    inv = dropped_1cat_inverse(cats[0])
+                    add_result_column(result, feature, inv)
                     continue
                 cats = cats[~dropped_class_mask]
 
             enc_size = len(cats)
             x_feature = X[:, j:j + enc_size]
             idx = cp.argmax(x_feature, axis=1)
-            inv = Series(cats[idx]).reset_index(drop=True)
+            inv = cats[idx]
+            if self.input_type == 'df':
+                inv = Series(cats[idx]).reset_index(drop=True)
 
             if self.handle_unknown == 'ignore':
                 not_null_idx = x_feature.any(axis=1)
-                inv.iloc[~not_null_idx] = None
+                if not_null_idx.any():
+                    if self.input_type == 'array':
+                        raise ValueError('Found an unknown category during '
+                                         'inverse_transform, which is not '
+                                         'supported with cupy arrays')
+                    inv[~not_null_idx] = None
             elif self.drop is not None:
                 # drop will either be None or handle_unknown will be error. If
                 # self.drop is not None, then we can safely assume that all of
                 # the nulls in each column are the dropped value
                 dropped_mask = cp.asarray(x_feature.sum(axis=1) == 0).flatten()
                 if dropped_mask.any():
-                    inv[dropped_mask] = feature_enc.inverse_transform(
-                        Series(self.drop_idx_[feature]))[0]
+                    drop_idx = self.drop_idx_[feature]
+                    inv[dropped_mask] = drop_inverse(feature_enc, drop_idx)
 
-            result[feature] = inv
+            add_result_column(result, feature, inv)
             j += enc_size
         return result
