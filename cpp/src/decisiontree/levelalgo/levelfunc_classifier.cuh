rapidsai/cuml

commit d38b4f9705267ff0693358ce2484417ab9279feb

Refactoring tree parameters related arguments of few more functions and style related corrections

cpp/src/decisiontree/levelalgo/levelfunc_classifier.cuh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2020, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,12 +34,10 @@ template <typename T>
 void grow_deep_tree_classification(
   const T* data, const int* labels, unsigned int* rowids, const int Ncols,
   const float colper, int n_sampled_rows, const int nrows,
-  const int n_unique_labels, const int nbins, const int maxdepth,
-  const int maxleaves, const int min_rows_per_node,
-  const ML::CRITERION split_cr, const int split_algo,
-  const float min_impurity_decrease, int& depth_cnt, int& leaf_cnt,
-  std::vector<SparseTreeNode<T, int>>& sparsetree, const int treeid,
-  std::shared_ptr<TemporaryMemory<T, int>> tempmem) {
+  const int n_unique_labels,
+  const ML::DecisionTree::DecisionTreeParams& tree_params, int& depth_cnt,
+  int& leaf_cnt, std::vector<SparseTreeNode<T, int>>& sparsetree,
+  const int treeid, std::shared_ptr<TemporaryMemory<T, int>> tempmem) {
   const int ncols_sampled = (int)(colper * Ncols);
   unsigned int* flagsptr = tempmem->d_flags->data();
   unsigned int* sample_cnt = tempmem->d_sample_cnt->data();
@@ -47,7 +45,7 @@ void grow_deep_tree_classification(
                  tempmem->stream);
   std::vector<unsigned int> histvec(n_unique_labels, 0);
   T initial_metric;
-  if (split_cr == ML::CRITERION::GINI) {
+  if (tree_params.split_criterion == ML::CRITERION::GINI) {
     initial_metric_classification<T, GiniFunctor>(labels, sample_cnt, nrows,
                                                   n_unique_labels, histvec,
                                                   initial_metric, tempmem);
@@ -56,7 +54,7 @@ void grow_deep_tree_classification(
                                                      n_unique_labels, histvec,
                                                      initial_metric, tempmem);
   }
-  int reserve_depth = std::min(tempmem->swap_depth, maxdepth);
+  int reserve_depth = std::min(tempmem->swap_depth, tree_params.max_depth);
   size_t total_nodes = pow(2, (reserve_depth + 1)) - 1;
 
   unsigned int* h_parent_hist = tempmem->h_parent_hist->data();
@@ -104,7 +102,7 @@ void grow_deep_tree_classification(
   }
   std::vector<unsigned int> feature_selector(h_colids, h_colids + Ncols);
 
-  int scatter_algo_depth = std::min(tempmem->swap_depth, maxdepth);
+  int scatter_algo_depth = std::min(tempmem->swap_depth, tree_params.max_depth);
   for (int depth = 0; (depth < scatter_algo_depth) && (n_nodes_nextitr != 0);
        depth++) {
     depth_cnt = depth + 1;
@@ -120,37 +118,40 @@ void grow_deep_tree_classification(
                             ncols_sampled, n_nodes, mtg, dist, feature_selector,
                             tempmem, d_rng);
     get_histogram_classification(data, labels, flagsptr, sample_cnt, nrows,
-                                 Ncols, ncols_sampled, n_unique_labels, nbins,
-                                 n_nodes, split_algo, tempmem, d_histogram);
+                                 Ncols, ncols_sampled, n_unique_labels,
+                                 tree_params.n_bins, n_nodes,
+                                 tree_params.split_algo, tempmem, d_histogram);
 
     float* infogain = tempmem->h_outgain->data();
-    if (split_cr == ML::CRITERION::GINI) {
+    if (tree_params.split_criterion == ML::CRITERION::GINI) {
       get_best_split_classification<T, GiniFunctor, GiniDevFunctor>(
         h_histogram, d_histogram, h_colids, d_colids, h_colstart, d_colstart,
-        Ncols, ncols_sampled, nbins, n_unique_labels, n_nodes, depth,
-        min_rows_per_node, split_algo, infogain, h_parent_hist, h_child_hist,
-        sparsetree, sparsesize, sparse_nodelist, h_split_colidx, h_split_binidx,
-        d_split_colidx, d_split_binidx, tempmem);
+        Ncols, ncols_sampled, tree_params.n_bins, n_unique_labels, n_nodes,
+        depth, tree_params.min_rows_per_node, tree_params.split_algo, infogain,
+        h_parent_hist, h_child_hist, sparsetree, sparsesize, sparse_nodelist,
+        h_split_colidx, h_split_binidx, d_split_colidx, d_split_binidx,
+        tempmem);
     } else {
       get_best_split_classification<T, EntropyFunctor, EntropyDevFunctor>(
         h_histogram, d_histogram, h_colids, d_colids, h_colstart, d_colstart,
-        Ncols, ncols_sampled, nbins, n_unique_labels, n_nodes, depth,
-        min_rows_per_node, split_algo, infogain, h_parent_hist, h_child_hist,
-        sparsetree, sparsesize, sparse_nodelist, h_split_colidx, h_split_binidx,
-        d_split_colidx, d_split_binidx, tempmem);
+        Ncols, ncols_sampled, tree_params.n_bins, n_unique_labels, n_nodes,
+        depth, tree_params.min_rows_per_node, tree_params.split_algo, infogain,
+        h_parent_hist, h_child_hist, sparsetree, sparsesize, sparse_nodelist,
+        h_split_colidx, h_split_binidx, d_split_colidx, d_split_binidx,
+        tempmem);
     }
 
     CUDA_CHECK(cudaStreamSynchronize(tempmem->stream));
-    leaf_eval_classification(infogain, depth, min_impurity_decrease, maxdepth,
-                             n_unique_labels, maxleaves, h_new_node_flags,
-                             sparsetree, sparsesize, h_parent_hist,
-                             n_nodes_nextitr, sparse_nodelist, leaf_cnt);
+    leaf_eval_classification(
+      infogain, depth, tree_params.min_impurity_decrease, tree_params.max_depth,
+      n_unique_labels, tree_params.max_depth, h_new_node_flags, sparsetree,
+      sparsesize, h_parent_hist, n_nodes_nextitr, sparse_nodelist, leaf_cnt);
 
     MLCommon::updateDevice(d_new_node_flags, h_new_node_flags, n_nodes,
                            tempmem->stream);
-    make_level_split(data, nrows, Ncols, ncols_sampled, nbins, n_nodes,
-                     split_algo, d_split_colidx, d_split_binidx,
-                     d_new_node_flags, flagsptr, tempmem);
+    make_level_split(data, nrows, Ncols, ncols_sampled, tree_params.n_bins,
+                     n_nodes, tree_params.split_algo, d_split_colidx,
+                     d_split_binidx, d_new_node_flags, flagsptr, tempmem);
     CUDA_CHECK(cudaStreamSynchronize(tempmem->stream));
     if (depth != (scatter_algo_depth - 1)) {
       memcpy(h_parent_hist, h_child_hist,
@@ -187,26 +188,26 @@ void grow_deep_tree_classification(
   sparsetree.resize(sparsetree.size() - lastsize);
   convert_scatter_to_gather(flagsptr, sample_cnt, n_nodes, nrows, d_nodecount,
                             d_nodestart, d_samplelist, tempmem);
-  for (int depth = tempmem->swap_depth; (depth < maxdepth) && (n_nodes != 0);
-       depth++) {
+  for (int depth = tempmem->swap_depth;
+       (depth < tree_params.max_depth) && (n_nodes != 0); depth++) {
     depth_cnt = depth + 1;
     //Algorithm starts here
     update_feature_sampling(h_colids, d_colids, h_colstart, d_colstart, Ncols,
                             ncols_sampled, lastsize, mtg, dist,
                             feature_selector, tempmem, d_rng);
 
-    if (split_cr == ML::CRITERION::GINI) {
+    if (tree_params.split_criterion == ML::CRITERION::GINI) {
       best_split_gather_classification<T, GiniDevFunctor>(
         data, labels, d_colids, d_colstart, d_nodestart, d_samplelist, nrows,
-        Ncols, ncols_sampled, n_unique_labels, nbins, n_nodes, split_algo,
-        sparsetree.size() + lastsize, min_impurity_decrease, tempmem,
-        d_sparsenodes, d_nodelist);
+        Ncols, ncols_sampled, n_unique_labels, tree_params.n_bins, n_nodes,
+        tree_params.split_algo, sparsetree.size() + lastsize,
+        tree_params.min_impurity_decrease, tempmem, d_sparsenodes, d_nodelist);
     } else {
       best_split_gather_classification<T, EntropyDevFunctor>(
         data, labels, d_colids, d_colstart, d_nodestart, d_samplelist, nrows,
-        Ncols, ncols_sampled, n_unique_labels, nbins, n_nodes, split_algo,
-        sparsetree.size() + lastsize, min_impurity_decrease, tempmem,
-        d_sparsenodes, d_nodelist);
+        Ncols, ncols_sampled, n_unique_labels, tree_params.n_bins, n_nodes,
+        tree_params.split_algo, sparsetree.size() + lastsize,
+        tree_params.min_impurity_decrease, tempmem, d_sparsenodes, d_nodelist);
     }
     MLCommon::updateHost(h_sparsenodes, d_sparsenodes, lastsize,
                          tempmem->stream);
@@ -225,7 +226,7 @@ void grow_deep_tree_classification(
     n_nodes = h_counter[0];
   }
   if (n_nodes != 0) {
-    if (split_cr == ML::CRITERION::GINI) {
+    if (tree_params.split_criterion == ML::CRITERION::GINI) {
       make_leaf_gather_classification<T, GiniDevFunctor>(
         labels, d_nodestart, d_samplelist, n_unique_labels, d_sparsenodes,
         d_nodelist, n_nodes, tempmem);

cpp/src/decisiontree/decisiontree_impl.cuh
@@ -347,9 +347,7 @@ void DecisionTreeBase<T, L>::base_fit(
   } else {
     tempmem = std::make_shared<TemporaryMemory<T, L>>(
       device_allocator_in, host_allocator_in, stream_in, nrows, ncols,
-      tree_params.max_features, unique_labels, tree_params.n_bins,
-      tree_params.split_algo, tree_params.max_depth,
-      tree_params.shuffle_features);
+      unique_labels, tree_params);
     tree_params.quantile_per_tree = true;
   }
 
@@ -430,13 +428,10 @@ void DecisionTreeClassifier<T>::grow_deep_tree(
   const int treeid, std::shared_ptr<TemporaryMemory<T, int>> tempmem) {
   int leaf_cnt = 0;
   int depth_cnt = 0;
-  grow_deep_tree_classification(
-    data, labels, rowids, ncols, colper, n_sampled_rows, nrows,
-    this->n_unique_labels, this->tree_params.n_bins,
-    this->tree_params.max_depth, this->tree_params.max_leaves,
-    this->tree_params.min_rows_per_node, this->tree_params.split_criterion,
-    this->tree_params.split_algo, this->tree_params.min_impurity_decrease,
-    depth_cnt, leaf_cnt, sparsetree, treeid, tempmem);
+  grow_deep_tree_classification(data, labels, rowids, ncols, colper,
+                                n_sampled_rows, nrows, this->n_unique_labels,
+                                this->tree_params, depth_cnt, leaf_cnt,
+                                sparsetree, treeid, tempmem);
   this->depth_counter = depth_cnt;
   this->leaf_counter = leaf_cnt;
 }
@@ -449,13 +444,9 @@ void DecisionTreeRegressor<T>::grow_deep_tree(
   const int treeid, std::shared_ptr<TemporaryMemory<T, T>> tempmem) {
   int leaf_cnt = 0;
   int depth_cnt = 0;
-  grow_deep_tree_regression(
-    data, labels, rowids, ncols, colper, n_sampled_rows, nrows,
-    this->tree_params.n_bins, this->tree_params.max_depth,
-    this->tree_params.max_leaves, this->tree_params.min_rows_per_node,
-    this->tree_params.split_criterion, this->tree_params.split_algo,
-    this->tree_params.min_impurity_decrease, depth_cnt, leaf_cnt, sparsetree,
-    treeid, tempmem);
+  grow_deep_tree_regression(data, labels, rowids, ncols, colper, n_sampled_rows,
+                            nrows, this->tree_params, depth_cnt, leaf_cnt,
+                            sparsetree, treeid, tempmem);
   this->depth_counter = depth_cnt;
   this->leaf_counter = leaf_cnt;
 }

cpp/src/decisiontree/levelalgo/levelfunc_regressor.cuh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2020, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,11 +35,9 @@ template <typename T>
 void grow_deep_tree_regression(
   const T* data, const T* labels, unsigned int* rowids, const int Ncols,
   const float colper, const int n_sampled_rows, const int nrows,
-  const int nbins, int maxdepth, const int maxleaves,
-  const int min_rows_per_node, const ML::CRITERION split_cr, int split_algo,
-  const float min_impurity_decrease, int& depth_cnt, int& leaf_cnt,
-  std::vector<SparseTreeNode<T, T>>& sparsetree, const int treeid,
-  std::shared_ptr<TemporaryMemory<T, T>> tempmem) {
+  const ML::DecisionTree::DecisionTreeParams& tree_params, int& depth_cnt,
+  int& leaf_cnt, std::vector<SparseTreeNode<T, T>>& sparsetree,
+  const int treeid, std::shared_ptr<TemporaryMemory<T, T>> tempmem) {
   const int ncols_sampled = (int)(colper * Ncols);
   unsigned int* flagsptr = tempmem->d_flags->data();
   unsigned int* sample_cnt = tempmem->d_sample_cnt->data();
@@ -49,14 +47,14 @@ void grow_deep_tree_regression(
   T mean;
   T initial_metric;
   unsigned int count;
-  if (split_cr == ML::CRITERION::MSE) {
+  if (tree_params.split_criterion == ML::CRITERION::MSE) {
     initial_metric_regression<T, SquareFunctor>(labels, sample_cnt, nrows, mean,
                                                 count, initial_metric, tempmem);
   } else {
     initial_metric_regression<T, AbsFunctor>(labels, sample_cnt, nrows, mean,
                                              count, initial_metric, tempmem);
   }
-  int reserve_depth = std::min(tempmem->swap_depth, maxdepth);
+  int reserve_depth = std::min(tempmem->swap_depth, tree_params.max_depth);
   size_t total_nodes = pow(2, (reserve_depth + 1)) - 1;
 
   std::vector<T> sparse_meanstate;
@@ -112,7 +110,7 @@ void grow_deep_tree_regression(
   std::vector<unsigned int> feature_selector(h_colids, h_colids + Ncols);
   float* infogain = tempmem->h_outgain->data();
 
-  int scatter_algo_depth = std::min(tempmem->swap_depth, maxdepth);
+  int scatter_algo_depth = std::min(tempmem->swap_depth, tree_params.max_depth);
   for (int depth = 0; (depth < scatter_algo_depth) && (n_nodes_nextitr != 0);
        depth++) {
     depth_cnt = depth + 1;
@@ -130,42 +128,44 @@ void grow_deep_tree_regression(
     init_parent_value(sparse_meanstate, sparse_countstate, sparse_nodelist,
                       sparsesize, depth, tempmem);
 
-    if (split_cr == ML::CRITERION::MSE) {
-      get_mse_regression_fused<T>(
-        data, labels, flagsptr, sample_cnt, nrows, Ncols, ncols_sampled, nbins,
-        n_nodes, split_algo, tempmem, d_mseout, d_predout, d_count);
+    if (tree_params.split_criterion == ML::CRITERION::MSE) {
+      get_mse_regression_fused<T>(data, labels, flagsptr, sample_cnt, nrows,
+                                  Ncols, ncols_sampled, tree_params.n_bins,
+                                  n_nodes, tree_params.split_algo, tempmem,
+                                  d_mseout, d_predout, d_count);
       get_best_split_regression<T, MSEGain<T>>(
         h_mseout, d_mseout, h_predout, d_predout, h_count, d_count, h_colids,
-        d_colids, h_colstart, d_colstart, Ncols, ncols_sampled, nbins, n_nodes,
-        depth, min_rows_per_node, split_algo, sparsesize, infogain,
-        sparse_meanstate, sparse_countstate, sparsetree, sparse_nodelist,
-        h_split_colidx, h_split_binidx, d_split_colidx, d_split_binidx,
-        tempmem);
+        d_colids, h_colstart, d_colstart, Ncols, ncols_sampled,
+        tree_params.n_bins, n_nodes, depth, tree_params.min_rows_per_node,
+        tree_params.split_algo, sparsesize, infogain, sparse_meanstate,
+        sparse_countstate, sparsetree, sparse_nodelist, h_split_colidx,
+        h_split_binidx, d_split_colidx, d_split_binidx, tempmem);
 
     } else {
       get_mse_regression<T, AbsFunctor>(
-        data, labels, flagsptr, sample_cnt, nrows, Ncols, ncols_sampled, nbins,
-        n_nodes, split_algo, tempmem, d_mseout, d_predout, d_count);
+        data, labels, flagsptr, sample_cnt, nrows, Ncols, ncols_sampled,
+        tree_params.n_bins, n_nodes, tree_params.split_algo, tempmem, d_mseout,
+        d_predout, d_count);
       get_best_split_regression<T, MAEGain<T>>(
         h_mseout, d_mseout, h_predout, d_predout, h_count, d_count, h_colids,
-        d_colids, h_colstart, d_colstart, Ncols, ncols_sampled, nbins, n_nodes,
-        depth, min_rows_per_node, split_algo, sparsesize, infogain,
-        sparse_meanstate, sparse_countstate, sparsetree, sparse_nodelist,
-        h_split_colidx, h_split_binidx, d_split_colidx, d_split_binidx,
-        tempmem);
+        d_colids, h_colstart, d_colstart, Ncols, ncols_sampled,
+        tree_params.n_bins, n_nodes, depth, tree_params.min_rows_per_node,
+        tree_params.split_algo, sparsesize, infogain, sparse_meanstate,
+        sparse_countstate, sparsetree, sparse_nodelist, h_split_colidx,
+        h_split_binidx, d_split_colidx, d_split_binidx, tempmem);
     }
 
     CUDA_CHECK(cudaStreamSynchronize(tempmem->stream));
-    leaf_eval_regression(infogain, depth, min_impurity_decrease, maxdepth,
-                         maxleaves, h_new_node_flags, sparsetree, sparsesize,
-                         sparse_meanstate, n_nodes_nextitr, sparse_nodelist,
-                         leaf_cnt);
+    leaf_eval_regression(
+      infogain, depth, tree_params.min_impurity_decrease, tree_params.max_depth,
+      tree_params.max_leaves, h_new_node_flags, sparsetree, sparsesize,
+      sparse_meanstate, n_nodes_nextitr, sparse_nodelist, leaf_cnt);
 
     MLCommon::updateDevice(d_new_node_flags, h_new_node_flags, n_nodes,
                            tempmem->stream);
-    make_level_split(data, nrows, Ncols, ncols_sampled, nbins, n_nodes,
-                     split_algo, d_split_colidx, d_split_binidx,
-                     d_new_node_flags, flagsptr, tempmem);
+    make_level_split(data, nrows, Ncols, ncols_sampled, tree_params.n_bins,
+                     n_nodes, tree_params.split_algo, d_split_colidx,
+                     d_split_binidx, d_new_node_flags, flagsptr, tempmem);
   }
 
   // Start of gather algorithm
@@ -198,8 +198,8 @@ void grow_deep_tree_regression(
   sparsetree.resize(sparsetree.size() - lastsize);
   convert_scatter_to_gather(flagsptr, sample_cnt, n_nodes, nrows, d_nodecount,
                             d_nodestart, d_samplelist, tempmem);
-  for (int depth = tempmem->swap_depth; (depth < maxdepth) && (n_nodes != 0);
-       depth++) {
+  for (int depth = tempmem->swap_depth;
+       (depth < tree_params.max_depth) && (n_nodes != 0); depth++) {
     depth_cnt = depth + 1;
     //Algorithm starts here
     update_feature_sampling(h_colids, d_colids, h_colstart, d_colstart, Ncols,
@@ -208,9 +208,9 @@ void grow_deep_tree_regression(
 
     best_split_gather_regression(
       data, labels, d_colids, d_colstart, d_nodestart, d_samplelist, nrows,
-      Ncols, ncols_sampled, nbins, n_nodes, split_algo, split_cr,
-      sparsetree.size() + lastsize, min_impurity_decrease, tempmem,
-      d_sparsenodes, d_nodelist);
+      Ncols, ncols_sampled, tree_params.n_bins, n_nodes, tree_params.split_algo,
+      tree_params.split_criterion, sparsetree.size() + lastsize,
+      tree_params.min_impurity_decrease, tempmem, d_sparsenodes, d_nodelist);
 
     MLCommon::updateHost(h_sparsenodes, d_sparsenodes, lastsize,
                          tempmem->stream);

cpp/src/decisiontree/memory.cuh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2020, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,35 +25,26 @@ template <class T, class L>
 TemporaryMemory<T, L>::TemporaryMemory(
   const std::shared_ptr<MLCommon::deviceAllocator> device_allocator_in,
   const std::shared_ptr<MLCommon::hostAllocator> host_allocator_in,
-  const cudaStream_t stream_in, int N, int Ncols, float colper, int n_unique,
-  int n_bins, const int split_algo, int depth, bool col_shuffle) {
+  const cudaStream_t stream_in, int N, int Ncols, int n_unique,
+  const ML::DecisionTree::DecisionTreeParams& tree_params) {
   stream = stream_in;
-  splitalgo = split_algo;
-
   max_shared_mem = MLCommon::getSharedMemPerBlock();
   num_sms = MLCommon::getMultiProcessorCount();
   device_allocator = device_allocator_in;
   host_allocator = host_allocator_in;
-  LevelMemAllocator(N, Ncols, colper, n_unique, n_bins, depth, split_algo,
-                    col_shuffle);
+  LevelMemAllocator(N, Ncols, n_unique, tree_params);
 }
 
 template <class T, class L>
-TemporaryMemory<T, L>::TemporaryMemory(const ML::cumlHandle_impl& handle,
-                                       cudaStream_t stream_in, int N, int Ncols,
-                                       float colper, int n_unique, int n_bins,
-                                       const int split_algo, int depth,
-                                       bool col_shuffle) {
-  //Assign Stream from cumlHandle
+TemporaryMemory<T, L>::TemporaryMemory(
+  const ML::cumlHandle_impl& handle, cudaStream_t stream_in, int N, int Ncols,
+  int n_unique, const ML::DecisionTree::DecisionTreeParams& tree_params) {
   stream = stream_in;
-  splitalgo = split_algo;
-
   max_shared_mem = MLCommon::getSharedMemPerBlock();
   num_sms = MLCommon::getMultiProcessorCount();
   device_allocator = handle.getDeviceAllocator();
   host_allocator = handle.getHostAllocator();
-  LevelMemAllocator(N, Ncols, colper, n_unique, n_bins, depth, split_algo,
-                    col_shuffle);
+  LevelMemAllocator(N, Ncols, n_unique, tree_params);
 }
 
 template <class T, class L>
@@ -80,18 +71,18 @@ void TemporaryMemory<T, L>::print_info(int depth, int nrows, int ncols,
 }
 
 template <class T, class L>
-void TemporaryMemory<T, L>::LevelMemAllocator(int nrows, int ncols,
-                                              float colper, int n_unique,
-                                              int nbins, int depth,
-                                              const int split_algo,
-                                              bool col_shuffle) {
+void TemporaryMemory<T, L>::LevelMemAllocator(
+  int nrows, int ncols, int n_unique,
+  const ML::DecisionTree::DecisionTreeParams& tree_params) {
+  int nbins = tree_params.n_bins;
+  int depth = tree_params.max_depth;
   if (depth > swap_depth || (depth == -1)) {
     max_nodes_per_level = pow(2, swap_depth);
   } else {
     max_nodes_per_level = pow(2, depth);
   }
   size_t maxnodes = max_nodes_per_level;
-  size_t ncols_sampled = (size_t)(ncols * colper);
+  size_t ncols_sampled = (size_t)(ncols * tree_params.max_features);
   if (depth < 64) {
     gather_max_nodes = std::min((size_t)(nrows + 1),
                                 (size_t)(pow((size_t)2, (size_t)depth) + 1));
@@ -134,7 +125,7 @@ void TemporaryMemory<T, L>::LevelMemAllocator(int nrows, int ncols,
   totalmem =
     3 * parentsz * sizeof(int) + childsz * sizeof(T) + (nrows + 1) * sizeof(T);
 
-  if (split_algo == 0) {
+  if (tree_params.split_algo == 0) {
     d_globalminmax = new MLCommon::device_buffer<T>(
       device_allocator, stream, 2 * maxnodes * ncols_sampled);
     h_globalminmax = new MLCommon::host_buffer<T>(host_allocator, stream,
@@ -149,7 +140,7 @@ void TemporaryMemory<T, L>::LevelMemAllocator(int nrows, int ncols,
   }
   d_sample_cnt =
     new MLCommon::device_buffer<unsigned int>(device_allocator, stream, nrows);
-  if (col_shuffle == true) {
+  if (tree_params.shuffle_features == true) {
     d_colids = new MLCommon::device_buffer<unsigned int>(
       device_allocator, stream, ncols_sampled * gather_max_nodes);
     h_colids = new MLCommon::host_buffer<unsigned int>(
@@ -251,7 +242,7 @@ void TemporaryMemory<T, L>::LevelMemAllocator(int nrows, int ncols,
     max_nodes_pred /= 2;  // For occupancy purposes.
     max_nodes_mse /= 2;   // For occupancy purposes.
   }
-  if (split_algo == ML::SPLIT_ALGO::HIST) {
+  if (tree_params.split_algo == ML::SPLIT_ALGO::HIST) {
     size_t shmem_per_node = 2 * sizeof(T);
     max_nodes_minmax = max_shared_mem / shmem_per_node;
     max_nodes_minmax /= 2;

cpp/src/decisiontree/memory.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2020, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@
 #include <common/device_buffer.hpp>
 #include <common/host_buffer.hpp>
 #include <cuml/common/cuml_allocator.hpp>
+#include <cuml/tree/decisiontree.hpp>
 #include "common/cumlHandle.hpp"
 
 template <class T, class L>
@@ -68,8 +69,6 @@ struct TemporaryMemory {
   MLCommon::device_buffer<unsigned int> *d_colstart = nullptr;
   MLCommon::host_buffer<unsigned int> *h_colids = nullptr;
   MLCommon::host_buffer<unsigned int> *h_colstart = nullptr;
-  //Split algo
-  int splitalgo;
 
   //For level algorithm
   MLCommon::device_buffer<unsigned int> *d_flags = nullptr;
@@ -110,17 +109,21 @@ struct TemporaryMemory {
   TemporaryMemory(
     const std::shared_ptr<MLCommon::deviceAllocator> device_allocator_in,
     const std::shared_ptr<MLCommon::hostAllocator> host_allocator_in,
-    const cudaStream_t stream_in, int N, int Ncols, float colper, int n_unique,
-    int n_bins, const int split_algo, int depth, bool col_shuffle);
+    const cudaStream_t stream_in, int N, int Ncols, int n_unique,
+    const ML::DecisionTree::DecisionTreeParams &tree_params);
+
   TemporaryMemory(const ML::cumlHandle_impl &handle, cudaStream_t stream_in,
-                  int N, int Ncols, float colper, int n_unique, int n_bins,
-                  const int split_algo, int depth, bool colshuffle);
+                  int N, int Ncols, int n_unique,
+                  const ML::DecisionTree::DecisionTreeParams &tree_params);
+
   ~TemporaryMemory();
-  void LevelMemAllocator(int nrows, int ncols, float colper, int n_unique,
-                         int nbins, int depth, const int split_algo,
-                         bool col_shuffle);
+
+  void LevelMemAllocator(
+    int nrows, int ncols, int n_unique,
+    const ML::DecisionTree::DecisionTreeParams &tree_params);
 
   void LevelMemCleaner();
+
   void print_info(int depth, int nrows, int ncols, float colper);
 };
 #include "memory.cuh"

cpp/src/randomforest/randomforest_impl.cuh
@@ -187,12 +187,8 @@ void rfClassifier<T>::fit(const cumlHandle& user_handle, const T* input,
   std::shared_ptr<TemporaryMemory<T, int>> tempmem[n_streams];
   for (int i = 0; i < n_streams; i++) {
     tempmem[i] = std::make_shared<TemporaryMemory<T, int>>(
-      handle, handle.getInternalStream(i), n_rows, n_cols,
-      this->rf_params.tree_params.max_features, n_unique_labels,
-      this->rf_params.tree_params.n_bins,
-      this->rf_params.tree_params.split_algo,
-      this->rf_params.tree_params.max_depth,
-      this->rf_params.tree_params.shuffle_features);
+      handle, handle.getInternalStream(i), n_rows, n_cols, n_unique_labels,
+      this->rf_params.tree_params);
   }
   //Preprocess once only per forest
   if ((this->rf_params.tree_params.split_algo == SPLIT_ALGO::GLOBAL_QUANTILE) &&
@@ -455,12 +451,8 @@ void rfRegressor<T>::fit(const cumlHandle& user_handle, const T* input,
   std::shared_ptr<TemporaryMemory<T, T>> tempmem[n_streams];
   for (int i = 0; i < n_streams; i++) {
     tempmem[i] = std::make_shared<TemporaryMemory<T, T>>(
-      handle, handle.getInternalStream(i), n_rows, n_cols,
-      this->rf_params.tree_params.max_features, 1,
-      this->rf_params.tree_params.n_bins,
-      this->rf_params.tree_params.split_algo,
-      this->rf_params.tree_params.max_depth,
-      this->rf_params.tree_params.shuffle_features);
+      handle, handle.getInternalStream(i), n_rows, n_cols, 1,
+      this->rf_params.tree_params);
   }
   //Preprocess once only per forest
   if ((this->rf_params.tree_params.split_algo == SPLIT_ALGO::GLOBAL_QUANTILE) &&

commit 88f050feed54395fe4fa077c8f4d7848f44006cd

Fixing an error introduced in commit d38b4f970

cpp/src/decisiontree/levelalgo/levelfunc_classifier.cuh
@@ -144,7 +144,7 @@ void grow_deep_tree_classification(
     CUDA_CHECK(cudaStreamSynchronize(tempmem->stream));
     leaf_eval_classification(
       infogain, depth, tree_params.min_impurity_decrease, tree_params.max_depth,
-      n_unique_labels, tree_params.max_depth, h_new_node_flags, sparsetree,
+      n_unique_labels, tree_params.max_leaves, h_new_node_flags, sparsetree,
       sparsesize, h_parent_hist, n_nodes_nextitr, sparse_nodelist, leaf_cnt);
 
     MLCommon::updateDevice(d_new_node_flags, h_new_node_flags, n_nodes,