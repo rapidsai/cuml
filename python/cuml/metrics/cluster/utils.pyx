306e8026cfaac5e2a8a4cf03f8b2652dd3cabc1f
Simon Andersen <sandersen@nvidia.com>
2020-04-24 16:30:37 -0500

    Use make_monotonic as required by mutual_info_score prims

--- python/cuml/metrics/cluster/utils.pyx
+++ python/cuml/metrics/cluster/utils.pyx
@@ -18,13 +18,11 @@
 # distutils: language = c++
 # cython: embedsignature = True
 # cython: language_level = 3
-from collections import namedtuple
-
-import numpy as np
 import cupy as cp
+from cuml.metrics.utils import sorted_unique_labels
+from cuml.prims.label import make_monotonic
 from libc.stdint cimport uintptr_t
-
-from cuml.utils import input_to_dev_array, with_cupy_rmm
+from cuml.utils import with_cupy_rmm, input_to_cuml_array
 
 
 @with_cupy_rmm
@@ -32,29 +30,30 @@ def prepare_cluster_metric_inputs(labels_true, labels_pred):
     """Helper function to avoid code duplication for homogeneity score, mutual
     info score and completeness score.
     """
-    cdef uintptr_t preds_ptr
-    cdef uintptr_t ground_truth_ptr
-
-    preds_m, preds_ptr, n_rows, _, _ = input_to_dev_array(
-        labels_pred,
-        check_dtype=np.int32,
+    y_true, n_rows, _, dtype = input_to_cuml_array(
+        labels_true,
+        check_dtype=[cp.int32, cp.int64],
         check_cols=1
     )
 
-    ground_truth_m, ground_truth_ptr, _, _, _ = input_to_dev_array(
-        labels_true,
-        check_dtype=np.int32,
+    y_pred, _, _, _ = input_to_cuml_array(
+        labels_pred,
+        check_dtype=dtype,
         check_rows=n_rows,
         check_cols=1
     )
 
-    cp_ground_truth_m = cp.asarray(ground_truth_m)
-    cp_preds_m = cp.asarray(preds_m)
+    cdef uintptr_t preds_ptr = y_pred.ptr
+    cdef uintptr_t ground_truth_ptr = y_true.ptr
 
-    lower_class_range = min(cp.min(cp_ground_truth_m),
-                            cp.min(cp_preds_m))
-    upper_class_range = max(cp.max(cp_ground_truth_m),
-                            cp.max(cp_preds_m))
+    classes = sorted_unique_labels(y_true, y_pred)
+
+    # TODO: Do not call make_monotonic inplace and create a new CumlArray
+    y_true = make_monotonic(y_true, classes=classes, copy=False)[0]
+    y_pred = make_monotonic(y_pred, classes=classes, copy=False)[0]
+
+    lower_class_range = 0
+    upper_class_range = len(classes)
 
     return (ground_truth_ptr, preds_ptr,
             n_rows,
