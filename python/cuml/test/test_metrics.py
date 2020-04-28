d65e248b2088c693272fe723ab5372662af640e7
Simon Andersen <sandersen@nvidia.com>
2020-04-28 16:53:35 -0500

    Revert homogeneity_score changes to keep both PR separated

--- python/cuml/test/test_metrics.py
+++ python/cuml/test/test_metrics.py
@@ -34,7 +34,6 @@ from numpy.testing import assert_almost_equal
 from sklearn.datasets import make_classification
 from sklearn.metrics import accuracy_score as sk_acc_score
 from sklearn.metrics.cluster import adjusted_rand_score as sk_ars
-from sklearn.metrics.cluster import homogeneity_score as sk_homogeneity_score
 from sklearn.metrics.cluster import mutual_info_score as sk_mutual_info_score
 from sklearn.preprocessing import StandardScaler
 
@@ -174,61 +173,12 @@ def test_rand_index_score(name, nrows):
     assert array_equal(cu_score, cu_score_using_sk)
 
 
-def score_homogeneity(ground_truth, predictions, use_handle):
-    return score_labeling_with_handle(cuml.metrics.homogeneity_score,
-                                      ground_truth,
-                                      predictions,
-                                      use_handle,
-                                      dtype=np.int32)
-
-
 def score_mutual_info(ground_truth, predictions, use_handle):
     return score_labeling_with_handle(cuml.metrics.mutual_info_score,
                                       ground_truth,
                                       predictions,
                                       use_handle,
                                       dtype=np.int32)
-
-
-@pytest.mark.parametrize('use_handle', [True, False])
-@pytest.mark.parametrize('data', [([0, 0, 1, 1], [1, 1, 0, 0]),
-                                  ([0, 0, 1, 1], [0, 0, 1, 1])])
-def test_homogeneity_perfect_labeling(use_handle, data):
-    # Perfect labelings are homogeneous
-    hom = score_homogeneity(*data, use_handle)
-    assert_almost_equal(hom, 1.0, decimal=4)
-
-
-@pytest.mark.parametrize('use_handle', [True, False])
-@pytest.mark.parametrize('data', [([0, 0, 1, 1], [0, 0, 1, 2]),
-                                  ([0, 0, 1, 1], [0, 1, 2, 3])])
-def test_homogeneity_non_perfect_labeling(use_handle, data):
-    # Non-perfect labelings that further split classes into more clusters can
-    # be perfectly homogeneous
-    hom = score_homogeneity(*data, use_handle)
-    assert_almost_equal(hom, 1.0, decimal=4)
-
-
-@pytest.mark.parametrize('use_handle', [True, False])
-@pytest.mark.parametrize('data', [([0, 0, 1, 1], [0, 1, 0, 1]),
-                                  ([0, 0, 1, 1], [0, 0, 0, 0])])
-def test_homogeneity_non_homogeneous_labeling(use_handle, data):
-    # Clusters that include samples from different classes do not make for an
-    # homogeneous labeling
-    hom = score_homogeneity(*data, use_handle)
-    assert_almost_equal(hom, 0.0, decimal=4)
-
-
-@pytest.mark.parametrize('use_handle', [True, False])
-@pytest.mark.parametrize('input_range', [[0, 1000],
-                                         [-1000, 1000]])
-def test_homogeneity_score_big_array(use_handle, input_range):
-    a, b, _, _ = generate_random_labels(lambda rd: rd.randint(*input_range,
-                                                              int(10e4),
-                                                              dtype=np.int32))
-    score = score_homogeneity(a, b, use_handle)
-    ref = sk_homogeneity_score(a, b)
-    np.testing.assert_almost_equal(score, ref, decimal=4)
 
 
 @pytest.mark.parametrize('use_handle', [True, False])
