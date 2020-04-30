2a0c07fb3d564a8a54ff4aab2fe454e45d1530ec
divyegala <divyegala@gmail.com>
2020-04-30 00:07:36 -0500

    utils update

--- python/cuml/dask/datasets/utils.py
+++ python/cuml/dask/datasets/utils.py
@@ -27,10 +27,25 @@ def _get_labels(t):
     return t[1]
 
 
-def _dask_array_from_delayed(part, nrows, ncols, dtype):
-    if ncols:
-        return da.from_delayed(dask.delayed(part), shape=(nrows, ncols),
-                               meta=cp.zeros((1)), dtype=dtype)
-    else:
-        return da.from_delayed(dask.delayed(part), shape=(nrows, ),
-                               meta=cp.zeros((1)), dtype=dtype)
+def _dask_array_from_delayed(part, dtype, nrows, ncols=None):
+    # NOTE: ncols = None is for when we want to create a
+    # flat array of ndim == 1. When ncols = 1, we go ahead
+    # and make an array of shape (nrows, 1)
+
+    shape = (nrows, ncols) if ncols else (nrows, )
+    return da.from_delayed(dask.delayed(part), shape=shape,
+                           meta=cp.zeros((1)), dtype=dtype)
+
+
+def _create_delayed(parts, dtype, rows_per_part, ncols=None):
+    """
+        This function takes a list of GPU futures and returns
+        a list of delayed dask arrays, with each part having
+        a corresponding dask.array in the list
+    """
+
+    return [_dask_array_from_delayed(part,
+                                     dtype,
+                                     rows_per_part[idx],
+                                     ncols)
+            for idx, part in enumerate(parts)]
