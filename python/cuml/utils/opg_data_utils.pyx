ad15777567fb0f0c83fb7b33f453b8aca80e1542
Dante Gama Dessavre <dante.gamadessavre@gmail.com>
2020-05-06 12:58:39 -0500

    DOC Added docstrings

--- python/cuml/utils/opg_data_utils.pyx
+++ python/cuml/utils/opg_data_utils.pyx
@@ -22,6 +22,21 @@ from libc.stdint cimport uintptr_t, uint32_t, uint64_t
 
 
 def build_data_t(arys):
+    """
+    Function to create a floatData_t** or doubleData_t** from a list of
+    cumlArrays
+
+    Parameters
+    ----------
+
+    arys: list of cumlArrays of the same dtype, np.float32 or np.float64
+
+    Returns
+    -------
+    ptr: pointer to either a floatData_t** or doubleData_t**, depending on
+        dtype of input
+
+    """
     cdef floatData_t **data_f32
     cdef doubleData_t **data_f64
     cdef uintptr_t ary_ptr
@@ -56,6 +71,17 @@ def build_data_t(arys):
 
 
 def free_data_t(data_t, n, dtype):
+    """
+    Function to free a floatData_t** or doubleData_t**
+
+    Parameters
+    ----------
+    data_t: a floatData_t** or doubleData_t**
+    n: number of elements in the floatData_t** or doubleData_t**
+    dtype: np.float32 or np.float64 indicating whether data_t is a
+        floatData_t** or doubleData_t**
+    """
+
     cdef uintptr_t data_ptr = data_t
     cdef floatData_t **d32
     cdef doubleData_t **d64
@@ -73,6 +99,19 @@ def free_data_t(data_t, n, dtype):
 
 
 def build_rank_size_pair(arys, rank):
+    """
+    Funciton to build a rankSizePair** mapping the rank to the sizes of arrays
+    in arys
+
+    Parameters
+    ----------
+    arys: list of arrays (usual acceptable array/dataframe formats)
+    rank: rank to be mapped
+
+    Returns:
+    --------
+    ptr: pointer to the rankSizePair**
+    """
     cdef RankSizePair **rankSizePair = <RankSizePair**> \
         malloc(sizeof(RankSizePair**)
                * len(arys))
@@ -87,71 +126,17 @@ def build_rank_size_pair(arys, rank):
 
 
 def free_rank_size_pair(rank_size_t, n):
+    """
+    Function to free a rankSizePair**
+
+    Parameters
+    ----------
+    rank_size_t: rankSizePair** to be freed.
+    n: number of elements in the rankSizePair** to be freed.
+    """
     cdef uintptr_t rs_ptr = rank_size_t
     cdef RankSizePair **rankSizePair = <RankSizePair**> rs_ptr
 
     for idx in range(n):
         free(<RankSizePair*>rankSizePair[idx])
     free(<RankSizePair**>rankSizePair)
-
-
-def _build_dataFloat(arr_interfaces):
-    cdef floatData_t **dataF = <floatData_t **> \
-        malloc(sizeof(floatData_t *)
-               * len(arr_interfaces))
-
-    cdef uintptr_t input_ptr
-    for x_i in range(len(arr_interfaces)):
-        x = arr_interfaces[x_i]
-        input_ptr = x["data"]
-        dataF[x_i] = <floatData_t *> malloc(sizeof(floatData_t))
-        dataF[x_i].ptr = <float *> input_ptr
-        dataF[x_i].totalSize = <size_t> x["shape"][0]
-    return <size_t>dataF
-
-
-def _build_dataDouble(arr_interfaces):
-    cdef doubleData_t **dataD = <doubleData_t **> \
-        malloc(sizeof(doubleData_t *)
-               * len(arr_interfaces))
-
-    cdef uintptr_t input_ptr
-    for x_i in range(len(arr_interfaces)):
-        x = arr_interfaces[x_i]
-        input_ptr = x["data"]
-        dataD[x_i] = <doubleData_t *> malloc(sizeof(doubleData_t))
-        dataD[x_i].ptr = <double *> input_ptr
-        dataD[x_i].totalSize = <size_t> x["shape"][0]
-    return <size_t>dataD
-
-
-def _freeDoubleD(data, arr_interfaces):
-    cdef uintptr_t data_ptr = data
-    cdef doubleData_t **d = <doubleData_t**>data_ptr
-    for x_i in range(len(arr_interfaces)):
-        free(d[x_i])
-    free(d)
-
-
-def _freeFloatD(data, arr_interfaces):
-    cdef uintptr_t data_ptr = data
-    cdef floatData_t **d = <floatData_t**>data_ptr
-    for x_i in range(len(arr_interfaces)):
-        free(d[x_i])
-    free(d)
-
-
-def _build_predData(partsToSizes, rank, n_cols, dtype):
-    arr_interfaces_trans = []
-    for idx, rankSize in enumerate(partsToSizes):
-        rk, size = rankSize
-        if rank == rk:
-            trans_ary = CumlArray.zeros((size, n_cols),
-                                        order="F",
-                                        dtype=dtype)
-
-            arr_interfaces_trans.append({"obj": trans_ary,
-                                         "data": trans_ary.ptr,
-                                         "shape": (size, n_cols)})
-
-    return arr_interfaces_trans
