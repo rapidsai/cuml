3ebb502bc652cb8d5540c005631f7fc80e2d8d7b
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-05-05 20:25:23 -0700

    ENH updated dask cluster algos to use verbosity arg

--- python/cuml/dask/cluster/kmeans.py
+++ python/cuml/dask/cluster/kmeans.py
@@ -31,6 +31,8 @@ from cuml.dask.common.utils import raise_exception_from_futures
 from dask.distributed import wait
 from cuml.utils.memory_utils import with_cupy_rmm
 
+import cuml.common.logger as logger
+
 
 class KMeans(BaseEstimator, DelayedPredictionMixin, DelayedTransformMixin):
     """
@@ -56,8 +58,8 @@ class KMeans(BaseEstimator, DelayedPredictionMixin, DelayedTransformMixin):
         The more iterations of EM, the more accurate, but slower.
     tol : float (default = 1e-4)
         Stopping criterion when centroid means do not change much.
-    verbose : boolean (default = 0)
-        If True, prints diagnositc information.
+    verbosity : int (default = cuml.common.logger.LEVEL_INFO)
+        Logging level for printing diagnostic information
     random_state : int (default = 1)
         If you want results to be the same when you restart Python,
         select a state.
@@ -91,9 +93,9 @@ class KMeans(BaseEstimator, DelayedPredictionMixin, DelayedTransformMixin):
 
     """
 
-    def __init__(self, client=None, verbose=False, **kwargs):
+    def __init__(self, client=None, verbosity=logger.LEVEL_INFO, **kwargs):
         super(KMeans, self).__init__(client=client,
-                                     verbose=verbose,
+                                     verbosity=verbosity,
                                      **kwargs)
 
     @staticmethod
