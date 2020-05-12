193c153421641aa129b5af7012158aee49f7bb16
Tamas Bela Feher <tfeher@nvidia.com>
2020-05-13 01:00:04 +0200

    PRF Implement CUTLASS GEMM in GramMatrixBase

--- cpp/src_prims/matrix/grammatrix.h
+++ cpp/src_prims/matrix/grammatrix.h
@@ -19,6 +19,7 @@
 #include <cuda_utils.h>
 #include <distance/distance.h>
 #include <linalg/cublas_wrappers.h>
+#include <linalg/cutlass_wrappers.h>
 #include <linalg/gemm.h>
 
 namespace MLCommon {
@@ -122,11 +123,16 @@ class GramMatrixBase {
    */
   void linear(const math_t *x1, int n1, int n_cols, const math_t *x2, int n2,
               math_t *out, cudaStream_t stream, int ld1, int ld2, int ld_out) {
-    math_t alpha = 1.0;
-    math_t beta = 0.0;
-    CUBLAS_CHECK(LinAlg::cublasgemm(cublas_handle, CUBLAS_OP_N, CUBLAS_OP_T, n1,
-                                    n2, n_cols, &alpha, x1, ld1, x2, ld2, &beta,
-                                    out, ld_out, stream));
+    // Temporaryli switch to CUTLASS based gemm path.
+    // math_t alpha = 1.0;
+    // math_t beta = 0.0;
+    // CUBLAS_CHECK(LinAlg::cublasgemm(cublas_handle, CUBLAS_OP_N, CUBLAS_OP_T, n1,
+    //                                 n2, n_cols, &alpha, x1, ld1, x2, ld2, &beta,
+    //                                 out, ld_out, stream));
+
+    // Test whether fin_op is applied: we add +1 which should fail GramMatrixTest
+    auto fin_op = [] __device__(math_t d_val, int idx) { return d_val + 1; };
+    gemm(x1, n1, n_cols, x2, n2, out, stream, ld1, ld2, ld_out, fin_op);
   }
 
   /** Calculates the Gram matrix using Euclidean distance.
@@ -155,6 +161,38 @@ class GramMatrixBase {
     Distance::distance<Distance::EucUnexpandedL2, math_t, math_t, math_t,
                        OutputTile_t>(x1, x2, out, n1, n2, n_cols, NULL, 0,
                                      fin_op, stream, false);
+  }
+
+  /** Calculates the Gram matrix using GEMM with epilogue.
+   *
+   * Can be used as a building block for more complex kernel functions.
+   *
+   * @param [in] x1 device array of vectors in column major format,
+   *  size [n1*n_cols]
+   * @param [in] n1 number vectors in x1
+   * @param [in] n_cols number of columns (features) in x1 and x2
+   * @param [in] x2 device array of vectors in column major format,
+   *   size [n2*n_cols]
+   * @param [in] n2 number vectors in x2
+   * @param [out] out device buffer to store the Gram matrix in column major
+   *   format, size [n1*n2]
+   * @param [in] stream cuda stream
+   * @param ld1 leading dimension of x1 (usually it is n1)
+   * @param ld2 leading dimension of x2 (usually it is n2)
+   * @param ld_out leading dimension of out (usually it is n1)
+   * @param fin_op final device lambda to be applied in epilogue
+   */
+  template <typename FinalLambda>
+  void gemm(const math_t *x1, int n1, int n_cols, const math_t *x2, int n2,
+            math_t *out, cudaStream_t stream, int ld1, int ld2, int ld_out,
+            FinalLambda fin_op) {
+    typedef cutlass::Shape<8, 128, 128> OutputTile_t;
+    using EpilogueFunctor = cutlass::gemm::LinearScaling<math_t>;
+    auto epilogue_init = [](typename EpilogueFunctor::Params &p) { return 0; };
+    //cuML's cuntom gemm wrapper uses ldc = ldd
+    MLCommon::LinAlg::gemm<math_t, math_t, math_t, OutputTile_t>(
+      CUBLAS_OP_N, CUBLAS_OP_T, n1, n2, n_cols, (math_t)1, x1, ld1, x2, ld2,
+      (math_t)0, nullptr, ld_out, out, epilogue_init, fin_op, stream);
   }
 };
 };  // end namespace Matrix
