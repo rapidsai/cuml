d7251e92497496c4339b2d1095f414c85927fcd4
Eli Fajardo <efajardo@nvidia.com>
2020-05-19 18:43:21 +0000

    add matplotlib for notebook testing

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -62,6 +62,7 @@ conda install -c conda-forge -c rapidsai -c rapidsai-nightly -c nvidia \
       "statsmodels" \
       "xgboost====1.0.2dev.rapidsai0.13" \
       "lightgbm" \
+      "matplotlib" \
       "ipython=7.3*" \
       "jupyterlab"
 
