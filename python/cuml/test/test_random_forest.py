bbe056de8f5bd7eed90a534dbd815f61506ef14c
Vinay D <vinayd@nvidia.com>
2020-05-20 15:29:48 +0530

    Moved printing test to test_random_forest.py

--- python/cuml/test/test_random_forest.py
+++ python/cuml/test/test_random_forest.py
@@ -687,3 +687,47 @@ def test_rf_classification_proba(small_clf, datatype,
         # Max difference of 0.0061 is seen between the mse values of
         # predict proba function of fil and sklearn
         assert fil_mse <= (sk_mse + 0.0061)
+
+@pytest.mark.parametrize('n_estimators', [5, 10, 20])
+@pytest.mark.parametrize('detailed_printing', [True, False])
+def test_rf_printing(capfd, n_estimators, detailed_printing):
+
+    X, y = make_classification(n_samples=500, n_features=10,
+                               n_clusters_per_class=1, n_informative=5,
+                               random_state=94929, n_classes=2)
+
+    X = X.astype(np.float32)
+    y = y.astype(np.int32)
+
+    # Create a handle for the cuml model
+    handle, stream = get_handle(True, n_streams=1)
+
+    # Initialize cuML Random Forest classification model
+    cuml_model = curfc(handle=handle, max_features=1.0, rows_sample=1.0,
+                       n_bins=16, split_algo=0, split_criterion=0,
+                       min_rows_per_node=2, seed=23707, n_streams=1,
+                       n_estimators=n_estimators, max_leaves=-1,
+                       max_depth=16)
+
+    # Train model on the data
+    cuml_model.fit(X, y)
+
+    if detailed_printing:
+        cuml_model.print_detailed()
+    else:
+        cuml_model.print_summary()
+
+    # Read the captured output
+    printed_output = capfd.readouterr().out
+
+    # Test 1: Output is non-zero
+    assert '' != printed_output
+
+    # Count the number of trees printed
+    tree_count = 0
+    for line in printed_output.split('\n'):
+        if line.strip().startswith('Tree #'):
+            tree_count += 1
+
+    # Test 2: Correct number of trees are printed
+    assert n_estimators == tree_count
