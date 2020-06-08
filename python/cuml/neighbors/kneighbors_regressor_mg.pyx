60c2e3ac9507cf59298a4c89a3faec7d5c2f5309
wxbn <wxbn@live.fr>
2020-06-08 10:11:13 +0000

    KNN Regressor Cython code refactor

--- python/cuml/neighbors/kneighbors_regressor_mg.pyx
+++ python/cuml/neighbors/kneighbors_regressor_mg.pyx
@@ -34,7 +34,7 @@ from libc.stdint cimport uintptr_t
 from libcpp cimport bool
 from libcpp.memory cimport shared_ptr
 
-from cuml.neighbors import NearestNeighbors
+from cuml.neighbors import KNeighborsMG
 from cudf.core import DataFrame as cudfDataFrame
 
 cdef extern from "cumlprims/opg/selection/knn.hpp" namespace \
@@ -59,38 +59,7 @@ cdef extern from "cumlprims/opg/selection/knn.hpp" namespace \
     ) except +
 
 
-def _free_mem(out_vec, out_i_vec, out_d_vec,
-              idx_local_parts, idx_desc,
-              q_local_parts, q_desc,
-              lbls_local_parts):
-
-    free(<void*><uintptr_t>out_vec)
-    free(<void*><uintptr_t>out_i_vec)
-    free(<void*><uintptr_t>out_d_vec)
-
-    cdef floatData_t* ptr
-
-    cdef vector[floatData_t*] *idx_local_parts_v = \
-        <vector[floatData_t *]*><uintptr_t>idx_local_parts
-    for i in range(idx_local_parts_v.size()):
-        ptr = idx_local_parts_v.at(i)
-        free(<void*>ptr)
-    free(<void*><uintptr_t>idx_local_parts)
-
-    cdef vector[floatData_t*] *q_local_parts_v = \
-        <vector[floatData_t *]*><uintptr_t>q_local_parts
-    for i in range(q_local_parts_v.size()):
-        ptr = q_local_parts_v.at(i)
-        free(<void*>ptr)
-    free(<void*><uintptr_t>q_local_parts)
-
-    free(<void*><uintptr_t>idx_desc)
-    free(<void*><uintptr_t>q_desc)
-
-    free(<void*><uintptr_t>lbls_local_parts)
-
-
-class KNeighborsRegressorMG(NearestNeighbors):
+class KNeighborsRegressorMG(KNeighborsMG):
     """
     Multi-node Multi-GPU K-Nearest Neighbors Regressor Model.
 
@@ -127,87 +96,44 @@ class KNeighborsRegressorMG(NearestNeighbors):
         -------
         predictions : outputs, indices, distances
         """
+        out_type = self.get_out_type(data, query)
+
+        out_type = self.get_out_type(data, query)
+
+        input = self.gen_local_input(data, data_parts_to_ranks, data_nrows,
+                                     query, query_parts_to_ranks, query_nrows,
+                                     ncols, rank, convert_dtype)
+
+        output = self.gen_local_output(data, convert_dtype, dtype='int32')
+
+        query_cais = input['cais']['query']
+        local_query_rows = list(map(lambda x: x.shape[0], query_cais))
+        result = self.alloc_local_output(local_query_rows)
+
+        cdef vector[floatData_t*] *out_result_local_parts \
+            = new vector[floatData_t*]()
+        output_cais = []
+        for n_rows in local_query_rows:
+            o_cai = CumlArray.zeros(shape=(n_rows, n_outputs),
+                                    order="C", dtype=np.float32)
+            output_cais.append(o_cai)
+            out_result_local_parts.push_back(new floatData_t(
+                <float*><uintptr_t>o_cai.ptr, n_rows * n_outputs))
+
         cdef cumlHandle* handle_ = <cumlHandle*><size_t>self.handle.getHandle()
-        if len(data) > 0:
-            self._set_output_type(data[0])
-        out_type = self.output_type
-        if len(query) > 0:
-            out_type = self._get_output_type(query[0])
-
-        idx = [d[0] for d in data]
-        lbls = [d[1] for d in data]
-        self.n_dims = ncols
-
-        idx_cai, idx_local_parts, idx_desc = \
-            _build_part_inputs(idx, data_parts_to_ranks,
-                               data_nrows, ncols, rank, convert_dtype)
-
-        q_cai, q_local_parts, q_desc = \
-            _build_part_inputs(query, query_parts_to_ranks,
-                               query_nrows, ncols, rank, convert_dtype)
-
-        cdef vector[float_ptr_vector] *lbls_local_parts = \
-            new vector[float_ptr_vector](<int>len(lbls))
-        lbls_dev_arr = []
-        for i, arr in enumerate(lbls):
-            for j in range(arr.shape[1]):
-                if isinstance(arr, cudfDataFrame):
-                    col = arr.iloc[:, j]
-                else:
-                    col = arr[:, j]
-                lbls_arr, _, _, _ = \
-                    input_to_cuml_array(col, order="F",
-                                        convert_to_dtype=(np.float32
-                                                          if convert_dtype
-                                                          else None),
-                                        check_dtype=[np.float32])
-                lbls_dev_arr.append(lbls_arr)
-                lbls_local_parts.at(i).push_back(<float*><uintptr_t>
-                                                 lbls_arr.ptr)
-
-        cdef vector[floatData_t*] *out_vec \
-            = new vector[floatData_t*]()
-        cdef vector[int64Data_t*] *out_i_vec \
-            = new vector[int64Data_t*]()
-        cdef vector[floatData_t*] *out_d_vec \
-            = new vector[floatData_t*]()
-
-        output = []
-        output_i = []
-        output_d = []
-
-        for query_part in q_cai:
-            n_rows = query_part.shape[0]
-            o_ary = CumlArray.zeros(shape=(n_rows, n_outputs),
-                                    order="C", dtype=np.float32)
-            i_ary = CumlArray.zeros(shape=(n_rows, self.n_neighbors),
-                                    order="C", dtype=np.int64)
-            d_ary = CumlArray.zeros(shape=(n_rows, self.n_neighbors),
-                                    order="C", dtype=np.float32)
-
-            output.append(o_ary)
-            output_i.append(i_ary)
-            output_d.append(d_ary)
-
-            out_vec.push_back(new floatData_t(
-                <float*><uintptr_t>o_ary.ptr, n_rows * n_outputs))
-
-            out_i_vec.push_back(new int64Data_t(
-                <int64_t*><uintptr_t>i_ary.ptr, n_rows * self.n_neighbors))
-
-            out_d_vec.push_back(new floatData_t(
-                <float*><uintptr_t>d_ary.ptr, n_rows * self.n_neighbors))
 
         knn_regress(
             handle_[0],
-            out_vec,
-            out_i_vec,
-            out_d_vec,
-            deref(<vector[floatData_t*]*><uintptr_t>idx_local_parts),
-            deref(<PartDescriptor*><uintptr_t>idx_desc),
-            deref(<vector[floatData_t*]*><uintptr_t>q_local_parts),
-            deref(<PartDescriptor*><uintptr_t>q_desc),
-            deref(<vector[float_ptr_vector]*><uintptr_t>lbls_local_parts),
+            out_result_local_parts,
+            <vector[int64Data_t*]*><uintptr_t>result['indices'],
+            <vector[floatData_t*]*><uintptr_t>result['distances'],
+            deref(<vector[floatData_t*]*><uintptr_t>
+                  input['data']['local_parts']),
+            deref(<PartDescriptor*><uintptr_t>input['data']['desc']),
+            deref(<vector[floatData_t*]*><uintptr_t>
+                  input['query']['local_parts']),
+            deref(<PartDescriptor*><uintptr_t>input['query']['desc']),
+            deref(<vector[float_ptr_vector]*><uintptr_t>output['outputs']),
             <bool>False,  # column-major index
             <bool>False,  # column-major query
             <int>self.n_neighbors,
@@ -218,17 +144,17 @@ class KNeighborsRegressorMG(NearestNeighbors):
 
         self.handle.sync()
 
-        _free_mem(<uintptr_t>out_vec,
-                  <uintptr_t>out_i_vec,
-                  <uintptr_t>out_d_vec,
-                  <uintptr_t>idx_local_parts,
-                  <uintptr_t>idx_desc,
-                  <uintptr_t>q_local_parts,
-                  <uintptr_t>q_desc,
-                  <uintptr_t>lbls_local_parts)
+        self.free_mem(input, result)
+        free(<void*><uintptr_t>output['outputs'])
 
-        output = list(map(lambda o: o.to_output(out_type), output))
-        output_i = list(map(lambda o: o.to_output(out_type), output_i))
-        output_d = list(map(lambda o: o.to_output(out_type), output_d))
+        for i in range(out_result_local_parts.size()):
+            free(<void*>out_result_local_parts.at(i))
+        free(<void*><uintptr_t>out_result_local_parts)
+
+        output = list(map(lambda o: o.to_output(out_type), output_cais))
+        output_i = list(map(lambda o: o.to_output(out_type),
+                            result['cais']['indices']))
+        output_d = list(map(lambda o: o.to_output(out_type),
+                            result['cais']['distances']))
 
         return output, output_i, output_d
