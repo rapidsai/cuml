f46a9892f7af60bcd125d7c2d192baf16d344231
salonijain27 <salj7856@gmail.com>
2020-06-17 12:27:16 -0500

    update rf and fil to accept storage type as bool or 'auto'

--- python/cuml/ensemble/__init__.py
+++ python/cuml/ensemble/__init__.py
@@ -18,4 +18,4 @@
 from cuml.ensemble.randomforestclassifier import RandomForestClassifier
 from cuml.ensemble.randomforestregressor import RandomForestRegressor
 from cuml.ensemble.randomforest_common import _check_fil_parameter_validity, \
-    _obtain_treelite_model, _obtain_fil_model
+    _obtain_fil_model
