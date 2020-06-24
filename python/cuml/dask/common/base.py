5db73f00ef2a8e4749d8de2342af8e5f61a3979a
Corey J. Nolet <cjnolet@gmail.com>
2020-06-23 20:18:50 -0400

    Fixing typo

--- python/cuml/dask/common/base.py
+++ python/cuml/dask/common/base.py
@@ -55,7 +55,7 @@ class BaseEstimator(object):
         Returns
         -------
 
-        mode : Trained single-GPU model or None if the model has not
+        model : Trained single-GPU model or None if the model has not
                yet been trained.
         """
 
