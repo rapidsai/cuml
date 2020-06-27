b5c716da529faac7924c3a789ccea5996818c89d
Corey J. Nolet <cjnolet@gmail.com>
2020-06-27 02:40:40 -0400

    Fixing bug

--- cpp/src_prims/selection/knn.cuh
+++ cpp/src_prims/selection/knn.cuh
@@ -312,6 +312,7 @@ void brute_force_knn(std::vector<float *> &input, std::vector<int> &sizes,
     CUDA_CHECK(cudaStreamSynchronize(internalStreams[i]));
   }
 
+  // This is necessary for proper index translations
   knn_merge_parts(out_D, out_I, res_D, res_I, n, input.size(), k, userStream,
 	  			  trans.data());
 
