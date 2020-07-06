2ee0a8a4b7841a167b28d9ce83c75f6bd2d90f41
wxbn <wxbn@live.fr>
2020-07-06 09:12:15 +0000

    Fix problem with brute_force_knn

--- cpp/src_prims/selection/knn.cuh
+++ cpp/src_prims/selection/knn.cuh
@@ -286,10 +286,8 @@ void brute_force_knn(std::vector<float *> &input, std::vector<int> &sizes,
     CUDA_CHECK(cudaStreamSynchronize(internalStreams[i]));
   }
 
-  if (input.size() > 1) {
-    knn_merge_parts(out_D, out_I, res_D, res_I, n, input.size(), k, userStream,
-                    trans.data());
-  }
+  knn_merge_parts(out_D, out_I, res_D, res_I, n, input.size(), k, userStream,
+                  trans.data());
 
   // Perform necessary post-processing
   if ((m == faiss::MetricType::METRIC_L2 ||
