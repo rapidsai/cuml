6c1b75582d4d6bec244604eca8650ea483bf257b
Jiwei Liu <jiweil@dgx11.aselab.nvidia.com>
2020-07-20 19:46:02 -0700

    refactor works

--- python/cuml/preprocessing/TargetEncoder.py
+++ python/cuml/preprocessing/TargetEncoder.py
@@ -1,88 +1,89 @@
 import numpy as np
 import cudf as gd
 import cupy as cp
-import dask_cudf
 
 class TargetEncoder:
     
-    def __init__(self, folds, smooth, seed=42, mode='gpu'):
+    def __init__(self, folds, smooth, seed=42):
         self.folds = folds
         self.seed = seed
         self.smooth = smooth
-        if mode=='gpu':
-            self.np = cp
-            self.df = gd
-        else:
-            self.np = np
-            self.df = pd
-        self.mode = mode
+        self.y_col = '__TARGET__'
+        self.x_col = '__FEA__' 
+        self.out_col = '__TARGET_ENCODE__'
+        self.fold_col = '__FOLD__'
+        self.id_col = '__INDEX__'
+ 
+    def fit_transform(self, x, y):
+        cp.random.seed(self.seed)
+        train = self._to_frame(x)
+        x_cols = [i for i in train.columns.tolist() if i!=self.id_col]
         
-    def fit_transform(self, train, x_col, y_col, y_mean=None, out_col = None, out_dtype=None):
-        
-        self.y_col = y_col
-        self.np.random.seed(self.seed)
-        
-        if 'fold' not in train.columns:
-            fsize = len(train)//self.folds
-            if isinstance(train,dask_cudf.core.DataFrame):
-                #train['fold'] = train.map_partitions(lambda cudf_df: cudf_df.index%self.folds)
-                train['fold'] = 1
-                train['fold'] = train['fold'].cumsum()
-                train['fold'] = train['fold']//fsize
-                train['fold'] = train['fold']%self.folds
+        if isinstance(y, gd.Series):
+            train[self.y_col] = y.values
+        elif isinstance(y, cp.ndarray):
+            if len(y.shape) == 1:
+                train[self.y_col] = y
+            elif y.shape[1] == 1:
+                train[self.y_col] = y[:,0]
             else:
-                #train['fold'] = self.np.random.randint(0,self.folds,len(train))
-                train['fold'] = (train.index.values//fsize)%self.folds
+                assert 0
+        else:
+            assert 0
+        train[self.fold_col] = cp.random.randint(0,self.folds,len(train))
         
-        if out_col is None:
-            tag = x_col if isinstance(x_col,str) else '_'.join(x_col)
-            out_col = f'TE_{tag}_{self.y_col}'
+        self.mean = train[self.y_col].mean()#.compute().astype('float32')
         
-        if y_mean is None:
-            y_mean = train[y_col].mean()#.compute().astype('float32')
-        self.mean = y_mean
+        cols = [self.fold_col]+x_cols
         
-        cols = ['fold',x_col] if isinstance(x_col,str) else ['fold']+x_col
-        
-        agg_each_fold = train.groupby(cols).agg({y_col:['count','sum']}).reset_index()
+        agg_each_fold = train.groupby(cols).agg({self.y_col:['count','sum']}).reset_index()
         agg_each_fold.columns = cols + ['count_y','sum_y']
         
-        agg_all = agg_each_fold.groupby(x_col).agg({'count_y':'sum','sum_y':'sum'}).reset_index()
-        cols = [x_col] if isinstance(x_col,str) else x_col
+        agg_all = agg_each_fold.groupby(x_cols).agg({'count_y':'sum','sum_y':'sum'}).reset_index()
+        cols = x_cols
         agg_all.columns = cols + ['count_y_all','sum_y_all']
         
-        agg_each_fold = agg_each_fold.merge(agg_all,on=x_col,how='left')
+        agg_each_fold = agg_each_fold.merge(agg_all,on=x_cols,how='left')
         agg_each_fold['count_y_all'] = agg_each_fold['count_y_all'] - agg_each_fold['count_y']
         agg_each_fold['sum_y_all'] = agg_each_fold['sum_y_all'] - agg_each_fold['sum_y']
-        agg_each_fold[out_col] = (agg_each_fold['sum_y_all']+self.smooth*self.mean)/(agg_each_fold['count_y_all']+self.smooth)
+        agg_each_fold[self.out_col] = (agg_each_fold['sum_y_all']+self.smooth*self.mean)/(agg_each_fold['count_y_all']+self.smooth)
         agg_each_fold = agg_each_fold.drop(['count_y_all','count_y','sum_y_all','sum_y'],axis=1)
         
-        agg_all[out_col] = (agg_all['sum_y_all']+self.smooth*self.mean)/(agg_all['count_y_all']+self.smooth)
+        agg_all[self.out_col] = (agg_all['sum_y_all']+self.smooth*self.mean)/(agg_all['count_y_all']+self.smooth)
         agg_all = agg_all.drop(['count_y_all','sum_y_all'],axis=1)
         self.agg_all = agg_all
         
-        train.columns
-        cols = ['fold',x_col] if isinstance(x_col,str) else ['fold']+x_col
+        cols = [self.fold_col]+x_cols
         train = train.merge(agg_each_fold,on=cols,how='left')
         del agg_each_fold
-        #self.agg_each_fold = agg_each_fold
-        if self.mode=='gpu':
-            if isinstance(train,dask_cudf.core.DataFrame):
-                train[out_col] = train.map_partitions(lambda cudf_df: cudf_df[out_col].nans_to_nulls())
-            else:
-                train[out_col] = train[out_col].nans_to_nulls()
-        train[out_col] = train[out_col].fillna(self.mean)
-        
-        if out_dtype is not None:
-            train[out_col] = train[out_col].astype(out_dtype)
-        return train
+        return self._get_return_value(train)
     
-    def transform(self, test, x_col, out_col = None, out_dtype=None):
-        if out_col is None:
-            tag = x_col if isinstance(x_col,str) else '_'.join(x_col)
-            out_col = f'TE_{tag}_{self.y_col}'
-        test = test.merge(self.agg_all,on=x_col,how='left')
-        test[out_col] = test[out_col].fillna(self.mean)
-        if out_dtype is not None:
-            test[out_col] = test[out_col].astype(out_dtype)
-        return test
+    def transform(self, x):
+        test = self._to_frame(x)
+        x_cols = [i for i in test.columns.tolist() if i!=self.id_col]
+        test = test.merge(self.agg_all,on=x_cols,how='left')
+        return self._get_return_value(test) 
+
+    def _get_return_value(self, df):
+        df[self.out_col] = df[self.out_col].nans_to_nulls()
+        df[self.out_col] = df[self.out_col].fillna(self.mean)
+        df = df.sort_values(self.id_col) 
+        res = df[self.out_col].values.copy()
+        del df
+        return res 
+
+    def _to_frame(self, x):
+        if isinstance(x, gd.DataFrame):
+            df = x.copy()
+        elif isinstance(x, gd.Series):
+            df = x.to_frame()
+        elif isinstance(x, cp.ndarray):
+            df = gd.DataFrame()
+            if len(x.shape) == 1:
+                df[self.x_col] = x
+            else:
+                df = gd.DataFrame(x, columns=[f'{self.x_col}_{i}' for i in range(x.shape[1])])
+        else:
+            assert 0
+        df[self.id_col] = cp.arange(len(x))
+        return df
