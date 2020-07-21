32c56d2b788323a3d2f40ea7d2758e1a54bd04d3
Jiwei Liu <jiweil@dgx11.aselab.nvidia.com>
2020-07-21 12:33:57 -0700

    add docstring

--- python/cuml/preprocessing/TargetEncoder.py
+++ python/cuml/preprocessing/TargetEncoder.py
@@ -1,13 +1,68 @@
+#
+# Copyright (c) 2019, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import numpy as np
 import cudf as gd
 import cupy as cp
 
 class TargetEncoder:
-    
-    def __init__(self, folds, smooth, seed=42):
-        self.folds = folds
+    """
+    A cudf based implementation of target encoding
+
+    Parameters
+    ----------
+    n_folds : int (default=5)
+        Default number of folds
+    smooth : int (default=0)
+        Number of samples to smooth the encoding
+    seed : int (default=42)
+        Random seed
+    split : {'random', 'continuous', 'interleaved'}, default='interleaved'
+    Examples
+    --------
+    Converting a categorical implementation to a numerical one
+
+    .. code-block:: python
+
+        from cudf import DataFrame, Series
+
+        train = DataFrame({'category': ['a', 'b', 'b', 'a'],
+                           'label': [1, 0, 1, 1]})
+        test = DataFrame({'category': ['a', 'c', 'b', 'a']})
+
+        # There is only one correct way to do this
+        le = TargetEncoder()
+        train_encoded = le.fit_transform(train.category, train.label)
+        test_encoded = le.transform(test.category)
+        print(train_encoded)
+        print(test_encoded)
+
+    Output:
+
+    .. code-block:: python
+
+        [1. 1. 0. 1.]
+        [1.   0.75 0.5  1.  ]
+
+    """    
+    def __init__(self, n_folds=4, smooth=0, seed=42, split='interleaved'):
+        self.n_folds = n_folds
         self.seed = seed
         self.smooth = smooth
+        self.split = split 
         self.y_col = '__TARGET__'
         self.x_col = '__FEA__' 
         self.out_col = '__TARGET_ENCODE__'
@@ -27,11 +82,26 @@ class TargetEncoder:
             elif y.shape[1] == 1:
                 train[self.y_col] = y[:,0]
             else:
-                assert 0
+                raise ValueError(f'Input of shape {y.shape} is not a 1-D array.')
         else:
-            assert 0
-        train[self.fold_col] = cp.random.randint(0,self.folds,len(train))
+            raise TypeError(
+                "Input of type {type(y)} is not cudf.Series, "
+                "or cupy.ndarray") 
+
+        if self.split == 'random':
+            train[self.fold_col] = cp.random.randint(0,self.n_folds,len(train))
+        elif self.split == 'continuous':
+            train[self.fold_col] = cp.arange(len(train))/(len(train)/self.n_folds)
+            train[self.fold_col] = train[self.fold_col]%self.n_folds
+        elif self.split == 'interleaved':
+            train[self.fold_col] = cp.arange(len(train))
+            train[self.fold_col] = train[self.fold_col]%self.n_folds
+        else:
+            msg = ("split should be either 'random' or 'continuous' or 'interleaved', "
+                   "got {0}.".format(self.split))
+            raise ValueError(msg)
         
+        train[self.fold_col] = train[self.fold_col].astype('int32')       
         self.mean = train[self.y_col].mean()#.compute().astype('float32')
         
         cols = [self.fold_col]+x_cols
@@ -84,6 +154,8 @@ class TargetEncoder:
             else:
                 df = gd.DataFrame(x, columns=[f'{self.x_col}_{i}' for i in range(x.shape[1])])
         else:
-            assert 0
+            raise TypeError(
+                f"Input of type {x.shape} is not cudf.Series, cudf.DataFrame "
+                "or cupy.ndarray")
         df[self.id_col] = cp.arange(len(x))
         return df
