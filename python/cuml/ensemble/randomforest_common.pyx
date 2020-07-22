1ef2c440230e884cbe979a4ec719a81e11a43726
Nick Becker <nickb500@gmail.com>
2020-07-22 09:03:22 -0700

    move classes_ logic to classifier section of dataset prep in rf common

--- python/cuml/ensemble/randomforest_common.pyx
+++ python/cuml/ensemble/randomforest_common.pyx
@@ -241,10 +241,10 @@ class BaseRandomForestModel(Base):
             if y_dtype != np.int32:
                 raise TypeError("The labels `y` need to be of dtype"
                                 " `int32`")
-            unique_labels = rmm_cupy_ary(cp.unique, y_m)
+            self.classes_ = rmm_cupy_ary(cp.unique, y_m)
             self.num_classes = len(unique_labels)
             for i in range(self.num_classes):
-                if i not in unique_labels:
+                if i not in self.classes_:
                     raise ValueError("The labels need "
                                      "to be consecutive values from "
                                      "0 to the number of unique label values")
