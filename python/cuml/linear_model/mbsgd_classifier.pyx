ee3e65cd38726e3496e1835bae103cabba4f8a0f
Nick Becker <nickb500@gmail.com>
2020-07-23 09:35:06 -0700

    missed flake8 with cython files again

--- python/cuml/linear_model/mbsgd_classifier.pyx
+++ python/cuml/linear_model/mbsgd_classifier.pyx
@@ -173,7 +173,7 @@ class MBSGDClassifier(Base, ClassifierMixin):
         """
         self._set_n_features_in(X)
         self.cu_mbsgd_classifier._estimator_type = self._estimator_type
-        
+
         self.cu_mbsgd_classifier.fit(X, y, convert_dtype=convert_dtype)
         self.coef_ = self.cu_mbsgd_classifier.coef_
         self.classes_ = self.cu_mbsgd_classifier.classes_
