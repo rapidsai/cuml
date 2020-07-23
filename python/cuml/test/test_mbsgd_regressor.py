6461cd5f61544a0e028f21675d86214e8e739dc6
Nanthini Balasubramanian <nathanb@nvidia.com>
2020-07-23 16:26:27 +0000

    ADD new test file for solver attributes

--- python/cuml/test/test_mbsgd_regressor.py
+++ python/cuml/test/test_mbsgd_regressor.py
@@ -120,11 +120,3 @@ def test_mbsgd_regressor_default(make_dataset):
                      convert_dtype=datatype)
 
     assert cu_r2 > 0.9
-
-
-def test_mbsgd_regressor_attributes(make_dataset):
-    nrows, datatype, X_train, X_test, y_train, y_test = make_dataset
-    clf = cumlMBSGRegressor()
-    clf.fit(X_train, y_train)
-    assert hasattr(clf, "dtype")
-    assert hasattr(clf, "solver_model")
