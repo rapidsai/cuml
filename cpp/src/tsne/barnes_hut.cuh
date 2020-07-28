fca92312c3f9f0f4524d4ced4fb6a0dfb55362c4
Devin Robison <drobison@nvidia.com>
2020-07-28 11:51:47 -0600

    Style fix

--- cpp/src/tsne/barnes_hut.cuh
+++ cpp/src/tsne/barnes_hut.cuh
@@ -259,10 +259,13 @@ void Barnes_Hut(float *VAL, const int *COL, const int *ROW, const int NNZ,
     MLCommon::copy(&h_flag, flag_unstable_computation.data(), 1, stream);
     if (h_flag) {
       CUML_LOG_ERROR(
-        "Detected zero divisor in attractive force kernel after '%d' iterations;"
-        " returning early. Your final results may not be accurate. In some cases"
+        "Detected zero divisor in attractive force kernel after '%d' "
+        "iterations;"
+        " returning early. Your final results may not be accurate. In some "
+        "cases"
         " this error can be resolved by increasing perplexity, and n_neighbors;"
-        " if the problem persists, please use 'method=exact'.", iter);
+        " if the problem persists, please use 'method=exact'.",
+        iter);
       break;
     }
 
