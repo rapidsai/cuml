7a858e80b325120e8f846ef3adc8adaee8f0a2ba
Jiwei Liu <jiweil@dgx11.aselab.nvidia.com>
2020-07-28 15:11:15 -0700

    made changes as required

--- python/cuml/preprocessing/TargetEncoder.py
+++ python/cuml/preprocessing/TargetEncoder.py
@@ -14,24 +14,42 @@
 # limitations under the License.
 #
 
-import cudf as gd
+import cudf
 import cupy as cp
 from cuml.common.exceptions import NotFittedError
 
 
 class TargetEncoder:
     """
-    A cudf based implementation of target encoding
+    A cudf based implementation of target encoding [1], which converts
+    one or mulitple categorical variables, 'Xs', with the average of
+    corresponding values of the target variable, 'Y'. The input data is
+    grouped by the columns `Xs` and the aggregated mean value of `Y` of
+    each group is calculated to replace each value of `Xs`. Several
+    optimizations are applied to prevent label leakage and parallelize
+    the execution.
 
     Parameters
     ----------
-    n_folds : int (default=5)
-        Default number of folds
+    n_folds : int (default=4)
+        Default number of folds for fitting training data. To prevent
+        label leakage in `fit`, we split data into `n_folds` and
+        encode one fold using the target variables of the remaining folds.
     smooth : int (default=0)
         Number of samples to smooth the encoding
     seed : int (default=42)
         Random seed
-    split : {'random', 'continuous', 'interleaved'}, default='interleaved'
+    split_method : {'random', 'continuous', 'interleaved'},
+        default='interleaved'
+        Method to split train data into `n_folds`.
+        'random': random split.
+        'continuous': consecutive samples are grouped into one folds.
+        'interleaved': samples are assign to each fold in a round robin way.
+
+    References
+    ----------
+    .. [1] https://maxhalford.github.io/blog/target-encoding/
+
     Examples
     --------
     Converting a categorical implementation to a numerical one
@@ -44,7 +62,6 @@ class TargetEncoder:
                            'label': [1, 0, 1, 1]})
         test = DataFrame({'category': ['a', 'c', 'b', 'a']})
 
-        # There is only one correct way to do this
         encoder = TargetEncoder()
         train_encoded = encoder.fit_transform(train.category, train.label)
         test_encoded = encoder.transform(test.category)
@@ -59,11 +76,12 @@ class TargetEncoder:
         [1.   0.75 0.5  1.  ]
 
     """
-    def __init__(self, n_folds=4, smooth=0, seed=42, split='interleaved'):
+    def __init__(self, n_folds=4, smooth=0, seed=42,
+                 split_method='interleaved'):
         self.n_folds = n_folds
         self.seed = seed
         self.smooth = smooth
-        self.split = split
+        self.split = split_method
         self.y_col = '__TARGET__'
         self.x_col = '__FEA__'
         self.out_col = '__TARGET_ENCODE__'
@@ -72,6 +90,22 @@ class TargetEncoder:
         self.train = None
 
     def fit(self, x, y):
+        """
+        Fit a TargetEncoder instance to a set of categories
+
+        Parameters
+        ----------
+        x: cudf.Series or cudf.DataFrame or cupy.ndarray
+           categories to be encoded. It's elements may or may
+           not be unique
+        y : cudf.Series or cupy.ndarray
+            Series containing the target variable.
+
+        Returns
+        -------
+        self : TargetEncoder
+            A fitted instance of itself to allow method chaining
+        """
         res, train = self._fit_transform(x, y)
         self.train_encode = res
         self.train = train
@@ -79,12 +113,36 @@ class TargetEncoder:
         return self
 
     def fit_transform(self, x, y):
+        """
+        Simultaneously fit and transform an input
+
+        This is functionally equivalent to (but faster than)
+        `TargetEncoder().fit(y).transform(y)`
+        """
         self.fit(x, y)
         return self.train_encode
 
     def transform(self, x):
+        """
+        Transform an input into its categorical keys.
+
+        This is intended for test data. For fitting and transforming
+        the training data, prefer `fit_transform`.
+
+        Parameters
+        ----------
+        x : cudf.Series
+            Input keys to be transformed. Its values doesn't have to
+            match the categories given to `fit`
+
+        Returns
+        -------
+        encoded : cupy.ndarray
+            The ordinally encoded input series
+
+        """
         self._check_is_fitted()
-        test = self._to_frame(x)
+        test = self._to_cudf_frame(x)
         if self._is_train_df(test):
             return self.train_encode
         x_cols = [i for i in test.columns.tolist() if i != self.id_col]
@@ -93,10 +151,10 @@ class TargetEncoder:
 
     def _fit_transform(self, x, y):
         cp.random.seed(self.seed)
-        train = self._to_frame(x)
+        train = self._to_cudf_frame(x)
         x_cols = [i for i in train.columns.tolist() if i != self.id_col]
 
-        if isinstance(y, gd.Series):
+        if isinstance(y, cudf.Series):
             train[self.y_col] = y.values
         elif isinstance(y, cp.ndarray):
             if len(y.shape) == 1:
@@ -110,6 +168,8 @@ class TargetEncoder:
             raise TypeError(
                 "Input of type {type(y)} is not cudf.Series, "
                 "or cupy.ndarray")
+
+        self.n_folds = min(self.n_folds, len(train))
 
         if self.split == 'random':
             train[self.fold_col] = cp.random.randint(0,
@@ -175,6 +235,10 @@ class TargetEncoder:
             raise NotFittedError(msg)
 
     def _is_train_df(self, df):
+        """
+        Return True if the dataframe `df` is the training dataframe, which 
+        is used in `fit_transform`
+        """
         if len(df) != len(self.train):
             return False
         for col in df.columns:
@@ -186,25 +250,28 @@ class TargetEncoder:
         return True
 
     def _get_return_value(self, df):
+        """
+        Return the result encoding in the same row order as input
+        """
         df[self.out_col] = df[self.out_col].nans_to_nulls()
         df[self.out_col] = df[self.out_col].fillna(self.mean)
         df = df.sort_values(self.id_col)
         res = df[self.out_col].values.copy()
         return res
 
-    def _to_frame(self, x):
-        if isinstance(x, gd.DataFrame):
+    def _to_cudf_frame(self, x):
+        if isinstance(x, cudf.DataFrame):
             df = x.copy()
-        elif isinstance(x, gd.Series):
-            df = x.to_frame()
+        elif isinstance(x, cudf.Series):
+            df = x.to_frame().copy()
         elif isinstance(x, cp.ndarray):
-            df = gd.DataFrame()
+            df = cudf.DataFrame()
             if len(x.shape) == 1:
                 df[self.x_col] = x
             else:
-                df = gd.DataFrame(x,
-                                  columns=[f'{self.x_col}_{i}'
-                                           for i in range(x.shape[1])])
+                df = cudf.DataFrame(x,
+                                    columns=[f'{self.x_col}_{i}'
+                                             for i in range(x.shape[1])])
         else:
             raise TypeError(
                 f"Input of type {x.shape} is not cudf.Series, cudf.DataFrame "
