f6dd662281fd86d2cb8b15eb4b2a160b266c1735
Corey J. Nolet <cjnolet@gmail.com>
2020-07-28 12:53:58 -0400

    Adding in and out to knn doxygen

--- cpp/src/knn/knn.cu
+++ cpp/src/knn/knn.cu
@@ -99,18 +99,18 @@ void knn_class_proba(cumlHandle &handle, std::vector<float *> &out,
  * a series of input arrays and combine the results into a single
  * output array for indexes and distances.
  *
- * @param handle the cuml handle to use
- * @param input an array of pointers to the input arrays
- * @param sizes an array of sizes of input arrays
- * @param n_params array size of input and sizes
- * @param D the dimensionality of the arrays
- * @param search_items array of items to search of dimensionality D
- * @param n number of rows in search_items
- * @param res_I the resulting index array of size n * k
- * @param res_D the resulting distance array of size n * k
- * @param k the number of nearest neighbors to return
- * @param rowMajorIndex is the index array in row major layout?
- * @param rowMajorQuery is the query array in row major layout?
+ * @param[in] handle the cuml handle to use
+ * @param[in] input an array of pointers to the input arrays
+ * @param[in] sizes an array of sizes of input arrays
+ * @param[in] n_params array size of input and sizes
+ * @param[in] D the dimensionality of the arrays
+ * @param[in] search_items array of items to search of dimensionality D
+ * @param[in] n number of rows in search_items
+ * @param[out] res_I the resulting index array of size n * k
+ * @param[out] res_D the resulting distance array of size n * k
+ * @param[in] k the number of nearest neighbors to return
+ * @param[in] rowMajorIndex is the index array in row major layout?
+ * @param[in] rowMajorQuery is the query array in row major layout?
  */
 extern "C" cumlError_t knn_search(const cumlHandle_t handle, float **input,
                                   int *sizes, int n_params, int D,
