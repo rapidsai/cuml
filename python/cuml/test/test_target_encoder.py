8dc70fc2a66e21c12eec6ec22673663c205eaabc
Jiwei Liu <jiweil@dgx11.aselab.nvidia.com>
2020-07-29 12:43:57 -0700

    add more tests & more tiny docstrings

--- python/cuml/test/test_target_encoder.py
+++ python/cuml/test/test_target_encoder.py
@@ -14,7 +14,9 @@
 
 from cuml.preprocessing.TargetEncoder import TargetEncoder
 import cudf
+import pandas
 import numpy as np
+import cupy as cp
 from cuml.test.utils import array_equal
 
 
@@ -34,6 +36,48 @@ def test_targetencoder_fit_transform():
 
 
 def test_targetencoder_transform():
+    train = cudf.DataFrame({'category': ['a', 'b', 'b', 'a'],
+                            'label': [1, 0, 1, 1]})
+    test = cudf.DataFrame({'category': ['b', 'b', 'a', 'b']})
+    encoder = TargetEncoder()
+    encoder.fit_transform(train.category, train.label)
+    test_encoded = encoder.transform(test.category)
+    answer = np.array([0.5, 0.5, 1., 0.5])
+    assert array_equal(test_encoded, answer)
+
+    encoder = TargetEncoder()
+    encoder.fit(train.category, train.label)
+    test_encoded = encoder.transform(test.category)
+    assert array_equal(test_encoded, answer)
+
+
+def test_targetencoder_multi_column():
+    """
+    Test jointly encoding multiple columns
+    """
+    train = cudf.DataFrame({'cat_1': ['a', 'b', 'b', 'a', 'a', 'b'],
+                            'cat_2': [1, 1, 2, 2, 1, 2],
+                            'label': [1, 0, 1, 1, 0, 1]})
+    test = cudf.DataFrame({'cat_1': ['b', 'b', 'a', 'b'],
+                           'cat_2': [1, 2, 1, 2]})
+    encoder = TargetEncoder()
+    train_encoded = encoder.fit_transform(train[['cat_1', 'cat_2']],
+                                          train.label)
+    test_encoded = encoder.transform(test[['cat_1', 'cat_2']])
+    train_answer = np.array([2./3, 2./3, 1., 2./3, 2./3, 1.])
+    test_answer = np.array([0., 1., 0.5, 1.])
+    assert array_equal(train_encoded, train_answer)
+    assert array_equal(test_encoded, test_answer)
+
+    encoder = TargetEncoder()
+    encoder.fit(train[['cat_1', 'cat_2']], train.label)
+    train_encoded = encoder.transform(train[['cat_1', 'cat_2']])
+    test_encoded = encoder.transform(test[['cat_1', 'cat_2']])
+    assert array_equal(train_encoded, train_answer)
+    assert array_equal(test_encoded, test_answer)
+
+
+def test_targetencoder_newly_encountered():
     """
     Note that there are newly-encountered values in test,
     namely, 'c' and 'd'.
@@ -67,4 +111,53 @@ def test_one_category():
     answer = np.array([1.5, 1.5, 1.5, 1.5])
     assert array_equal(test_encoded, answer)
 
+
+def test_targetencoder_pandas():
+    """
+    Note that there are newly-encountered values in test,
+    namely, 'c' and 'd'.
+    """
+    train = pandas.DataFrame({'category': ['a', 'b', 'b', 'a'],
+                              'label': [1, 0, 1, 1]})
+    test = pandas.DataFrame({'category': ['c', 'b', 'a', 'd']})
+    encoder = TargetEncoder()
+    encoder.fit_transform(train.category, train.label)
+    test_encoded = encoder.transform(test.category)
+    answer = np.array([0.75, 0.5, 1., 0.75])
     assert array_equal(test_encoded, answer)
+    print(type(test_encoded))
+    assert isinstance(test_encoded, np.ndarray)
+
+
+def test_targetencoder_numpy():
+    """
+    Note that there are newly-encountered values in x_test,
+    namely, 3 and 4.
+    """
+    x_train = np.array([1, 2, 2, 1])
+    y_train = np.array([1, 0, 1, 1])
+    x_test = np.array([1, 2, 3, 4])
+    encoder = TargetEncoder()
+    encoder.fit_transform(x_train, y_train)
+    test_encoded = encoder.transform(x_test)
+    answer = np.array([1., 0.5, 0.75, 0.75])
+    assert array_equal(test_encoded, answer)
+    print(type(test_encoded))
+    assert isinstance(test_encoded, np.ndarray)
+
+
+def test_targetencoder_cumpy():
+    """
+    Note that there are newly-encountered values in x_test,
+    namely, 3 and 4.
+    """
+    x_train = cp.array([1, 2, 2, 1])
+    y_train = cp.array([1, 0, 1, 1])
+    x_test = cp.array([1, 2, 3, 4])
+    encoder = TargetEncoder()
+    encoder.fit_transform(x_train, y_train)
+    test_encoded = encoder.transform(x_test)
+    answer = np.array([1., 0.5, 0.75, 0.75])
+    assert array_equal(test_encoded, answer)
+    print(type(test_encoded))
+    assert isinstance(test_encoded, cp.ndarray)
