ca509e5527b200367d8e68118d483cb7c9f566f6
Jiwei Liu <jiweil@dgx11.aselab.nvidia.com>
2020-08-02 07:52:38 -0700

    add large random test

--- python/cuml/test/test_target_encoder.py
+++ python/cuml/test/test_target_encoder.py
@@ -18,6 +18,7 @@ import pandas
 import numpy as np
 import cupy as cp
 from cuml.test.utils import array_equal
+import pytest
 
 
 def test_targetencoder_fit_transform():
@@ -48,6 +49,28 @@ def test_targetencoder_transform():
     encoder = TargetEncoder()
     encoder.fit(train.category, train.label)
     test_encoded = encoder.transform(test.category)
+    assert array_equal(test_encoded, answer)
+
+
+@pytest.mark.parametrize('n_samples', [5000, 500000])
+@pytest.mark.parametrize('dtype', [np.int32, np.int64, np.float32, np.float64])
+def test_targetencoder_random(n_samples, dtype):
+
+    x = cp.random.randint(0, 1000, n_samples).astype(dtype)
+    y = cp.random.randint(0, 2, n_samples).astype(dtype)
+    xt = cp.random.randint(0, 1000, n_samples).astype(dtype)
+
+    encoder = TargetEncoder()
+    encoder.fit_transform(x, y)
+    test_encoded = encoder.transform(xt)
+
+    df_train = cudf.DataFrame({'x': x, 'y': y})
+    dg = df_train.groupby('x', as_index=False).agg({'y': 'mean'})
+    df_test = cudf.DataFrame({'x': xt})
+    df_test['row_id'] = cp.arange(len(df_test))
+    df_test = df_test.merge(dg, on='x', how='left')
+    df_test = df_test.sort_values('row_id')
+    answer = df_test['y'].fillna(cp.mean(y).item()).values
     assert array_equal(test_encoded, answer)
 
 
@@ -146,7 +169,7 @@ def test_targetencoder_numpy():
     assert isinstance(test_encoded, np.ndarray)
 
 
-def test_targetencoder_cumpy():
+def test_targetencoder_cupy():
     """
     Note that there are newly-encountered values in x_test,
     namely, 3 and 4.
