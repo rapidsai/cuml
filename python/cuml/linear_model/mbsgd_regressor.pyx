f2384ab357af609fe170b60871cfefc6c7de4430
Dante Gama Dessavre <dante.gamadessavre@gmail.com>
2020-08-04 13:54:12 -0500

    FEA Change all linear_models to use decorator

--- python/cuml/linear_model/mbsgd_regressor.pyx
+++ python/cuml/linear_model/mbsgd_regressor.pyx
@@ -19,6 +19,7 @@
 # cython: embedsignature = True
 # cython: language_level = 3
 from cuml.common.base import Base, RegressorMixin
+from cuml.common.doc_utils import generate_docstring
 from cuml.solvers import SGD
 
 
@@ -146,26 +147,11 @@ class MBSGDRegressor(Base, RegressorMixin):
         self.n_iter_no_change = n_iter_no_change
         self.cu_mbsgd_classifier = SGD(**self.get_params())
 
+    @generate_docstring()
     def fit(self, X, y, convert_dtype=True):
         """
         Fit the model with X and y.
 
-        Parameters
-        ----------
-        X : array-like (device or host) shape = (n_samples, n_features)
-            Dense matrix (floats or doubles) of shape (n_samples, n_features).
-            Acceptable formats: cuDF DataFrame, NumPy ndarray, Numba device
-            ndarray, cuda array interface compliant array like CuPy
-
-        y : array-like (device or host) shape = (n_samples, 1)
-            Dense vector (floats or doubles) of shape (n_samples, 1).
-            Acceptable formats: cuDF Series, NumPy ndarray, Numba device
-            ndarray, cuda array interface compliant array like CuPy
-
-        convert_dtype : bool, optional (default = True)
-            When set to True, the fit method will, when necessary, convert
-            y to be the same data type as X if they differ. This
-            will increase memory used for the method.
         """
         self._set_n_features_in(X)
         self.cu_mbsgd_classifier.fit(X, y, convert_dtype=convert_dtype)
@@ -174,26 +160,14 @@ class MBSGDRegressor(Base, RegressorMixin):
 
         return self
 
+    @generate_docstring(return_values={'name': 'preds',
+                                       'type': 'dense',
+                                       'description': 'Predicted values',
+                                       'shape': '(n_samples, 1)'})
     def predict(self, X, convert_dtype=False):
         """
         Predicts the y for X.
 
-        Parameters
-        ----------
-        X : array-like (device or host) shape = (n_samples, n_features)
-            Dense matrix (floats or doubles) of shape (n_samples, n_features).
-            Acceptable formats: cuDF DataFrame, NumPy ndarray, Numba device
-            ndarray, cuda array interface compliant array like CuPy
-
-        convert_dtype : bool, optional (default = False)
-            When set to True, the predict method will, when necessary, convert
-            the input to the data type which was used to train the model. This
-            will increase memory used for the method.
-
-        Returns
-        ----------
-        y: Type specified by `output_type`
-           Dense vector (floats or doubles) of shape (n_samples, 1)
         """
 
         preds = self.cu_mbsgd_classifier.predict(X,
