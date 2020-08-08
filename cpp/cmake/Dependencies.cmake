2291ca1a260f1436a8e0e6a232973e4362aa4c64
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-08-07 19:31:40 -0700

    removed the commented out nvgraph dependencies

--- cpp/cmake/Dependencies.cmake
+++ cpp/cmake/Dependencies.cmake
@@ -129,27 +129,6 @@ ExternalProject_Add(spdlog
   INSTALL_COMMAND   "")
 
 ##############################################################################
-# - nvgraph ------------------------------------------------------------------
-
-# set(NVGRAPH_DIR ${CMAKE_CURRENT_BINARY_DIR}/nvgraph CACHE STRING
-#   "Path to nvgraph install directory")
-# ExternalProject_Add(nvgraph
-#     GIT_REPOSITORY    https://github.com/rapidsai/nvgraph.git
-#     GIT_TAG           feb0c5721e6e0e27ee51b2f56fd39c7d1e805a64
-#     PREFIX            ${NVGRAPH_DIR}
-#     CMAKE_ARGS        -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-#                       -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
-#                       ${NVGRAPH_DIR}/src/nvgraph/cpp
-#     UPDATE_COMMAND    "")
-
-# add_library(nvgraphlib STATIC IMPORTED)
-
-# add_dependencies(nvgraphlib nvgraph)
-
-# set_property(TARGET nvgraphlib
-#   PROPERTY IMPORTED_LOCATION ${NVGRAPH_DIR}/lib64/libnvgraph.so)
-
-##############################################################################
 # - faiss --------------------------------------------------------------------
 
 if(BUILD_STATIC_FAISS)
@@ -161,7 +140,7 @@ if(BUILD_STATIC_FAISS)
     CONFIGURE_COMMAND LIBS=-pthread
                       CPPFLAGS=-w
                       LDFLAGS=-L${CMAKE_INSTALL_PREFIX}/lib
-                               ${CMAKE_CURRENT_BINARY_DIR}/faiss/src/faiss/configure
+                              ${CMAKE_CURRENT_BINARY_DIR}/faiss/src/faiss/configure
 	                      --prefix=${CMAKE_CURRENT_BINARY_DIR}/faiss
 	                      --with-blas=${BLAS_LIBRARIES}
 	                      --with-cuda=${CUDA_TOOLKIT_ROOT_DIR}
