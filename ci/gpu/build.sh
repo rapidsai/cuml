bd0cb8d6045f52920d1b97ce0befe82ca8f67f77
Mike Wendt <mike.wendt@me.com>
2020-08-12 03:04:09 -0400

    FIX Update libcumlprims to version w/ CUDA 11

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -52,7 +52,7 @@ conda install -c conda-forge -c rapidsai -c rapidsai-nightly -c nvidia \
       "cudatoolkit=${CUDA_REL}" \
       "cudf=${MINOR_VERSION}" \
       "rmm=${MINOR_VERSION}" \
-      "libcumlprims=0.15.0a200720" \
+      "libcumlprims=0.15.0a200812" \
       "dask-cudf=${MINOR_VERSION}" \
       "dask-cuda=${MINOR_VERSION}" \
       "ucx-py=${MINOR_VERSION}" \
