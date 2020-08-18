daaf7944997457551db30fab8d44b58285191132
vibhujawa <vibhujawa@gmail.com>
2020-08-17 21:13:59 -0700

    added idf_ excecption to conftest.py

--- python/cuml/test/conftest.py
+++ python/cuml/test/conftest.py
@@ -105,7 +105,13 @@ def fail_on_bad_cuml_array_name(monkeypatch):
 
         supported_type = get_supported_input_type(value)
 
-        if (supported_type == CumlArray):
+        if name == 'idf_':
+            # We skip this test because idf_' fot tfidf setter returns
+            # a sparse diagonal matrix and getter gets a cupy array
+            # see discussion at:
+            # https://github.com/rapidsai/cuml/pull/2698/files#r471865982
+            pass
+        elif (supported_type == CumlArray):
             assert name.startswith("_"), "Invalid CumlArray Use! CumlArray \
                 attributes need a leading underscore. Attribute: '{}' In: {}" \
                     .format(name, self.__repr__())
