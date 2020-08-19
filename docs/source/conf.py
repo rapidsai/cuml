1849e971c0c6553cd1bc83844351bdebd7c23371
Dante Gama Dessavre <dante.gamadessavre@gmail.com>
2020-08-19 12:37:22 -0500

    FIX Remove stray variable in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,8 +20,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../python'))
 
-os.environ["BUILD_CUML_DOCSTRINGS"] = "True"
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
