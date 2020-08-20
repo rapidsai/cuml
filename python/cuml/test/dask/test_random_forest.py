b124b66bcf8686b2707a77b53e2448217029177e
Nanthini Balasubramanian <nathanb@nvidia.com>
2020-08-20 04:35:09 +0000

    Update conditions

--- python/cuml/test/dask/test_random_forest.py
+++ python/cuml/test/dask/test_random_forest.py
@@ -334,8 +334,18 @@ def test_single_input(client, model_type, ignore_empty_partitions):
         cu_rf_mg = cuRFR_mg(n_bins=1,
                             ignore_empty_partitions=ignore_empty_partitions)
 
-    if not ignore_empty_partitions and \
-       len(client.scheduler_info()['workers'].keys()) > 1:
+    if ignore_empty_partitions:
+        cu_rf_mg.fit(X, y)
+        cuml_mod_predict = cu_rf_mg.predict(X)
+        cuml_mod_predict = cp.asnumpy(cp.array(cuml_mod_predict.compute()))
+
+        y = cp.asnumpy(cp.array(y.compute()))
+
+        acc_score = accuracy_score(cuml_mod_predict, y)
+
+        assert acc_score == 1.0
+
+    elif len(client.scheduler_info()['workers'].keys()) > 1:
         with pytest.raises(ValueError):
             cu_rf_mg.fit(X, y)
     else:
