272f79c1bc74dd4175a56bbab2747426e8f7bc7b
divyegala <divyegala@gmail.com>
2020-09-10 13:47:15 -0700

    typo in testing channel

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -42,7 +42,7 @@ nvidia-smi
 
 logger "Activate conda env..."
 source activate gdf
-conda install -c conda-forge -c rapidsai -c rapidsai-nightly -c nvidia -c rapidsai/label/testing \
+conda install -c conda-forge -c rapidsai -c rapidsai-nightly -c nvidia -c rapidsai-nightly/label/testing \
       "cudatoolkit=${CUDA_REL}" \
       "cudf=${MINOR_VERSION}" \
       "rmm=${MINOR_VERSION}" \
