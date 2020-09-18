d13bbb614c662ed57307f0e0e8f11651654de958
Corey J. Nolet <cjnolet@gmail.com>
2020-09-18 17:45:33 -0400

    This darn style checker is going to kill me.....

--- cpp/test/prims/sparse/distances_l2exp.cu
+++ cpp/test/prims/sparse/distances_l2exp.cu
@@ -20,8 +20,8 @@
 
 #include <gtest/gtest.h>
 #include <raft/sparse/cusparse_wrappers.h>
-#include <sparse/distances.cuh>
 #include <test_utils.h>
+#include <sparse/distances.cuh>
 
 namespace MLCommon {
 namespace Sparse {
