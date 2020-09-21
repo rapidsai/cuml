44bbcd7b059fc1e751bc19918889f4523e286587
Corey J. Nolet <cjnolet@users.noreply.github.com>
2020-09-21 10:08:20 -0400

    [REVIEW] Enabling MG Gtests w/ RAFT MPI Comms (#2775)
    * Building MG gtests w/ new raft mpi comms
    
    * Updating changelog
    
    * Removing unused test for unused SPMG utility
    
    * Fixing clang format for mg tests
    
    * Remoing unecessary file
    
    * Fixing doc utils to use both x and y descriptions, rather than just x
    
    * Removing constraints in PCA transform() & inverse_transform() that number of rows be >1.
    
    * Updating changelog
    
    * ASSERT n_rows > 0
    
    * Fixing clang format
    
    * Using spelled out number one instead of 1
    
    * Removing cout
    
    * Using axis=1 in df.drop() for train_test_split
    
    * Review feedback
    
    * Addressing review
    
    * Fixing knn mg test style
    
    Co-authored-by: John Zedlewski <904524+JohnZed@users.noreply.github.com>

--- cpp/cmake/Dependencies.cmake
+++ cpp/cmake/Dependencies.cmake
@@ -87,6 +87,19 @@ set(RMM_INSTALL_DIR ENV{RMM_ROOT})
 find_package(RMM
              REQUIRED)
 
+##############################################################################
+# - NCCL ---------------------------------------------------------------------
+
+if(BUILD_CUML_MPI_COMMS OR BUILD_CUML_STD_COMMS)
+  find_package(NCCL REQUIRED)
+endif(BUILD_CUML_MPI_COMMS OR BUILD_CUML_STD_COMMS)
+
+##############################################################################
+# - MPI ---------------------------------------------------------------------
+
+if(BUILD_CUML_MPI_COMMS)
+  find_package(MPI REQUIRED)
+endif(BUILD_CUML_MPI_COMMS)
 
 ##############################################################################
 # - cub - (header only) ------------------------------------------------------
