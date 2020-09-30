1ca89de9545d63c70392db38deee8c9a01e89e0e
divyegala <divyegala@gmail.com>
2020-09-30 12:44:26 -0500

    reverting testing label

--- ci/cpu/libcuml/build_libcuml.sh
+++ ci/cpu/libcuml/build_libcuml.sh
@@ -6,10 +6,10 @@ if [[ -z "$PROJECT_FLASH" || "$PROJECT_FLASH" == "0" ]]; then
   if [ "$BUILD_LIBCUML" == '1' -o "$BUILD_CUML" == '1' ]; then
     echo "Building libcuml"
     CUDA_REL=${CUDA_VERSION%.*}
-    conda build -c rapidsai-nightly/label/testing conda/recipes/libcuml --python=${PYTHON}
+    conda build conda/recipes/libcuml --python=${PYTHON}
   fi
 else
   if [ "$BUILD_LIBCUML" == '1' ]; then
-    conda build -c rapidsai-nightly/label/testing conda/recipes/libcuml --dirty --no-remove-work-dir --python=${PYTHON}
+    conda build conda/recipes/libcuml --dirty --no-remove-work-dir --python=${PYTHON}
   fi
 fi
