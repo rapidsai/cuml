eb7a239ef1c64e114324212314f4046ecb08ef70
William Hicks <whicks@nvidia.com>
2020-10-21 15:25:49 -0400

    Update raft commit off development branch

--- cpp/cmake/Dependencies.cmake
+++ cpp/cmake/Dependencies.cmake
@@ -38,8 +38,8 @@ else(DEFINED ENV{RAFT_PATH})
   set(RAFT_DIR ${CMAKE_CURRENT_BINARY_DIR}/raft CACHE STRING "Path to RAFT repo")
 
   ExternalProject_Add(raft
-    GIT_REPOSITORY    https://github.com/wphicks/raft.git
-    GIT_TAG           fea-move-distancetype
+    GIT_REPOSITORY    https://github.com/rapidsai/raft.git
+    GIT_TAG           9ed6adf971e693d74ad5c8f8cdf8c081021179b1
     PREFIX            ${RAFT_DIR}
     CONFIGURE_COMMAND ""
     BUILD_COMMAND     ""
