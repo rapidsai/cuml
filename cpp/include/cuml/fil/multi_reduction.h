95fdc78582ef3ad68e55ab6eb2f42a644362588e
Levs Dolgovs <ldolgovs@nvidia.com>
2020-11-13 19:19:45 -0800

    long comment line

--- cpp/include/cuml/fil/multi_reduction.h
+++ cpp/include/cuml/fil/multi_reduction.h
@@ -5,8 +5,8 @@ template <int R, typename T>
 /**
  per_thread holds one value per thread in shared memory
  n_groups is the number of indendent reductions
- per_thread values are ordered such that the stride is 1 for values belonging to the same group
- and n_groups for values that are to be added together
+ per_thread values are ordered such that the stride is 1 for values belonging
+ to the same group and n_groups for values that are to be added together
  n_values is the size of each individual reduction,
  that is the number of values to be reduced to a single value
 */
