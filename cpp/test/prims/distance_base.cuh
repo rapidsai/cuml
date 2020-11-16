33069cfd727d1659ac20b59c11d109188c41d227
William Hicks <wphicks@users.noreply.github.com>
2020-11-16 13:02:10 -0500

    [REVIEW] Move DistanceType enum to RAFT (#3141)
    * Update all DistanceType references
    
    * Style fix
    
    * Update changelog

--- cpp/test/prims/distance_base.cuh
+++ cpp/test/prims/distance_base.cuh
@@ -27,7 +27,7 @@ namespace Distance {
 template <typename DataType>
 __global__ void naiveDistanceKernel(DataType *dist, const DataType *x,
                                     const DataType *y, int m, int n, int k,
-                                    ML::Distance::DistanceType type,
+                                    raft::distance::DistanceType type,
                                     bool isRowMajor) {
   int midx = threadIdx.x + blockIdx.x * blockDim.x;
   int nidx = threadIdx.y + blockIdx.y * blockDim.y;
@@ -39,8 +39,8 @@ __global__ void naiveDistanceKernel(DataType *dist, const DataType *x,
     auto diff = x[xidx] - y[yidx];
     acc += diff * diff;
   }
-  if (type == ML::Distance::DistanceType::EucExpandedL2Sqrt ||
-      type == ML::Distance::DistanceType::EucUnexpandedL2Sqrt)
+  if (type == raft::distance::DistanceType::EucExpandedL2Sqrt ||
+      type == raft::distance::DistanceType::EucUnexpandedL2Sqrt)
     acc = raft::mySqrt(acc);
   int outidx = isRowMajor ? midx * n + nidx : midx + m * nidx;
   dist[outidx] = acc;
@@ -103,24 +103,24 @@ __global__ void naiveCosineDistanceKernel(DataType *dist, const DataType *x,
 
 template <typename DataType>
 void naiveDistance(DataType *dist, const DataType *x, const DataType *y, int m,
-                   int n, int k, ML::Distance::DistanceType type,
+                   int n, int k, raft::distance::DistanceType type,
                    bool isRowMajor) {
   static const dim3 TPB(16, 32, 1);
   dim3 nblks(raft::ceildiv(m, (int)TPB.x), raft::ceildiv(n, (int)TPB.y), 1);
 
   switch (type) {
-    case ML::Distance::DistanceType::EucUnexpandedL1:
+    case raft::distance::DistanceType::EucUnexpandedL1:
       naiveL1DistanceKernel<DataType>
         <<<nblks, TPB>>>(dist, x, y, m, n, k, isRowMajor);
       break;
-    case ML::Distance::DistanceType::EucUnexpandedL2Sqrt:
-    case ML::Distance::DistanceType::EucUnexpandedL2:
-    case ML::Distance::DistanceType::EucExpandedL2Sqrt:
-    case ML::Distance::DistanceType::EucExpandedL2:
+    case raft::distance::DistanceType::EucUnexpandedL2Sqrt:
+    case raft::distance::DistanceType::EucUnexpandedL2:
+    case raft::distance::DistanceType::EucExpandedL2Sqrt:
+    case raft::distance::DistanceType::EucExpandedL2:
       naiveDistanceKernel<DataType>
         <<<nblks, TPB>>>(dist, x, y, m, n, k, type, isRowMajor);
       break;
-    case ML::Distance::DistanceType::EucExpandedCosine:
+    case raft::distance::DistanceType::EucExpandedCosine:
       naiveCosineDistanceKernel<DataType>
         <<<nblks, TPB>>>(dist, x, y, m, n, k, isRowMajor);
       break;
@@ -144,7 +144,7 @@ template <typename DataType>
   return os;
 }
 
-template <ML::Distance::DistanceType distanceType, typename DataType,
+template <raft::distance::DistanceType distanceType, typename DataType,
           typename OutputTile_t>
 void distanceLauncher(DataType *x, DataType *y, DataType *dist, DataType *dist2,
                       int m, int n, int k, DistanceInputs<DataType> &params,
@@ -158,7 +158,7 @@ void distanceLauncher(DataType *x, DataType *y, DataType *dist, DataType *dist2,
     x, y, dist, m, n, k, workspace, worksize, fin_op, stream, isRowMajor);
 }
 
-template <ML::Distance::DistanceType distanceType, typename DataType>
+template <raft::distance::DistanceType distanceType, typename DataType>
 class DistanceTest : public ::testing::TestWithParam<DistanceInputs<DataType>> {
  public:
   void SetUp() override {
--- cpp/test/prims/trustworthiness.cu
+++ cpp/test/prims/trustworthiness.cu
@@ -428,7 +428,7 @@ class TrustworthinessScoreTest : public ::testing::Test {
     // euclidean test
     score =
       trustworthiness_score<float,
-                            ML::Distance::DistanceType::EucUnexpandedL2Sqrt>(
+                            raft::distance::DistanceType::EucUnexpandedL2Sqrt>(
         d_X, d_X_embedded, 50, 30, 8, 5, allocator, stream);
 
     allocator->deallocate(d_X, X.size() * sizeof(float), stream);
