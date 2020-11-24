d4679acbd705aa166416f3ce3c7d5140a3e62442
Vibhu Jawa <vjawa@nvidia.com>
2020-11-24 13:42:57 -0800

    Distributed TFIDF Transformer fix

--- python/cuml/dask/feature_extraction/text/tfidf_transformer.py
+++ python/cuml/dask/feature_extraction/text/tfidf_transformer.py
@@ -35,6 +35,7 @@ class TfidfTransformer(BaseEstimator, DelayedTransformMixin):
 
     Examples
     --------
+    .. code-block:: python
 
         import cupy as cp
         from sklearn.datasets import fetch_20newsgroups
@@ -80,7 +81,7 @@ class TfidfTransformer(BaseEstimator, DelayedTransformMixin):
     def __init__(self, client=None, verbose=False, **kwargs):
 
         """
-        Create new  distributed  TF-IDF  transformer instance
+        Create new  distributed TF-IDF transformer instance
 
         Parameters
         -----------
@@ -131,10 +132,11 @@ class TfidfTransformer(BaseEstimator, DelayedTransformMixin):
         ----------
 
         X : dask.Array with blocks containing dense or sparse cupy arrays
+
         Returns
         -------
 
-        cuml.dask.naive_bayes.TfidfTransformer current model instance
+        cuml.dask.feature_extraction.text.TfidfTransformer instance
         """
 
         # Only Dask.Array supported for now
@@ -181,18 +183,31 @@ class TfidfTransformer(BaseEstimator, DelayedTransformMixin):
         return arrs.shape[0]
 
     def fit_transform(self, X):
-        return self.fit(X).transform(X)
-
-    def transform(self, X):
         """
-        Use distributed TFIDFTransformer to transforme the
-        given set of data samples.
+        Fit distributed TFIDFTransformer and then transform the given set of data samples.
 
         Parameters
         ----------
 
         X : dask.Array with blocks containing dense or sparse cupy arrays
 
+        Returns
+        -------
+
+        dask.Array with blocks containing transformed sparse cupy arrays
+
+        """
+        return self.fit(X).transform(X)
+
+    def transform(self, X):
+        """
+        Use distributed TFIDFTransformer to transform the
+        given set of data samples.
+
+        Parameters
+        ----------
+
+        X : dask.Array with blocks containing dense or sparse cupy arrays
 
         Returns
         -------
