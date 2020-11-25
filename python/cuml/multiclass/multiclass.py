a197b14225c8797e58dc799fb056ee369ea36091
Tamas Bela Feher <tfeher@nvidia.com>
2020-11-25 21:41:33 +0100

    Remove n_jobs arg, improve docstring

--- python/cuml/multiclass/multiclass.py
+++ python/cuml/multiclass/multiclass.py
@@ -18,6 +18,7 @@ import sklearn.multiclass
 
 from cuml.common.array import CumlArray
 from cuml.common.base import Base, ClassifierMixin
+from cuml.common.doc_utils import generate_docstring
 from cuml.common import input_to_host_array
 
 
@@ -31,18 +32,44 @@ class MulticlassClassifier(Base, ClassifierMixin):
     Berofe passing the data to scikit-learn, it is converted to host (numpy)
     array. Under the hood the data is partitioned for binary classification,
     and it is transformed back to the device by the cuML estimator. These
-    copies back and forth the device and the host have some overhead, but it is
-    often negligible compared to the time to train the model. For more details
-    see issue https://github.com/rapidsai/cuml/issues/2876.
+    copies back and forth the device and the host have some overhead. For more
+    details see issue https://github.com/rapidsai/cuml/issues/2876.
 
+    Parameters
+    ----------
+    estimator : cuML estimator
+    handle : cuml.Handle
+        Specifies the cuml.handle that holds internal CUDA state for
+        computations in this model. Most importantly, this specifies the CUDA
+        stream that will be used for the model's computations, so users can
+        run different models concurrently in different streams by creating
+        handles in several streams.
+        If it is None, a new one is created.
+    verbose : int or boolean, default=False
+         Sets logging level. It must be one of `cuml.common.logger.level_*`.
+         See :ref:`verbosity-levels` for more info.
+    output_type : {'input', 'cudf', 'cupy', 'numpy', 'numba'}, default=None
+        Variable to control output type of the results and attributes of
+        the estimator. If None, it'll inherit the output type set at the
+        module level, `cuml.global_output_type`.
+        See :ref:`output-data-type-configuration` for more info
+    strategy: string {'ovr', 'ovo'}, default='ovr'
+        Multiclass classification strategy: 'ovr': one vs. rest or 'ovo': one
+        vs. one
+
+    Attributes
+    ----------
+    classes_ : float, shape (n_classes_)
+        Array of class labels.
+    n_classes_ : int
+        Number of classes.
     """
     def __init__(self, estimator, handle=None, verbose=False,
-                 output_type=None, n_jobs=None, strategy='ovr'):
+                 output_type=None, strategy='ovr'):
         super().__init__(handle=handle, verbose=verbose,
                          output_type=output_type)
         self.strategy = strategy
         self.estimator = estimator
-        self.n_jobs = n_jobs
 
     @property
     @cuml.internals.api_base_return_array_skipall
@@ -54,14 +81,16 @@ class MulticlassClassifier(Base, ClassifierMixin):
     def n_classes_(self):
         return self.multiclass_estimator.n_classes_
 
+    @generate_docstring(y='dense_anydtype')
     def fit(self, X, y) -> 'MulticlassClassifier':
+        """ Fit a multiclass classifier. """
         if self.strategy == 'ovr':
             self.multiclass_estimator = sklearn.multiclass.\
-                OneVsRestClassifier(self.estimator, n_jobs=self.n_jobs)
+                OneVsRestClassifier(self.estimator, n_jobs=None)
         elif self.strategy == 'ovo':
             self.multiclass_estimator = \
                 sklearn.multiclass.OneVsOneClassifier(
-                    self.estimator, n_jobs=self.n_jobs)
+                    self.estimator, n_jobs=None)
         else:
             raise ValueError('Invalid multiclass strategy ' +
                              str(self.strategy) + ', must be one of '
@@ -71,20 +100,31 @@ class MulticlassClassifier(Base, ClassifierMixin):
         with cuml.internals.exit_internal_api():
             return self.multiclass_estimator.fit(X, y)
 
+    @generate_docstring(return_values={'name': 'preds',
+                                       'type': 'dense',
+                                       'description': 'Predicted values',
+                                       'shape': '(n_samples, 1)'})
     def predict(self, X) -> CumlArray:
+        """ Predict using multi class classifier. """
         X, _, _, _, _ = input_to_host_array(X)
         with cuml.internals.exit_internal_api():
             preds = self.multiclass_estimator.predict(X)
         return preds
 
+    @generate_docstring(return_values={'name': 'results',
+                                       'type': 'dense',
+                                       'description': 'Decision function \
+                                       values',
+                                       'shape': '(n_samples, 1)'})
     def decision_function(self, X) -> CumlArray:
+        """ Calculate the decision function. """
         X, _, _, _, _ = input_to_host_array(X)
         with cuml.internals.exit_internal_api():
             df = self.multiclass_estimator.decision_function(X)
         return df
 
     def get_param_names(self):
-        return super().get_param_names() + ['estimator', 'strategy', 'n_jobs']
+        return super().get_param_names() + ['estimator', 'strategy']
 
 
 class OneVsRestClassifier(MulticlassClassifier):
@@ -95,19 +135,36 @@ class OneVsRestClassifier(MulticlassClassifier):
     Berofe passing the data to scikit-learn, it is converted to host (numpy)
     array. Under the hood the data is partitioned for binary classification,
     and it is transformed back to the device by the cuML estimator. These
-    copies back and forth the device and the host have some overhead, but it is
-    often negligible compared to the time to train the model. For more details
-    see issue https://github.com/rapidsai/cuml/issues/2876.
+    copies back and forth the device and the host have some overhead. For more
+    details see issue https://github.com/rapidsai/cuml/issues/2876.
 
     For documentation see `scikit-learn's OneVsOneClassifier
     <https://scikit-learn.org/stable/modules/generated/sklearn.multiclass.OneVsRestClassifier.html>`_. # noqa: E501
 
+    Parameters
+    ----------
+    estimator : cuML estimator
+    handle : cuml.Handle
+        Specifies the cuml.handle that holds internal CUDA state for
+        computations in this model. Most importantly, this specifies the CUDA
+        stream that will be used for the model's computations, so users can
+        run different models concurrently in different streams by creating
+        handles in several streams.
+        If it is None, a new one is created.
+    verbose : int or boolean, default=False
+         Sets logging level. It must be one of `cuml.common.logger.level_*`.
+         See :ref:`verbosity-levels` for more info.
+    output_type : {'input', 'cudf', 'cupy', 'numpy', 'numba'}, default=None
+        Variable to control output type of the results and attributes of
+        the estimator. If None, it'll inherit the output type set at the
+        module level, `cuml.global_output_type`.
+        See :ref:`output-data-type-configuration` for more info
     """
     def __init__(self, estimator, *args, handle=None, verbose=False,
-                 output_type=None, n_jobs=None):
+                 output_type=None):
         super(OneVsRestClassifier, self).__init__(
             estimator, *args, handle=handle, verbose=verbose,
-            output_type=output_type, n_jobs=n_jobs, strategy='ovr')
+            output_type=output_type, strategy='ovr')
 
 
 class OneVsOneClassifier(MulticlassClassifier):
@@ -118,16 +175,33 @@ class OneVsOneClassifier(MulticlassClassifier):
     Berofe passing the data to scikit-learn, it is converted to host (numpy)
     array. Under the hood the data is partitioned for binary classification,
     and it is transformed back to the device by the cuML estimator. These
-    copies back and forth the device and the host have some overhead, but it is
-    often negligible compared to the time to train the model. For more details
-    see issue https://github.com/rapidsai/cuml/issues/2876.
+    copies back and forth the device and the host have some overhead. For more
+    details see issue https://github.com/rapidsai/cuml/issues/2876.
 
     For documentation see `scikit-learn's OneVsOneClassifier
     <https://scikit-learn.org/stable/modules/generated/sklearn.multiclass.OneVsOneClassifier.html>`_. # noqa: E501
 
+    Parameters
+    ----------
+    estimator : cuML estimator
+    handle : cuml.Handle
+        Specifies the cuml.handle that holds internal CUDA state for
+        computations in this model. Most importantly, this specifies the CUDA
+        stream that will be used for the model's computations, so users can
+        run different models concurrently in different streams by creating
+        handles in several streams.
+        If it is None, a new one is created.
+    verbose : int or boolean, default=False
+         Sets logging level. It must be one of `cuml.common.logger.level_*`.
+         See :ref:`verbosity-levels` for more info.
+    output_type : {'input', 'cudf', 'cupy', 'numpy', 'numba'}, default=None
+        Variable to control output type of the results and attributes of
+        the estimator. If None, it'll inherit the output type set at the
+        module level, `cuml.global_output_type`.
+        See :ref:`output-data-type-configuration` for more info
     """
     def __init__(self, estimator, *args, handle=None, verbose=False,
-                 output_type=None, n_jobs=None):
+                 output_type=None):
         super(OneVsOneClassifier, self).__init__(
             estimator, *args, handle=handle, verbose=verbose,
-            output_type=output_type, n_jobs=n_jobs, strategy='ovo')
+            output_type=output_type, strategy='ovo')
