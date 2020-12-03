70042b3a03a28969356d0c95d1f91c6f43ce4e7f
viclafargue <viclafargue@nvidia.com>
2020-12-03 11:19:45 +0000

    Remove parameters that are not yet effective from documentation

--- python/cuml/dask/neighbors/kneighbors_regressor.py
+++ python/cuml/dask/neighbors/kneighbors_regressor.py
@@ -44,13 +44,6 @@ class KNeighborsRegressor(NearestNeighbors):
     ----------
     n_neighbors : int (default=5)
         Default number of neighbors to query
-    algorithm : string (default='brute')
-        The query algorithm to use. Currently, only 'brute' is supported.
-    metric : string (default='euclidean').
-        Distance metric to use.
-    weights : string (default='uniform')
-        Sample weights to use. Currently, only the 'uniform' strategy is
-        supported.
     batch_size: int (optional, default 1024)
         Maximum number of queries processed at once. This parameter can
         greatly affect the throughput of the algorithm.
@@ -64,11 +57,6 @@ class KNeighborsRegressor(NearestNeighbors):
     verbose : int or boolean, default=False
         Sets logging level. It must be one of `cuml.common.logger.level_*`.
         See :ref:`verbosity-levels` for more info.
-    output_type : {'input', 'cudf', 'cupy', 'numpy', 'numba'}, default=None
-        Variable to control output type of the results and attributes of
-        the estimator. If None, it'll inherit the output type set at the
-        module level, `cuml.global_output_type`.
-        See :ref:`output-data-type-configuration` for more info.
     """
     def __init__(self, client=None, streams_per_handle=0,
                  verbose=False, batch_size=1024, **kwargs):
