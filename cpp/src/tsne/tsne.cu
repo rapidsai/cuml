af4aa64c03fa103099f3a5017eb6e60b42220c0e
divyegala <divyegala@gmail.com>
2020-12-11 13:22:07 -0600

    adding class runner

--- cpp/src/tsne/tsne.cu
+++ cpp/src/tsne/tsne.cu
@@ -15,17 +15,7 @@
  */
 
 #include <cuml/manifold/tsne.h>
-#include <raft/cudart_utils.h>
-#include <common/device_buffer.hpp>
-#include <cuml/common/logger.hpp>
-#include <cuml/manifold/common.hpp>
-#include <rmm/device_uvector.hpp>
-#include "distances.cuh"
-#include "exact_kernels.cuh"
-#include "utils.cuh"
-
-#include "barnes_hut.cuh"
-#include "exact_tsne.cuh"
+#include "tsne_runner.cuh"
 
 namespace ML {
 
@@ -40,109 +30,14 @@ void _fit(const raft::handle_t &handle, tsne_input &input, const int dim,
           const float pre_momentum, const float post_momentum,
           const long long random_state, int verbosity,
           const bool initialize_embeddings, bool barnes_hut) {
-  auto n = input.n;
-  auto p = input.d;
-  auto *Y = input.y;
+  TSNE_runner<tsne_input, knn_value_idx, knn_value_t> runner(
+    handle, input, dim, n_neighbors, theta, epssq, perplexity,
+    perplexity_max_iter, perplexity_tol, early_exaggeration, exaggeration_iter,
+    min_gain, pre_learning_rate, post_learning_rate, max_iter, min_grad_norm,
+    pre_momentum, post_momentum, random_state, verbosity, initialize_embeddings,
+    barnes_hut);
 
-  ML::Logger::get().setLevel(verbosity);
-  if (dim > 2 and barnes_hut) {
-    barnes_hut = false;
-    CUML_LOG_WARN(
-      "Barnes Hut only works for dim == 2. Switching to exact solution.");
-  }
-  if (n_neighbors > n) n_neighbors = n;
-  if (n_neighbors > 1023) {
-    CUML_LOG_WARN("FAISS only supports maximum n_neighbors = 1023.");
-    n_neighbors = 1023;
-  }
-  // Perplexity must be less than number of datapoints
-  // "How to Use t-SNE Effectively" https://distill.pub/2016/misread-tsne/
-  if (perplexity > n) perplexity = n;
-
-  CUML_LOG_DEBUG("Data size = (%d, %d) with dim = %d perplexity = %f", n, p,
-                 dim, perplexity);
-  if (perplexity < 5 or perplexity > 50)
-    CUML_LOG_WARN(
-      "Perplexity should be within ranges (5, 50). Your results might be a"
-      " bit strange...");
-  if (n_neighbors < perplexity * 3.0f)
-    CUML_LOG_WARN(
-      "# of Nearest Neighbors should be at least 3 * perplexity. Your results"
-      " might be a bit strange...");
-
-  auto d_alloc = handle.get_device_allocator();
-  cudaStream_t stream = handle.get_stream();
-
-  START_TIMER;
-  //---------------------------------------------------
-  // Get distances
-  CUML_LOG_DEBUG("Getting distances.");
-
-  MLCommon::Sparse::COO<float> COO_Matrix(d_alloc, stream);
-
-  // artificial scope for safe destruction of indices/distances buffers
-  {
-    rmm::device_uvector<knn_value_idx> indices(0, stream);
-    rmm::device_uvector<knn_value_t> distances(0, stream);
-
-    if (input.alloc_knn_graph()) {
-      indices = rmm::device_uvector<knn_value_idx>(n * n_neighbors, stream);
-      distances = rmm::device_uvector<knn_value_t>(n * n_neighbors, stream);
-    }
-
-    TSNE::get_distances(handle, input, indices.data(), distances.data(),
-                        n_neighbors, stream);
-    //---------------------------------------------------
-    END_TIMER(DistancesTime);
-
-    START_TIMER;
-    //---------------------------------------------------
-    // Normalize distances
-    CUML_LOG_DEBUG("Now normalizing distances so exp(D) doesn't explode.");
-    TSNE::normalize_distances(n, distances.data(), n_neighbors, stream);
-    //---------------------------------------------------
-    END_TIMER(NormalizeTime);
-
-    START_TIMER;
-    //---------------------------------------------------
-    // Optimal perplexity
-    CUML_LOG_DEBUG("Searching for optimal perplexity via bisection search.");
-    MLCommon::device_buffer<float> P(d_alloc, stream, n * n_neighbors);
-    TSNE::perplexity_search(distances.data(), P.data(), perplexity,
-                            perplexity_max_iter, perplexity_tol, n, n_neighbors,
-                            handle);
-
-    //---------------------------------------------------
-    END_TIMER(PerplexityTime);
-
-    START_TIMER;
-    //---------------------------------------------------
-    // Convert data to COO layout
-    TSNE::symmetrize_perplexity(P.data(), indices.data(), n, n_neighbors,
-                                early_exaggeration, &COO_Matrix, stream,
-                                handle);
-    P.release(stream);
-  }
-
-  const int NNZ = COO_Matrix.nnz;
-  float *VAL = COO_Matrix.vals();
-  const int *COL = COO_Matrix.cols();
-  const int *ROW = COO_Matrix.rows();
-  //---------------------------------------------------
-  END_TIMER(SymmetrizeTime);
-
-  if (barnes_hut) {
-    TSNE::Barnes_Hut(VAL, COL, ROW, NNZ, handle, Y, n, theta, epssq,
-                     early_exaggeration, exaggeration_iter, min_gain,
-                     pre_learning_rate, post_learning_rate, max_iter,
-                     min_grad_norm, pre_momentum, post_momentum, random_state,
-                     initialize_embeddings);
-  } else {
-    TSNE::Exact_TSNE(VAL, COL, ROW, NNZ, handle, Y, n, dim, early_exaggeration,
-                     exaggeration_iter, min_gain, pre_learning_rate,
-                     post_learning_rate, max_iter, min_grad_norm, pre_momentum,
-                     post_momentum, random_state, initialize_embeddings);
-  }
+  runner.run();
 }
 
 void TSNE_fit(const raft::handle_t &handle, float *X, float *Y, int n, int p,
