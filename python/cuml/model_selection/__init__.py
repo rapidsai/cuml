77d303d8563706ba70382177390ad22e7163de37
Patrick Liu <patrick.liu01@gmail.com>
2020-12-30 23:56:05 +0000

    deprecation without code duplication

--- python/cuml/model_selection/__init__.py
+++ python/cuml/model_selection/__init__.py
@@ -1,4 +1,8 @@
+from cuml.model_selection._split import _stratify_split
+from cuml.model_selection._split import _approximate_mode
 from cuml.model_selection._split import train_test_split
 
 
-__all__ = ['train_test_split']
+__all__ = ['_stratify_split',
+           '_approximate_mode',
+           'train_test_split']
