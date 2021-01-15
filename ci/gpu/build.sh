4fcd77569af40889d842c8f696891cec784faaea
Michael Demoret <42954918+mdemoret-nv@users.noreply.github.com>
2021-01-14 18:48:33 -0700

    Committing temp workaround to pin ucx=1.8

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -62,6 +62,10 @@ gpuci_conda_retry install -c conda-forge -c rapidsai -c rapidsai-nightly -c nvid
 # gpuci_conda_retry remove --force rapids-build-env rapids-notebook-env
 # gpuci_conda_retry install -y "your-pkg=1.0.0"
 
+# TEMP: Workaround until this issue is resolved: https://github.com/rapidsai/ucx-py/issues/668
+gpuci_conda_retry remove --force rapids-build-env rapids-notebook-env
+gpuci_conda_retry install -y "ucx=1.8"
+
 gpuci_logger "Install contextvars if needed"
 py_ver=$(python -c "import sys; print('.'.join(map(str, sys.version_info[:2])))")
 if [ "$py_ver" == "3.6" ];then
