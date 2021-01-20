e0845f05977d60d63d7474e69d9706d22363358f
John Zedlewski <jzedlewski@nvidia.com>
2021-01-20 15:18:20 -0800

    Also xfail zlib errors when downloading newsgroups data

--- python/cuml/test/conftest.py
+++ python/cuml/test/conftest.py
@@ -18,7 +18,7 @@ import cupy as cp
 import pytest
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import CountVectorizer
-
+import zlib
 
 def pytest_configure(config):
     cp.cuda.set_allocator(None)
@@ -30,7 +30,7 @@ def nlp_20news():
         twenty_train = fetch_20newsgroups(subset='train',
                                           shuffle=True,
                                           random_state=42)
-    except IOError:
+    except (IOError, zlib.error):
         pytest.xfail(reason="Error fetching 20 newsgroup dataset")
 
     count_vect = CountVectorizer()
