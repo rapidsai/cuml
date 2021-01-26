ac9ed6bd30a673eb8c1a51f9935989ef5d12c6b0
William Hicks <whicks@nvidia.com>
2021-01-26 16:04:41 -0500

    Ensure make_classification respects output type

--- python/cuml/datasets/classification.py
+++ python/cuml/datasets/classification.py
@@ -41,7 +41,7 @@ def _generate_hypercube(samples, dimensions, rng):
     return out
 
 
-@cuml.internals.api_return_any()
+@cuml.internals.api_return_generic()
 def make_classification(n_samples=100, n_features=20, n_informative=2,
                         n_redundant=2, n_repeated=0, n_classes=2,
                         n_clusters_per_class=2, weights=None, flip_y=0.01,
