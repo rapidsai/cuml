d2e6e0a5b85294c36d0867fb25a5be5a94098ff5
Vinay Deshpande <vinayd@nvidia.com>
2021-02-02 20:53:24 +0530

    Enable feature sampling for the experimental backend of Random Forest (#3364)
    Feature sampling was not supported for experimental backend of Random Forest (RF). This PR implements feature sampling without needing any auxiliary memory storage. Thus the `colids` array is also removed.
    
    Authors:
      - Vinay Deshpande (@vinaydes)
    
    Approvers:
      - Thejaswi. N. S (@teju85)
      - Philip Hyunsu Cho (@hcho3)
      - John Zedlewski (@JohnZed)
    
    URL: https://github.com/rapidsai/cuml/pull/3364

--- cpp/src/decisiontree/batched-levelalgo/input.cuh
+++ cpp/src/decisiontree/batched-levelalgo/input.cuh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2020, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2021, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,8 +35,6 @@ struct Input {
   IdxT nSampledCols;
   /** indices of sampled rows */
   IdxT* rowids;
-  /** indices of sampled cols */
-  IdxT* colids;
   /** number of classes (useful only in classification) */
   IdxT nclasses;
   /** quantiles/histogram computed on the dataset (col-major) */
