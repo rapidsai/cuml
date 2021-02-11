4e58700c01e9b42655432d3fa844ae2f9d4eee9a
Corey J. Nolet <cjnolet@gmail.com>
2021-02-11 18:09:30 -0500

    Making progress

--- cpp/src/hierarchy/fix_connectivities.cuh
+++ cpp/src/hierarchy/fix_connectivities.cuh
@@ -19,6 +19,8 @@
 
 #include <raft/mr/device/buffer.hpp>
 #include <raft/linalg/norm.cuh>
+#include <raft/sparse/convert/csr.cuh>
+#include <sparse/coo.cuh>
 
 #include <raft/cudart_utils.h>
 #include <distance/fused_l2_nn.cuh>
@@ -67,6 +69,64 @@ struct FusedL2NNReduceOp {
   }
 };
 
+
+
+template<typename value_idx, typename value_t, typename reduction>
+__global__ void count_components_by_color(value_idx *out,
+                                    const value_idx *colors_indptr,
+                                    const value_idx *colors_nn,
+                                    value_idx n_colors) {
+
+  value_idx tid = blockDim.x * blockIdx.x + threadIdx.x;
+  value_idx row = blockIdx.x;
+
+  __shared__ extern value_idx smem[];
+
+  value_idx start_offset = colors_indptr[row];
+  value_idx stop_offset = colors_indptr[row+1];
+
+  for(value_idx i = tid; i < (stop_offset - start_offset); i+= blockDim.x) {
+    // todo: reduce by key
+    smem[colors_nn] = 1;
+  }
+
+  value_idx unique = 0;
+  for(value_idx i = tid; i < n_colors; i += blockDim.x) {
+    unique += smem[i];
+  }
+
+  // todo: shfl reduce to 0 & write out
+}
+
+template<typename value_idx, typename value_t, typename reduction>
+__global__ void min_components_by_color(value_idx *out_cols,
+                                          value_idx *out_vals,
+                                          value_idx *colors_indptr,
+                                          value_idx *colors_nn,
+                                          cub::KeyValuePair<value_idx, value_t> *idx_dists,
+                                          value_idx n_colors) {
+
+  value_idx tid = blockDim.x * blockIdx.x + threadIdx.x;
+  value_idx row = blockIdx.x;
+
+  __shared__ extern value_idx smem[];
+
+  value_idx start_offset = colors_indptr[row];
+  value_idx stop_offset = colors_indptr[row+1];
+
+  for(value_idx i = tid; i < (stop_offset - start_offset); i+= blockDim.x) {
+    // todo: reduce by key
+    atomicMin(smem + colors_nn[i], idx_dists);
+  }
+
+  __syncthreads();
+
+  for(value_idx i = tid; i < n_colors; i += blockDim.x) {
+    // write out
+  }
+}
+
+
 /**
  * Connects components by computing a 1-nn to neighboring
  * components of each data point (e.g. component(nn) != component(self))
@@ -83,8 +143,7 @@ struct FusedL2NNReduceOp {
  * @param stream
  */
 template<typename value_idx, typename value_t>
-void connect_components(value_idx *nn_indices,
-                        value_t *nn_dists,
+void connect_components(raft::sparse::COO<value_t, value_idx> &out,
                         const value_t *X,
                         const value_idx *colors,
                         value_idx n_rows,
@@ -101,6 +160,12 @@ void connect_components(value_idx *nn_indices,
   raft::mr::device::buffer<value_idx> nn_colors(d_alloc, stream, n_rows);
   raft::mr::device::buffer<cub::KeyValuePair<value_idx, value_t>> temp_inds_dists(d_alloc, stream, n_rows);
 
+  // sort nn_colors by key w/ original colors
+  thrust::device_ptr<value_idx> t_colors = thrust::device_pointer_cast(colors);
+  thrust::device_ptr<value_idx> t_nn_colors = thrust::device_pointer_cast(nn_colors.data());
+  thrust::device_ptr<value_t> t_data = thrust::device_pointer_cast(temp_inds_dists.data());
+  thrust::counting_iterator<value_idx> arg_sort(0);
+
   auto red_op = FusedL2NNReduceOp<value_idx, value_t>(colors, nn_colors);
   MLCommon::Distance::fusedL2NN<value_t, cub::KeyValuePair<value_idx, value_t>,
                                 value_idx>(temp_inds_dists.data(), X, X,
@@ -108,15 +173,50 @@ void connect_components(value_idx *nn_indices,
                                 n_rows, n_rows, n_cols, workspace.data(),
                                 red_op, true, true, stream);
 
-  // sort nn_colors by key w/ original colors
-  thrust::device_ptr<value_idx> t_colors = thrust::device_pointer_cast(colors);
-  thrust::device_ptr<value_idx> t_nn_colors = thrust::device_pointer_cast(nn_colors.data());
-  thrust::device_ptr<value_t> t_data = thrust::device_pointer_cast(temp_inds_dists.data());
+  // max_color + 1 = number of connected components
+  value_idx n_components =
+    *(thrust::max_element(thrust::cuda::par.on(stream), t_colors,
+                          t_colors + n_rows)) + 1;
 
-  auto first = thrust::make_zip_iterator(thrust::make_tuple(t_rows, t_cols));
+  raft::mr::device::buffer<value_idx> colors_indptr(d_alloc, stream, n_components);
 
-  thrust::sort_by_key(thrust::cuda::par.on(stream), t_data, t_data + nnz,
-                      first);
+  auto keys = thrust::make_zip_iterator(thrust::make_tuple(t_colors, t_nn_colors));
+  auto vals = thrust::make_zip_iterator(thrust::make_tuple(t_data, arg_sort));
+
+  // get all the colors in contiguous locations so we can map them to warps
+  thrust::sort_by_key(thrust::cuda::par.on(stream), keys, keys + n_rows, vals);
+
+  // create an indptr array for newly sorted colors
+  raft::sparse::convert::sorted_coo_to_csr(colors, n_rows, colors_indptr.data(), n_components);
+
+  // create degree array for closest components per row
+  raft::mr::device::buffer<value_idx> color_neigh_degrees(d_alloc, stream, n_components);
+
+  // map each component to a separate warp, perform warp reduce by key to find
+  // number of unique components in output.
+  count_components_by_color<<<n_components, 256, n_components * sizeof(value_idx), stream>>>(
+    color_neigh_degrees.data(), colors_indptr.data(), temp_inds_dists.data(), n_components);
+
+  // TODO: Sum color_neigh_degrees to find nnz
+
+  // map each component to a separate warp, perform warp reduce by key to
+  // find min for each component in output
+  min_components_by_color<<<n_components, 256, n_components * sizeof(value_idx), stream>>>(
+    out.);
+
+  // symmetrize
+
+  /**
+   * void symmetrize(const raft::handle_t &handle, const value_idx *rows,
+                const value_idx *cols, const value_t *vals, size_t m, size_t n,
+                size_t nnz, raft::sparse::COO<value_t, value_idx> &out)
+   */
+   raft::sparse::linalg::symmetrize(
+    
+    );
+
+
+
 }
 
 
