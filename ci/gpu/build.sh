835c19de9afd4f0f5b1dc58e5b3d788e10f88499
Michael Demoret <42954918+mdemoret-nv@users.noreply.github.com>
2021-02-24 12:45:47 -0700

    Fix Codecov.io Coverage Upload for Branch Builds (#3524)
    Testing the following undocumented option:
    ```yml
    codecov:
      allow_coverage_offsets: true
    ```
    
    Suggested by Codecov.io support [here](https://community.codecov.io/t/unable-to-determine-a-parent-commit-to-compare-against-in-base-branch-after-squash-and-merge/2480/15?u=mdemoret-nv) to fix the linked issue with missing base reports.
    
    Authors:
      - Michael Demoret (@mdemoret-nv)
    
    Approvers:
      - AJ Schmidt (@ajschmidt8)
      - John Zedlewski (@JohnZed)
    
    URL: https://github.com/rapidsai/cuml/pull/3524

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -243,16 +243,19 @@ fi
 
 if [ -n "${CODECOV_TOKEN}" ]; then
 
+    # NOTE: The code coverage upload needs to work for both PR builds and normal
+    # branch builds (aka `branch-0.XX`). Ensure the following settings to the
+    # codecov CLI will work with and without a PR
     gpuci_logger "Uploading Code Coverage to codecov.io"
 
     # Directory containing reports
     REPORT_DIR="${WORKSPACE}/python/cuml"
 
     # Base name to use in Codecov UI
-    CODECOV_NAME="${OS},py${PYTHON},cuda${CUDA}"
+    CODECOV_NAME=${JOB_BASE_NAME:-"${OS},py${PYTHON},cuda${CUDA}"}
 
     # Codecov args needed by both calls
-    EXTRA_CODECOV_ARGS=""
+    EXTRA_CODECOV_ARGS="-c"
 
     # Save the OS PYTHON and CUDA flags
     EXTRA_CODECOV_ARGS="${EXTRA_CODECOV_ARGS} -e OS,PYTHON,CUDA"
@@ -264,8 +267,14 @@ if [ -n "${CODECOV_TOKEN}" ]; then
         EXTRA_CODECOV_ARGS="${EXTRA_CODECOV_ARGS} -C ${REPORT_HASH}"
     fi
 
-    # Append the PR ID. This is needed when running the build inside docker
-    EXTRA_CODECOV_ARGS="${EXTRA_CODECOV_ARGS} -P ${PR_ID} -c"
+    # Append the PR ID. This is needed when running the build inside docker for
+    # PR builds
+    if [ -n "${PR_ID}" ]; then
+        EXTRA_CODECOV_ARGS="${EXTRA_CODECOV_ARGS} -P ${PR_ID}"
+    fi
+
+    # Set the slug since this does not work in jenkins.
+    export CODECOV_SLUG="${PR_AUTHOR:-"rapidsai"}/cuml"
 
     # Upload the two reports with separate flags. Delete the report on success
     # to prevent further CI steps from re-uploading
