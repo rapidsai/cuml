Fix style & reduce test precision

python/cuml/test/test_metrics.py
@@ -1092,21 +1092,20 @@ def test_pairwise_distances_output_types(input_type, output_type, use_global):
 @pytest.mark.parametrize("matrix_size", [(30, 30), (400, 2), (2, 400),
                                          (500, 40)])
 def test_sparse_pairwise_distances(metric: str, matrix_size):
+    # Test the sparse_pairwise_distance helper function.
     if metric in ["inner_product", "lp", "linf", "hellinger"]:
         pytest.skip('Skipping metrics not supported by sklearn')
-    if has_scipy():
-        import scipy
-        from scipy import sparse
-    else:
+    if not has_scipy():
         pytest.skip('Skipping sparse_pairwise_distances if Scipy is missing')
-    # Test the sparse_pairwise_distance helper function.
-
+    import scipy
+    from scipy import sparse
+    
     # For fp64, compare at 13 decimals, (2 places less than the ~15 max)
-    compare_precision = 10
+    compare_precision = 7
 
     # Compare to sklearn, single input
     X = scipy.sparse.random(matrix_size[0], matrix_size[1], format='csr',
-                            dtype=np.float64)
+                            dtype=np.float64, random_state=123)
     S = sparse_pairwise_distance(X, metric=metric)
     S2 = sklearn_pairwise_distances(X.todense(), metric=metric)
     cp.testing.assert_array_almost_equal(S, S2, decimal=compare_precision)
@@ -1123,21 +1122,22 @@ def test_sparse_pairwise_distances(metric: str, matrix_size):
     cp.testing.assert_array_almost_equal(S, S2, decimal=compare_precision)
 
     # Compare to sklearn, with Y dim != X dim
-    Y = scipy.sparse.random(3, matrix_size[1], format='csr')
+    Y = scipy.sparse.random(3, matrix_size[1], format='csr', random_state=123)
     S = pairwise_distances(X, Y, metric=metric)
     S2 = sklearn_pairwise_distances(X.todense(), Y.todense(), metric=metric)
     cp.testing.assert_array_almost_equal(S, S2, decimal=compare_precision)
 
     # Change precision of one parameter
     Y = scipy.sparse.random(matrix_size[0], matrix_size[1], format='csr',
-                            dtype=np.float32)
+                            dtype=np.float32, random_state=123)
     S = pairwise_distances(X, Y, metric=metric)
     S2 = sklearn_pairwise_distances(X.todense(), Y.todense(), metric=metric)
     cp.testing.assert_array_almost_equal(S, S2, decimal=compare_precision)
 
     # Change to cupyx with different precision
     Y = cupyx.scipy.sparse.random(matrix_size[0], matrix_size[1],
-                                  format='csr', dtype=np.float32)
+                                  format='csr', dtype=np.float32,
+                                  random_state=123)
     S = sparse_pairwise_distance(X, Y, metric=metric)
     S2 = sklearn_pairwise_distances(X.todense(), Y.todense().get(),
                                     metric=metric)
@@ -1148,16 +1148,16 @@ def test_sparse_pairwise_distances(metric: str, matrix_size):
 
     # Change precision of both parameters to float
     X = scipy.sparse.random(matrix_size[0], matrix_size[1], format='csr',
-                            dtype=np.float32)
+                            dtype=np.float32, random_state=123)
     Y = scipy.sparse.random(matrix_size[0], matrix_size[1], format='csr',
-                            dtype=np.float32)
+                            dtype=np.float32, random_state=123)
     S = pairwise_distances(X, Y, metric=metric)
     S2 = sklearn_pairwise_distances(X.todense(), Y.todense(), metric=metric)
     cp.testing.assert_array_almost_equal(S, S2, decimal=compare_precision)
 
     # Test sending an int type with convert_dtype=True
     Y = (scipy.sparse.random(Y.shape[0], Y.shape[1], dtype=cp.float32,
-                             format='csr') * 100)
+                             format='csr', random_state=123) * 100)
     Y = Y.astype(cp.int32)
     S = pairwise_distances(X, Y, metric=metric, convert_dtype=True)
     S2 = sklearn_pairwise_distances(X.todense(), Y.todense(), metric=metric)