c0ceb93eb8302c47ff8358f2ff59ecdfc101cdc1
Mahesh Doijade <mdoijade@nvidia.com>
2021-03-05 22:53:31 +0530

    Remove cutlass usage in row major input for euclidean exp/unexp distance matrix calculation. make use of contraction kernels for gemm NT calculation. add PairwiseDistances class which will serve as common class for other distance metrics like cosine as well

--- cpp/cmake/Dependencies.cmake
+++ cpp/cmake/Dependencies.cmake
@@ -39,7 +39,7 @@ else(DEFINED ENV{RAFT_PATH})
 
   ExternalProject_Add(raft
     GIT_REPOSITORY    https://github.com/rapidsai/raft.git
-    GIT_TAG           4a79adcb0c0e87964dcdc9b9122f242b5235b702
+    GIT_TAG           479b99bfae19747bc4159ee6267bdc6ec959af95
     PREFIX            ${RAFT_DIR}
     CONFIGURE_COMMAND ""
     BUILD_COMMAND     ""
