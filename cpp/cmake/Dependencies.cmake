8add1bd66c34c5e16fbcf7a818cd447c0cbd62f0
viclafargue <viclafargue@nvidia.com>
2021-03-12 16:29:24 +0000

    Update RAFT commit tag

--- cpp/cmake/Dependencies.cmake
+++ cpp/cmake/Dependencies.cmake
@@ -39,7 +39,7 @@ else(DEFINED ENV{RAFT_PATH})
 
   ExternalProject_Add(raft
     GIT_REPOSITORY    https://github.com/rapidsai/raft.git
-    GIT_TAG           4a79adcb0c0e87964dcdc9b9122f242b5235b702
+    GIT_TAG           68ef1e1c844354cf7e4a2da976f19fdcd71de122
     PREFIX            ${RAFT_DIR}
     CONFIGURE_COMMAND ""
     BUILD_COMMAND     ""
