4f4ae584914ff58e77faabf3f9c2a5a9736ef191
Louis Sugy <lsugy@nvidia.com>
2021-03-30 03:45:10 +0200

    Support for precomputed distance matrix in DBSCAN (#3585)
    Closes #3302
    
    ## Notes about performance
    
    If we don't count the cost of pre-computing the distance matrix (which is done by the user), the single-GPU code runs slightly faster when the distance matrix is pre-computed. (note: this is 2d, greater speedups expected for larger dimensions!)
    
    ![dbscan_precomputed_sg](https://user-images.githubusercontent.com/17441062/110022273-01f81280-7d2c-11eb-859f-929b691d76d5.png)
    ![dbscan_precomputed_timeline](https://user-images.githubusercontent.com/17441062/110022288-058b9980-7d2c-11eb-968c-eca35898f659.png)
    
    As I have stated in a comment in the code, it works with two kernels: one that uses a coalesced reduction to compute the vertex degrees from the distance matrix, and one that uses a 2D copy fused with an unary operation to get the boolean neighborhood matrix.
    
    _**Note:** the performance of this step could be better if `adj` was a row-major B*N matrix instead of column-major. We could fuse everything into one efficient kernel. It is something to keep in mind when we re-write `csr_adj_graph_batched`._
    
    ## Notes about MNMG
    
    Cf #3615
    
    Authors:
      - Louis Sugy (@Nyrio)
    
    Approvers:
      - Tamas Bela Feher (@tfeher)
      - Thejaswi. N. S (@teju85)
      - Dante Gama Dessavre (@dantegd)
    
    URL: https://github.com/rapidsai/cuml/pull/3585

--- python/cuml/test/dask/test_dbscan.py
+++ python/cuml/test/dask/test_dbscan.py
@@ -72,6 +72,45 @@ def test_dbscan(datatype, nrows, ncols,
 
 
 @pytest.mark.mg
+@pytest.mark.parametrize('max_mbytes_per_batch', [unit_param(1),
+                         quality_param(1e2), stress_param(None)])
+@pytest.mark.parametrize('datatype', [np.float32, np.float64])
+@pytest.mark.parametrize('nrows', [unit_param(500), quality_param(5000),
+                         stress_param(10000)])
+@pytest.mark.parametrize('out_dtype', ["int32", "int64"])
+def test_dbscan_precomputed(datatype, nrows, max_mbytes_per_batch, out_dtype,
+                            client):
+    from cuml.dask.cluster.dbscan import DBSCAN as cuDBSCAN
+
+    # 2-dimensional dataset for easy distance matrix computation
+    X, y = make_blobs(n_samples=nrows, cluster_std=0.01,
+                      n_features=2, random_state=0)
+
+    # Precompute distances
+    Xc = np.array([[complex(p[0], p[1]) for p in X]])
+    X_dist = np.abs(Xc - Xc.T, dtype=datatype)
+
+    eps = 1
+    cuml_dbscan = cuDBSCAN(eps=eps, min_samples=2, metric='precomputed',
+                           max_mbytes_per_batch=max_mbytes_per_batch,
+                           output_type='numpy')
+
+    cu_labels = cuml_dbscan.fit_predict(X_dist, out_dtype=out_dtype)
+
+    sk_dbscan = skDBSCAN(eps=eps, min_samples=2, metric='precomputed',
+                         algorithm="brute")
+    sk_labels = sk_dbscan.fit_predict(X_dist)
+
+    # Check the core points are equal
+    assert array_equal(cuml_dbscan.core_sample_indices_,
+                       sk_dbscan.core_sample_indices_)
+
+    # Check the labels are correct
+    assert_dbscan_equal(sk_labels, cu_labels, X,
+                        cuml_dbscan.core_sample_indices_, eps)
+
+
+@pytest.mark.mg
 @pytest.mark.parametrize("name", [
                                  'noisy_moons',
                                  'blobs',
