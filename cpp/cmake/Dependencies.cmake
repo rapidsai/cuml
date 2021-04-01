3bf9dc2ffac15ccc9cba681ed9bfdfe9347b22f2
Corey J. Nolet <cjnolet@gmail.com>
2021-04-01 16:21:17 -0400

    Updating raft hash

--- cpp/cmake/Dependencies.cmake
+++ cpp/cmake/Dependencies.cmake
@@ -40,7 +40,6 @@ else(DEFINED ENV{RAFT_PATH})
   ExternalProject_Add(raft
     GIT_REPOSITORY    https://github.com/rapidsai/raft.git
     GIT_TAG           91844e25f3cd2b3ba7ca3d550d67a2767ea0851a
-    GIT_TAG           d1fd927bc4ec67bfd765620b5fa93f17c54cfa70
     PREFIX            ${RAFT_DIR}
     CONFIGURE_COMMAND ""
     BUILD_COMMAND     ""
