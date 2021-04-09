ca3cd6e499eb520e9da3e0b180ea6102df91fd73
Mahesh Doijade <36705640+mdoijade@users.noreply.github.com>
2021-04-09 14:23:34 +0530

    Add support in column major distance metrics to use contractions_nt instead of cutlass (#3691)
    This change depends on RAFT pull request - https://github.com/rapidsai/raft/pull/188
    -- Adds column major input support in prims_benchmark's Distance bench
    -- Removes cutlass from all distance metrics in column major case.
    -- Removes cutlass from all the distance metrics test cases.
    
    Authors:
      - Mahesh Doijade (https://github.com/mdoijade)
    
    Approvers:
      - Corey J. Nolet (https://github.com/cjnolet)
      - Thejaswi. N. S (https://github.com/teju85)
    
    URL: https://github.com/rapidsai/cuml/pull/3691

--- cpp/src_prims/distance/l1.cuh
+++ cpp/src_prims/distance/l1.cuh
@@ -15,11 +15,6 @@
  */
 
 #pragma once
-#include <linalg/custom_accum.h>
-#include <linalg/cutlass_gemm.cuh>
-#include <type_traits>
-
-#include <raft/cuda_utils.cuh>
 #include "pairwise_distance_base.cuh"
 
 namespace MLCommon {
@@ -34,23 +29,33 @@ namespace Distance {
  * @tparam IdxT           index data-type
 
  * @tparam FinalLambda    final lambda called on final distance value
- *
+ * @tparam isRowMajor     true if input/output is row major,
+                          false for column major
  * @param[in]       x input matrix
  * @param[in]       y input matrix
  * @param[in]       m number of rows of A and C/D
  * @param[in]       n number of columns of B and C/D
  * @param[in]       k number of cols of A and rows of B
+ * @param[in]       lda leading dimension of A
+ * @param[in]       ldb leading dimension of B
+ * @param[in]       ldd leading dimension of C/D
  * @param[output]   pD output matrix
  * @param fin_op    the final gemm epilogue lambda
  */
 template <typename DataT, typename AccT, typename OutT, typename IdxT,
-          int VecLen, typename FinalLambda>
+          int VecLen, typename FinalLambda, bool isRowMajor>
 static void l1Impl(const DataT *x, const DataT *y, IdxT m, IdxT n, IdxT k,
-                   OutT *dOutput, FinalLambda fin_op, cudaStream_t stream) {
-  typedef typename raft::linalg::Policy4x4<DataT, VecLen>::Policy Policy;
-  dim3 grid(raft::ceildiv<int>(m, Policy::Mblk),
-            raft::ceildiv<int>(n, Policy::Nblk));
-  dim3 blk(Policy::Nthreads);
+                   IdxT lda, IdxT ldb, IdxT ldd, OutT *dOutput,
+                   FinalLambda fin_op, cudaStream_t stream) {
+  typedef typename raft::linalg::Policy4x4<DataT, VecLen>::Policy RowPolicy;
+  typedef typename raft::linalg::Policy4x4<DataT, VecLen>::ColPolicy ColPolicy;
+
+  typedef
+    typename std::conditional<isRowMajor, RowPolicy, ColPolicy>::type KPolicy;
+
+  dim3 grid(raft::ceildiv<int>(m, KPolicy::Mblk),
+            raft::ceildiv<int>(n, KPolicy::Nblk));
+  dim3 blk(KPolicy::Nthreads);
 
   // Accumulation operation lambda
   auto core_lambda = [] __device__(AccT & acc, DataT & x, DataT & y) {
@@ -60,33 +65,44 @@ static void l1Impl(const DataT *x, const DataT *y, IdxT m, IdxT n, IdxT k,
 
   // epilogue operation lambda for final value calculation
   auto epilog_lambda = [] __device__(
-                         AccT acc[Policy::AccRowsPerTh][Policy::AccColsPerTh],
+                         AccT acc[KPolicy::AccRowsPerTh][KPolicy::AccColsPerTh],
                          DataT * regxn, DataT * regyn) { return; };
 
-  pairwiseDistanceMatKernel<false, DataT, AccT, OutT, IdxT, Policy,
-                            decltype(core_lambda), decltype(epilog_lambda),
-                            FinalLambda>
-    <<<grid, blk, Policy::SmemSize, stream>>>(x, y, nullptr, nullptr, m, n, k,
-                                              dOutput, core_lambda,
-                                              epilog_lambda, fin_op);
+  if (isRowMajor) {
+    pairwiseDistanceMatKernel<false, DataT, AccT, OutT, IdxT, KPolicy,
+                              decltype(core_lambda), decltype(epilog_lambda),
+                              FinalLambda, isRowMajor>
+      <<<grid, blk, KPolicy::SmemSize, stream>>>(
+        x, y, nullptr, nullptr, m, n, k, lda, ldb, ldd, dOutput, core_lambda,
+        epilog_lambda, fin_op);
+  } else {
+    pairwiseDistanceMatKernel<false, DataT, AccT, OutT, IdxT, KPolicy,
+                              decltype(core_lambda), decltype(epilog_lambda),
+                              FinalLambda, isRowMajor>
+      <<<grid, blk, KPolicy::SmemSize, stream>>>(
+        x, y, nullptr, nullptr, m, n, k, lda, ldb, ldd, dOutput, core_lambda,
+        epilog_lambda, fin_op);
+  }
 
   CUDA_CHECK(cudaGetLastError());
 }
 
 template <typename DataT, typename AccT, typename OutT, typename IdxT,
-          typename FinalLambda>
-void l1(IdxT m, IdxT n, IdxT k, const DataT *x, const DataT *y, OutT *dOutput,
-        FinalLambda fin_op, cudaStream_t stream) {
-  size_t bytes = sizeof(DataT) * k;
-  if (16 % sizeof(DataT) == 0 && bytes % 16 == 0) {
-    l1Impl<DataT, AccT, OutT, IdxT, 16 / sizeof(DataT), FinalLambda>(
-      x, y, m, n, k, dOutput, fin_op, stream);
-  } else if (8 % sizeof(DataT) == 0 && bytes % 8 == 0) {
-    l1Impl<DataT, AccT, OutT, IdxT, 8 / sizeof(DataT), FinalLambda>(
-      x, y, m, n, k, dOutput, fin_op, stream);
+          typename FinalLambda, bool isRowMajor>
+void l1(IdxT m, IdxT n, IdxT k, IdxT lda, IdxT ldb, IdxT ldd, const DataT *x,
+        const DataT *y, OutT *dOutput, FinalLambda fin_op,
+        cudaStream_t stream) {
+  size_t bytesA = sizeof(DataT) * lda;
+  size_t bytesB = sizeof(DataT) * ldb;
+  if (16 % sizeof(DataT) == 0 && bytesA % 16 == 0 && bytesB % 16 == 0) {
+    l1Impl<DataT, AccT, OutT, IdxT, 16 / sizeof(DataT), FinalLambda,
+           isRowMajor>(x, y, m, n, k, lda, ldb, ldd, dOutput, fin_op, stream);
+  } else if (8 % sizeof(DataT) == 0 && bytesA % 8 == 0 && bytesB % 8 == 0) {
+    l1Impl<DataT, AccT, OutT, IdxT, 8 / sizeof(DataT), FinalLambda, isRowMajor>(
+      x, y, m, n, k, lda, ldb, ldd, dOutput, fin_op, stream);
   } else {
-    l1Impl<DataT, AccT, OutT, IdxT, 1, FinalLambda>(x, y, m, n, k, dOutput,
-                                                    fin_op, stream);
+    l1Impl<DataT, AccT, OutT, IdxT, 1, FinalLambda, isRowMajor>(
+      x, y, m, n, k, lda, ldb, ldd, dOutput, fin_op, stream);
   }
 }
 
@@ -96,7 +112,6 @@ void l1(IdxT m, IdxT n, IdxT k, const DataT *x, const DataT *y, OutT *dOutput,
  * @tparam InType input data-type (for A and B matrices)
  * @tparam AccType accumulation data-type
  * @tparam OutType output data-type (for C and D matrices)
- * @tparam OutputTile_ output tile size for the thread block
  * @tparam FinalLambda user-defined epilogue lamba
  * @tparam Index_ Index type
  * @param m number of rows of A and C/D
@@ -110,74 +125,24 @@ void l1(IdxT m, IdxT n, IdxT k, const DataT *x, const DataT *y, OutT *dOutput,
  * @param isRowMajor whether the input and output matrices are row major
  */
 template <typename InType, typename AccType, typename OutType,
-          typename OutputTile_, typename FinalLambda, typename Index_ = int>
+          typename FinalLambda, typename Index_ = int>
 void l1Impl(int m, int n, int k, const InType *pA, const InType *pB,
             OutType *pD, FinalLambda fin_op, cudaStream_t stream,
             bool isRowMajor) {
   typedef std::is_same<OutType, bool> is_bool;
-
+  typedef
+    typename std::conditional<is_bool::value, OutType, AccType>::type L1OutType;
+  Index_ lda, ldb, ldd;
+  L1OutType *pDcast = reinterpret_cast<L1OutType *>(pD);
   if (isRowMajor) {
-    typedef typename std::conditional<is_bool::value, OutType, AccType>::type
-      L1OutType;
-    l1<InType, AccType, L1OutType, Index_, FinalLambda>(
-      m, n, k, pA, pB, reinterpret_cast<L1OutType *>(pD), fin_op, stream);
+    lda = k, ldb = k, ldd = n;
+    l1<InType, AccType, L1OutType, Index_, FinalLambda, true>(
+      m, n, k, lda, ldb, ldd, pA, pB, pDcast, fin_op, stream);
 
   } else {
-    typedef typename std::conditional<is_bool::value, AccType, OutType>::type
-      EffOutType;
-    EffOutType *pDCast =
-      reinterpret_cast<EffOutType *>(pD);  // Pretend to be EffOutType;
-
-    typedef cutlass::Shape<8, 8, 8> AccumulatorsPerThread_;
-    typedef LinAlg::ThreadL1NormAdd<
-      AccumulatorsPerThread_, cutlass::Shape<1, 4, 8>, InType, InType, AccType>
-      MainLoopFunctor_;
-    typedef LinAlg::CustomGemmConfig<InType, AccType, EffOutType, OutputTile_,
-                                     AccumulatorsPerThread_, MainLoopFunctor_>
-      GemmConfig_;
-
-    typedef UnexpandedDistanceFragmentMultiplyAdd FragmentMultiplyAdd_;
-
-    typedef UnexpandedDistanceEpilogueFunctor<EffOutType, GemmConfig_,
-                                              FragmentMultiplyAdd_>
-      EpilogueFunctor_;
-
-    typedef typename std::conditional<
-      is_bool::value,
-      BoolEpilogueTraitsHelper<GemmConfig_, EpilogueFunctor_, Index_>,
-      cutlass::gemm::GemmEpilogueTraitsHelper<
-        GemmConfig_, EpilogueFunctor_, Index_>>::type EpilogueTraitsHelper_;
-
-    typedef typename cutlass::gemm::SimplifiedGemmEpilogueTraits<
-      GemmConfig_, EpilogueFunctor_, Index_, EpilogueTraitsHelper_>
-      GemmEpilogueTraits_;
-    typedef UnexpandedDistanceGemmEpilogue<GemmEpilogueTraits_> GemmEpilogue_;
-    typedef typename EpilogueFunctor_::Params EpiParams;
-
-    cublasOperation_t transa, transb;
-    const InType *aPtr, *bPtr;
-    Index_ lda, ldb, ldd;
-    Index_ gemm_m, gemm_n;
-    transa = CUBLAS_OP_N;
-    transb = CUBLAS_OP_T;
-    aPtr = pA;
-    bPtr = pB;
-    lda = m;
-    ldb = n;
-    ldd = m;
-    gemm_m = m;
-    gemm_n = n;
-
-    LinAlg::gemm<InType, AccType, EffOutType, OutputTile_,
-                 AccumulatorsPerThread_, MainLoopFunctor_, Index_, GemmConfig_,
-                 EpilogueFunctor_, GemmEpilogueTraits_, GemmEpilogue_>(
-      transa, transb, gemm_m, gemm_n, k, (EffOutType)1, aPtr, lda, bPtr, ldb,
-      (EffOutType)0, nullptr, ldd, pDCast,
-      [] HD(EpiParams & p) {
-        int err = p.initializeExtra(nullptr, nullptr, false);
-        return err;
-      },
-      fin_op, stream);
+    lda = n, ldb = m, ldd = m;
+    l1<InType, AccType, L1OutType, Index_, FinalLambda, false>(
+      n, m, k, lda, ldb, ldd, pB, pA, pDcast, fin_op, stream);
   }
 }
 }  // namespace Distance
