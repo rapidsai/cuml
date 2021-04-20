3dca6cab3444007d8f75876fffc3672b3344859e
achirkin <achirkin@users.noreply.github.com>
2021-04-20 14:36:34 +0200

    Change the default tolerance to match sklearn and linear_regression.pyx

--- python/cuml/solvers/qn.pyx
+++ python/cuml/solvers/qn.pyx
@@ -254,7 +254,7 @@ class QN(Base,
     intercept_ = CumlArrayDescriptor()
 
     def __init__(self, *, loss='sigmoid', fit_intercept=True,
-                 l1_strength=0.0, l2_strength=0.0, max_iter=1000, tol=1e-3,
+                 l1_strength=0.0, l2_strength=0.0, max_iter=1000, tol=1e-4,
                  linesearch_max_iter=50, lbfgs_memory=5,
                  verbose=False, handle=None, output_type=None):
 
