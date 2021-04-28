22abfd4476c6cc1eae65b02f11fc99d2fb797e5f
Levs Dolgovs <ldolgovs@nvidia.com>
2021-04-28 15:59:24 -0700

    stop sprawling code duplication

--- python/cuml/fil/fil.pyx
+++ python/cuml/fil/fil.pyx
@@ -316,36 +316,8 @@ cdef class ForestInference_impl():
         return preds
 
     def load_from_treelite_model_handle(self, **kwargs):
-        cdef treelite_params_t treelite_params
-
-        self.output_class = kwargs['output_class']
-        treelite_params.output_class = self.output_class
-        treelite_params.threshold = kwargs['threshold']
-        treelite_params.algo = self.get_algo(kwargs['algo'])
-        treelite_params.storage_type =\
-            self.get_storage_type(kwargs['storage_type'])
-        treelite_params.blocks_per_sm = kwargs['blocks_per_sm']
-        cdef char* forest_shape_str
-        if kwargs['forest_shape_file'] is not None:
-            treelite_params.pforest_shape_str = &forest_shape_str
-        else:
-            treelite_params.pforest_shape_str = NULL
-
         self.forest_data = NULL
-        cdef handle_t* handle_ =\
-            <handle_t*><size_t>self.handle.getHandle()
-        cdef uintptr_t model_ptr = <uintptr_t>kwargs['model_handle']
-
-        from_treelite(handle_[0],
-                      &self.forest_data,
-                      <ModelHandle> model_ptr,
-                      &treelite_params)
-        if kwargs['forest_shape_file'] is not None:
-            kwargs['forest_shape_file'].write(forest_shape_str)
-            free(forest_shape_str)
-        TreeliteQueryNumClass(<ModelHandle> model_ptr,
-                              & self.num_class)
-        return self
+        return self.load_using_treelite_handle(**kwargs)
 
     def load_from_treelite_model(self, **kwargs):
         cdef TreeliteModel model = kwargs['model']
