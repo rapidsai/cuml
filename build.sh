852cdc738545ada8f42f85a61015f2aa57288186
Dante Gama Dessavre <dante.gamadessavre@gmail.com>
2021-05-09 19:11:48 -0500

    FIX build.sh and setup.py updates to account for recent changes

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ ARGS=$*
 REPODIR=$(cd $(dirname $0); pwd)
 
 VALIDTARGETS="clean libcuml cuml cpp-mgtests prims bench prims-bench cppdocs pydocs"
-VALIDFLAGS="-v -g -n --allgpuarch --buildfaiss --buildgtest --singlegpu --nvtx --show_depr_warn --codecov --ccache -h --help "
+VALIDFLAGS="-v -g -n --allgpuarch --buildfaiss --buildgtest --singlegpu --nolibcumltest --nvtx --show_depr_warn --codecov --ccache -h --help "
 VALIDARGS="${VALIDTARGETS} ${VALIDFLAGS}"
 HELP="$0 [<target> ...] [<flag> ...]
  where <target> is:
@@ -28,8 +28,8 @@ HELP="$0 [<target> ...] [<flag> ...]
                       around the C++ code.
    cuml             - build the cuml Python package
    cpp-mgtests      - build libcuml mnmg tests. Builds MPI communicator, adding MPI as dependency.
-   prims            - build the ML prims tests
-   bench            - build the cuml C++ benchmark
+   prims            - build the ml-prims tests
+   bench            - build the libcuml C++ benchmark
    prims-bench      - build the ml-prims C++ benchmark
    cppdocs          - build the C++ API doxygen documentation
    pydocs           - build the general and Python API documentation
@@ -42,12 +42,12 @@ HELP="$0 [<target> ...] [<flag> ...]
    --buildfaiss     - build faiss statically into libcuml
    --buildgtest     - build googletest library
    --singlegpu      - Build libcuml and cuml without multigpu components
+   --nolibcumltest  - disable building libcuml C++ tests for a faster build
    --nvtx           - Enable nvtx for profiling support
    --show_depr_warn - show cmake deprecation warnings
    --codecov        - Enable code coverage support by compiling with Cython linetracing
                       and profiling enabled (WARNING: Impacts performance)
    --ccache         - Use ccache to cache previous compilations
-   --cuda           - Set the CUDA version to use. Use the string 'auto' to automatically determine, the version number (i.e. '10.1', '11.0', etc.), or the full path to the CUDA installation
 
  default action (no args) is to build and install 'libcuml', 'cuml', and 'prims' targets only for the detected GPU arch
 
@@ -75,7 +75,7 @@ CCACHE=OFF
 CLEAN=0
 BUILD_DISABLE_DEPRECATION_WARNING=ON
 BUILD_CUML_STD_COMMS=ON
-BUILD_CPP_MG_TESTS=OFF
+BUILD_CUML_MG_TESTS=OFF
 BUILD_STATIC_FAISS=OFF
 CUDA="auto"
 
@@ -84,7 +84,6 @@ CUDA="auto"
 #         CONDA_PREFIX, but there is no fallback from there!
 INSTALL_PREFIX=${INSTALL_PREFIX:=${PREFIX:=${CONDA_PREFIX}}}
 PARALLEL_LEVEL=${PARALLEL_LEVEL:=""}
-
 
 # Default to Ninja if generator is not specified
 export CMAKE_GENERATOR="${CMAKE_GENERATOR:=Ninja}"
@@ -131,7 +130,7 @@ LONG_ARGUMENT_LIST=(
     "nvtx"
     "show_depr_warn"
     "codecov"
-    "cuda:"
+    "ccache"
 )
 
 # Short arguments
@@ -155,7 +154,7 @@ eval set -- "$opts"
 
 while true; do
     case "$1" in
-        h)
+        -h)
             show_help
             exit 0
             ;;
@@ -189,10 +188,6 @@ while true; do
             ;;
         --codecov )
             CUML_EXTRA_PYTHON_ARGS="${CUML_EXTRA_PYTHON_ARGS} --linetrace=1 --profile"
-            ;;
-        --cuda )
-            shift
-            CUDA=$1
             ;;
         --ccache )
             CCACHE=ON
@@ -230,59 +225,28 @@ fi
 # Configure for building all C++ targets
 if completeBuild || hasArg libcuml || hasArg prims || hasArg bench || hasArg prims-bench || hasArg cppdocs || hasArg cpp-mgtests; then
     if (( ${BUILD_ALL_GPU_ARCH} == 0 )); then
-        GPU_ARCH=""
+        CUML_CMAKE_CUDA_ARCHITECTURES="NATIVE"
         echo "Building for the architecture of the GPU in the system..."
     else
-        GPU_ARCH="-DGPU_ARCHS=ALL"
+        CUML_CMAKE_CUDA_ARCHITECTURES="ALL"
         echo "Building for *ALL* supported GPU architectures..."
-    fi
-
-    CUML_CUDA_ROOT=""
-
-    # Now, determine CUDA architecture
-    if [[ "${CUDA}" == "auto" ]]; then
-        # Need to decide if we are doing anything here
-        CUML_CUDA_ROOT="/usr/local/cuda"
-    elif [[ "${CUDA}" =~ ^[0-9]{1,2}\.?[0-9]?$ ]]; then
-        # Number form. Convert to ##.# form
-        major=`echo "$CUDA." | cut -d. -f1`
-        minor=`echo "$CUDA." | cut -d. -f2`
-        CUML_CUDA_ROOT="/usr/local/cuda-${major}.${minor:-0}"
-    else
-        CUML_CUDA_ROOT="${CUDA}"
-    fi
-
-    # Check for the existance of version.txt which CMake uses to identify CUDA
-    if [[ -e "${CUML_CUDA_ROOT}/version.txt" ]]; then
-        echo "Building cuML with $(cat "${CUML_CUDA_ROOT}/version.txt")"
-        export CUDAToolkit_ROOT="$CUML_CUDA_ROOT"
-    elif [[ -x "${CUML_CUDA_ROOT}/bin/nvcc" ]]; then
-        echo "Building cuML with CUDA Version $(${CUML_CUDA_ROOT}/bin/nvcc --version | sed -nr 's/^Cuda compilation tools.*V([0-9]{1,2}\.[0-9]\.[0-9]{0,2})$/\1/p')"
-        export CUDAToolkit_ROOT="$CUML_CUDA_ROOT"
-    else
-        echo "Invalid CUDA directory specified: ${CUML_CUDA_ROOT}"
-        exit -1
     fi
 
     mkdir -p ${LIBCUML_BUILD_DIR}
     cd ${LIBCUML_BUILD_DIR}
 
     cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
-          -DBLAS_LIBRARIES=${INSTALL_PREFIX}/lib/libopenblas.so.0 \
-          ${GPU_ARCH} \
+          -DCMAKE_CUDA_ARCHITECTURES=${RAFT_CMAKE_CUDA_ARCHITECTURES} \
           -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
           -DBUILD_CUML_C_LIBRARY=ON \
           -DSINGLEGPU=${SINGLEGPU_CPP_FLAG} \
-          -DWITH_UCX=ON \
-          -DBUILD_CUML_MPI_COMMS=${BUILD_CPP_MG_TESTS} \
-          -DBUILD_CUML_MG_TESTS=${BUILD_CPP_MG_TESTS} \
-          -DBUILD_STATIC_FAISS=${BUILD_STATIC_FAISS} \
+          -DBUILD_CUML_MPI_COMMS=${BUILD_CUML_MG_TESTS} \
+          -DBUILD_CUML_MG_TESTS=${BUILD_CUML_MG_TESTS} \
           -DNVTX=${NVTX} \
-          -DPARALLEL_LEVEL=${PARALLEL_LEVEL} \
           -DUSE_CCACHE=${CCACHE} \
-          -DNCCL_PATH=${INSTALL_PREFIX} \
           -DDISABLE_DEPRECATION_WARNING=${BUILD_DISABLE_DEPRECATION_WARNING} \
           -DCMAKE_PREFIX_PATH=${INSTALL_PREFIX} \
+          -DCMAKE_MESSAGE_LOG_LEVEL=VERBOSE \
           ${CUML_EXTRA_CMAKE_ARGS} \
           ..
 fi
@@ -291,7 +255,10 @@ fi
 
 MAKE_TARGETS=
 if hasArg libcuml; then
-    MAKE_TARGETS="${MAKE_TARGETS}cuml++ cuml ml"
+    MAKE_TARGETS="${MAKE_TARGETS}cuml cuml_c"
+    if !hasArg --nolibcumltest; then
+      MAKE_TARGETS="${MAKE_TARGETS} ml"
+    fi
 fi
 if hasArg cpp-mgtests; then
     MAKE_TARGETS="${MAKE_TARGETS} ml_mg"
