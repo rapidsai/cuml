34b258136c710201f2175e84e4fb4ae90c7f2687
Garrison Hess <garrison.hess@gmail.com>
2021-05-12 19:11:41 -0400

    added tests for poly and rbf kernels

--- cpp/test/sg/kpca_test.cu
+++ cpp/test/sg/kpca_test.cu
@@ -97,6 +97,63 @@ class KPcaTest : public ::testing::TestWithParam<KPcaInputs<T>> {
     kpcaTransform(handle, data, alphas, lambdas, trans_data, prms, stream);
   }
 
+
+  void allKernelsTest() {
+    params = ::testing::TestWithParam<KPcaInputs<T>>::GetParam();
+    raft::random::Rng r(params.seed, raft::random::GenTaps);
+    int len = params.len;
+
+    raft::allocate(data, len);
+    raft::allocate(data_back, len);
+    raft::allocate(trans_data, len); // transformed data
+
+    std::vector<T> data_h = {1.0, 2.0, 5.0, 4.0, 2.0, 1.0};
+    data_h.resize(len);
+    raft::update_device(data, data_h.data(), len, stream);
+
+    //  could change this to n_row * min(n_row, n_col) once truncation is working
+    int len_alphas = params.n_row * params.n_row;  
+    raft::allocate(alphas, len_alphas);
+    raft::allocate(lambdas, params.n_row);
+
+    //  unscaled eigenvectors - sklearn does not re-scale them until the transform step
+    //  alphas - linear, poly, rbf
+    std::vector<T> alphas_ref_h = {{-0.6525, -0.0987, 0.7513, -0.4907, 0.8105, -0.3197},
+                                  {-0.5430, -0.2565, 0.7995, -0.6097, 0.7751, -0.1653},
+                                  {-0.4341, -0.3818, 0.8159, -0.6915, 0.7217, -0.0301}};
+  
+    alphas_ref_h.resize(len_alphas);
+    std::vector<T> lambdas_ref_h = {{12.6759, 0.6574}, {1790.3207, 210.3639}, {1.0230, 0.9177}};
+    lambdas_ref_h.resize(params.n_row);
+    //  tranformed - linear, poly, rbf
+    std::vector<T> trans_data_ref_h = {{-2.3231,-0.3517, 2.6749, -0.3979, 0.6571,-0.2592},
+                                      {-22.9760, -10.8554, 33.8314, -8.8438, 11.2426, -2.3987},
+                                      {-0.4391, -0.3862, 0.8253, -0.6624, 0.6914, -0.0289}};
+    trans_data_ref_h.resize(len);
+
+    raft::allocate(alphas_ref, len_alphas);
+    raft::allocate(lambdas_ref, params.n_row);
+    raft::allocate(trans_data_ref, len);
+
+    raft::update_device(alphas_ref, alphas_ref_h.data(), len_alphas, stream);
+    raft::update_device(lambdas_ref, lambdas_ref_h.data(), params.n_row, stream);
+    raft::update_device(trans_data_ref, trans_data_ref_h.data(), len, stream);
+
+    paramsKPCA prms;
+    prms.n_cols = params.n_col;
+    prms.n_rows = params.n_row;
+    prms.n_components = params.n_col;
+    prms.kernel = MLCommon::Matrix::KernelParams{Matrix::LINEAR, 0, 0, 0};
+    if (params.algo == 0)
+      prms.algorithm = solver::COV_EIG_DQ;
+    else
+      prms.algorithm = solver::COV_EIG_JACOBI;
+
+    kpcaFit(handle, data, alphas, lambdas, prms, stream);
+    kpcaTransform(handle, data, alphas, lambdas, trans_data, prms, stream);
+  }
+
+
   void SetUp() override {
     CUDA_CHECK(cudaStreamCreate(&stream));
     handle.set_stream(stream);
@@ -126,11 +183,11 @@ class KPcaTest : public ::testing::TestWithParam<KPcaInputs<T>> {
 
 const std::vector<KPcaInputs<float>> inputsf2 = {
   {0.01f, 3 * 2, 3, 2, 1024 * 128, 1024, 128, 1234ULL, 0},
-  {0.01f, 3 * 2, 3, 2, 256 * 32, 256, 32, 1234ULL, 1}};
+  {0.01f, 3 * 2, 3, 2, 1024 * 128, 1024, 128, 1234ULL, 0}};
 
 const std::vector<KPcaInputs<double>> inputsd2 = {
   {0.01, 3 * 2, 3, 2, 1024 * 128, 1024, 128, 1234ULL, 0},
-  {0.01, 3 * 2, 3, 2, 256 * 32, 256, 32, 1234ULL, 1}};
+  {0.01f, 3 * 2, 3, 2, 1024 * 128, 1024, 128, 1234ULL, 0}};
 
 
 typedef KPcaTest<float> KPcaTestValF;
