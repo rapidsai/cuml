57364fbae8ef00d8c224714eb3c4b9d457ce9be1
Dante Gama Dessavre <dante.gamadessavre@gmail.com>
2021-05-25 18:52:25 -0500

    Update to CMake 3.20 features, `rapids-cmake` and `CPM` (#3844)
    Depends on https://github.com/rapidsai/raft/pull/187
    
    Based on the work started by @mdemoret-nv on https://github.com/mdemoret-nv/cuml/tree/imp-modernize-cmake with updates for CPM, rapids-cmake and consistency with the equivalent PRs in RAFT  and cuGraph
    
    Details of changes will be added soon
    
    cc @trxcllnt and @robertmaynard for viz
    
    Authors:
      - Dante Gama Dessavre (https://github.com/dantegd)
      - Michael Demoret (https://github.com/mdemoret-nv)
    
    Approvers:
      - Robert Maynard (https://github.com/robertmaynard)
      - AJ Schmidt (https://github.com/ajschmidt8)
      - John Zedlewski (https://github.com/JohnZed)
    
    URL: https://github.com/rapidsai/cuml/pull/3844

---
+++ cpp/cmake/thirdparty/get_raft.cmake
@@ -0,0 +1,49 @@
+#=============================================================================
+# Copyright (c) 2021, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#=============================================================================
+
+function(find_and_configure_raft)
+
+    set(oneValueArgs VERSION FORK PINNED_TAG)
+    cmake_parse_arguments(PKG "${options}" "${oneValueArgs}"
+                          "${multiValueArgs}" ${ARGN} )
+
+    rapids_cpm_find(raft ${PKG_VERSION}
+      GLOBAL_TARGETS      raft::raft
+      BUILD_EXPORT_SET    cuml-exports
+      INSTALL_EXPORT_SET  cuml-exports
+        CPM_ARGS
+            GIT_REPOSITORY https://github.com/${PKG_FORK}/raft.git
+            GIT_TAG        ${PKG_PINNED_TAG}
+            SOURCE_SUBDIR  cpp
+            OPTIONS
+              "BUILD_TESTS OFF"
+
+    )
+
+    message(VERBOSE "CUML: Using RAFT located in ${raft_SOURCE_DIR}")
+
+endfunction()
+
+set(CUML_MIN_VERSION_raft "${CUML_VERSION_MAJOR}.${CUML_VERSION_MINOR}.00")
+set(CUML_BRANCH_VERSION_raft "${CUML_VERSION_MAJOR}.${CUML_VERSION_MINOR}")
+
+# Change pinned tag here to test a commit in CI
+# To use a different RAFT locally, set the CMake variable
+# CPM_raft_SOURCE=/path/to/local/raft
+find_and_configure_raft(VERSION    ${CUML_MIN_VERSION_raft}
+                        FORK       rapidsai
+                        PINNED_TAG branch-${CUML_BRANCH_VERSION_raft}
+                        )
