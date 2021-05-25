34fed60cc5b96e8c7e371fa3c1cc6607c7dcb172
jolorunyomi <joseph.cloudengineer@gmail.com>
2021-05-25 14:02:28 -0500

    add support for space in jenkins workspace

--- ci/docs/build.sh
+++ ci/docs/build.sh
@@ -57,12 +57,12 @@ cd "$DOCS_WORKSPACE"
 
 for PROJECT in ${PROJECTS[@]}; do
     if [ ! -d "api/$PROJECT/$BRANCH_VERSION" ]; then
-        mkdir -p api/$PROJECT/$BRANCH_VERSION
+        mkdir -p "api/$PROJECT/$BRANCH_VERSION"
     fi
     rm -rf "$DOCS_WORKSPACE/api/$PROJECT/$BRANCH_VERSION/*"
 done
 
 
-mv "$PROJECT_WORKSPACE/cpp/build/html/"* "$DOCS_WORKSPACE/api/libcuml/"$BRANCH_VERSION
-mv "$PROJECT_WORKSPACE/docs/build/html/"* "$DOCS_WORKSPACE/api/cuml/"$BRANCH_VERSION
+mv "$PROJECT_WORKSPACE/cpp/build/html/*" "$DOCS_WORKSPACE/api/libcuml/$BRANCH_VERSION"
+mv "$PROJECT_WORKSPACE/docs/build/html/*" "$DOCS_WORKSPACE/api/cuml/$BRANCH_VERSION"
 
