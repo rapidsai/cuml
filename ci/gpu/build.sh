fe4318fd1d612b66121d280a4f0a91c5d9871bec
Dante Gama Dessavre <dante.gamadessavre@gmail.com>
2021-05-28 13:02:20 -0500

    Update ci/gpu/build.sh
    Co-authored-by: jakirkham <jakirkham@gmail.com>

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -195,7 +195,7 @@ else
     
     gpuci_logger "Install the main version of dask and distributed"
     set -x
-    pip install "git+https://github.com/dask/distributed.git@main" --upgrade --no-deps
+    pip install "git+https://github.com/jakirkham/distributed.git@drop_whowhat_client" --upgrade --no-deps
     pip install "git+https://github.com/jakirkham/dask.git@drop_whowhat_client" --upgrade --no-deps
     set +x
 
