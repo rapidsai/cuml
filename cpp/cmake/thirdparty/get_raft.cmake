ea418888c31462c876589ae75f39608b1de43bab
Mahesh Doijade <mdoijade@nvidia.com>
2021-06-03 20:23:23 +0530

    test mdoijade fork raft gridStridedDist branch

--- cpp/cmake/thirdparty/get_raft.cmake
+++ cpp/cmake/thirdparty/get_raft.cmake
@@ -44,6 +44,6 @@ set(CUML_BRANCH_VERSION_raft "${CUML_VERSION_MAJOR}.${CUML_VERSION_MINOR}")
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_raft(VERSION    ${CUML_MIN_VERSION_raft}
-                        FORK       rapidsai
-                        PINNED_TAG branch-${CUML_BRANCH_VERSION_raft}
+                        FORK       mdoijade
+                        PINNED_TAG gridStridedDist
                         )
