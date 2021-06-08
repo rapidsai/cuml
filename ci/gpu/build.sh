71e06857fa89260827ff390b9c9e1150b4416618
Dante Gama Dessavre <dante.gamadessavre@gmail.com>
2021-06-08 17:01:50 -0500

    FIX add back xgboost now that package is published

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -55,11 +55,11 @@ gpuci_conda_retry install -c conda-forge -c rapidsai -c rapidsai-nightly -c nvid
       "dask-cuda=${MINOR_VERSION}" \
       "ucx-py=0.21" \
       "ucx-proc=*=gpu" \
+      "xgboost=1.4.2dev.rapidsai${MINOR_VERSION}" \
       "rapids-build-env=${MINOR_VERSION}.*" \
       "rapids-notebook-env=${MINOR_VERSION}.*" \
       "rapids-doc-env=${MINOR_VERSION}.*" \
       "shap>=0.37,<=0.39"
-      # "xgboost=1.4.2dev.rapidsai${MINOR_VERSION}" \
 
 # https://docs.rapids.ai/maintainers/depmgmt/
 # gpuci_conda_retry remove --force rapids-build-env rapids-notebook-env
