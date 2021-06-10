7983e2cb727eed705f942adf62a1923e91fa7838
Corey J. Nolet <cjnolet@users.noreply.github.com>
2021-06-10 15:36:40 -0400

    [HOTFIX] [REVIEW] DBSCAN batch size bug (32-bit int for device mem query) (#3971)
    * Fixing hdbscan batch size bug (changing batch size to size_t)
    
    * size_t return type
    
    * And here

--- cpp/src/dbscan/dbscan.cuh
+++ cpp/src/dbscan/dbscan.cuh
@@ -28,7 +28,7 @@ namespace ML {
 namespace Dbscan {
 
 template <typename Index_ = int>
-Index_ compute_batch_size(size_t &estimated_memory, Index_ n_rows,
+size_t compute_batch_size(size_t &estimated_memory, Index_ n_rows,
                           Index_ n_owned_rows, size_t max_mbytes_per_batch = 0,
                           Index_ neigh_per_row = 0) {
   // In real applications, it's unlikely that the sparse adjacency matrix
@@ -56,11 +56,11 @@ Index_ compute_batch_size(size_t &estimated_memory, Index_ n_rows,
   // from the over-estimation of the sparse adjacency matrix
 
   // Batch size determined based on available memory
-  Index_ batch_size =
+  size_t batch_size =
     (max_mbytes_per_batch * 1000000 - est_mem_fixed) / est_mem_per_row;
 
   // Limit batch size to number of owned rows
-  batch_size = std::min(n_owned_rows, batch_size);
+  batch_size = std::min((size_t)n_owned_rows, batch_size);
 
   // To avoid overflow, we need: batch_size <= MAX_LABEL / n_rows (floor div)
   Index_ MAX_LABEL = std::numeric_limits<Index_>::max();
@@ -144,7 +144,7 @@ void dbscanFitImpl(const raft::handle_t &handle, T *input, Index_ n_rows,
   }
 
   size_t estimated_memory;
-  Index_ batch_size = compute_batch_size<Index_>(
+  size_t batch_size = compute_batch_size<Index_>(
     estimated_memory, n_rows, n_owned_rows, max_mbytes_per_batch);
 
   CUML_LOG_DEBUG(
