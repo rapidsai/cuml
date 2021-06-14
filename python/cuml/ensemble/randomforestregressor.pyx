2308d9f7331a549b41edc1212956b86dd23fb026
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2021-06-14 17:15:38 +1200

    Apply suggestions from code review
    Co-authored-by: Thejaswi. N. S <rao.thejaswi@gmail.com>

--- python/cuml/ensemble/randomforestregressor.pyx
+++ python/cuml/ensemble/randomforestregressor.pyx
@@ -229,7 +229,7 @@ class RandomForestRegressor(BaseRandomForestModel,
         for mean square error' : 'mse'
     use_experimental_backend : boolean (default = True)
         Deprecated and currrently has no effect.
-        .. deprecated:: 21.06
+        .. deprecated:: 21.08
     max_batch_size: int (default = 128)
         Maximum number of nodes that can be processed in a given batch. This is
         used only when 'use_experimental_backend' is true.
