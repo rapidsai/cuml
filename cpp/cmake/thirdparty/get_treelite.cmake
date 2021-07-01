420e16653b66511f2360701300fd37fda8d99e32
ptaylor <paul.e.taylor@me.com>
2021-07-01 14:24:21 -0500

    only augment treelite if CPM downloaded it from github

--- cpp/cmake/thirdparty/get_treelite.cmake
+++ cpp/cmake/thirdparty/get_treelite.cmake
@@ -36,22 +36,24 @@ function(find_and_configure_treelite)
 
     set(Treelite_ADDED ${Treelite_ADDED} PARENT_SCOPE)
 
-    target_include_directories(treelite
-        PUBLIC $<BUILD_INTERFACE:${Treelite_SOURCE_DIR}/include>
-               $<BUILD_INTERFACE:${Treelite_BINARY_DIR}/include>)
-    target_include_directories(treelite_static
-        PUBLIC $<BUILD_INTERFACE:${Treelite_SOURCE_DIR}/include>
-               $<BUILD_INTERFACE:${Treelite_BINARY_DIR}/include>)
-    target_include_directories(treelite_runtime
-        PUBLIC $<BUILD_INTERFACE:${Treelite_SOURCE_DIR}/include>
-               $<BUILD_INTERFACE:${Treelite_BINARY_DIR}/include>)
-    target_include_directories(treelite_runtime_static
-        PUBLIC $<BUILD_INTERFACE:${Treelite_SOURCE_DIR}/include>
-               $<BUILD_INTERFACE:${Treelite_BINARY_DIR}/include>)
+    if(Treelite_ADDED)
+        target_include_directories(treelite
+            PUBLIC $<BUILD_INTERFACE:${Treelite_SOURCE_DIR}/include>
+                   $<BUILD_INTERFACE:${Treelite_BINARY_DIR}/include>)
+        target_include_directories(treelite_static
+            PUBLIC $<BUILD_INTERFACE:${Treelite_SOURCE_DIR}/include>
+                   $<BUILD_INTERFACE:${Treelite_BINARY_DIR}/include>)
+        target_include_directories(treelite_runtime
+            PUBLIC $<BUILD_INTERFACE:${Treelite_SOURCE_DIR}/include>
+                   $<BUILD_INTERFACE:${Treelite_BINARY_DIR}/include>)
+        target_include_directories(treelite_runtime_static
+            PUBLIC $<BUILD_INTERFACE:${Treelite_SOURCE_DIR}/include>
+                   $<BUILD_INTERFACE:${Treelite_BINARY_DIR}/include>)
 
-    if(Treelite_ADDED AND NOT TARGET treelite::treelite_static)
-        add_library(treelite::treelite_static ALIAS treelite_static)
-        add_library(treelite::treelite_runtime_static ALIAS treelite_runtime_static)
+        if(NOT TARGET treelite::treelite_static)
+            add_library(treelite::treelite_static ALIAS treelite_static)
+            add_library(treelite::treelite_runtime_static ALIAS treelite_runtime_static)
+        endif()
     endif()
 
 endfunction()
