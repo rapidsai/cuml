aadbb1911c3551f3ae1499a0e3c71289699b9e55
Corey J. Nolet <cjnolet@users.noreply.github.com>
2021-07-11 15:41:20 -0400

    Exposing condensed hierarchy through cython for easier unit-level testing (#4004)
    Authors:
      - Corey J. Nolet (https://github.com/cjnolet)
    
    Approvers:
      - Divye Gala (https://github.com/divyegala)
      - Dante Gama Dessavre (https://github.com/dantegd)
    
    URL: https://github.com/rapidsai/cuml/pull/4004

--- cpp/include/cuml/cluster/hdbscan.hpp
+++ cpp/include/cuml/cluster/hdbscan.hpp
@@ -327,6 +327,14 @@ void hdbscan(const raft::handle_t& handle,
              HDBSCAN::Common::HDBSCANParams& params,
              HDBSCAN::Common::hdbscan_output<int, float>& out);
 
+void build_condensed_hierarchy(const raft::handle_t& handle,
+                               const int* children,
+                               const float* delta,
+                               const int* sizes,
+                               int min_cluster_size,
+                               int n_leaves,
+                               HDBSCAN::Common::CondensedHierarchy<int, float>& condensed_tree);
+
 void _extract_clusters(const raft::handle_t& handle,
                        size_t n_leaves,
                        int n_edges,
