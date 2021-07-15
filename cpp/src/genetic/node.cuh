5a052de98116eb8a6028f1e8325cfa29a72724ab
venkywonka <gvenkatarama@nvidia.com>
2021-07-15 16:00:16 +0530

    fix improper merge conflict

--- cpp/src/genetic/node.cuh
+++ cpp/src/genetic/node.cuh
@@ -25,17 +25,19 @@ namespace detail {
 
 static constexpr float MIN_VAL = 0.001f;
 
-HDI bool is_terminal(node::type t)
-{
+HDI bool is_terminal(node::type t) {
   return t == node::type::variable || t == node::type::constant;
 }
 
 HDI bool is_nonterminal(node::type t) { return !is_terminal(t); }
 
-HDI int arity(node::type t)
-{
-  if (node::type::unary_begin <= t && t <= node::type::unary_end) { return 1; }
-  if (node::type::binary_begin <= t && t <= node::type::binary_end) { return 2; }
+HDI int arity(node::type t) {
+  if (node::type::unary_begin <= t && t <= node::type::unary_end) {
+    return 1;
+  }
+  if (node::type::binary_begin <= t && t <= node::type::binary_end) {
+    return 2;
+  }
   return 0;
 }
 
@@ -52,11 +54,11 @@ DI float evaluate_node(const node& n, const float* data, const uint64_t stride,
     // of operators.
     switch (n.t) {
       // binary operators
-<<<<<<< HEAD
       case node::type::add:
         return in[0] + in[1];
       case node::type::atan2:
         return atan2f(in[0], in[1]);
+      case node::type::div:
         return abs_inval1 < MIN_VAL ? 1.0f : fdividef(in[0], in[1]);
       case node::type::fdim:
         return fdimf(in[0], in[1]);
@@ -117,11 +119,12 @@ DI float evaluate_node(const node& n, const float* data, const uint64_t stride,
         return tanf(in[0]);
       case node::type::tanh:
         return tanhf(in[0]);
-=======
-      case node::type::add: return inval + inval1;
-      case node::type::atan2: return atan2f(inval, inval1);
-      case node::type::div: return abs_inval1 < MIN_VAL ? 1.f : fdividef(inval, inval1);
-      case node::type::fdim: return fdimf(inval, inval1);
+      // shouldn't reach here!
+      default:
+        return 0.f;
+    };
+  }
+}
 
 }  // namespace detail
 }  // namespace genetic
