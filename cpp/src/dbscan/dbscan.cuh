f9835fbf532157054bb80c2556541bcdb05fae69
viktorkovesd <vkovesd88@gmail.com>
2021-10-12 17:09:29 +0200

    Assertion raised from C++, exception from Python, if the input array of DBSCAN.fit is empty

--- cpp/src/dbscan/dbscan.cuh
+++ cpp/src/dbscan/dbscan.cuh
@@ -60,9 +60,7 @@ size_t compute_batch_size(size_t& estimated_memory,
   // from the over-estimation of the sparse adjacency matrix
 
   // Batch size determined based on available memory
-  if (est_mem_per_row == 0) {
-    THROW("Estimated memory per row is 0 for DBScan");
-  }
+  ASSERT(est_mem_per_row > 0, "Estimated memory per row is 0 for DBSCAN");
   size_t batch_size = (max_mbytes_per_batch * 1000000 - est_mem_fixed) / est_mem_per_row;
 
   // Limit batch size to number of owned rows
@@ -121,9 +119,7 @@ void dbscanFitImpl(const raft::handle_t& handle,
   Index_ start_row{0};
   Index_ n_owned_rows{n_rows};
 
-  if (n_rows == 0) {
-    THROW("No rows in the input array. DBScan cannot be fitted!");
-  }
+  ASSERT(n_rows > 0, "No rows in the input array. DBSCAN cannot be fitted!");
 
   if (opg) {
     const auto& comm     = handle.get_comms();
