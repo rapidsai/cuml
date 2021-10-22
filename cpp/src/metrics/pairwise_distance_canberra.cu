ee2e8630bcce756cc93a294fd6f4205c4295bd9c
Corey J. Nolet <cjnolet@users.noreply.github.com>
2021-10-22 16:33:35 -0400

    RAFT distance prims public API update (#4280)
    Authors:
      - Corey J. Nolet (https://github.com/cjnolet)
    
    Approvers:
      - Divye Gala (https://github.com/divyegala)
      - Dante Gama Dessavre (https://github.com/dantegd)
    
    URL: https://github.com/rapidsai/cuml/pull/4280

--- cpp/src/metrics/pairwise_distance_canberra.cu
+++ cpp/src/metrics/pairwise_distance_canberra.cu
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-#include <raft/distance/distance.cuh>
+#include <raft/distance/distance.hpp>
 #include <raft/handle.hpp>
 #include <rmm/device_uvector.hpp>
 #include "pairwise_distance_canberra.cuh"
@@ -33,12 +33,9 @@ void pairwise_distance_canberra(const raft::handle_t& handle,
                                 bool isRowMajor,
                                 double metric_arg)
 {
-  // Allocate workspace
-  rmm::device_uvector<char> workspace(1, handle.get_stream());
-
   // Call the distance function
-  raft::distance::pairwise_distance_impl<double, int, raft::distance::DistanceType::Canberra>(
-    x, y, dist, m, n, k, workspace, handle.get_stream(), isRowMajor);
+  raft::distance::distance<raft::distance::DistanceType::Canberra, double, double, double, int>(
+    x, y, dist, m, n, k, handle.get_stream(), isRowMajor);
 }
 
 void pairwise_distance_canberra(const raft::handle_t& handle,
@@ -51,12 +48,9 @@ void pairwise_distance_canberra(const raft::handle_t& handle,
                                 bool isRowMajor,
                                 float metric_arg)
 {
-  // Allocate workspace
-  rmm::device_uvector<char> workspace(1, handle.get_stream());
-
   // Call the distance function
-  raft::distance::pairwise_distance_impl<float, int, raft::distance::DistanceType::Canberra>(
-    x, y, dist, m, n, k, workspace, handle.get_stream(), isRowMajor);
+  raft::distance::distance<raft::distance::DistanceType::Canberra, float, float, float, int>(
+    x, y, dist, m, n, k, handle.get_stream(), isRowMajor);
 }
 
 }  // namespace Metrics
