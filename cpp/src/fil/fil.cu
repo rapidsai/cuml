f311778b6bdf5ca069b8a74064d2e631b229abdd
Levs Dolgovs <ldolgovs@nvidia.com>
2021-10-29 16:13:15 -0700

    compile

--- cpp/src/fil/fil.cu
+++ cpp/src/fil/fil.cu
@@ -661,7 +661,7 @@ inline std::size_t bit_pool_size(const tl::Tree<T, L>& tree, const categorical_s
     stack.pop();
     while (!tree.IsLeaf(node_id)) {
       if (tree.SplitType(node_id) == tl::SplitFeatureType::kCategorical &&
-          tree.MatchingCategories(tl_node_id).size() > 0) {
+          tree.MatchingCategories(node_id).size() > 0) {
         int fid = tree.SplitIndex(node_id);
         size += cat_sets.sizeof_mask(fid);
       }
