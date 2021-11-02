9501228dc7064ddc9de399ee889439e646d4d53a
Thejaswi Rao <rao.thejaswi@gmail.com>
2021-11-02 02:25:13 -0700

    updated copyright years and fixed flake8 errors

--- python/cuml/benchmark/run_benchmarks.py
+++ python/cuml/benchmark/run_benchmarks.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2019, NVIDIA CORPORATION.
+# Copyright (c) 2019-2021, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,8 +21,8 @@ import json
 
 
 PrecisionMap = {
-    "fp32" : np.float32,
-    "fp64" : np.float64,
+    "fp32": np.float32,
+    "fp64": np.float64,
 }
 
 
