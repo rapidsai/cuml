52271371018c3a1c788c254128f022b4c4d89b9c
divyegala <divyegala@gmail.com>
2021-11-03 13:14:42 -0700

    adding testing label for cumlprims

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -46,7 +46,7 @@ gpuci_logger "Activate conda env"
 conda activate rapids
 
 gpuci_logger "Install dependencies"
-gpuci_mamba_retry install -c conda-forge -c rapidsai -c rapidsai-nightly -c nvidia \
+gpuci_mamba_retry install -c conda-forge -c rapidsai -c rapidsai-nightly/label/testing -c rapidsai-nightly -c nvidia \
       "cudatoolkit=${CUDA_REL}" \
       "cudf=${MINOR_VERSION}" \
       "rmm=${MINOR_VERSION}" \
