88b6543b5da86796dc6f85805b737e22f5b54df9
Levs Dolgovs <ldolgovs@nvidia.com>
2021-11-08 20:09:44 -0800

    moved to local generator that's passed in a thread-safe manner

--- python/cuml/test/test_fil.py
+++ python/cuml/test/test_fil.py
@@ -17,7 +17,7 @@ import numpy as np
 import pytest
 import os
 import pandas as pd
-from random import sample, seed
+from math import ceil
 
 from cuml import ForestInference
 from cuml.test.utils import array_equal, unit_param, \
@@ -491,8 +491,9 @@ def test_output_args(small_classifier_and_preds):
     assert array_equal(fil_preds, xgb_preds, 1e-3)
 
 
-def to_categorical(features, n_categorical):
-    # returns data in two formats: pandas (for LightGBM) and numpy (for FIL)
+def to_categorical(features, n_categorical, invalid_pct, rng):
+    """ returns data in two formats: pandas (for LightGBM) and numpy (for FIL)
+    """
     # the main bottleneck (>80%) of to_categorical() is the pandas operations
     n_features = features.shape[1]
     df_cols = {}
@@ -503,19 +504,22 @@ def to_categorical(features, n_categorical):
     rough_n_categories = 100
     # round into rough_n_categories bins
     cat_cols = (cat_cols * rough_n_categories).astype(int)
+    # randomly inject invalid categories
+    invalid_idx = rng.choice(
+        a=cat_cols.size,
+        size=ceil(cat_cols.size * invalid_pct / 100),
+        replace=False,
+        shuffle=False)
+    cat_cols.flat[invalid_idx] += rough_n_categories
+
     new_features = features.copy()
     new_features[:, :n_categorical] = cat_cols
 
     # shuffle the columns around
-    seed(42)
-    new_idx = sample(range(n_features), k=n_features)
+    new_idx = rng.choice(n_features, n_features, replace=False, shuffle=True)
     new_matrix = new_features[:, new_idx]
     for icol in range(n_categorical):
         col = cat_cols[:, icol]
-        # randomly inject invalid categories
-        n_rows = features.shape[0]
-        invalid_idx = sample(range(n_rows), k=n_rows // 10 + 1)
-        col[invalid_idx] += rough_n_categories
         df_cols[icol] = pd.Series(pd.Categorical(col,
                                                  categories=np.unique(col)))
     # all numerical columns
@@ -542,26 +546,22 @@ def test_lightgbm(tmp_path, num_classes, n_categorical):
         n_rows = 500
         n_informative = 'auto'
 
+    state = np.random.RandomState(43210)
     X, y = simulate_data(n_rows,
                          n_features,
                          num_classes,
                          n_informative=n_informative,
-                         random_state=43210,
+                         random_state=state,
                          classification=True)
+    rng = np.random.default_rng(hash(state))
     if n_categorical > 0:
-        X_fit, X = to_categorical(X, n_categorical)
+        X_fit, X = to_categorical(X, n_categorical, 10, rng)
     else:
         X_fit = X
 
     train_data = lgb.Dataset(X_fit, label=y)
     num_round = 5
     model_path = str(os.path.join(tmp_path, 'lgb.model'))
-
-    def fil_load():
-        return ForestInference.load(model_path,
-                                    algo='TREE_REORG',
-                                    output_class=True,
-                                    model_type="lightgbm")
 
     if num_classes == 2:
         param = {'objective': 'binary',
@@ -571,7 +571,10 @@ def test_lightgbm(tmp_path, num_classes, n_categorical):
         bst.save_model(model_path)
         # binary classification
         gbm_proba = bst.predict(X)
-        fm = fil_load()
+        fm = ForestInference.load(model_path,
+                                  algo='TREE_REORG',
+                                  output_class=True,
+                                  model_type="lightgbm")
         fil_proba = fm.predict_proba(X)[:, 1]
         gbm_preds = (gbm_proba > 0.5).astype(int)
         fil_preds = fm.predict(X)
@@ -586,7 +589,10 @@ def test_lightgbm(tmp_path, num_classes, n_categorical):
         lgm.fit(X_fit, y)
         lgm.booster_.save_model(model_path)
         lgm_preds = lgm.predict(X).astype(int)
-        fm = fil_load()
+        fm = ForestInference.load(model_path,
+                                  algo='TREE_REORG',
+                                  output_class=True,
+                                  model_type="lightgbm")
         assert array_equal(lgm.booster_.predict(X).argmax(axis=1), lgm_preds)
         assert array_equal(lgm_preds, fm.predict(X))
         # lightgbm uses float64 thresholds, while FIL uses float32
