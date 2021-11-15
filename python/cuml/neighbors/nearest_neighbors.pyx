45120786607cb3da4c9cbb50d0c974d003b2d77c
NV-jpt <86264103+NV-jpt@users.noreply.github.com>
2021-11-14 23:32:43 -0500

    Update of "Gracefully accept 'n_jobs', a common sklearn parameter, in NearestNeighbors Estimator" (#4267)
    This pull request partially solves [[FEA] #3461](https://github.com/rapidsai/cuml/issues/3461)
    
    This quick-fix has been created to enable cuML's NearestNeighbor estimator to gracefully accept sklearns 'n_jobs' parameter as a pass-through.
    
    The purpose of making this quick fix is to allow Imbalanced-Learn samplers to rely on cuML's NearestNeighbor estimator, without producing an error when setting the estimators n_jobs parameter .set_params(**{"n_jobs": self.n_jobs})
    
    The[ original PR ](https://github.com/rapidsai/cuml/pull/4178)to address this issue was not sufficient, as [`set_params()`](https://github.com/rapidsai/cuml/blob/2fee231ac28d982f64c4a746c25be19750812e81/python/cuml/common/base.pyx#L248) will still raise a ValueError if "n_jobs" is not returned by [`get_param_names()`](https://github.com/rapidsai/cuml/blob/2fee231ac28d982f64c4a746c25be19750812e81/python/cuml/neighbors/nearest_neighbors.pyx#L453)
    
    Authors:
      - https://github.com/NV-jpt
      - Dante Gama Dessavre (https://github.com/dantegd)
    
    Approvers:
      - William Hicks (https://github.com/wphicks)
    
    URL: https://github.com/rapidsai/cuml/pull/4267

--- python/cuml/neighbors/nearest_neighbors.pyx
+++ python/cuml/neighbors/nearest_neighbors.pyx
@@ -454,7 +454,7 @@ class NearestNeighbors(Base,
     def get_param_names(self):
         return super().get_param_names() + \
             ["n_neighbors", "algorithm", "metric",
-                "p", "metric_params", "algo_params"]
+                "p", "metric_params", "algo_params", "n_jobs"]
 
     @staticmethod
     def _build_metric_type(metric):
