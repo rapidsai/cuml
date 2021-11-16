254cb34c6009f1bc865113ae2ae915dc518e2188
Dante Gama Dessavre <dante.gamadessavre@gmail.com>
2021-11-16 10:41:07 -0600

    FIX change call in dask nn test

--- python/cuml/test/dask/test_nearest_neighbors.py
+++ python/cuml/test/dask/test_nearest_neighbors.py
@@ -113,7 +113,7 @@ def test_compare_skl(nrows, ncols, nclusters, n_parts, n_neighbors,
 
     out_d, out_i = cumlModel.kneighbors(X_cudf)
 
-    local_i = np.array(out_i.compute().to_cupy(), dtype="int64")
+    local_i = np.array(out_i.compute().to_numpy(), dtype="int64")
 
     sklModel = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
     skl_y_hat = sklModel.predict(X)
