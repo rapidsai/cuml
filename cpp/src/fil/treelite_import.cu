bdbd95c2250503ac145cf6f6f8a2412d44c40d42
Levs Dolgovs <ldolgovs@nvidia.com>
2021-11-23 17:31:30 -0800

    side-by-side

--- cpp/src/fil/treelite_import.cu
+++ cpp/src/fil/treelite_import.cu
@@ -69,6 +69,29 @@ int tree_root(const tl::Tree<T, L>& tree)
   return 0;  // Treelite format assumes that the root is 0
 }
 
+template <typename T, typename L, typename InnerFunc, typename LeafFunc, typename A>
+inline void walk_tree(const tl::Tree<T, L>& tree, const A tree_accumulator, InnerFunc inner_func, LeafFunc leaf_func)
+{
+  // trees of this depth aren't used, so it most likely means bad input data,
+  // e.g. cycles in the forest
+  typedef std::pair<int, decltype(inner_func(tree, 0))> pair_t;
+  std::stack<pair_t> stack;
+  stack.push(pair_t(tree_root(tree), {}));
+  while (!stack.empty()) {
+    const pair_t& pair = stack.top();
+    int node_id        = pair.first;
+    auto descent_accumulator  = pair.second;
+    stack.pop();
+    while (!tree.IsLeaf(node_id)) {
+      auto [left_acc, right_acc] = inner_func({node_id, descent_accumulator, tree_accumulator});
+      stack.push(pair_t(tree.LeftChild(node_id), left_acc));
+      node_id = tree.RightChild(node_id);
+      descent_accumulator = right_acc;
+    }
+    leaf_func({node_id, descent_accumulator, tree_accumulator});
+  }
+}
+
 template <typename T, typename L>
 inline int max_depth(const tl::Tree<T, L>& tree)
 {
@@ -80,6 +103,16 @@ inline int max_depth(const tl::Tree<T, L>& tree)
   std::stack<pair_t> stack;
   stack.push(pair_t(root_index, 0));
   int max_depth = 0;
+  /// node visitor lambdas need this to visit the node
+  struct invitation {
+    int node_id, depth, max_depth;
+  };
+  int lambda_max_depth = walk_tree(tree, 0, [=](invitation inv){
+      ASSERT(inv.depth < DEPTH_LIMIT, "depth limit reached, might be a cycle in the tree");
+      return std::pair(inv.depth + 1, inv.depth + 1);
+    }, [=](invitation inv){
+      inv.max_depth = std::max(inv.max_depth, inv.depth);
+    });
   while (!stack.empty()) {
     const pair_t& pair = stack.top();
     int node_id        = pair.first;
@@ -94,6 +127,7 @@ inline int max_depth(const tl::Tree<T, L>& tree)
     // only need to update depth for leaves
     max_depth = std::max(max_depth, depth);
   }
+  ASSERT(lambda_max_depth == max_depth, "internal error: lambda walk wrong");
   return max_depth;
 }
 
