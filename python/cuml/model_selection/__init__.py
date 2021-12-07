67908fb5b1c24103b6ab9a3b7ebaa691ed1bc6c4
Dante Gama Dessavre <dante.gamadessavre@gmail.com>
2021-12-07 16:37:51 -0600

    FIX Missing whitespace

--- python/cuml/model_selection/__init__.py
+++ python/cuml/model_selection/__init__.py
@@ -21,11 +21,11 @@ if has_sklearn():
     from sklearn.model_selection import GridSearchCV
 
 
-GridSearchCV.__doc__ = """
-This code is developed and maintained by scikit-learn and imported
-by cuML to maintain the familiar sklearn namespace structure.
-cuML includes tests to ensure full compatibility of these wrappers
-with CUDA-based data and cuML estimators, but all of the underlying code
-is due to the scikit-learn developers.\n\n""" + GridSearchCV.__doc__
+    GridSearchCV.__doc__ = """
+    This code is developed and maintained by scikit-learn and imported
+    by cuML to maintain the familiar sklearn namespace structure.
+    cuML includes tests to ensure full compatibility of these wrappers
+    with CUDA-based data and cuML estimators, but all of the underlying code
+    is due to the scikit-learn developers.\n\n""" + GridSearchCV.__doc__
 
 __all__ = ['train_test_split', 'GridSearchCV']
