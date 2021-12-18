81648cb3acd176a7f35a61d01da3db084994e1a5
Levs Dolgovs <36520083+levsnv@users.noreply.github.com>
2021-12-17 23:01:16 -0800

    Unify dense and sparse tests (#4417)
    The pull request includes changes from #4328. To view only changes pertinent to unifying tests, see
    https://github.com/levsnv/cuml/pull/3/files?diff=unified&w=1
    
    Authors:
      - Levs Dolgovs (https://github.com/levsnv)
    
    Approvers:
      - Andy Adinets (https://github.com/canonizer)
      - William Hicks (https://github.com/wphicks)
    
    URL: https://github.com/rapidsai/cuml/pull/4417

--- cpp/src/fil/common.cuh
+++ cpp/src/fil/common.cuh
@@ -28,12 +28,6 @@
 
 #include "internal.cuh"
 
-// needed for node_traits<...>
-namespace treelite {
-template <typename, typename>
-struct ModelImpl;
-}
-
 namespace ML {
 namespace fil {
 
@@ -119,33 +113,6 @@ struct sparse_storage : storage_base {
 
 typedef sparse_storage<sparse_node16> sparse_storage16;
 typedef sparse_storage<sparse_node8> sparse_storage8;
-
-struct dense_forest;
-template <typename node_t>
-struct sparse_forest;
-
-template <typename node_t>
-struct node_traits {
-  using storage              = sparse_storage<node_t>;
-  using forest               = sparse_forest<node_t>;
-  static const bool IS_DENSE = false;
-  static const storage_type_t storage_type_enum =
-    std::is_same<sparse_node16, node_t>() ? SPARSE : SPARSE8;
-  template <typename threshold_t, typename leaf_t>
-  static void check(const treelite::ModelImpl<threshold_t, leaf_t>& model);
-};
-
-template <>
-struct node_traits<dense_node> {
-  using storage                                 = dense_storage;
-  using forest                                  = dense_forest;
-  static const bool IS_DENSE                    = true;
-  static const storage_type_t storage_type_enum = DENSE;
-  template <typename threshold_t, typename leaf_t>
-  static void check(const treelite::ModelImpl<threshold_t, leaf_t>& model)
-  {
-  }
-};
 
 /// all model parameters mostly required to compute shared memory footprint,
 /// also the footprint itself
