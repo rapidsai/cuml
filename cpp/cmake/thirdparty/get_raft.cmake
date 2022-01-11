b89aa6d83f14cfd8c084ca64afad9c600d0092fb
Corey J. Nolet <cjnolet@gmail.com>
2022-01-11 11:17:39 -0500

    Updating tag and copyright years

--- cpp/cmake/thirdparty/get_raft.cmake
+++ cpp/cmake/thirdparty/get_raft.cmake
@@ -1,5 +1,5 @@
 #=============================================================================
-# Copyright (c) 2021, NVIDIA CORPORATION.
+# Copyright (c) 2021-2022, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,8 +30,8 @@ function(find_and_configure_raft)
             BUILD_EXPORT_SET    cuml-exports
             INSTALL_EXPORT_SET  cuml-exports
             CPM_ARGS
-            GIT_REPOSITORY https://github.com/${PKG_FORK}/raft.git
-            GIT_TAG        ${PKG_PINNED_TAG}
+            GIT_REPOSITORY https://github.com/robertmaynard/raft.git
+            GIT_TAG        refactor_cmake_raft_target_logic
             SOURCE_SUBDIR  cpp
             OPTIONS
               "BUILD_TESTS OFF"
@@ -57,6 +57,6 @@ set(CUML_BRANCH_VERSION_raft "${CUML_VERSION_MAJOR}.${CUML_VERSION_MINOR}")
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_raft(VERSION    ${CUML_MIN_VERSION_raft}
-        FORK       rapidsai
-        PINNED_TAG branch-${CUML_BRANCH_VERSION_raft}
+        FORK       robertmaynard
+        PINNED_TAG refactor_cmake_raft_target_logic
         )
