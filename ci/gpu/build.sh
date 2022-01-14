a6d721565bf27078772f136c0a380bb45b25a952
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2022-01-13 17:59:19 -0800

    Use gpuci_conda_retry to remove metapackages

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -64,7 +64,7 @@ gpuci_mamba_retry install -c conda-forge -c rapidsai -c rapidsai-nightly -c nvid
       "rapids-doc-env=${MINOR_VERSION}.*" \
       "shap>=0.37,<=0.39"
 
-gpuci_mamba_retry remove -c conda-forge -c rapidsai -c rapidsai-nightly -c nvidia \
+gpuci_conda_retry remove -c conda-forge -c rapidsai -c rapidsai-nightly -c nvidia \
       --force rapids-build-env rapids-notebook-env
 gpuci_mamba_retry install -c conda-forge -c rapidsai -c rapidsai-nightly -c nvidia \
       treelite=2.2.0
