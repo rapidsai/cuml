88d0e42c4dc93e4e53208b1b669bcc97c8646a8a
Victor Lafargue <viclafargue@nvidia.com>
2022-01-18 16:22:29 +0100

    Remove RAFT memory management (#4468)
    Completes https://github.com/rapidsai/raft/pull/400
    
    Authors:
      - Victor Lafargue (https://github.com/viclafargue)
    
    Approvers:
      - Corey J. Nolet (https://github.com/cjnolet)
    
    URL: https://github.com/rapidsai/cuml/pull/4468

--- cpp/test/sg/sgd.cu
+++ cpp/test/sg/sgd.cu
@@ -18,6 +18,7 @@
 #include <raft/cudart_utils.h>
 #include <raft/linalg/cusolver_wrappers.h>
 #include <raft/matrix/matrix.hpp>
+#include <rmm/device_uvector.hpp>
 #include <solver/sgd.cuh>
 #include <test_utils.h>
 
@@ -38,31 +39,44 @@ struct SgdInputs {
 
 template <typename T>
 class SgdTest : public ::testing::TestWithParam<SgdInputs<T>> {
+ public:
+  SgdTest()
+    : params(::testing::TestWithParam<SgdInputs<T>>::GetParam()),
+      stream(handle.get_stream()),
+      coef(params.n_col, stream),
+      coef_ref(params.n_col, stream),
+      coef2(params.n_col, stream),
+      coef2_ref(params.n_col, stream),
+      pred_log(0, stream),
+      pred_log_ref(0, stream),
+      pred_svm(0, stream),
+      pred_svm_ref(0, stream)
+  {
+    CUDA_CHECK(cudaMemsetAsync(coef.data(), 0, coef.size() * sizeof(T), stream));
+    CUDA_CHECK(cudaMemsetAsync(coef2.data(), 0, coef2.size() * sizeof(T), stream));
+    linearRegressionTest();
+    logisticRegressionTest();
+    svmTest();
+  }
+
  protected:
   void linearRegressionTest()
   {
-    auto stream = handle.get_stream();
-    params      = ::testing::TestWithParam<SgdInputs<T>>::GetParam();
-    int len     = params.n_row * params.n_col;
-
-    raft::allocate(data, len, stream);
-    raft::allocate(labels, params.n_row, stream);
-    raft::allocate(coef, params.n_col, stream, true);
-    raft::allocate(coef2, params.n_col, stream, true);
-    raft::allocate(coef_ref, params.n_col, stream);
-    raft::allocate(coef2_ref, params.n_col, stream);
+    int len = params.n_row * params.n_col;
+    rmm::device_uvector<T> data(len, stream);
+    rmm::device_uvector<T> labels(params.n_row, stream);
 
     T data_h[len] = {1.0, 1.0, 2.0, 2.0, 1.0, 2.0, 2.0, 3.0};
-    raft::update_device(data, data_h, len, stream);
+    raft::update_device(data.data(), data_h, len, stream);
 
     T labels_h[params.n_row] = {6.0, 8.0, 9.0, 11.0};
-    raft::update_device(labels, labels_h, params.n_row, stream);
+    raft::update_device(labels.data(), labels_h, params.n_row, stream);
 
     T coef_ref_h[params.n_col] = {2.087, 2.5454557};
-    raft::update_device(coef_ref, coef_ref_h, params.n_col, stream);
+    raft::update_device(coef_ref.data(), coef_ref_h, params.n_col, stream);
 
     T coef2_ref_h[params.n_col] = {1.000001, 1.9999998};
-    raft::update_device(coef2_ref, coef2_ref_h, params.n_col, stream);
+    raft::update_device(coef2_ref.data(), coef2_ref_h, params.n_col, stream);
 
     bool fit_intercept               = false;
     intercept                        = T(0);
@@ -79,11 +93,11 @@ class SgdTest : public ::testing::TestWithParam<SgdInputs<T>> {
     int n_iter_no_change             = 10;
 
     sgdFit(handle,
-           data,
+           data.data(),
            params.n_row,
            params.n_col,
-           labels,
-           coef,
+           labels.data(),
+           coef.data(),
            &intercept,
            fit_intercept,
            params.batch_size,
@@ -103,11 +117,11 @@ class SgdTest : public ::testing::TestWithParam<SgdInputs<T>> {
     fit_intercept = true;
     intercept2    = T(0);
     sgdFit(handle,
-           data,
+           data.data(),
            params.n_row,
            params.n_col,
-           labels,
-           coef2,
+           labels.data(),
+           coef2.data(),
            &intercept2,
            fit_intercept,
            params.batch_size,
@@ -127,29 +141,26 @@ class SgdTest : public ::testing::TestWithParam<SgdInputs<T>> {
 
   void logisticRegressionTest()
   {
-    auto stream = handle.get_stream();
-    params      = ::testing::TestWithParam<SgdInputs<T>>::GetParam();
-    int len     = params.n_row2 * params.n_col2;
-
-    T* coef_class;
-    raft::allocate(data_logreg, len, stream);
-    raft::allocate(data_logreg_test, len, stream);
-    raft::allocate(labels_logreg, params.n_row2, stream);
-    raft::allocate(coef_class, params.n_col2, stream, true);
-    raft::allocate(pred_log, params.n_row2, stream);
-    raft::allocate(pred_log_ref, params.n_row2, stream);
+    int len = params.n_row2 * params.n_col2;
+    rmm::device_uvector<T> data_logreg(len, stream);
+    rmm::device_uvector<T> data_logreg_test(len, stream);
+    rmm::device_uvector<T> labels_logreg(params.n_row2, stream);
+    rmm::device_uvector<T> coef_class(params.n_row2, stream);
+    pred_log.resize(params.n_row2, stream);
+    pred_log_ref.resize(params.n_row2, stream);
+    CUDA_CHECK(cudaMemsetAsync(coef_class.data(), 0, coef_class.size() * sizeof(T), stream));
 
     T data_h[len] = {0.1, -2.1, 5.4, 5.4, -1.5, -2.15, 2.65, 2.65, 3.25, -0.15, -7.35, -7.35};
-    raft::update_device(data_logreg, data_h, len, stream);
+    raft::update_device(data_logreg.data(), data_h, len, stream);
 
     T data_test_h[len] = {0.3, 1.1, 2.1, -10.1, 0.5, 2.5, -3.55, -20.5, -1.3, 3.0, -5.0, 15.0};
-    raft::update_device(data_logreg_test, data_test_h, len, stream);
+    raft::update_device(data_logreg_test.data(), data_test_h, len, stream);
 
     T labels_logreg_h[params.n_row2] = {0.0, 1.0, 1.0, 0.0};
-    raft::update_device(labels_logreg, labels_logreg_h, params.n_row2, stream);
+    raft::update_device(labels_logreg.data(), labels_logreg_h, params.n_row2, stream);
 
     T pred_log_ref_h[params.n_row2] = {1.0, 0.0, 1.0, 1.0};
-    raft::update_device(pred_log_ref, pred_log_ref_h, params.n_row2, stream);
+    raft::update_device(pred_log_ref.data(), pred_log_ref_h, params.n_row2, stream);
 
     bool fit_intercept               = true;
     T intercept_class                = T(0);
@@ -166,11 +177,11 @@ class SgdTest : public ::testing::TestWithParam<SgdInputs<T>> {
     int n_iter_no_change             = 10;
 
     sgdFit(handle,
-           data_logreg,
+           data_logreg.data(),
            params.n_row2,
            params.n_col2,
-           labels_logreg,
-           coef_class,
+           labels_logreg.data(),
+           coef_class.data(),
            &intercept_class,
            fit_intercept,
            params.batch_size,
@@ -188,43 +199,39 @@ class SgdTest : public ::testing::TestWithParam<SgdInputs<T>> {
            stream);
 
     sgdPredictBinaryClass(handle,
-                          data_logreg_test,
+                          data_logreg_test.data(),
                           params.n_row2,
                           params.n_col2,
-                          coef_class,
+                          coef_class.data(),
                           intercept_class,
-                          pred_log,
+                          pred_log.data(),
                           loss,
                           stream);
-
-    CUDA_CHECK(cudaFree(coef_class));
   }
 
   void svmTest()
   {
-    auto stream = handle.get_stream();
-    params      = ::testing::TestWithParam<SgdInputs<T>>::GetParam();
-    int len     = params.n_row2 * params.n_col2;
+    int len = params.n_row2 * params.n_col2;
 
-    T* coef_class;
-    raft::allocate(data_svmreg, len, stream);
-    raft::allocate(data_svmreg_test, len, stream);
-    raft::allocate(labels_svmreg, params.n_row2, stream);
-    raft::allocate(coef_class, params.n_col2, stream, true);
-    raft::allocate(pred_svm, params.n_row2, stream);
-    raft::allocate(pred_svm_ref, params.n_row2, stream);
+    rmm::device_uvector<T> data_svmreg(len, stream);
+    rmm::device_uvector<T> data_svmreg_test(len, stream);
+    rmm::device_uvector<T> labels_svmreg(params.n_row2, stream);
+    rmm::device_uvector<T> coef_class(params.n_row2, stream);
+    pred_svm.resize(params.n_row2, stream);
+    pred_svm_ref.resize(params.n_row2, stream);
+    CUDA_CHECK(cudaMemsetAsync(coef_class.data(), 0, coef_class.size() * sizeof(T), stream));
 
     T data_h[len] = {0.1, -2.1, 5.4, 5.4, -1.5, -2.15, 2.65, 2.65, 3.25, -0.15, -7.35, -7.35};
-    raft::update_device(data_svmreg, data_h, len, stream);
+    raft::update_device(data_svmreg.data(), data_h, len, stream);
 
     T data_test_h[len] = {0.3, 1.1, 2.1, -10.1, 0.5, 2.5, -3.55, -20.5, -1.3, 3.0, -5.0, 15.0};
-    raft::update_device(data_svmreg_test, data_test_h, len, stream);
+    raft::update_device(data_svmreg_test.data(), data_test_h, len, stream);
 
     T labels_svmreg_h[params.n_row2] = {0.0, 1.0, 1.0, 0.0};
-    raft::update_device(labels_svmreg, labels_svmreg_h, params.n_row2, stream);
+    raft::update_device(labels_svmreg.data(), labels_svmreg_h, params.n_row2, stream);
 
     T pred_svm_ref_h[params.n_row2] = {1.0, 0.0, 1.0, 1.0};
-    raft::update_device(pred_svm_ref, pred_svm_ref_h, params.n_row2, stream);
+    raft::update_device(pred_svm_ref.data(), pred_svm_ref_h, params.n_row2, stream);
 
     bool fit_intercept               = true;
     T intercept_class                = T(0);
@@ -241,11 +248,11 @@ class SgdTest : public ::testing::TestWithParam<SgdInputs<T>> {
     int n_iter_no_change             = 10;
 
     sgdFit(handle,
-           data_svmreg,
+           data_svmreg.data(),
            params.n_row2,
            params.n_col2,
-           labels_svmreg,
-           coef_class,
+           labels_svmreg.data(),
+           coef_class.data(),
            &intercept_class,
            fit_intercept,
            params.batch_size,
@@ -263,54 +270,26 @@ class SgdTest : public ::testing::TestWithParam<SgdInputs<T>> {
            stream);
 
     sgdPredictBinaryClass(handle,
-                          data_svmreg_test,
+                          data_svmreg_test.data(),
                           params.n_row2,
                           params.n_col2,
-                          coef_class,
+                          coef_class.data(),
                           intercept_class,
-                          pred_svm,
+                          pred_svm.data(),
                           loss,
                           stream);
-
-    CUDA_CHECK(cudaFree(coef_class));
-  }
-
-  void SetUp() override
-  {
-    linearRegressionTest();
-    logisticRegressionTest();
-    svmTest();
-  }
-
-  void TearDown() override
-  {
-    CUDA_CHECK(cudaFree(data));
-    CUDA_CHECK(cudaFree(labels));
-    CUDA_CHECK(cudaFree(coef));
-    CUDA_CHECK(cudaFree(coef_ref));
-    CUDA_CHECK(cudaFree(coef2));
-    CUDA_CHECK(cudaFree(coef2_ref));
-    CUDA_CHECK(cudaFree(data_logreg));
-    CUDA_CHECK(cudaFree(data_logreg_test));
-    CUDA_CHECK(cudaFree(labels_logreg));
-    CUDA_CHECK(cudaFree(data_svmreg));
-    CUDA_CHECK(cudaFree(data_svmreg_test));
-    CUDA_CHECK(cudaFree(labels_svmreg));
-    CUDA_CHECK(cudaFree(pred_svm));
-    CUDA_CHECK(cudaFree(pred_svm_ref));
-    CUDA_CHECK(cudaFree(pred_log));
-    CUDA_CHECK(cudaFree(pred_log_ref));
   }
 
  protected:
-  SgdInputs<T> params;
-  T *data, *labels, *coef, *coef_ref;
-  T *coef2, *coef2_ref;
-  T *data_logreg, *data_logreg_test, *labels_logreg;
-  T *data_svmreg, *data_svmreg_test, *labels_svmreg;
-  T *pred_svm, *pred_svm_ref, *pred_log, *pred_log_ref;
-  T intercept, intercept2;
   raft::handle_t handle;
+  cudaStream_t stream = 0;
+
+  SgdInputs<T> params;
+  rmm::device_uvector<T> coef, coef_ref;
+  rmm::device_uvector<T> coef2, coef2_ref;
+  rmm::device_uvector<T> pred_log, pred_log_ref;
+  rmm::device_uvector<T> pred_svm, pred_svm_ref;
+  T intercept, intercept2;
 };
 
 const std::vector<SgdInputs<float>> inputsf2 = {{0.01f, 4, 2, 4, 3, 2}};
@@ -320,35 +299,37 @@ const std::vector<SgdInputs<double>> inputsd2 = {{0.01, 4, 2, 4, 3, 2}};
 typedef SgdTest<float> SgdTestF;
 TEST_P(SgdTestF, Fit)
 {
-  auto stream = handle.get_stream();
   ASSERT_TRUE(raft::devArrMatch(
-    coef_ref, coef, params.n_col, raft::CompareApproxAbs<float>(params.tol), stream));
+    coef_ref.data(), coef.data(), params.n_col, raft::CompareApproxAbs<float>(params.tol)));
 
   ASSERT_TRUE(raft::devArrMatch(
-    coef2_ref, coef2, params.n_col, raft::CompareApproxAbs<float>(params.tol), stream));
+    coef2_ref.data(), coef2.data(), params.n_col, raft::CompareApproxAbs<float>(params.tol)));
 
   ASSERT_TRUE(raft::devArrMatch(
-    pred_log_ref, pred_log, params.n_row, raft::CompareApproxAbs<float>(params.tol), stream));
+    pred_log_ref.data(), pred_log.data(), params.n_row, raft::CompareApproxAbs<float>(params.tol)));
 
   ASSERT_TRUE(raft::devArrMatch(
-    pred_svm_ref, pred_svm, params.n_row, raft::CompareApproxAbs<float>(params.tol), stream));
+    pred_svm_ref.data(), pred_svm.data(), params.n_row, raft::CompareApproxAbs<float>(params.tol)));
 }
 
 typedef SgdTest<double> SgdTestD;
 TEST_P(SgdTestD, Fit)
 {
-  auto stream = handle.get_stream();
   ASSERT_TRUE(raft::devArrMatch(
-    coef_ref, coef, params.n_col, raft::CompareApproxAbs<double>(params.tol), stream));
+    coef_ref.data(), coef.data(), params.n_col, raft::CompareApproxAbs<double>(params.tol)));
 
   ASSERT_TRUE(raft::devArrMatch(
-    coef2_ref, coef2, params.n_col, raft::CompareApproxAbs<double>(params.tol), stream));
+    coef2_ref.data(), coef2.data(), params.n_col, raft::CompareApproxAbs<double>(params.tol)));
 
-  ASSERT_TRUE(raft::devArrMatch(
-    pred_log_ref, pred_log, params.n_row, raft::CompareApproxAbs<double>(params.tol), stream));
+  ASSERT_TRUE(raft::devArrMatch(pred_log_ref.data(),
+                                pred_log.data(),
+                                params.n_row,
+                                raft::CompareApproxAbs<double>(params.tol)));
 
-  ASSERT_TRUE(raft::devArrMatch(
-    pred_svm_ref, pred_svm, params.n_row, raft::CompareApproxAbs<double>(params.tol), stream));
+  ASSERT_TRUE(raft::devArrMatch(pred_svm_ref.data(),
+                                pred_svm.data(),
+                                params.n_row,
+                                raft::CompareApproxAbs<double>(params.tol)));
 }
 
 INSTANTIATE_TEST_CASE_P(SgdTests, SgdTestF, ::testing::ValuesIn(inputsf2));
