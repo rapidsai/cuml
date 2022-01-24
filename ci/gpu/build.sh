2b4c73fe454a899fd2bc33371c0b5a6b5bc8bcb8
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2022-01-24 10:59:09 -0800

    Update build.sh

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -64,13 +64,8 @@ gpuci_mamba_retry install -c conda-forge -c rapidsai -c rapidsai-nightly -c nvid
       "rapids-doc-env=${MINOR_VERSION}.*" \
       "shap>=0.37,<=0.39"
 
-gpuci_conda_retry remove -c conda-forge -c rapidsai -c rapidsai-nightly -c nvidia \
-      --force rapids-build-env rapids-notebook-env
-gpuci_mamba_retry install -c conda-forge -c rapidsai -c rapidsai-nightly -c nvidia \
-      treelite=2.2.1
-
 # https://docs.rapids.ai/maintainers/depmgmt/
-# gpuci_mamba_retry remove --force rapids-build-env rapids-notebook-env
+# gpuci_conda_retry remove --force rapids-build-env rapids-notebook-env
 # gpuci_mamba_retry install -y "your-pkg=1.0.0"
 
 gpuci_logger "Install contextvars if needed"
