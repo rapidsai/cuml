64b552ef2a4f3a69f0ff187e0154347372d33e49
Corey J. Nolet <cjnolet@gmail.com>
2022-02-09 20:13:49 -0500

    using proper raft pi

--- cpp/cmake/thirdparty/get_raft.cmake
+++ cpp/cmake/thirdparty/get_raft.cmake
@@ -58,7 +58,7 @@ set(CUML_BRANCH_VERSION_raft "${CUML_VERSION_MAJOR}.${CUML_VERSION_MINOR}")
 # CPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_raft(VERSION          ${CUML_MIN_VERSION_raft}
                         FORK             cjnolet
-                        PINNED_TAG       imp-2204-moving_remaining_linalg
+                        PINNED_TAG       imp-2204_moving_remaining_linalg
                         USE_RAFT_NN      ${CUML_USE_RAFT_NN}
                         USE_FAISS_STATIC ${CUML_USE_FAISS_STATIC}
                         )
