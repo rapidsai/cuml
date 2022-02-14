e82c25058e7062554980346a1b774c61021b5d12
Corey J. Nolet <cjnolet@gmail.com>
2022-02-14 15:08:11 -0500

    Referencing correct filename

--- cpp/src/hdbscan/detail/reachability_faiss.cuh
+++ cpp/src/hdbscan/detail/reachability_faiss.cuh
@@ -2,7 +2,7 @@
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
- * LICENSE file thirdparty/LICENSE.faiss
+ * LICENSE file thirdparty/LICENSES/LICENSE.faiss
  */
 
 /*
