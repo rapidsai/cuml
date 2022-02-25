328e192ef5f2f056035cee2eaf0f49e0eb5b6306
ayodeawe <jawe@nvidia.com>
2022-02-25 09:24:49 -0600

    rm variables that exist in CI container

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -34,10 +34,6 @@ fi
 # ucx-py version
 export UCX_PY_VERSION='0.25.*'
 
-export CMAKE_CUDA_COMPILER_LAUNCHER="sccache"
-export CMAKE_CXX_COMPILER_LAUNCHER="sccache"
-export CMAKE_C_COMPILER_LAUNCHER="sccache"
-
 ################################################################################
 # SETUP - Check environment
 ################################################################################
