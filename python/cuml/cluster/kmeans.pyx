4391b6c3f09f0df65d9131206364fe2ace7c4616
Mickael Ide <mide@nvidia.com>
2022-03-24 17:32:37 +0100

    Simplify KMeans example doc and dev guide

--- python/cuml/cluster/kmeans.pyx
+++ python/cuml/cluster/kmeans.pyx
@@ -125,24 +125,13 @@ class KMeans(Base,
         >>> # Both import methods supported
         >>> from cuml import KMeans
         >>> from cuml.cluster import KMeans
-        >>>
         >>> import cudf
         >>> import numpy as np
         >>> import pandas as pd
         >>>
-        >>> def np2cudf(df):
-        ...     # convert numpy array to cuDF dataframe
-        ...     df = pd.DataFrame(
-        ...        {'fea%d'%i:df[:,i] for i in range(df.shape[1])})
-        ...     pdf = cudf.DataFrame()
-        ...     for c,column in enumerate(df):
-        ...         pdf[str(c)] = df[column]
-        ...     return pdf
-        ...
-        >>>
         >>> a = np.asarray([[1.0, 1.0], [1.0, 2.0], [3.0, 2.0], [4.0, 3.0]],
         ...                dtype=np.float32)
-        >>> b = np2cudf(a)
+        >>> b = cudf.DataFrame(a)
         >>> # Input:
         >>> b
             0    1
