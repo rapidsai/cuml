55f259ddc420ad8a2a6a77c26fd71b47d6c9da51
ptaylor <paul.e.taylor@me.com>
2022-03-30 14:41:12 -0700

    ensure gputreeshap installs into custom include dir

--- cpp/cmake/thirdparty/get_gputreeshap.cmake
+++ cpp/cmake/thirdparty/get_gputreeshap.cmake
@@ -28,8 +28,19 @@ function(find_and_configure_gputreeshap)
     )
 
     if (GPUTreeShap_ADDED)
+        include(GNUInstallDirs)
         install(TARGETS GPUTreeShap
                 EXPORT  gputreeshap-exports)
+
+        # clear out incorrect INTERFACE_SOURCES
+        set_target_properties(GPUTreeShap PROPERTIES INTERFACE_SOURCES "")
+        get_target_property(all_includes GPUTreeShap INTERFACE_INCLUDE_DIRECTORIES)
+        # clear out incorrect INTERFACE_INCLUDE_DIRECTORIES
+        set_target_properties(GPUTreeShap PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "")
+        # set INTERFACE_INCLUDE_DIRECTORIES appropriately
+        target_include_directories(GPUTreeShap INTERFACE
+            $<BUILD_INTERFACE:${all_includes}>
+            $<INSTALL_INTERFACE:${CMAKE_INCLUDE_DIR}>)
 
         # generate gputreeshap-targets.cmake for install dir
         rapids_export(INSTALL GPUTreeShap
@@ -43,15 +54,6 @@ function(find_and_configure_gputreeshap)
             GLOBAL_TARGETS GPUTreeShap
             NAMESPACE GPUTreeShap::)
 
-        # clear out incorrect INTERFACE_SOURCES
-        set_target_properties(GPUTreeShap PROPERTIES INTERFACE_SOURCES "")
-        get_target_property(all_includes GPUTreeShap INTERFACE_INCLUDE_DIRECTORIES)
-        # clear out incorrect INTERFACE_INCLUDE_DIRECTORIES
-        set_target_properties(GPUTreeShap PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "")
-        # set INTERFACE_INCLUDE_DIRECTORIES appropriately
-        target_include_directories(GPUTreeShap INTERFACE
-            $<BUILD_INTERFACE:${all_includes}>
-            $<INSTALL_INTERFACE:include>)
     endif()
 
     # do `find_dependency(GPUTreeShap) in build and install`
