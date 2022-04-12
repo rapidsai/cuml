696439d1b2acbfed301c402df36d9f0aa396c8cc
Mickael Ide <mide@nvidia.com>
2022-04-13 01:32:17 +0200

    Test kmeans

--- cpp/src/kmeans/kmeans.cu
+++ cpp/src/kmeans/kmeans.cu
@@ -15,6 +15,7 @@
  */
 
 #include "sg_impl.cuh"
+#include <raft/cluster/kmeans.cuh>
 #include <cuml/cluster/kmeans.hpp>
 
 namespace ML {
@@ -32,9 +33,40 @@ void fit_predict(const raft::handle_t& handle,
                  float& inertia,
                  int& n_iter)
 {
-  impl::fit(handle, params, X, n_samples, n_features, sample_weight, centroids, inertia, n_iter);
+  /*impl::fit(handle, params, X, n_samples, n_features, sample_weight, centroids, inertia, n_iter);
   impl::predict(
-    handle, params, centroids, X, n_samples, n_features, sample_weight, true, labels, inertia);
+    handle, params, centroids, X, n_samples, n_features, sample_weight, true, labels, inertia);*/
+  raft::cluster::KMeansParams rParams;
+  rParams.n_clusters = params.n_clusters;
+  rParams.init = raft::cluster::KMeansParams::InitMethod::KMeansPlusPlus;
+  rParams.max_iter = params.max_iter;
+  rParams.tol = params.tol;
+  rParams.verbosity = params.verbosity;
+  rParams.seed = params.seed;
+  rParams.metric = params.metric;
+  rParams.n_init = params.n_init;
+  rParams.oversampling_factor = params.oversampling_factor;
+  rParams.batch_samples = params.batch_samples;
+  rParams.batch_centroids = params.batch_centroids;
+  rParams.inertia_check = params.inertia_check;
+  auto X_view = raft::make_device_matrix_view(const_cast<float*>(X), n_samples, n_features);
+  std::optional<raft::device_vector_view<float>> sw = std::nullopt;
+  if (sample_weight != nullptr)
+    sw = std::make_optional(raft::make_device_vector_view(const_cast<float*>(sample_weight), n_samples));
+  std::optional<raft::device_matrix_view<float>> rCentroids = std::nullopt;
+  if (centroids != nullptr)
+    rCentroids = std::make_optional(raft::make_device_matrix_view(centroids, params.n_clusters, n_features));
+  auto rLabels = raft::make_device_vector_view(labels, n_samples);
+
+  raft::cluster::kmeans_fit_predict<float, int>(handle,
+    rParams,
+    X_view,
+    sw,
+    rCentroids,
+    rLabels,
+    inertia,
+    n_iter);
+  handle.sync_stream(handle.get_stream());
 }
 
 void fit_predict(const raft::handle_t& handle,
@@ -48,9 +80,41 @@ void fit_predict(const raft::handle_t& handle,
                  double& inertia,
                  int& n_iter)
 {
-  impl::fit(handle, params, X, n_samples, n_features, sample_weight, centroids, inertia, n_iter);
+  /*impl::fit(handle, params, X, n_samples, n_features, sample_weight, centroids, inertia, n_iter);
   impl::predict(
-    handle, params, centroids, X, n_samples, n_features, sample_weight, true, labels, inertia);
+    handle, params, centroids, X, n_samples, n_features, sample_weight, true, labels, inertia);*/
+
+  raft::cluster::KMeansParams rParams;
+  rParams.n_clusters = params.n_clusters;
+  rParams.init = raft::cluster::KMeansParams::InitMethod::KMeansPlusPlus;
+  rParams.max_iter = params.max_iter;
+  rParams.tol = params.tol;
+  rParams.verbosity = params.verbosity;
+  rParams.seed = params.seed;
+  rParams.metric = params.metric;
+  rParams.n_init = params.n_init;
+  rParams.oversampling_factor = params.oversampling_factor;
+  rParams.batch_samples = params.batch_samples;
+  rParams.batch_centroids = params.batch_centroids;
+  rParams.inertia_check = params.inertia_check;
+  auto X_view = raft::make_device_matrix_view((double*)X, n_samples, n_features);
+  std::optional<raft::device_vector_view<double>> sw = std::nullopt;
+  if (sample_weight != nullptr)
+    sw = std::make_optional(raft::make_device_vector_view(const_cast<double*>(sample_weight), n_samples));
+  std::optional<raft::device_matrix_view<double>> rCentroids = std::nullopt;
+  if (centroids != nullptr)
+    rCentroids = std::make_optional(raft::make_device_matrix_view(centroids, params.n_clusters, n_features));
+  auto rLabels = raft::make_device_vector_view(labels, n_samples);
+
+  raft::cluster::kmeans_fit_predict<double, int>(handle,
+    rParams,
+    X_view,
+    sw,
+    rCentroids,
+    rLabels,
+    inertia,
+    n_iter);
+  handle.sync_stream(handle.get_stream());
 }
 
 // ----------------------------- fit ---------------------------------//
--- cpp/src/spectral/spectral.cu
+++ cpp/src/spectral/spectral.cu
@@ -16,7 +16,7 @@
 
 #include <raft/sparse/coo.hpp>
 
-#include <raft/sparse/linalg/spectral.hpp>
+//#include <raft/sparse/linalg/spectral.hpp>
 
 namespace raft {
 class handle_t;
@@ -49,7 +49,7 @@ void fit_embedding(const raft::handle_t& handle,
                    float* out,
                    unsigned long long seed)
 {
-  raft::sparse::spectral::fit_embedding(handle, rows, cols, vals, nnz, n, n_components, out, seed);
+  //raft::sparse::spectral::fit_embedding(handle, rows, cols, vals, nnz, n, n_components, out, seed);
 }
 }  // namespace Spectral
 }  // namespace ML
