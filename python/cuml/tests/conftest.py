306032216c9f70907592a5001e06af061dde3175
Jordan Jacobelli <jjacobelli@nvidia.com>
2022-05-09 17:23:43 +0200

    Use conda to build python packages during GPU tests (#4702)
    This PR convert the `from sources` build we are doing in GPU test job to a `conda build`. This is done for the following reasons:
    - This is required step to improve the Ops CI/CD setup to a more convenient pipeline
    - This is required to start using `conda` compilers and `mamba` to build RAPIDS packages
    - This prevent us from manually managing and installing the dependencies in GPU job
    - This ensure the packages can be installed
    - This ensure the tests are running and working against the package content and not the build results. Currently the Python packages are not tested.
    
    This may increase the global pipeline time, but the usage of `mamba` should resolve this as `mamba` is faster than `conda` to build packages
    
    Authors:
      - Jordan Jacobelli (https://github.com/Ethyling)
    
    Approvers:
      - AJ Schmidt (https://github.com/ajschmidt8)
      - Dante Gama Dessavre (https://github.com/dantegd)
    
    URL: https://github.com/rapidsai/cuml/pull/4702

--- python/cuml/tests/conftest.py
+++ python/cuml/tests/conftest.py
@@ -30,6 +30,71 @@ from sklearn.feature_extraction.text import CountVectorizer
 from cuml.testing.utils import create_synthetic_dataset
 
 
+# Add the import here for any plugins that should be loaded EVERY TIME
+pytest_plugins = ("cuml.testing.plugins.quick_run_plugin")
+
+
+def pytest_addoption(parser):
+    # Any custom option, that should be available at any time (not just a
+    # plugin), goes here.
+
+    group = parser.getgroup('cuML Custom Options')
+
+    group.addoption(
+        "--run_stress",
+        action="store_true",
+        default=False,
+        help=("Runs tests marked with 'stress'. These are the most intense "
+              "tests that take the longest to run and are designed to stress "
+              "the hardware's compute resources."))
+
+    group.addoption(
+        "--run_quality",
+        action="store_true",
+        default=False,
+        help=("Runs tests marked with 'quality'. These tests are more "
+              "computationally intense than 'unit', but less than 'stress'"))
+
+    group.addoption(
+        "--run_unit",
+        action="store_true",
+        default=False,
+        help=("Runs tests marked with 'unit'. These are the quickest tests "
+              "that are focused on accuracy and correctness."))
+
+
+def pytest_collection_modifyitems(config, items):
+
+    should_run_quality = config.getoption("--run_quality")
+    should_run_stress = config.getoption("--run_stress")
+
+    # Run unit is implied if no --run_XXX is set
+    should_run_unit = config.getoption("--run_unit") or not (
+        should_run_quality or should_run_stress)
+
+    # Mark the tests as "skip" if needed
+    if not should_run_unit:
+        skip_unit = pytest.mark.skip(
+            reason="Stress tests run with --run_unit flag.")
+        for item in items:
+            if "unit" in item.keywords:
+                item.add_marker(skip_unit)
+
+    if not should_run_quality:
+        skip_quality = pytest.mark.skip(
+            reason="Quality tests run with --run_quality flag.")
+        for item in items:
+            if "quality" in item.keywords:
+                item.add_marker(skip_quality)
+
+    if not should_run_stress:
+        skip_stress = pytest.mark.skip(
+            reason="Stress tests run with --run_stress flag.")
+        for item in items:
+            if "stress" in item.keywords:
+                item.add_marker(skip_stress)
+
+
 def pytest_configure(config):
     cp.cuda.set_allocator(None)
     # max_gpu_memory: Capacity of the GPU memory in GB
