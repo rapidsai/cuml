571b176cbe1cd172ad940abe38c044af65a5c6f7
Raymond Douglass <ray@raydouglass.com>
2022-05-19 11:03:52 -0400

    DOC

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -33,7 +33,7 @@ export MINOR_VERSION=`echo $GIT_DESCRIBE_TAG | grep -o -E '([0-9]+\.[0-9]+)'`
 unset GIT_DESCRIBE_TAG
 
 # ucx-py version
-export UCX_PY_VERSION='0.26.*'
+export UCX_PY_VERSION='0.27.*'
 
 ################################################################################
 # SETUP - Check environment
