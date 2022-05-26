a11afacef216191a637797ac20ace02bae684a0d
GALI PREM SAGAR <sagarprem75@gmail.com>
2022-05-26 18:23:09 -0500

    Apply suggestions from code review

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -121,8 +121,8 @@ if [[ -z "$PROJECT_FLASH" || "$PROJECT_FLASH" == "0" ]]; then
 
     gpuci_logger "Install the main version of dask and distributed"
     set -x
-    pip install "git+https://github.com/dask/distributed.git@2022.05.1" --upgrade --no-deps
-    pip install "git+https://github.com/dask/dask.git@2022.05.1" --upgrade --no-deps
+    pip install "git+https://github.com/dask/distributed.git@2022.05.2" --upgrade --no-deps
+    pip install "git+https://github.com/dask/dask.git@2022.05.2" --upgrade --no-deps
     set +x
 
     gpuci_logger "Python pytest for cuml"
@@ -194,8 +194,8 @@ else
     gpuci_logger "Install the main version of dask, distributed, and dask-glm"
     set -x
 
-    pip install "git+https://github.com/dask/distributed.git@2022.05.1" --upgrade --no-deps
-    pip install "git+https://github.com/dask/dask.git@2022.05.1" --upgrade --no-deps
+    pip install "git+https://github.com/dask/distributed.git@2022.05.2" --upgrade --no-deps
+    pip install "git+https://github.com/dask/dask.git@2022.05.2" --upgrade --no-deps
     pip install "git+https://github.com/dask/dask-glm@main" --force-reinstall --no-deps
     pip install sparse
 
