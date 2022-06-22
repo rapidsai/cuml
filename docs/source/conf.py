142cd3d9777ac3f621cbdc8fdc11977cd3e751f2
GALI PREM SAGAR <sagarprem75@gmail.com>
2022-06-22 17:21:26 -0500

    sphinx fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2018-2021, NVIDIA CORPORATION.
+# Copyright (c) 2018-2022, NVIDIA CORPORATION.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -88,7 +88,7 @@ release = '22.08.00'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
