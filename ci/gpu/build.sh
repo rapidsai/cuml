f345fbda1807e1baad2446740e72f263beaf6360
Corey J. Nolet <cjnolet@gmail.com>
2022-09-20 17:47:07 -0400

    Installing hdbscan in the proper place

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -202,6 +202,7 @@ else
     pip install "git+https://github.com/dask/distributed.git@2022.7.1" --upgrade --no-deps
     pip install "git+https://github.com/dask/dask.git@2022.7.1" --upgrade --no-deps
     pip install "git+https://github.com/dask/dask-glm@main" --force-reinstall --no-deps
+    pip install "git+https://github.com/hdbscan/hdbscan.git@master" --upgrade --no-deps
     pip install sparse
 
     set +x
