889b9db7c5ce7952a0c6d74d15b9a23e065dcffd
Dante Gama Dessavre <danteg@nvidia.com>
2022-11-02 13:52:00 -0500

    ENH Some enhancements

--- python/cmake/ConfigureCythonAlgorithms.cmake
+++ python/cmake/ConfigureCythonAlgorithms.cmake
@@ -16,6 +16,9 @@
 function(add_module_gpu_default FILENAME)
     set (extra_args ${ARGN})
     list(LENGTH extra_args extra_count)
+    if(NOT DEFINED cython_sources)
+        set(cython_sources "")
+    endif()
     if (${extra_count} GREATER 0 OR
         ${CUML_UNIVERSAL} OR
         ${CUML_GPU})
@@ -29,6 +32,9 @@ endfunction()
 function(add_module_universal_default FILENAME)
     set (extra_args ${ARGN})
     list(LENGTH extra_args extra_count)
+    if(NOT DEFINED cython_sources)
+        set(cython_sources "")
+    endif()
     if (${extra_count} GREATER 0 OR
         ${CUML_UNIVERSAL})
       list(APPEND cython_sources
@@ -36,3 +42,4 @@ function(add_module_universal_default FILENAME)
       set (cython_sources ${cython_sources} PARENT_SCOPE)
     endif()
 endfunction()
+
