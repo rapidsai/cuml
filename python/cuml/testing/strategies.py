f2a095388527cbe81fc12548a8b92bc90016b691
Simon Adorf <sadorf@nvidia.com>
2022-11-04 01:51:14 -0700

    Improve customizability of hypothesis regression_datasets() strategy.
    And improvide strategies documentation and extend strategy testing.

--- python/cuml/testing/strategies.py
+++ python/cuml/testing/strategies.py
@@ -14,21 +14,23 @@
 #
 from hypothesis import assume
 from hypothesis.extra.numpy import arrays, floating_dtypes
-from hypothesis.strategies import booleans, composite, floats, integers, just
+from hypothesis.strategies import (composite, floats, integers, just, none,
+                                   one_of)
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 
 
 @composite
-def datasets(
+def standard_datasets(
     draw,
     dtypes=floating_dtypes(),
     n_samples=integers(min_value=0, max_value=200),
     n_features=integers(min_value=0, max_value=200),
+    *,
     n_targets=just(1),
 ):
     """
-    Generic datasets that can serve as an input to an estimator.
+    Returns a strategy to generate standard estimator input datasets.
 
     Parameters
     ----------
@@ -58,56 +60,114 @@ def datasets(
     return draw(X), draw(y)
 
 
+def combined_datasets_strategy(* datasets, name=None, doc=None):
+    """
+    Combine multiple datasets strategies into a single datasets strategy.
+
+    This function will return a new strategy that will build the provided
+    strategy functions with the common parameters (dtypes, n_samples,
+    n_features) and then draw from one of them.
+
+    Parameters:
+    -----------
+    * datasets: list[Callable[[dtypes, n_samples, n_features], SearchStrategy]]
+        A list of functions that return a dataset search strategy when called
+        with the shown arguments.
+    name: The name of the returned search strategy, default="datasets"
+        Defaults to a combination of names of the provided dataset stratgegy
+        functions.
+    doc: The doc-string of the returned search strategy, default=None
+        Defaults to a generic doc-string.
+
+    Returns
+    -------
+    Datasets search strategy: SearchStrategy[array, array]
+    """
+
+    @composite
+    def strategy(
+        draw,
+        dtypes=floating_dtypes(),
+        n_samples=integers(min_value=0, max_value=200),
+        n_features=integers(min_value=0, max_value=200)
+    ):
+        """Datasets strategy composed of multiple datasets strategies."""
+        datasets_strategies = (
+            dataset(dtypes, n_samples, n_features) for dataset in datasets)
+        return draw(one_of(datasets_strategies))
+
+    strategy.__name__ = "datasets" if name is None else name
+    if doc is not None:
+        strategy.__doc__ = doc
+
+    return strategy
+
+
 @composite
 def split_datasets(
     draw,
-    datasets=datasets(n_samples=integers(min_value=10, max_value=200)),
-    train_sizes=floats(min_value=0.1, max_value=1.0, exclude_max=True),
+    datasets,
+    test_sizes=floats(min_value=0.25, max_value=1.0, exclude_max=True),
 ):
     """
     Split a generic search strategy for datasets into test and train subsets.
+
+    The resulting split is guaranteed to have at least one sample in both the
+    train and test split respectively.
 
     Note: This function uses the sklearn.model_selection.train_test_split
     function.
 
     See also:
-    datasets(): A search strategy for datasets that can serve as input to this
-    strategy.
+    standard_datasets(): A search strategy for datasets that can serve as input
+    to this strategy.
 
     Parameters
     ----------
     datasets: SearchStrategy[dataset]
         A search strategy for datasets.
-    train_sizes: SearchStrategy[float]
-        A search strategy for the train size. Must be provided as float and is
+    test_sizes: SearchStrategy[float],
+        default=floats(min_value=0.25, max_value=0.1, exclude_max=True)
+        A search strategy for the test size. Must be provided as float and is
         limited by valid inputs to sklearn's train_test_split() function.
 
     Returns
     -------
-    splitting: list, length=2 * len(arrays)
-        List with a drawn train-test split of the drawn dataset.
+    (X_train, X_test, y_train, y_test): SearchStrategy[4 * array]
+        The train-test split of the input and output samples drawn from
+        the provided datasets search strategy.
     """
     X, y = draw(datasets)
-    ts = draw(train_sizes)
-    assume(int(len(X) * ts) > 0)  # train_test_split limitation
-    return train_test_split(X, y, train_size=ts)
+    test_size = draw(test_sizes)
+    assume(int(len(X) * test_size) > 0)
+    assume(int(len(X) * (1.0 - test_size)) > 0)
+    return train_test_split(X, y, test_size=test_size)
 
 
 @composite
-def regression_datasets(
+def standard_regression_datasets(
     draw,
     dtypes=floating_dtypes(),
-    n_samples=integers(min_value=0, max_value=200),
-    n_features=integers(min_value=0, max_value=200),
-    n_informatives=integers(min_value=0, max_value=200),
-    is_normal=booleans(),
+    n_samples=integers(min_value=0, max_value=100),
+    n_features=integers(min_value=0, max_value=100),
+    *,
+    n_informative=None,
+    n_targets=just(1),
+    bias=just(0.0),
+    effective_rank=none(),
+    tail_strength=just(0.5),
+    noise=just(0.0),
+    shuffle=just(True),
+    coef=just(False),
+    random_state=None,
 ):
     """
-    Generic datasets that can serve as an input to an estimator.
+    Returns a strategy to generate regression problem input datasets.
 
-    See also:
-    datasets(): Generate generic datasets.
-    split_datasets(): Split dataset into test-train subsets.
+    Note:
+    This function uses the sklearn.datasets.make_regression function to
+    generate the regression problem from the provided search strategies.
+
 
     Parameters
     ----------
@@ -117,29 +177,60 @@ def regression_datasets(
         Returned arrays will have number of rows drawn from these values.
     n_features: SearchStrategy[int]
         Returned arrays will have number of columns drawn from these values.
-    n_informatives: SearchStrategy[int]
-        Determines the number of informative features in a normal dataset.
-    is_normal: SearchStrategy[bool]
-        Whether the returned dataset is considered normal or more random.
+    n_informative: SearchStrategy[int], default=None
+        Defaults to value of n_features if None.
+        A search strategy for the number of informative features. This will
+        default to the value provided for n_features.
+    n_targets: SearchStrategy[int], default=just(1)
+        A search strategy for the number of targets, that means the number of
+        columns of the returned y output array.
+    bias: SearchStrategy[float], default=just(0.0)
+        A search strategy for the bias term.
+    effective_rank=none()
+        If not None, a search strategy for the effective rank of the input data
+        for the regression problem. See sklearn.dataset.make_regression() for a
+        detailed explanation of this parameter.
+    tail_strength: SearchStrategy[float], default=just(0.5)
+        See sklearn.dataset.make_regression() for a detailed explanation of
+        this parameter.
+    noise: SearchStrategy[float], default=just(0.0)
+        A search strategy for the standard deviation of the gaussian noise.
+    shuffle: SearchStrategy[bool], default=just(True)
+        A boolean search strategy to determine whether samples and features
+        are shuffled.
+    random_state: int, RandomState instance or None, default=None
+        Pass a random state or integer to determine the random number
+        generation for data set generation.
 
     Returns
     -------
-    (X, y):  tuple(SearchStrategy[array], SearchStrategy[array])
-        A tuple of search strategies for the requested arrays.
+    (X, y):  SearchStrategy[array, array]
+        A search strategy for a tuple of two arrays subject to the
+        provided parameters.
     """
-    if draw(is_normal):
-        dtype_ = draw(dtypes)
-        X, y = make_regression(
-            n_samples=draw(n_samples),
-            n_features=draw(n_features),
-            n_informative=draw(n_informatives),
-        )
-        return X.astype(dtype_), y.astype(dtype_)
-    else:
-        return draw(
-            datasets(
-                dtypes=dtypes,
-                n_samples=n_samples,
-                n_features=n_features,
-            )
-        )
+    X, y = make_regression(
+        n_samples=draw(n_samples),
+        n_features=draw(n_features),
+        n_informative=draw(n_informative or n_features),
+        n_targets=draw(n_targets),
+        bias=draw(bias),
+        effective_rank=draw(effective_rank),
+        tail_strength=draw(tail_strength),
+        noise=draw(noise),
+        shuffle=draw(shuffle),
+        random_state=random_state,
+    )
+    dtype_ = draw(dtypes)
+    return X.astype(dtype_), y.astype(dtype_)
+
+
+regression_datasets = combined_datasets_strategy(
+    standard_datasets, standard_regression_datasets,
+    name="regression_datasets",
+    doc="""
+    Returns strategy for the generation of regression problem datasets.
+
+    Drawn from the standard_datasets and the standard_regression_datasets
+    strategies.
+    """
+)
--- python/cuml/tests/test_strategies.py
+++ python/cuml/tests/test_strategies.py
@@ -13,28 +13,81 @@
 # limitations under the License.
 #
 from cuml.testing.strategies import (
-    datasets,
+    standard_regression_datasets,
     regression_datasets,
     split_datasets,
+    standard_datasets,
 )
 from hypothesis import given
+from hypothesis import strategies as st
+from hypothesis.extra.numpy import floating_dtypes
 
 
-@given(datasets())
-def test_datasets(dataset):
+@given(standard_datasets())
+def test_standard_datasets_default(dataset):
     X, y = dataset
 
     assert X.ndim == 2
-    assert y.ndim in (0, 1, 2)
+    assert X.shape[0] <= 200
+    assert X.shape[1] <= 200
+    assert (y.ndim == 0) or (y.ndim in (1, 2) and y.shape[0] <= 200)
 
 
-@given(split_datasets())
+@given(split_datasets(standard_datasets()))
 def test_split_datasets(split_dataset):
     X_train, X_test, y_train, y_test = split_dataset
 
     assert X_train.ndim == X_test.ndim == 2
+    assert X_train.shape[1] == X_test.shape[1]
+    assert 2 <= (len(X_train) + len(X_test)) <= 200
+
     assert y_train.ndim == y_test.ndim
     assert y_train.ndim in (0, 1, 2)
+    assert (y_train.ndim == 0) or (2 <= (len(y_train) + len(y_test)) <= 200)
+
+
+@given(standard_regression_datasets())
+def test_standard_regression_datasets_default(dataset):
+    X, y = dataset
+    assert X.ndim == 2
+    assert X.shape[0] <= 200
+    assert X.shape[1] <= 200
+    assert (y.ndim == 0) or (y.ndim in (1, 2) and y.shape[0] <= 200)
+    assert X.dtype == y.dtype
+
+
+@given(
+    standard_regression_datasets(
+        dtypes=floating_dtypes(sizes=64),
+        n_samples=st.integers(min_value=2, max_value=200),
+        n_features=st.integers(min_value=1, max_value=200),
+        n_informative=st.just(10),
+        random_state=0,
+    )
+)
+def test_standard_regression_datasets(dataset):
+
+    from sklearn.datasets import make_regression
+
+    X, y = dataset
+    assert X.ndim == 2
+    assert X.shape[0] <= 200
+    assert X.shape[1] <= 200
+    assert (y.ndim == 1 and y.shape[0] <= 200) or y.ndim == 0
+    assert X.dtype == y.dtype
+
+    X_cmp, y_cmp = make_regression(
+        n_samples=X.shape[0], n_features=X.shape[1], random_state=0
+    )
+
+    assert X.dtype.type == X_cmp.dtype.type
+    assert X.ndim == X_cmp.ndim
+    assert X.shape == X_cmp.shape
+    assert y.dtype.type == y_cmp.dtype.type
+    assert y.ndim == y_cmp.ndim
+    assert y.shape == y_cmp.shape
+    assert (X == X_cmp).all()
+    assert (y == y_cmp).all()
 
 
 @given(regression_datasets())
@@ -42,13 +95,16 @@ def test_regression_datasets(dataset):
     X, y = dataset
 
     assert X.ndim == 2
-    assert y.ndim in (0, 1, 2)
+    assert X.shape[0] <= 200
+    assert X.shape[1] <= 200
+    assert (y.ndim == 0) or (y.ndim in (1, 2) and y.shape[0] <= 200)
 
 
-@given(split_datasets(datasets=regression_datasets()))
+@given(split_datasets(regression_datasets()))
 def test_split_regression_datasets(split_dataset):
     X_train, X_test, y_train, y_test = split_dataset
 
     assert X_train.ndim == X_test.ndim == 2
     assert y_train.ndim == y_test.ndim
     assert y_train.ndim in (0, 1, 2)
+    assert 2 <= (len(X_train) + len(X_test)) <= 200
