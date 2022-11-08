d20341a602797b2c684cc0a4c3ac762f15dbb16c
GALI PREM SAGAR <sagarprem75@gmail.com>
2022-11-08 08:22:13 -0600

    Update `dask` nightly install command in CI (#4978)
    This PR updates dask nightly install to correctly install the packages.
    
    Authors:
      - GALI PREM SAGAR (https://github.com/galipremsagar)
    
    Approvers:
      - Peter Andreas Entschev (https://github.com/pentschev)
      - Ray Douglass (https://github.com/raydouglass)
    
    URL: https://github.com/rapidsai/cuml/pull/4978

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -102,8 +102,8 @@ function install_dask {
     gpuci_logger "Install the conda-forge or nightly version of dask and distributed"
     set -x
     if [[ "${INSTALL_DASK_MAIN}" == 1 ]]; then
-        gpuci_logger "gpuci_mamba_retry update dask"
-        gpuci_mamba_retry update dask
+        gpuci_logger "gpuci_mamba_retry install -c dask/label/dev 'dask/label/dev::dask' 'dask/label/dev::distributed'"
+        gpuci_mamba_retry install -c dask/label/dev "dask/label/dev::dask" "dask/label/dev::distributed"
         conda list
     else
         gpuci_logger "gpuci_mamba_retry install conda-forge::dask=={$DASK_STABLE_VERSION} conda-forge::distributed=={$DASK_STABLE_VERSION} conda-forge::dask-core=={$DASK_STABLE_VERSION} --force-reinstall"
