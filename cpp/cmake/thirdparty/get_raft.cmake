e66bfcd4e19ad03d9436a081e645ad5c84f95ee0
Corey J. Nolet <cjnolet@gmail.com>
2022-11-09 11:26:47 -0500

    Updating branch

--- cpp/cmake/thirdparty/get_raft.cmake
+++ cpp/cmake/thirdparty/get_raft.cmake
@@ -81,8 +81,8 @@ endfunction()
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_raft(VERSION          ${CUML_MIN_VERSION_raft}
-                        FORK             wphicks
-                        PINNED_TAG       b6e33dadf3da4dd134dee5b7aac569d4801f7bee
+                        FORK             cjnolet
+                        PINNED_TAG       braft-22.12-test
 
                         # When PINNED_TAG above doesn't match cuml,
                         # force local raft clone in build directory
