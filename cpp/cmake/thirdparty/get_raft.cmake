a406fdcdf50d14059da29e25502a6ba93e511e59
Corey J. Nolet <cjnolet@gmail.com>
2022-11-09 17:10:35 -0500

    Reverting pin

--- cpp/cmake/thirdparty/get_raft.cmake
+++ cpp/cmake/thirdparty/get_raft.cmake
@@ -81,8 +81,8 @@ endfunction()
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_raft(VERSION          ${CUML_MIN_VERSION_raft}
-                        FORK             cjnolet
-                        PINNED_TAG       braft-22.12-test
+                        FORK             rapidsai
+                        PINNED_TAG       ${CUML_BRANCH_VERSION_raft}
 
                         # When PINNED_TAG above doesn't match cuml,
                         # force local raft clone in build directory
