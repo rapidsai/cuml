a054328d3daa885525d8eff75158f3147893a89b
William Hicks <whicks@nvidia.com>
2022-11-16 14:21:53 -0500

    Correct base docstring

--- python/cuml/internals/base.pyx
+++ python/cuml/internals/base.pyx
@@ -122,7 +122,7 @@ class Base(TagsMixin,
         handles in several streams.
         If it is None, a new one is created.
     verbose : int or boolean, default=False
-        Sets logging level. It must be one of `cuml.common.logger.level_*`.
+        Sets logging level. It must be one of `cuml.internals.logger.level_*`.
         See :ref:`verbosity-levels` for more info.
     output_type : {'input', 'array', 'dataframe', 'series', 'df_obj', 'numba', 'cupy', 'numpy', 'cudf', 'pandas'}, default=None
         Return results and set estimator attributes to the indicated output
