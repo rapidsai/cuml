cb9eb2bdae1d1d7f9a35923c43b652da6886117f
Vyas Ramasubramani <vyasr@nvidia.com>
2022-11-17 16:18:25 -0800

    Remove support for linking to treelite in wheel.

--- cpp/cmake/thirdparty/get_treelite.cmake
+++ cpp/cmake/thirdparty/get_treelite.cmake
@@ -14,70 +14,12 @@
 # limitations under the License.
 #=============================================================================
 
-# function(find_treelite_in_python_wheel PKG_VERSION PKG_PINNED_TAG)
-
-#   function(find_treelite_lib _name _dir _bin _src)
-#     if(_dir)
-#       list(APPEND CMAKE_PREFIX_PATH "${_dir}")
-#     endif()
-#     rapids_find_generate_module(
-#       ${_name}
-#       NO_CONFIG
-#       VERSION ${PKG_VERSION}
-#       BUILD_EXPORT_SET cuml-exports
-#       INSTALL_EXPORT_SET cuml-exports
-#       HEADER_NAMES "__init__.py"
-#       LIBRARY_NAMES "${_name}"
-#     )
-#     find_package(${_name} MODULE REQUIRED GLOBAL)
-#     if(NOT TARGET treelite::${_name})
-#       add_library(treelite::${_name} ALIAS ${_name})
-#     endif()
-#     # clear out INTERFACE_INCLUDE_DIRECTORIES
-#     foreach(_target IN ITEMS "${_name}::${_name}")
-#       get_target_property(_val ${_target} INTERFACE_INCLUDE_DIRECTORIES)
-#       set_target_properties(${_target} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "")
-#       message(VERBOSE "find_treelite_lib ${_target} INTERFACE_INCLUDE_DIRECTORIES is '${_val}'")
-#     endforeach()
-#     # Set INTERFACE_INCLUDE_DIRECTORIES to the downloaded binary/source include dirs
-#     target_include_directories(${_name}
-#                      INTERFACE $<BUILD_INTERFACE:${_bin}/include>
-#                                $<BUILD_INTERFACE:${_src}/include>
-#                                $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
-#   endfunction()
-
-#   CPMAddPackage(
-#     NAME           Treelite
-#     VERSION        ${PKG_VERSION}
-#     GIT_REPOSITORY https://github.com/dmlc/treelite.git
-#     GIT_TAG        ${PKG_PINNED_TAG}
-#     DOWNLOAD_ONLY
-#   )
-
-#   install(DIRECTORY ${Treelite_BINARY_DIR}/include/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
-#   install(DIRECTORY ${Treelite_SOURCE_DIR}/include/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
-
-#   find_treelite_lib(treelite
-#     "${CUML_TREELITE_WHEEL_DIR}"
-#     "${Treelite_BINARY_DIR}" "${Treelite_SOURCE_DIR}")
-#   find_treelite_lib(treelite_runtime
-#     "${CUML_TREELITE_RUNTIME_WHEEL_DIR}"
-#     "${Treelite_BINARY_DIR}" "${Treelite_SOURCE_DIR}")
-
-#   set(TREELITE_LIBS treelite::treelite treelite::treelite_runtime PARENT_SCOPE)
-# endfunction()
 
 function(find_and_configure_treelite)
 
     set(oneValueArgs VERSION PINNED_TAG EXCLUDE_FROM_ALL BUILD_STATIC_LIBS)
     cmake_parse_arguments(PKG "${options}" "${oneValueArgs}"
                           "${multiValueArgs}" ${ARGN} )
-
-    # if(CUML_LINK_TO_TREELITE_WHEEL AND (NOT PKG_BUILD_STATIC_LIBS))
-    #   find_treelite_in_python_wheel(${PKG_VERSION} ${PKG_PINNED_TAG})
-    #   set(TREELITE_LIBS ${TREELITE_LIBS} PARENT_SCOPE)
-    #   return()
-    # endif()
 
     if(NOT PKG_BUILD_STATIC_LIBS)
       list(APPEND TREELITE_LIBS treelite::treelite treelite::treelite_runtime)
