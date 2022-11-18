c242eb16e2efa212a580451f3f6f35688a509b39
William Hicks <whicks@nvidia.com>
2022-11-18 10:52:41 -0500

    Correct handling of dataframes in testing

--- python/cuml/testing/utils.py
+++ python/cuml/testing/utils.py
@@ -36,6 +36,7 @@ import cudf
 import cuml
 from cuml.internals.base import Base
 from cuml.internals.input_utils import input_to_cuml_array, is_array_like
+from cuml.internals.mem_type import MemoryType
 import pytest
 
 
@@ -216,9 +217,14 @@ def as_type(type, *args):
             result.append(arg)
         else:
             # make sure X with a single feature remains 2 dimensional
-            if type == 'cudf' and len(arg.shape) > 1:
+            if type in ('cudf', 'pandas', 'df_obj') and len(arg.shape) > 1:
+                if type == 'pandas':
+                    mem_type = MemoryType.host
+                else:
+                    mem_type = None
                 result.append(input_to_cuml_array(
-                    arg).array.to_output('dataframe'))
+                    arg).array.to_output(output_type='dataframe',
+                                         output_mem_type=mem_type))
             else:
                 result.append(input_to_cuml_array(arg).array.to_output(type))
     if len(result) == 1:
