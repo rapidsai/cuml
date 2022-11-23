42574dc8c4ee990c9d2f0c690460a65c6868194a
Vyas Ramasubramani <vyasr@nvidia.com>
2022-11-22 19:09:33 -0800

    Undo unnecessary changes to get_raft.cmake.

--- cpp/cmake/thirdparty/get_raft.cmake
+++ cpp/cmake/thirdparty/get_raft.cmake
@@ -15,25 +15,14 @@
 #=============================================================================
 
 set(CUML_MIN_VERSION_raft "${CUML_VERSION_MAJOR}.${CUML_VERSION_MINOR}.00")
-
-if(NOT DEFINED CUML_RAFT_VERSION)
-  set(CUML_RAFT_VERSION "${CUML_VERSION_MAJOR}.${CUML_VERSION_MINOR}")
-endif()
-
-if(NOT DEFINED CUML_RAFT_BRANCH)
-  set(CUML_RAFT_BRANCH "branch-${CUML_RAFT_VERSION}")
-endif()
-
-if(NOT DEFINED CUML_RAFT_REPOSITORY)
-  set(CUML_RAFT_REPOSITORY "https://github.com/rapidsai/raft.git")
-endif()
+set(CUML_BRANCH_VERSION_raft "${CUML_VERSION_MAJOR}.${CUML_VERSION_MINOR}")
 
 function(find_and_configure_raft)
-    set(oneValueArgs VERSION REPO PINNED_TAG EXCLUDE_FROM_ALL USE_RAFT_DIST USE_RAFT_NN USE_RAFT_STATIC USE_FAISS_STATIC CLONE_ON_PIN NVTX)
+    set(oneValueArgs VERSION FORK PINNED_TAG EXCLUDE_FROM_ALL USE_RAFT_DIST USE_RAFT_NN USE_RAFT_STATIC USE_FAISS_STATIC CLONE_ON_PIN NVTX)
     cmake_parse_arguments(PKG "${options}" "${oneValueArgs}"
             "${multiValueArgs}" ${ARGN} )
 
-    if(PKG_CLONE_ON_PIN AND NOT PKG_PINNED_TAG STREQUAL "branch-${CUML_RAFT_VERSION}")
+    if(PKG_CLONE_ON_PIN AND NOT PKG_PINNED_TAG STREQUAL "branch-${CUML_BRANCH_VERSION_raft }")
       message(STATUS "CUML: RAFT pinned tag found: ${PKG_PINNED_TAG}. Cloning raft locally.")
       set(CPM_DOWNLOAD_raft ON)
     elseif(PKG_USE_RAFT_STATIC AND (NOT CPM_raft_SOURCE))
@@ -59,8 +48,8 @@ function(find_and_configure_raft)
     # the raft-config.cmake re-evaluates the RAFT_NVTX value
     set(RAFT_NVTX ${PKG_NVTX})
 
-    set(RAFT_BUILD_SHARED_LIBS ON)
-    if(PKG_USE_RAFT_STATIC)
+    set(RAFT_BUILD_SHARED_LIBS ${PKG_USE_RAFT_STATIC})
+    if()
       set(RAFT_BUILD_SHARED_LIBS OFF)
     endif()
 
@@ -72,7 +61,7 @@ function(find_and_configure_raft)
       INSTALL_EXPORT_SET  cuml-exports
       COMPONENTS          ${RAFT_COMPONENTS}
       CPM_ARGS
-        GIT_REPOSITORY         ${PKG_REPO}
+        GIT_REPOSITORY         https://github.com/${PKG_FORK}/raft.git
         GIT_TAG                ${PKG_PINNED_TAG}
         SOURCE_SUBDIR          cpp
         EXCLUDE_FROM_ALL       ${PKG_EXCLUDE_FROM_ALL}
@@ -98,8 +87,8 @@ endfunction()
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_raft(VERSION          ${CUML_MIN_VERSION_raft}
-                        REPO             ${CUML_RAFT_REPOSITORY}
-                        PINNED_TAG       ${CUML_RAFT_BRANCH}
+                        FORK             rapidsai
+                        PINNED_TAG       branch-${CUML_BRANCH_VERSION_raft}
                         EXCLUDE_FROM_ALL ${CUML_EXCLUDE_RAFT_FROM_ALL}
                         # When PINNED_TAG above doesn't match cuml,
                         # force local raft clone in build directory
