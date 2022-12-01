5ca47013a3679b34e878a8d193b0eb271d4c97be
Mickael Ide <mide@nvidia.com>
2022-12-01 03:51:28 +0100

    Correct docstring of hashing vectorizer

--- python/cuml/feature_extraction/_vectorizers.py
+++ python/cuml/feature_extraction/_vectorizers.py
@@ -738,22 +738,18 @@ class HashingVectorizer(_VectorizerMixin):
 
     .. code-block:: python
 
-        from cuml.feature_extraction.text import HashingVectorizer
-        corpus = [
-            'This is the first document.',
-            'This document is the second document.',
-            'And this is the third one.',
-            'Is this the first document?',
-        ]
-        vectorizer = HashingVectorizer(n_features=2**4)
-        X = vectorizer.fit_transform(corpus)
-        print(X.shape)
-
-    Output:
-
-    .. code-block:: python
-
-        (4, 16)
+        >>> from cuml.feature_extraction.text import HashingVectorizer
+        >>> import pandas as pd
+        >>> corpus = [
+        >>>     'This is the first document.',
+        >>>     'This document is the second document.',
+        >>>     'And this is the third one.',
+        >>>     'Is this the first document?',
+        >>> ]
+        >>> vectorizer = HashingVectorizer(n_features=2**4)
+        >>> X = vectorizer.fit_transform(pd.Series(corpus))
+        >>> print(X.shape)
+        ... (4, 16)
 
     See Also
     --------
