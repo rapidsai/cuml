9fe3042b779986330b852fa73ba059dbd1f74206
viclafargue <viclafargue@nvidia.com>
2022-12-09 10:53:47 +0100

    Check sklearn presence before importing the Pipeline

--- python/cuml/pipeline/__init__.py
+++ python/cuml/pipeline/__init__.py
@@ -19,14 +19,14 @@ from cuml.common.import_utils import has_sklearn
 if has_sklearn():
     from sklearn.pipeline import Pipeline, make_pipeline
 
-disclaimer = """
-This code is developed and maintained by scikit-learn and imported
-by cuML to maintain the familiar sklearn namespace structure.
-cuML includes tests to ensure full compatibility of these wrappers
-with CUDA-based data and cuML estimators, but all of the underlying code
-is due to the scikit-learn developers.\n\n"""
+    disclaimer = """
+    This code is developed and maintained by scikit-learn and imported
+    by cuML to maintain the familiar sklearn namespace structure.
+    cuML includes tests to ensure full compatibility of these wrappers
+    with CUDA-based data and cuML estimators, but all of the underlying code
+    is due to the scikit-learn developers.\n\n"""
 
-Pipeline.__doc__ = disclaimer + Pipeline.__doc__
-make_pipeline.__doc__ = disclaimer + make_pipeline.__doc__
+    Pipeline.__doc__ = disclaimer + Pipeline.__doc__
+    make_pipeline.__doc__ = disclaimer + make_pipeline.__doc__
 
-__all__ = ['Pipeline', 'make_pipeline']
+    __all__ = ['Pipeline', 'make_pipeline']
