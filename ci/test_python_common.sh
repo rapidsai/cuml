c27cbfb4c6e3ae0d63bf3e9b94fbc51fbbaa2ba7
AJ Schmidt <aschmidt@nvidia.com>
2022-12-19 20:58:32 -0500

    clean up python test scripts
    since the Python tests scripts have been separated and now each only contain a single `pytest` invocation, the error handling bits can be removed entirely

--- ci/test_python_common.sh
+++ ci/test_python_common.sh
@@ -36,5 +36,3 @@ rapids-mamba-retry install \
 
 rapids-logger "Check GPU usage"
 nvidia-smi
-
-set +e
--- ci/test_python_singlegpu.sh
+++ ci/test_python_singlegpu.sh
@@ -16,12 +16,3 @@ pytest \
   --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cuml-coverage.xml" \
   --cov-report=term \
   .
-exitcode=$?
-
-if (( ${exitcode} != 0 )); then
-    SUITEERROR=${exitcode}
-    echo "FAILED: 1 or more tests in cuml"
-fi
-popd
-
-exit ${SUITEERROR}
