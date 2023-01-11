a28513cfe3c208b7f968cb989e070d57a2d91d39
Bradley Dice <bdice@bradleydice.com>
2023-01-11 13:02:14 -0600

    Remove outdated macOS deployment target from build script. (#5125)
    Small cleanup to remove `MACOSX_DEPLOYMENT_TARGET` references from a build script (macOS is not supported by RAPIDS).
    
    Authors:
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - Dante Gama Dessavre (https://github.com/dantegd)
      - AJ Schmidt (https://github.com/ajschmidt8)
    
    URL: https://github.com/rapidsai/cuml/pull/5125

--- conda/recipes/libcuml/build.sh
+++ conda/recipes/libcuml/build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 # Copyright (c) 2018-2022, NVIDIA CORPORATION.
 
-if [ -n "$MACOSX_DEPLOYMENT_TARGET" ]; then
-    # C++11 requires 10.9
-    # but cudatoolkit 8 is build for 10.11
-    export MACOSX_DEPLOYMENT_TARGET=10.11
-fi
-
 ./build.sh -n libcuml prims -v --allgpuarch
