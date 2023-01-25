f03f176781ef8a36de7647cbaf5cf6ec4932fc77
Ajay Thorve <ajaythorve1994@gmail.com>
2023-01-24 17:38:22 -0800

    add -W flag to pydocs build

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -34,8 +34,8 @@ popd
 # Build Python docs
 rapids-logger "Build Python docs"
 pushd docs
-sphinx-build -b dirhtml ./source _html
-sphinx-build -b text ./source _text
+sphinx-build -b dirhtml ./source _html -W
+sphinx-build -b text ./source _text -W
 popd
 
 if [[ "${RAPIDS_BUILD_TYPE}" == "branch" ]]; then
