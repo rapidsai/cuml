5a396fda583c0af15bf580afe8df106fb8fbce39
AJ Schmidt <aschmidt@nvidia.com>
2023-01-25 17:29:43 -0500

    fix unrelated sed command

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -60,7 +60,7 @@ for FILE in dependencies.yaml conda/environments/*.yaml; do
    sed_runner "s/rapids-doc-env=${CURRENT_SHORT_TAG}/rapids-doc-env=${NEXT_SHORT_TAG}/g" ${FILE};
 done
 
-sed_runner "s|/branch-.*/|/branch-${NEXT_SHORT_TAG}/|g" python/README.md
+sed_runner "s|/branch-.*?/|/branch-${NEXT_SHORT_TAG}/|g" python/README.md
 
 # Wheel builds clone cumlprims_mg, update its branch
 sed_runner "s/extra-repo-sha: branch-.*/extra-repo-sha: branch-${NEXT_SHORT_TAG}/g" .github/workflows/*.yaml
