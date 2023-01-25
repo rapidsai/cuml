99268bbaa1cedab419e06068cfe6b1f164383900
Nick Becker <nickb500@gmail.com>
2023-01-25 15:24:50 -0500

    re-add svc(linear) and tweak imports in the runner to use the local version

--- python/cuml/benchmark/run_benchmarks.py
+++ python/cuml/benchmark/run_benchmarks.py
@@ -16,7 +16,8 @@
 """Command-line ML benchmark runner"""
 
 import json
-from cuml.benchmark import algorithms, datagen, runners
+# from cuml.benchmark import algorithms, datagen, runners
+import algorithms, datagen, runners
 from cuml.internals.safe_imports import cpu_only_import
 np = cpu_only_import('numpy')
 
