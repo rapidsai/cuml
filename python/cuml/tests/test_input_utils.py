f6d72fc3d3ed10511908b63759566b14cf0d2b29
GALI PREM SAGAR <sagarprem75@gmail.com>
2023-01-26 10:06:41 -0600

    [REVIEW] Adapt to changes in `cudf.core.buffer.Buffer` (#5154)
    This PR adapts to the breaking changes being introduced in: https://github.com/rapidsai/cudf/pull/12587
    
    Authors:
       - GALI PREM SAGAR (https://github.com/galipremsagar)
    
    Approvers:
       - Lawrence Mitchell (https://github.com/wence-)
       - AJ Schmidt (https://github.com/ajschmidt8)
       - Dante Gama Dessavre (https://github.com/dantegd)

--- python/cuml/tests/test_input_utils.py
+++ python/cuml/tests/test_input_utils.py
@@ -333,7 +333,11 @@ def check_numpy_order(ary, order):
 def check_ptr(a, b, input_type):
     if input_type == 'cudf':
         for (_, col_a), (_, col_b) in zip(a._data.items(), b._data.items()):
-            assert col_a.base_data.ptr == col_b.base_data.ptr
+            with cudf.core.buffer.acquire_spill_lock():
+                assert (
+                    col_a.base_data.get_ptr(mode="read") ==
+                    col_b.base_data.get_ptr(mode="read")
+                )
     else:
         def get_ptr(x):
             try:
