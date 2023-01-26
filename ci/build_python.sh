be7446f1a78177262e953c795dfe9292c73b01b3
galipremsagar <sagarprem75@gmail.com>
2023-01-25 19:55:52 -0800

    revert temporary changes

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -12,18 +12,12 @@ rapids-print-env
 rapids-logger "Begin py build"
 
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
-PY_VER=${RAPIDS_PY_VERSION//./}
-LIBCUDF_CHANNEL=$(rapids-get-artifact ci/cudf/pull-request/12587/046025a/cudf_conda_cpp_cuda11_$(arch).tar.gz)
-CUDF_CHANNEL=$(rapids-get-artifact ci/cudf/pull-request/12587/046025a/cudf_conda_python_cuda11_${PY_VER}_$(arch).tar.gz)
-
 
 # TODO: Remove `--no-test` flag once importing on a CPU
 # node works correctly
 rapids-mamba-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
-  --channel "${LIBCUDF_CHANNEL}" \
-  --channel "${CUDF_CHANNEL}" \
   conda/recipes/cuml
 
 rapids-upload-conda-to-s3 python
--- ci/test_python_common.sh
+++ ci/test_python_common.sh
@@ -21,9 +21,6 @@ set -u
 rapids-logger "Downloading artifacts from previous jobs"
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 PYTHON_CHANNEL=$(rapids-download-conda-from-s3 python)
-PY_VER=${RAPIDS_PY_VERSION//./}
-LIBCUDF_CHANNEL=$(rapids-get-artifact ci/cudf/pull-request/12587/046025a/cudf_conda_cpp_cuda11_$(arch).tar.gz)
-CUDF_CHANNEL=$(rapids-get-artifact ci/cudf/pull-request/12587/046025a/cudf_conda_python_cuda11_${PY_VER}_$(arch).tar.gz)
 
 RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${PWD}/test-results"}
 RAPIDS_COVERAGE_DIR=${RAPIDS_COVERAGE_DIR:-"${PWD}/coverage-results"}
@@ -34,9 +31,7 @@ rapids-print-env
 rapids-mamba-retry install \
   --channel "${CPP_CHANNEL}" \
   --channel "${PYTHON_CHANNEL}" \
-  --channel "${LIBCUDF_CHANNEL}" \
-  --channel "${CUDF_CHANNEL}" \
-  libcuml cuml libcudf cudf dask-cudf
+  libcuml cuml
 
 rapids-logger "Check GPU usage"
 nvidia-smi
