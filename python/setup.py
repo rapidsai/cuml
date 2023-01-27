e90b05386d4e560688b06871d188e96c08ce02ca
Dante Gama Dessavre <dante.gamadessavre@gmail.com>
2023-01-26 23:10:38 -0600

    UPD Update scikit-learn version in setup.py for wheels

--- python/setup.py
+++ python/setup.py
@@ -164,7 +164,7 @@ setup(name=f'cuml{cuda_suffix}',
               "numpydoc",
               "umap-learn",
               "statsmodels",
-              "scikit-learn==0.24",
+              "scikit-learn==1.2",
               "hdbscan @ git+https://github.com/scikit-learn-contrib/hdbscan.git@master",  # noqa:E501
               "dask-glm @ git+https://github.com/dask/dask-glm@main",
               "dask-cuda"
