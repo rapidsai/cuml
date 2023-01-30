46fc6a69b8fed41c55a263cae320818ef27bb25e
Corey J. Nolet <cjnolet@gmail.com>
2023-01-30 17:41:53 -0500

    Fixing typo

--- cpp/cmake/thirdparty/get_raft.cmake
+++ cpp/cmake/thirdparty/get_raft.cmake
@@ -92,7 +92,7 @@ endfunction()
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_raft(VERSION          ${CUML_MIN_VERSION_raft}
-                        FORK             rapidsi
+                        FORK             rapidsai
 			PINNED_TAG       ${CUML_BRANCH_VERSION_raft}
                         EXCLUDE_FROM_ALL ${CUML_EXCLUDE_RAFT_FROM_ALL}
                         # When PINNED_TAG above doesn't match cuml,
