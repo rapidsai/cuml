82d8a527b4cf7618a1c8f44b14a07aa8d3e46a1e
Dante Gama Dessavre <danteg@nvidia.com>
2023-01-30 20:27:32 -0600

    ENH Simplify CI script

--- ci/build_test_python_cpu.sh
+++ ci/build_test_python_cpu.sh
@@ -17,13 +17,6 @@ rapids-mamba-retry mambabuild \
   --no-test \
   conda/recipes/cuml-cpu
 
-rapids-mamba-retry create -n test python=${RAPIDS_PY_VERSION}
-
-# Temporarily allow unbound variables for conda activation.
-set +u
-conda activate test
-set -u
-
 # todo: use dependenncies file
 rapids-mamba-retry install "scikit-learn=1.2" "hdbscan=0.8.29" "umap-learn=0.5.3" "nvtx"
 
