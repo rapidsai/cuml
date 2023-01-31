9ad324e8f67109e51d4f3768909b57375fe5d767
Simon Adorf <sadorf@nvidia.com>
2023-01-31 00:54:28 -0800

    Run pre-commit checks after downloading cmake format file.

--- ci/check_style.sh
+++ ci/check_style.sh
@@ -14,15 +14,15 @@ rapids-dependency-file-generator \
 rapids-mamba-retry env create --force -f env.yaml -n checks
 conda activate checks
 
-# Run pre-commit checks
-pre-commit run --hook-stage manual --all-files --show-diff-on-failure
-
-set +e
-
 FORMAT_FILE_URL=https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-23.02/cmake-format-rapids-cmake.json
 export RAPIDS_CMAKE_FORMAT_FILE=/tmp/rapids_cmake_ci/cmake-formats-rapids-cmake.json
 mkdir -p $(dirname ${RAPIDS_CMAKE_FORMAT_FILE})
 wget -O ${RAPIDS_CMAKE_FORMAT_FILE} ${FORMAT_FILE_URL}
+
+# Run pre-commit checks
+pre-commit run --hook-stage manual --all-files --show-diff-on-failure
+
+set +e
 
 # clang-tidy check
 # NOTE:
