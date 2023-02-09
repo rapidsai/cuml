0e1ac45450aa8262c8c2bde049bf1ae6d271a0cb
Simon Adorf <sadorf@nvidia.com>
2023-02-09 01:33:42 -0800

    Consolidate internals namespace imports.

--- python/cuml/internals/__init__.py
+++ python/cuml/internals/__init__.py
@@ -21,20 +21,18 @@ from cuml.internals.base_helpers import (
 from cuml.internals.api_decorators import (
     _deprecate_pos_args,
     api_base_fit_transform,
-    api_base_return_any_skipall,
     api_base_return_any,
-    api_base_return_array_skipall,
+    api_base_return_any_skipall,
     api_base_return_array,
-    api_base_return_generic_skipall,
+    api_base_return_array_skipall,
     api_base_return_generic,
+    api_base_return_generic_skipall,
     api_base_return_sparse_array,
     api_return_any,
     api_return_array,
     api_return_generic,
     api_return_sparse_array,
     exit_internal_api,
-)
-from cuml.internals.api_decorators import (
     in_internal_api,
     set_api_output_dtype,
     set_api_output_type,
