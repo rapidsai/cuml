ad44b8fa53c6be3bf81f8fec90952f1ec0f95da8
Simon Adorf <sadorf@nvidia.com>
2023-02-13 08:32:42 -0800

    Move the clang-tidy check into custom job.
    Since the pre-commit checks do not require a build are much faster to
    execute.

--- ci/check_style.sh
+++ ci/check_style.sh
@@ -14,47 +14,5 @@ rapids-dependency-file-generator \
 rapids-mamba-retry env create --force -f env.yaml -n checks
 conda activate checks
 
-FORMAT_FILE_URL=https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-23.02/cmake-format-rapids-cmake.json
-export RAPIDS_CMAKE_FORMAT_FILE=/tmp/rapids_cmake_ci/cmake-formats-rapids-cmake.json
-mkdir -p $(dirname ${RAPIDS_CMAKE_FORMAT_FILE})
-wget -O ${RAPIDS_CMAKE_FORMAT_FILE} ${FORMAT_FILE_URL}
-
 # Run pre-commit checks
 pre-commit run --all-files --show-diff-on-failure
-
-set +e
-
-# clang-tidy check
-# NOTE:
-#   explicitly pass GPU_ARCHS flag to avoid having to evaluate gpu archs
-# because there's no GPU on the CI machine where this script runs!
-# NOTE:
-#   also, sync all dependencies as they'll be needed by clang-tidy to find
-# relevant headers
-function setup_and_run_clang_tidy() {
-    local LD_LIBRARY_PATH_CACHED=$LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-    mkdir cpp/build && \
-        cd cpp/build && \
-        cmake -DGPU_ARCHS=70 \
-              -DBLAS_LIBRARIES=${CONDA_PREFIX}/lib/libopenblas.so.0 \
-              .. && \
-        make treelite && \
-        cd ../.. && \
-        python cpp/scripts/run-clang-tidy.py
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH_CACHED
-}
-TIDY=$(setup_and_run_clang_tidy 2>&1)
-TIDY_RETVAL=$?
-
-# Output results if failure otherwise show pass
-if [ "$TIDY_RETVAL" != "0" ]; then
-  echo -e "\n\n>>>> FAILED: clang tidy check; begin output\n\n"
-  echo -e "$TIDY"
-  echo -e "\n\n>>>> FAILED: clang tidy check; end output\n\n"
-else
-  echo -e "\n\n>>>> PASSED: clang tidy check\n\n"
-fi
-
-
-exit $TIDY_RETVAL
