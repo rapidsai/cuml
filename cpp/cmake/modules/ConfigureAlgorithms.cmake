4f668677ca9493b462121f8290806a772fb5bb98
Corey J. Nolet <cjnolet@gmail.com>
2023-03-14 17:52:03 -0400

    Tests seem to pass w/ the new compiled bits

--- cpp/cmake/modules/ConfigureAlgorithms.cmake
+++ cpp/cmake/modules/ConfigureAlgorithms.cmake
@@ -1,5 +1,5 @@
 #=============================================================================
-# Copyright (c) 2022, NVIDIA CORPORATION.
+# Copyright (c) 2022-2023, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,8 +16,7 @@
 
 
 if(CUML_ALGORITHMS STREQUAL "ALL")
-    set(CUML_USE_RAFT_NN ON)
-    set(CUML_USE_RAFT_DIST ON)
+    set(CUML_RAFT_COMPILED ON)
     set(LINK_TREELITE ON)
     set(LINK_CUFFT ON)
     set(all_algo ON)
@@ -31,8 +30,7 @@ else()
     set(BUILD_CUML_C_LIBRARY OFF)
     set(BUILD_CUML_BENCH OFF)
     set(BUILD_CUML_EXAMPLES OFF)
-    set(CUML_USE_RAFT_NN OFF)
-    set(CUML_USE_RAFT_DIST OFF)
+    set(CUML_RAFT_COMPILED OFF)
 
     foreach(algo ${CUML_ALGORITHMS})
       string(TOLOWER ${algo} lower_algo)
@@ -106,7 +104,7 @@ else()
     endif()
 
     if(knn_algo)
-        set(CUML_USE_RAFT_NN ON)
+        set(CUML_RAFT_COMPILED ON)
     endif()
 
     if(randomforest_algo)
@@ -119,7 +117,7 @@ else()
     endif()
 
     if(metrics_algo)
-      set(CUML_USE_RAFT_DIST ON)
+        set(CUML_RAFT_COMPILED ON)
     endif()
 endif()
 
--- cpp/cmake/thirdparty/get_raft.cmake
+++ cpp/cmake/thirdparty/get_raft.cmake
@@ -1,5 +1,5 @@
 #=============================================================================
-# Copyright (c) 2021-2022, NVIDIA CORPORATION.
+# Copyright (c) 2021-2023, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ set(CUML_MIN_VERSION_raft "${CUML_VERSION_MAJOR}.${CUML_VERSION_MINOR}.00")
 set(CUML_BRANCH_VERSION_raft "${CUML_VERSION_MAJOR}.${CUML_VERSION_MINOR}")
 
 function(find_and_configure_raft)
-    set(oneValueArgs VERSION FORK PINNED_TAG EXCLUDE_FROM_ALL USE_RAFT_DIST USE_RAFT_NN USE_RAFT_STATIC USE_FAISS_STATIC CLONE_ON_PIN NVTX)
+	set(oneValueArgs VERSION FORK PINNED_TAG EXCLUDE_FROM_ALL USE_RAFT_STATIC COMPILE_LIBRARY CLONE_ON_PIN NVTX)
     cmake_parse_arguments(PKG "${options}" "${oneValueArgs}"
             "${multiValueArgs}" ${ARGN} )
 
@@ -30,33 +30,22 @@ function(find_and_configure_raft)
       set(CPM_DOWNLOAD_raft ON)
     endif()
 
-    if(PKG_USE_RAFT_DIST)
-      string(APPEND RAFT_COMPONENTS "distance")
-    endif()
-
-    if(PKG_USE_RAFT_NN)
-      string(APPEND RAFT_COMPONENTS " nn")
-    endif()
 
     # We need RAFT::distributed for MG tests
     if(BUILD_CUML_MG_TESTS)
       string(APPEND RAFT_COMPONENTS " distributed")
     endif()
 
-    if(PKG_USE_RAFT_DIST AND PKG_USE_RAFT_NN)
-      set(RAFT_COMPILE_LIBRARIES ON)
+    if(PKG_COMPILE_LIBRARY AND NOT PKG_USE_RAFT_STATIC)
+      string(APPEND RAFT_COMPONENTS " compiled")
+      set(RAFT_COMPILE_LIBRARY ON)
     else()
-      set(RAFT_COMPILE_LIBRARIES OFF)
+      set(RAFT_COMPILE_LIBRARY OFF)
     endif()
 
     # We need to set this each time so that on subsequent calls to cmake
     # the raft-config.cmake re-evaluates the RAFT_NVTX value
     set(RAFT_NVTX ${PKG_NVTX})
-
-    set(RAFT_BUILD_SHARED_LIBS ON)
-    if(${PKG_USE_RAFT_STATIC})
-      set(RAFT_BUILD_SHARED_LIBS OFF)
-    endif()
 
     message(VERBOSE "CUML: raft FIND_PACKAGE_ARGUMENTS COMPONENTS ${RAFT_COMPONENTS}")
 
@@ -72,10 +61,7 @@ function(find_and_configure_raft)
         EXCLUDE_FROM_ALL       ${PKG_EXCLUDE_FROM_ALL}
         OPTIONS
           "BUILD_TESTS OFF"
-          "BUILD_SHARED_LIBS ${RAFT_BUILD_SHARED_LIBS}"
-          "RAFT_COMPILE_LIBRARIES ${RAFT_COMPILE_LIBRARIES}"
-          "RAFT_COMPILE_NN_LIBRARY ${PKG_USE_RAFT_NN}"
-          "RAFT_COMPILE_DIST_LIBRARY ${PKG_USE_RAFT_DIST}"
+          "RAFT_COMPILE_LIBRARY ${RAFT_COMPILE_LIBRARIES}"
           "RAFT_USE_FAISS_STATIC ${PKG_USE_FAISS_STATIC}"
     )
 
@@ -92,16 +78,14 @@ endfunction()
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_raft(VERSION          ${CUML_MIN_VERSION_raft}
-                        FORK             rapidsai
-                        PINNED_TAG       branch-${CUML_BRANCH_VERSION_raft}
+                        FORK             cjnolet
+                        PINNED_TAG       fea-2304-remove-nn-lib
                         EXCLUDE_FROM_ALL ${CUML_EXCLUDE_RAFT_FROM_ALL}
                         # When PINNED_TAG above doesn't match cuml,
                         # force local raft clone in build directory
                         # even if it's already installed.
                         CLONE_ON_PIN     ${CUML_RAFT_CLONE_ON_PIN}
-                        USE_RAFT_NN      ${CUML_USE_RAFT_NN}
-                        USE_RAFT_DIST    ${CUML_USE_RAFT_DIST}
+			COMPILE_LIBRARY  ON
                         USE_RAFT_STATIC  ${CUML_USE_RAFT_STATIC}
-                        USE_FAISS_STATIC ${CUML_USE_FAISS_STATIC}
                         NVTX             ${NVTX}
                         )
