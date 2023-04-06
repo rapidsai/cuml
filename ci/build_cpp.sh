42c530e22a3e7490f0a057a0b140c835e949db8d
Vyas Ramasubramani <vyasr@nvidia.com>
2023-04-06 09:24:45 -0700

    Pull raft and culmprims from CUDA 12 PRs

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -9,11 +9,13 @@ export CMAKE_GENERATOR=Ninja
 
 RAPIDS_CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
 LIBRMM_CHANNEL=$(rapids-get-artifact ci/rmm/pull-request/1223/72e0c74/rmm_conda_cpp_cuda${RAPIDS_CUDA_MAJOR}_$(arch).tar.gz)
+LIBRAFT_CHANNEL=$(rapids-get-artifact ci/raft/pull-request/1388/f1f61a8/raft_conda_cpp_cuda${RAPIDS_CUDA_MAJOR}_$(arch).tar.gz)
+LIBCUMLPRIMS_CHANNEL=$(rapids-get-artifact ci/cumlprims_mg/pull-request/129/93c1ffe/cumlprims_mg_conda_cpp_cuda${RAPIDS_CUDA_MAJOR}_$(arch).tar.gz)
 
 rapids-print-env
 
 rapids-logger "Begin cpp build"
 
-rapids-mamba-retry mambabuild --channel "${LIBRMM_CHANNEL}" conda/recipes/libcuml
+rapids-mamba-retry mambabuild --channel "${LIBRMM_CHANNEL}" --channel "${LIBRAFT_CHANNEL}" --channel "${LIBCUMLPRIMS_CHANNEL}" conda/recipes/libcuml
 
 rapids-upload-conda-to-s3 cpp
