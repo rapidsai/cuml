347195b6616a6f7c2c930b1fe9fcb66daf060a03
Dante Gama Dessavre <danteg@nvidia.com>
2023-04-12 21:30:34 -0500

    UPD Update RMM hash

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -8,7 +8,7 @@ source rapids-env-update
 export CMAKE_GENERATOR=Ninja
 
 RAPIDS_CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
-LIBRMM_CHANNEL=$(rapids-get-artifact ci/rmm/pull-request/1223/72e0c74/rmm_conda_cpp_cuda${RAPIDS_CUDA_MAJOR}_$(arch).tar.gz)
+LIBRMM_CHANNEL=$(rapids-get-artifact ci/rmm/pull-request/1223/042a67e/rmm_conda_cpp_cuda${RAPIDS_CUDA_MAJOR}_$(arch).tar.gz)
 LIBRAFT_CHANNEL=$(rapids-get-artifact ci/raft/pull-request/1388/7bddaee/raft_conda_cpp_cuda${RAPIDS_CUDA_MAJOR}_$(arch).tar.gz)
 LIBCUMLPRIMS_CHANNEL=$(rapids-get-artifact ci/cumlprims_mg/pull-request/129/85effb7/cumlprims_mg_conda_cpp_cuda${RAPIDS_CUDA_MAJOR}_$(arch).tar.gz)
 
