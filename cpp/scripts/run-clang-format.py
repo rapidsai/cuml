7e96fe9b71d1a7744cb9f1daa9daed3c0ffd35bc
Bradley Dice <bdice@bradleydice.com>
2023-04-13 11:50:46 -0500

    Update specific references in cuml docs and scripts.

--- cpp/scripts/run-clang-format.py
+++ cpp/scripts/run-clang-format.py
@@ -23,7 +23,7 @@ import tempfile
 import shutil
 
 
-EXPECTED_VERSION = "11.1.0"
+EXPECTED_VERSION = "16.0.1"
 VERSION_REGEX = re.compile(r"clang-format version ([0-9.]+)")
 # NOTE: populate this list with more top-level dirs as we add more of them to
 #       to the cuml repo
