92efbe544c19203ea8e81b839e94203dc7a69d7a
Allard Hendriksen <ahendriksen@nvidia.com>
2023-04-17 12:08:37 +0200

    Use RAFT artifacts deployed from PR 1415

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -11,6 +11,10 @@ rapids-print-env
 
 rapids-logger "Begin cpp build"
 
+LIBRAFT_CHANNEL=$(rapids-get-artifact ci/raft/pull-request/1415/7203c76/raft_conda_cpp_cuda11_$(arch).tar.gz)
+
+rapids-mamba-retry mambabuild --channel "${LIBRAFT_CHANNEL}" conda/recipes/libcuml
+
 rapids-mamba-retry mambabuild conda/recipes/libcuml
 
 rapids-upload-conda-to-s3 cpp
--- ci/build_python.sh
+++ ci/build_python.sh
@@ -13,11 +13,18 @@ rapids-logger "Begin py build"
 
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 
+PY_VER=${RAPIDS_PY_VERSION//./}
+
+LIBRAFT_CHANNEL=$(rapids-get-artifact ci/raft/pull-request/1415/7203c76/raft_conda_cpp_cuda11_$(arch).tar.gz)
+RAFT_CHANNEL=$(rapids-get-artifact ci/rmm/pull-request/1415/7203c76/raft_conda_python_cuda11_${PY_VER}_$(arch).tar.gz)
+
 # TODO: Remove `--no-test` flag once importing on a CPU
 # node works correctly
 rapids-mamba-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
+  --channel "${LIBRAFT_CHANNEL}" \
+  --channel "${RAFT_CHANNEL}" \
   conda/recipes/cuml
 
 rapids-upload-conda-to-s3 python
