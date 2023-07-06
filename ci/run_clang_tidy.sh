03580f2d94309d95ded40adb11ab734add6667c0
Simon Adorf <sadorf@nvidia.com>
2023-07-06 14:18:51 -0700

    Remove obsolete code from run_clang_tidy.sh script.

--- ci/run_clang_tidy.sh
+++ ci/run_clang_tidy.sh
@@ -15,11 +15,8 @@ rapids-mamba-retry env create --force -f env.yaml -n clang_tidy
 # Temporarily allow unbound variables for conda activation.
 set +u && conda activate clang_tidy && set -u
 
-FORMAT_FILE_URL=https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-23.08/cmake-format-rapids-cmake.json
-export RAPIDS_CMAKE_FORMAT_FILE=/tmp/rapids_cmake_ci/cmake-formats-rapids-cmake.json
-mkdir -p $(dirname ${RAPIDS_CMAKE_FORMAT_FILE})
-wget -O ${RAPIDS_CMAKE_FORMAT_FILE} ${FORMAT_FILE_URL}
-
-export LD_LIBRARY_PATH="$CONDA_PREFIX/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 ./build.sh libcuml
+
+rapids-logger "Run clang-tidy"
+
 python cpp/scripts/run-clang-tidy.py
