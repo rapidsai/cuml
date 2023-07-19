af38cdf143a9b5ecbc070f370a0e30fcdccba4aa
divyegala <divyegala@gmail.com>
2023-07-19 14:38:16 -0700

    account for upstream changes

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -3,19 +3,12 @@
 
 set -euo pipefail
 
-if [[ ! -d "/tmp/gha-tools" ]]; then
-  git clone https://github.com/divyegala/gha-tools.git -b wheel-local-runs /tmp/gha-tools
-fi
-
-if [[ -z "${RAPIDS_EPOCH_TIMESTAMP}" ]]; then
-  export RAPIDS_EPOCH_TIMESTAMP="$(date +%s)"
-fi
-
-source /tmp/gha-tools/tools/rapids-configure-sccache
+source rapids-configure-sccache
+source rapids-date-string
 
 # Use gha-tools rapids-pip-wheel-version to generate wheel version then
 # update the necessary files
-version_override="$(rapids-pip-wheel-version ${RAPIDS_EPOCH_TIMESTAMP})"
+version_override="$(rapids-pip-wheel-version ${RAPIDS_DATE_STRING})"
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 
@@ -30,4 +23,4 @@ SKBUILD_CONFIGURE_OPTIONS="-DCUML_BUILD_WHEELS=ON -DDETECT_CONDA_ENV=OFF -DDISAB
 mkdir -p final_dist
 python -m auditwheel repair -w final_dist dist/*
 
-RAPIDS_PY_WHEEL_NAME="cuml_${RAPIDS_PY_CUDA_SUFFIX}" /tmp/gha-tools/tools/rapids-upload-wheels-to-s3 final_dist
+RAPIDS_PY_WHEEL_NAME="cuml_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 final_dist
