f2e95b7870aa473c4014b977be6285d72d57d74f
Divye Gala <divyegala@gmail.com>
2023-07-24 18:17:09 -0400

    Apply suggestions from code review
    Co-authored-by: AJ Schmidt <ajschmidt8@users.noreply.github.com>

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -12,13 +12,18 @@ version_override="$(rapids-pip-wheel-version ${RAPIDS_DATE_STRING})"
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 
-bash ci/release/apply_wheel_modifications.sh ${version_override} "-${RAPIDS_PY_CUDA_SUFFIX}"
+ci/release/apply_wheel_modifications.sh ${version_override} "-${RAPIDS_PY_CUDA_SUFFIX}"
 echo "The package name and/or version was modified in the package source. The git diff is:"
 git diff
 
 cd python/
 
-SKBUILD_CONFIGURE_OPTIONS="-DCUML_BUILD_WHEELS=ON -DDETECT_CONDA_ENV=OFF -DDISABLE_DEPRECATION_WARNINGS=ON -DCPM_cumlprims_mg_SOURCE=${GITHUB_WORKSPACE}/cumlprims_mg/" python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check
+SKBUILD_CONFIGURE_OPTIONS="-DCUML_BUILD_WHEELS=ON -DDETECT_CONDA_ENV=OFF -DDISABLE_DEPRECATION_WARNINGS=ON -DCPM_cumlprims_mg_SOURCE=${GITHUB_WORKSPACE}/cumlprims_mg/" \
+  python -m pip wheel . \
+    -w dist \
+    -vvv \
+    --no-deps \
+    --disable-pip-version-check
 
 mkdir -p final_dist
 python -m auditwheel repair -w final_dist dist/*
