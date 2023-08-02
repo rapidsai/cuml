1b9a2f71a279d4a0cd52b3b7acbb1011006d9656
Intron7 <severin.dicks@icloud.com>
2023-08-02 09:58:51 +0200

    added reference to cuml issue

--- python/cuml/prims/stats/covariance.py
+++ python/cuml/prims/stats/covariance.py
@@ -168,7 +168,8 @@ def cov(x, y, mean_x=None, mean_y=None, return_gram=False, return_mean=False):
             "X and Y must have same shape %s != %s" % (x.shape, y.shape)
         )
 
-    # Fix for cupy issue #7699: addressing problems with sparse matrix multiplication (spGEMM)
+    # Fix for cuml issue #5475 & cupy issue #7699
+    # addressing problems with sparse matrix multiplication (spGEMM)
     if (
         x is y
         and cupyx.scipy.sparse.issparse(x)
@@ -239,9 +240,10 @@ def _cov_sparse(x, return_gram=False, return_mean=False):
     Computes the mean and the covariance of matrix X of
     the form Cov(X, X) = E(XX) - E(X)E(X)
 
-    This is a temporary fix for cupy issue #7699, where the
-    operation `x.T.dot(x)` did not work for larger
-    sparse matrices.
+    This is a temporary fix for
+    cuml issue #5475 and cupy issue #7699,
+    where the operation `x.T.dot(x)` did not work for
+    larger sparse matrices.
 
     Parameters
     ----------
