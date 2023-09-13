9b1ee68df031430a723807fea34d96979d74ab94
Bradley Dice <bdice@bradleydice.com>
2023-09-13 17:43:45 -0500

    Update to doxygen 1.9.1. (#5580)
    This PR updates cuml to doxygen 1.9.1. This version was chosen because it is what ships with Ubuntu 22.04. I also ran `doxygen -u` to update `Doxyfile.in`.
    
    See also:
    - https://github.com/rapidsai/cudf/pull/14059
    - https://github.com/rapidsai/rmm/pull/1337
    
    Authors:
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - William Hicks (https://github.com/wphicks)
      - Vyas Ramasubramani (https://github.com/vyasr)
      - Jake Awe (https://github.com/AyodeAwe)
    
    URL: https://github.com/rapidsai/cuml/pull/5580

--- cpp/cmake/doxygen.cmake
+++ cpp/cmake/doxygen.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2021, NVIDIA CORPORATION.
+# Copyright (c) 2019-2023, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # limitations under the License.
 #
 
-find_package(Doxygen 1.8.12)
+find_package(Doxygen 1.9.1)
 
 function(add_doxygen_target)
   if(Doxygen_FOUND)
