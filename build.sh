32ff8974fdc59d0e053a1c45cd039fe747edc78b
Dante Gama Dessavre <danteg@nvidia.com>
2023-09-26 15:18:54 -0500

    FIX wrong cmake conditional by accident

--- build.sh
+++ build.sh
@@ -297,7 +297,7 @@ if (! hasArg --configure-only) && (completeBuild || hasArg cuml || hasArg pydocs
 fi
 
 if hasArg cuml-cpu; then
-    SKBUILD_CONFIGURE_OPTIONS="${CUML_EXTRA_CMAKE_ARGS} -DCUML_CPU=ON -DFIND_CUML_CPP=OFF" \
+    SKBUILD_CONFIGURE_OPTIONS="${CUML_EXTRA_CMAKE_ARGS} -DCUML_CPU=ON" \
         SKBUILD_BUILD_OPTIONS="-j${PARALLEL_LEVEL}" \
-        python -m pip install --no-build-isolation --no-deps ${REPODIR}/python
+        python -m pip install --no-build-isolation --no-deps -v ${REPODIR}/python
 fi
