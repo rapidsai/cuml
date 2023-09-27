db61f1730631d19258b7cbd4cb5c689a56c813d7
Dante Gama Dessavre <danteg@nvidia.com>
2023-09-27 08:36:30 -0500

    Remove accidentally commited local changes

--- conda/recipes/cuml-cpu/build_cuml_cpu.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Copyright (c) 2023, NVIDIA CORPORATION.
-
-# This assumes the script is executed from the root of the repo directory
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-./build.sh clean cuml-cpu -v
