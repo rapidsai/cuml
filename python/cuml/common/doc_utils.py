7c21a1a9d709580ff74a90390618572f8d097aa1
Jiaming Yuan <jm.yuan@outlook.com>
2023-11-14 09:02:44 +0800

    black.

--- python/cuml/common/doc_utils.py
+++ python/cuml/common/doc_utils.py
@@ -95,7 +95,7 @@ _parameters_docstrings = {
     "    If True, values in the inverse transform below this parameter\n"
     "    are clipped to 0.",
     None: "{name} : None\n"
-    "    Ignored. This parameter exists for compatibility only."
+    "    Ignored. This parameter exists for compatibility only.",
 }
 
 _parameter_possible_values = [
@@ -224,7 +224,6 @@ def generate_docstring(
         if (
             "X" in params or "y" in params or parameters
         ) and not skip_parameters_heading:
-
             func.__doc__ += "\nParameters\n----------\n"
 
         # Check if we want to prepend the parameters
