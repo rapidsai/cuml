b88b7bd9333230e58f2b2605258c09012ef74175
Peter Andreas Entschev <peter@entschev.com>
2023-12-12 02:53:23 -0800

    Enable UCX/UCXX tests in CI

--- ci/test_python_dask.sh
+++ ci/test_python_dask.sh
@@ -10,12 +10,36 @@ set +e
 
 rapids-logger "pytest cuml-dask"
 cd python/cuml/tests/dask
+
+# Run tests (no UCX-Py/UCXX)
 pytest \
   --cache-clear \
   --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml-dask.xml" \
   --cov-config=../../../.coveragerc \
   --cov=cuml_dask \
   --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cuml-dask-coverage.xml" \
+  --cov-report=term \
+  .
+
+# Run tests (UCX-Py only)
+pytest \
+  --run_ucx
+  --cache-clear \
+  --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml-dask-ucx.xml" \
+  --cov-config=../../../.coveragerc \
+  --cov=cuml_dask \
+  --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cuml-dask-ucx-coverage.xml" \
+  --cov-report=term \
+  .
+
+# Run tests (UCXX only)
+pytest \
+  --run_ucx
+  --cache-clear \
+  --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml-dask-ucxx.xml" \
+  --cov-config=../../../.coveragerc \
+  --cov=cuml_dask \
+  --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cuml-dask-ucxx-coverage.xml" \
   --cov-report=term \
   .
 
