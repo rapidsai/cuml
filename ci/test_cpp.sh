966f742d1c43550d179f441d3bd84f810eaf7c21
Bradley Dice <bdice@bradleydice.com>
2023-12-19 08:29:41 -0800

    Revert testing changes.

--- ci/test_cpp.sh
+++ ci/test_cpp.sh
@@ -19,8 +19,6 @@ conda activate test
 set -u
 
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
-LIBRMM_CHANNEL=$(rapids-get-pr-conda-artifact rmm 1404 cpp)
-LIBRAFT_CHANNEL=$(rapids-get-pr-conda-artifact raft 2049 cpp)
 
 RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${PWD}/test-results"}/
 mkdir -p "${RAPIDS_TESTS_DIR}"
@@ -29,8 +27,6 @@ rapids-print-env
 
 rapids-mamba-retry install \
   --channel "${CPP_CHANNEL}" \
-  --channel "${LIBRMM_CHANNEL}" \
-  --channel "${LIBRAFT_CHANNEL}" \
   libcuml libcuml-tests
 
 rapids-logger "Check GPU usage"
