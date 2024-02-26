5661d023d5e2d34b1d12b097aff1986e77880598
Bradley Dice <bdice@bradleydice.com>
2024-02-26 12:54:28 -0600

    Install test dependencies at the same time as cuml packages. (#5781)
    This PR creates a conda environment containing both test dependencies and cuml packages. This is a workaround for some issues seen with conda being unable to downgrade from Arrow 15 (in the initial environment with test dependencies) to Arrow 14 (currently pinned by cudf, which is a dependency of cuml).
    
    This is a partial solution for https://github.com/rapidsai/build-planning/issues/22. (More work is needed for other RAPIDS repos.)
    
    Depends on https://github.com/rapidsai/dependency-file-generator/pull/67.
    
    Authors:
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - Charles Blackmon-Luca (https://github.com/charlesbluca)
      - GALI PREM SAGAR (https://github.com/galipremsagar)
      - Vyas Ramasubramani (https://github.com/vyasr)
      - Jake Awe (https://github.com/AyodeAwe)
      - https://github.com/jakirkham
      - Dante Gama Dessavre (https://github.com/dantegd)
    
    URL: https://github.com/rapidsai/cuml/pull/5781

--- ci/test_cpp.sh
+++ ci/test_cpp.sh
@@ -5,11 +5,15 @@ set -euo pipefail
 
 . /opt/conda/etc/profile.d/conda.sh
 
+rapids-logger "Downloading artifacts from previous jobs"
+CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
+
 rapids-logger "Generate C++ testing dependencies"
 rapids-dependency-file-generator \
   --output conda \
   --file_key test_cpp \
-  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch)" | tee env.yaml
+  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch)" \
+  --prepend-channels "${CPP_CHANNEL}" | tee env.yaml
 
 rapids-mamba-retry env create --force -f env.yaml -n test
 
@@ -18,16 +22,10 @@ set +u
 conda activate test
 set -u
 
-CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
+rapids-print-env
 
 RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${PWD}/test-results"}/
 mkdir -p "${RAPIDS_TESTS_DIR}"
-
-rapids-print-env
-
-rapids-mamba-retry install \
-  --channel "${CPP_CHANNEL}" \
-  libcuml libcuml-tests
 
 rapids-logger "Check GPU usage"
 nvidia-smi
--- ci/test_notebooks.sh
+++ ci/test_notebooks.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-# Copyright (c) 2020-2023, NVIDIA CORPORATION.
+# Copyright (c) 2020-2024, NVIDIA CORPORATION.
 set -euo pipefail
 
 . /opt/conda/etc/profile.d/conda.sh
+
+rapids-logger "Downloading artifacts from previous jobs"
+CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
+PYTHON_CHANNEL=$(rapids-download-conda-from-s3 python)
 
 rapids-logger "Generate Notebook testing dependencies"
 rapids-dependency-file-generator \
   --output conda \
   --file_key test_notebooks \
-  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" | tee env.yaml
+  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" \
+  --prepend-channels "${CPP_CHANNEL};${PYTHON_CHANNEL}" | tee env.yaml
 
 rapids-mamba-retry env create --force -f env.yaml -n test
 
@@ -17,16 +22,7 @@ set +u
 conda activate test
 set -u
 
-rapids-logger "Downloading artifacts from previous jobs"
-CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
-PYTHON_CHANNEL=$(rapids-download-conda-from-s3 python)
-
 rapids-print-env
-
-rapids-mamba-retry install \
-  --channel "${CPP_CHANNEL}" \
-  --channel "${PYTHON_CHANNEL}" \
-  libcuml cuml
 
 rapids-logger "Check GPU usage"
 nvidia-smi
