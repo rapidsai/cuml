cb2f69be96f377697e461cfb04d3dd6c4b2a57ea
Hyunsu Cho <phcho@nvidia.com>
2024-03-19 20:26:44 -0700

    Add an assertion

--- cpp/src/decisiontree/decisiontree.cuh
+++ cpp/src/decisiontree/decisiontree.cuh
@@ -209,6 +209,7 @@ tl::Tree<T, T> build_treelite_tree(const DT::TreeMetaDataNode<T, L>& rf_tree,
         } else {
           // if rf_tree.num_outputs < num_class, fill the remainder with zero
           // Most likely this happens when a binary classifier is fit with all-0 labels
+          ASSERT(rf_tree.num_outputs <= num_class, "num_class too small");
           std::vector<T> leaf_vector(num_class, T(0));
           std::copy(leaf_begin, leaf_begin + rf_tree.num_outputs, leaf_vector.begin());
           tl_tree.SetLeafVector(tl_node_id, leaf_vector);
