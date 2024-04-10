a695651b2013b0d31b75487fcec557793bc7f805
Peter Andreas Entschev <peter@entschev.com>
2024-04-10 01:26:14 -0700

    Disable `test_fil_skl_classification`

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -27,7 +27,7 @@ else
     ./ci/run_cuml_singlegpu_pytests.sh \
       --numprocesses=8 \
       --dist=worksteal \
-      -k 'not test_sparse_pca_inputs' \
+      -k 'not test_sparse_pca_inputs and not test_fil_skl_classification' \
       --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml.xml"
 
     # Run test_sparse_pca_inputs separately
