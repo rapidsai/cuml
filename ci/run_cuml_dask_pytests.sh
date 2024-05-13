6be59e832833d3ecb2be61a8e03a4e8cf32060c9
Peter Andreas Entschev <peter@entschev.com>
2024-05-13 12:30:49 -0700

    Re-enable Dask wheel tests

--- ci/run_cuml_dask_pytests.sh
+++ ci/run_cuml_dask_pytests.sh
@@ -4,14 +4,11 @@
 # Support invoking run_cuml_dask_pytests.sh outside the script directory
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../python/cuml/tests/dask
 
-# Disable non-UCXX tests for wheels tests
-if [[ -v CONDA_PREFIX ]]; then
-    rapids-logger "pytest cuml-dask (No UCX-Py/UCXX)"
-    python -m pytest --cache-clear "$@" .
+rapids-logger "pytest cuml-dask (No UCX-Py/UCXX)"
+python -m pytest --cache-clear "$@" .
 
-    rapids-logger "pytest cuml-dask (UCX-Py only)"
-    python -m pytest --cache-clear --run_ucx "$@" .
-fi
+rapids-logger "pytest cuml-dask (UCX-Py only)"
+python -m pytest --cache-clear --run_ucx "$@" .
 
 rapids-logger "pytest cuml-dask (UCXX only)"
 python -m pytest --cache-clear --run_ucxx "$@" .
