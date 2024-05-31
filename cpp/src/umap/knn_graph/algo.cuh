783838871c1d552678e37969be739e375c158e69
jinsolp <soleeep99@gmail.com>
2024-05-31 23:02:44 +0000

    fix comment

--- cpp/src/umap/knn_graph/algo.cuh
+++ cpp/src/umap/knn_graph/algo.cuh
@@ -83,8 +83,7 @@ inline void launcher(const raft::handle_t& handle,
                                         params->metric,
                                         params->p);
   } else {  // nn_descent
-    // number of columns (n_neightbors) should be smaller than the graph degree computed by nn
-    // descent
+    // n_neightbors should be smaller than the graph degree computed by nn descent
     assert(n_neighbors <= params->nn_descent_params.graph_degree);
     auto dataset =
       raft::make_host_matrix_view<const float, int64_t>(inputsA.X, inputsA.n, inputsA.d);
