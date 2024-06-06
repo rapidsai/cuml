e354b8b3a5e1450ff662354a62c065fbb9e96baa
Dante Gama Dessavre <danteg@nvidia.com>
2024-06-06 17:57:28 -0500

    FIX Remove accidentally commited change

--- python/cuml/_thirdparty/sklearn/preprocessing/_data.py
+++ python/cuml/_thirdparty/sklearn/preprocessing/_data.py
@@ -2301,8 +2301,6 @@ class QuantileTransformer(TransformerMixin,
         X = np.asnumpy(X)
         if self.subsample is not None and self.subsample < n_samples:
             # Take a subsample of `X`
-            resample = \
-                cpu_only_import_from('sklearn.utils._indexing', 'resample')
             X = resample(
                 X, replace=False, n_samples=self.subsample, random_state=random_state
             )
