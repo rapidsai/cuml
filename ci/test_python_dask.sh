9f8ecd6d14ec68190d31279e23cc17bf90c7b09e
ptaylor <paul.e.taylor@me.com>
2024-07-02 20:23:49 +0000

    make ci/run_cuml_dask_pytests.sh environment-agnostic again

--- ci/test_python_dask.sh
+++ ci/test_python_dask.sh
@@ -11,13 +11,23 @@ EXITCODE=0
 trap "EXITCODE=1" ERR
 set +e
 
-# Run tests
-./ci/run_cuml_dask_pytests.sh \
-  --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml-dask.xml" \
-  --cov-config=../../../.coveragerc \
-  --cov=cuml_dask \
-  --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cuml-dask-coverage.xml" \
+test_args=(
+  --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml-dask.xml"
+  --cov-config=../../../.coveragerc
+  --cov=cuml_dask
+  --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cuml-dask-coverage.xml"
   --cov-report=term
+)
+
+# Run tests
+rapids-logger "pytest cuml-dask (No UCX-Py/UCXX)"
+timeout 2h ./ci/run_cuml_dask_pytests.sh "${test_args[@]}"
+
+rapids-logger "pytest cuml-dask (UCX-Py only)"
+timeout 5m ./ci/run_cuml_dask_pytests.sh "${test_args[@]}" --run_ucx
+
+rapids-logger "pytest cuml-dask (UCXX only)"
+timeout 5m ./ci/run_cuml_dask_pytests.sh "${test_args[@]}" --run_ucxx
 
 rapids-logger "Test script exiting with value: $EXITCODE"
 exit ${EXITCODE}
