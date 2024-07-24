36330494e5b8386a59a4215f115892321234617e
James Lamb <jlamb@nvidia.com>
2024-07-24 12:56:30 -0500

    more update-version fixes

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -53,8 +53,8 @@ DEPENDENCIES=(
   rapids-dask-dependency
   rmm
 )
-for FILE in dependencies.yaml conda/environments/*.yaml; do
-  for DEP in "${DEPENDENCIES[@]}"; do
+for DEP in "${DEPENDENCIES[@]}"; do
+  for FILE in dependencies.yaml conda/environments/*.yaml; do
     sed_runner "/-.* ${DEP}\(-cu[[:digit:]]\{2\}\)\{0,1\}==/ s/==.*/==${NEXT_SHORT_TAG_PEP440}.*,>=0.0.0a0/g" "${FILE}"
   done
   for FILE in python/*/pyproject.toml; do
