7ca536c7c23cf49ec4f0e279473bf0d29d6ce7da
divyegala <divyegala@gmail.com>
2024-08-28 01:21:31 -0700

    skip dask pytests

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -35,9 +35,10 @@ rapids-logger "pytest cuml single GPU"
   -k 'test_sparse_pca_inputs' \
   --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml-sparse-pca.xml"
 
-rapids-logger "pytest cuml-dask"
-./ci/run_cuml_dask_pytests.sh \
-  --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml-dask.xml"
+# Temporarily disabled. Reference issue: https://github.com/rapidsai/cuml/issues/6050
+# rapids-logger "pytest cuml-dask"
+# ./ci/run_cuml_dask_pytests.sh \
+#   --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml-dask.xml"
 
 rapids-logger "Test script exiting with value: $EXITCODE"
 exit ${EXITCODE}
