adb0dfde1063f6fb64e99b1d182a3c315d4b9cdb
Tim Head <betatim@gmail.com>
2024-10-16 07:46:52 +0000

    Run common tests on all estimators

--- python/cuml/cuml/tests/test_common.py
+++ python/cuml/cuml/tests/test_common.py
@@ -16,10 +16,29 @@
 
 from sklearn.utils import estimator_checks
 
+from cuml.internals.utils import all_estimators
 from cuml import LogisticRegression
 
 
-@estimator_checks.parametrize_with_checks([LogisticRegression()])
+DEFAULT_PARAMETERS = {
+    "MulticlassClassifier": dict(estimator=LogisticRegression()),
+    "OneVsOneClassifier": dict(estimator=LogisticRegression()),
+    "OneVsRestClassifier": dict(estimator=LogisticRegression()),
+}
+
+
+def constructed_estimators():
+    """Build list of instances of all estimators in cuml"""
+    for name, Estimator in all_estimators(
+        type_filter=["classifier", "regressor", "cluster"]
+    ):
+        if name in DEFAULT_PARAMETERS:
+            yield Estimator(**DEFAULT_PARAMETERS[name])
+        else:
+            yield Estimator()
+
+
+@estimator_checks.parametrize_with_checks(list(constructed_estimators()))
 def test_sklearn_compatible_estimator(estimator, check):
     # Check that all estimators pass the "common estimator" checks
     # provided by scikit-learn
