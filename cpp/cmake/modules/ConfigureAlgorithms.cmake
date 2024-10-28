5d7fe0cb625e95a2e6c95606bc825454c1eb8ee7
Hyunsu Cho <phcho@nvidia.com>
2024-10-28 14:05:37 -0700

    Make cuVS optional if CUML_ALGORITHMS is set

--- cpp/cmake/modules/ConfigureAlgorithms.cmake
+++ cpp/cmake/modules/ConfigureAlgorithms.cmake
@@ -1,5 +1,5 @@
 #=============================================================================
-# Copyright (c) 2022-2023, NVIDIA CORPORATION.
+# Copyright (c) 2022-2024, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -121,6 +121,10 @@ else()
 
     if(metrics_algo)
         set(CUML_RAFT_COMPILED ON)
+    endif()
+
+    if(hdbscan_algo OR kmeans_algo OR knn_algo OR metrics_algo OR tsne_algo OR umap_algo)
+        set(LINK_CUVS ON)
     endif()
 endif()
 
