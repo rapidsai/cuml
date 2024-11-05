4f3b89a50020d0e745c24852989efa7efa90eae7
Hyunsu Cho <phcho@nvidia.com>
2024-11-05 11:05:24 -0800

    Use feature/fil-backend-r24.11 branch of Treelite

--- cpp/cmake/thirdparty/get_treelite.cmake
+++ cpp/cmake/thirdparty/get_treelite.cmake
@@ -78,7 +78,7 @@ function(find_and_configure_treelite)
     rapids_export_find_package_root(BUILD Treelite [=[${CMAKE_CURRENT_LIST_DIR}]=] EXPORT_SET cuml-exports)
 endfunction()
 
-find_and_configure_treelite(VERSION     4.3.0
-                        PINNED_TAG  575e4208f2b18e40d818c338ecb95d7a26e69aab
+find_and_configure_treelite(VERSION     4.4.0
+                        PINNED_TAG  da70beb846dc5bc235643684b1b6176585dd094a
                         EXCLUDE_FROM_ALL  ${CUML_EXCLUDE_TREELITE_FROM_ALL}
                         BUILD_STATIC_LIBS ${CUML_USE_TREELITE_STATIC})
