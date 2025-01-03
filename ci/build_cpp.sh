2f13133d091fdf882faf6a773a2c04b8c86ded4b
Vyas Ramasubramani <vyasr@nvidia.com>
2025-01-03 18:32:59 +0000

    Revert testing changes

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2022-2025, NVIDIA CORPORATION.
+# Copyright (c) 2022-2024, NVIDIA CORPORATION.
 
 set -euo pipefail
 
@@ -17,10 +17,7 @@ rapids-logger "Begin cpp build"
 
 sccache --zero-stats
 
-LIBRMM_CHANNEL=$(rapids-get-pr-conda-artifact rmm 1776 cpp)
-LIBRAFT_CHANNEL=$(rapids-get-pr-conda-artifact raft 2534 cpp)
-
-RAPIDS_PACKAGE_VERSION=$(rapids-generate-version) rapids-conda-retry mambabuild --channel "${LIBRMM_CHANNEL}" --channel "${LIBRAFT_CHANNEL}" conda/recipes/libcuml
+RAPIDS_PACKAGE_VERSION=$(rapids-generate-version) rapids-conda-retry mambabuild conda/recipes/libcuml
 
 sccache --show-adv-stats
 
--- ci/build_python.sh
+++ ci/build_python.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2022-2025, NVIDIA CORPORATION.
+# Copyright (c) 2022-2024, NVIDIA CORPORATION.
 
 set -euo pipefail
 
@@ -17,10 +17,6 @@ rapids-generate-version > ./VERSION
 
 rapids-logger "Begin py build"
 
-LIBRMM_CHANNEL=$(rapids-get-pr-conda-artifact rmm 1776 cpp)
-PYLIBRMM_CHANNEL=$(rapids-get-pr-conda-artifact rmm 1776 python)
-LIBRAFT_CHANNEL=$(rapids-get-pr-conda-artifact raft 2534 cpp)
-PYLIBRAFT_CHANNEL=$(rapids-get-pr-conda-artifact raft 2534 python)
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 
 sccache --zero-stats
@@ -30,10 +26,6 @@ sccache --zero-stats
 RAPIDS_PACKAGE_VERSION=$(head -1 ./VERSION) rapids-conda-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
-  --channel "${LIBRMM_CHANNEL}" \
-  --channel "${PYLIBRMM_CHANNEL}" \
-  --channel "${LIBRAFT_CHANNEL}" \
-  --channel "${PYLIBRAFT_CHANNEL}" \
   conda/recipes/cuml
 
 sccache --show-adv-stats
