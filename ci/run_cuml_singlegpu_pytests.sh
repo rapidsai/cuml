3aa0a82eb3d51c959203fb4de996b9921beade0f
Gil Forsyth <gforsyth@nvidia.com>
2025-01-23 15:37:41 -0500

    Return nonzero error code if cd fails

--- ci/run_cuml_singlegpu_pytests.sh
+++ ci/run_cuml_singlegpu_pytests.sh
@@ -2,6 +2,6 @@
 # Copyright (c) 2024-2025, NVIDIA CORPORATION.
 
 # Support invoking run_cuml_singlegpu_pytests.sh outside the script directory
-cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../python/cuml/cuml/tests || exit
+cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../python/cuml/cuml/tests || exit 1
 
 python -m pytest --cache-clear --ignore=dask -m "not memleak" "$@" .
