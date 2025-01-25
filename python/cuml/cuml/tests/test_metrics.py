deda71d2159156006c4d11a77fc7311c2a07a9c6
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2025-01-24 20:50:55 -0800

    Update python/cuml/cuml/tests/test_metrics.py

--- python/cuml/cuml/tests/test_metrics.py
+++ python/cuml/cuml/tests/test_metrics.py
@@ -163,9 +163,7 @@ def test_r2_score(datatype, use_handle):
     np.testing.assert_almost_equal(score, 0.98, decimal=7)
 
 
-@pytest.mark.filterwarnings(
-    "ignore:Using `__dataframe__` is deprecated"
-)
+@pytest.mark.filterwarnings("ignore:Using `__dataframe__` is deprecated")
 def test_sklearn_search():
     """Test ensures scoring function works with sklearn machinery"""
     import numpy as np
