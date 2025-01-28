6ef81185610ea7b14eee340442d8985d2278f177
Dante Gama Dessavre <danteg@nvidia.com>
2025-01-28 12:57:57 -0600

    Make memleak pytests run on nightly jobs

--- ci/test_python_singlegpu.sh
+++ ci/test_python_singlegpu.sh
@@ -31,16 +31,20 @@ rapids-logger "pytest cuml single GPU"
   --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cuml-accel-coverage.xml" \
   --cov-report=term
 
-rapids-logger "memory leak pytests"
 
-./ci/run_cuml_singlegpu_memleak_pytests.sh \
-  --numprocesses=1 \
-  --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml-memleak.xml" \
-  --cov-config=../../.coveragerc \
-  --cov=cuml \
-  --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cuml-memleak-coverage.xml" \
-  --cov-report=term \
-  -m "memleak"
+if [ "${RAPIDS_BUILD_TYPE}" == "nightly" ]; then
+  rapids-logger "memory leak pytests"
+
+  ./ci/run_cuml_singlegpu_memleak_pytests.sh \
+    --numprocesses=1 \
+    --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml-memleak.xml" \
+    --cov-config=../../.coveragerc \
+    --cov=cuml \
+    --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cuml-memleak-coverage.xml" \
+    --cov-report=term \
+    -m "memleak"
+fi
 
 rapids-logger "Test script exiting with value: $EXITCODE"
 exit ${EXITCODE}
+
