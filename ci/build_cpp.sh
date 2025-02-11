399ba53d72c0b1cffaa8168bd0d9ade7d6e2dc47
Vyas Ramasubramani <vyasr@nvidia.com>
2025-02-11 06:10:06 +0000

    Revert testing changes

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -16,14 +16,8 @@ rapids-print-env
 rapids-logger "Begin cpp build"
 
 sccache --zero-stats
-LIBRMM_CHANNEL=$(_rapids-get-pr-artifact rmm 1808 cpp conda)
-LIBRAFT_CHANNEL=$(_rapids-get-pr-artifact raft 2566 cpp conda)
-LIBCUVS_CHANNEL=$(_rapids-get-pr-artifact cuvs 644 cpp conda)
 
 RAPIDS_PACKAGE_VERSION=$(rapids-generate-version) rapids-conda-retry mambabuild \
-  --channel "${LIBRMM_CHANNEL}" \
-  --channel "${LIBRAFT_CHANNEL}" \
-  --channel "${LIBCUVS_CHANNEL}" \
   conda/recipes/libcuml
 
 sccache --show-adv-stats
--- ci/build_python.sh
+++ ci/build_python.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2022-2025, NVIDIA CORPORATION.
+# Copyright (c) 2022-2024, NVIDIA CORPORATION.
 
 set -euo pipefail
 
@@ -18,12 +18,6 @@ rapids-generate-version > ./VERSION
 rapids-logger "Begin py build"
 
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
-LIBRMM_CHANNEL=$(_rapids-get-pr-artifact rmm 1808 cpp conda)
-LIBRAFT_CHANNEL=$(_rapids-get-pr-artifact raft 2566 cpp conda)
-LIBCUVS_CHANNEL=$(_rapids-get-pr-artifact cuvs 644 cpp conda)
-PYLIBRMM_CHANNEL=$(_rapids-get-pr-artifact rmm 1808 python conda)
-PYLIBRAFT_CHANNEL=$(_rapids-get-pr-artifact raft 2566 python conda)
-PYLIBCUVS_CHANNEL=$(_rapids-get-pr-artifact cuvs 644 python conda)
 
 sccache --zero-stats
 
@@ -31,12 +25,6 @@ sccache --zero-stats
 # node works correctly
 RAPIDS_PACKAGE_VERSION=$(head -1 ./VERSION) rapids-conda-retry mambabuild \
   --no-test \
-  --channel "${LIBRMM_CHANNEL}" \
-  --channel "${LIBRAFT_CHANNEL}" \
-  --channel "${LIBCUVS_CHANNEL}" \
-  --channel "${PYLIBRMM_CHANNEL}" \
-  --channel "${PYLIBRAFT_CHANNEL}" \
-  --channel "${PYLIBCUVS_CHANNEL}" \
   --channel "${CPP_CHANNEL}" \
   conda/recipes/cuml
 
@@ -50,12 +38,6 @@ if [[ ${RAPIDS_CUDA_MAJOR} == "11" ]]; then
 
   RAPIDS_PACKAGE_VERSION=$(head -1 ./VERSION) rapids-conda-retry mambabuild \
   --no-test \
-  --channel "${LIBRMM_CHANNEL}" \
-  --channel "${LIBRAFT_CHANNEL}" \
-  --channel "${LIBCUVS_CHANNEL}" \
-  --channel "${PYLIBRMM_CHANNEL}" \
-  --channel "${PYLIBRAFT_CHANNEL}" \
-  --channel "${PYLIBCUVS_CHANNEL}" \
   conda/recipes/cuml-cpu
 
   sccache --show-adv-stats
