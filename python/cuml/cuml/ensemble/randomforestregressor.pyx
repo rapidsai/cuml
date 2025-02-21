4c47b4cb95ac08818539ad6a4ffea3dfdc6e03b6
William Hicks <whicks@nvidia.com>
2025-02-21 13:01:37 -0800

    Begin parameter translation for RandomForest

--- python/cuml/cuml/ensemble/randomforestregressor.pyx
+++ python/cuml/cuml/ensemble/randomforestregressor.pyx
@@ -261,7 +261,9 @@ class RandomForestRegressor(BaseRandomForestModel,
     _cpu_estimator_import_path = 'sklearn.ensemble.RandomForestRegressor'
 
     _hyperparam_interop_translator = {
-        "criterion": "NotImplemented",
+        "criterion": {
+            "NotImplemented",
+        },
         "oob_score": {
             True: "NotImplemented",
         },
@@ -275,7 +277,7 @@ class RandomForestRegressor(BaseRandomForestModel,
 
     @device_interop_preparation
     def __init__(self, *,
-                 split_criterion=2,
+                 split_criterion=None,
                  accuracy_metric='r2',
                  handle=None,
                  verbose=False,
