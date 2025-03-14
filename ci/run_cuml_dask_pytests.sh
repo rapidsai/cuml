cc68aee1b224cdca0587ecd7c57b098b07b9b1af
Gil Forsyth <gforsyth@users.noreply.github.com>
2025-03-14 10:06:56 -0400

    Add shellcheck to pre-commit and fix warnings (#6246)
    `shellcheck` is a fast, static analysis tool for shell scripts. It's good at
    flagging up unused variables, unintentional glob expansions, and other potential
    execution and security headaches that arise from the wonders of `bash` (and
    other shlangs).                   
                                                                                                                  
    This PR adds a `pre-commit` hook to run `shellcheck` on all of the `sh-lang`
    files in the `ci/` directory, and the changes requested by `shellcheck` to make
    the existing files pass the check.                              
                                                                                                                  
    xref: rapidsai/build-planning#135
    
    Authors:
      - Gil Forsyth (https://github.com/gforsyth)
    
    Approvers:
      - Dante Gama Dessavre (https://github.com/dantegd)
      - James Lamb (https://github.com/jameslamb)
    
    URL: https://github.com/rapidsai/cuml/pull/6246

--- ci/run_cuml_dask_pytests.sh
+++ ci/run_cuml_dask_pytests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Copyright (c) 2024, NVIDIA CORPORATION.
+# Copyright (c) 2024-2025, NVIDIA CORPORATION.
 
 # Support invoking run_cuml_dask_pytests.sh outside the script directory
-cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../python/cuml/cuml/tests/dask
+cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../python/cuml/cuml/tests/dask || exit 1
 
 python -m pytest --cache-clear "$@" .
