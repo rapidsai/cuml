66567b162f15320b434c4914df4f226f4cf3d1dc
Simon Adorf <sadorf@nvidia.com>
2025-03-31 16:11:14 -0500

    Handle sparse inputs in PCA.

--- python/cuml/cuml/decomposition/pca.pyx
+++ python/cuml/cuml/decomposition/pca.pyx
@@ -36,6 +36,7 @@ from cuml.internals.input_utils import input_to_cuml_array
 from cuml.internals.input_utils import input_to_cupy_array
 from cuml.common.array_descriptor import CumlArrayDescriptor
 from cuml.common import using_output_type
+from cuml.common.sparse_utils import is_sparse
 from cuml.prims.stats import cov
 from cuml.internals.input_utils import sparse_scipy_to_cp
 from cuml.common.exceptions import NotFittedError
@@ -428,9 +429,15 @@ class PCA(UniversalBase,
         Fit the model with X. y is currently ignored.
 
         """
-        X_m, n_rows, n_cols, _ = input_to_cuml_array(X,
-                                                     convert_to_dtype=(np.float32 if convert_dtype else None),
-                                                     check_dtype=[np.float32, np.float64])
+        if is_sparse(X):
+            self.n_samples_, self.n_features_in_, self.dtype = \
+                (X.shape[0], X.shape[1], X.dtype)
+        else:
+            X_m, self.n_samples_, self.n_features_in_, self.dtype = \
+                input_to_cuml_array(X,
+                                    convert_to_dtype=(np.float32 if convert_dtype
+                                                      else None),
+                                    check_dtype=[np.float32, np.float64])
 
         if self.n_components is None:
             logger.warn(
@@ -438,7 +445,7 @@ class PCA(UniversalBase,
                 ' n_components argument defaults to using'
                 ' min(n_samples, n_features) rather than 1'
             )
-            self.n_components_ = min(n_rows, n_cols)
+            self.n_components_ = min(self.n_samples_, self.n_features_in_)
         else:
             self.n_components_ = self.n_components
 
@@ -448,10 +455,6 @@ class PCA(UniversalBase,
             X = sparse_scipy_to_cp(X, dtype=None)
             return self._sparse_fit(X)
 
-        # Store values from input_to_cuml_array call
-        self.n_samples_ = n_rows
-        self.n_features_in_ = n_cols
-        self.dtype = X_m.dtype
         cdef uintptr_t _input_ptr = X_m.ptr
         self.feature_names_in_ = X_m.index
 
