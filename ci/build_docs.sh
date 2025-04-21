a2bbd3448cc275c583d2b5a105c5586e2d91650e
VenkateshJaya <jjayabaskar@nvidia.com>
2025-04-21 14:31:22 -0700

    added downloads from github

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 rapids-logger "Downloading artifacts from previous jobs"
-CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
-PYTHON_CHANNEL=$(rapids-download-conda-from-s3 python)
+CPP_CHANNEL=$(rapids-download-conda-from-github cpp)
+PYTHON_CHANNEL=$(rapids-download-conda-from-github python)
 
 rapids-logger "Create test conda environment"
 . /opt/conda/etc/profile.d/conda.sh
