5084cfdc973d23e8a4e760fbaffe20964ea94a3e
viclafargue <viclafargue@nvidia.com>
2025-04-23 14:40:21 +0200

    Updating expected values in SVC testing to account for CCCL update

--- cpp/tests/sg/svc_test.cu
+++ cpp/tests/sg/svc_test.cu
@@ -1094,10 +1094,10 @@ TYPED_TEST(SmoSolverTest, SmoSolveTest)
                           {1, 1, 2, 2, 1, 2, 2, 3},  // x_support
                           {0, 2, 3, 5}}},            // support idx
     {smoInput<TypeParam>{10, 0.001, KernelParams{LINEAR, 3, 1, 0}, 100, 1},
-     smoOutput<TypeParam>{3, {-2, 4, -2, 0, 0}, -1.0, {-2, 2}, {}, {}}},
+     smoOutput<TypeParam>{3, {2, -4, 2, 0, 0}, -1.0, {-2, 2}, {}, {}}},
     {smoInput<TypeParam>{1, 1e-6, KernelParams{POLYNOMIAL, 3, 1, 1}, 100, 1},
      smoOutput<TypeParam>{
-       3, {-0.02556136, 0.03979708, -0.01423571}, -1.07739149, {}, {1, 1, 2, 1, 2, 2}, {0, 2, 3}}}};
+       3, {-0.0255614, 0.0397971, -0.0142357}, -1.07739149, {}, {1, 1, 2, 1, 2, 2}, {0, 2, 3}}}};
 
   for (auto d : data) {
     auto p   = d.first;
@@ -1186,13 +1186,8 @@ TYPED_TEST(SmoSolverTest, SvcTest)
                          this->x_dev.data(),
                          this->y_dev.data(),
                          true},
-     smoOutput2<TypeParam>{4,
-                           {},
-                           -1.0f,
-                           {-2, 2},
-                           {1, 1, 2, 2, 1, 2, 2, 3},
-                           {0, 2, 3, 5},
-                           {-1.0, -3.0, 1.0, -1.0, 3.0, 1.0}}},
+     smoOutput2<TypeParam>{
+       3, {}, -1.0f, {-2, 2}, {1, 2, 2, 2, 2, 3}, {2, 3, 5}, {-1.0, -3.0, 1.0, -1.0, 3.0, 1.0}}},
     {svcInput<TypeParam>{1,
                          1e-6,
                          KernelParams{POLYNOMIAL, 3, 1, 0},
@@ -1203,7 +1198,7 @@ TYPED_TEST(SmoSolverTest, SvcTest)
                          true},
      smoOutput2<TypeParam>{
        3,
-       {-0.03900895, 0.05904058, -0.02003163},
+       {-0.0390089, 0.0590406, -0.0200316},
        -0.99999959,
        {},
        {1, 1, 2, 1, 2, 2},
