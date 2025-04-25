87b14dead2b9ed20620ca4e28681b7f5aab923e9
Jim Crist-Harif <jcristharif@gmail.com>
2025-04-25 08:28:58 -0500

    Split wheel tests into two jobs (#6585)
    The wheel tests in CI previousl ran both the single-gpu and multi-gpu tests. This made them take up to 90 minutes a run, which was getting long.
    
    We now split them into two separate runs (one for single-gpu, one for multi-gpu), which should help speed things up.
    
    Fixes #6582.
    
    Authors:
      - Jim Crist-Harif (https://github.com/jcrist)
    
    Approvers:
      - Gil Forsyth (https://github.com/gforsyth)
    
    URL: https://github.com/rapidsai/cuml/pull/6585

--- ci/test_wheel_dask.sh
+++ ci/test_wheel_dask.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2023-2025, NVIDIA CORPORATION.
+# Copyright (c) 2025, NVIDIA CORPORATION.
 
 set -euo pipefail
 
@@ -10,15 +10,6 @@ LIBCUML_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="libcuml_${RAPIDS_PY_CUDA_SUFFIX}" rap
 RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${PWD}/test-results"}
 mkdir -p "${RAPIDS_TESTS_DIR}"
 
-# Install just minimal dependencies first
-rapids-pip-retry install \
-  "${LIBCUML_WHEELHOUSE}"/libcuml*.whl \
-  "${CUML_WHEELHOUSE}"/cuml*.whl
-
-# Try to import cuml with just a minimal install"
-rapids-logger "Importing cuml with minimal dependencies"
-python -c "import cuml"
-
 # echo to expand wildcard before adding `[extra]` requires for pip
 rapids-pip-retry install \
    "${LIBCUML_WHEELHOUSE}"/libcuml*.whl \
@@ -28,17 +19,8 @@ EXITCODE=0
 trap "EXITCODE=1" ERR
 set +e
 
-rapids-logger "pytest cuml single GPU"
-./ci/run_cuml_singlegpu_pytests.sh \
-  --numprocesses=8 \
-  --dist=worksteal \
-  -k 'not test_sparse_pca_inputs' \
-  --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml.xml"
-
-# Run test_sparse_pca_inputs separately
-./ci/run_cuml_singlegpu_pytests.sh \
-  -k 'test_sparse_pca_inputs' \
-  --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml-sparse-pca.xml"
+rapids-logger "pytest cuml dask"
+timeout 2h ./ci/run_cuml_dask_pytests.sh --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml-dask.xml"
 
 rapids-logger "Test script exiting with value: $EXITCODE"
 exit ${EXITCODE}
