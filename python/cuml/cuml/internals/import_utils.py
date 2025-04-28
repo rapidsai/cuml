9c8df8ad67a1fe50dc92449b4c6203f5d76780a6
Jim Crist-Harif <jcristharif@gmail.com>
2025-04-28 10:05:42 -0700

    Remove `has_dask_cudf`

--- python/cuml/cuml/internals/import_utils.py
+++ python/cuml/cuml/internals/import_utils.py
@@ -30,15 +30,6 @@ def has_dask():
         return False
 
 
-def has_dask_cudf():
-    try:
-        import dask_cudf  # NOQA
-
-        return True
-    except ImportError:
-        return False
-
-
 def has_dask_sql():
     try:
         import dask_sql  # NOQA
