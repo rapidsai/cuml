a28798dfc7cf8b7f55c180cc89cd61c005ad5a39
James Lamb <jlamb@nvidia.com>
2025-05-30 12:18:11 -0500

    do not try to use PR artifacts at all

--- ci/use_conda_packages_from_prs.sh
+++ ci/use_conda_packages_from_prs.sh
@@ -1,31 +1,31 @@
-#!/bin/bash
-# Copyright (c) 2025, NVIDIA CORPORATION.
+# #!/bin/bash
+# # Copyright (c) 2025, NVIDIA CORPORATION.
 
-set +x
+# set +x
 
-# download CI artifacts
-echo "--- downloading artifacts ---"
-LIBCUMLPRIMS_MG_CHANNEL=$(rapids-get-pr-conda-artifact cumlprims_mg 256 cpp)
-echo "--- done downloading artifacts ---"
+# # download CI artifacts
+# echo "--- downloading artifacts ---"
+# LIBCUMLPRIMS_MG_CHANNEL=$(rapids-get-pr-conda-artifact cumlprims_mg 256 cpp)
+# echo "--- done downloading artifacts ---"
 
-# For `rattler` builds:
-#
-# Add these channels to the array checked by 'rapids-rattler-channel-string'.
-# This ensures that when conda packages are built with strict channel priority enabled,
-# the locally-downloaded packages will be preferred to remote packages (e.g. nightlies).
-#
-RAPIDS_PREPENDED_CONDA_CHANNELS=(
-    "${LIBCUMLPRIMS_MG_CHANNEL}"
-)
-export RAPIDS_PREPENDED_CONDA_CHANNELS
+# # For `rattler` builds:
+# #
+# # Add these channels to the array checked by 'rapids-rattler-channel-string'.
+# # This ensures that when conda packages are built with strict channel priority enabled,
+# # the locally-downloaded packages will be preferred to remote packages (e.g. nightlies).
+# #
+# RAPIDS_PREPENDED_CONDA_CHANNELS=(
+#     "${LIBCUMLPRIMS_MG_CHANNEL}"
+# )
+# export RAPIDS_PREPENDED_CONDA_CHANNELS
 
-# For tests and `conda-build` builds:
-#
-# Add these channels to the system-wide conda configuration.
-# This results in PREPENDING them to conda's channel list, so
-# these packages should be found first if strict channel priority is enabled.
-#
-for _channel in "${RAPIDS_PREPENDED_CONDA_CHANNELS[@]}"
-do
-   conda config --system --add channels "${_channel}"
-done
+# # For tests and `conda-build` builds:
+# #
+# # Add these channels to the system-wide conda configuration.
+# # This results in PREPENDING them to conda's channel list, so
+# # these packages should be found first if strict channel priority is enabled.
+# #
+# for _channel in "${RAPIDS_PREPENDED_CONDA_CHANNELS[@]}"
+# do
+#    conda config --system --add channels "${_channel}"
+# done
