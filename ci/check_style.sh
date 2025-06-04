e44e9c4c937fc5c32a99f442a00a9e860a9fa75b
aamijar <aamijar230@gmail.com>
2025-06-04 23:02:46 +0000

    test ci

--- ci/check_style.sh
+++ ci/check_style.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-# Copyright (c) 2020-2025, NVIDIA CORPORATION.
+# Copyright (c) 2020-2024, NVIDIA CORPORATION.
 
 set -euo pipefail
-
-source ./ci/use_conda_packages_from_prs.sh
-
 
 rapids-logger "Create checks conda environment"
 . /opt/conda/etc/profile.d/conda.sh
