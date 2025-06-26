abcdf28ef7a1a98def3f0abf61874b05b13788e9
Simon Adorf <sadorf@nvidia.com>
2025-06-26 14:14:14 -0500

    Parametrize directly on the list.

--- python/cuml/cuml/tests/test_fil.py
+++ python/cuml/cuml/tests/test_fil.py
@@ -850,8 +850,8 @@ def test_apply(train_device, infer_device, n_classes, tmp_path):
         np.testing.assert_equal(pred_leaf, expected_pred_leaf)
 
 
-@pytest.mark.parametrize("test_empty_category_list", [True, False])
-def test_missing_categorical(test_empty_category_list):
+@pytest.mark.parametrize("category_list", [[], [0, 2]])
+def test_missing_categorical(category_list):
     builder = treelite.model_builder.ModelBuilder(
         threshold_type="float32",
         leaf_output_type="float32",
@@ -875,7 +875,7 @@ def test_missing_categorical(test_empty_category_list):
     builder.start_node(0)
     builder.categorical_test(
         feature_id=0,
-        category_list=[] if test_empty_category_list else [0, 2],
+        category_list=category_list,
         default_left=False,
         category_list_right_child=False,
         left_child_key=1,
