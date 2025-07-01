4ebb20f6402ea301efd666c41244b0559e038b08
aamijar <aamijar230@gmail.com>
2025-07-01 22:11:21 +0000

    fix cov

--- cpp/src/pca/pca.cuh
+++ cpp/src/pca/pca.cuh
@@ -127,8 +127,8 @@ void pcaFit(const raft::handle_t& handle,
   auto len = prms.n_cols * prms.n_cols;
   rmm::device_uvector<math_t> cov(len, stream);
 
-  raft::stats::cov<true>(
-    handle, cov.data(), input, mu, prms.n_cols, prms.n_rows, false, true, stream);
+  raft::stats::cov<false>(
+    handle, cov.data(), input, mu, prms.n_cols, prms.n_rows, true, true, stream);
   truncCompExpVars(
     handle, cov.data(), components, explained_var, explained_var_ratio, noise_vars, prms, stream);
 
