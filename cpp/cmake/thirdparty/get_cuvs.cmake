3b6fc7fb32f02778f8bb199adf13683a7ce251ea
Tarang Jain <jaintarang2015@gmail.com>
2025-07-03 11:12:55 -0700

    revert cuvs tag

--- cpp/cmake/thirdparty/get_cuvs.cmake
+++ cpp/cmake/thirdparty/get_cuvs.cmake
@@ -49,7 +49,7 @@ function(find_and_configure_cuvs)
       INSTALL_EXPORT_SET  cuml-exports
       CPM_ARGS
         GIT_REPOSITORY         https://github.com/${PKG_FORK}/cuvs.git
-        GIT_TAG                branch-25.04
+        GIT_TAG                ${PKG_PINNED_TAG}
         SOURCE_SUBDIR          cpp
         EXCLUDE_FROM_ALL       ${PKG_EXCLUDE_FROM_ALL}
         OPTIONS
