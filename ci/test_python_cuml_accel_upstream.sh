69335f4bd4a2320902ff9ca6b7939289c787d4d5
Jim Crist-Harif <jcristharif@gmail.com>
2025-07-09 10:49:29 -0700

    Run HDBSCAN test suite in CI
    This adds the HDBSCAN upstream test suite to CI.
    
    A few complications:
    
    - The upstream `hdbscan` library has seen a lapse in maintenance lately.
      Thus, the test suite won't run cleanly (or even import) without
      sklearn < 1.6. Rather than pinning the sklearn version across the
      upstream test jobs (which would require a new entry in
      `dependencies.yaml` and changes to the `test_python_common.sh`
      script), for now we just skip the HDBSCAN tests if that condition
      isn't met. The oldest deps run will run the HDBSCAN tests, but not the
      newest deps run.
    - There are many failures in the HDBSCAN tests, most of which are memory
      errors. The xfail list here is stable (I've ran it 10s of times to be
      sure). That said, if you use `pytest-randomly` to shuffle the test
      order, you'll see a different set of failures. Not great.

--- ci/test_python_cuml_accel_upstream.sh
+++ ci/test_python_cuml_accel_upstream.sh
@@ -15,5 +15,22 @@ set +e
 rapids-logger "UMAP test suite"
 timeout 15m ./python/cuml/cuml_accel_tests/upstream/umap/run-tests.sh
 
+# The upstream HDBSCAN tests require scikit-learn < 1.6. Skip the HDBSCAN
+# tests if an incompatible version is installed.
+hdbscan_sklearn_version_check=0
+python -c "
+import sklearn
+import sys
+from packaging.version import Version
+sys.exit(Version(sklearn.__version__) >= Version('1.6'))
+" || hdbscan_sklearn_version_check=$?
+
+if [ $hdbscan_sklearn_version_check -eq 0 ]; then
+    rapids-logger "HDBSCAN test suite"
+    timeout 15m ./python/cuml/cuml_accel_tests/upstream/hdbscan/run-tests.sh
+else
+    rapids-logger "Skipping HDBSCAN test suite, scikit-learn version not compatible"
+fi
+
 rapids-logger "Test script exiting with value: $EXITCODE"
 exit ${EXITCODE}
