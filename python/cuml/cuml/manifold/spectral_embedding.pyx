0e5b61af6b4f90a4b5d6697097ff663bbcf7e9ab
aamijar <aamijar230@gmail.com>
2025-07-11 19:58:12 +0000

    fix cuml import

--- python/cuml/cuml/manifold/spectral_embedding.pyx
+++ python/cuml/cuml/manifold/spectral_embedding.pyx
@@ -32,6 +32,7 @@ from pylibraft.common.cpp.mdspan cimport (
 )
 from pylibraft.common.handle cimport device_resources
 
+import cuml
 from cuml.common import input_to_cuml_array
 from cuml.internals.array import CumlArray
 from cuml.internals.base import Base
