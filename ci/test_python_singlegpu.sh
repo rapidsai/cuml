dca9f0cdf0c45dca9643b3c932f95a28d2b3a0f9
Vyas Ramasubramani <vyasr@nvidia.com>
2025-07-30 12:40:05 -0700

    Fix more paths

--- ci/test_python_singlegpu.sh
+++ ci/test_python_singlegpu.sh
@@ -16,7 +16,7 @@ rapids-logger "pytest cuml single GPU"
   --numprocesses=8 \
   --dist=worksteal \
   --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml.xml" \
-  --cov-config=../../.coveragerc \
+  --cov-config=../.coveragerc \
   --cov=cuml \
   --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cuml-coverage.xml"
 
