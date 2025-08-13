5d5b5d8b277a416cb8a8d28a9444a51a7ed851a4
Jim Crist-Harif <jcristharif@gmail.com>
2025-08-13 11:22:58 -0500

    Remove deprecation warnings in 25.10 (#7109)
    This removes a few deprecated things in `cuml` proper that were deprecated in 25.08 and slated for removal in 25.10:
    
    - Removes deprecated `penalty="none"` support in the SGD solver
    - Removes deprecated `connectivity` parameter in HDBSCAN
    - Removes deprecated `threshold` parameter in FIL load methods
    - Removes deprecated `hdbscan.prediction` namespace
    - Removes deprecated access to the hdbscan prediction functions from the `cuml.cluster` namespace.
    
    Authors:
      - Jim Crist-Harif (https://github.com/jcrist)
    
    Approvers:
      - Dante Gama Dessavre (https://github.com/dantegd)
    
    URL: https://github.com/rapidsai/cuml/pull/7109

--- python/cuml/cuml/cluster/hdbscan/__init__.py
+++ python/cuml/cuml/cluster/hdbscan/__init__.py
@@ -14,7 +14,6 @@
 # limitations under the License.
 #
 
-import cuml.cluster.hdbscan.prediction as prediction
 from cuml.cluster.hdbscan.hdbscan import (
     HDBSCAN,
     all_points_membership_vectors,
