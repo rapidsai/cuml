32a2e39fa616c24b50d383791657bba9ab9ed2d1
Jim Crist-Harif <jcristharif@gmail.com>
2025-09-03 16:12:21 -0500

    Delay sklearn imports of metaestimators

--- python/cuml/cuml/pipeline/__init__.py
+++ python/cuml/cuml/pipeline/__init__.py
@@ -13,17 +13,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-from sklearn.pipeline import Pipeline, make_pipeline
-
-disclaimer = """
-This code is developed and maintained by scikit-learn and imported
+"""This code is developed and maintained by scikit-learn and imported
 by cuML to maintain the familiar sklearn namespace structure.
 cuML includes tests to ensure full compatibility of these wrappers
 with CUDA-based data and cuML estimators, but all of the underlying code
-is due to the scikit-learn developers.\n\n"""
+is due to the scikit-learn developers."""
 
-Pipeline.__doc__ = disclaimer + Pipeline.__doc__
-make_pipeline.__doc__ = disclaimer + make_pipeline.__doc__
+from sklearn.pipeline import Pipeline, make_pipeline
 
 __all__ = ["Pipeline", "make_pipeline"]
+
+
+def __getattr__(name):
+    if name == "Pipeline":
+        from sklearn.pipeline import Pipeline
+
+        return Pipeline
+    elif name == "make_pipeline":
+        from sklearn.pipeline import make_pipeline
+
+        return make_pipeline
+    raise AttributeError(f"module {__name__} has no attribute {name}")
