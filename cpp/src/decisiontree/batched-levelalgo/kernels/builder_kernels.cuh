939136cd3694cc0f6d6c1a340c1e3e29ab520d78
Tarang Jain <jaintarang2015@gmail.com>
2025-11-05 12:09:06 -0800

    fix compilation

--- cpp/src/decisiontree/batched-levelalgo/kernels/builder_kernels.cuh
+++ cpp/src/decisiontree/batched-levelalgo/kernels/builder_kernels.cuh
@@ -121,8 +121,8 @@ HDI IdxT lower_bound(DataT* array, IdxT len, DataT element)
 
 template <typename IdxT>
 struct CustomDifference {
-  CustomDifference(IdxT discard_value) : discard_value(discard_value) {}
-  IdxT discard_value = cuda::std::max<IdxT>();
+  __device__ CustomDifference(IdxT discard_value) : discard_value(discard_value) {}
+  IdxT discard_value;
   __device__ IdxT operator()(const IdxT& lhs, const IdxT& rhs)
   {
     if (lhs == rhs or rhs == discard_value)
