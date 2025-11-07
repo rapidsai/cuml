334620e9b34e0e2be38b339e791ba969c21f8a75
ptaylor <paul.e.taylor@me.com>
2025-11-07 05:45:42 +0000

    don't stop the server

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -6,7 +6,6 @@ set -euo pipefail
 
 source rapids-configure-sccache
 export SCCACHE_NO_CACHE=1
-sccache --stop-server
 
 source rapids-date-string
 
--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -9,7 +9,6 @@ package_dir=$2
 
 source rapids-configure-sccache
 export SCCACHE_NO_CACHE=1
-sccache --stop-server
 source rapids-date-string
 source rapids-init-pip
 
