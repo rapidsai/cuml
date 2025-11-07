2a615b5f35c3f501bc9db694a2774661075fb0e4
jinsolp <jinsolp@nvidia.com>
2025-11-07 02:45:48 +0000

    documenting

--- cpp/src/hdbscan/detail/condense.cuh
+++ cpp/src/hdbscan/detail/condense.cuh
@@ -24,6 +24,8 @@
 #include <thrust/fill.h>
 #include <thrust/reduce.h>
 
+#include <cstdint>
+
 namespace ML {
 namespace HDBSCAN {
 namespace detail {
@@ -36,25 +38,35 @@ value_t get_lambda_h(value_idx node, value_idx num_points, const value_t* deltas
   return delta > 0.0 ? 1.0 / delta : std::numeric_limits<value_t>::max();
 }
 
+/*
+ * Bottom-up implementation using omp threads
+ * This is called from build_condensed_hierarchy
+ */
 template <typename value_idx, typename value_t>
-void condense_hierarchy_pointer_chase(
-  const std::vector<value_idx>& children,  // 2*(n_leaves-1) array 32
-  const std::vector<value_t>& deltas,      // (n_leaves-1) array 16
-  const std::vector<value_idx>& sizes,     // (n_leaves-1) array 16
-  std::vector<value_idx>& out_parent,      // (root + 1) * 2 66
-  std::vector<value_idx>& out_child,
-  std::vector<value_t>& out_lambda,
-  std::vector<value_idx>& out_sizes,
-  int n_leaves,         // 17
-  int min_cluster_size  // 3
-)
+void condense_hierarchy_cpu(const std::vector<value_idx>& children,
+                            const std::vector<value_t>& deltas,
+                            const std::vector<value_idx>& sizes,
+                            std::vector<value_idx>& out_parent,
+                            std::vector<value_idx>& out_child,
+                            std::vector<value_t>& out_lambda,
+                            std::vector<value_idx>& out_sizes,
+                            int n_leaves,
+                            int min_cluster_size)
 {
-  int n_nodes        = 2 * n_leaves - 1;    // 33
-  int root           = n_nodes - 1;         // 32
-  int internal_nodes = n_nodes - n_leaves;  // 16 (17 ~ 32)
-  std::vector<int> parent(n_nodes, -1);     // root node has parent -1
-  std::vector<int> is_persistent(internal_nodes, false);
+  int n_nodes        = 2 * n_leaves - 1;
+  int root           = n_nodes - 1;
+  int internal_nodes = n_nodes - n_leaves;
+  std::vector<int> parent(n_nodes, -1);
+  std::vector<uint8_t> is_persistent(internal_nodes, false);
 
+  /*
+   * Mark whether each internal node forms a persistent cluster, and get parent information for easy
+   * pointer chasing.
+   *
+   * A node is considered "persistent" if both of its child clusters contain at least
+   * min_cluster_size points. Persistence indicates that this node can represent a valid cluster on
+   * its own without either child being too small.
+   */
 #pragma omp parallel for
   for (int node = n_leaves; node < n_nodes; node++) {
     int left      = children[(node - n_leaves) * 2];
@@ -69,9 +81,19 @@ void condense_hierarchy_pointer_chase(
       (left_count >= min_cluster_size && right_count >= min_cluster_size) ? 1 : 0;
   }
 
-  // find your relabel
   std::vector<int> relabel(n_nodes, 0);
-  // only this part really needs to be done on the cpu
+
+  /*
+   * Build the parent–child relationships between internal and leaf nodes,
+   * and assign each leaf a representative ancestor and a lambda value.
+   *
+   *   - Every leaf node should be connected to an internal node
+   *     that lies directly below the nearest persistent node in the hierarchy.
+   *   - The `relabel` array stores this mapping: for each node, it records the
+   *     ancestor that serves as its cluster representative.
+   *  - The first internal ancestor whose size >= min_cluster_size provides
+   *     the lambda value for the leaf.
+   */
 #pragma omp parallel for
   for (int node = 0; node < n_nodes; node++) {
     int cur             = node;
@@ -108,6 +130,9 @@ void condense_hierarchy_pointer_chase(
     }
   }
 
+  /*
+   * Add edges for internal (persistent) nodes to their children using the relabel array.
+   */
 #pragma omp parallel for
   for (int node = n_leaves; node < n_nodes; node++) {
     if (is_persistent[node - n_leaves]) {
@@ -142,7 +167,6 @@ void condense_hierarchy_pointer_chase(
  *
  * @tparam value_idx
  * @tparam value_t
- * @tparam tpb
  * @param handle
  * @param[in] children parents/children from single-linkage dendrogram
  * @param[in] delta distances from single-linkage dendrogram
@@ -153,100 +177,7 @@ void condense_hierarchy_pointer_chase(
  * @param[out] condensed_tree output dendrogram. will likely no longer be
  *                            a binary tree.
  */
-// template <typename value_idx, typename value_t, int tpb = 256>
-// void build_condensed_hierarchy(const raft::handle_t& handle,
-//                                const value_idx* children,
-//                                const value_t* delta,
-//                                const value_idx* sizes,
-//                                int min_cluster_size,
-//                                int n_leaves,
-//                                Common::CondensedHierarchy<value_idx, value_t>& condensed_tree)
-// {
-//   cudaStream_t stream = handle.get_stream();
-//   auto exec_policy    = handle.get_thrust_policy();
-
-//   // Root is the last edge in the dendrogram
-//   value_idx root = 2 * (n_leaves - 1);
-
-//   auto d_ptr           = thrust::device_pointer_cast(children);
-//   value_idx n_vertices = *(thrust::max_element(exec_policy, d_ptr, d_ptr + root)) + 1;
-
-//   // Prevent potential infinite loop from labeling disconnected
-//   // connectivities graph.
-//   RAFT_EXPECTS(n_vertices == root,
-//                "Multiple components found in MST or MST is invalid. "
-//                "Cannot find single-linkage solution. Found %d vertices "
-//                "total.",
-//                static_cast<int>(n_vertices));
-
-//   rmm::device_uvector<bool> frontier(root + 1, stream);
-//   rmm::device_uvector<bool> next_frontier(root + 1, stream);
-
-//   thrust::fill(exec_policy, frontier.begin(), frontier.end(), false);
-//   thrust::fill(exec_policy, next_frontier.begin(), next_frontier.end(), false);
-
-//   // Array to propagate the lambda of subtrees actively being collapsed
-//   // through multiple bfs iterations.
-//   rmm::device_uvector<value_t> ignore(root + 1, stream);
-
-//   // Propagate labels from root
-//   rmm::device_uvector<value_idx> relabel(root + 1, handle.get_stream());
-//   thrust::fill(exec_policy, relabel.begin(), relabel.end(), -1);
-
-//   raft::update_device(relabel.data() + root, &root, 1, handle.get_stream());
-
-//   // Flip frontier for root
-//   constexpr bool start = true;
-//   raft::update_device(frontier.data() + root, &start, 1, handle.get_stream());
-
-//   rmm::device_uvector<value_idx> out_parent((root + 1) * 2, stream);
-//   rmm::device_uvector<value_idx> out_child((root + 1) * 2, stream);
-//   rmm::device_uvector<value_t> out_lambda((root + 1) * 2, stream);
-//   rmm::device_uvector<value_idx> out_size((root + 1) * 2, stream);
-
-//   thrust::fill(exec_policy, out_parent.begin(), out_parent.end(), -1);
-//   thrust::fill(exec_policy, out_child.begin(), out_child.end(), -1);
-//   thrust::fill(exec_policy, out_lambda.begin(), out_lambda.end(), -1);
-//   thrust::fill(exec_policy, out_size.begin(), out_size.end(), -1);
-//   thrust::fill(exec_policy, ignore.begin(), ignore.end(), -1);
-
-//   // While frontier is not empty, perform single bfs through tree
-//   size_t grid = raft::ceildiv(root + 1, static_cast<value_idx>(tpb));
-
-//   value_idx n_elements_to_traverse =
-//     thrust::reduce(exec_policy, frontier.data(), frontier.data() + root + 1, 0);
-
-//   while (n_elements_to_traverse > 0) {
-//     // TODO: Investigate whether it would be worth performing a gather/argmatch in order
-//     // to schedule only the number of threads needed. (it might not be worth it)
-//     condense_hierarchy_kernel<<<grid, tpb, 0, handle.get_stream()>>>(frontier.data(),
-//                                                                      next_frontier.data(),
-//                                                                      ignore.data(),
-//                                                                      relabel.data(),
-//                                                                      children,
-//                                                                      delta,
-//                                                                      sizes,
-//                                                                      n_leaves,
-//                                                                      min_cluster_size,
-//                                                                      out_parent.data(),
-//                                                                      out_child.data(),
-//                                                                      out_lambda.data(),
-//                                                                      out_size.data());
-
-//     thrust::copy(exec_policy, next_frontier.begin(), next_frontier.end(), frontier.begin());
-//     thrust::fill(exec_policy, next_frontier.begin(), next_frontier.end(), false);
-
-//     n_elements_to_traverse = thrust::reduce(
-//       exec_policy, frontier.data(), frontier.data() + root + 1, static_cast<value_idx>(0));
-
-//     handle.sync_stream(stream);
-//   }
-
-//   condensed_tree.condense(out_parent.data(), out_child.data(), out_lambda.data(),
-//   out_size.data());
-// }
-
-template <typename value_idx, typename value_t, int tpb = 256>
+template <typename value_idx, typename value_t>
 void build_condensed_hierarchy(const raft::handle_t& handle,
                                const value_idx* children,
                                const value_t* delta,
@@ -260,30 +191,32 @@ void build_condensed_hierarchy(const raft::handle_t& handle,
 
   value_idx root = 2 * (n_leaves - 1);
 
+  // copying input arrays to host
   std::vector<value_idx> h_children(2 * (n_leaves - 1));
   std::vector<value_t> h_delta(n_leaves - 1);
   std::vector<value_idx> h_sizes(n_leaves - 1);
-
   raft::copy(h_children.data(), children, 2 * (n_leaves - 1), stream);
   raft::copy(h_delta.data(), delta, n_leaves - 1, stream);
   raft::copy(h_sizes.data(), sizes, n_leaves - 1, stream);
   handle.sync_stream(stream);
 
+  // allocate output arrays
   std::vector<value_idx> h_out_parent((root + 1) * 2, -1);
   std::vector<value_idx> h_out_child((root + 1) * 2, -1);
   std::vector<value_t> h_out_lambda((root + 1) * 2, -1);
   std::vector<value_idx> h_out_sizes((root + 1) * 2, -1);
 
-  condense_hierarchy_pointer_chase(h_children,
-                                   h_delta,
-                                   h_sizes,
-                                   h_out_parent,
-                                   h_out_child,
-                                   h_out_lambda,
-                                   h_out_sizes,
-                                   n_leaves,
-                                   min_cluster_size);
+  condense_hierarchy_cpu(h_children,
+                         h_delta,
+                         h_sizes,
+                         h_out_parent,
+                         h_out_child,
+                         h_out_lambda,
+                         h_out_sizes,
+                         n_leaves,
+                         min_cluster_size);
 
+  // allocate and copy output arrays to device
   rmm::device_uvector<value_idx> out_parent((root + 1) * 2, stream);
   rmm::device_uvector<value_idx> out_child((root + 1) * 2, stream);
   rmm::device_uvector<value_t> out_lambda((root + 1) * 2, stream);
