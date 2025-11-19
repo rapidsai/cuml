21c5182c89724a87816fc11297a59c5834b20d3d
Jim Crist-Harif <jcristharif@gmail.com>
2025-11-19 14:50:26 -0600

    Cleanup `cuml.multiclass` (#7508)
    This:
    
    - Cleans up our multiclass implementations _slightly_. In the future we'll want to do redo these implementations so we're not just wrappers around sklearn (requiring host transfers etc...), but for now at least we're following conventions in #7317.
    - Deprecates `MulticlassClassifier` in favor of `OneVsOneClassifier`/`OneVsRestClassifier`. This is to reduce our estimator maintenance a bit - sklearn only provides the latter 2, no need for us to add another estimator class.
    
    Part of #7317.
    
    Authors:
      - Jim Crist-Harif (https://github.com/jcrist)
    
    Approvers:
      - Anupam (https://github.com/aamijar)
      - Simon Adorf (https://github.com/csadorf)
    
    URL: https://github.com/rapidsai/cuml/pull/7508

--- python/cuml/cuml/multiclass/multiclass.py
+++ python/cuml/cuml/multiclass/multiclass.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: Copyright (c) 2020-2025, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 #
+import warnings
 
 import cuml.internals
 from cuml.common import (
@@ -13,38 +14,107 @@ from cuml.internals.base import Base
 from cuml.internals.mixins import ClassifierMixin
 
 
-class MulticlassClassifier(Base, ClassifierMixin):
+class _BaseMulticlassClassifier(Base, ClassifierMixin):
+    """Shared base class for multiclass classifiers"""
+
+    def __init__(
+        self,
+        estimator,
+        *,
+        handle=None,
+        verbose=False,
+        output_type=None,
+    ):
+        super().__init__(
+            handle=handle, verbose=verbose, output_type=output_type
+        )
+        self.estimator = estimator
+
+    @classmethod
+    def _get_param_names(cls):
+        return [*super()._get_param_names(), "estimator"]
+
+    @property
+    @cuml.internals.api_base_return_array_skipall
+    def classes_(self):
+        return self.multiclass_estimator.classes_
+
+    @generate_docstring(y="dense_anydtype")
+    def fit(self, X, y) -> "_BaseMulticlassClassifier":
+        """
+        Fit a multiclass classifier.
+        """
+        import sklearn.multiclass
+
+        opts = {
+            "ovo": sklearn.multiclass.OneVsOneClassifier,
+            "ovr": sklearn.multiclass.OneVsRestClassifier,
+        }
+        if (cls := opts.get(self.strategy)) is None:
+            raise ValueError(
+                f"Expected `strategy` to be one of {list(opts)}, got {self.strategy}"
+            )
+        X = input_to_host_array_with_sparse_support(X)
+        y = input_to_host_array(y).array
+
+        with cuml.internals.exit_internal_api():
+            wrapper = cls(self.estimator, n_jobs=None).fit(X, y)
+
+        self.multiclass_estimator = wrapper
+        return self
+
+    @generate_docstring(
+        return_values={
+            "name": "preds",
+            "type": "dense",
+            "description": "Predicted values",
+            "shape": "(n_samples, 1)",
+        }
+    )
+    def predict(self, X) -> CumlArray:
+        """
+        Predict using multi class classifier.
+        """
+        X = input_to_host_array_with_sparse_support(X)
+
+        with cuml.internals.exit_internal_api():
+            return self.multiclass_estimator.predict(X)
+
+    @generate_docstring(
+        return_values={
+            "name": "results",
+            "type": "dense",
+            "description": "Decision function values",
+            "shape": "(n_samples, 1)",
+        }
+    )
+    def decision_function(self, X) -> CumlArray:
+        """
+        Calculate the decision function.
+        """
+        X = input_to_host_array_with_sparse_support(X)
+        with cuml.internals.exit_internal_api():
+            return self.multiclass_estimator.decision_function(X)
+
+
+class MulticlassClassifier(_BaseMulticlassClassifier):
     """
     Wrapper around scikit-learn multiclass classifiers that allows to
     choose different multiclass strategies.
 
+    .. deprecated:: 25.12
+
+        This estimator was deprecated in 25.12 and will be removed in 26.02.
+        Please use OneVsOneClassifier or OneVsRestClassifier directly instead.
+
     The input can be any kind of cuML compatible array, and the output type
     follows cuML's output type configuration rules.
 
-    Berofe passing the data to scikit-learn, it is converted to host (numpy)
+    Before passing the data to scikit-learn, it is converted to host (numpy)
     array. Under the hood the data is partitioned for binary classification,
     and it is transformed back to the device by the cuML estimator. These
     copies back and forth the device and the host have some overhead. For more
     details see issue https://github.com/rapidsai/cuml/issues/2876.
-
-    Examples
-    --------
-
-    .. code-block:: python
-
-        >>> from cuml.linear_model import LogisticRegression
-        >>> from cuml.multiclass import MulticlassClassifier
-        >>> from cuml.datasets.classification import make_classification
-
-        >>> X, y = make_classification(n_samples=10, n_features=6,
-        ...                            n_informative=4, n_classes=3,
-        ...                            random_state=137)
-
-        >>> cls = MulticlassClassifier(LogisticRegression(), strategy='ovo')
-        >>> cls.fit(X, y)
-        MulticlassClassifier(estimator=LogisticRegression())
-        >>> cls.predict(X)
-        array([1, 1, 0, 1, 1, 1, 2, 2, 1, 2])
 
     Parameters
     ----------
@@ -76,6 +146,21 @@ class MulticlassClassifier(Base, ClassifierMixin):
     n_classes_ : int
         Number of classes.
 
+    Examples
+    --------
+    >>> from cuml.linear_model import LogisticRegression
+    >>> from cuml.multiclass import MulticlassClassifier
+    >>> from cuml.datasets.classification import make_classification
+
+    >>> X, y = make_classification(n_samples=10, n_features=6,
+    ...                            n_informative=4, n_classes=3,
+    ...                            random_state=137)
+
+    >>> cls = MulticlassClassifier(LogisticRegression(), strategy='ovo')
+    >>> cls.fit(X, y)
+    MulticlassClassifier(estimator=LogisticRegression())
+    >>> cls.predict(X)
+    array([1, 1, 0, 1, 1, 1, 2, 2, 1, 2])
     """
 
     def __init__(
@@ -87,93 +172,30 @@ class MulticlassClassifier(Base, ClassifierMixin):
         output_type=None,
         strategy="ovr",
     ):
+        warnings.warn(
+            "MulticlassClassifier was deprecated in version 25.12 and will be "
+            "removed in version 26.02. Please use OneVsOneClassifier or "
+            "OneVsRestClassifier directly instead.",
+            FutureWarning,
+        )
+
         super().__init__(
-            handle=handle, verbose=verbose, output_type=output_type
+            estimator, handle=handle, verbose=verbose, output_type=output_type
         )
         self.strategy = strategy
-        self.estimator = estimator
-
-        import sklearn.multiclass
-
-        if self.strategy == "ovr":
-            self.multiclass_estimator = sklearn.multiclass.OneVsRestClassifier(
-                self.estimator, n_jobs=None
-            )
-        elif self.strategy == "ovo":
-            self.multiclass_estimator = sklearn.multiclass.OneVsOneClassifier(
-                self.estimator, n_jobs=None
-            )
-        else:
-            raise ValueError(
-                "Invalid multiclass strategy "
-                + str(self.strategy)
-                + ", must be one of "
-                '{"ovr", "ovo"}'
-            )
-
-    @property
-    @cuml.internals.api_base_return_array_skipall
-    def classes_(self):
-        return self.multiclass_estimator.classes_
-
-    @generate_docstring(y="dense_anydtype")
-    def fit(self, X, y) -> "MulticlassClassifier":
-        """
-        Fit a multiclass classifier.
-        """
-        X = input_to_host_array_with_sparse_support(X)
-
-        y = input_to_host_array(y).array
-        with cuml.internals.exit_internal_api():
-            self.multiclass_estimator.fit(X, y)
-            return self
-
-    @generate_docstring(
-        return_values={
-            "name": "preds",
-            "type": "dense",
-            "description": "Predicted values",
-            "shape": "(n_samples, 1)",
-        }
-    )
-    def predict(self, X) -> CumlArray:
-        """
-        Predict using multi class classifier.
-        """
-        X = input_to_host_array_with_sparse_support(X)
-
-        with cuml.internals.exit_internal_api():
-            return self.multiclass_estimator.predict(X)
-
-    @generate_docstring(
-        return_values={
-            "name": "results",
-            "type": "dense",
-            "description": "Decision function \
-                                       values",
-            "shape": "(n_samples, 1)",
-        }
-    )
-    def decision_function(self, X) -> CumlArray:
-        """
-        Calculate the decision function.
-        """
-        X = input_to_host_array_with_sparse_support(X)
-        with cuml.internals.exit_internal_api():
-            return self.multiclass_estimator.decision_function(X)
 
     @classmethod
     def _get_param_names(cls):
-        return super()._get_param_names() + ["estimator", "strategy"]
+        return [*super()._get_param_names(), "strategy"]
 
 
-class OneVsRestClassifier(MulticlassClassifier):
+class OneVsRestClassifier(_BaseMulticlassClassifier):
     """
     Wrapper around Sckit-learn's class with the same name. The input can be
     any kind of cuML compatible array, and the output type follows cuML's
     output type configuration rules.
 
-    Berofe passing the data to scikit-learn, it is converted to host (numpy)
+    Before passing the data to scikit-learn, it is converted to host (numpy)
     array. Under the hood the data is partitioned for binary classification,
     and it is transformed back to the device by the cuML estimator. These
     copies back and forth the device and the host have some overhead. For more
@@ -182,26 +204,6 @@ class OneVsRestClassifier(MulticlassClassifier):
     For documentation see `scikit-learn's OneVsRestClassifier
     <https://scikit-learn.org/stable/modules/generated/sklearn.multiclass.OneVsRestClassifier.html>`_.
 
-    Examples
-    --------
-
-    .. code-block:: python
-
-        >>> from cuml.linear_model import LogisticRegression
-        >>> from cuml.multiclass import OneVsRestClassifier
-        >>> from cuml.datasets.classification import make_classification
-
-        >>> X, y = make_classification(n_samples=10, n_features=6,
-        ...                            n_informative=4, n_classes=3,
-        ...                            random_state=137)
-
-        >>> cls = OneVsRestClassifier(LogisticRegression())
-        >>> cls.fit(X, y)
-        OneVsRestClassifier(estimator=LogisticRegression())
-        >>> cls.predict(X)
-        array([1, 1, 0, 1, 1, 1, 2, 2, 1, 2])
-
-
     Parameters
     ----------
     estimator : cuML estimator
@@ -221,34 +223,34 @@ class OneVsRestClassifier(MulticlassClassifier):
         type. If None, the output type set at the module level
         (`cuml.global_settings.output_type`) will be used. See
         :ref:`output-data-type-configuration` for more info.
+
+    Examples
+    --------
+    >>> from cuml.linear_model import LogisticRegression
+    >>> from cuml.multiclass import OneVsRestClassifier
+    >>> from cuml.datasets.classification import make_classification
+
+    >>> X, y = make_classification(n_samples=10, n_features=6,
+    ...                            n_informative=4, n_classes=3,
+    ...                            random_state=137)
+
+    >>> cls = OneVsRestClassifier(LogisticRegression())
+    >>> cls.fit(X, y)
+    OneVsRestClassifier(estimator=LogisticRegression())
+    >>> cls.predict(X)
+    array([1, 1, 0, 1, 1, 1, 2, 2, 1, 2])
     """
 
-    def __init__(
-        self, estimator, *args, handle=None, verbose=False, output_type=None
-    ):
-        super().__init__(
-            estimator,
-            *args,
-            handle=handle,
-            verbose=verbose,
-            output_type=output_type,
-            strategy="ovr",
-        )
-
-    @classmethod
-    def _get_param_names(cls):
-        param_names = super()._get_param_names()
-        param_names.remove("strategy")
-        return param_names
+    strategy = "ovr"
 
 
-class OneVsOneClassifier(MulticlassClassifier):
+class OneVsOneClassifier(_BaseMulticlassClassifier):
     """
     Wrapper around Sckit-learn's class with the same name. The input can be
     any kind of cuML compatible array, and the output type follows cuML's
     output type configuration rules.
 
-    Berofe passing the data to scikit-learn, it is converted to host (numpy)
+    Before passing the data to scikit-learn, it is converted to host (numpy)
     array. Under the hood the data is partitioned for binary classification,
     and it is transformed back to the device by the cuML estimator. These
     copies back and forth the device and the host have some overhead. For more
@@ -257,25 +259,6 @@ class OneVsOneClassifier(MulticlassClassifier):
     For documentation see `scikit-learn's OneVsOneClassifier
     <https://scikit-learn.org/stable/modules/generated/sklearn.multiclass.OneVsOneClassifier.html>`_.
 
-    Examples
-    --------
-
-    .. code-block:: python
-
-        >>> from cuml.linear_model import LogisticRegression
-        >>> from cuml.multiclass import OneVsOneClassifier
-        >>> from cuml.datasets.classification import make_classification
-
-        >>> X, y = make_classification(n_samples=10, n_features=6,
-        ...                            n_informative=4, n_classes=3,
-        ...                            random_state=137)
-
-        >>> cls = OneVsOneClassifier(LogisticRegression())
-        >>> cls.fit(X, y)
-        OneVsOneClassifier(estimator=LogisticRegression())
-        >>> cls.predict(X)
-        array([1, 1, 0, 1, 1, 1, 2, 2, 1, 2])
-
     Parameters
     ----------
     estimator : cuML estimator
@@ -295,22 +278,22 @@ class OneVsOneClassifier(MulticlassClassifier):
         type. If None, the output type set at the module level
         (`cuml.global_settings.output_type`) will be used. See
         :ref:`output-data-type-configuration` for more info.
+
+    Examples
+    --------
+    >>> from cuml.linear_model import LogisticRegression
+    >>> from cuml.multiclass import OneVsOneClassifier
+    >>> from cuml.datasets.classification import make_classification
+
+    >>> X, y = make_classification(n_samples=10, n_features=6,
+    ...                            n_informative=4, n_classes=3,
+    ...                            random_state=137)
+
+    >>> cls = OneVsOneClassifier(LogisticRegression())
+    >>> cls.fit(X, y)
+    OneVsOneClassifier(estimator=LogisticRegression())
+    >>> cls.predict(X)
+    array([1, 1, 0, 1, 1, 1, 2, 2, 1, 2])
     """
 
-    def __init__(
-        self, estimator, *args, handle=None, verbose=False, output_type=None
-    ):
-        super().__init__(
-            estimator,
-            *args,
-            handle=handle,
-            verbose=verbose,
-            output_type=output_type,
-            strategy="ovo",
-        )
-
-    @classmethod
-    def _get_param_names(cls):
-        param_names = super()._get_param_names()
-        param_names.remove("strategy")
-        return param_names
+    strategy = "ovo"
