32237b5396c94ca0d7e490f344c4aecbd9731c7e
aamijar <aamijar230@gmail.com>
2025-11-20 16:30:31 +0000

    remove unused

--- python/cuml/cuml/cluster/spectral_clustering.pyx
+++ python/cuml/cuml/cluster/spectral_clustering.pyx
@@ -14,30 +14,12 @@ from cuml.internals.array import CumlArray
 from cuml.internals.base import Base
 from cuml.internals.utils import check_random_seed
 
-from libc.stdint cimport int64_t, uint64_t, uintptr_t
-from libcpp cimport bool
+from libc.stdint cimport uint64_t, uintptr_t
 from pylibraft.common.cpp.mdspan cimport (
     device_vector_view,
     make_device_vector_view,
 )
-from pylibraft.common.handle cimport device_resources, handle_t
-
-
-cdef extern from "cuml/datasets/make_blobs.hpp" namespace "ML::Datasets" nogil:
-    cdef void make_blobs(const handle_t& handle,
-                         float* out,
-                         int64_t* labels,
-                         int64_t n_rows,
-                         int64_t n_cols,
-                         int64_t n_clusters,
-                         bool row_major,
-                         const float* centers,
-                         const float* cluster_std,
-                         const float cluster_std_scalar,
-                         bool shuffle,
-                         float center_box_min,
-                         float center_box_max,
-                         uint64_t seed) except +
+from pylibraft.common.handle cimport device_resources
 
 
 cdef extern from "cuml/cluster/spectral_clustering.hpp" \
@@ -162,58 +144,3 @@ class SpectralClustering(Base):
     def fit(self, X, y=None) -> "SpectralClustering":
         self.fit_predict(X, y)
         return self
-
-
-def raft_make_blobs(n_samples, n_features, n_clusters,
-                    cluster_std=1.0, shuffle=True,
-                    center_box_min=-10.0, center_box_max=10.0,
-                    random_state=None, handle=None):
-    """
-    Wrapper for raft's C++ make_blobs implementation.
-    This ensures we use the same data generation as the C++ tests.
-    """
-    if handle is None:
-        handle = Handle()
-
-    cdef handle_t *handle_ = <handle_t*><size_t>handle.getHandle()
-
-    # Allocate output arrays
-    X = cp.empty((n_samples, n_features), dtype=np.float32)
-    y = cp.empty(n_samples, dtype=np.int64)
-
-    # Get pointers
-    cdef uintptr_t X_ptr = X.data.ptr
-    cdef uintptr_t y_ptr = y.data.ptr
-
-    # Set random seed
-    if random_state is None:
-        random_state = 0
-    cdef uint64_t seed = check_random_seed(random_state)
-
-    # Cast Python variables to C types before nogil block
-    cdef int64_t c_n_samples = n_samples
-    cdef int64_t c_n_features = n_features
-    cdef int64_t c_n_clusters = n_clusters
-    cdef float c_cluster_std = cluster_std
-    cdef bool c_shuffle = shuffle
-    cdef float c_center_box_min = center_box_min
-    cdef float c_center_box_max = center_box_max
-
-    # Call C++ make_blobs
-    with nogil:
-        make_blobs(handle_[0],
-                   <float*>X_ptr,
-                   <int64_t*>y_ptr,
-                   c_n_samples,
-                   c_n_features,
-                   c_n_clusters,
-                   True,  # row_major
-                   NULL,  # centers
-                   NULL,  # cluster_std array
-                   c_cluster_std,
-                   c_shuffle,
-                   c_center_box_min,
-                   c_center_box_max,
-                   seed)
-
-    return X, y
