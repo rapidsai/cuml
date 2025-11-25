6dbbe54646f5474141c314923af70152c9093a4a
Simon Adorf <sadorf@nvidia.com>
2025-11-25 14:24:59 -0600

    CI: Add or adjust job-specific timeouts for GHA test jobs (#7533)
    Adds or adjusts job-specific timeouts for all GHA test jobs.
    
    Authors:
      - Simon Adorf (https://github.com/csadorf)
    
    Approvers:
      - Nate Rock (https://github.com/rockhowse)
    
    URL: https://github.com/rapidsai/cuml/pull/7533

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -48,14 +48,14 @@ rapids-logger "Testing libcuml linkage"
 python -m pytest --cache-clear python/libcuml/tests/test_libcuml_linkage.py -v
 
 rapids-logger "pytest cuml single GPU"
-./ci/run_cuml_singlegpu_pytests.sh \
+timeout 1h ./ci/run_cuml_singlegpu_pytests.sh \
   --numprocesses=8 \
   --dist=worksteal \
   -k 'not test_sparse_pca_inputs' \
   --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml.xml"
 
 # Run test_sparse_pca_inputs separately
-./ci/run_cuml_singlegpu_pytests.sh \
+timeout 1h ./ci/run_cuml_singlegpu_pytests.sh \
   -k 'test_sparse_pca_inputs' \
   --junitxml="${RAPIDS_TESTS_DIR}/junit-cuml-sparse-pca.xml"
 
