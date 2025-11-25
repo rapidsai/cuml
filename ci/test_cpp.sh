5c9e23a1e590d36d8140c8913bea584b3f908d15
aamijar <aamijar230@gmail.com>
2025-11-25 06:45:54 +0000

    fix conflicts

--- ci/test_cpp.sh
+++ ci/test_cpp.sh
@@ -9,8 +9,6 @@ cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../
 
 . /opt/conda/etc/profile.d/conda.sh
 
-rapids-logger "Configuring conda strict channel priority"
-conda config --set channel_priority strict
 
 rapids-logger "Downloading artifacts from previous jobs"
 CPP_CHANNEL=$(rapids-download-conda-from-github cpp)
