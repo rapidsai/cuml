be957370900cb625d66ca7c56db198e1187386ea
Jim Crist-Harif <jcristharif@gmail.com>
2025-12-10 10:21:09 -0600

    Readd `memory_utils` as a deprecated namespace

--- python/cuml/cuml/internals/__init__.py
+++ python/cuml/cuml/internals/__init__.py
@@ -2,6 +2,8 @@
 # SPDX-FileCopyrightText: Copyright (c) 2019-2025, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 #
+# TODO: remove in 26.04
+import cuml.internals.memory_utils
 from cuml.internals.base import Base
 from cuml.internals.internals import GraphBasedDimRedCallback
 from cuml.internals.outputs import (
---
+++ python/cuml/cuml/internals/memory_utils.py
@@ -0,0 +1,20 @@
+#
+# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION.
+# SPDX-License-Identifier: Apache-2.0
+#
+def __getattr__(name):
+    import warnings
+
+    if name in ("set_global_output_type", "using_output_type"):
+        warnings.warn(
+            f"Accessing {name!r} from the `cuml.internals.memory_utils` "
+            f"namespace is deprecated and will be removed in 26.04. Please "
+            f"use `cuml.{name}` instead.",
+            FutureWarning,
+        )
+        import cuml.internals.outputs as mod
+
+        return getattr(mod, name)
+    raise AttributeError(
+        f"module 'cuml.internals.memory_utils' has no attribute {name!r}"
+    )
