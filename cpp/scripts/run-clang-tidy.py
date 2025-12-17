a1a23c34922537f2cd8f63a9884be2a45e74019c
Divye Gala <divyegala@gmail.com>
2025-12-17 00:11:16 +0000

    update clang-tidy

--- cpp/scripts/run-clang-tidy.py
+++ cpp/scripts/run-clang-tidy.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 import tomli
 
-EXPECTED_VERSION = "15.0.7"
+EXPECTED_VERSION = "20.1.4"
 VERSION_REGEX = re.compile(r"  LLVM version ([0-9.]+)")
 GPU_ARCH_REGEX = re.compile(r"sm_(\d+)")
 SPACES = re.compile(r"\s+")
