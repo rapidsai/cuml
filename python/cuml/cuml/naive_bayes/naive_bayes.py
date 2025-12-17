5df1b0aafcf97f46b75ecb100ed8a6ee0a315bc4
Jim Crist-Harif <jcristharif@gmail.com>
2025-12-17 13:34:41 -0600

    Delete some unnecessary definitions

--- python/cuml/cuml/naive_bayes/naive_bayes.py
+++ python/cuml/cuml/naive_bayes/naive_bayes.py
@@ -1349,29 +1349,6 @@ class BernoulliNB(_BaseDiscreteNB):
             smoothed_cc.reshape(-1, 1)
         )
 
-    def fit(self, X, y, sample_weight=None):
-        """
-        Fit Naive Bayes classifier with enhanced error handling
-
-        Parameters
-        ----------
-        X : {array-like, sparse matrix} of shape (n_samples, n_features)
-            Training vectors, where n_samples is the number of samples and
-            n_features is the number of features.
-        y : array-like of shape (n_samples,)
-            Target values.
-        sample_weight : array-like of shape (n_samples,), default=None
-            Weights applied to individual samples (1. for unweighted).
-
-        Returns
-        -------
-        self : object
-        """
-        # Reset internal state to ensure clean fit
-        self.fit_called_ = False
-        # Call parent's fit through partial_fit
-        return super().fit(X, y, sample_weight)
-
     @classmethod
     def _get_param_names(cls):
         return super()._get_param_names() + ["binarize"]
@@ -1693,72 +1670,6 @@ class CategoricalNB(_BaseDiscreteNB):
         if x_min < 0:
             raise ValueError("Negative values in data passed to CategoricalNB")
         return X
-
-    @reflect(reset=True)
-    def fit(self, X, y, sample_weight=None) -> "CategoricalNB":
-        """Fit Naive Bayes classifier according to X, y
-
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-            Training vectors, where n_samples is the number of samples and
-            n_features is the number of features. Here, each feature of X is
-            assumed to be from a different categorical distribution.
-            It is further assumed that all categories of each feature are
-            represented by the numbers 0, ..., n - 1, where n refers to the
-            total number of categories for the given feature. This can, for
-            instance, be achieved with the help of OrdinalEncoder.
-        y : array-like of shape (n_samples,)
-            Target values.
-        sample_weight : array-like of shape (n_samples), default=None
-            Weights applied to individual samples (1. for unweighted).
-            Currently sample weight is ignored.
-
-        Returns
-        -------
-        self : object
-        """
-        return super().fit(X, y, sample_weight=sample_weight)
-
-    @reflect(reset=True)
-    def partial_fit(
-        self, X, y, classes=None, sample_weight=None
-    ) -> "CategoricalNB":
-        """Incremental fit on a batch of samples.
-        This method is expected to be called several times consecutively
-        on different chunks of a dataset so as to implement out-of-core
-        or online learning.
-        This is especially useful when the whole dataset is too big to fit in
-        memory at once.
-        This method has some performance overhead hence it is better to call
-        partial_fit on chunks of data that are as large as possible
-        (as long as fitting in the memory budget) to hide the overhead.
-
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-            Training vectors, where n_samples is the number of samples and
-            n_features is the number of features. Here, each feature of X is
-            assumed to be from a different categorical distribution.
-            It is further assumed that all categories of each feature are
-            represented by the numbers 0, ..., n - 1, where n refers to the
-            total number of categories for the given feature. This can, for
-            instance, be achieved with the help of OrdinalEncoder.
-        y : array-like of shape (n_samples)
-            Target values.
-        classes : array-like of shape (n_classes), default=None
-            List of all the classes that can possibly appear in the y vector.
-            Must be provided at the first call to partial_fit, can be omitted
-            in subsequent calls.
-        sample_weight : array-like of shape (n_samples), default=None
-            Weights applied to individual samples (1. for unweighted).
-            Currently sample weight is ignored.
-
-        Returns
-        -------
-        self : object
-        """
-        return super().partial_fit(X, y, classes, sample_weight=sample_weight)
 
     def _count_sparse(
         self, x_coo_rows, x_coo_cols, x_coo_data, x_shape, Y, classes
