c51856931f784194c93cffb84ed80c1b7322c60b
Simon Adorf <sadorf@nvidia.com>
2026-01-02 13:20:30 -0600

    Fix handle parameter type doc-string.

--- python/cuml/cuml/covariance/ledoit_wolf.py
+++ python/cuml/cuml/covariance/ledoit_wolf.py
@@ -113,13 +113,12 @@ class LedoitWolf(Base, InteropMixin):
         Size of blocks into which the covariance matrix will be split
         during its Ledoit-Wolf estimation. This is purely a memory
         optimization and does not affect results.
-    handle : cuml.Handle
-        Specifies the cuml.handle that holds internal CUDA state for
-        computations in this model. Most importantly, this specifies the
-        CUDA stream that will be used for the model's computations, so
-        users can run different models concurrently in different streams
-        by creating handles in several streams.
-        If it is None, a new one is created.
+    handle : cuml.Handle or None, default=None
+
+        .. deprecated:: 26.02
+            The `handle` argument was deprecated in 26.02 and will be removed
+            in 26.04. There's no need to pass in a handle, cuml now manages
+            this resource automatically.
     verbose : int or boolean, default=False
         Sets logging level. It must be one of `cuml.common.logger.level_*`.
         See :ref:`verbosity-levels` for more info.
