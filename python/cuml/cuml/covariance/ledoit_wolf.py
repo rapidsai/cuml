2f29a8a0a3f8eee813f132010518e9fe6db2a829
Simon Adorf <sadorf@nvidia.com>
2026-01-02 13:20:46 -0600

    Fix output_type doc-string.

--- python/cuml/cuml/covariance/ledoit_wolf.py
+++ python/cuml/cuml/covariance/ledoit_wolf.py
@@ -123,7 +123,7 @@ class LedoitWolf(Base, InteropMixin):
         Sets logging level. It must be one of `cuml.common.logger.level_*`.
         See :ref:`verbosity-levels` for more info.
     output_type : {'input', 'array', 'dataframe', 'series', 'df_obj', \
-            'numba', 'cupy', 'numpy', 'cudf', 'pandas'}, default=None
+        'numba', 'cupy', 'numpy', 'cudf', 'pandas'}, default=None
         Return results and set estimator attributes to the indicated output
         type. If None, the output type set at the module level
         (`cuml.global_settings.output_type`) will be used. See
