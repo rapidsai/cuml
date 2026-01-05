0ff159cc3b128fb973ff624d8ac4870a5d821de8
Tim Head <betatim@gmail.com>
2026-01-05 13:40:11 +0100

    Copyrights

--- python/cuml/tests/test_lars.py
+++ python/cuml/tests/test_lars.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2020-2026, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2020-20265 NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 #
 
