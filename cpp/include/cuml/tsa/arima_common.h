f9726a2f1aff1f358c4408b0410b829ee814790a
Divye Gala <divyegala@gmail.com>
2026-01-14 15:27:53 -0800

    Avoid installing CCCL headers in wheels (#7662)
    Uncompressed size of CCCL headers: 19M
    No of headers per CCCL library:
    ```
    cudax/include:
    208
    cub/cub:
    172
    libcudacxx/include/cuda:
    950
    thrust/thrust:
    729
    ```
    
    Before this PR:
    * compressed size: 0.232G
    * uncompressed size: 0.273G
    
    After this PR:
    * compressed size: 0.229G
    * uncompressed size: 0.258G
    
    Authors:
      - Divye Gala (https://github.com/divyegala)
      - Jinsol Park (https://github.com/jinsolp)
    
    Approvers:
      - James Lamb (https://github.com/jameslamb)
      - Simon Adorf (https://github.com/csadorf)
    
    URL: https://github.com/rapidsai/cuml/pull/7662

--- cpp/include/cuml/tsa/arima_common.h
+++ cpp/include/cuml/tsa/arima_common.h
@@ -12,9 +12,6 @@
 #include <rmm/resource_ref.hpp>
 
 #include <cuda_runtime.h>
-#include <thrust/execution_policy.h>
-#include <thrust/for_each.h>
-#include <thrust/iterator/counting_iterator.h>
 
 #include <algorithm>
 
@@ -112,43 +109,7 @@ struct ARIMAParams {
    *                         [mu, ar, ma, sar, sma, sigma2] (device)
    * @param[in]  stream      CUDA stream
    */
-  void pack(const ARIMAOrder& order, int batch_size, DataT* param_vec, cudaStream_t stream) const
-  {
-    int N         = order.complexity();
-    auto counting = thrust::make_counting_iterator(0);
-    // The device lambda can't capture structure members...
-    const DataT *_mu = mu, *_beta = beta, *_ar = ar, *_ma = ma, *_sar = sar, *_sma = sma,
-                *_sigma2 = sigma2;
-    thrust::for_each(
-      thrust::cuda::par.on(stream), counting, counting + batch_size, [=] __device__(int bid) {
-        DataT* param = param_vec + bid * N;
-        if (order.k) {
-          *param = _mu[bid];
-          param++;
-        }
-        for (int i = 0; i < order.n_exog; i++) {
-          param[i] = _beta[order.n_exog * bid + i];
-        }
-        param += order.n_exog;
-        for (int ip = 0; ip < order.p; ip++) {
-          param[ip] = _ar[order.p * bid + ip];
-        }
-        param += order.p;
-        for (int iq = 0; iq < order.q; iq++) {
-          param[iq] = _ma[order.q * bid + iq];
-        }
-        param += order.q;
-        for (int iP = 0; iP < order.P; iP++) {
-          param[iP] = _sar[order.P * bid + iP];
-        }
-        param += order.P;
-        for (int iQ = 0; iQ < order.Q; iQ++) {
-          param[iQ] = _sma[order.Q * bid + iQ];
-        }
-        param += order.Q;
-        *param = _sigma2[bid];
-      });
-  }
+  void pack(const ARIMAOrder& order, int batch_size, DataT* param_vec, cudaStream_t stream) const;
 
   /**
    * Unpack a parameter vector into separate arrays of parameters.
@@ -159,43 +120,7 @@ struct ARIMAParams {
    *                        [mu, ar, ma, sar, sma, sigma2] (device)
    * @param[in]  stream     CUDA stream
    */
-  void unpack(const ARIMAOrder& order, int batch_size, const DataT* param_vec, cudaStream_t stream)
-  {
-    int N         = order.complexity();
-    auto counting = thrust::make_counting_iterator(0);
-    // The device lambda can't capture structure members...
-    DataT *_mu = mu, *_beta = beta, *_ar = ar, *_ma = ma, *_sar = sar, *_sma = sma,
-          *_sigma2 = sigma2;
-    thrust::for_each(
-      thrust::cuda::par.on(stream), counting, counting + batch_size, [=] __device__(int bid) {
-        const DataT* param = param_vec + bid * N;
-        if (order.k) {
-          _mu[bid] = *param;
-          param++;
-        }
-        for (int i = 0; i < order.n_exog; i++) {
-          _beta[order.n_exog * bid + i] = param[i];
-        }
-        param += order.n_exog;
-        for (int ip = 0; ip < order.p; ip++) {
-          _ar[order.p * bid + ip] = param[ip];
-        }
-        param += order.p;
-        for (int iq = 0; iq < order.q; iq++) {
-          _ma[order.q * bid + iq] = param[iq];
-        }
-        param += order.q;
-        for (int iP = 0; iP < order.P; iP++) {
-          _sar[order.P * bid + iP] = param[iP];
-        }
-        param += order.P;
-        for (int iQ = 0; iQ < order.Q; iQ++) {
-          _sma[order.Q * bid + iQ] = param[iQ];
-        }
-        param += order.Q;
-        _sigma2[bid] = *param;
-      });
-  }
+  void unpack(const ARIMAOrder& order, int batch_size, const DataT* param_vec, cudaStream_t stream);
 };
 
 /**
