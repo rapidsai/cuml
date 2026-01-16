a0980aec00d9e72e58fb3536f9cc3a5d6bb01ad4
Robert Maynard <rmaynard@nvidia.com>
2026-01-16 13:50:43 -0500

    Update copright text

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# SPDX-FileCopyrightText: Copyright (c) 2019-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2019-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
 # cuml build script
