f363239b3fd4b16e86a38414e9cbf23a54aa6f09
vic <viclafargue@nvidia.com>
2026-01-23 16:01:55 +0100

    document to_cuvs

--- cpp/include/cuml/matrix/kernel_params.hpp
+++ cpp/include/cuml/matrix/kernel_params.hpp
@@ -21,6 +21,13 @@ struct KernelParams {
   double gamma;
   double coef0;
 
+  /**
+   * @brief Convert to cuvs KernelParams.
+   *
+   * @note For PRECOMPUTED kernels, the returned cuvs params will have kernel_type
+   *       set to LINEAR as a placeholder, since cuvs doesn't have a PRECOMPUTED type.
+   *       The kernel value won't be used in this case.
+   */
   cuvs::distance::kernels::KernelParams to_cuvs() const;
 };
 
