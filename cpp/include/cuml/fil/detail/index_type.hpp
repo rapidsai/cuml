3205eebe2be5c20a4c4fc86a24e6e84b26c5667c
Hyunsu Cho <phcho@nvidia.com>
2026-01-28 01:02:31 -0800

    Fix integer overflow in FIL

--- cpp/include/cuml/fil/detail/index_type.hpp
+++ cpp/include/cuml/fil/detail/index_type.hpp
@@ -1,11 +1,11 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
  */
 #pragma once
 #include <stdint.h>
 namespace ML {
 namespace fil {
-using index_type = uint32_t;
+using index_type = uint64_t;
 }
 }  // namespace ML
