ac7297be5f77956812d5ba2782a7a6756b24d4d8
Gil Forsyth <gforsyth@nvidia.com>
2026-02-03 13:43:26 -0500

    chore: add missing curly braces

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 rapids-logger "Downloading artifacts from previous jobs"
 CPP_CHANNEL=$(rapids-download-conda-from-github cpp)
-PYTHON_CHANNEL=$(rapids-download-from-github "$(rapids-package-name "conda_python" cuml --stable --cuda "$RAPIDS_CUDA_VERSION}")")
+PYTHON_CHANNEL=$(rapids-download-from-github "$(rapids-package-name "conda_python" cuml --stable --cuda "${RAPIDS_CUDA_VERSION}")")
 
 rapids-logger "Create test conda environment"
 . /opt/conda/etc/profile.d/conda.sh
--- ci/test_python_common.sh
+++ ci/test_python_common.sh
@@ -11,7 +11,7 @@ conda config --set channel_priority strict
 
 rapids-logger "Downloading artifacts from previous jobs"
 CPP_CHANNEL=$(rapids-download-conda-from-github cpp)
-PYTHON_CHANNEL=$(rapids-download-from-github "$(rapids-package-name "conda_python" cuml --stable --cuda "$RAPIDS_CUDA_VERSION}")")
+PYTHON_CHANNEL=$(rapids-download-from-github "$(rapids-package-name "conda_python" cuml --stable --cuda "${RAPIDS_CUDA_VERSION}")")
 
 rapids-logger "Generate Python testing dependencies"
 rapids-dependency-file-generator \
