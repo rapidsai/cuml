89cceb1cf0577a45247881b903ae43f4262cbe46
Jim Crist-Harif <jcristharif@gmail.com>
2026-02-18 14:00:30 -0600

    Support `radius_neighbors_graph` in `cuml.accel`

--- python/cuml/cuml/accel/_wrappers/sklearn/neighbors.py
+++ python/cuml/cuml/accel/_wrappers/sklearn/neighbors.py
@@ -1,10 +1,12 @@
 #
-# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 #
 
 import cuml.neighbors
 from cuml.accel.estimator_proxy import ProxyBase
+from cuml.common.sparse_utils import is_sparse
+from cuml.internals.interop import UnsupportedOnGPU
 
 __all__ = (
     "NearestNeighbors",
@@ -17,6 +19,21 @@ __all__ = (
 class NearestNeighbors(ProxyBase):
     _gpu_class = cuml.neighbors.NearestNeighbors
     _other_attributes = frozenset(("_fit_method", "_tree", "_fit_X"))
+
+    def _gpu_radius_neighbors_graph(
+        self, X=None, radius=None, mode="connectivity", sort_results=False
+    ):
+        if mode != "connectivity":
+            raise UnsupportedOnGPU(f"`mode={mode!r}` is not supported")
+        if sort_results:
+            raise UnsupportedOnGPU("`sort_results=True` is not supported")
+        if is_sparse(X):
+            raise UnsupportedOnGPU("Sparse inputs are not supported")
+        if self.effective_metric_ not in cuml.neighbors.VALID_METRICS["rbc"]:
+            raise UnsupportedOnGPU(
+                f"metric={self.effective_metric_!r} is not supported"
+            )
+        return self._gpu.radius_neighbors_graph(X=X, radius=radius)
 
 
 class KNeighborsClassifier(ProxyBase):
