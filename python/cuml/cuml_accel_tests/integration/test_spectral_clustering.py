a779f6099461573b41a32828e25d7e0c4cfac0c8
Anupam <54245698+aamijar@users.noreply.github.com>
2026-03-10 16:13:02 -0700

    Update python/cuml/cuml_accel_tests/integration/test_spectral_clustering.py
    Co-authored-by: Victor Lafargue <viclafargue@nvidia.com>

--- python/cuml/cuml_accel_tests/integration/test_spectral_clustering.py
+++ python/cuml/cuml_accel_tests/integration/test_spectral_clustering.py
@@ -35,7 +35,7 @@ def test_spectral_clustering_n_clusters(clustering_data, n_clusters):
         random_state=42,
     ).fit(X)
     y_pred = sc.labels_
-    adjusted_rand_score(y_true, y_pred)
+    assert adjusted_rand_score(y_true, y_pred) > 0.8
 
 
 @pytest.mark.parametrize("n_neighbors", [5, 10, 20])
