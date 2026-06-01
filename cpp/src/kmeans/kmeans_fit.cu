4c38e75beb4ea935faa0a05081896bfed5ffcabd
tarang-jain <tarangj@nvidia.com>
2026-06-01 12:51:03 -0700

    signatures from header

--- cpp/src/kmeans/kmeans_fit.cu
+++ cpp/src/kmeans/kmeans_fit.cu
@@ -13,28 +13,6 @@
 #include <optional>
 #include <vector>
 
-namespace cuvs::cluster::kmeans::mg {
-
-void fit(raft::resources const& handle,
-         const cuvs::cluster::kmeans::params& params,
-         const std::vector<raft::device_matrix_view<const float, int64_t>>& X_parts,
-         const std::optional<std::vector<raft::device_vector_view<const float, int64_t>>>&
-           sample_weight_parts,
-         raft::device_matrix_view<float, int64_t> centroids,
-         raft::host_scalar_view<float> inertia,
-         raft::host_scalar_view<int64_t> n_iter);
-
-void fit(raft::resources const& handle,
-         const cuvs::cluster::kmeans::params& params,
-         const std::vector<raft::device_matrix_view<const double, int64_t>>& X_parts,
-         const std::optional<std::vector<raft::device_vector_view<const double, int64_t>>>&
-           sample_weight_parts,
-         raft::device_matrix_view<double, int64_t> centroids,
-         raft::host_scalar_view<double> inertia,
-         raft::host_scalar_view<int64_t> n_iter);
-
-}  // namespace cuvs::cluster::kmeans::mg
-
 namespace ML {
 namespace kmeans {
 
