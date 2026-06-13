06a6d0bf61821839647cab49c701730d703716f3
Minh Vu <vuhoangminh97@gmail.com>
2026-06-13 15:32:36 +0200

    Validate random forest n_streams

--- cpp/tests/sg/rf_test.cu
+++ cpp/tests/sg/rf_test.cu
@@ -642,6 +642,27 @@ INSTANTIATE_TEST_CASE_P(RfTests,
                                                                            n_labels,
                                                                            double_precision)));
 
+TEST(RfTests, InvalidNStreams)
+{
+  for (auto n_streams : {0, -1}) {
+    EXPECT_THROW(set_rf_params(3,
+                               100,
+                               1.0,
+                               256,
+                               1,
+                               2,
+                               0.0,
+                               false,
+                               1,
+                               1.0,
+                               0,
+                               CRITERION::MSE,
+                               n_streams,
+                               128),
+                 raft::exception);
+  }
+}
+
 TEST(RfTests, IntegerOverflow)
 {
   std::size_t m = 1000000;
