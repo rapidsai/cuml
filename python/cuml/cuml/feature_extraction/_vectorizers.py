4b0ce3e7a7950394ebc5813dd5a26f79b122879b
Jim Crist-Harif <jcristharif@gmail.com>
2026-06-16 11:22:06 -0500

    `cuml.common.sparsefuncs` -> `cuml.common.sparse`

--- python/cuml/cuml/feature_extraction/_vectorizers.py
+++ python/cuml/cuml/feature_extraction/_vectorizers.py
@@ -13,7 +13,7 @@ from cudf import Series
 from sklearn.exceptions import NotFittedError
 
 import cuml.internals.logger as logger
-from cuml.common.sparsefuncs import csr_row_normalize_l1, csr_row_normalize_l2
+from cuml.common.sparse import csr_row_normalize_l1, csr_row_normalize_l2
 from cuml.feature_extraction._stop_words import ENGLISH_STOP_WORDS
 
 CUPY_SPARSE_DTYPES = [cp.float32, cp.float64, cp.complex64, cp.complex128]
