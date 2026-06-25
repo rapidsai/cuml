cefbf7bdcd7866484549ade35f0694755fba26c0
Jim Crist-Harif <jcristharif@gmail.com>
2026-06-25 11:30:56 -0500

    Respond to feedback

--- python/cuml/cuml/benchmark/gpu_check.py
+++ python/cuml/cuml/benchmark/gpu_check.py
@@ -48,7 +48,7 @@ def get_available_input_types():
         List of available input type strings.
     """
     cpu_types = ["numpy", "pandas"]
-    if is_gpu_available():
+    if is_gpu_available() and is_cuml_available():
         return cpu_types + ["cupy", "cupy-c", "cudf"]
     return cpu_types
 
