4a0991b2d4b132ca24f42276b9aaacbfe932df74
Minh Vu <vuhoangminh97@gmail.com>
2026-07-04 21:33:29 +0200

    Fix benchmark allocator test fixture abstraction in PR #8319

--- cpp/tests/prims/ml_benchmark.cpp
+++ cpp/tests/prims/ml_benchmark.cpp
@@ -9,21 +9,42 @@
 
 #include <gtest/gtest.h>
 
+#include <benchmark/benchmark.h>
+
 #include <cstddef>
 #include <limits>
 
 namespace MLCommon {
 namespace Bench {
 
+class TestFixture : public Fixture {
+ public:
+  TestFixture() : Fixture("MLBenchmarkFixtureTest") {}
+
+  void runBenchmark(::benchmark::State&) override {}
+
+  template <typename T>
+  void testAlloc(T*& ptr, size_t len, bool init = false)
+  {
+    alloc(ptr, len, init);
+  }
+
+  template <typename T>
+  void testDealloc(T* ptr, size_t len)
+  {
+    dealloc(ptr, len);
+  }
+};
+
 TEST(MlBenchmarkFixtureAllocator, ThrowOnHugeAllocationOrDeallocationLength)
 {
-  Fixture fixture("MLBenchmarkAllocatorTest");
+  TestFixture fixture;
 
   int* ptr      = nullptr;
   auto const len = std::numeric_limits<size_t>::max() / sizeof(int) + 1;
 
-  EXPECT_THROW(fixture.alloc(ptr, len), raft::exception);
-  EXPECT_THROW(fixture.dealloc(ptr, len), raft::exception);
+  EXPECT_THROW(fixture.testAlloc(ptr, len), raft::exception);
+  EXPECT_THROW(fixture.testDealloc(ptr, len), raft::exception);
 }
 
 }  // namespace Bench
