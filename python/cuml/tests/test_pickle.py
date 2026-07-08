d8b7806f3bf568dcc9c68d084f56865495108344
Jim Crist-Harif <jcristharif@gmail.com>
2026-07-08 17:52:32 -0500

    Fix `cupy` array pickling with order

--- python/cuml/tests/test_pickle.py
+++ python/cuml/tests/test_pickle.py
@@ -4,6 +4,7 @@
 
 import pickle
 
+import cupy as cp
 import numpy as np
 import pytest
 import scipy.sparse as scipy_sparse
@@ -165,6 +166,18 @@ def make_dataset(datatype, nrows, ncols, n_info):
     y = y.astype(datatype)
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
     return X_train, y_train, X_test
+
+
+def test_cupy_pickle_roundtrip_order():
+    """Test for workaround for https://github.com/cupy/cupy/issues/10084"""
+    X_c = cp.array([[1, 2], [3, 4], [5, 6]], order="C")
+    X_f = cp.array([[1, 2], [3, 4], [5, 6]], order="f")
+
+    X_c2 = pickle.loads(pickle.dumps(X_c))
+    X_f2 = pickle.loads(pickle.dumps(X_f))
+
+    assert X_c2.flags.c_contiguous
+    assert X_f2.flags.f_contiguous
 
 
 @pytest.mark.parametrize("datatype", [np.float32, np.float64])
