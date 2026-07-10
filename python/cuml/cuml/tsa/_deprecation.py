52c54683ad9765f5808378325b04cedb8bbf18e7
Simon Adorf <sadorf@nvidia.com>
2026-07-10 13:16:20 +0000

    Inline TSA removal version in warning

--- python/cuml/cuml/tsa/_deprecation.py
+++ python/cuml/cuml/tsa/_deprecation.py
@@ -3,16 +3,13 @@
 
 import warnings
 
-_REMOVAL_VERSION = "26.12"
-
 
 def warn_deprecated_tsa_api(api_name):
     warnings.warn(
         (
             f"`{api_name}` was deprecated in version 26.08 and will be "
-            f"removed in version {_REMOVAL_VERSION}. `cuml.tsa` was "
-            "deprecated in version 26.08 and will be removed in version "
-            f"{_REMOVAL_VERSION}."
+            "removed in version 26.12. `cuml.tsa` was "
+            "deprecated in version 26.08 and will be removed in version 26.12."
         ),
         FutureWarning,
         stacklevel=2,
