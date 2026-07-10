7258e5b500f710cd54f66ca62c13658562d8da48
Simon Adorf <sadorf@nvidia.com>
2026-07-10 13:25:16 +0000

    Deprecate remaining cuml.tsa functions

--- python/cuml/cuml/tsa/_deprecation.py
+++ python/cuml/cuml/tsa/_deprecation.py
@@ -1,10 +1,13 @@
 # SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
+import functools
 import warnings
 
+from cuml.internals.outputs import in_internal_context
 
-def warn_deprecated_tsa_api(api_name):
+
+def warn_deprecated_tsa_api(api_name, *, stacklevel=2):
     warnings.warn(
         (
             f"`{api_name}` was deprecated in version 26.08 and will be "
@@ -12,5 +15,18 @@ def warn_deprecated_tsa_api(api_name):
             "deprecated in version 26.08 and will be removed in version 26.12."
         ),
         FutureWarning,
-        stacklevel=2,
+        stacklevel=stacklevel,
     )
+
+
+def deprecated_tsa_api(api_name):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            if not in_internal_context():
+                warn_deprecated_tsa_api(api_name, stacklevel=3)
+            return func(*args, **kwargs)
+
+        return wrapped
+
+    return decorator
