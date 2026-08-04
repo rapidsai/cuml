42c4154f90e6e123cc604a1c7b23c4245c00b068
Minh Vu <vuhoangminh97@gmail.com>
2026-08-04 18:06:22 +0200

    Use cupyx error state for kernel ridge solve

--- python/cuml/cuml/kernel_ridge/kernel_ridge.py
+++ python/cuml/cuml/kernel_ridge/kernel_ridge.py
@@ -5,9 +5,10 @@
 import warnings
 
 import cupy as cp
+import cupyx
 import numpy as np
 from cupy import linalg
-from cupyx import geterr, lapack, seterr
+from cupyx import lapack
 
 from cuml.common.doc_utils import generate_docstring
 from cuml.internals.base import Base
@@ -25,19 +26,15 @@ from cuml.metrics import pairwise_kernels
 
 # cholesky solve with fallback to least squares for singular problems
 def _safe_solve(K, y):
-    err_mode = geterr()["linalg"]
     try:
-        try:
+        with cupyx.errstate(linalg="raise"):
             # we need to set the error mode of cupy to raise
             # otherwise we silently get an array of NaNs
-            seterr(linalg="raise")
             dual_coef = lapack.posv(K, y)
             # Perform following check as a workaround for cusolver issue to be
             # fixed in a future CUDA version
             if cp.all(cp.isnan(dual_coef)):
                 raise np.linalg.LinAlgError
-        finally:
-            seterr(linalg=err_mode)
     except np.linalg.LinAlgError:
         warnings.warn(
             "Singular matrix in solving dual problem. Using "
