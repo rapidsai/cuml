7e9970fd6b703a2be8fce74c3df3ac5f46b4c959
James Lamb <jaylamb20@gmail.com>
2026-08-06 19:38:14 -0500

    ensure nightly builds always produce new packages, expand 'changed-files' lists (#8448)
    Contributes to https://github.com/rapidsai/build-planning/issues/218
    
    * uses the new patterns for versioning nightly packages (see https://github.com/rapidsai/shared-workflows/pull/603), to ensure they're always uploaded on new builds. 
    
    For wheels:
    
    ```text
    # before
    26.10.0a55 
    
    # after
    26.10.0a55.post260803200854
    ```
    
    And for conda:
    
    ```text
    # before
    26.10.0a55[build=cuda12_260803_9da146ef]
    
    # after
    26.10.0a55[build=cuda12_260803200854_9da146ef]
    ```
    
    Other changes:
    
    * updates `changed-files` lists to avoid triggering test jobs in PR CI when only `.github/workflows/{build,test}.yaml` are changed
    
    ## Notes for Reviewers
    
    ### How I tested this
    
    See https://github.com/rapidsai/cugraph-gnn/pull/508
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    URL: https://github.com/rapidsai/cuml/pull/8448

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2022-2026, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2022-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 set -euo pipefail
 
 source rapids-configure-sccache
-source rapids-date-string
+source rapids-datetime-string
 
 export CMAKE_GENERATOR=Ninja
 
